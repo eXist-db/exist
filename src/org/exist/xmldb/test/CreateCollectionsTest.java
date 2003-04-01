22711ca39b605c0ffc2fe729bfe44085923bc3ca
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2003-04-01 12:12:48 +0000

    more assertion testing interface Collection
    svn path=/trunk/eXist-1.0/; revision=55
    
    
    Former-commit-id: 9409c2f7b89dc2de6f9c1d31a89ae0aee4edf8e4

--- src/org/exist/xmldb/test/CreateCollectionsTest.java
+++ src/org/exist/xmldb/test/CreateCollectionsTest.java
@@ -2,6 +2,7 @@ package org.exist.xmldb.test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.HashSet;
 
 import junit.framework.TestCase;
 import org.exist.util.XMLFilenameFilter;
@@ -42,34 +43,112 @@ public class CreateCollectionsTest extends TestCase {
 	public void testCreateCollection() {
 		assertNotNull(root);
 		try {
+			System.out.println(
+				"Created Collection: "
+					+ root.getName()
+					+ "( "
+					+ root.getClass()
+					+ " )");
+
+			Service[] services = root.getServices();
+			System.out.println("services array: " + services);
+			assertTrue(
+				"Collection must provide at least one Service",
+				services != null && services.length > 0);
+			System.out.println("  number of services: " + services.length);
+			for (int i = 0; i < services.length; i++) {
+				System.out.println(
+					"  Service: "
+						+ services[i].getName()
+						+ "( "
+						+ services[i].getClass()
+						+ " )");
+			}
+
+			Collection parentCollection = root.getParentCollection();
+			System.out.println("root parentCollection: " + parentCollection);
+			assertNull("root collection has no parent", parentCollection);
+
 			CollectionManagementService service =
 				(CollectionManagementService) root.getService(
 					"CollectionManagementService",
 					"1.0");
 			assertNotNull(service);
 			Collection testCollection = service.createCollection("test");
+
 			assertNotNull(testCollection);
+			int ccc = testCollection.getChildCollectionCount();
+			assertTrue(
+				"Collection just created: ChildCollectionCount==0",
+				ccc == 0);
+			assertTrue(
+				"Collection state should be Open after creation",
+				testCollection.isOpen());
+
 			System.out.println("---------------------------------------");
 			System.out.println("storing files ...");
 			System.out.println("---------------------------------------");
 			File f = new File("samples/shakespeare");
 			File files[] = f.listFiles(new XMLFilenameFilter());
-			XMLResource res;
-			String xml;
+
 			for (int i = 0; i < files.length; i++) {
-				res =
-					(XMLResource) testCollection.createResource(
-						files[i].getName(),
-						"XMLResource");
-				assertNotNull(res);
-				xml = XMLUtil.readFile(files[i], "UTF-8");
-				res.setContent(xml);
-				testCollection.storeResource(res);
+				// XMLResource storeResourceFromFile(File f, Collection col)
+				storeResourceFromFile(files[i], testCollection);
 			}
+
+			HashSet fileNames = new HashSet();
+			for (int i = 0; i < files.length; i++) {
+				String file = files[i].toString();
+				int lastSeparator = file.lastIndexOf(File.separatorChar);
+				fileNames.add(file.substring(lastSeparator + 1));
+			}
+			System.out.println("fileNames: " + fileNames.toString());
+
+			String[] resourcesNames = testCollection.listResources();
+			int resourceCount = testCollection.getResourceCount();
+			System.out.println(
+				"testCollection.getResourceCount()=" + resourceCount);
+			for (int i = 0; i < resourceCount; i++) {
+				System.out.println("resourcesNames[i]=" + resourcesNames[i]);
+				assertTrue(
+					"resourcesNames must contain fileNames just stored",
+					fileNames.contains(resourcesNames[i]));
+			}
+
+			String fileToRemove = "macbeth.xml";
+			Resource resMacbeth = testCollection.getResource(fileToRemove);
+			assertNotNull("getResource(" + fileToRemove + "\")", resMacbeth);
+			testCollection.removeResource(resMacbeth);
+			assertTrue(
+				"After removal resource count must decrease",
+				testCollection.getResourceCount() == resourceCount - 1);
+			// restore the resource just removed :
+			storeResourceFromFile(
+				new File(
+					"samples/shakespeare" + File.separatorChar + fileToRemove),
+				testCollection);
+
 		} catch (XMLDBException e) {
 			fail(e.getMessage());
 		} catch (IOException e) {
 			fail(e.getMessage());
 		}
+	}
+
+	private XMLResource storeResourceFromFile(
+		File file,
+		Collection testCollection)
+		throws XMLDBException, IOException {
+		XMLResource res;
+		String xml;
+		res =
+			(XMLResource) testCollection.createResource(
+				file.getName(),
+				"XMLResource");
+		assertNotNull("storeResourceFromFile", res);
+		xml = XMLUtil.readFile(file, "UTF-8");
+		res.setContent(xml);
+		testCollection.storeResource(res);
+		return res;
 	}
 }
