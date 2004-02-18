b970af6f43f62670c7118b53de6aba471f6a98ae
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2004-02-18 16:02:58 +0000

    more usable
    svn path=/trunk/eXist-1.0/; revision=230
    
    
    Former-commit-id: 2d6917e3ccb67b2288d2066782146f35e93a8b51

--- samples/http/upload.sh
+++ samples/http/upload.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-if [ $# -ne 2 ]
+if [ $# -le 1 ]
 then
-    echo "Usage: upload.sh filename collection"
+    echo "Usage: upload.sh filename collection [curl arguments]"
+    echo "Example: upload.sh test.xml /db/my_collec --user admin:admin"
     exit 1
 fi
 
 DOC=`basename $1`
 URL="http://localhost:8080/exist/servlet$2/$DOC"
 
-curl -i --upload-file $1 $URL
+curl -i --upload-file $1 $URL $3 $4 $5 $6 $7 $8 $9
