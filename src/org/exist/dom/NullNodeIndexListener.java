a3db4d6003a839160f261b6abd996e1d4ea1cca1
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2004-03-02 20:53:59 +0000

    introduce "Null Object" Design Pattern + clean code
    svn path=/trunk/eXist-1.0/; revision=260

---
+++ src/org/exist/dom/NullNodeIndexListener.java
@@ -0,0 +1,14 @@
+/*  $Id$ */
+
+package org.exist.dom;
+
+/** Applies Null Object Design Pattern
+ * @author Jean-Marc Vanel - http:///jmvanel.free.fr */
+public class NullNodeIndexListener implements NodeIndexListener {
+	/** Singleton */
+	public static final NodeIndexListener INSTANCE = new NullNodeIndexListener();
+	/** @see org.exist.dom.NodeIndexListener#nodeChanged(org.exist.dom.NodeImpl) */
+	public void nodeChanged(NodeImpl node) {	}
+	/** @see org.exist.dom.NodeIndexListener#nodeChanged(long, long) */
+	public void nodeChanged(long oldAddress, long newAddress) {	}
+}
