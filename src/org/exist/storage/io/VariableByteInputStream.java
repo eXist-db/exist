fadcaaed11c682c252a26b698fe9f95eac6071e1
Wolfgang Meier <wolfgangmm@gmail.com>
2004-05-03 13:01:35 +0000

    Moved VariableByteOutput classes to their own package. Performance improvements.
    svn path=/trunk/eXist-1.0/; revision=308

---
+++ src/org/exist/storage/io/VariableByteInputStream.java
@@ -0,0 +1,60 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2001-04 Wolfgang M. Meier
+ *  wolfgang@exist-db.org
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * 
+ *  $Id$
+ */
+package org.exist.storage.io;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+
+/**
+ * Implements VariableByteInput on top of an InputStream.
+ *  
+ * @author wolf
+ */
+public class VariableByteInputStream extends AbstractVariableByteInput {
+
+    private InputStream is;
+    
+    /**
+     * 
+     */
+    public VariableByteInputStream(InputStream is) {
+        super();
+        this.is = is;
+    }
+
+    /* (non-Javadoc)
+     * @see java.io.InputStream#read()
+     */
+    public int read() throws IOException {
+        return is.read();
+    }
+    
+    
+    /* (non-Javadoc)
+     * @see java.io.InputStream#available()
+     */
+    public int available() throws IOException {
+        return is.available();
+    } 
+}
