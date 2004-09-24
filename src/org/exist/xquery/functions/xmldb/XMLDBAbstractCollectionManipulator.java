87c12389e662ce38447c26dcaff062b4ca2fe142
Luigi Bai <lpb+github@kandl.houston.tx.us>
2004-09-24 14:00:21 +0000

    Adding some utility functions for manipulating Collections, Resources, and Users.
    svn path=/trunk/eXist-1.0/; revision=643
    
    
    Former-commit-id: 41d5b2c5b93bf7e3eedf9e6aa46af9334e3b742a

---
+++ src/org/exist/xquery/functions/xmldb/XMLDBAbstractCollectionManipulator.java
@@ -0,0 +1,88 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2001-03 Wolfgang M. Meier
+ *  wolfgang@exist-db.org
+ *  http://exist.sourceforge.net
+ *
+ *  Modifications Copyright (C) 2004 Luigi P. Bai
+ *  finder@users.sf.net
+ *  Licensed as below under the LGPL.
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  
+ */
+package org.exist.xquery.functions.xmldb;
+
+import org.xmldb.api.base.Collection;
+import org.xmldb.api.base.XMLDBException;
+
+import org.exist.xmldb.LocalCollection;
+import org.exist.xquery.BasicFunction;
+import org.exist.xquery.Cardinality;
+import org.exist.xquery.FunctionSignature;
+import org.exist.xquery.XPathException;
+import org.exist.xquery.XQueryContext;
+import org.exist.xquery.value.JavaObjectValue;
+import org.exist.xquery.value.Sequence;
+import org.exist.xquery.value.SequenceType;
+import org.exist.xquery.value.Type;
+import org.exist.xquery.value.StringValue;
+
+import java.util.Iterator;
+
+public abstract class XMLDBAbstractCollectionManipulator extends BasicFunction {
+
+	public XMLDBAbstractCollectionManipulator(XQueryContext context, FunctionSignature signature) {
+		super(context, signature);
+	}
+	
+/* (non-Javadoc)
+ * @see org.exist.xquery.BasicFunction#eval(org.exist.xquery.value.Sequence[], org.exist.xquery.value.Sequence)
+ *
+ */
+	
+	public Sequence eval(Sequence[] args, Sequence contextSequence)
+		throws XPathException {
+        
+        if (args.length == 0)
+            throw new XPathException("Expected a collection as the first argument.");
+        
+        // If the incoming is a collection object, use it:
+        Collection collection = null;
+        Object o = args[0].itemAt(0);
+        if (o instanceof JavaObjectValue) {
+            o = ((JavaObjectValue) o).getObject();
+            if (o instanceof Collection)
+                collection = (Collection) o;
+        } 
+        
+        if (collection == null) {
+            // Otherwise, just extract the name as a string:
+            String collectionURI = args[0].getStringValue();
+            try {
+                collection = new LocalCollection(context.getUser(), context.getBroker().getBrokerPool(), collectionURI);
+                collection.getName();
+            } catch (XMLDBException xe) {
+                XPathException xpe = new  XPathException("Could not locate collection "+collectionURI);
+                xpe.initCause(xe);
+                throw xpe;
+            }
+        }
+	
+        return  evalWithCollection(collection, args, contextSequence);
+	}
+
+    abstract protected Sequence evalWithCollection(Collection c, Sequence[] args, Sequence contextSequence) throws XPathException;
+}
