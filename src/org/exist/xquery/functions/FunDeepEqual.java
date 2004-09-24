f558c5d89c105d8b91493a2bb538864335283fa7
Wolfgang Meier <wolfgangmm@gmail.com>
2004-09-24 09:49:32 +0000

    Added fn:deep-equal implementation (by Piotr)
    svn path=/trunk/eXist-1.0/; revision=638
    
    
    Former-commit-id: e00f3afdab80ae18cd7109c84de9b15541959a7d

---
+++ src/org/exist/xquery/functions/FunDeepEqual.java
@@ -0,0 +1,170 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2001-04 Wolfgang M. Meier
+ *  wolfgang@exist-db.org
+ *  http://exist.sourceforge.net
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * 
+ *  $Id$
+ */
+package org.exist.xquery.functions;
+
+import org.exist.dom.NodeProxy;
+import org.exist.dom.QName;
+import org.exist.memtree.AttributeImpl;
+import org.exist.xquery.*;
+import org.exist.xquery.value.*;
+import org.w3c.dom.*;
+import org.w3c.dom.Attr;
+import org.w3c.dom.Node;
+
+/**
+ * Implements the fn:deep-equal library function.
+ *
+ * @author <a href="mailto:piotr@ideanest.com">Piotr Kaminski</a>
+ */
+public class FunDeepEqual extends Function {
+
+	public final static FunctionSignature signature =
+		new FunctionSignature(
+			new QName("deep-equal", Module.BUILTIN_FUNCTION_NS),
+			new SequenceType[] {
+					new SequenceType(Type.ITEM, Cardinality.ZERO_OR_MORE), 
+					new SequenceType(Type.ITEM, Cardinality.ZERO_OR_MORE)
+			},
+			new SequenceType(Type.BOOLEAN, Cardinality.ONE));
+
+	public FunDeepEqual(XQueryContext context) {
+		super(context, signature);
+	}
+
+	public int getDependencies() {
+		return Dependency.CONTEXT_SET | Dependency.CONTEXT_ITEM;
+	}
+	
+	public Sequence eval(Sequence contextSequence, Item contextItem) throws XPathException {
+		Sequence[] args = getArguments(contextSequence, contextItem);
+		int length = args[0].getLength();
+		if (length != args[1].getLength()) return BooleanValue.FALSE;
+		for (int i=0; i<length; i++) {
+			if (!deepEquals(args[0].itemAt(i), args[1].itemAt(i))) return BooleanValue.FALSE;
+		}
+		return BooleanValue.TRUE;
+	}
+	
+	private boolean deepEquals(Item a, Item b) {
+		try {
+			if (Type.subTypeOf(a.getType(), Type.ATOMIC) || Type.subTypeOf(b.getType(), Type.ATOMIC)) {
+				if (!Type.subTypeOf(a.getType(), Type.ATOMIC) || !Type.subTypeOf(b.getType(), Type.ATOMIC)) return false;
+				return a.atomize().compareTo(context.getDefaultCollator(), b.atomize()) == 0;
+			} else {
+				assert Type.subTypeOf(a.getType(), Type.NODE);
+				assert Type.subTypeOf(b.getType(), Type.NODE);
+				if (a.getType() != b.getType()) return false;
+				NodeValue nva = (NodeValue) a, nvb = (NodeValue) b;
+				if (nva.equals(nvb)) return true;		// shortcut!
+				Node na, nb;
+				switch(a.getType()) {
+					case Type.DOCUMENT:
+						// NodeValue.getNode() doesn't seem to work for document nodes
+						na = nva instanceof Node ? (Node) nva : ((NodeProxy) nva).getDocument();
+						nb = nvb instanceof Node ? (Node) nvb : ((NodeProxy) nvb).getDocument();
+						return compareContents(na, nb);
+					case Type.ELEMENT:
+						na = nva.getNode(); nb = nvb.getNode();
+						return compareElements(na, nb);
+					case Type.ATTRIBUTE:
+						na = nva.getNode(); nb = nvb.getNode();
+						return
+							compareNames(na, nb)
+							&& safeEquals(na.getNodeValue(), nb.getNodeValue());
+					case Type.PROCESSING_INSTRUCTION:
+					case Type.NAMESPACE:
+						na = nva.getNode(); nb = nvb.getNode();
+						return
+							safeEquals(na.getNodeName(), nb.getNodeName())
+							&& safeEquals(nva.getStringValue(), nvb.getStringValue());
+					case Type.TEXT:
+					case Type.COMMENT:
+						return safeEquals(nva.getStringValue(), nvb.getStringValue());
+					
+					default: throw new RuntimeException("unexpected item type " + Type.getTypeName(a.getType()));
+				}
+			}
+		} catch (XPathException e) {
+			return false;
+		}
+	}
+	
+	private boolean compareElements(Node a, Node b) {
+		return
+			compareNames(a, b)
+			&& compareAttributes(a, b)
+			&& compareContents(a, b);
+	}
+	
+	private boolean compareContents(Node a, Node b) {
+		a = findNextTextOrElementNode(a.getFirstChild());
+		b = findNextTextOrElementNode(b.getFirstChild());
+		while(!(a == null || b == null)) {
+			if (a.getNodeType() != b.getNodeType()) return false;
+			switch(a.getNodeType()) {
+				case Node.TEXT_NODE:
+					if (!safeEquals(a.getNodeValue(), b.getNodeValue())) return false;
+					break;
+				case Node.ELEMENT_NODE:
+					if (!compareElements(a, b)) return false;
+					break;
+				default:
+					throw new RuntimeException("unexpected node type " + a.getNodeType());
+			}
+			a = findNextTextOrElementNode(a.getNextSibling());
+			b = findNextTextOrElementNode(b.getNextSibling());
+		}
+		return a == b;		// both null
+	}
+	
+	private Node findNextTextOrElementNode(Node n) {
+		while (n != null && !(n.getNodeType() == Node.ELEMENT_NODE || n.getNodeType() == Node.TEXT_NODE)) {
+			n = n.getNextSibling();
+		}
+		return n;
+	}
+	
+	private boolean compareAttributes(Node a, Node b) {
+		NamedNodeMap nnma = a.getAttributes(), nnmb = b.getAttributes();
+		if (nnma.getLength() != nnmb.getLength()) return false;
+		for (int i=0; i<nnma.getLength(); i++) {
+			Node ta = nnma.item(i);
+			Node tb = ta.getLocalName() == null ? nnmb.getNamedItem(ta.getNodeName()) : nnmb.getNamedItemNS(ta.getNamespaceURI(), ta.getLocalName());
+			if (tb == null || !safeEquals(ta.getNodeValue(), tb.getNodeValue())) return false;
+		}
+		return true;
+	}
+	
+	private boolean compareNames(Node a, Node b) {
+		if (a.getLocalName() != null || b.getLocalName() != null) {
+			return safeEquals(a.getNamespaceURI(), b.getNamespaceURI()) && safeEquals(a.getLocalName(), b.getLocalName());
+		} else {
+			return safeEquals(a.getNodeName(), b.getNodeName());
+		}
+	}
+	
+	private boolean safeEquals(Object a, Object b) {
+		return a == null ? b == null : a.equals(b);
+	}
+
+}
---
+++ src/org/exist/xquery/test/DeepEqualTest.java
@@ -0,0 +1,295 @@
+package org.exist.xquery.test;
+
+import org.exist.xmldb.CollectionManagementServiceImpl;
+import org.exist.xmldb.RemoteCollectionManagementService;
+import org.xmldb.api.DatabaseManager;
+import org.xmldb.api.base.*;
+import org.xmldb.api.base.Database;
+import org.xmldb.api.base.XMLDBException;
+import org.xmldb.api.modules.*;
+import org.xmldb.api.modules.CollectionManagementService;
+import org.xmldb.api.modules.XMLResource;
+
+import junit.framework.TestCase;
+import junit.textui.TestRunner;
+
+
+public class DeepEqualTest extends TestCase {
+
+	private final static String URI = "xmldb:exist:///db";
+	private final static String DRIVER = "org.exist.xmldb.DatabaseImpl";
+
+	private XPathQueryService query;
+	private Collection c;
+	
+	public static void main(String[] args) {
+		TestRunner.run(DeepEqualTest.class);
+	}
+	
+	public DeepEqualTest(String name) {
+		super(name);
+	}
+	
+	public void testAtomic1() throws XMLDBException {
+		assertQuery(true, "deep-equal('hello', 'hello')");
+	}
+	
+	public void testAtomic2() throws XMLDBException {
+		assertQuery(false, "deep-equal('hello', 'goodbye')");
+	}
+	
+	public void testAtomic3() throws XMLDBException {
+		assertQuery(true, "deep-equal(42, 42)");
+	}
+	
+	public void testAtomic4() throws XMLDBException {
+		assertQuery(false, "deep-equal(42, 17)");
+	}
+	
+	public void testAtomic5() throws XMLDBException {
+		assertQuery(false, "deep-equal(42, 'hello')");
+	}
+	
+	public void testEmptySeq() throws XMLDBException {
+		assertQuery(true, "deep-equal((), ())");
+	}
+	
+	public void testDiffLengthSeq1() throws XMLDBException {
+		assertQuery(false, "deep-equal((), 42)");
+	}
+	
+	public void testDiffLengthSeq2() throws XMLDBException {
+		assertQuery(false, "deep-equal((), (42, 'hello'))");
+	}
+	
+	public void testDiffKindNodes1() throws XMLDBException {
+		createDocument("test", "<test key='value'>hello</test>");
+		assertQuery(false, "deep-equal(/test, /test/@key)");
+	}
+	
+	public void testDiffKindNodes2() throws XMLDBException {
+		createDocument("test", "<test key='value'>hello</test>");
+		assertQuery(false, "deep-equal(/test, /test/text())");
+	}
+	
+	public void testDiffKindNodes3() throws XMLDBException {
+		createDocument("test", "<test key='value'>hello</test>");
+		assertQuery(false, "deep-equal(/test/@key, /test/text())");
+	}
+	
+	public void testSameNode1() throws XMLDBException {
+		createDocument("test", "<test key='value'>hello</test>");
+		assertQuery(true, "deep-equal(/test, /test)");
+	}
+		
+	public void testSameNode2() throws XMLDBException {
+		createDocument("test", "<test key='value'>hello</test>");
+		assertQuery(true, "deep-equal(/test/@key, /test/@key)");
+	}
+		
+	public void testSameNode3() throws XMLDBException {
+		createDocument("test", "<test key='value'>hello</test>");
+		assertQuery(true, "deep-equal(/test/text(), /test/text())");
+	}
+	
+	public void testDocuments1() throws XMLDBException {
+		createDocument("test1", "<test key='value'>hello</test>");
+		createDocument("test2", "<test key='value'>hello</test>");
+		assertQuery(true, "deep-equal(document('test1'), document('test2'))");
+	}
+	
+	public void testDocuments2() throws XMLDBException {
+		createDocument("test1", "<test key='value'>hello</test>");
+		createDocument("test2", "<notatest/>");
+		assertQuery(false, "deep-equal(document('test1'), document('test2'))");
+	}
+	
+	public void testText1() throws XMLDBException {
+		createDocument("test", "<test><g1><a>1</a><b>2</b></g1><g2><c>1</c><d>2</d></g2></test>");
+		assertQuery(true, "deep-equal(//a/text(), //c/text())");
+	}
+	
+	public void testText2() throws XMLDBException {
+		createDocument("test", "<test><g1><a>1</a><b>2</b></g1><g2><c>1</c><d>2</d></g2></test>");
+		assertQuery(false, "deep-equal(//a/text(), //b/text())");
+	}
+	
+	public void testText3() throws XMLDBException {
+		createDocument("test", "<test><g1><a>1</a><b>2</b></g1><g2><c>1</c><d>2</d></g2></test>");
+		assertQuery(true, "deep-equal(//g1/text(), //g2/text())");
+	}
+	
+	public void testText4() throws XMLDBException {
+		createDocument("test", "<test><a>12</a><b>1<!--blah-->2</b></test>");
+		assertQuery(false, "deep-equal(//a/text(), //b/text())");
+	}
+	
+	public void testAttributes1() throws XMLDBException {
+		createDocument("test", "<test><e1 a='1'/><e2 a='1' b='2' c='1'/><e3 a='2'/></test>");
+		assertQuery(true, "deep-equal(//e1/@a, //e2/@a)");
+	}
+	
+	public void testAttributes2() throws XMLDBException {
+		createDocument("test", "<test><e1 a='1'/><e2 a='1' b='2' c='1'/><e3 a='2'/></test>");
+		assertQuery(false, "deep-equal(//e1/@a, //e2/@b)");
+	}
+	
+	public void testAttributes3() throws XMLDBException {
+		createDocument("test", "<test><e1 a='1'/><e2 a='1' b='2' c='1'/><e3 a='2'/></test>");
+		assertQuery(false, "deep-equal(//e1/@a, //e2/@c)");
+	}
+	
+	public void testAttributes4() throws XMLDBException {
+		createDocument("test", "<test><e1 a='1'/><e2 a='1' b='2' c='1'/><e3 a='2'/></test>");
+		assertQuery(false, "deep-equal(//e1/@a, //e3/@a)");
+	}
+	
+	public void testNSAttributes1() throws XMLDBException {
+		createDocument("test", "<test xmlns:n='urn:blah' xmlns:p='urn:foo' xmlns:q='urn:blah'><e1 n:a='1'/><e2 n:a='1' p:a='1' p:b='1'/><e3 n:a='2'/><e4 q:a='1'/></test>");
+		assertQuery(true, "declare namespace n = 'urn:blah'; declare namespace p = 'urn:foo'; declare namespace q = 'urn:blah'; deep-equal(//e1/@n:a, //e2/@n:a)");
+	}
+	
+	public void testNSAttributes2() throws XMLDBException {
+		createDocument("test", "<test xmlns:n='urn:blah' xmlns:p='urn:foo' xmlns:q='urn:blah'><e1 n:a='1'/><e2 n:a='1' p:a='1' p:b='1'/><e3 n:a='2'/><e4 q:a='1'/></test>");
+		assertQuery(true, "declare namespace n = 'urn:blah'; declare namespace p = 'urn:foo'; declare namespace q = 'urn:blah'; deep-equal(//e1/@q:a, //e4/@n:a)");
+	}
+	
+	public void testNSAttributes3() throws XMLDBException {
+		createDocument("test", "<test xmlns:n='urn:blah' xmlns:p='urn:foo' xmlns:q='urn:blah'><e1 n:a='1'/><e2 n:a='1' p:a='1' p:b='1'/><e3 n:a='2'/><e4 q:a='1'/></test>");
+		assertQuery(false, "declare namespace n = 'urn:blah'; declare namespace p = 'urn:foo'; declare namespace q = 'urn:blah'; deep-equal(//e1/@n:a, //e2/@p:a)");
+	}
+	
+	public void testNSAttributes4() throws XMLDBException {
+		createDocument("test", "<test xmlns:n='urn:blah' xmlns:p='urn:foo' xmlns:q='urn:blah'><e1 n:a='1'/><e2 n:a='1' p:a='1' p:b='1'/><e3 n:a='2'/><e4 q:a='1'/></test>");
+		assertQuery(false, "declare namespace n = 'urn:blah'; declare namespace p = 'urn:foo'; declare namespace q = 'urn:blah'; deep-equal(//e1/@n:a, //e2/@p:b)");
+	}
+	
+	public void testNSAttributes5() throws XMLDBException {
+		createDocument("test", "<test xmlns:n='urn:blah' xmlns:p='urn:foo' xmlns:q='urn:blah'><e1 n:a='1'/><e2 n:a='1' p:a='1' p:b='1'/><e3 n:a='2'/><e4 q:a='1'/></test>");
+		assertQuery(false, "declare namespace n = 'urn:blah'; declare namespace p = 'urn:foo'; declare namespace q = 'urn:blah'; deep-equal(//e1/@n:a, //e3/@n:a)");
+	}
+	
+	public void testElements1() throws XMLDBException {
+		createDocument("test", "<test><a/><a/></test>");
+		assertQuery(true, "deep-equal(/test/*[1], /test/*[2])");
+	}
+	
+	public void testElements2() throws XMLDBException {
+		createDocument("test", "<test><a/><b/></test>");
+		assertQuery(false, "deep-equal(/test/*[1], /test/*[2])");
+	}
+	
+	public void testElements3() throws XMLDBException {
+		createDocument("test", "<test><a a='1' b='2'/><a b='2' a='1'/></test>");
+		assertQuery(true, "deep-equal(/test/*[1], /test/*[2])");
+	}
+	
+	public void testElements4() throws XMLDBException {
+		createDocument("test", "<test><a a='1'/><a b='2' a='1'/></test>");
+		assertQuery(false, "deep-equal(/test/*[1], /test/*[2])");
+	}
+	
+	public void testElements5() throws XMLDBException {
+		createDocument("test", "<test><a a='1' c='2'/><a b='2' a='1'/></test>");
+		assertQuery(false, "deep-equal(/test/*[1], /test/*[2])");
+	}
+	
+	public void testElements6() throws XMLDBException {
+		createDocument("test", "<test><a a='1' b='2'/><a a='2' b='2'/></test>");
+		assertQuery(false, "deep-equal(/test/*[1], /test/*[2])");
+	}
+	
+	public void testElements7() throws XMLDBException {
+		createDocument("test", "<test><a>hello</a><a>hello</a></test>");
+		assertQuery(true, "deep-equal(/test/*[1], /test/*[2])");
+	}
+	
+	public void testElements8() throws XMLDBException {
+		createDocument("test", "<test><a>hello</a><a>bye</a></test>");
+		assertQuery(false, "deep-equal(/test/*[1], /test/*[2])");
+	}
+	
+	public void testElements9() throws XMLDBException {
+		createDocument("test", "<test><a><!--blah--></a><a/></test>");
+		assertQuery(true, "deep-equal(/test/*[1], /test/*[2])");
+	}
+	
+	public void testElements10() throws XMLDBException {
+		createDocument("test", "<test><a><b/><!--blah-->hello</a><a><b/>hello</a></test>");
+		assertQuery(true, "deep-equal(/test/*[1], /test/*[2])");
+	}
+	
+	public void testElements11() throws XMLDBException {
+		createDocument("test", "<test><a><b/>hello</a><a>hello</a></test>");
+		assertQuery(false, "deep-equal(/test/*[1], /test/*[2])");
+	}
+	
+	public void testElements12() throws XMLDBException {
+		createDocument("test", "<test><a><b/></a><a>hello</a></test>");
+		assertQuery(false, "deep-equal(/test/*[1], /test/*[2])");
+	}
+	
+	public void testElements13() throws XMLDBException {
+		createDocument("test", "<test><a><b/></a><a><b/>hello</a></test>");
+		assertQuery(false, "deep-equal(/test/*[1], /test/*[2])");
+	}
+	
+	public void testNSElements1() throws XMLDBException {
+		createDocument("test", "<test xmlns:p='urn:foo' xmlns:q='urn:foo'><p:a/><q:a/></test>");
+		assertQuery(true, "deep-equal(/test/*[1], /test/*[2])");
+	}
+	
+	public void testNSElements2() throws XMLDBException {
+		createDocument("test", "<test xmlns:p='urn:foo' xmlns:q='urn:bar'><p:a/><q:a/></test>");
+		assertQuery(false, "deep-equal(/test/*[1], /test/*[2])");
+	}
+	
+	private void assertQuery(boolean expected, String q) throws XMLDBException {
+		ResourceSet rs = query.query(q);
+		assertEquals(1, rs.getSize());
+		assertEquals(Boolean.toString(expected), rs.getResource(0).getContent());
+	}
+	
+	private XMLResource createDocument(String name, String content) throws XMLDBException {
+		XMLResource res = (XMLResource) c.createResource(name, XMLResource.RESOURCE_TYPE);
+		res.setContent(content);
+		c.storeResource(res);
+		return res;
+	}
+
+	private Collection setupTestCollection() throws XMLDBException {
+		Collection root = DatabaseManager.getCollection(URI, "admin", "");
+		CollectionManagementService rootcms = (CollectionManagementService) root.getService("CollectionManagementService", "1.0");
+		Collection c = root.getChildCollection("test");
+		if(c != null) rootcms.removeCollection("test");
+		rootcms.createCollection("test");
+		c = DatabaseManager.getCollection(URI+"/test", "admin", "");
+		assertNotNull(c);
+		return c;
+	}
+
+	protected void setUp() {
+		try {
+			// initialize driver
+			Database database = (Database) Class.forName(DRIVER).newInstance();
+			database.setProperty("create-database", "true");
+			DatabaseManager.registerDatabase(database);
+			
+			c = setupTestCollection();
+			query = (XPathQueryService) c.getService("XPathQueryService", "1.0");
+
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new RuntimeException("failed setup", e);
+		}
+	}
+	
+	protected void tearDown() {
+		try {
+			if (c != null) c.close();
+		} catch (XMLDBException e) {
+			throw new RuntimeException("failed teardown", e);
+		}
+	}
+	
+}
