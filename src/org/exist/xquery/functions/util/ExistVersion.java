9949eabee58c428b2d70810ed3b056670f299f43
Giulio Valentino <gvalentino>
2004-10-05 14:37:39 +0000

    add util:eXist-version()
    svn path=/trunk/eXist-1.0/; revision=686

---
+++ src/org/exist/xquery/functions/util/ExistVersion.java
@@ -0,0 +1,65 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2001-03 Wolfgang M. Meier
+ *  wolfgang@exist-db.org
+ *  http://exist.sourceforge.net
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  
+ *  $Id$
+ */
+package org.exist.xquery.functions.util;
+
+import org.exist.dom.QName;
+import org.exist.xquery.Cardinality;
+import org.exist.xquery.Function;
+import org.exist.xquery.FunctionSignature;
+import org.exist.xquery.XQueryContext;
+import org.exist.xquery.value.Item;
+import org.exist.xquery.value.Sequence;
+import org.exist.xquery.value.SequenceType;
+import org.exist.xquery.value.Type;
+import org.exist.xquery.value.StringValue;
+
+/**
+ * Return the eXist version
+ * 
+ * @author wolf
+ */
+public class ExistVersion extends Function {
+
+	public final static FunctionSignature signature =
+		new FunctionSignature(
+			new QName("eXist-version", UtilModule.NAMESPACE_URI, UtilModule.PREFIX),
+			"Return the eXist version.",
+			null,
+			new SequenceType(Type.STRING, Cardinality.EXACTLY_ONE));
+
+	public ExistVersion(XQueryContext context) {
+		super(context, signature);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.exist.xquery.Expression#eval(org.exist.dom.DocumentSet, org.exist.xquery.value.Sequence, org.exist.xquery.value.Item)
+	 */
+	public Sequence eval(
+		Sequence contextSequence,
+		Item contextItem)
+		{
+		
+		return new StringValue("1.0 beta rc2");
+	}
+
+}
