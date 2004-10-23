b5326beb373e4c3a7c87dc9fe45cbf2403e1d25c
Wolfgang Meier <wolfgangmm@gmail.com>
2004-10-23 10:37:26 +0000

    Removed old jetty dir. New jetty is in tools/jetty.
    svn path=/trunk/eXist-1.0/; revision=753

--- Jetty-4.1.4/cgi-bin/env.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-echo "Content-Type: text/html"
-echo
-echo "<H1>CGI Environment</H1>"
-echo "<PRE>"
-set 2>&1
-echo "</PRE>"
-
--- Jetty-4.1.4/cgi-bin/redirect.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# Test redirecting from CGI
-# $Id: redirect.sh,v 1.1 2001/07/14 20:04:47 bretts Exp $
-echo "Status: 302 Moved"
-echo "Location: http://${HTTP_HOST}/"
-echo
