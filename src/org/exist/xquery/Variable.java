92e0e66db67be9bf8294c0ba49e4bf985acf54e8
Wolfgang Meier <wolfgangmm@gmail.com>
2004-12-18 11:49:19 +0000

    wrong dependency returned by getDependencies()
    svn path=/trunk/eXist-1.0/; revision=907

--- src/org/exist/xquery/Variable.java
+++ src/org/exist/xquery/Variable.java
@@ -80,7 +80,7 @@ public class Variable {
 //			return Dependency.CONTEXT_SET + Dependency.LOCAL_VARS;
 		
 		if(context.getCurrentStackSize() > positionInStack)
-			return Dependency.CONTEXT_SET + Dependency.CONTEXT_ITEM;
+			return Dependency.CONTEXT_SET;
 		else
 			return Dependency.CONTEXT_SET + Dependency.LOCAL_VARS;
 	}
