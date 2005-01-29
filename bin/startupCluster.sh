69aa5bf2d09bb0340d96a2eb5fe3d1128c479f71
Francesco Mondora <francesco.mondora@gmail.com>
2005-01-29 15:00:38 +0000

    Added unix bash script for starting up a cluster version
    svn path=/trunk/eXist-1.0/; revision=988
    
    
    Former-commit-id: 5e6352cc51c3ac5d1ca25ce85b793ac52bcc0f07

---
+++ bin/startupCluster.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+# -----------------------------------------------------------------------------
+# startup.sh - Start Script for Jetty + eXist
+#
+# $Id$
+# -----------------------------------------------------------------------------
+
+exist_home () {
+	case "$0" in
+		/*)
+			p=$0
+		;;
+		*)
+			p=`/bin/pwd`/$0
+		;;
+	esac
+		(cd `/usr/bin/dirname $p` ; /bin/pwd)
+}
+
+if [ -z "$EXIST_HOME" ]; then
+	EXIST_HOME_1=`exist_home`
+	EXIST_HOME="$EXIST_HOME_1/.."
+fi
+
+if [ ! -f "$EXIST_HOME/start.jar" ]; then
+	echo "Unable to find start.jar. Please set EXIST_HOME to point to your installation directory."
+	exit 1
+fi
+
+OPTIONS="-Dexist.home=$EXIST_HOME"
+
+if [ -n "$JETTY_HOME" ]; then
+	OPTIONS="-Djetty.home=$JETTY_HOME $OPTIONS"
+fi
+
+# save LANG
+if [ -n "$LANG" ]; then
+	OLD_LANG="$LANG"
+fi
+# set LANG to UTF-8
+LANG=en_US.UTF-8
+
+if [ -z "$JAVA_OPTIONS" ]; then
+    export JAVA_OPTIONS="-Xms16000k -Xmx128000k -Dfile.encoding=UTF-8"
+fi
+
+JAVA_ENDORSED_DIRS="$EXIST_HOME"/lib/endorsed
+#DEBUG_START="-Dexist.start.debug=true"
+
+$JAVA_HOME/bin/java $JAVA_OPTIONS -Djava.endorsed.dirs=$JAVA_ENDORSED_DIRS \
+	$DEBUG_START $OPTIONS -jar "$EXIST_HOME/start.jar" \
+	cluster $*
+
+if [ -n "$OLD_LANG" ]; then
+	LANG="$OLD_LANG"
+fi
