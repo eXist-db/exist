d57251c91c1d47501b9f8376634f1e6dd675d72f
Giulio Valentino <gvalentino>
2005-02-07 11:51:44 +0000

    *** empty log message ***
    svn path=/trunk/eXist-1.0/; revision=1029
    
    
    Former-commit-id: b278d9a92b20384de89145aa9ef5190275ec41dd

---
+++ bin/startup_cluster.bat
@@ -0,0 +1,28 @@
+@echo off
+if not "%JAVA_HOME%" == "" goto gotJavaHome
+echo Java environment not found. Please set
+echo your JAVA_HOME environment variable to
+echo the home of your JDK.
+goto :eof
+
+:gotJavaHome
+if not "%EXIST_HOME%" == "" goto gotExistHome
+
+rem try to guess
+set EXIST_HOME=.
+if exist %EXIST_HOME%\start.jar goto gotExistHome
+set EXIST_HOME=..
+if exist %EXIST_HOME%\start.jar goto gotExistHome
+
+echo EXIST_HOME not found. Please set your
+echo EXIST_HOME environment variable to the
+echo home directory of eXist.
+goto :eof
+
+:gotExistHome
+set JAVA_ENDORSED_DIRS="%EXIST_HOME%"\lib\endorsed
+set JAVA_OPTS="-Xms16000k -Xmx128000k -Dfile.encoding=UTF-8 -Djava.endorsed.dirs=%JAVA_ENDORSED_DIRS%"
+
+%JAVA_HOME%\bin\java "%JAVA_OPTS%"  -Dexist.home="%EXIST_HOME%" -jar "%EXIST_HOME%\start.jar" cluster %1
+:eof
+
