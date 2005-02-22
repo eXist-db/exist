eab3232a480c77326913873274e9d12718d2b9e7
Wolfgang Meier <wolfgangmm@gmail.com>
2005-02-22 16:36:56 +0000

    *** empty log message ***
    svn path=/trunk/eXist-1.0/; revision=1072
    
    
    Former-commit-id: 57314184a1de6a62330d8a014d028879c8c5ffbb

--- src/org/exist/util/UTF8.java
+++ src/org/exist/util/UTF8.java
@@ -336,5 +336,51 @@ public class UTF8 {
 
 		return bytlen;
 	}
+    
+    /**
+     * Static method to generate the UTF-8 representation of a Unicode character.
+     * This particular code is taken from saxon (see http://saxon.sf.net).
+     * 
+     * @param in the Unicode character, or the high half of a surrogate pair
+     * @param in2 the low half of a surrogate pair (ignored unless the first argument is in the
+     * range for a surrogate pair)
+     * @param out an array of at least 4 bytes to hold the UTF-8 representation.
+     * @return the number of bytes in the UTF-8 representation
+     */
+     public static int getUTF8Encoding(char in, char in2, byte[] out) {
+         // See Tony Graham, "Unicode, a Primer", page 92
+         int i = (int)in;
+         if (i<=0x7f) {
+             out[0] = (byte)i;
+             return 1;
+         } else if (i<=0x7ff) {
+             out[0] = (byte)(0xc0 | ((in >> 6) & 0x1f));
+             out[1] = (byte)(0x80 | (in & 0x3f));
+             return 2;
+         } else if (i>=0xd800 && i<=0xdbff) {
+             // surrogate pair
+             int j = (int)in2;
+             if (!(j>=0xdc00 && j<=0xdfff)) {
+                 throw new IllegalArgumentException("Malformed Unicode Surrogate Pair (" + i + "," + j + ")");
+             }
+             byte xxxxxx = (byte)(j & 0x3f);
+             byte yyyyyy = (byte)(((i & 0x03) << 4) | ((j >> 6) & 0x0f));
+             byte zzzz = (byte)((i >> 2) & 0x0f);
+             byte uuuuu = (byte)(((i >> 6) & 0x0f) + 1);
+             out[0] = (byte)(0xf0 | ((uuuuu >> 2) & 0x07));
+             out[1] = (byte)(0x80 | ((uuuuu & 0x03) << 4) | zzzz);
+             out[2] = (byte)(0x80 | yyyyyy);
+             out[3] = (byte)(0x80 | xxxxxx);
+             return 4;
+         } else if (i>=0xdc00 && i<=0xdfff) {
+             // second half of surrogate pair - ignore it
+             return 0;
+         } else {
+             out[0] = (byte)(0xe0 | ((in >> 12) & 0x0f));
+             out[1] = (byte)(0x80 | ((in >> 6) & 0x3f));
+             out[2] = (byte)(0x80 | (in & 0x3f));
+             return 3;
+         }
+     }
 
 }
