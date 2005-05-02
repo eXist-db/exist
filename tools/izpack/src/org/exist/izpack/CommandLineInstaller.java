27d7ab0e343eadc1a5d766345ea7d5b170d3e590
Wolfgang Meier <wolfgangmm@gmail.com>
2005-05-02 12:33:34 +0000

    Added a command-line mode to the installer for installation on headless systems.
    svn path=/trunk/eXist-1.0/; revision=1212
    
    
    Former-commit-id: 1a148cc646fdc851fa6753bc99ff51ecffe06fb0

---
+++ tools/izpack/src/org/exist/izpack/CommandLineInstaller.java
@@ -0,0 +1,135 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2001-04 The eXist Team
+ *
+ *  http://exist-db.org
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  
+ *  $Id$
+ */
+package org.exist.izpack;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.Writer;
+import java.util.List;
+
+import org.apache.avalon.excalibur.cli.CLArgsParser;
+import org.apache.avalon.excalibur.cli.CLOption;
+import org.apache.avalon.excalibur.cli.CLOptionDescriptor;
+import org.apache.avalon.excalibur.cli.CLUtil;
+
+import com.izforge.izpack.installer.AutomatedInstaller;
+
+/**
+ * @author wolf
+ *
+ */
+public class CommandLineInstaller {
+
+	private final static int HELP_OPT = 'h';
+	private final static int PATH_OPT = 'p';
+	
+	private final static CLOptionDescriptor OPTIONS[] = new CLOptionDescriptor[] {
+        new CLOptionDescriptor( "help", CLOptionDescriptor.ARGUMENT_DISALLOWED,
+				HELP_OPT, "print help on command line options and exit." ),
+        new CLOptionDescriptor( "path", CLOptionDescriptor.ARGUMENT_REQUIRED,
+				PATH_OPT, "the directory where eXist should be installed.")
+	};
+	
+	/**
+	 * 
+	 */
+	public CommandLineInstaller() {
+	}
+
+	protected void execute(String[] args) throws Exception {
+		if (args.length == 0 && !System.getProperty("os.name").startsWith("OpenServer")) {
+			System.out.println("\neXist Installation");
+			System.out.println("------------------------");
+			System.out.println("Using GUI mode ...");
+			System.out.println("On a headless system, add parameter -p install-path");
+			System.out.println("to install without GUI.\n");
+ 			
+ 			// can't load the GUIInstaller class on headless machines,
+ 			// so we use Class.forName to force lazy loading.
+ 			Class.forName("com.izforge.izpack.installer.GUIInstaller").newInstance();
+ 		} else {
+			CLArgsParser optParser = new CLArgsParser( args, OPTIONS );
+			if(optParser.getErrorString() != null) {
+	            System.err.println( "ERROR: " + optParser.getErrorString());
+	            return;
+	        }
+	        List opt = optParser.getArguments();
+			int size = opt.size();
+			String installPath = System.getProperty("user.home") + "/eXist";
+	        CLOption option;
+			for(int i = 0; i < size; i++) {
+	            option = (CLOption)opt.get(i);
+	            switch(option.getId()) {
+	                case HELP_OPT :
+	                    printHelp();
+	                    return;
+	                case PATH_OPT :
+						installPath = option.getArgument();
+						break;
+	            }
+			}
+			
+			String filename = File.createTempFile("inst", ".xml").toString();
+			Writer w = new BufferedWriter(new FileWriter(filename));
+			w.write("<AutomatedInstallation langpack=\"eng\">\n");
+			w.write("<com.izforge.izpack.panels.HelloPanel/>\n" + 
+					"    <com.izforge.izpack.panels.PacksPanel>\n" + 
+					"        <selected>\n" + 
+					"            <pack index=\"0\"/>\n" + 
+					"            <pack index=\"1\"/>\n" + 
+					"            <pack index=\"2\"/>\n" + 
+					"        </selected>\n" + 
+					"    </com.izforge.izpack.panels.PacksPanel>\n");
+			w.write("<com.izforge.izpack.panels.TargetPanel>\n" + 
+					"        <installpath>" + installPath + "</installpath>\n" + 
+					"    </com.izforge.izpack.panels.TargetPanel>\n");
+			w.write("<com.izforge.izpack.panels.InstallPanel/>\n");
+			w.write("<com.izforge.izpack.panels.FinishPanel/>\n");
+			w.write("</AutomatedInstallation>");
+			w.close();
+			
+			new AutomatedInstaller(filename);
+			
+			new File(filename).delete();
+ 		}
+	}
+	
+	private static void printHelp() {
+        System.out.println("Usage: java " + CommandLineInstaller.class.getName() + " [options]");
+        System.out.println(CLUtil.describeOptions(OPTIONS).toString());
+    }
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		CommandLineInstaller inst = new CommandLineInstaller();
+		try {
+			inst.execute(args);
+		} catch (Exception e) {
+			System.err.println("Installer exited with an error: " + e.getMessage());
+		}
+	}
+
+}
