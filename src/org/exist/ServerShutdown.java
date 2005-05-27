e8ddee7bcd9cc34d29bcf8493f9e461fe57b776b
Wolfgang Meier <wolfgangmm@gmail.com>
2005-05-27 20:51:00 +0000

    ServerShutdown should try to obtain the server's database URI from client.properties if the file is found in the path.
    svn path=/trunk/eXist-1.0/; revision=1282

--- src/org/exist/ServerShutdown.java
+++ src/org/exist/ServerShutdown.java
@@ -5,7 +5,13 @@
  */
 package org.exist;
 
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
+import java.util.Properties;
 
 import org.apache.avalon.excalibur.cli.CLArgsParser;
 import org.apache.avalon.excalibur.cli.CLOption;
@@ -24,83 +30,118 @@ import org.xmldb.api.base.XMLDBException;
  */
 public class ServerShutdown {
 
-	//	command-line options
-	private final static int HELP_OPT = 'h';
-	private final static int URI_OPT = 'l';
-	private final static int USER_OPT = 'u';
-	private final static int PASSWORD_OPT = 'p';
+    // command-line options
+    private final static int HELP_OPT = 'h';
 
-	private final static CLOptionDescriptor OPTIONS[] =
-		new CLOptionDescriptor[] {
-			new CLOptionDescriptor(
-				"help",
-				CLOptionDescriptor.ARGUMENT_DISALLOWED,
-				HELP_OPT,
-				"print help on command line options and exit."),
-			new CLOptionDescriptor(
-				"user",
-				CLOptionDescriptor.ARGUMENT_REQUIRED,
-				USER_OPT,
-				"specify username (has to be a member of group dba)."),
-			new CLOptionDescriptor(
-				"password",
-				CLOptionDescriptor.ARGUMENT_REQUIRED,
-				PASSWORD_OPT,
-				"specify password for the user."),
-			new CLOptionDescriptor(
-				"uri",
-				CLOptionDescriptor.ARGUMENT_REQUIRED,
-				URI_OPT,
-				"the XML:DB URI of the database instance to be shut down.")};
+    private final static int URI_OPT = 'l';
 
-	public static void main(String[] args) {
-		CLArgsParser optParser = new CLArgsParser(args, OPTIONS);
-		if (optParser.getErrorString() != null) {
-			System.err.println("ERROR: " + optParser.getErrorString());
-			return;
-		}
-		String user = "admin";
-		String passwd = "";
-		String uri = "xmldb:exist://localhost:8080/exist/xmlrpc";
-		List opt = optParser.getArguments();
-		int size = opt.size();
-		CLOption option;
-		for(int i = 0; i < size; i++) {
-			option = (CLOption)opt.get(i);
-			switch(option.getId()) {
-				case HELP_OPT:
-					System.out.println("Usage: java " + ServerShutdown.class.getName() + " [options]");
-					System.out.println(CLUtil.describeOptions(OPTIONS).toString());
-					return;
-				case USER_OPT:
-					user = option.getArgument();
-					break;
-				case PASSWORD_OPT:
-					passwd = option.getArgument();
-					break;
-				case URI_OPT:
-					uri = option.getArgument();
-			}
-		}
-		try {
-			// initialize database drivers
-			Class cl = Class.forName("org.exist.xmldb.DatabaseImpl");
-			// create the default database
-			Database database = (Database) cl.newInstance();
-			DatabaseManager.registerDatabase(database);
-			if(!uri.endsWith("/db"))
-				uri = uri + "/db";
-			Collection root = DatabaseManager.getCollection(uri, user, passwd);
-			DatabaseInstanceManager manager = (DatabaseInstanceManager)
-				root.getService("DatabaseInstanceManager", "1.0");
-			System.out.println("Shutting down database instance at ");
-			System.out.println('\t' + uri);
-			manager.shutdown();
-		} catch(XMLDBException e) {
-			System.err.println("ERROR: " + e.getMessage());
-			e.printStackTrace();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-	}
+    private final static int USER_OPT = 'u';
+
+    private final static int PASSWORD_OPT = 'p';
+
+    private final static CLOptionDescriptor OPTIONS[] = new CLOptionDescriptor[] {
+            new CLOptionDescriptor("help",
+                    CLOptionDescriptor.ARGUMENT_DISALLOWED, HELP_OPT,
+                    "print help on command line options and exit."),
+            new CLOptionDescriptor("user",
+                    CLOptionDescriptor.ARGUMENT_REQUIRED, USER_OPT,
+                    "specify username (has to be a member of group dba)."),
+            new CLOptionDescriptor("password",
+                    CLOptionDescriptor.ARGUMENT_REQUIRED, PASSWORD_OPT,
+                    "specify password for the user."),
+            new CLOptionDescriptor("uri", CLOptionDescriptor.ARGUMENT_REQUIRED,
+                    URI_OPT,
+                    "the XML:DB URI of the database instance to be shut down.") };
+
+    public static void main(String[] args) {
+        CLArgsParser optParser = new CLArgsParser(args, OPTIONS);
+        if (optParser.getErrorString() != null) {
+            System.err.println("ERROR: " + optParser.getErrorString());
+            return;
+        }
+        Properties properties = loadProperties();
+        String user = "admin";
+        String passwd = "";
+        String uri = properties.getProperty("uri", "xmldb:exist://localhost:8080/exist/xmlrpc");
+        List opt = optParser.getArguments();
+        int size = opt.size();
+        CLOption option;
+        for (int i = 0; i < size; i++) {
+            option = (CLOption) opt.get(i);
+            switch (option.getId()) {
+                case HELP_OPT:
+                    System.out.println("Usage: java "
+                            + ServerShutdown.class.getName() + " [options]");
+                    System.out.println(CLUtil.describeOptions(OPTIONS)
+                            .toString());
+                    return;
+                case USER_OPT:
+                    user = option.getArgument();
+                    break;
+                case PASSWORD_OPT:
+                    passwd = option.getArgument();
+                    break;
+                case URI_OPT:
+                    uri = option.getArgument();
+            }
+        }
+        try {
+            // initialize database drivers
+            Class cl = Class.forName("org.exist.xmldb.DatabaseImpl");
+            // create the default database
+            Database database = (Database) cl.newInstance();
+            DatabaseManager.registerDatabase(database);
+            if (!uri.endsWith("/db"))
+                uri = uri + "/db";
+            Collection root = DatabaseManager.getCollection(uri, user, passwd);
+            DatabaseInstanceManager manager = (DatabaseInstanceManager) root
+                    .getService("DatabaseInstanceManager", "1.0");
+            System.out.println("Shutting down database instance at ");
+            System.out.println('\t' + uri);
+            manager.shutdown();
+        } catch (XMLDBException e) {
+            System.err.println("ERROR: " + e.getMessage());
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static Properties loadProperties() {
+
+        Properties clientProps = new Properties();
+
+        String home = System.getProperty("exist.home");
+        File propFile;
+        if (home == null)
+            propFile = new File("client.properties");
+        else
+            propFile = new File(home
+                    + System.getProperty("file.separator", "/")
+                    + "client.properties");
+        InputStream pin = null;
+
+        // Try to load from file
+        try {
+            pin = new FileInputStream(propFile);
+        } catch (FileNotFoundException ex) {
+            // File not found, no exception handling
+        }
+
+        if (pin == null) {
+            // Try to load via classloader
+            pin = ServerShutdown.class.getResourceAsStream("client.properties");
+        }
+
+        if (pin != null) {
+
+            // Try to load properties from stream
+            try {
+                clientProps.load(pin);
+            } catch (IOException ex) {
+                //
+            }
+        }
+        return clientProps;
+    }
 }
