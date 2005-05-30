62c31d382ddfe57cb6471ce7118260939d499303
Dannes Wessels <dannes@exist-db.org>
2005-05-30 18:21:38 +0000

    XP: Correct java is used now (selected by %JAVA_HOME%) and EXIST_HOME is set now (if not set before)
    svn path=/trunk/eXist-1.0/; revision=1308
    
    
    Former-commit-id: 1ee6af824be7c0ab6abe2669fb2e0588b19a7efd

--- build.bat
+++ build.bat
@@ -7,18 +7,22 @@ echo the home of you JDK.
 goto :eof
 
 :gotJavaHome
+if not "%EXIST_HOME%" == "" goto gotExistHome
+set EXIST_HOME=%CD%
+
+:gotExistHome
 set ANT_HOME=%EXIST_HOME%\tools
 set _LIBJARS=%CLASSPATH%;%ANT_HOME%\lib\ant-launcher.jar;%ANT_HOME%\lib\junit.jar;%JAVA_HOME%\lib\tools.jar
 
 set JAVA_ENDORSED_DIRS=%EXIST_HOME%\lib\endorsed
-set JAVA_OPTS=-Xms32000k -Xmx256000k -Djava.endorsed.dirs=%JAVA_ENDORSED_DIRS% -Dant.home=%ANT_HOME%
+set JAVA_OPTS=-Xms64M -Xmx512M -Djava.endorsed.dirs="%JAVA_ENDORSED_DIRS%" -Dant.home="%ANT_HOME%"
 
 echo eXist Build
 echo -------------------
-
-echo Building with classpath %_LIBJARS%
+echo JAVA_HOME=%JAVA_HOME%
+echo EXIST_HOME=%EXIST_HOME%
+echo _LIBJARS=%_LIBJARS%
 
 echo Starting Ant...
 
-java %JAVA_OPTS% -classpath %_LIBJARS% org.apache.tools.ant.launch.Launcher %1 %2 %3 %4 %5
-
+"%JAVA_HOME%\bin\java" %JAVA_OPTS% -classpath "%_LIBJARS%" org.apache.tools.ant.launch.Launcher %1 %2 %3 %4 %5
