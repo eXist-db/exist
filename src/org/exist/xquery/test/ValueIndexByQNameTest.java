7c028a5bef6c6d5bd4bd986a989f5cb37d8d72be
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2005-05-30 11:57:38 +0000

    add test for attribute index and mixed content
    svn path=/trunk/eXist-1.0/; revision=1303

--- src/org/exist/xquery/test/ValueIndexByQNameTest.java
+++ src/org/exist/xquery/test/ValueIndexByQNameTest.java
@@ -2,8 +2,6 @@
 $Id$ */
 package org.exist.xquery.test;
 
-import org.xmldb.api.base.Resource;
-import org.xmldb.api.base.ResourceSet;
 import org.xmldb.api.modules.XPathQueryService;
 
 /**
@@ -11,14 +9,14 @@ import org.xmldb.api.modules.XPathQueryService;
  */
 public class ValueIndexByQNameTest extends ValueIndexTest {
 
-    private String CONFIG =
+    private String config =
     	"<collection xmlns=\"http://exist-db.org/collection-config/1.0\">" + 
     	"	<index xmlns:x=\"http://www.foo.com\" xmlns:xx=\"http://test.com\">" + 
     	"		<fulltext default=\"none\">" + 
     	"			<include path=\"//item/name\"/>" + 
     	"			<include path=\"//item/mixed\"/>" + 
     	"		</fulltext>" + 
-//    	"		<create path=\"//item/itemno\" type=\"xs:integer\"/>" + 
+    	"		<create qname=\"itemno\" type=\"xs:integer\"/>" + 
 
 //    	"		<create-by-qname qname=\"//item/name\" type=\"xs:string\"/>" + 
     	"		<create qname=\"name\" type=\"xs:string\"/>" + 
@@ -27,43 +25,35 @@ public class ValueIndexByQNameTest extends ValueIndexTest {
 //    	"		<create path=\"//item/price\" type=\"xs:double\"/>" + 
 //    	"		<create path=\"//item/price/@specialprice\" type=\"xs:boolean\"/>" + 
 //    	"		<create path=\"//item/x:rating\" type=\"xs:double\"/>" +
-//    	"		<create path=\"//item/@xx:test\" type=\"xs:integer\"/>" +
-//    	"       <create path=\"//item/mixed\" type=\"xs:string\"/>" +
+    	"		<create qname='xx:test' type='xs:integer' />" +
+    	"       <create path='mixed' type='xs:string' />" +
     	"	</index>" + 
     	"</collection>";
     
-	/** ? @see org.exist.xquery.test.ValueIndexTest#testStrings()
-	 */
+	/** @see org.exist.xquery.test.ValueIndexTest#testStrings() */
 	public void testStrings() throws Exception {
         configureCollection();
         XPathQueryService service = storeXMLFileAndGetQueryService("items.xml", "src/org/exist/xquery/test/items.xml");
-        
-        // queryResource(service, "items.xml", "//item[name = 'Racing Bicycle']", 1);
-        queryResource(service, "items.xml", 
-            	"util:qname-index-lookup( xs:QName('name'), 'Racing Bicycle' ) " , 1 );
-    	// "util:qname-index-lookup( xs:QName('name'), 'Racing Bicycle' ) / parent::item" , 1 );
 
-//        queryResource(service, "items.xml", "//item[name > 'Racing Bicycle']", 4);
-//        queryResource(service, "items.xml", "//item[itemno = 3]", 1);
-//        ResourceSet result = queryResource(service, "items.xml", "for $i in //item[stock <= 10] return $i/itemno", 5);
-//        for (long i = 0; i < result.getSize(); i++) {
-//            Resource res = result.getResource(i);
-//            System.out.println(res.getContent());
-//        }
-//        
-//        queryResource(service, "items.xml", "//item[stock > 20]", 1);
-//        queryResource(service, "items.xml", "declare namespace x=\"http://www.foo.com\"; //item[x:rating > 8.0]", 2);
-//        queryResource(service, "items.xml", "declare namespace xx=\"http://test.com\"; //item[@xx:test = 123]", 1);
+        // queryResource(service, "items.xml", "//item[name > 'Racing Bicycle']", 4 );
+
+         queryResource(service, "items.xml", 
+        		"util:qname-index-lookup( xs:QName('name'), 'Racing Bicycle' ) / parent::item" , 1 );       
+         queryResource(service, "items.xml",
+        	"util:qname-index-lookup( xs:QName('itemno'), 3) / parent::item", 1);    
+        queryResource(service, "items.xml", 
+        		"declare namespace xx='http://test.com'; " +
+        		"util:qname-index-lookup( xs:QName('xx:test'), 123) ", 1);
+
 //        queryResource(service, "items.xml", "//item[name &= 'Racing Bicycle']", 1);
 //        queryResource(service, "items.xml", "//item[mixed = 'uneven']", 1);
-//		queryResource(service, "items.xml", "//item[mixed = 'external']", 1);
+		queryResource(service, "items.xml", 
+			"util:qname-index-lookup( xs:QName('mixed'), 'external' )", 1);
 //		queryResource(service, "items.xml", "//item[fn:matches(mixed, 'un.*')]", 2);
 	}
 
-	/** ? @see org.exist.xquery.test.ValueIndexTest#getCollectionConfig()
-	 */
+	/** @see org.exist.xquery.test.ValueIndexTest#getCollectionConfig() */
 	protected String getCollectionConfig() {
-		return CONFIG;
+		return config;
 	}
-
 }
