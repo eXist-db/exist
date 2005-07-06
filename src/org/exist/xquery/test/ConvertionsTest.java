c687e1f9e82053033eb517ae3eb69c8768d1f52e
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2005-07-06 17:25:14 +0000

    provide conversion from QName to string
    svn path=/trunk/eXist-1.0/; revision=1403
    
    
    Former-commit-id: abe8c57fcbf3f4aa183027b3faf80b52fea578fc

--- src/org/exist/xquery/test/ConvertionsTest.java
+++ src/org/exist/xquery/test/ConvertionsTest.java
@@ -20,9 +20,6 @@ import org.xmldb.api.modules.XPathQueryService;
  */
 public class ConvertionsTest extends TestCase {
 
-	private String[] testvalues;
-	private String[] resultvalues;
-	
 	private XPathQueryService service;
 	private Collection root = null;
 	private Database database = null;
@@ -40,25 +37,16 @@ public class ConvertionsTest extends TestCase {
 	}	
 	
 
-/** test conversion from QName to string
- * >>>>>> currently crashes; when you will want to solve the bug , 
- * just change bugtestQName2string in testQName2string <<<<<<<
- * org.apache.cocoon.ProcessingException: XMLDBException occurred: A QName cannot be converted to xs:string [at line 201, column 12]
-In call to function:
-f:eval(xs:string) [201, 12]
-f:main() [224, 11]: org.xmldb.api.base.XMLDBException: A QName cannot be converted to xs:string [at line 201, column 12]
-In call to function:
-f:eval(xs:string) [201, 12]
-f:main() [224, 11]
- */	
-	public void bugtestQName2string() throws XPathException {
+/** test conversion from QName to string */	
+	public void testQName2string() throws XPathException {
 		ResourceSet result 	= null;
 		String		r		= "";
 		String		query	= null;
 		try {
-			query = "let $a := ( xs:QName('foo:bar'), xs:QName('foo:john'), xs:QName('foo:doe') )" +
-						"for $b in $a" +
-							"return" +
+			query = "declare namespace foo = 'http://foo'; \n" +
+					"let $a := ( xs:QName('foo:bar'), xs:QName('foo:john'), xs:QName('foo:doe') )\n" +
+						"for $b in $a \n" +
+							"return \n" +
 								"<blah>{string($b)}</blah>" ;
 			result 	= service.query( query );
 			/* which returns :
@@ -67,7 +55,7 @@ f:main() [224, 11]
 				<blah>foo:doe</blah>"
 			*/
 			r = (String) result.getResource(0).getContent();
-			assertEquals( "foo:bar", r );
+			assertEquals( "<blah>foo:bar</blah>", r );
 			assertEquals( "XQuery: " + query, 3, result.getSize() );
 			
 		} catch (XMLDBException e) {
@@ -100,5 +88,4 @@ f:main() [224, 11]
 		dim.shutdown();
 		//System.out.println("tearDown PASSED");
 	}
-
 }
