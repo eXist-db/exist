ac1a2a5ca6167cdc1cd65d8198156ceacbaf8575
Wolfgang Meier <wolfgangmm@gmail.com>
2005-10-04 13:52:02 +0000

    Applied patch to near() function provided by Leif-J��ran: function now handles minimal as well as maximal word distance. The maximum and/or minimum distance is 1 (next word) if not supplied.
    svn path=/trunk/eXist-1.0/; revision=1784
    
    
    Former-commit-id: fe57a1331a6ebec3b7a121c4252864f738e66b8c

--- src/org/exist/xquery/functions/ExtNear.java
+++ src/org/exist/xquery/functions/ExtNear.java
@@ -1,24 +1,24 @@
 /*
- *  eXist Open Source Native XML Database
- *  Copyright (C) 2001-03 Wolfgang M. Meier
- *  wolfgang@exist-db.org
- *  http://exist.sourceforge.net
+ * eXist Open Source Native XML Database
+ * Copyright (C) 2001-2005 Wolfgang M. Meier
+ * wolfgang@exist-db.org
+ * http://exist.sourceforge.net
  *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public License
- *  as published by the Free Software Foundation; either version 2
- *  of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
  *
- *  $Id$
+ * $Id$
  */
 package org.exist.xquery.functions;
 
@@ -52,177 +52,204 @@ import org.exist.xquery.value.Type;
  */
 public class ExtNear extends ExtFulltext {
 
-	private int max_distance = 1;
-	private Expression distance = null;
+    private int min_distance = 1;
+    private int max_distance = 1;
+    private Expression minDistance = null;
+    private Expression maxDistance = null;
 
-	public ExtNear(XQueryContext context) {
-		super(context, Constants.FULLTEXT_AND);
-	}
+    public ExtNear(XQueryContext context) {
+	super(context, Constants.FULLTEXT_AND);
+    }
 
-	/* (non-Javadoc)
+    /* (non-Javadoc)
      * @see org.exist.xquery.functions.ExtFulltext#analyze(org.exist.xquery.Expression)
      */
     public void analyze(Expression parent, int flags) throws XPathException {
         super.analyze(parent, flags);
-        if(distance != null)
-            distance.analyze(this, flags);
+
+        if(maxDistance != null) {
+            maxDistance.analyze(this, flags);
+	}
+        if(minDistance != null) {
+            minDistance.analyze(this, flags);
+	}
     }
     
-	public Sequence evalQuery(
-		String searchArg,
-		NodeSet nodes)
-		throws XPathException {
-		if(distance != null)
-			max_distance = ((IntegerValue)distance.eval(nodes).convertTo(Type.INTEGER)).getInt();
-		try {
-			getSearchTerms(context, searchArg);
-		} catch (EXistException e) {
-			throw new XPathException(e.getMessage(), e);
-		}
-		NodeSet hits = processQuery(nodes);
-		if (hits == null)
-			return Sequence.EMPTY_SEQUENCE;
+    public Sequence evalQuery(
+			      String searchArg,
+			      NodeSet nodes)
+	throws XPathException {
+	if(maxDistance != null) {
+	    max_distance = ((IntegerValue) maxDistance.eval(nodes).convertTo(Type.INTEGER)).getInt();
+	}
+	if(minDistance != null) {
+	    min_distance = ((IntegerValue) minDistance.eval(nodes).convertTo(Type.INTEGER)).getInt();
+	}
+	try {
+	    getSearchTerms(context, searchArg);
+	} catch (EXistException e) {
+	    throw new XPathException(e.getMessage(), e);
+	}
+	NodeSet hits = processQuery(nodes);
+	if (hits == null)
+	    return Sequence.EMPTY_SEQUENCE;
+	    
+	boolean hasWildcards = false;
+	for(int i = 0; i < terms.length; i++) {
+	    hasWildcards |=
+		NativeTextEngine.containsWildcards(terms[i]);
+	}
+	return hasWildcards
+	    ? patternMatch(context, hits)
+	    : exactMatch(context, hits);
+    }
+
+    private Sequence exactMatch(XQueryContext context, NodeSet result) {
+	// walk through hits and calculate term-distances
+	String value;
+	String term;
+	String word;
+	TextToken token;
+	NodeProxy current;
+	NodeSet r = new ExtArrayNodeSet();
+	Tokenizer tok = context.getBroker().getTextEngine().getTokenizer();
+	int j;
+	int current_distance;
+	for (Iterator i = result.iterator(); i.hasNext();) {
+	    current = (NodeProxy) i.next();
+	    value = current.getNodeValueSeparated();
+	    tok.setText(value);
+	    j = 0;
+	    if (j < terms.length) {
+		term = terms[j];
+	    } else {
+		break;
+	    }
+
+	    current_distance = -1;
+	    
+	    while ((token = tok.nextToken()) != null) {
+		word = token.getText().toLowerCase();
+
+		if (current_distance > max_distance) {
+		    // reset
+		    j = 0;
+		    term = terms[j];
+		    current_distance = -1;
 				
-		boolean hasWildcards = false;
-		for(int i = 0; i < terms.length; i++) {
-			hasWildcards |=
-				NativeTextEngine.containsWildcards(terms[i]);
-		}
-		return hasWildcards
-			? patternMatch(context, hits)
-			: exactMatch(context, hits);
-	}
-
-	private Sequence exactMatch(XQueryContext context, NodeSet result) {
-		// walk through hits and calculate term-distances
-		String value;
-		String term;
-		String word;
-		TextToken token;
-		NodeProxy current;
-		NodeSet r = new ExtArrayNodeSet();
-		Tokenizer tok = context.getBroker().getTextEngine().getTokenizer();
-		int j;
-		int distance;
-		for (Iterator i = result.iterator(); i.hasNext();) {
-			current = (NodeProxy) i.next();
-			value = current.getNodeValueSeparated();
-			tok.setText(value);
-			j = 0;
-			if (j < terms.length)
-				term = terms[j];
-			else
-				break;
-			distance = -1;
-			while ((token = tok.nextToken()) != null) {
-				word = token.getText().toLowerCase();
-				if (distance > max_distance) {
-					// reset
-					j = 0;
-					term = terms[j];
-					distance = -1;
-					
-				} // that else would cause some words to be ignored in the matching
-				if (word.equalsIgnoreCase(term)) {
-					distance = 0;
-					j++;
-					if (j == terms.length) {
-						// all terms found
-						r.add(current);
-						break;
-					} else
-						term = terms[j];
-
-				} else if (j > 0 && word.equalsIgnoreCase(terms[0])) {
-					// first search term found: start again
-					j = 1;
-					term = terms[j];
-					distance = 0 ;
-					continue;
-				} // that else MAY cause the distance count to be off by one but i'm not sure
-				if (-1 < distance)
-					++distance;
+		} // that else would cause some words to be ignored in the matching
+		if (word.equalsIgnoreCase(term)) {
+		    boolean withIn = current_distance >= min_distance ? true : false;
+		    current_distance = 0;
+		    j++;
+		    if (j == terms.length) {
+			// all terms found
+			if (withIn) {
+			    r.add(current);
 			}
-		}
-//		LOG.debug("found " + r.getLength());
-		return r;
-	}
+			break;
+		    } else {
+			term = terms[j];
+		    }
 
-	private Sequence patternMatch(XQueryContext context, NodeSet result) {
-		// generate list of search term patterns
-		Pattern patterns[] = new Pattern[terms.length];
+		} else if (j > 0 && word.equalsIgnoreCase(terms[0])) {
+		    // first search term found: start again
+		    j = 1;
+		    term = terms[j];
+		    current_distance = 0;
+		    continue;
+		} // that else MAY cause the distance counts to be off by one but i'm not sure
+		if (-1 < current_distance) {
+		    ++current_distance;
+		}
+	    }
+	}
+	//		LOG.debug("found " + r.getLength());
+	return r;
+    }
+
+    private Sequence patternMatch(XQueryContext context, NodeSet result) {
+	// generate list of search term patterns
+	Pattern patterns[] = new Pattern[terms.length];
         Matcher matchers[] = new Matcher[terms.length];
-		for (int i = 0; i < patterns.length; i++)
-			try {
+	for (int i = 0; i < patterns.length; i++)
+	    try {
                 patterns[i] = Pattern.compile(GlobToRegex.globToRegexp(terms[i]), 
-                        Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
+					      Pattern.CASE_INSENSITIVE | Pattern.UNICODE_CASE);
                 matchers[i] = patterns[i].matcher("");
-			} catch (PatternSyntaxException e) {
-				LOG.warn("malformed pattern", e);
-				return Sequence.EMPTY_SEQUENCE;
-			}
-
-		// walk through hits and calculate term-distances
-		String value;
-		Pattern term;
-		String word;
-		TextToken token;
-		NodeProxy current;
-		ExtArrayNodeSet r = new ExtArrayNodeSet(100);
-		Matcher matcher;
-		Tokenizer tok = context.getBroker().getTextEngine().getTokenizer();
-		int j;
-		int distance;
-		for (Iterator i = result.iterator(); i.hasNext();) {
-			current = (NodeProxy) i.next();
-			value = current.getNodeValueSeparated();
-			tok.setText(value);
-			j = 0;
-			if (j < patterns.length) {
-				term = patterns[j];
+	    } catch (PatternSyntaxException e) {
+		LOG.warn("malformed pattern", e);
+		return Sequence.EMPTY_SEQUENCE;
+	    }
+	
+	// walk through hits and calculate term-distances
+	String value;
+	Pattern term;
+	String word;
+	TextToken token;
+	NodeProxy current;
+	ExtArrayNodeSet r = new ExtArrayNodeSet(100);
+	Matcher matcher;
+	Tokenizer tok = context.getBroker().getTextEngine().getTokenizer();
+	int j;
+	int current_distance;
+	for (Iterator i = result.iterator(); i.hasNext();) {
+	    current = (NodeProxy) i.next();
+	    value = current.getNodeValueSeparated();
+	    tok.setText(value);
+	    j = 0;
+	    if (j < patterns.length) {
+		term = patterns[j];
                 matcher = matchers[j];
-            } else
-				break;
-			distance = -1;
-			while ((token = tok.nextToken()) != null) {
-				word = token.getText().toLowerCase();
-				if (distance > max_distance) {
-					// reset
-					j = 0;
-					term = patterns[j];
-                    matcher = matchers[j];
-					distance = -1;
-					continue;
-				}
-                matcher.reset(word);
-                matchers[0].reset(word);
-				if (matcher.matches()) {
-					distance = 0;
-					j++;
-					if (j == patterns.length) {
-						// all terms found
-						r.add(current);
-						break;
-					} else {
-						term = patterns[j];
-                        matcher = matchers[j];
-                    }
-				} else if (j > 0 && matchers[0].matches()) {
-					// first search term found: start again
-					j = 1;
-					term = patterns[j];
-                    matcher = matchers[j];
-					distance = 0;
-					continue;
-				} else if (-1 < distance)
-					++distance;
+            } else {
+		break;
+	    }
 
-			}
+	    current_distance = -1;
+	    
+	    while ((token = tok.nextToken()) != null) {
+		word = token.getText().toLowerCase();
+		if (current_distance > max_distance) {
+		    // reset
+		    j = 0;
+		    term = patterns[j];
+		    matcher = matchers[j];
+		    current_distance = -1;
+		    continue;
 		}
-		return r;
+		matcher.reset(word);
+		matchers[0].reset(word);
+		if (matcher.matches()) {
+		    boolean withIn = current_distance >= min_distance ? true : false;
+		    current_distance = 0;
+		    j++;
+		    if (j == patterns.length) {
+			// all terms found
+			if (withIn) {
+			    r.add(current);
+			}
+			break;
+		    } else {
+			term = patterns[j];
+                        matcher = matchers[j];
+		    }
+		} else if (j > 0 && matchers[0].matches()) {
+		    // first search term found: start again
+		    j = 1;
+		    term = patterns[j];
+		    matcher = matchers[j];
+		    current_distance = 0;
+		    continue;
+		} else if (-1 < current_distance) {
+		    ++current_distance;
+		}
+		
+	    }
 	}
-
-	/* (non-Javadoc)
+	return r;
+    }
+    
+    /* (non-Javadoc)
      * @see org.exist.xquery.functions.ExtFulltext#dump(org.exist.xquery.util.ExpressionDumper)
      */
     public void dump(ExpressionDumper dumper) {
@@ -233,7 +260,11 @@ public class ExtNear extends ExtFulltext {
         dumper.display(")");
     }
     
-	public void setDistance(Expression expr) {
-		distance = expr;
-	}
+    public void setMaxDistance(Expression expr) {
+	maxDistance = expr;
+    }
+ 
+    public void setMinDistance(Expression expr) {
+	minDistance = expr;
+    }
 }
