079f00e236410ca06a0a95d26fe0e13018a9b72a
Adam Retter <adam.retter@googlemail.com>
2005-11-14 16:49:03 +0000

    Added set-serializer() to context extension module.
    svn path=/trunk/eXist-1.0/; revision=2013

---
+++ extensions/modules/src/org/exist/xquery/modules/context/SetSerializerFunction.java
@@ -0,0 +1,72 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2001-03 Wolfgang M. Meier
+ *  wolfgang@exist-db.org
+ *  http://exist.sourceforge.net
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  
+ *  $Id$
+ */
+package org.exist.xquery.modules.context;
+
+import org.exist.dom.QName;
+import org.exist.xquery.Function;
+import org.exist.xquery.Cardinality;
+import org.exist.xquery.FunctionSignature;
+import org.exist.xquery.XPathException;
+import org.exist.xquery.XQueryContext;
+import org.exist.xquery.value.Item;
+import org.exist.xquery.value.Sequence;
+import org.exist.xquery.value.SequenceIterator;
+import org.exist.xquery.value.SequenceType;
+import org.exist.xquery.value.StringValue;
+import org.exist.xquery.value.Type;
+import org.exist.xquery.value.ValueSequence;
+
+/**
+ * @author Adam Retter (adam.retter@devon.gov.uk)
+ */
+public class SetSerializerFunction extends Function {
+
+	public final static FunctionSignature signature = new FunctionSignature(
+			new QName("set-serializer", ContextModule.NAMESPACE_URI, ContextModule.PREFIX),
+			"Set's the Serializer named in $a to use for output. $b indicates whether output should be indented and $c indicates whether the xml declaration should be omitted.",
+			new SequenceType[] {
+				new SequenceType(Type.STRING, Cardinality.EXACTLY_ONE),
+				new SequenceType(Type.BOOLEAN, Cardinality.EXACTLY_ONE),
+				new SequenceType(Type.BOOLEAN, Cardinality.EXACTLY_ONE)
+			},
+			new SequenceType(Type.ITEM, Cardinality.EMPTY)
+	);
+	
+
+	public SetSerializerFunction(XQueryContext context)
+	{
+		super(context, signature);
+	}
+
+	public Sequence eval(Sequence contextSequence, Item contextItem) throws XPathException
+	{
+		String name = getArgument(0).eval(contextSequence, contextItem).getStringValue();
+		String indent = getArgument(1).eval(contextSequence, contextItem).effectiveBooleanValue(); 
+		String omitxmldeclaration = getArgument(2).eval(contextSequence, contextItem).effectiveBooleanValue();
+		
+		context.setXQuerySerializer(name, indent, omitxmldeclaration);
+		
+		return Sequence.EMPTY_SEQUENCE;
+	}
+
+}
