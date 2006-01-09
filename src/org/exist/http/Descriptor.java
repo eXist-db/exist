fc42de8bbb8e10128577a3dca55556b00ab12300
Adam Retter <adam.retter@googlemail.com>
2006-01-09 17:57:41 +0000

    Bought licence header up to date (sorry Pierrick ;-))
    svn path=/trunk/eXist-1.0/; revision=2455
    
    
    Former-commit-id: ee4359425f2efea6d72450aca205b2bb0c9c58d4

--- src/org/exist/http/Descriptor.java
+++ src/org/exist/http/Descriptor.java
@@ -1,22 +1,22 @@
 /*
- *  eXist Open Source Native XML Database
- *  Copyright (C) 2001,  Wolfgang M. Meier
+ * eXist Open Source Native XML Database Copyright (C) 2001-06 Wolfgang M.
+ * Meier meier@ifs.tu-darmstadt.de http://exist.sourceforge.net
  *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Library General Public License
- *  as published by the Free Software Foundation; either version 2
- *  of the License, or (at your option) any later version.
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as published by the
+ * Free Software Foundation; either version 2 of the License, or (at your
+ * option) any later version.
  *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Library General Public License for more details.
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License
+ * for more details.
  *
- *  You should have received a copy of the GNU Library General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation,
+ * Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- *  $Id$
+ * $Id$
  */
 
 package org.exist.http;
