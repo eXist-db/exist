aa3ded4ad9eac47c218a14d040604d935759c293
Jean-Marc Vanel <jeanmarc.vanel@gmail.com>
2006-02-11 08:49:11 +0000

    Store the currently running trigger. Not yet in use - will be used by XQueryTrigger and Modification .
    svn path=/trunk/eXist-1.0/; revision=2653

--- src/org/exist/collections/triggers/TriggerStatePerThread.java
+++ src/org/exist/collections/triggers/TriggerStatePerThread.java
@@ -5,7 +5,7 @@ import org.exist.storage.txn.Txn;
 /** Finite State Machine, managing the state of a Running trigger, 
  * allows to avoid infinite recursions by forbidding another trigger to run
  * where there is allready one; feature trigger_update
- * TODO: encapsulate the ThreadLocal ; apply "state" design pattern */
+ * TODO: apply "state" design pattern */
 public class TriggerStatePerThread {
 
 	public static final int NO_TRIGGER_RUNNING = (0);
@@ -18,18 +18,23 @@ public class TriggerStatePerThread {
 	    };
 	};
 
-	public static boolean verifyUniqueTriggerPerThreadBeforePrepare() {
+	public static boolean verifyUniqueTriggerPerThreadBeforePrepare(DocumentTrigger trigger) {
 		if( getTriggerRunningState() == NO_TRIGGER_RUNNING ) {
-			setTriggerRunningState( TRIGGER_RUNNING_PREPARE );
+			setTriggerRunningState( TRIGGER_RUNNING_PREPARE, trigger );
 			return true;
 		} else {
 			return false;
 		}
 	}
 	
-	public static boolean verifyUniqueTriggerPerThreadBeforeFinish() {
+	public static boolean verifyUniqueTriggerPerThreadBeforeFinish(DocumentTrigger trigger) {
+
+		// another trigger is allready running
+		if( trigger != getRunningTrigger() )
+			return false;
+		
 		if ( getTriggerRunningState() == TRIGGER_RUNNING_PREPARE) {
-			setTriggerRunningState( TRIGGER_RUNNING_FINISH );
+			setTriggerRunningState( TRIGGER_RUNNING_FINISH, trigger );
 			return true;
 		} else {
 			return false;
@@ -38,14 +43,19 @@ public class TriggerStatePerThread {
 	
 	public static class TriggerState {
 		private int state;
+		private DocumentTrigger currentTrigger;
 		private Txn transaction;
 		public TriggerState(int state) {
 			super();
-			this.setState(state);
+			this.setState(state, null);
 		}
 		
-		private void setState(int state) {
+		private void setState( int state, DocumentTrigger trigger) {
 			this.state = state;
+			if( state == NO_TRIGGER_RUNNING )
+				this.currentTrigger = null;
+			else
+				this.currentTrigger = trigger;
 		}
 
 		private int getState() {
@@ -57,21 +67,28 @@ public class TriggerStatePerThread {
 		}
 		Txn getTransaction() {
 			return transaction;
+		}
+
+		public DocumentTrigger getTrigger() {
+			return currentTrigger;
 		}	
 	}
 	
 	public static int getTriggerRunningState() {
 		return ((TriggerState)triggerRunningState.get()).getState();
 	}
+
+	public static DocumentTrigger getRunningTrigger() {
+		return ((TriggerState)triggerRunningState.get()).getTrigger();
+	}
 	
-	public static void setTriggerRunningState(int state) {
-		((TriggerState)triggerRunningState.get()).setState(state);
+	public static void setTriggerRunningState(int state, DocumentTrigger trigger) {
+		((TriggerState)triggerRunningState.get()).setState(state, trigger);
 	}
 
 	public static Txn getTransaction() {
 		return ((TriggerState)triggerRunningState.get()).getTransaction();
 	}
-	
 	public static void setTransaction(Txn transaction) {
 		((TriggerState)triggerRunningState.get()).setTransaction(transaction);
 	}
