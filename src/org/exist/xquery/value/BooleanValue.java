a3c3749545f47628ee273a8462e3045284ee64b1
Pierrick Brihaye <pierrick.brihaye@free.fr>
2006-02-22 21:48:37 +0000

    Fixed comparisons in BooleanValue
    svn path=/trunk/eXist-1.0/; revision=2735

--- src/org/exist/xquery/value/BooleanValue.java
+++ src/org/exist/xquery/value/BooleanValue.java
@@ -97,11 +97,11 @@ public class BooleanValue extends AtomicValue implements Indexable {
 				case Constants.LT:
 					return (!value) && otherVal;
 				case Constants.LTEQ:
-					return (!value) && otherVal;
+					return value == otherVal || (!value) && otherVal;
 				case Constants.GT:
 					return value && (!otherVal);
 				case Constants.GTEQ:
-					return value && (!otherVal);
+					return value == otherVal || value && (!otherVal);
 				default :
 					throw new XPathException("Type error: cannot apply this operator to a boolean value");
 			}
