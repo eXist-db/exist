6a64ea15a3a482cb1ea5fa9ded8e3a7ea3a0911a
Adam Retter <adam.retter@googlemail.com>
2006-03-01 13:52:00 +0000

    Changed licencing to BSD and added Javadoc
    svn path=/trunk/eXist-1.0/; revision=2807

--- extensions/modules/src/org/exist/xquery/modules/context/SetSerializerFunction.java
+++ extensions/modules/src/org/exist/xquery/modules/context/SetSerializerFunction.java
@@ -1,43 +1,55 @@
 /*
- *  eXist Open Source Native XML Database
- *  Copyright (C) 2001-03 Wolfgang M. Meier
- *  wolfgang@exist-db.org
- *  http://exist.sourceforge.net
+ * eXist Context Module Extension SetSerializerFunction
+ *
+ * Released under the BSD License
+ *
+ * Copyright (c) 2006, Adam retter <adam.retter@devon.gov.uk>
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+ * 		Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+ *  	Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+ *  	Neither the name of the Devon Portal Project nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
  *  
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public License
- *  as published by the Free Software Foundation; either version 2
- *  of the License, or (at your option) any later version.
- *  
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *  
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *  
+ *  THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS
+ *  IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ *  IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR
+ *  PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR
+ *  CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ *  SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
+ *  PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS;
+ *  OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
+ *  LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR
+ *  OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF
+ *  ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ *
  *  $Id$
  */
+
 package org.exist.xquery.modules.context;
 
 import org.exist.dom.QName;
-import org.exist.xquery.Function;
 import org.exist.xquery.Cardinality;
+import org.exist.xquery.Function;
 import org.exist.xquery.FunctionSignature;
-import org.exist.xquery.XPathException;
-import org.exist.xquery.XQueryContext;
 import org.exist.xquery.value.Item;
 import org.exist.xquery.value.Sequence;
-import org.exist.xquery.value.SequenceIterator;
 import org.exist.xquery.value.SequenceType;
-import org.exist.xquery.value.StringValue;
 import org.exist.xquery.value.Type;
-import org.exist.xquery.value.ValueSequence;
+import org.exist.xquery.XPathException;
+import org.exist.xquery.XQueryContext;
 
 /**
- * @author Adam Retter (adam.retter@devon.gov.uk)
+ * eXist Context Module Extension SetSerializerFunction 
+ * 
+ * The Serializer Setting functionality of the eXist Context Module Extension that
+ * allows the eXist XQuery Serializer to be set from within the executing XQuery
+ * 
+ * @author Adam Retter <adam.retter@devon.gov.uk>
+ * @serial 2006-03-01
+ * @version 1.3
+ *
+ * @see org.exist.xquery.Function
  */
 public class SetSerializerFunction extends Function {
 
@@ -52,12 +64,26 @@ public class SetSerializerFunction extends Function {
 			new SequenceType(Type.ITEM, Cardinality.EMPTY)
 	);
 	
-
+	/**
+	 * SetSerializerFunction Constructor
+	 * 
+	 * @param context	The Context of the calling XQuery
+	 */
 	public SetSerializerFunction(XQueryContext context)
 	{
 		super(context, signature);
 	}
 
+	/**
+	 * evaluate the call to the xquery set-serializer() function,
+	 * it is really the main entry point of this class
+	 * 
+	 * @param contextSequence	the Context Sequence to operate on
+	 * @param contextItem		the Context Item to operate on
+	 * @return		An Empty Sequence
+	 * 
+	 * @see org.exist.xquery.Function#eval(org.exist.xquery.value.Sequence, org.exist.xquery.value.Item)
+	 */
 	public Sequence eval(Sequence contextSequence, Item contextItem) throws XPathException
 	{
 		String name = getArgument(0).eval(contextSequence, contextItem).getStringValue();
