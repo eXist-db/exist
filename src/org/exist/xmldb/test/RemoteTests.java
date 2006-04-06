9737480266c9bf0c29a3096a04511c9efd09cb9b
Wolfgang Meier <wolfgangmm@gmail.com>
2006-04-06 15:10:55 +0000

    Added test for retrieving results from a remote ResourceSet.
    svn path=/trunk/eXist-1.0/; revision=3067

--- src/org/exist/xmldb/test/RemoteTests.java
+++ src/org/exist/xmldb/test/RemoteTests.java
@@ -32,7 +32,8 @@ public class RemoteTests {
 	public static Test suite() {
 		TestSuite suite = new TestSuite("Remote tests for org.exist.xmldb.test");
 		suite.addTest(new TestSuite(RemoteCollectionTest.class));
-		suite.addTest(new TestSuite(RemoteDatabaseImplTest.class));		
+		suite.addTest(new TestSuite(RemoteDatabaseImplTest.class));
+		suite.addTest(new TestSuite(RemoteQueryTest.class));
 		suite.addTest(new TestSuite(DOMTestJUnit.class));
 		return suite;
 	}
