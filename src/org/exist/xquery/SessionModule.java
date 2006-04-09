00e7899c22f7c08c737050ce5193f87e62b0a4e7
Adam Retter <adam.retter@googlemail.com>
2006-04-09 18:11:02 +0000

    Split the large request module into three modules - request, response and session
    svn path=/trunk/eXist-1.0/; revision=3109
    
    
    Former-commit-id: 1d77c2dc10cd43c789c4de846dc8dd7a6af4e2b7

---
+++ src/org/exist/xquery/SessionModule.java
@@ -0,0 +1,77 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2001-06 Wolfgang M. Meier
+ *  wolfgang@exist-db.org
+ *  http://exist.sourceforge.net
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  
+ *  $Id
+ */
+package org.exist.xquery.functions.session;
+
+import org.exist.dom.QName;
+import org.exist.xquery.AbstractInternalModule;
+import org.exist.xquery.FunctionDef;
+import org.exist.xquery.XPathException;
+
+/**
+ * @author Adam Retter (adam.retter@devon.gov.uk)
+ */
+public class SessionModule extends AbstractInternalModule {
+
+	public static final String NAMESPACE_URI = "http://exist-db.org/xquery/session";
+	public static final String PREFIX = "session";
+	public static final QName SESSION_VAR = new QName("session", NAMESPACE_URI, PREFIX);
+	
+	public static final FunctionDef[] functions = {
+		new FunctionDef(Create.signature, Create.class),
+		new FunctionDef(EncodeURL.signature, EncodeURL.class),
+		new FunctionDef(GetID.signature, GetID.class),
+		new FunctionDef(GetAttribute.signature, GetAttribute.class),
+		new FunctionDef(GetAttributeNames.signature, GetAttributeNames.class),
+		new FunctionDef(Invalidate.signature, Invalidate.class),
+		new FunctionDef(SetAttribute.signature, SetAttribute.class),
+		new FunctionDef(SetCurrentUser.signature, SetCurrentUser.class)
+	};
+	
+	public SessionModule() throws XPathException {
+		super(functions);
+		// predefined module global variables:
+		declareVariable(SESSION_VAR, null);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.exist.xquery.Module#getDescription()
+	 */
+	public String getDescription() {
+		return "Functions dealing with the HTTP session"; 
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.exist.xquery.Module#getNamespaceURI()
+	 */
+	public String getNamespaceURI() {
+		return NAMESPACE_URI;
+	}
+
+	/* (non-Javadoc)
+	 * @see org.exist.xquery.Module#getDefaultPrefix()
+	 */
+	public String getDefaultPrefix() {
+		return PREFIX;
+	}
+
+}
