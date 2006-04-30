7052f75d3a086d074f3a64464ebe593a9f2441a3
Wolfgang Meier <wolfgangmm@gmail.com>
2006-04-30 14:30:25 +0000

    Updated IRCProxy.
    svn path=/trunk/eXist-1.0/; revision=3360
    
    
    Former-commit-id: a2725ee03937794e460cbbd2abaa8a96fc2770ba

--- webapp/irclog/scripts/irc.js
+++ webapp/irclog/scripts/irc.js
@@ -13,13 +13,24 @@ var NICK_COLORS = [
 	'#0033cc',
 	'#ff99ff',
 	'#9900cc',
-	'#ffdd00'
+	'#ffdd00',
+	'#ddff00',
+	'#00ffdd',
+	'#224455',
+	'#442255'
 ];
 
 var CHANNELS = [ '#existdb', '#testaabb'];
-
+	
 function newChat() {
 	var chat = new Chat();
+	chat.connect();
+//	chat.mainWindow();
+//	chat.dispatchEvent(Chat.EV_MESSAGE, ['wolf', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Fusce purus. Mauris sed orci. Sed feugiat hendrerit justo. Fusce eu mauris a urna lobortis cursus. Quisque fermentum, diam quis semper scelerisque, purus nibh mollis nibh, euismod ultrices tortor pede ac risus. Cras vitae metus. Maecenas ut libero. Maecenas non justo. Pellentesque suscipit ullamcorper leo. Maecenas ipsum. Mauris velit dui, feugiat quis, bibendum sed, eleifend iaculis, diam. Sed nunc velit, venenatis non, scelerisque ut, porttitor ut, nisl.']);
+//	chat.dispatchEvent(Chat.EV_MESSAGE, ['hans', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Fusce purus. Mauris sed orci. Sed feugiat hendrerit justo. Fusce eu mauris a urna lobortis cursus. Quisque fermentum, diam quis semper scelerisque, purus nibh mollis nibh, euismod ultrices tortor pede ac risus. Cras vitae metus. Maecenas ut libero. Maecenas non justo. Pellentesque suscipit ullamcorper leo. Maecenas ipsum. Mauris velit dui, feugiat quis, bibendum sed, eleifend iaculis, diam. Sed nunc velit, venenatis non, scelerisque ut, porttitor ut, nisl.']);
+//	chat.dispatchEvent(Chat.EV_NOTICE, ["This is a notice"]);
+//	chat.dispatchEvent(Chat.EV_JOIN, ["wolf", "joined the chat"]);
+//	chat.dispatchEvent(Chat.EV_MESSAGE, ['rudi', 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Fusce purus. Mauris sed orci. Sed feugiat hendrerit justo. Fusce eu mauris a urna lobortis cursus. Quisque fermentum, diam quis semper scelerisque, purus nibh mollis nibh, euismod ultrices tortor pede ac risus. Cras vitae metus. Maecenas ut libero. Maecenas non justo. Pellentesque suscipit ullamcorper leo. Maecenas ipsum. Mauris velit dui, feugiat quis, bibendum sed, eleifend iaculis, diam. Sed nunc velit, venenatis non, scelerisque ut, porttitor ut, nisl.']);
 }
 
 function dispatchEvent(sessionId, ev) {
@@ -39,6 +50,9 @@ var Chat = Class.create();
 Chat.EV_MESSAGE = 'message';
 Chat.EV_NOTICE = 'notice';
 Chat.EV_USERS = 'users';
+Chat.EV_JOIN = 'join';
+Chat.EV_PART = 'part';
+Chat.EV_PING = 'ping';
 
 Chat.__connections = Array();
 Chat.__connectionCnt = 0;
@@ -56,17 +70,6 @@ Chat.copyColors = function (input) {
 	return colors;
 };
 
-Chat.unload = function (event) {
-	if (confirm('Moving away from this page will close all ' +
-			'active connections. Are you sure?')) {
-		Chat.__connections.each(function (chat) {
-			chat.close();
-		});
-		return true;
-	}
-	return false;
-};
-
 Chat.prototype = {
 	
 	channel: null,
@@ -81,8 +84,6 @@ Chat.prototype = {
 		this.userMap = new Object();
 		this.colorList = Chat.copyColors(NICK_COLORS);
 		Chat.__connections.push(this);
-		
-		this.connect('Please choose a nick name and channel.');
 	},
 	
 	connect: function (msg) {
@@ -93,6 +94,8 @@ Chat.prototype = {
 			});
 			return list;
 		}
+		if (!msg)
+			msg = 'Please choose a nick and channel.';
 		var content =
 			'<p>' + msg + '</p>' +
 			'<fieldset>' +
@@ -104,25 +107,24 @@ Chat.prototype = {
 			'	<label for="nickname">Nick:</label>' +
 			'</fieldset>';
 		var options = { width: 300, height: 200, top: 200, left: 100 };
-		var modal = new ModalDialog("Login", content, options,
+		new ModalDialog("Login", content, options,
 				this.gotNick.bind(this));
 	},
 	
 	gotNick: function (dialog, result) {
 		if (result == ModalDialog.OK) {
-			var nick = $('nickname').value;
+		    this.nick = $('nickname').value;
 			var select = $('channel');
 			this.channel = select.options[select.selectedIndex].value;
 			dialog.close();
+			this.mainWindow();
 			var ajax = new Ajax.Request('../irc/IRCServlet', {
 				method: 'post',
-				parameters: 'nick=' + encodeURIComponent(nick) + 
+				parameters: 'nick=' + encodeURIComponent(this.nick) + 
 					'&channel=' + encodeURIComponent(this.channel),
 				onComplete: this.connected.bind(this),
 				onFailure: this.connectFailed.bind(this)
 			});
-		} else {
-			this.chatWindow.close();
 		}
 	},
 	
@@ -132,54 +134,71 @@ Chat.prototype = {
 	
 	connected: function (response) {
 		this.session = response.getResponseHeader('X-IRC-Session');
-		alert(this.session);
-		this.mainWindow();
 		$(this.handle + '_progress').style.display = 'none';
-		$(this.handle).src = '../irc/IRCServlet?session=' + this.session;
+		
+		// create the hidden iframe that will keep our connection
+		// with the server
+		new Insertion.Top(document.body, 
+			'<iframe class="irclog" id="' + this.handle + '" style="display: none;"></iframe>');
+		var iframe = $(this.handle);
+		iframe.src = '../irc/IRCServlet?session=' + this.session;
+		Event.observe(iframe.window, 'load', function (ev) {
+			alert('Reloading ...');
+			iframe.src = '../irc/IRCServlet?session=' + this.session;
+		}, false);
 	},
 	
 	mainWindow: function () {
 		var html =
-			'<div class="irc_main">' +
-			'	<div class="irc_content" id="' + this.handle + '_o">' +
-			'		<img id="' + this.handle + '_progress" src="images/indicator_medium.gif"/>' +
-			'		<div class="irclog" id="' + this.handle + '_t">' +
+			'<div class="irc">' +
+			'	<div class="irc_main">' +
+			'		<div class="irc_content" id="' + this.handle + '_o">' +
+			'			<img id="' + this.handle + '_progress" src="images/indicator_medium.gif"/>' +
+			'			<table class="irclog" id="' + this.handle + '_t">' +
+			'				<tbody><tr><td></td></tr></tbody>' +
+			'			</table>' +
+			'		</div>' +
+			'		<div class="irc_actions">' +
+			'			<button type="button" id="' + this.handle + '_btn_reload">Refresh Connection</button>' +
+			'			<button type="button" id="' + this.handle + '_btn_disconnect">Disconnect</button>' +
+			'		</div>' +
+			'		<div class="ircinput">' +
+			'			<textarea id="' + this.handle + '_i"></textarea>' +
 			'		</div>' +
 			'	</div>' +
-			'	<div class="ircinput">' +
-			'		<textarea id="' + this.handle + '_i"></textarea>' +
+			'	<div class="irc_userlist" id="' + this.handle + '_users">' +
+			'		<ul id="' + this.handle + '_users_l">' +
+			'		</ul>' +
 			'	</div>' +
-			'</div>' +
-			'<div class="irc_userlist" id="' + this.handle + '_users">' +
-			'	<ul id="' + this.handle + '_users_l">' +
-			'	</ul>' +
 			'</div>';
 		var options = { width: 800, height: 500, top: 100, left: 100,
-				closeHandler: this.close.bind(this) };
-		this.chatWindow = new FloatingWindow(this.nick + '@' + this.channel, html, options);
-		var dim = this.chatWindow.getDimensions();
+				closeHandler: this.close.bind(this),
+				resizeHandler: this.resize.bind(this) 
+		};
+		this.chatWindow = new FloatingWindow(this.nick + '@' + this.channel, options);
+		this.chatWindow.setContent(html);
+		var input = $(this.handle + '_i');
+		
+		Event.observe(input, 'keypress', this.sendMessage.bindAsEventListener(this), false);
+		
+		Event.observe(this.handle + '_btn_reload', 'click', this.refresh.bindAsEventListener(this), false);
+		Event.observe(this.handle + '_btn_disconnect', 'click', this.disconnect.bindAsEventListener(this), false);
 		
 		this.chatWindow.display();
-		
+	},
+	
+	resize: function (dimensions) {
 		var input = $(this.handle + '_i');
 		var div = $(this.handle + '_o');
 		div.style.height = 
-			(dim.height - div.offsetTop - input.parentNode.offsetHeight - 25) + 'px';
+			(dimensions.height - div.offsetTop - input.parentNode.offsetHeight - 44) + 'px';
 		div.style.overflow = 'auto';
 		
-		Event.observe(input, 'keypress', this.sendMessage.bindAsEventListener(this), false);
 		input.style.width = div.offsetWidth + 'px';
 		
 		div = $(this.handle + '_users');
-		div.style.height = 
-			(dim.height - div.offsetTop - input.parentNode.offsetHeight - 25) + 'px';
+		div.style.height = (dimensions.height - div.offsetTop - 10) + 'px';
 		div.style.overflow = 'auto';
-		
-		// create the hidden iframe that will keep our connection
-		// with the server
-		var iframe = 
-			'<iframe class="irclog" id="' + this.handle + '" style="display: none;"></iframe>';
-		new Insertion.Top(document.body, iframe);
 	},
 	
 	sendMessage: function (event) {
@@ -205,6 +224,10 @@ Chat.prototype = {
 	close: function (handle) {
 		if (this.usersWindow)
 			this.usersWindow.close();
+		this.closeChannel();
+	},
+	
+	closeChannel: function () {
 		var ajax = new Ajax.Request('../irc/IRCServlet', {
 			method: 'post',
 			parameters: 'close=true&session=' + this.session,
@@ -218,32 +241,31 @@ Chat.prototype = {
 	
 	dispatchEvent: function (ev, args) {
 		switch (ev) {
+			case Chat.EV_PING :
+				// server sent a ping and waits for a response
+				new Ajax.Request('../irc/IRCServlet', {
+					method: 'post',
+					parameters: 'pong=true&session=' + this.session,
+					onFailure: function (response) {
+						alert('Connection to server lost?');
+					}
+				});
+				break;
 			case Chat.EV_MESSAGE :
-				var color = this.userMap[args[0]] ? this.userMap[args[0]] : DEFAULT_NICK_COLOR;
-				var html =
-					'<div class="irc_line">' +
-					'	<span class="message">' + args[1] + '</span>' +
-					'	<span class="nick" style="color: ' + color + '">' + args[0] + '</span>' +
-					'</div>';
-				new Insertion.Bottom($(this.handle + '_t'), html);
+				this.writeLine('message', args[1], args[0]);
 				break;
 			case Chat.EV_NOTICE :
-				var html =
-					'<div class="notice">' + args[0] + '</div>';
-				new Insertion.Bottom(this.handle + '_t', html);
+				this.writeLine('notice', args[0]);
 				break;
 			case Chat.EV_JOIN :
 				this.colorList = Chat.copyColors(NICK_COLORS);
-				var html =
-					'<div class="join">' + args[1] + '</div>';
-				new Insertion.Bottom(this.handle + '_t', html);
+				this.writeLine('join', args[1]);
 				this.userMap[args[0]] = this.colorList.pop();
 				this.updateUserList();
 				break;
 			case Chat.EV_PART :
-				var html =
-					'<div class="part">' + args[1] + '</div>';
-				new Insertion.Bottom(this.handle + '_t', html);
+				this.writeLine('part', args[1]);
+				this.colorList.push(this.userMap[args[0]]);
 				delete this.userMap[args[0]];
 				this.updateUserList();
 				break;
@@ -255,7 +277,39 @@ Chat.prototype = {
 				break;
 		}
 		var div = $(this.handle + '_o');
-		div.scrollTop = div.scrollHeight;
+		if (div)
+			div.scrollTop = div.scrollHeight;
+	},
+	
+	writeLine: function (className, message, nick) {
+		var tbody = $(this.handle + '_t').tBodies[0];
+		var time = new Date();
+		var timeStr = Chat.formatDayMonth(time.getHours()) + ':' + 
+			Chat.formatDayMonth(time.getMinutes()) + ':' + 
+			Chat.formatDayMonth(time.getSeconds());
+				
+		var tr = document.createElement('tr');
+		tr.className = 'irc_line';
+		tbody.appendChild(tr);
+		
+		var td = document.createElement('td');
+		td.className = 'time';
+		tr.appendChild(td);
+		td.appendChild(document.createTextNode(timeStr));
+		
+		td = document.createElement('td');
+		if (nick) {
+			var color = this.userMap[nick] ? this.userMap[nick] : DEFAULT_NICK_COLOR;
+			td.style.color = color;
+			td.className = 'nick';
+			td.appendChild(document.createTextNode(nick));
+		}
+		tr.appendChild(td);
+		
+		td = document.createElement('td');
+		tr.appendChild(td);
+		td.appendChild(document.createTextNode(message));
+		td.className = className;
 	},
 	
 	updateUserList: function () {
@@ -265,191 +319,43 @@ Chat.prototype = {
 			var color = this.userMap[user];
 			new Insertion.Bottom(userDiv, '<li style="color: ' + color + '">' + user + '</li>');
 		}
+	},
+	
+	refresh: function () {
+		new Ajax.Request('../irc/IRCServlet', {
+			method: 'post',
+			parameters: 'refresh=true&session=' + this.session,
+			onComplete: this.refreshDone.bind(this),
+			onFailure: function (response) {
+				alert('Connection to server lost?');
+			}
+		});
+		new Insertion.Bottom(this.handle + '_t', '<div class="notice">refreshing connection ...</div>');
+	},
+	
+	refreshDone: function (response) {
+		var iframe = $(this.handle);
+		iframe.src = '../irc/IRCServlet?session=' + this.session;
+		new Insertion.Bottom(this.handle + '_t', '<div class="notice">refresh done.</div>');
+	},
+	
+	disconnect: function () {
+		this.closeChannel();
+		var iframe = $(this.handle);
+		iframe.src = '';
+		new Insertion.Bottom(this.handle + '_t', '<div class="notice">refresh done.</div>');
 	}
 };
 
-Event.observe(window, 'unload', Chat.unload, false);
+Event.observe(window, 'unload', documentUnload, false);
 
-var ModalDialog = Class.create();
-
-ModalDialog.OK = 0;
-ModalDialog.CANCEL = 1;
-
-ModalDialog.prototype = {
-	
-	mResult: null,
-	mCallback: null,
-	
-	initialize: function (title, content, options, callback) {
-		this.mCallback = callback;
-		var pageDim = getPageSize();
-		var width = options.width;
-		options.left = (pageDim.pageWidth - width) / 2;
-		options.zIndex = 1001;
-		var html =
-			'<div class="modal">' +
-			'	<div class="modal_content">' + content + '</div>' +
-			'	<div class="modal_buttons">' +
-			'		<button id="modal_btn_ok" type="button">Ok</button>' +
-			'		<button id="modal_btn_cancel" type="button">Cancel</button>' +
-			'	</div>' +
-			'</div>';
-		
-		this.initOverlay(pageDim);
-		this.win = new FloatingWindow(title, html, options);
-		
-		Event.observe($('modal_btn_ok'), 'click', this.btnClick.bindAsEventListener(this), false);
-		Event.observe($('modal_btn_cancel'), 'click', this.btnClick.bindAsEventListener(this), false);
-		
-		this.win.display();
-	},
-	
-	initOverlay: function (pageDim) {
-		var objOverlay = $('overlay');
-		if (objOverlay) {
-			objOverlay.style.display = 'block';
-		} else {
-			new Insertion.Top(document.body, '<div id="overlay"/>');
-			objOverlay = $('overlay');
-			Element.setStyle(objOverlay, {
-				position: 'absolute',
-				top: '0', left: '0',
-				zIndex: '1000', width: '100%',
-				height: pageDim.pageHeight + 'px'
-			});
-		}
-	},
-	
-	btnClick: function (event) {
-		var button = Event.element(event);
-		if (button.id == 'modal_btn_ok')
-			this.mResult = ModalDialog.OK;
-		else
-			this.mResult = ModalDialog.CANCEL;
-		this.mCallback(this, this.mResult);
-	},
-	
-	close: function () {
-		this.win.close();
-		$('overlay').style.display = 'none';
+function documentUnload() {
+	if (confirm('Moving away from this page will close all ' +
+			'active connections. Are you sure?')) {
+		Chat.__connections.each(function (chat) {
+			chat.close();
+		});
+		return true;
 	}
-};
-
-var FloatingWindow = Class.create();
-
-FloatingWindow.__counter = 0;
-FloatingWindow.__zIndex = 1;
-
-FloatingWindow.prototype = {
-	
-	initialize: function (title, content, options) {
-		this.handle = 'win' + FloatingWindow.__counter++;
-		this.contentId = this.handle + '_content';
-		if (options.closeHandler)
-			// remember handler to be called upon close
-			this.closeHandler = options.closeHandler;
-			
-		var html =
-			'<div id="' + this.handle +'" class="window" style="display: none">' +
-			'	<img class="window_closeButton" src="images/close.gif" />' +
-			'	<h1 id="' + this.handle + '_title">' + title + '</h1>' +
-			'	<div id="' + this.contentId + '" class="window_content">' + 
-			content + '</div>' +
-			'</div>';
-		// insert the window div into the document
-		new Insertion.Top(document.body, html);
-		var win = $(this.handle);
-		if (options.zIndex)
-			win.style.zIndex = options.zIndex;
-		else
-			win.style.zIndex = FloatingWindow.__zIndex++;
-		if (options.width)
-			win.style.width = options.width + 'px';
-		if (options.height)
-			win.style.height = options.height + 'px';
-		if (options.left)
-			win.style.left = options.left + 'px';
-		if (options.right)
-			win.style.right = options.right + 'px';
-		win.style.top = options.top + 'px';
-		
-		// attach a listener to the close image
-		var img = win.getElementsByTagName('img');
-		Event.observe(img[0], 'click', this.close.bind(this), false);
-		
-		var content = $(this.contentId);
-		var dim = Element.getDimensions(win);
-		content.style.height = (dim.height - content.offsetTop - 26) + 'px';
-		new Draggable(win, { revert: false, handle: this.handle + '_title' });
-	},
-	
-	display: function() {
-		// new Effect.Appear(this.handle);
-		$(this.handle).style.display = '';
-	},
-	
-	close: function() {
-		document.body.removeChild($(this.handle));
-		if (this.closeHandler)
-			this.closeHandler(this.handle);
-	},
-	
-	getContentId: function() {
-		return this.contentId;
-	},
-	
-	getDimensions: function() {
-		var win = $(this.handle);
-		win.style.visibility = 'hidden';
-		win.style.display = 'block';
-		var dim = Element.getDimensions(this.contentId);
-		win.style.display = 'none';
-		win.style.visibility = '';
-		return dim;
-	}
-};
-
-function getPageSize(){
-	
-	var xScroll, yScroll;
-	
-	if (window.innerHeight && window.scrollMaxY) {	
-		xScroll = document.body.scrollWidth;
-		yScroll = window.innerHeight + window.scrollMaxY;
-	} else if (document.body.scrollHeight > document.body.offsetHeight){ // all but Explorer Mac
-		xScroll = document.body.scrollWidth;
-		yScroll = document.body.scrollHeight;
-	} else { // Explorer Mac...would also work in Explorer 6 Strict, Mozilla and Safari
-		xScroll = document.body.offsetWidth;
-		yScroll = document.body.offsetHeight;
-	}
-	
-	var windowWidth, windowHeight;
-	if (self.innerHeight) {	// all except Explorer
-		windowWidth = self.innerWidth;
-		windowHeight = self.innerHeight;
-	} else if (document.documentElement && document.documentElement.clientHeight) { // Explorer 6 Strict Mode
-		windowWidth = document.documentElement.clientWidth;
-		windowHeight = document.documentElement.clientHeight;
-	} else if (document.body) { // other Explorers
-		windowWidth = document.body.clientWidth;
-		windowHeight = document.body.clientHeight;
-	}	
-	
-	// for small pages with total height less then height of the viewport
-	if(yScroll < windowHeight){
-		pageHeight = windowHeight;
-	} else { 
-		pageHeight = yScroll;
-	}
-
-	// for small pages with total width less then width of the viewport
-	if(xScroll < windowWidth){	
-		pageWidth = windowWidth;
-	} else {
-		pageWidth = xScroll;
-	}
-
-	return { pageWidth: pageWidth, pageHeight: pageHeight, windowWidth: windowWidth,
-		windowHeight: windowHeight };
+	return false;
 }
