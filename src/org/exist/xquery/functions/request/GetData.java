76720be51f90dd24432915f7f95e40659c2cce94
Adam Retter <adam.retter@googlemail.com>
2006-05-02 10:40:06 +0000

    If the Request body was an XML document then this function will now return the XML document as a node, otherwise it will fallback to returning a string representation of the Request body.
    svn path=/trunk/eXist-1.0/; revision=3394

--- src/org/exist/xquery/functions/request/GetData.java
+++ src/org/exist/xquery/functions/request/GetData.java
@@ -22,12 +22,22 @@
  */
 package org.exist.xquery.functions.request;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.net.MalformedURLException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
 
 import org.exist.dom.QName;
 import org.exist.http.servlets.RequestWrapper;
+import org.exist.memtree.DocumentImpl;
+import org.exist.memtree.SAXAdapter;
 import org.exist.xquery.BasicFunction;
 import org.exist.xquery.Cardinality;
 import org.exist.xquery.FunctionSignature;
@@ -39,6 +49,10 @@ import org.exist.xquery.value.Sequence;
 import org.exist.xquery.value.SequenceType;
 import org.exist.xquery.value.StringValue;
 import org.exist.xquery.value.Type;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+import org.xml.sax.XMLReader;
 
 
 /**
@@ -52,9 +66,9 @@ public class GetData extends BasicFunction {
 				"get-data",
 				RequestModule.NAMESPACE_URI,
 				RequestModule.PREFIX),
-			"Returns the content of a POST request as string",
+			"Returns the content of a POST request as an XML document or a string representaion",
 			null,
-			new SequenceType(Type.STRING, Cardinality.ZERO_OR_ONE));
+			new SequenceType(Type.ITEM, Cardinality.ZERO_OR_ONE));
 	
 	public GetData(XQueryContext context) {
 		super(context, signature);
@@ -63,38 +77,105 @@ public class GetData extends BasicFunction {
 	/* (non-Javadoc)
 	 * @see org.exist.xquery.BasicFunction#eval(org.exist.xquery.value.Sequence[], org.exist.xquery.value.Sequence)
 	 */
-	public Sequence eval(Sequence[] args, Sequence contextSequence)
-			throws XPathException {
-		RequestModule myModule =
-			(RequestModule) context.getModule(RequestModule.NAMESPACE_URI);
+	public Sequence eval(Sequence[] args, Sequence contextSequence)throws XPathException {
+		
+		RequestModule myModule = (RequestModule) context.getModule(RequestModule.NAMESPACE_URI);
 
 		// request object is read from global variable $request
 		Variable var = myModule.resolveVariable(RequestModule.REQUEST_VAR);
 		if(var == null)
 			throw new XPathException("No request object found in the current XQuery context.");
-		if (var.getValue().getItemType() != Type.JAVA_OBJECT)
+		if(var.getValue().getItemType() != Type.JAVA_OBJECT)
 			throw new XPathException("Variable $request is not bound to an Java object.");
 		JavaObjectValue value = (JavaObjectValue) var.getValue().itemAt(0);
-		if (value.getObject() instanceof RequestWrapper) {
-			RequestWrapper request = (RequestWrapper)value.getObject();
-			try {
+		
+		if(value.getObject() instanceof RequestWrapper)
+		{
+			RequestWrapper request = (RequestWrapper)value.getObject();	
+			
+			//first, get the content of the request
+			byte[] bufRequestData = null;
+			try
+			{
 				InputStream is = request.getInputStream();
-				ByteArrayOutputStream bos = new ByteArrayOutputStream(request
-						.getContentLength());
+				ByteArrayOutputStream bos = new ByteArrayOutputStream(request.getContentLength());
 				byte[] buf = new byte[256];
 				int l = 0;
-				while ((l = is.read(buf)) > -1) {
+				while ((l = is.read(buf)) > -1)
+				{
 					bos.write(buf, 0, l);
 				}
-				String encoding = request.getCharacterEncoding();
-				if(encoding == null)
-					encoding = "UTF-8";
-				String s = new String(bos.toByteArray(), encoding);
+				bufRequestData = bos.toByteArray();
+			}
+			catch(IOException ioe)
+			{
+				throw new XPathException("An IO exception ocurred: " + ioe.getMessage(), ioe);
+			}
+			
+			//if the POST content is not null, it may be an xml document, so we first try to return the xml document
+			if(bufRequestData != null)
+			{				
+				try
+				{	//TODO: code taken from DocUtils, could add a parse(byte[]) method to DocUtils to share the code? 
+					Sequence document = Sequence.EMPTY_SEQUENCE;
+					
+					//try and construct xml document from input stream, we use eXist's in-memory DOM implementation
+					DocumentImpl memtreeDoc = null;
+					SAXParserFactory factory = SAXParserFactory.newInstance();
+					factory.setNamespaceAware(true);	
+					//TODO : we should be able to cope with context.getBaseURI()				
+					InputSource src = new InputSource(new ByteArrayInputStream(bufRequestData));
+					SAXParser parser = factory.newSAXParser();
+					XMLReader reader = parser.getXMLReader();
+					SAXAdapter adapter = new SAXAdapter();
+					reader.setContentHandler(adapter);
+					reader.parse(src);					
+					Document doc = adapter.getDocument();
+					memtreeDoc = (org.exist.memtree.DocumentImpl)doc;
+					memtreeDoc.setContext(context);
+					document = memtreeDoc;
+					
+					if(document != Sequence.EMPTY_SEQUENCE)
+					{
+						//return the xml document
+						return document;
+					}
+				}
+				catch (ParserConfigurationException e)
+				{				
+					//do nothing, we will default to trying to return a string below
+				}
+				catch (SAXException e)
+				{
+					//do nothing, we will default to trying to return a string below
+				}
+				catch (IOException e)
+				{
+					//do nothing, we will default to trying to return a string below
+				}						
+			}
+			
+			//else, return a string representation of the xml document
+			String encoding = request.getCharacterEncoding();
+			if(encoding == null)
+			{
+				encoding = "UTF-8";
+			}
+			
+			try
+			{
+				String s = new String(bufRequestData, encoding);
 				return new StringValue(s);
-			} catch (IOException e) {
+			
+			}
+			catch (IOException e)
+			{
 				throw new XPathException("An IO exception ocurred: " + e.getMessage(), e);
 			}
-		} else
+		}
+		else
+		{
 			throw new XPathException("Variable $request is not bound to a Request object.");
+		}
 	}
 }
