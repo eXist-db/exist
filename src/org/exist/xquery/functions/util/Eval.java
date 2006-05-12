e3f8ef50ca1840c2d6ba6071c64cdf980962cd34
Wolfgang Meier <wolfgangmm@gmail.com>
2006-05-12 08:20:00 +0000

    util:eval can now load the query to be executed from an URI if the query argument is of type xs:anyURI. Example: util:eval("xmldb:exist:///db/test/test.xq") will evaluate the main XQuery module stored in /db/test/test.xq in the database.
    svn path=/trunk/eXist-1.0/; revision=3511

--- src/org/exist/xquery/functions/util/Eval.java
+++ src/org/exist/xquery/functions/util/Eval.java
@@ -23,13 +23,22 @@
 package org.exist.xquery.functions.util;
 
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URISyntaxException;
 
+import org.exist.dom.BinaryDocument;
+import org.exist.dom.DocumentImpl;
 import org.exist.dom.DocumentSet;
 import org.exist.dom.QName;
 import org.exist.memtree.ReferenceNode;
+import org.exist.security.PermissionDeniedException;
+import org.exist.source.DBSource;
 import org.exist.source.Source;
+import org.exist.source.SourceFactory;
 import org.exist.source.StringSource;
 import org.exist.storage.XQueryPool;
+import org.exist.storage.lock.Lock;
+import org.exist.xmldb.XmldbURI;
 import org.exist.xquery.BasicFunction;
 import org.exist.xquery.Cardinality;
 import org.exist.xquery.CompiledXQuery;
@@ -39,6 +48,7 @@ import org.exist.xquery.XQuery;
 import org.exist.xquery.XQueryContext;
 import org.exist.xquery.value.BooleanValue;
 import org.exist.xquery.value.EmptySequence;
+import org.exist.xquery.value.Item;
 import org.exist.xquery.value.NodeValue;
 import org.exist.xquery.value.Sequence;
 import org.exist.xquery.value.SequenceType;
@@ -58,33 +68,51 @@ public class Eval extends BasicFunction {
 		new FunctionSignature(
 				new QName("eval", UtilModule.NAMESPACE_URI, UtilModule.PREFIX),
 				"Dynamically evaluates its string argument as an XPath/XQuery expression. " +
+                "The query is specified via the first argument. If it is of type xs:string, util:eval " +
+                "tries to execute this string as the query. If the first argument is an xs:anyURI, " +
+                "the function will try to load the query from the resource to which the URI resolves. " +
+                "If the URI has no scheme, it is assumed that the query is stored in the db and the " +
+                "URI is interpreted as a database path. This is the same as calling " +
+                "util:eval('xmldb:exist:///db/test/test.xq'). " +
 				"The argument expression will inherit the current execution context, i.e. all " +
 				"namespace declarations and variable declarations are visible from within the " +
 				"inner expression. It will return an empty sequence if you pass a whitespace string.",
 				new SequenceType[] {
-					new SequenceType(Type.STRING, Cardinality.EXACTLY_ONE),
+					new SequenceType(Type.ITEM, Cardinality.EXACTLY_ONE),
 				},
 				new SequenceType(Type.NODE, Cardinality.ZERO_OR_MORE)),
 		new FunctionSignature(
 				new QName("eval", UtilModule.NAMESPACE_URI, UtilModule.PREFIX),
 				"Dynamically evaluates its string argument as an XPath/XQuery expression. " +
+                "The query is specified via the first argument. If it is of type xs:string, util:eval " +
+                "tries to execute this string as the query. If the first argument is an xs:anyURI, " +
+                "the function will try to load the query from the resource to which the URI resolves. " +
+                "If the URI has no scheme, it is assumed that the query is stored in the db and the " +
+                "URI is interpreted as a database path. This is the same as calling " +
+                "util:eval('xmldb:exist:///db/test/test.xq'). " +
 				"The argument expression will inherit the current execution context, i.e. all " +
 				"namespace declarations and variable declarations are visible from within the " +
 				"inner expression. It will return an empty sequence if you pass a whitespace string.",
 				new SequenceType[] {
-					new SequenceType(Type.STRING, Cardinality.EXACTLY_ONE),
+					new SequenceType(Type.ITEM, Cardinality.EXACTLY_ONE),
 					new SequenceType(Type.BOOLEAN, Cardinality.EXACTLY_ONE)
 				},
 				new SequenceType(Type.NODE, Cardinality.ZERO_OR_MORE)),
 		new FunctionSignature(
 				new QName("eval-with-context", UtilModule.NAMESPACE_URI, UtilModule.PREFIX),
 				"Dynamically evaluates its string argument as an XPath/XQuery expression. " +
+                "The query is specified via the first argument. If it is of type xs:string, util:eval " +
+                "tries to execute this string as the query. If the first argument is an xs:anyURI, " +
+                "the function will try to load the query from the resource to which the URI resolves. " +
+                "If the URI has no scheme, it is assumed that the query is stored in the db and the " +
+                "URI is interpreted as a database path. This is the same as calling " +
+                "util:eval('xmldb:exist:///db/test/test.xq'). " +
 				"A new execution context will be created before the expression is evaluated. Static " +
 				"context properties can be set via the XML fragment in the second parameter. The " +
 				"XML fragment should have the format: <static-context><variable name=\"qname\">" +
 				"variable value</variable></static-context>.",
 				new SequenceType[] {
-					new SequenceType(Type.STRING, Cardinality.EXACTLY_ONE),
+					new SequenceType(Type.ITEM, Cardinality.EXACTLY_ONE),
 					new SequenceType(Type.NODE, Cardinality.ZERO_OR_ONE),
 					new SequenceType(Type.BOOLEAN, Cardinality.EXACTLY_ONE)
 				},
@@ -92,22 +120,36 @@ public class Eval extends BasicFunction {
 		new FunctionSignature(
 				new QName("eval-inline", UtilModule.NAMESPACE_URI, UtilModule.PREFIX),
 				"Dynamically evaluates the XPath/XQuery expression specified in $b within " +
-				"the current instance of the query engine. The evaluation context is taken from " +
+				"the current instance of the query engine. " +
+                "The query is specified via the first argument. If it is of type xs:string, util:eval " +
+                "tries to execute this string as the query. If the first argument is an xs:anyURI, " +
+                "the function will try to load the query from the resource to which the URI resolves. " +
+                "If the URI has no scheme, it is assumed that the query is stored in the db and the " +
+                "URI is interpreted as a database path. This is the same as calling " +
+                "util:eval('xmldb:exist:///db/test/test.xq'). " +
+                "The evaluation context is taken from " +
 				"argument $a.",
 				new SequenceType[] {
 					new SequenceType(Type.ITEM, Cardinality.ZERO_OR_MORE),
-					new SequenceType(Type.STRING, Cardinality.EXACTLY_ONE)
+					new SequenceType(Type.ITEM, Cardinality.EXACTLY_ONE)
 				},
 				new SequenceType(Type.ITEM, Cardinality.ZERO_OR_MORE)),
 		new FunctionSignature(
 				new QName("eval-inline", UtilModule.NAMESPACE_URI, UtilModule.PREFIX),
 				"Dynamically evaluates the XPath/XQuery expression specified in $b within " +
-				"the current instance of the query engine. The evaluation context is taken from " +
+				"the current instance of the query engine. " +
+                "The query is specified via the first argument. If it is of type xs:string, util:eval " +
+                "tries to execute this string as the query. If the first argument is an xs:anyURI, " +
+                "the function will try to load the query from the resource to which the URI resolves. " +
+                "If the URI has no scheme, it is assumed that the query is stored in the db and the " +
+                "URI is interpreted as a database path. This is the same as calling " +
+                "util:eval('xmldb:exist:///db/test/test.xq'). " +
+                "The evaluation context is taken from " +
 				"argument $a. The third argument, $c, specifies if the compiled query expression " +
 				"should be cached. The cached query will be globally available within the db instance.",
 				new SequenceType[] {
 					new SequenceType(Type.ITEM, Cardinality.ZERO_OR_MORE),
-					new SequenceType(Type.STRING, Cardinality.EXACTLY_ONE),
+					new SequenceType(Type.ITEM, Cardinality.EXACTLY_ONE),
 					new SequenceType(Type.BOOLEAN, Cardinality.EXACTLY_ONE)
 				},
 				new SequenceType(Type.ITEM, Cardinality.ZERO_OR_MORE))
@@ -134,10 +176,18 @@ public class Eval extends BasicFunction {
 			exprContext = args[argCount++];
 		}
 		// get the query expression
-		String expr = StringValue.expand(args[argCount++].getStringValue());
-		if ("".equals(expr.trim()))
-		  return new EmptySequence();
+        Item expr = args[argCount++].itemAt(0);
+        Source querySource;
+        if (Type.subTypeOf(expr.getType(), Type.ANY_URI)) {
+            querySource = loadQueryFromURI(expr);
+        } else {
+            String queryStr = expr.getStringValue();
+            if ("".equals(queryStr.trim()))
+              return new EmptySequence();
+            querySource = new StringSource(expr.getStringValue());
+        }
 		
+        LOG.debug("Source: " + querySource.getClass().getName());
 		NodeValue contextInit = null;
 		if (isCalledAs("eval-with-context")) {
 			// set the context initialization param for later use
@@ -159,10 +209,9 @@ public class Eval extends BasicFunction {
 			context.getProfiler().start(this, "eval: " + expr);
 		
 		Sequence sequence = null;
-		Source source = new StringSource(expr);
 		XQuery xquery = context.getBroker().getXQueryService();
 		XQueryPool pool = xquery.getXQueryPool();
-		CompiledXQuery compiled = cache ? pool.borrowCompiledXQuery(context.getBroker(), source) : null;
+		CompiledXQuery compiled = cache ? pool.borrowCompiledXQuery(context.getBroker(), querySource) : null;
 		XQueryContext innerContext;
 		if (contextInit != null) {
 			// eval-with-context: initialize a new context
@@ -173,7 +222,7 @@ public class Eval extends BasicFunction {
 			innerContext = context;
 		try {
 			if(compiled == null) {
-			    compiled = xquery.compile(innerContext, source);
+			    compiled = xquery.compile(innerContext, querySource);
 			} else {
 				compiled.setContext(innerContext);
 			}
@@ -185,7 +234,7 @@ public class Eval extends BasicFunction {
 			throw new XPathException(getASTNode(), e.getMessage(), e);
 		} finally {
 			if (cache && compiled!=null)
-				pool.returnCompiledXQuery(source, compiled);
+				pool.returnCompiledXQuery(querySource, compiled);
 			if (oldDocs != null)
 				context.setStaticallyKnownDocuments(oldDocs);
 			context.popNamespaceContext();
@@ -193,6 +242,58 @@ public class Eval extends BasicFunction {
 				context.getProfiler().end(this, "eval: " + expr, sequence);
 		}
 	}
+
+    /**
+     * @param expr
+     * @param querySource
+     * @return
+     * @throws XPathException
+     * @throws NullPointerException
+     * @throws IllegalArgumentException
+     */
+    private Source loadQueryFromURI(Item expr) throws XPathException, NullPointerException, IllegalArgumentException {
+        String location = expr.getStringValue();
+        Source querySource = null;
+        if (location.indexOf(':') < 0 || location.startsWith(XmldbURI.XMLDB_URI_PREFIX)) {
+            try {
+                XmldbURI locationUri = XmldbURI.xmldbUriFor(location);
+                XmldbURI moduleLoadPathUri = XmldbURI.xmldbUriFor(context.getModuleLoadPath());
+                locationUri = moduleLoadPathUri.resolveCollectionPath(locationUri);
+                
+                DocumentImpl sourceDoc = null;
+                try {
+                    sourceDoc = context.getBroker().getXMLResource(locationUri.toCollectionPathURI(), Lock.READ_LOCK);
+                    if (sourceDoc == null)
+                        throw new XPathException("source for module " + location + " not found in database");
+                    if (sourceDoc.getResourceType() != DocumentImpl.BINARY_FILE ||
+                            !sourceDoc.getMetadata().getMimeType().equals("application/xquery"))
+                        throw new XPathException("source for module " + location + " is not an XQuery or " +
+                        "declares a wrong mime-type");
+                    querySource = new DBSource(context.getBroker(), (BinaryDocument) sourceDoc, true);
+                } catch (PermissionDeniedException e) {
+                    throw new XPathException("permission denied to read module source from " + location);
+                } finally {
+                    if(sourceDoc != null)
+                        sourceDoc.getUpdateLock().release(Lock.READ_LOCK);
+                }
+            } catch(URISyntaxException e) {
+                throw new XPathException(e.getMessage(), e);
+            }
+        } else {
+            // No. Load from file or URL
+            try {
+                //TODO: use URIs to ensure proper resolution of relative locations
+                querySource = SourceFactory.getSource(context.getModuleLoadPath(), location, true);
+            } catch (MalformedURLException e) {
+                throw new XPathException("source location for query at " + location + " should be a valid URL: " +
+                        e.getMessage());
+            } catch (IOException e) {
+                throw new XPathException("source for query at " + location + " not found: " +
+                        e.getMessage());
+            }
+        }
+        return querySource;
+    }
 
 	/**
 	 * Read to optional static-context fragment to initialize
