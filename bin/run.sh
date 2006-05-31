6058b3dfa8963478a6ac415305bbf8659cf2e3c3
Wolfgang Meier <wolfgangmm@gmail.com>
2006-05-31 20:23:08 +0000

    changed run.sh to use start.jar and pass profiler settings
    svn path=/trunk/eXist-1.0/; revision=3638

--- bin/run.sh
+++ bin/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 #unset LANG
 
+YJP_HOME=/home/wolf/Java/yjp-5.5.2
+
 if [ -z "$EXIST_HOME" ]; then
     EXIST_HOME_1=`dirname "$0"`
     EXIST_HOME=`dirname "$EXIST_HOME_1"`
@@ -15,22 +17,16 @@ if [ -z "$EXIST_BASE" ]; then
     EXIST_BASE=$EXIST_HOME
 fi
 
-LOCALCLASSPATH=$JAVA_HOME/lib/tools.jar:$EXIST_BASE/exist.jar:$EXIST_BASE/exist-optional.jar:$EXIST_BASE/examples.jar:$EXIST_BASE/tools/ant/lib/junit.jar:$EXIST_BASE/tools/ant/lib/xmlunit1.0.jar:$EXIST_BASE/tools/jetty/lib/org.mortbay.jetty.jar
-
-JARS=`ls -1 $EXIST_BASE/lib/core/*.jar $EXIST_BASE/lib/optional/*.jar`
-for jar in $JARS
-do
-   LOCALCLASSPATH=$jar:$LOCALCLASSPATH ;
-done
-
-LOCALCLASSPATH=$CLASSPATH:$LOCALCLASSPATH
-
 if [ -z "$JAVA_OPTS" ]; then
     JAVA_OPTS="-Xms64000k -Xmx128000k -Dfile.encoding=UTF-8"
 fi
 
 JAVA_ENDORSED_DIRS="$EXIST_HOME"/lib/endorsed
 
-$JAVA_HOME/bin/java $PROF $JAVA_OPTS \
+PROFILER_OPTS=-agentlib:yjpagent
+export LD_LIBRARY_PATH="$YJP_HOME/bin/linux-amd64/"
+
+$JAVA_HOME/bin/java $JAVA_OPTS \
 	-Djava.endorsed.dirs=$JAVA_ENDORSED_DIRS \
-	-Dexist.home=$EXIST_HOME -classpath $LOCALCLASSPATH $*
+	-Dexist.home=$EXIST_HOME $PROFILER_OPTS \
+	-jar start.jar $*
