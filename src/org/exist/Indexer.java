90d5b424ffa1da829c74ec389958ec16e7773fba
alexmilowski <alex@milowski.com>
2006-07-17 18:39:42 +0000

    Fixed javadoc comment on constructor.
    svn path=/trunk/eXist-1.0/; revision=3866
    
    
    Former-commit-id: ba86624e446e791031a7795724b8eeb43f573dcd

--- src/org/exist/Indexer.java
+++ src/org/exist/Indexer.java
@@ -132,10 +132,9 @@ public class Indexer extends Observable implements ContentHandler, LexicalHandle
 	 *  Create a new parser using the given database broker and
 	 * user to store the document.
 	 *
-	 *@param  broker              
-	 *@param  user                user identity
-	 *@param  replace             replace existing documents?
-	 *@param  privileged		  used by the security manager to
+	 *@param  broker              The database broker to use.
+         *@param  transaction         The transaction to use for indexing
+	 *@param  priv		      used by the security manager to
 	 *							  indicate that it needs privileged
 	 *                            access to the db.
 	 *@exception  EXistException  
