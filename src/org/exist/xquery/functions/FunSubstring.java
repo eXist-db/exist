e1bd1d0d4f7d9dc43190e0d1771f60be1fab9b39
Adam Retter <adam.retter@googlemail.com>
2006-08-07 20:41:05 +0000

    New version of fn:substring(), cleaner easier to read code with comments. fixes bug 1535920
    svn path=/trunk/eXist-1.0/; revision=3978

--- src/org/exist/xquery/functions/FunSubstring.java
+++ src/org/exist/xquery/functions/FunSubstring.java
@@ -42,6 +42,7 @@ import org.exist.xquery.value.Type;
 /**
  * Built-in function fn:substring().
  *
+ *	@author Adam Retter <adam.retter@devon.gov.uk>
  */
 public class FunSubstring extends Function {
 	
@@ -73,8 +74,10 @@ public class FunSubstring extends Function {
 		return Type.STRING;
 	}
 		
-	public Sequence eval(Sequence contextSequence, Item contextItem) throws XPathException {
-        if (context.getProfiler().isEnabled()) {
+	public Sequence eval(Sequence contextSequence, Item contextItem) throws XPathException
+	{
+		//start profiler
+		if (context.getProfiler().isEnabled()) {
             context.getProfiler().start(this);       
             context.getProfiler().message(this, Profiler.DEPENDENCIES, "DEPENDENCIES", Dependency.getDependenciesName(this.getDependencies()));
             if (contextSequence != null)
@@ -83,66 +86,176 @@ public class FunSubstring extends Function {
                 context.getProfiler().message(this, Profiler.START_SEQUENCES, "CONTEXT ITEM", contextItem.toSequence());
         }  
         
-        Expression arg0 = getArgument(0);
-		Expression arg1 = getArgument(1);
-		Expression arg2 = null;
-		if (getArgumentCount() > 2)
-			arg2 = getArgument(2);
-
+		//get arguments
+        Expression argSourceString = getArgument(0);
+		Expression argStartingLoc = getArgument(1);
+		Expression argLength = null;
+		
+		//get the context sequence
 		if(contextItem != null)
 			contextSequence = contextItem.toSequence();
-        
         Sequence result;
-		Sequence seq = arg0.eval(contextSequence);
-		if(seq.isEmpty())
-			//If the value of $sourceString is the empty sequence, the zero-length string is returned.
+		Sequence seqSourceString = argSourceString.eval(contextSequence);
+		
+		//If the value of $sourceString is the empty sequence return EMPTY_STRING, there must be a string to operate on!
+		if(seqSourceString.isEmpty())
+		{
 			result = StringValue.EMPTY_STRING;
-        else {
-    		String string = seq.getStringValue();
-    		NumericValue startValue = ((NumericValue)(arg1.eval(contextSequence).itemAt(0).convertTo(Type.NUMBER))).round();
-    		if (startValue.isNaN())
-    			result = StringValue.EMPTY_STRING;
-    		else if (startValue.isInfinite())
-    			result = StringValue.EMPTY_STRING;
-    		else {
-    			int start = startValue.getInt();    		
-	    		if (start >= string.length()) 
-	    			//The characters returned do not extend beyond $sourceString
-	    			result = StringValue.EMPTY_STRING;    			
-	    		else if (arg2 == null) {
-	    			//If $startingLoc is zero or negative, only those characters in positions greater than zero are returned.
-	    			//Because we will substract 1 ;-)
-	    			if (start < 1)
-	    				start = 1;
-	    			result = new StringValue(string.substring(start - 1));
-	    		} else {
-	    			NumericValue lengthValue =((NumericValue)(arg2.eval(contextSequence).itemAt(0).convertTo(Type.NUMBER))).round();
-	    			if (lengthValue.isNaN())
-	    				result = StringValue.EMPTY_STRING;
-	    			else if (((NumericValue)lengthValue.plus(startValue)).isInfinite() ||
-	    					((NumericValue)lengthValue.plus(startValue)).getInt() > string.length()) {
-	        			if (start < 0)
-	        				start = 0;        				
-	        			result = new StringValue(string.substring(start));
-	    			} else {
-		    			int length = lengthValue.getInt();
-		    			//If $startingLoc is zero or negative, only those characters in positions greater than zero are returned.
-		    			if (start <= 0) {    				
-		    				//Because we will will add 1
-		    				length = length + start - 1;
-		    				//Because we will substract 1 ;-)
-		    				start = 1;
-		    			}
-		    			result = new StringValue(string.substring(start - 1, start + length - 1));
-	    			}
-	    		}
-    		}
-        }
+		}
+		else
+		{
+			//get the string to substring
+        	String sourceString = seqSourceString.getStringValue();
+    		
+        	//check for a valid start position for the substring
+        	NumericValue startingLoc = ((NumericValue)(argStartingLoc.eval(contextSequence).itemAt(0).convertTo(Type.NUMBER))).round();
+        	if(!validStartPosition(startingLoc, sourceString.length()))
+        	{
+        		//invalid start position
+        		result = StringValue.EMPTY_STRING;
+        	}
+        	else
+        	{
+				//are there 2 or 3 arguments to this function?
+				if(getArgumentCount() > 2)
+				{
+					//three arguments, get the third argument value for the length
+					argLength = getArgument(2);
+					
+					//check for a valid length for the substring
+					NumericValue length = ((NumericValue)(argLength.eval(contextSequence).itemAt(0).convertTo(Type.NUMBER))).round();
+					if(!validLength(length))
+					{
+						//invalid length
+						result = StringValue.EMPTY_STRING;
+					}
+					else
+					{
+						//if the length extends past the end of the string, just return the string from the start position
+						if(length.getInt() > sourceString.length() || startingLoc.getInt() -1 + length.getInt() > sourceString.length() || length.isInfinite())
+						{
+							//fallback to fn:substring(string, start)
+							result = substring(sourceString, startingLoc);
+						}
+						else
+						{
+							//three arguments fn:substring(string, start, length)
+							result = substring(sourceString, startingLoc, length);
+						}
+					}
+				}
+				else
+				{
+					//two arguments fn:substring(string, start)
+					result = substring(sourceString, startingLoc);
+				}
+        	}
+		}
         
+		//end profiler
         if (context.getProfiler().isEnabled()) 
             context.getProfiler().end(this, "", result); 
         
-        return result;    
-        
+        return result;
 	}
+	
+	/**
+	 * Checks that the startPosition is valid for the length of the $sourceString
+	 * 
+	 * @param startPosition		The user specified startPosition for the fn:substring(), start index is 1
+	 * @param stringLength		The length of the $sourceString passed to fn:substring()
+	 * 
+	 * @return true if the startPosition is valid, false otherwise
+	 */
+	private boolean validStartPosition(NumericValue startPosition, int stringLength)
+	{
+		//if start position is not a number return false
+		if(startPosition.isNaN())
+			return false;
+		
+		//if start position is infinite return false
+		if(startPosition.isInfinite())
+			return false;
+		
+		//if the start position extends beyond $sourceString return EMPTY_STRING
+    	try
+    	{
+    		if(startPosition.getInt() >= stringLength) 
+    			return false;
+    	}
+    	catch(XPathException xpe)
+    	{
+    		return false;
+    	}
+    	
+    	//start position is valid
+    	return true;
+	}
+	
+	/**
+	 * Checks that the length is valid for the $sourceString
+	 * 
+	 * @param length		The user specified length for the fn:substring()
+	 * 
+	 * @return true if the length is valid, false otherwise
+	 */
+	private boolean validLength(NumericValue length)
+	{
+		//if length is not a number return false
+		if(length.isNaN())
+			return false;
+		
+		//length is valid
+		return true;
+	}
+	
+	/**
+	 * fn:substring($sourceString, $startingLoc)
+	 * 
+	 * @see http://www.w3.org/TR/xpath-functions/#func-substring
+	 * 
+	 * @param stringSource	The source string to substring
+	 * @param startingLoc	The Starting Location for the substring, start index is 1
+	 * 
+	 * @return The StringValue of the substring
+	 */
+	private StringValue substring(String sourceString, NumericValue startingLoc) throws XPathException
+	{
+		if(startingLoc.getInt() <= 1)
+		{
+			//start value is 1 or less, so just return the string
+			return new StringValue(sourceString);
+		}
+		
+		
+		
+		//start index of xs:string is 1, whereas java string is 0; so subtract 1
+		return new StringValue(sourceString.substring(startingLoc.getInt() - 1));
+	}
+	
+	/**
+	 * fn:substring($sourceString, $startingLoc, $length)
+	 * 
+	 * @see http://www.w3.org/TR/xpath-functions/#func-substring
+	 * 
+	 * @param stringSource	The source string to substring
+	 * @param startingLoc	The Starting Location for the substring, start index is 1
+	 * @param length	The length of the substring
+	 * 
+	 * @return The StringValue of the substring
+	 */
+	private StringValue substring(String sourceString, NumericValue startingLoc, NumericValue length) throws XPathException
+	{
+		//if start value is 1 or less, start at the start of the string and adjust the length appropriately
+		if(startingLoc.getInt() <= 1)
+		{
+			//the -1 is to transition from xs:string index which starts at 1 to Java string index which starts at 0
+			int endIndex = length.getInt() + (-1 + startingLoc.getInt());
+			
+			return new StringValue(sourceString.substring(0, endIndex));
+		}
+		
+		return new StringValue(sourceString.substring(startingLoc.getInt() - 1, startingLoc.getInt() - 1 + length.getInt()));
+	}
+	
 }
