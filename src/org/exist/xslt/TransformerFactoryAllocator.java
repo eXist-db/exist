42111887d02061e8d1a9858b6d72e71b1050906c
Dannes Wessels <dannes@exist-db.org>
2006-08-13 15:43:57 +0000

    Removed System.out.println messages
    svn path=/trunk/eXist-1.0/; revision=4039
    
    
    Former-commit-id: cf66fb59b091c626ed507ece81d6792217ce3e7d

--- src/org/exist/xslt/TransformerFactoryAllocator.java
+++ src/org/exist/xslt/TransformerFactoryAllocator.java
@@ -28,92 +28,89 @@ import org.apache.log4j.Logger;
 import org.exist.storage.DBBroker;
 
 /**
- * Allow's the TransformerFactory that is used for XSLT to be
- * chosen through configuration settings in conf.xml 
- * 
+ * Allows the TransformerFactory that is used for XSLT to be
+ * chosen through configuration settings in conf.xml
+ *
  * Within eXist this class should be used instead of
  * directly calling SAXTransformerFactory.newInstance() directly
- * 
- * @author Adam Retter <adam.retter@devon.gov.uk> 
+ *
+ * @author Adam Retter <adam.retter@devon.gov.uk>
  */
 
-public class TransformerFactoryAllocator
-{
-	/* 
-	 * problem is that even when a Xalan is defined in conf.xml, if Saxon JAR's are on the classpath then
-	 * when we create a Xalan TransformerFactory and newInstance() is called on it, it returns a Saxon TransformerFactory :-(
-	 */
-	
-	private final static Logger LOG = Logger.getLogger(TransformerFactoryAllocator.class);	//Logger
-	
-	//private constructor
-	private TransformerFactoryAllocator()
-	{
-	}
-	
-	/**
+public class TransformerFactoryAllocator {
+    /*
+     * problem is that even when a Xalan is defined in conf.xml, if Saxon JAR's 
+     * are on the classpath then when we create a Xalan TransformerFactory and 
+     * newInstance() is called on it, it returns a Saxon TransformerFactory :-(
+     */
+    
+    private final static Logger LOG = Logger.getLogger(TransformerFactoryAllocator.class);
+    
+    //private constructor
+    private TransformerFactoryAllocator() {
+    }
+    
+    /**
      * Get the TransformerFactory defined in conf.xml
      * If the class can't be found or the given class doesn't implement
      * the required interface, the default factory is returned.
-     * 
-     * @param broker	A database broker, used for reading the conf.xml configuration
-     * 
-     * @return	A SAXTransformerFactory, for which newInstance() can then be called
-     * 
-     * 
-     * Typical useage:
-     * 
+     *
+     * @param broker A database broker, used for reading the conf.xml configuration
+     *
+     * @return  A SAXTransformerFactory, for which newInstance() can then be called
+     *
+     *
+     * Typical usage:
+     *
      * Instead of SAXTransformerFactory.newInstance() use
      * TransformerFactoryAllocator.getTransformerFactory(broker).newInstance()
      */
-	public static SAXTransformerFactory getTransformerFactory(DBBroker broker)
-    {
-    	SAXTransformerFactory factory;
-
-    	//get the transformer class name from conf.xml
-    	String transformerFactoryClassName = (String)broker.getConfiguration().getProperty("transformer.class");
-    
-    	System.out.println("transformerFactoryClassName=" + transformerFactoryClassName);
-    	System.out.println("javax.xml.transform.TransformerFactory=" + System.getProperty("javax.xml.transform.TransformerFactory"));
-    	
-    	
-    	//was a TransformerFactory class specified
-    	if(transformerFactoryClassName == null)
-        {
-    		//no, use the system default
+    public static SAXTransformerFactory getTransformerFactory(DBBroker broker) {
+        SAXTransformerFactory factory;
+        
+        //get the transformer class name from conf.xml
+        String transformerFactoryClassName =
+                (String)broker.getConfiguration().getProperty("transformer.class");
+        
+        LOG.debug("transformerFactoryClassName=" + transformerFactoryClassName);
+        LOG.debug("javax.xml.transform.TransformerFactory="
+                + System.getProperty("javax.xml.transform.TransformerFactory"));
+        
+        
+        //was a TransformerFactory class specified
+        if(transformerFactoryClassName == null) {
+            //no, use the system default
             factory = (SAXTransformerFactory)TransformerFactory.newInstance();
-        }
-        else
-        {
-        	//try and load the specified TransformerFactory class
-            try
-            {
-                factory = (SAXTransformerFactory)Class.forName(transformerFactoryClassName).newInstance();
-            }
-            catch(ClassNotFoundException cnfe)
-            {
-                LOG.debug("Cannot find the requested TrAX factory '" + transformerFactoryClassName + "'. Using default TrAX Transformer Factory instead." );
+        } else {
+            //try and load the specified TransformerFactory class
+            try {
+                factory = (SAXTransformerFactory)
+                Class.forName(transformerFactoryClassName).newInstance();
+            } catch(ClassNotFoundException cnfe) {
+                LOG.debug("Cannot find the requested TrAX factory '"
+                        + transformerFactoryClassName
+                        + "'. Using default TrAX Transformer Factory instead." );
                 
                 //fallback to system default
                 factory = (SAXTransformerFactory)TransformerFactory.newInstance();
-            }
-            catch(ClassCastException cce)
-            {
-                LOG.debug( "The indicated class '" + transformerFactoryClassName + "' is not a TrAX Transformer Factory. Using default TrAX Transformer Factory instead." );
+            } catch(ClassCastException cce) {
+                LOG.debug( "The indicated class '" + transformerFactoryClassName
+                        + "' is not a TrAX Transformer Factory. "
+                        +"Using default TrAX Transformer Factory instead." );
                 
                 //fallback to system default
                 factory = (SAXTransformerFactory)TransformerFactory.newInstance();
-            }
-            catch(Exception e)
-            {
-                LOG.debug( "Error found loading the requested TrAX Transformer Factory '" + transformerFactoryClassName + "'. Using default TrAX Transformer Factory instead." );
+            } catch(Exception e) {
+                LOG.debug( "Error found loading the requested TrAX Transformer Factory '"
+                        + transformerFactoryClassName
+                        + "'. Using default TrAX Transformer Factory instead." );
                 
                 //fallback to system default
                 factory = (SAXTransformerFactory)TransformerFactory.newInstance();
             }
         }
-
+        
         return factory;
     }
-
+    
 }
