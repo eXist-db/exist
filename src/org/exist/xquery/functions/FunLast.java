152f6c2bdc77bfd7bb45bf687305b307dd7118ed
Leif-Jöran Olsson <ljo@fripost.org>
2006-08-14 14:45:46 +0000

    Final batch of describe-function() documentation updates for functions in http://www.w3.org/2003/05/xpath-functions (fn) namespace.
    svn path=/trunk/eXist-1.0/; revision=4070
    
    
    Former-commit-id: 8b8618e0736955daa4d89ba7b2f722c944dfea29

--- src/org/exist/xquery/functions/FunLast.java
+++ src/org/exist/xquery/functions/FunLast.java
@@ -1,20 +1,21 @@
-/* eXist Native XML Database
- * Copyright (C) 2000-2003,  the eXist team
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public License
+/*
+ * eXist Open Source Native XML Database
+ * Copyright (C) 2000-2006 The eXist team
+ *  
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
+ *  
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Library General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- * 
+ *  
  * $Id$
  */
 
