80a2bad62f810313fe9f3935c0b2bc0b6db40679
alexmilowski <alex@milowski.com>
2006-09-25 23:33:01 +0000

    Fixed allowing entities in attribute values constructed from expressions.
    svn path=/trunk/eXist-1.0/; revision=4391

--- src/org/exist/xquery/AttributeConstructor.java
+++ src/org/exist/xquery/AttributeConstructor.java
@@ -96,7 +96,9 @@ public class AttributeConstructor extends NodeConstructor {
 				buf.append(next);
 		}
 		StringValue result = new StringValue(buf.toString());
-		result.expand();
+                // String values as expressions are already expanded by
+                // the parser -- Alex
+		//result.expand();
 		return result;
 	}
 
