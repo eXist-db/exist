c6c29fda91c55b208ce35825e546d459a808e511
Wolfgang Meier <wolfgangmm@gmail.com>
2006-09-28 18:41:30 +0000

    Merged revisions 4404 and 4405 from DLN.
    svn path=/trunk/eXist-1.0/; revision=4433
    
    
    Former-commit-id: 8f4a61801b6412335151f162441b49699803e845

--- src/org/exist/dom/StoredNode.java
+++ src/org/exist/dom/StoredNode.java
@@ -364,6 +364,7 @@ public class StoredNode extends NodeImpl {
 	 * Release all memory resources hold by this node. 
 	 */
 	public void release() {
+        ownerDocument = null;
 		clear();
 		NodeObjectPool.getInstance().returnNode(this);
 	}
