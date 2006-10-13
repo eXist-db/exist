39500273192ac02b270ae8643a02020a74131da8
Dannes Wessels <dannes@exist-db.org>
2006-10-13 21:50:27 +0000

    Moved to BasicFunction, temoved code, added math:random()
    svn path=/trunk/eXist-1.0/; revision=4587
    
    
    Former-commit-id: 3e0315b3190296b7abcc539e85c69d95200f0b10

--- extensions/modules/src/org/exist/xquery/modules/math/Constants.java
+++ extensions/modules/src/org/exist/xquery/modules/math/Constants.java
@@ -1,8 +1,7 @@
 /*
  *  eXist Open Source Native XML Database
- *  Copyright (C) 2001-06 Wolfgang M. Meier
- *  wolfgang@exist-db.org
- *  http://exist.sourceforge.net
+ *  Copyright (C) 2001-06 The eXist Project
+ *  http://exist-db.org
  *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License
@@ -14,23 +13,21 @@
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  */
 package org.exist.xquery.modules.math;
 
 import org.exist.dom.QName;
+import org.exist.xquery.BasicFunction;
 import org.exist.xquery.Cardinality;
 import org.exist.xquery.Dependency;
-import org.exist.xquery.Function;
 import org.exist.xquery.FunctionSignature;
 import org.exist.xquery.Profiler;
 import org.exist.xquery.XPathException;
 import org.exist.xquery.XQueryContext;
 import org.exist.xquery.value.DoubleValue;
-import org.exist.xquery.value.Item;
 import org.exist.xquery.value.Sequence;
 import org.exist.xquery.value.SequenceType;
 import org.exist.xquery.value.Type;
@@ -38,7 +35,7 @@ import org.exist.xquery.value.Type;
 /**
  * @author Dannes Wessels
  */
-public class Constants extends Function {
+public class Constants extends BasicFunction {
     
     public final static FunctionSignature signature[] = {
         //Constant values
@@ -51,6 +48,12 @@ public class Constants extends Function {
         new FunctionSignature(
                 new QName("pi", MathModule.NAMESPACE_URI),
                 "Returns the value of pi.",
+                null,
+                new SequenceType(Type.DOUBLE, Cardinality.EXACTLY_ONE)
+                ),
+        new FunctionSignature(
+                new QName("random", MathModule.NAMESPACE_URI),
+                "Returns a value greater than or equal to 0.0 and less than 1.0.",
                 null,
                 new SequenceType(Type.DOUBLE, Cardinality.EXACTLY_ONE)
                 )
@@ -66,14 +69,12 @@ public class Constants extends Function {
         /* (non-Javadoc)
          * @see org.exist.xquery.Expression#eval(org.exist.dom.DocumentSet, org.exist.xquery.value.Sequence, org.exist.xquery.value.Item)
          */
-    public Sequence eval(Sequence contextSequence, Item contextItem) throws XPathException {
+    public Sequence eval(Sequence[] args, Sequence contextSequence) throws XPathException {
         if (context.getProfiler().isEnabled()) {
             context.getProfiler().start(this);
             context.getProfiler().message(this, Profiler.DEPENDENCIES, "DEPENDENCIES", Dependency.getDependenciesName(this.getDependencies()));
             if (contextSequence != null)
                 context.getProfiler().message(this, Profiler.START_SEQUENCES, "CONTEXT SEQUENCE", contextSequence);
-            if (contextItem != null)
-                context.getProfiler().message(this, Profiler.START_SEQUENCES, "CONTEXT ITEM", contextItem.toSequence());
         }
         
         
@@ -84,6 +85,9 @@ public class Constants extends Function {
             
         } else if("pi".equals(functionName)) {
             result=new DoubleValue(Math.PI);
+            
+        } else if("random".equals(functionName)) {
+            result=new DoubleValue(Math.random());
             
         } else {
             // DWES: can this be thrown here?
