3386b08f6b2db2b3048929dff5ad8fdec57e376c
Wolfgang Meier <wolfgangmm@gmail.com>
2006-10-30 20:46:20 +0000

    Deleted old src/ directory in trunk. To be replaced with branches/DLN/eXist-1.0/src.
    svn path=/trunk/eXist-1.0/; revision=4756

--- src/org/exist/client/xacml/NodeChangeListener.java
+++
@@ -1,8 +0,0 @@
-package org.exist.client.xacml;
-
-public interface NodeChangeListener
-{
-	void nodeChanged(XACMLTreeNode node);
-	void nodeAdded(XACMLTreeNode node, int newIndex);
-	void nodeRemoved(XACMLTreeNode removedNode, int oldChildIndex);
-}
