f231e9e90956f5ae0f8db05543ec7a91089bba5a
Wolfgang Meier <wolfgangmm@gmail.com>
2006-10-30 20:22:56 +0000

    Imported revisions 4723 to 4751 from DLN branch. This is the final sync before moving trunk to a branch
    svn path=/trunk/eXist-1.0/; revision=4754
    
    
    Former-commit-id: 7e4c8c5592a00849982781ea4b9b5183c9da3087

--- src/org/exist/cocoon/XMLDBSourceFactory.java
+++ src/org/exist/cocoon/XMLDBSourceFactory.java
@@ -1,22 +1,20 @@
 /*
- *  eXist Open Source Native XML Database
- *  Copyright (C) 2001-06 Wolfgang M. Meier
- *  wolfgang@exist-db.org
- *  http://exist.sourceforge.net
- *  
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public License
- *  as published by the Free Software Foundation; either version 2
- *  of the License, or (at your option) any later version.
- *  
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *  
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * Extended and slightly adopted version of the original XMLDBSource found in Apache Cocoon.
+ * The original license is:
+ *
+ *  Copyright 1999-2004 The Apache Software Foundation.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  *  
  *  $Id$
  */
