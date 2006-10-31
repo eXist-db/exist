27d999c011a254be7f4ab36348b896d0a77fb676
Wolfgang Meier <wolfgangmm@gmail.com>
2006-10-31 14:58:38 +0000

    Fixed MoveResourceTest. Some tests were commented out.
    svn path=/trunk/eXist/; revision=4759
    
    
    Former-commit-id: 6e80e1c6462e06ab7f6b146ab85fc562df2a7ac1

--- src/org/exist/storage/test/MoveResourceTest.java
+++ src/org/exist/storage/test/MoveResourceTest.java
@@ -39,9 +39,13 @@ import org.exist.storage.txn.Txn;
 import org.exist.test.TestConstants;
 import org.exist.util.Configuration;
 import org.exist.xmldb.XmldbURI;
+import org.exist.xmldb.CollectionManagementServiceImpl;
+import org.exist.EXistException;
 import org.xml.sax.InputSource;
 import org.xmldb.api.DatabaseManager;
 import org.xmldb.api.base.Database;
+import org.xmldb.api.base.Resource;
+import org.xmldb.api.base.XMLDBException;
 
 public class MoveResourceTest extends TestCase {
 
@@ -79,16 +83,16 @@ public class MoveResourceTest extends TestCase {
 			IndexInfo info = test2.validateXMLResource(transaction, broker, TestConstants.TEST_XML_URI, new InputSource(f.toURI().toASCIIString()));
 			assertNotNull(info);
 			test2.store(transaction, broker, info, new InputSource(f.toURI().toASCIIString()), false);
-			
+
             System.out.println("Moving document test.xml to new_test.xml ...");
 			broker.moveXMLResource(transaction, info.getDocument(), root, XmldbURI.create("new_test.xml"));
 			broker.saveCollection(transaction, root);
-            
+
 			transact.commit(transaction);
 			System.out.println("Transaction commited ...");
 		} catch (Exception e) {
             e.printStackTrace();
-	        fail(e.getMessage());  			
+	        fail(e.getMessage());
 		} finally {
 			pool.release(broker);
 		}
@@ -106,135 +110,155 @@ public class MoveResourceTest extends TestCase {
 	        assertNotNull(broker);
 	        Serializer serializer = broker.getSerializer();
 	        serializer.reset();
-	        
+
 	        DocumentImpl doc = broker.getXMLResource(XmldbURI.ROOT_COLLECTION_URI.append("test/new_test.xml"), Lock.READ_LOCK);
 	        assertNotNull("Document should not be null", doc);
 	        String data = serializer.serialize(doc);
 	        assertNotNull(data);
 //	        System.out.println(data);
 	        doc.getUpdateLock().release(Lock.READ_LOCK);
-            
+
             TransactionManager transact = pool.getTransactionManager();
             assertNotNull(transact);
             Txn transaction = transact.beginTransaction();
             assertNotNull(transaction);
             System.out.println("Transaction started ...");
-            
+
             Collection root = broker.openCollection(TestConstants.TEST_COLLECTION_URI, Lock.WRITE_LOCK);
             assertNotNull(root);
-            transaction.registerLock(root.getLock(), Lock.WRITE_LOCK);            
-            broker.removeCollection(transaction, root); 
-            
+            transaction.registerLock(root.getLock(), Lock.WRITE_LOCK);
+            broker.removeCollection(transaction, root);
+
             transact.commit(transaction);
             System.out.println("Transaction commited ...");
 	    } catch (Exception e) {
             e.printStackTrace();
-	        fail(e.getMessage());  	
+	        fail(e.getMessage());
 	    } finally {
 	        pool.release(broker);
 	    }
 	}
-	
-//	public void testStoreAborted() {
-//		BrokerPool.FORCE_CORRUPTION = true;
-//		BrokerPool pool = startDB();
-//
-//		DBBroker broker = null;
-//		try {
-//			broker = pool.get(SecurityManager.SYSTEM_USER);
-//
-//			TransactionManager transact = pool.getTransactionManager();
-//			Txn transaction = transact.beginTransaction();
-//
-//			System.out.println("Transaction started ...");
-//
-//			Collection root = broker.getOrCreateCollection(transaction,
-//					DBBroker.ROOT_COLLECTION +  "/test");
-//			broker.saveCollection(transaction, root);
-//
-//			Collection test = broker.getOrCreateCollection(transaction,
-//					DBBroker.ROOT_COLLECTION + "/test/test2");
-//			broker.saveCollection(transaction, test);
-//
-//			File f = new File("samples/shakespeare/r_and_j.xml");
-//			IndexInfo info = test.validate(transaction, broker, "test2.xml",
-//					new InputSource(f.toURI().toASCIIString()));
-//			test.store(transaction, broker, info, new InputSource(f.toURI()
-//					.toASCIIString()), false);
-//			
-//			transact.commit(transaction);
-//			
-//			transaction = transact.beginTransaction();
-//			
-//			broker.moveResource(transaction, info.getDocument(), root,
-//					"new_test2.xml");
-//			broker.saveCollection(transaction, root);
-//
-//			pool.getTransactionManager().getLogManager().flushToLog(true);
-//		} finally {
-//			pool.release(broker);
-//		}
-//	}
-	
-//	public void testReadAborted() {
-//	    BrokerPool.FORCE_CORRUPTION = false;
-//	    BrokerPool pool = startDB();
-//	    
-//	    System.out.println("testRead() ...\n");
-//	    
-//	    DBBroker broker = null;
-//	    try {
-//	        broker = pool.get(SecurityManager.SYSTEM_USER);
-//	        Serializer serializer = broker.getSerializer();
-//	        serializer.reset();
-//	        
-//	        DocumentImpl doc;
-//	        String data;
-//	        
-//	        doc = broker.openDocument(DBBroker.ROOT_COLLECTION + "/test/test2/test2.xml", Lock.READ_LOCK);
-//	        assertNotNull("Document should not be null", doc);
-//	        data = serializer.serialize(doc);
+
+	public void testStoreAborted() throws Exception {
+		BrokerPool.FORCE_CORRUPTION = true;
+		BrokerPool pool = startDB();
+
+		DBBroker broker = null;
+		try {
+			broker = pool.get(SecurityManager.SYSTEM_USER);
+
+			TransactionManager transact = pool.getTransactionManager();
+			Txn transaction = transact.beginTransaction();
+
+			System.out.println("Transaction started ...");
+
+			Collection root = broker.getOrCreateCollection(transaction,	TestConstants.TEST_COLLECTION_URI);
+			assertNotNull(root);
+			broker.saveCollection(transaction, root);
+
+			Collection test2 = broker.getOrCreateCollection(transaction,TestConstants.TEST_COLLECTION_URI2);
+			assertNotNull(test2);
+			broker.saveCollection(transaction, test2);
+
+			File f = new File("samples/shakespeare/r_and_j.xml");
+			IndexInfo info = test2.validateXMLResource(transaction, broker, XmldbURI.create("new_test2.xml"),
+					new InputSource(f.toURI().toASCIIString()));
+			test2.store(transaction, broker, info, new InputSource(f.toURI()
+					.toASCIIString()), false);
+
+			transact.commit(transaction);
+
+			transaction = transact.beginTransaction();
+
+			broker.moveXMLResource(transaction, info.getDocument(), root,
+					XmldbURI.create("new_test2.xml"));
+			broker.saveCollection(transaction, root);
+
+			pool.getTransactionManager().getJournal().flushToLog(true);
+		} finally {
+			pool.release(broker);
+		}
+	}
+
+public void testReadAborted() {
+	    BrokerPool.FORCE_CORRUPTION = false;
+	    BrokerPool pool = null;
+	    DBBroker broker = null;
+	    try {
+	    	System.out.println("testRead() ...\n");
+	    	pool = startDB();
+	    	assertNotNull(pool);
+	        broker = pool.get(SecurityManager.SYSTEM_USER);
+	        assertNotNull(broker);
+	        Serializer serializer = broker.getSerializer();
+	        serializer.reset();
+
+	        DocumentImpl doc = broker.getXMLResource(TestConstants.TEST_COLLECTION_URI2.append("new_test2.xml"), Lock.READ_LOCK);
+	        assertNotNull("Document should not be null", doc);
+	        String data = serializer.serialize(doc);
+	        assertNotNull(data);
 //	        System.out.println(data);
-//	        doc.getUpdateLock().release(Lock.READ_LOCK);
-//	        
-//	        doc = broker.openDocument(DBBroker.ROOT_COLLECTION +  "/test/new_test2.xml", Lock.READ_LOCK);
-//	        assertNull("Document should not exist", doc);
-//	    } finally {
-//	        pool.release(broker);
-//	    }
-//	}
-//	
-//	public void testXMLDBStore() {
-//		BrokerPool.FORCE_CORRUPTION = false;
-//	    BrokerPool pool = startDB();
-//	    
-//	    org.xmldb.api.base.Collection root = DatabaseManager.getCollection("xmldb:exist://" + DBBroker.ROOT_COLLECTION + , "admin", "");
-//	    CollectionManagementServiceImpl mgr = (CollectionManagementServiceImpl) 
-//	    	root.getService("CollectionManagementService", "1.0");
-//	    org.xmldb.api.base.Collection test = root.getChildCollection("test");
-//	    if (test == null)
-//	    	test = mgr.createCollection("test");
-//	    org.xmldb.api.base.Collection test2 = test.getChildCollection("test2");
-//	    if (test2 == null)
-//	    	test2 = mgr.createCollection("test2");
-//	    
-//	    File f = new File("samples/shakespeare/r_and_j.xml");
-//	    Resource res = test2.createResource("test3.xml", "XMLResource");
-//	    res.setContent(f);
-//	    test2.storeResource(res);
-//	    
-//	    mgr.moveResource(DBBroker.ROOT_COLLECTION +  "/test/test2/test3.xml", TestConstants.TEST_COLLECTION_URI, "new_test3.xml");
-//	}
-//	
-//	public void testXMLDBRead() {
-//		BrokerPool.FORCE_CORRUPTION = false;
-//	    BrokerPool pool = startDB();
-//	    
-//	    org.xmldb.api.base.Collection test = DatabaseManager.getCollection("xmldb:exist://" + DBBroker.ROOT_COLLECTION +  "/test", "admin", "");
-//	    Resource res = test.getResource("new_test3.xml");
-//	    assertNotNull("Document should not be null", res);
-//	    System.out.println(res.getContent());
-//	}
+	        doc.getUpdateLock().release(Lock.READ_LOCK);
+
+            TransactionManager transact = pool.getTransactionManager();
+            assertNotNull(transact);
+            Txn transaction = transact.beginTransaction();
+            assertNotNull(transaction);
+            System.out.println("Transaction started ...");
+
+            Collection root = broker.openCollection(TestConstants.TEST_COLLECTION_URI, Lock.WRITE_LOCK);
+            assertNotNull(root);
+            transaction.registerLock(root.getLock(), Lock.WRITE_LOCK);
+            broker.removeCollection(transaction, root);
+
+            transact.commit(transaction);
+            System.out.println("Transaction commited ...");
+	    } catch (Exception e) {
+            e.printStackTrace();
+	        fail(e.getMessage());
+	    } finally {
+	        pool.release(broker);
+	    }
+	}
+
+	public void testXMLDBStore() throws XMLDBException {
+		BrokerPool.FORCE_CORRUPTION = false;
+	    BrokerPool pool = startDB();
+
+	    org.xmldb.api.base.Collection root = DatabaseManager.getCollection("xmldb:exist://" + DBBroker.ROOT_COLLECTION, "admin", "");
+	    CollectionManagementServiceImpl mgr = (CollectionManagementServiceImpl)
+	    	root.getService("CollectionManagementService", "1.0");
+	    org.xmldb.api.base.Collection test = root.getChildCollection("test");
+	    if (test == null)
+	    	test = mgr.createCollection("test");
+	    org.xmldb.api.base.Collection test2 = test.getChildCollection("test2");
+	    if (test2 == null)
+	    	test2 = mgr.createCollection("test2");
+
+	    File f = new File("samples/shakespeare/r_and_j.xml");
+	    Resource res = test2.createResource("test3.xml", "XMLResource");
+	    res.setContent(f);
+	    test2.storeResource(res);
+
+	    mgr.moveResource(XmldbURI.create(DBBroker.ROOT_COLLECTION +  "/test2/test3.xml"),
+                TestConstants.TEST_COLLECTION_URI, XmldbURI.create("new_test3.xml"));
+	}
+
+	public void testXMLDBRead() throws XMLDBException {
+		BrokerPool.FORCE_CORRUPTION = false;
+	    BrokerPool pool = startDB();
+
+	    org.xmldb.api.base.Collection test = DatabaseManager.getCollection("xmldb:exist://" + DBBroker.ROOT_COLLECTION +  "/test", "admin", "");
+	    Resource res = test.getResource("new_test3.xml");
+	    assertNotNull("Document should not be null", res);
+	    System.out.println(res.getContent());
+
+        org.xmldb.api.base.Collection root = DatabaseManager.getCollection("xmldb:exist://" + DBBroker.ROOT_COLLECTION, "admin", "");
+	    CollectionManagementServiceImpl mgr = (CollectionManagementServiceImpl)
+	    	root.getService("CollectionManagementService", "1.0");
+        mgr.removeCollection(XmldbURI.create("test"));
+        mgr.removeCollection(XmldbURI.create("test2"));
+    }
 	
 	protected BrokerPool startDB() {
 		try {
