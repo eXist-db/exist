77ce03a5f5ad1f748fe5068762871f25c9d8786e
Leif-Jöran Olsson <ljo@fripost.org>
2006-11-30 16:32:05 +0000

    Added maxAge to response:set-cookie() on suggestion from Golikov. He should supply a test case.
    svn path=/trunk/eXist/; revision=4932
    
    
    Former-commit-id: c504870e2620073d4aad7e6a5486f857e2902a66

--- src/org/exist/http/servlets/ResponseWrapper.java
+++ src/org/exist/http/servlets/ResponseWrapper.java
@@ -1,25 +1,25 @@
 /*
- *  eXist Open Source Native XML Database
- *  Copyright (C) 2001-06 Wolfgang M. Meier
- *  wolfgang@exist-db.org
- *  http://exist.sourceforge.net
+ * eXist Open Source Native XML Database
+ * Copyright (C) 2001-2006 The eXist team
+ *  http://exist-db.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *  
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public License
- *  as published by the Free Software Foundation; either version 2
- *  of the License, or (at your option) any later version.
- *  
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *  
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *  
  *  $Id$
  */
+
 package org.exist.http.servlets;
 
 import java.io.IOException;
@@ -36,6 +36,13 @@ public interface ResponseWrapper {
 	 * @param value	Value of the Cookie
 	 */
 	public void addCookie(String name, String value);
+
+	/**
+	 * @param name	Name of the Cookie
+	 * @param value	Value of the Cookie
+	 * @param maxAge maxAge of the Cookie
+	 */
+	public void addCookie(String name, String value, int maxAge);
 	
 	/**
 	 * @param arg0
--- src/org/exist/xquery/functions/response/ResponseModule.java
+++ src/org/exist/xquery/functions/response/ResponseModule.java
@@ -1,25 +1,25 @@
 /*
- *  eXist Open Source Native XML Database
- *  Copyright (C) 2001-06 Wolfgang M. Meier
- *  wolfgang@exist-db.org
- *  http://exist.sourceforge.net
+ * eXist Open Source Native XML Database
+ * Copyright (C) 2001-2006 The eXist team
+ *  http://exist-db.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *  
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public License
- *  as published by the Free Software Foundation; either version 2
- *  of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *  
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *  
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *  
- *  $Id
+ *  $Id$
  */
+
 package org.exist.xquery.functions.response;
 
 import org.exist.dom.QName;
@@ -38,7 +38,8 @@ public class ResponseModule extends AbstractInternalModule {
 	
 	public static final FunctionDef[] functions = {
 		new FunctionDef(RedirectTo.signature, RedirectTo.class),
-		new FunctionDef(SetCookie.signature, SetCookie.class),
+		new FunctionDef(SetCookie.signatures[0], SetCookie.class),
+		new FunctionDef(SetCookie.signatures[1], SetCookie.class),
 		new FunctionDef(SetHeader.signature, SetHeader.class),
 		new FunctionDef(StreamBinary.signature, StreamBinary.class)
 	};
