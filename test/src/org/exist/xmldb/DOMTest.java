b16639dcbf9b09d8d0c7266ec78f11a08479ae95
Pierrick Brihaye <pierrick.brihaye@free.fr>
2006-12-03 18:22:37 +0000

    Don't catch encoding exception
    svn path=/trunk/eXist/; revision=4948
    
    
    Former-commit-id: a1ce98a41f5a15d8f3ee425809a8dfce49e9b69d

--- test/src/org/exist/xmldb/DOMTest.java
+++ test/src/org/exist/xmldb/DOMTest.java
@@ -283,11 +283,7 @@ public class DOMTest {
 			} else {
 				String s = (String) resource.getContent();
 				byte[] bytes;
-				try {
-					bytes = s.getBytes("UTF-8");
-				} catch (UnsupportedEncodingException e) {
-					bytes = s.getBytes();
-				}
+				bytes = s.getBytes("UTF-8");
 				ByteArrayInputStream bais = new ByteArrayInputStream(bytes);
 				DocumentBuilder db =
 					DocumentBuilderFactory.newInstance().newDocumentBuilder();
