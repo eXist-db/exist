00d5b7a888661c77bfe3812166ab815338aa01f4
Pierrick Brihaye <pierrick.brihaye@free.fr>
2006-12-03 18:21:22 +0000

    Fixed Wolf's last commit
    svn path=/trunk/eXist/; revision=4947

--- src/org/exist/xquery/functions/ExtRegexp.java
+++ src/org/exist/xquery/functions/ExtRegexp.java
@@ -173,7 +173,7 @@ public class ExtRegexp extends Function {
 				context.getBroker().getTextEngine().getNodesContaining(
 				    context,
 					nodes.getDocumentSet(),
-					nodes, null,
+					nodes, 
 					(String)terms.get(k), DBBroker.MATCH_REGEXP);
 		}
 		NodeSet result = hits[0];
