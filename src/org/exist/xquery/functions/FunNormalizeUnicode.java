25f71f34391317fcf7d5d48c9dda3c5371e2e261
Pierrick Brihaye <pierrick.brihaye@free.fr>
2006-12-04 20:26:41 +0000

    Improved normalize-unicode()
    svn path=/trunk/eXist/; revision=4957
    
    
    Former-commit-id: ed7a32419e8ab530b90269d644cde5c16a3f0254

--- src/org/exist/xquery/functions/FunNormalizeUnicode.java
+++ src/org/exist/xquery/functions/FunNormalizeUnicode.java
@@ -20,6 +20,10 @@
 
 package org.exist.xquery.functions;
 
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
 import org.exist.dom.QName;
 import org.exist.xquery.Cardinality;
 import org.exist.xquery.Dependency;
@@ -35,6 +39,14 @@ import org.exist.xquery.value.StringValue;
 import org.exist.xquery.value.Type;
 
 public class FunNormalizeUnicode extends CollatingFunction {
+	
+	private String normalizationForm = null;
+	private Class clazz = null;	
+	private Field  modeField = null;
+	private Object modeObject = null;
+	private static Integer DUMMY_INTEGER = new Integer(0);
+	private Constructor constructor = null;
+	private Method method = null;
 
     public final static FunctionSignature signatures [] = {
     	new FunctionSignature(
@@ -77,34 +89,52 @@ public class FunNormalizeUnicode extends CollatingFunction {
         
 		if (contextItem != null)
 		    contextSequence = contextItem.toSequence();
-
-		Sequence s1 = getArgument(0).eval(contextSequence);
-		//String s2 = getArgument(1).eval(contextSequence).getStringValue();
 		
         Sequence result;
+
+        Sequence s1 = getArgument(0).eval(contextSequence);
         if (s1.isEmpty())
             result = StringValue.EMPTY_STRING;
         else {
-        	Object returnedObject = null;
-        	try {
-        		//TODO : don't recreate each time
-	        	Class clazz = Class.forName("com.ibm.icu.text.Normalizer");
-	        	java.lang.reflect.Field modeField = clazz.getField("NFC");
-	        	java.lang.reflect.Constructor constructor = clazz.getConstructor(
-	        			new Class[] { String.class, modeField.getType(), Integer.TYPE}
-	        		);
-	        	//com.ibm.icu.text.Normalizer.Mode
-	        	Object mode = modeField.get(null);
-	        	Object[] args = new Object[] { s1.getStringValue(), mode, new Integer(0) };
-	        	//Normalizer n = new Normalizer(s1.getStringValue(), Normalizer.NFC, 0);
-	        	Object instance = constructor.newInstance(args);
-	        	java.lang.reflect.Method method = clazz.getMethod("getText", null);
-	        	//result = new StringValue(n.getText());
-	        	returnedObject = method.invoke(instance, null);
-        	} catch (Exception e) {
-        		throw new XPathException("Can not find the ICU4J library in the classpath");
-        	}
-        	result = new StringValue((String)returnedObject);
+			String newNormalizationForm = "NFC";
+			if (getArgumentCount() > 1)
+				newNormalizationForm = getArgument(1).eval(contextSequence).getStringValue().toUpperCase().trim();
+			//TODO : handle the "FULLY-NORMALIZED" string...
+			if ("".equals(newNormalizationForm))
+				result =  new StringValue(s1.getStringValue());
+			else {
+				Object returnedObject = null;
+				try {
+	        		if (clazz == null)
+	        			clazz = Class.forName("com.ibm.icu.text.Normalizer");
+	        		if (modeField == null || !normalizationForm.equals(newNormalizationForm)) {
+	        			try {
+	        				modeField = clazz.getField(newNormalizationForm);
+	        			} catch (NoSuchFieldException e) {
+	        				throw new XPathException("FOCH0003: unknown normalization form");
+	        			}
+	    	        	//com.ibm.icu.text.Normalizer.Mode
+	            		modeObject = modeField.get(null);
+	        			normalizationForm = newNormalizationForm;
+	        		}
+	        		if (constructor == null)
+	        			//Second argument shouldn't be a problem : modeField always has the same type
+	            		constructor = clazz.getConstructor(
+	            				new Class[] { String.class, modeField.getType(), Integer.TYPE}
+	    	        		);
+		        	Object[] args = new Object[] { s1.getStringValue(), modeObject, DUMMY_INTEGER };
+		        	if (method == null)
+		        		method = clazz.getMethod("getText", null);
+	
+		        	//Normalizer n = new Normalizer(s1.getStringValue(), Normalizer.NFC, 0);
+		        	Object instance = constructor.newInstance(args);
+		        	//result = new StringValue(n.getText());
+		        	returnedObject = method.invoke(instance, null);
+        		} catch (Exception e) {
+        			throw new XPathException("Can not find the ICU4J library in the classpath");
+        		}
+        		result = new StringValue((String)returnedObject);
+			}
         }
         
         if (context.getProfiler().isEnabled()) 
