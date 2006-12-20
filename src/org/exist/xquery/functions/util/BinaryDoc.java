92a7de7194bda2fca04ba2a3c1887a3d7957f258
Wolfgang Meier <wolfgangmm@gmail.com>
2006-12-20 12:42:45 +0000

    Bug fix: acquired lock not properly released in functions util:binary-doc and util:binary-doc-available.
    svn path=/trunk/eXist/; revision=5053
    
    
    Former-commit-id: 4ec0e90d65c22dc9347bfbbc9d017042be8fe154

--- src/org/exist/xquery/functions/util/BinaryDoc.java
+++ src/org/exist/xquery/functions/util/BinaryDoc.java
@@ -71,8 +71,9 @@ public class BinaryDoc extends BasicFunction {
         if (args[0].isEmpty())
             return defaultReturn;
         String path = args[0].getStringValue();
+        DocumentImpl doc = null;
         try {
-            DocumentImpl doc = context.getBroker().getXMLResource(XmldbURI.xmldbUriFor(path), Lock.READ_LOCK);
+            doc = context.getBroker().getXMLResource(XmldbURI.xmldbUriFor(path), Lock.READ_LOCK);
             if (doc == null)
                 return defaultReturn;
             if (doc.getResourceType() != DocumentImpl.BINARY_FILE)
@@ -87,6 +88,9 @@ public class BinaryDoc extends BasicFunction {
             throw new XPathException(getASTNode(), "Invalid resource uri",e);
         } catch (PermissionDeniedException e) {
             throw new XPathException(getASTNode(), path + ": permission denied to read resource");
+        } finally {
+            if (doc != null)
+                doc.getUpdateLock().release(Lock.READ_LOCK);
         }
     }
 }
