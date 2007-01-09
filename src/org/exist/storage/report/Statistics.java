3c2f9e337bdaa36072058ab0d3e312eab9246e8d
Pierrick Brihaye <pierrick.brihaye@free.fr>
2007-01-09 13:26:36 +0000

    Index files now register themselves as content loading observers. Still investigating to find why they can't deregister themselves !
    svn path=/trunk/eXist/; revision=5167

--- src/org/exist/storage/report/Statistics.java
+++ src/org/exist/storage/report/Statistics.java
@@ -5,9 +5,15 @@ $Id$
 package org.exist.storage.report;
 
 import java.util.Map;
+
 import org.exist.storage.IndexStats;
+import org.exist.storage.NativeElementIndex;
+import org.exist.storage.NativeTextEngine;
+import org.exist.storage.NativeValueIndex;
+import org.exist.storage.NativeValueIndexByQName;
 import org.exist.storage.dom.DOMFile;
 import org.exist.storage.index.BFile;
+import org.exist.storage.index.CollectionStore;
 import org.exist.util.Configuration;
 
 /**
@@ -20,18 +26,25 @@ public class Statistics {
 	 * @param indexStats
 	 */
 	public static void generateIndexStatistics(Configuration conf, Map indexStats) {
-		BFile db = (BFile) conf.getProperty("db-connection.elements");
-		if(db != null) 
-			indexStats.put("elements.dbx", new IndexStats(db));
-		db = (BFile) conf.getProperty("db-connection.collections");
-		if(db != null)
-			indexStats.put("collections.dbx", new IndexStats(db));
-		db = (BFile) conf.getProperty("db-connection.words");
-		if(db != null)
-			indexStats.put("words.dbx", new IndexStats(db));
-		DOMFile dom = (DOMFile) conf.getProperty("db-connection.dom");
+		DOMFile dom = (DOMFile) conf.getProperty(DOMFile.FILE_KEY_IN_CONFIG);
 		if(dom != null)
-			indexStats.put("dom.dbx", new IndexStats(dom));
+			indexStats.put(DOMFile.FILE_NAME, new IndexStats(dom));
+		BFile db = (BFile) conf.getProperty(CollectionStore.FILE_KEY_IN_CONFIG);
+		if(db != null)
+			indexStats.put(CollectionStore.FILE_NAME, new IndexStats(db));
+		db = (BFile) conf.getProperty(NativeElementIndex.FILE_KEY_IN_CONFIG);
+		if(db != null) 
+			indexStats.put(NativeElementIndex.FILE_NAME, new IndexStats(db));	
+		db = (BFile) conf.getProperty(NativeValueIndex.FILE_KEY_IN_CONFIG);
+		if(db != null) 
+			indexStats.put(NativeValueIndex.FILE_NAME, new IndexStats(db));	
+		db = (BFile) conf.getProperty(NativeValueIndexByQName.FILE_KEY_IN_CONFIG);
+		if(db != null) 
+			indexStats.put(NativeValueIndexByQName.FILE_NAME, new IndexStats(db));			
+		db = (BFile) conf.getProperty(NativeTextEngine.FILE_KEY_IN_CONFIG);
+		if(db != null)
+			indexStats.put(NativeTextEngine.FILE_NAME, new IndexStats(db));
+
 	}
 
 }
