ea23bfe73efc8a1ee846da06fa0e01435844459b
Pierrick Brihaye <pierrick.brihaye@free.fr>
2007-04-06 22:15:11 +0000

    Added fn:default-collation()
    svn path=/trunk/eXist/; revision=5607
    
    
    Former-commit-id: aeb8622f3f89623bbb68b1f38e7b9ffb14389748

---
+++ src/org/exist/xquery/functions/FunDefaultCollation.java
@@ -0,0 +1,73 @@
+/*
+ * eXist Open Source Native XML Database
+ * Copyright (C) 2000-2007 The eXist team
+ *  
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *  
+ * $Id: FunLast.java 5547 2007-03-28 19:46:33Z brihaye $
+ */
+
+package org.exist.xquery.functions;
+
+import org.exist.dom.QName;
+import org.exist.xquery.BasicFunction;
+import org.exist.xquery.Cardinality;
+import org.exist.xquery.Dependency;
+import org.exist.xquery.Function;
+import org.exist.xquery.FunctionSignature;
+import org.exist.xquery.Profiler;
+import org.exist.xquery.XPathException;
+import org.exist.xquery.XQueryContext;
+import org.exist.xquery.value.Sequence;
+import org.exist.xquery.value.SequenceType;
+import org.exist.xquery.value.StringValue;
+import org.exist.xquery.value.Type;
+
+/**
+ * Built-in function fn:last().
+ * 
+ * @author wolf
+ */
+public class FunDefaultCollation extends BasicFunction {
+
+	public final static FunctionSignature signature =
+		new FunctionSignature(
+			new QName("default-collation", Function.BUILTIN_FUNCTION_NS),
+			"Returns the context's default collation.",
+			null,
+			new SequenceType(Type.STRING, Cardinality.EXACTLY_ONE));
+
+	public FunDefaultCollation(XQueryContext context) {
+		super(context, signature);
+	}
+
+	public Sequence eval(Sequence[] args, Sequence contextSequence) throws XPathException {
+        if (context.getProfiler().isEnabled()) {
+            context.getProfiler().start(this);       
+            context.getProfiler().message(this, Profiler.DEPENDENCIES, "DEPENDENCIES", Dependency.getDependenciesName(this.getDependencies()));
+            if (contextSequence != null)
+                context.getProfiler().message(this, Profiler.START_SEQUENCES, "CONTEXT SEQUENCE", contextSequence);
+        }
+        
+        Sequence result = new StringValue(context.getDefaultCollation());
+        
+        if (context.getProfiler().isEnabled()) 
+            context.getProfiler().end(this, "", result); 
+        
+        return result;           
+		
+	}
+
+}
