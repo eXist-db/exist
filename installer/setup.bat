8f80cdc17d794024c736366a940c3d917416c4a1
Wolfgang Meier <wolfgangmm@gmail.com>
2007-04-15 19:44:43 +0000

    Trying to fix installer issues with setting the db password.
    svn path=/trunk/eXist/; revision=5683

---
+++ installer/setup.bat
@@ -0,0 +1,19 @@
+@echo off
+
+rem will be set by the installer
+set EXIST_HOME=$INSTALL_PATH
+
+set JAVA_HOME=$JAVA_HOME
+
+:gotJavaHome
+set JAVA_CMD="%JAVA_HOME%\bin\java"
+
+set JAVA_ENDORSED_DIRS="%EXIST_HOME%"\lib\endorsed
+set JAVA_OPTS="-Xms16000k -Xmx128000k -Djava.endorsed.dirs=%JAVA_ENDORSED_DIRS%"
+
+echo "JAVA_HOME: %JAVA_HOME%"
+echo "EXIST_HOME: %EXIST_HOME%"
+
+%JAVA_CMD% "%JAVA_OPTS%" -Dexist.home="%EXIST_HOME%" -jar "%EXIST_HOME%\start.jar" org.exist.Setup %1 %2 %3 %4 %5 %6 %7 %8 %9
+
+:eof
