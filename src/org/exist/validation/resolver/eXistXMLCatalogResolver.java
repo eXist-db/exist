26531630d87445d1c712ca85275cb45607e6b975
Dannes Wessels <dannes@exist-db.org>
2007-04-26 19:17:40 +0000

    Wrapper around xerces XMLCatalogResolver ; will be used for the upcoming new validation code.
    svn path=/trunk/eXist/; revision=5737

---
+++ src/org/exist/validation/resolver/eXistXMLCatalogResolver.java
@@ -0,0 +1,54 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2001-07 The eXist Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  $Id$
+ */
+
+package org.exist.validation.resolver;
+
+import java.io.IOException;
+
+import org.apache.log4j.Logger;
+import org.apache.xerces.util.XMLCatalogResolver;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+/**
+ *  Wrapper around xerces2's
+ *  <a href="http://xerces.apache.org/xerces2-j/javadocs/xerces2/org/apache/xerces/util/XMLCatalogResolver.html"
+ *                                                      >XMLCatalogresolver</a>
+ * @author Dannes Wessels
+ */
+public class eXistXMLCatalogResolver extends XMLCatalogResolver {
+    
+    private final static Logger LOG = Logger.getLogger(eXistXMLCatalogResolver.class);
+    
+    /**
+     * @see org.apache.xerces.util.XMLCatalogResolve#resolveEntity(String, String)
+     */
+    public InputSource resolveEntity(String publicId, String systemId)
+    throws SAXException, IOException {
+        LOG.debug("Resolving '"+publicId+"','"+systemId+"'");
+        InputSource is = super.resolveEntity(publicId, systemId);
+        LOG.debug("Resolved "+(is!=null));
+        return is;
+    }
+    
+}
