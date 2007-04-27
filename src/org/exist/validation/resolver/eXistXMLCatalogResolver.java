b60bedd1636113c24910ecc118885f51658242bc
Dannes Wessels <dannes@exist-db.org>
2007-04-27 15:19:46 +0000

    Updated resolver for upcoming validation feature..........
    svn path=/trunk/eXist/; revision=5740
    
    
    Former-commit-id: dab91599935fc3f30586a4080b4d285e059b812f

--- src/org/exist/validation/resolver/eXistXMLCatalogResolver.java
+++ src/org/exist/validation/resolver/eXistXMLCatalogResolver.java
@@ -26,6 +26,11 @@ import java.io.IOException;
 
 import org.apache.log4j.Logger;
 import org.apache.xerces.util.XMLCatalogResolver;
+import org.apache.xerces.xni.XMLResourceIdentifier;
+import org.apache.xerces.xni.XNIException;
+import org.apache.xerces.xni.parser.XMLInputSource;
+
+import org.w3c.dom.ls.LSInput;
 
 import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
@@ -34,21 +39,57 @@ import org.xml.sax.SAXException;
  *  Wrapper around xerces2's
  *  <a href="http://xerces.apache.org/xerces2-j/javadocs/xerces2/org/apache/xerces/util/XMLCatalogResolver.html"
  *                                                      >XMLCatalogresolver</a>
- * @author Dannes Wessels
+ * @author Dannes Wessels (dizzzz@exist-db.org)
  */
 public class eXistXMLCatalogResolver extends XMLCatalogResolver {
     
     private final static Logger LOG = Logger.getLogger(eXistXMLCatalogResolver.class);
     
     /**
-     * @see org.apache.xerces.util.XMLCatalogResolve#resolveEntity(String, String)
+     * @see org.apache.xerces.util.XMLCatalogResolver#resolveEntity(String, String)
      */
-    public InputSource resolveEntity(String publicId, String systemId)
-    throws SAXException, IOException {
+    public InputSource resolveEntity(String publicId, String systemId) throws SAXException, IOException {
         LOG.debug("Resolving '"+publicId+"','"+systemId+"'");
-        InputSource is = super.resolveEntity(publicId, systemId);
-        LOG.debug("Resolved "+(is!=null));
-        return is;
+        InputSource retValue = super.resolveEntity(publicId, systemId);
+        LOG.debug("Resolved "+(retValue!=null));
+        return retValue;
     }
+    
+    /**
+     * @see org.apache.xerces.util.XMLCatalogResolver#resolveResource(String, String, String, String, String)
+     */
+    public LSInput resolveResource(String type, String namespaceURI, String publicId, String systemId, String baseURI) {
+        LOG.debug("Resolving '"+type+"','"+namespaceURI+"','"+publicId+"','"+systemId+"','"+baseURI+"'");
+        LSInput retValue= super.resolveResource(type, namespaceURI, publicId, systemId, baseURI);
+        return retValue;
+    }
+    
+    /**
+     * @see org.apache.xerces.util.XMLCatalogResolver#resolveEntity(String, String, String, String)
+     */
+    public InputSource resolveEntity(String name, String publicId, String baseURI, String systemId) throws SAXException, IOException {
+        LOG.debug("Resolving '"+name+"','"+publicId+"','"+baseURI+"','"+systemId+"'");
+        InputSource retValue = super.resolveEntity(name, publicId, baseURI, systemId);
+        return retValue;
+    }
+    
+    /**
+     * @see org.apache.xerces.util.XMLCatalogResolver#resolveIdentifier(XMLResourceIdentifier)
+     */
+    public String resolveIdentifier(XMLResourceIdentifier xri) throws IOException, XNIException {
+        LOG.debug("Resolving '"+xri.toString()+"'");
+        String retValue = super.resolveIdentifier(xri);
+        return retValue;
+    }
+    
+    /**
+     * @see org.apache.xerces.util.XMLCatalogResolve#resolveEntity(XMLResourceIdentifier)
+     */
+    public XMLInputSource resolveEntity(XMLResourceIdentifier xri) throws XNIException, IOException {
+        LOG.debug("Resolving '"+xri.toString()+"'");
+        XMLInputSource retValue = super.resolveEntity(xri);
+        return retValue;
+    }
+    
     
 }
