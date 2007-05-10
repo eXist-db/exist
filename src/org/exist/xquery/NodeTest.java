96114fc5ceb390af22ab3bb574a2403b35e82bf6
Leif-Jöran Olsson <ljo@fripost.org>
2007-05-10 01:54:38 +0000

    Cosmetic changes, preparing functional commit.
    svn path=/trunk/eXist/; revision=5792
    
    
    Former-commit-id: 1c2531e1ef2eb27b4a8687ea0e67e6cd4a606a85

--- src/org/exist/xquery/NodeTest.java
+++ src/org/exist/xquery/NodeTest.java
@@ -1,20 +1,77 @@
+/*
+ * eXist Open Source Native XML Database
+ * Copyright (C) 2001-2007 The eXist Project
+ * http://exist-db.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *  
+ *  $Id$
+ */
 package org.exist.xquery;
 
 import org.exist.dom.NodeProxy;
 import org.exist.dom.QName;
 import org.w3c.dom.Node;
 
+/**
+ * The interface <code>NodeTest</code>
+ *
+ */
 public interface NodeTest {
 
+    /**
+     * The method <code>setType</code>
+     *
+     * @param nodeType an <code>int</code> value
+     */
     public void setType(int nodeType);
     
+    /**
+     * The method <code>getType</code>
+     *
+     * @return an <code>int</code> value
+     */
     public int getType();
     
-	public boolean matches(NodeProxy proxy);
-
-	public boolean matches(Node node);
-	
-	public boolean isWildcardTest();
-	
-	public QName getName();
+    /**
+     * The method <code>matches</code>
+     *
+     * @param proxy a <code>NodeProxy</code> value
+     * @return a <code>boolean</code> value
+     */
+    public boolean matches(NodeProxy proxy);
+    
+    /**
+     * The method <code>matches</code>
+     *
+     * @param node a <code>Node</code> value
+     * @return a <code>boolean</code> value
+     */
+    public boolean matches(Node node);
+    
+    /**
+     * The method <code>isWildcardTest</code>
+     *
+     * @return a <code>boolean</code> value
+     */
+    public boolean isWildcardTest();
+    
+    /**
+     * The method <code>getName</code>
+     *
+     * @return a <code>QName</code> value
+     */
+    public QName getName();
 }
