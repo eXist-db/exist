c749da53c0f400cc49a3bec060b9643ca2a446a5
Leif-Jöran Olsson <ljo@fripost.org>
2007-05-10 07:11:00 +0000

    Finally a functional commit again, which makes some further progress in XQTS, at least test Axes041-1 and ForExprType tests 036, 055, 056, 058  and 062 now pass. Eventhough the testsuite scared the hell out of me, this seems safe, since the the tests were finally passing when run individually. Can someone confirm testsuite failure fluctuation with template conf.xml?
    svn path=/trunk/eXist/; revision=5793
    
    
    Former-commit-id: f7835f34cf46ca998b342d2869c026db6676b260

--- src/org/exist/xquery/AnyNodeTest.java
+++ src/org/exist/xquery/AnyNodeTest.java
@@ -1,7 +1,23 @@
 /*
- * AnyNodeTest.java - Aug 30, 2003
+ * eXist Open Source Native XML Database
+ * Copyright (C) 2003 The eXist Project
+ * http://exist-db.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  * 
- * @author wolf
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *  
+ *  $Id$
  */
 package org.exist.xquery;
 
@@ -11,6 +27,12 @@ import org.exist.xquery.util.ExpressionDumper;
 import org.exist.xquery.value.Type;
 import org.w3c.dom.Node;
 
+/**
+ * The class <code>AnyNodeTest</code>
+ *
+ * @author Wolfgang Meier (wolfgang@exist-db.org)
+ *
+ */
 public class AnyNodeTest implements NodeTest {
 	
 	/* (non-Javadoc)
@@ -43,8 +65,14 @@ public class AnyNodeTest implements NodeTest {
 			if (proxy.getNodeType() != NodeProxy.UNKNOWN_NODE_TYPE)
 				return matches(proxy.getNode());
 			return proxy.getNodeType() != Node.ATTRIBUTE_NODE;
-		} else
-			return type != Node.ATTRIBUTE_NODE;
+		} else if (type == Type.ATTRIBUTE &&
+			   proxy.getNodeType() == Node.ATTRIBUTE_NODE) {
+		    return true;
+		} else {
+		    return type != Node.ATTRIBUTE_NODE;
+		}
+
+
 	}
     
     public void dump(ExpressionDumper dumper) {
