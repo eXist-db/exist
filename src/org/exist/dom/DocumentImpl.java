8690a2aadc91737e6bbe4c804a4e15a41432f659
Wolfgang Meier <wolfgangmm@gmail.com>
2007-05-17 19:47:33 +0000

    Fixed two major flaws in our implementation of document locks: since a read lock can be acquired by more than one thread, we have to keep track which thread releases which lock. Also, a thread may try to acquire the same lock more than once.
    svn path=/trunk/eXist/; revision=5835
    
    
    Former-commit-id: a8789f795d22d1db0c928f3eb008e3c065e4a509

--- src/org/exist/dom/DocumentImpl.java
+++ src/org/exist/dom/DocumentImpl.java
@@ -348,7 +348,7 @@ public class DocumentImpl extends NodeImpl implements Document, Comparable {
      */     
     public final synchronized Lock getUpdateLock() {
         if(updateLock == null)
-            updateLock = new MultiReadReentrantLock(this);
+            updateLock = new MultiReadReentrantLock();
         return updateLock;
     }
     
