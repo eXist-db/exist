28d115a63ceab3711577a1c4de8256dda786e34a
Dannes Wessels <dannes@exist-db.org>
2007-06-02 11:40:54 +0000

    Upgrade to junit 4.3.1 Upgrade xmlunit to latest beta
    svn path=/trunk/eXist/; revision=5984
    
    
    Former-commit-id: 3da25d1da46cf1244cc3efbaf63494977cff1093

--- tools/XFormsFilter/build.sh
+++ tools/XFormsFilter/build.sh
@@ -21,7 +21,7 @@ fi
 
 ANT_HOME="$EXIST_HOME/tools/ant"
 
-LOCALCLASSPATH=$CLASSPATH:$ANT_HOME/lib/ant-launcher.jar:$ANT_HOME/lib/junit-4.3.jar:.
+LOCALCLASSPATH=$CLASSPATH:$ANT_HOME/lib/ant-launcher.jar:$ANT_HOME/lib/junit-4.3.1.jar:.
 
 JAVA_OPTS="-Dant.home=$ANT_HOME -Dexist.home=$EXIST_HOME"
 
