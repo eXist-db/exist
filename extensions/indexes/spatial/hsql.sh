dd7a3b22dcf58eb11d33b279fcf5a74799eb963d
Leif-Jöran Olsson <ljo@fripost.org>
2007-06-14 00:54:02 +0000

    Added hsql.sh.
    svn path=/trunk/eXist/; revision=6089
    
    
    Former-commit-id: 8a51240aa1d4ec6f67639bf78c3d0efe15775a12

---
+++ extensions/indexes/spatial/hsql.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Run the HSQL Database Manager
+# $Id$
+
+if [ -z "${EXIST_HOME}" ]; then
+	EXIST_HOME="../../..";
+fi
+# set java options
+if [ -z "${CLIENT_JAVA_OPTIONS}" ]; then
+    CLIENT_JAVA_OPTIONS="-Xms64m -Xmx256m -Dfile.encoding=UTF-8";
+fi
+
+HSQL_LIB="${EXIST_HOME}/extensions/indexes/spatial/lib"
+
+if [ "x$1" = "x" ]; then
+    HSQL_DATA="${EXIST_HOME}/webapp\WEB-INF/data/spatial_index"
+else
+    HSQL_DATA="${EXIST_HOME}/$1"
+fi
+
+JAVA_OPTIONS="${CLIENT_JAVA_OPTIONS} -cp ${HSQL_LIB}/hsqldb-1.8.0.7.jar"
+
+
+${JAVA_HOME}/bin/java ${JAVA_OPTIONS} org.hsqldb.util.DatabaseManagerSwing --url jdbc:hsqldb:${HSQL_DATA}
