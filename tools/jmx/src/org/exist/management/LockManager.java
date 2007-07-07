f7d6db06048fe19290ef4c6ba2466b1688d635ee
Wolfgang Meier <wolfgangmm@gmail.com>
2007-07-07 17:30:22 +0000

    Added JMX agent to eXist. Currently exposes caching statistics (btree and data cache usage) and locking information (which thread is waiting for which lock). The code is split into two parts: the classes in src/org/exist/management have no dependencies on javax.managment libraries and will thus compile with any Java version. The JMX-dependant classes reside in tools/jmx and have to be compiled manually. They require Java 5 or newer.
    svn path=/trunk/eXist/; revision=6164
    
    
    Former-commit-id: 2ae85f527df410840fd3683609c0add804144489

---
+++ tools/jmx/src/org/exist/management/LockManager.java
@@ -0,0 +1,77 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2001-07 The eXist Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * $Id$
+ */
+package org.exist.management;
+
+import org.exist.storage.lock.DeadlockDetection;
+import org.exist.storage.lock.LockInfo;
+
+import javax.management.openmbean.*;
+import java.util.Map;
+import java.util.Iterator;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: wolf
+ * Date: Jul 6, 2007
+ * Time: 10:48:35 AM
+ * To change this template use File | Settings | File Templates.
+ */
+public class LockManager implements LockManagerMBean {
+
+    public TabularData getWaitingThreads() {
+        Map map = DeadlockDetection.getWaitingThreads();
+        try {
+            return lockMapToComposite(map);
+        } catch (OpenDataException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    private static String[] itemNames = { "waitingThread", "lockType", "lockMode", "owner", "waitingForRead", "waitingForWrite" };
+    private static String[] itemDescriptions = {
+            "Name of the thread waiting for the lock",
+            "Type of the lock (COLLECTION or RESOURCE)",
+            "Mode of the lock (READ or WRITE)",
+            "The names of the threads currently holding the lock",
+            "Names of threads currently waiting for a read lock",
+            "Names of threads currently waiting for a write lock"
+    };
+    private static String[] indexNames = { "waitingThread" };
+
+    private TabularData lockMapToComposite(Map map) throws OpenDataException {
+        OpenType[] itemTypes = { SimpleType.STRING, SimpleType.STRING, SimpleType.STRING, new ArrayType(1, SimpleType.STRING),
+                new ArrayType(1, SimpleType.STRING), new ArrayType(1, SimpleType.STRING) };
+        CompositeType lockType = new CompositeType("lockInfo", "Provides information on a thread waiting for a lock",
+                itemNames, itemDescriptions, itemTypes);
+        TabularType tabularType = new TabularType("waitingThreads", "Lists all threads waiting for a lock", lockType, indexNames);
+        TabularDataSupport data = new TabularDataSupport(tabularType);
+        for (Iterator i = map.entrySet().iterator(); i.hasNext(); ) {
+            Map.Entry entry = (Map.Entry) i.next();
+            LockInfo info = (LockInfo) entry.getValue();
+            Object[] itemValues = {entry.getKey(), info.getLockType(), info.getLockMode(), info.getOwners(), info.getWaitingForRead(),
+                    info.getWaitingForWrite()};
+            data.put(new CompositeDataSupport(lockType, itemNames, itemValues));
+        }
+        return data;
+    }
+}
