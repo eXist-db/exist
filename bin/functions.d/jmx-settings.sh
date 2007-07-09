943e8aa109c34a1844287e7c1ce9dde815f4ce9b
Leif-Jöran Olsson <ljo@fripost.org>
2007-07-09 16:50:00 +0000

    First step of bash script refactoring. NB JMX short option is now -k1099. Can anyone check to see if it behaves like earlier?
    svn path=/trunk/eXist/; revision=6192
    
    
    Former-commit-id: 6e86bee7ed9dd3929d3bf8aa0506c409cb934542

---
+++ bin/functions.d/jmx-settings.sh
@@ -0,0 +1,19 @@
+# -*-Shell-script-*-
+# Common eXist script functions and settings for JMX
+# $Id$
+
+JMX_ENABLED=0
+JMX_PORT=1099
+
+JMX_OPTS="-Dcom.sun.management.jmxremote \
+		-Dcom.sun.management.jmxremote.port=$JMX_PORT \
+		-Dcom.sun.management.jmxremote.authenticate=false \
+		-Dcom.sun.management.jmxremote.ssl=false"
+
+check_jmx_status() {
+    if [ ${JMX_ENABLED} -gt 0 ]; then
+	JAVA_OPTIONS="$JAVA_OPTIONS $JMX_OPTS"
+	echo "Using JMX: ${JMX_OPTS}"
+    fi
+
+}
--- bin/server.sh
+++ bin/server.sh
@@ -8,88 +8,30 @@
 #
 # In addition to the other parameter options for the standalone server 
 # pass -j or --jmx to enable JMX agent. The port for it can be specified 
-# with --jmx-port=1099
+# with -k1099 or --jmx-port=1099
 #
 
-JMX_ENABLED=0
-JMX_PORT=1099
+SCRIPTPATH=$(dirname `/bin/pwd`/$0)
+# source common functions and settings
+. ${SCRIPTPATH}/functions.d/eXist-settings.sh
+. ${SCRIPTPATH}/functions.d/jmx-settings.sh
+. ${SCRIPTPATH}/functions.d/getopt-settings.sh
 
-declare -a JAVA_OPTS
-NR_JAVA_OPTS=0
-if `getopt -T >/dev/null 2>&1` ; [ $? = 4 ] ; then
-    NON_JAVA_OPTS=`getopt -a -o h,j,d,p:,t: --long help,jmx,debug,http-port:,threads: \
-	-n 'server.sh' -- "$@"`
-else
-    NON_JAVA_OPTS=`getopt h,j,d,p:,t: $*`
-fi
+get_standaloneserver_getopts $*;
 
-eval set -- "$NON_JAVA_OPTS"
-while true ; do
-    case "$1" in
-        -j|--jmx) JMX_ENABLED=1; shift ;;
-        --jmx-port) JMX_PORT="$2"; shift 2 ;;
-        -p|--http-port|-t|--threads) JAVA_OPTS[$NR_JAVA_OPTS]="$1 $2"; let "NR_JAVA_OPTS += 1"; shift 2 ;;
-        --) shift ; break ;;
-        *) JAVA_OPTS[$NR_JAVA_OPTS]="$1"; let "NR_JAVA_OPTS += 1"; shift ;;
-    esac
-done
-# Collect the remaining arguments
-for arg; do
-    JAVA_OPTS[$NR_JAVA_OPTS]="$arg";
-    let "NR_JAVA_OPTS += 1";
-done
+check_exist_home $0;
 
-exist_home () {
-	case "$0" in
-		/*)
-			p=$0
-		;;
-		*)
-			p=`/bin/pwd`/$0
-		;;
-	esac
-		(cd `/usr/bin/dirname $p` ; /bin/pwd)
-}
-
-if [ -z "$EXIST_HOME" ]; then
-	EXIST_HOME_1=`exist_home`
-	EXIST_HOME="$EXIST_HOME_1/.."
-fi
-
-if [ ! -f "$EXIST_HOME/start.jar" ]; then
-	echo "Unable to find start.jar. Please set EXIST_HOME to point to your installation directory."
-	exit 1
-fi
-
-OPTIONS="-Dexist.home=$EXIST_HOME"
-
-# save LANG
-if [ -n "$LANG" ]; then
-	OLD_LANG="$LANG"
-fi
-# set LANG to UTF-8
-LANG=en_US.UTF-8
+set_exist_options;
 
 # set java options
-if [ -z "$JAVA_OPTIONS" ]; then
-	JAVA_OPTIONS="-Xms16000k -Xmx256000k -Dfile.encoding=UTF-8"
-fi
+set_java_options;
 
-JAVA_ENDORSED_DIRS="$EXIST_HOME"/lib/endorsed
-JAVA_OPTIONS="$JAVA_OPTIONS -Djava.endorsed.dirs=$JAVA_ENDORSED_DIRS"
+# enable the JMX agent? If so, concat to $JAVA_OPTIONS:
+check_jmx_status;
 
-# The following lines enable the JMX agent:
-if [ $JMX_ENABLED -gt 0 ]; then
-    JMX_OPTS="-Dcom.sun.management.jmxremote \
-		-Dcom.sun.management.jmxremote.port=$JMX_PORT \
-		-Dcom.sun.management.jmxremote.authenticate=false \
-		-Dcom.sun.management.jmxremote.ssl=false"
-    JAVA_OPTIONS="$JAVA_OPTIONS $JMX_OPTS"
-fi
-
+# save LANG
+set_locale_lang;
 
 $JAVA_HOME/bin/java $JAVA_OPTIONS $OPTIONS -jar "$EXIST_HOME/start.jar" standalone ${JAVA_OPTS[@]}
 
-if [ -n "$OLD_LANG" ]; then
-	LANG="$OLD_LANG"
-fi
+restore_locale_lang;
