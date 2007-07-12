ff06c26b3d811d4a132d49b0c2c6f4c51b56bf73
Dannes Wessels <dannes@exist-db.org>
2007-07-12 10:43:56 +0000

    Changed header, see src/template_java.txt
    svn path=/trunk/eXist/; revision=6220

--- extensions/modules/src/org/exist/xquery/modules/compression/CompressionModule.java
+++ extensions/modules/src/org/exist/xquery/modules/compression/CompressionModule.java
@@ -1,24 +1,23 @@
 /*
  *  eXist Open Source Native XML Database
- *  Copyright (C) 2001-03 Wolfgang M. Meier
- *  wolfgang@exist-db.org
- *  http://exist.sourceforge.net
- *  
+ *  Copyright (C) 2001-07 The eXist Project
+ *  http://exist-db.org
+ *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License
  *  as published by the Free Software Foundation; either version 2
  *  of the License, or (at your option) any later version.
- *  
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Lesser General Public License for more details.
- *  
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *  
- *  $Id: ExampleModule.java 1173 2005-04-20 11:15:18Z wolfgang_m $
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * $Id$
  */
 package org.exist.xquery.modules.compression;
 
--- extensions/modules/src/org/exist/xquery/modules/compression/ZipFunction.java
+++ extensions/modules/src/org/exist/xquery/modules/compression/ZipFunction.java
@@ -1,24 +1,23 @@
 /*
  *  eXist Open Source Native XML Database
- *  Copyright (C) 2001-03 Wolfgang M. Meier
- *  wolfgang@exist-db.org
- *  http://exist.sourceforge.net
- *  
+ *  Copyright (C) 2001-07 The eXist Project
+ *  http://exist-db.org
+ *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License
  *  as published by the Free Software Foundation; either version 2
  *  of the License, or (at your option) any later version.
- *  
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Lesser General Public License for more details.
- *  
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *  
- *  $Id$
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * $Id$
  */
 package org.exist.xquery.modules.compression;
 
@@ -45,7 +44,7 @@ import org.exist.xquery.value.Type;
 
 import org.xml.sax.SAXException;
 
-import java.io.ByteArrayOutputStream;
+import java.io.ByteArrayOutputStream;  // DIZZZZ: The common-io implementation is far more efficient
 import java.io.IOException;
 import java.util.Iterator;
 import java.util.List;
