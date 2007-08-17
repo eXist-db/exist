e3c2ed7af2bbfbc51efbc2e8fbc4b304fae1084c
Wolfgang Meier <wolfgangmm@gmail.com>
2007-08-17 21:22:33 +0000

    Binary storage format changed...
    svn path=/trunk/eXist/; revision=6377

--- src/org/exist/storage/dom/DOMFile.java
+++ src/org/exist/storage/dom/DOMFile.java
@@ -154,7 +154,7 @@ public class DOMFile extends BTree implements Lockable {
         LogEntryTypes.addEntryType(LOG_UPDATE_LINK, UpdateLinkLoggable.class);
     }
 
-    public final static short FILE_FORMAT_VERSION_ID = 6;
+    public final static short FILE_FORMAT_VERSION_ID = 7;
 
     // page types
     public final static byte LOB = 21;
