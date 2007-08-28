544d869df78f4e2cc9563436139a407b3ff3de69
Pierrick Brihaye <pierrick.brihaye@free.fr>
2007-08-28 15:04:08 +0000

    No XPTY0018 in eXist's processing model when enclosed expressions are processed
    svn path=/trunk/eXist/; revision=6431

--- src/org/exist/xquery/PathExpr.java
+++ src/org/exist/xquery/PathExpr.java
@@ -260,6 +260,7 @@ public class PathExpr extends AbstractExpression implements CompiledXQuery,
                 //!(expr instanceof VariableDeclaration) &&
                 !(expr instanceof VariableReference) &&
                 !(expr instanceof LetExpr) &&
+                !(expr instanceof EnclosedExpr) &&
                 !Type.subTypeOf(result.getItemType(), Type.ATOMIC)) {
                 throw new XPathException("XPTY0018: Cannot mix nodes and atomic values in the result of a path expression.");                    
             }
