8e33b610936023d30bf71676f09e0b12bdfe7355
Dannes Wessels <dannes@exist-db.org>
2007-09-27 19:49:04 +0000

    Keyword Id
    svn path=/trunk/eXist/; revision=6639
    
    
    Former-commit-id: 1fd6dbf4a4f8cf5b0c4545181ffb2224543bd597

--- src/org/exist/validation/ValidationContentHandler.java
+++ src/org/exist/validation/ValidationContentHandler.java
@@ -17,7 +17,7 @@
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
- *  $Id: Validation.java 6555 2007-09-15 18:13:31Z dizzzz $
+ *  $Id$
  */
 
 package org.exist.validation;
