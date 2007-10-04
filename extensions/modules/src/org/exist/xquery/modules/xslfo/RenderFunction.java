e2925e975f54c982327a8719cbb280d0de049f8e
Adam Retter <adam.retter@googlemail.com>
2007-10-04 13:42:52 +0000

    XSL-FO Extension Module, thanks very much to Chris Wallace, The University of the West of England and Craig Goodyer
    svn path=/trunk/eXist/; revision=6667
    
    
    Former-commit-id: ae1860eb1af115fb41bb029cd7fd3e8f927a2df5

---
+++ extensions/modules/src/org/exist/xquery/modules/xslfo/RenderFunction.java
@@ -0,0 +1,235 @@
+/*
+ *  eXist Apache FOP Transformation Extension
+ *  Copyright (C) 2007 Craig Goodyer at the University of the West of England
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  
+ *  $Id$
+ */
+
+package org.exist.xquery.modules.xslfo;
+
+import java.io.ByteArrayOutputStream;
+import java.util.Enumeration;
+import java.util.Properties;
+
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.sax.TransformerHandler;
+
+import org.apache.fop.apps.Fop;
+import org.apache.fop.apps.FOUserAgent;
+
+import org.exist.dom.QName;
+import org.exist.xquery.BasicFunction;
+import org.exist.xquery.Cardinality;
+import org.exist.xquery.FunctionSignature;
+import org.exist.xquery.XPathException;
+import org.exist.xquery.XQueryContext;
+import org.exist.xquery.value.Base64Binary;
+import org.exist.xquery.value.Item;
+import org.exist.xquery.value.NodeValue;
+import org.exist.xquery.value.Sequence;
+import org.exist.xquery.value.SequenceType;
+import org.exist.xquery.value.Type;
+import org.exist.xslt.TransformerFactoryAllocator;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * @author Craig Goodyer <craiggoodyer@gmail.com>
+ * @author Adam Retter <adam.retter@devon.gov.uk>
+ */
+public class RenderFunction extends BasicFunction
+{
+	public final static FunctionSignature signature = new FunctionSignature(
+		new QName("render", XSLFOModule.NAMESPACE_URI, XSLFOModule.PREFIX),
+		"Renders a given XSL-FO document. $a is the XSL-FO node, $b is the required mime-type, $c is parameters to the transformation. "
+		+ "Returns an xs:base64binary of the result."
+		+ "Parameters are specified with the structure: "
+		+ "<parameters><param name=\"param-name1\" value=\"param-value1\"/>"
+		+ "</parameters>. "
+		+ "Recognised rendering parameters are: author, title.",
+		new SequenceType[] {
+				new SequenceType(Type.NODE, Cardinality.EXACTLY_ONE),
+				new SequenceType(Type.STRING, Cardinality.EXACTLY_ONE),
+				new SequenceType(Type.NODE, Cardinality.ZERO_OR_ONE)
+		},                                                                                
+		new SequenceType(Type.BASE64_BINARY, Cardinality.ZERO_OR_ONE)
+	);
+	
+	/**
+	 * Constructor for RenderFunction, which returns a new instance of this class.
+	 * 
+	 * @param context
+	 * @param signature
+	 */
+	public RenderFunction(XQueryContext context, FunctionSignature signature)
+	{
+		super(context, signature);
+	}
+
+	/*
+	 * Actual implementation of the rendering process. When a
+	 * function in this module is called, this method is executed with the given
+	 * inputs. @param Sequence[] args (XSL-FO, mime-type, parameters) @param
+	 * Sequence contextSequence (default sequence)
+	 * 
+	 * @see org.exist.xquery.BasicFunction#eval(org.exist.xquery.value.Sequence[], org.exist.xquery.value.Sequence)
+	 */
+	public Sequence eval(Sequence[] args, Sequence contextSequence) throws XPathException
+	{
+		// gather input XSL-FO document
+		// if no input document (empty), return empty result as we need data to process
+		if (args[0].isEmpty())
+			return Sequence.EMPTY_SEQUENCE;
+		Item inputNode = args[0].itemAt(0);
+		
+		//get mime-type
+		String mimeType = args[1].getStringValue();
+		
+		// get parameters
+		Properties parameters = new Properties();
+		if(!args[2].isEmpty())
+		{
+			parameters = parseParameters(((NodeValue)args[2].itemAt(0)).getNode());
+			
+		}
+		
+		try
+		{
+			// setup a transformer handler
+			TransformerHandler handler = TransformerFactoryAllocator.getTransformerFactory(context.getBroker()).newTransformerHandler();
+			Transformer transformer = handler.getTransformer();
+			
+			//set the parameters if any
+			if(parameters.size() > 0)
+			{
+				Enumeration keys = parameters.keys(); 
+				while(keys.hasMoreElements())
+				{
+					String name = (String)keys.nextElement();
+					String value = parameters.getProperty(name);
+					transformer.setParameter(name, value);
+				}
+			}
+			
+			// setup the foUserAgent, using given parameters held in the transformer handler
+			FOUserAgent foUserAgent = setupFOUserAgent(parameters, transformer);
+			
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			
+			// create new instance of FOP using the mimetype, the created user agent, and the output stream
+			Fop fop = XSLFOModule.getFopFactory().newFop(mimeType, foUserAgent, baos);
+	
+			// Obtain FOP's DefaultHandler
+			DefaultHandler dh = fop.getDefaultHandler();
+				
+			//process the XSL-FO
+			dh.startDocument();
+			inputNode.toSAX(context.getBroker(), dh, new Properties());
+			dh.endDocument();
+			
+			//return the result
+			return new Base64Binary(baos.toByteArray());
+		}
+		catch(TransformerException te)
+		{
+			throw new XPathException(te);
+		}
+		catch(SAXException se)
+		{
+			throw new XPathException(se);
+		}
+	}
+	
+	/**
+	 * Parses a structiure like <parameters><param name="a" value="1"/><param name="b" value="2"/></parameters>
+	 * into a set of Properties
+	 * 
+	 * @param parameters The parameters Node
+	 * @return a set of name value properties for representing the parameters
+	 */
+	private Properties parseParameters(Node parameters) throws XPathException
+	{
+		Properties properties = new Properties();
+		
+		if(parameters != null && parameters.getNodeType() == Node.ELEMENT_NODE && parameters.getLocalName().equals("parameters"))
+		{
+			NodeList params = ((Element)parameters).getElementsByTagName("param");
+				
+			for(int i = 0; i < params.getLength(); i++)
+			{
+				Element param = ((Element)params.item(i));
+				
+				String name = param.getAttribute("name");
+				String value = param.getAttribute("value");
+				
+				if(name != null && value != null)
+				{
+						properties.setProperty(name, value);
+				}
+				else
+				{
+					LOG.warn("Name or value attribute missing for stylesheet parameter");
+				}
+			}
+		}
+		
+		return properties;
+	}
+	
+	/**
+	 * Setup the UserAgent for FOP, from given parameters *
+	 * 
+	 * @param transformer Created based on the XSLT, so containing any parameters to the XSL-FO specified in the XQuery
+	 * @param parameters any user defined parameters to the XSL-FO process
+	 * @return FOUserAgent The generated FOUserAgent to include any parameters passed in
+	 */
+	private FOUserAgent setupFOUserAgent(Properties parameters, Transformer transformer) throws TransformerException
+	{
+		// create an FOUserAgent from Modules FopFactory
+		FOUserAgent foUserAgent = XSLFOModule.getFopFactory().newFOUserAgent();
+
+		// setup the foUserAgent as per the parameters given
+		foUserAgent.setProducer("eXist with Apache FOP");
+		
+		if(transformer.getParameter("FOPauthor") != null)
+			foUserAgent.setAuthor(parameters.getProperty("author"));
+		
+		if(transformer.getParameter("FOPtitle") != null)
+			foUserAgent.setTitle(parameters.getProperty("title"));
+		
+		if(transformer.getParameter("FOPdpi") != null)
+		{
+			String dpiStr = (String) transformer.getParameter("dpi");
+			try
+			{
+				foUserAgent.setTargetResolution(Integer.parseInt(dpiStr));
+			}
+			catch (NumberFormatException nfe)
+			{
+				throw new TransformerException("Cannot parse value of \"dpi\" - " + dpiStr + " to configure FOUserAgent");
+			}
+		}
+
+		return foUserAgent;
+	}
+}
---
+++ extensions/modules/src/org/exist/xquery/modules/xslfo/XSLFOModule.java
@@ -0,0 +1,74 @@
+/*
+ *  eXist Apache FOP Transformation Extension
+ *  Copyright (C) 2007 Craig Goodyer at the University of the West of England
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  
+ *  $Id$
+ */
+
+package org.exist.xquery.modules.xslfo;
+
+import org.apache.fop.apps.FopFactory;
+
+import org.exist.xquery.AbstractInternalModule;
+import org.exist.xquery.FunctionDef;
+
+/**
+ * @author Craig Goodyer <craiggoodyer@gmail.com>
+ * @author Adam Retter <adam.retter@devon.gov.uk>
+ */
+public class XSLFOModule extends AbstractInternalModule {
+
+	public final static String NAMESPACE_URI = "http://exist-db.org/xquery/xslfo";
+	public final static String PREFIX = "xslfo";
+
+	private static FopFactory fopFactory = null;
+	
+	private final static FunctionDef[] functions = {
+		new FunctionDef(RenderFunction.signature, RenderFunction.class)
+	};
+	
+	public XSLFOModule()
+	{
+		super(functions);
+	}
+
+	public String getNamespaceURI() {
+		return NAMESPACE_URI;
+	}
+
+	public String getDefaultPrefix() {
+		return PREFIX;
+	}
+
+	public String getDescription() {
+		return "Module for performing XSL-FO transformations";
+	}
+
+	/**
+	 * Getter for the FOPFactory
+	 * 
+	 * @return FopFactory
+	 */
+	public static synchronized FopFactory getFopFactory()
+	{
+		if(fopFactory == null)
+		{
+			fopFactory = FopFactory.newInstance();
+		}
+		return fopFactory;
+	}
+}
