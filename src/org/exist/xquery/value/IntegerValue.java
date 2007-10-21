2f93267750ba3de7f10976b4dfc1b0a725de0597
Leif-Jöran Olsson <ljo@fripost.org>
2007-10-21 19:53:05 +0000

    One xqts fix, CastFOCA0003-1, and some header changes.
    svn path=/trunk/eXist/; revision=6766
    
    
    Former-commit-id: cfcc3e73ca7f5cfc70c2e9ad45d35a042f8add31

--- src/org/exist/xquery/value/IntegerValue.java
+++ src/org/exist/xquery/value/IntegerValue.java
@@ -1,21 +1,22 @@
 /*
- *  eXist Open Source Native XML Database
- *  Copyright (C) 2001-06,  Wolfgang M. Meier (meier@ifs.tu-darmstadt.de)
+ * eXist Open Source Native XML Database
+ * Copyright (C) 2001-2007 The eXist Project
+ * http://exist-db.org
  *
- *  This library is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Library General Public License
- *  as published by the Free Software Foundation; either version 2
- *  of the License, or (at your option) any later version.
- *
- *  This library is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Library General Public License for more details.
- *
- *  You should have received a copy of the GNU Library General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
  * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *  
  *  $Id$
  */
 package org.exist.xquery.value;
@@ -39,7 +40,7 @@ public class IntegerValue extends NumericValue {
 	private static final BigInteger ZERO_BIGINTEGER = new BigInteger("0");
 	private static final BigInteger ONE_BIGINTEGER = new BigInteger("1");
 	private static final BigInteger MINUS_ONE_BIGINTEGER = new BigInteger("-1");
-	private static final BigInteger LARGEST_LONG  = new BigInteger("9223372036854775808" );
+	private static final BigInteger LARGEST_LONG  = new BigInteger("9223372036854775808");
 	private static final BigInteger SMALLEST_LONG  = LARGEST_LONG.negate();
 	private static final BigInteger LARGEST_INT  = new BigInteger("4294967296");
 	private static final BigInteger SMALLEST_INT  = LARGEST_INT.negate();
