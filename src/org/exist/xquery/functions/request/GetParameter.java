7957c8be8221ef6c198c672a836dd9ce5a71a9a2
chaeron <andrzej@chaeron.com>
2007-10-25 03:11:48 +0000

    Added extra message signature to request:get-parameters to allow a default value to be returned even if there is no request object in context (eg. the xquery was called by the scheduler rather than through an HTTP request).
    svn path=/trunk/eXist/; revision=6786
    
    
    Former-commit-id: 88e7cda87c0b79ee9c98990f4daa0840e4f933bf

--- src/org/exist/xquery/functions/request/GetParameter.java
+++ src/org/exist/xquery/functions/request/GetParameter.java
@@ -40,13 +40,13 @@ import org.exist.xquery.value.Type;
  * @author wolf
  */
 public class GetParameter extends BasicFunction {
-
-	public final static FunctionSignature signature =
+	
+	public final static FunctionSignature signatures[] = {
 		new FunctionSignature(
 			new QName(
-				"get-parameter",
-				RequestModule.NAMESPACE_URI,
-				RequestModule.PREFIX),
+			"get-parameter",
+			RequestModule.NAMESPACE_URI,
+			RequestModule.PREFIX),
 			"Returns the HTTP request parameter identified by $a. If the parameter could not be found, " +
 			"the default value specified in $b is returned instead. Note: this function will not try to expand " +
 			"predefined entities like &amp; or &lt;, so a &amp; passed through a parameter will indeed " +
@@ -54,55 +54,91 @@ public class GetParameter extends BasicFunction {
 			new SequenceType[] {
 				new SequenceType(Type.STRING, Cardinality.EXACTLY_ONE),
 				new SequenceType(Type.ITEM, Cardinality.ZERO_OR_MORE)},
-			new SequenceType(Type.STRING, Cardinality.ZERO_OR_MORE));
+			new SequenceType(Type.STRING, Cardinality.ZERO_OR_MORE)),
+	
+		new FunctionSignature(
+			new QName(
+			"get-parameter",
+			RequestModule.NAMESPACE_URI,
+			RequestModule.PREFIX),
+			"Returns the HTTP request parameter identified by $a. If the parameter could not be found, " +
+			"the default value specified in $b is returned instead. Note: this function will not try to expand " +
+			"predefined entities like &amp; or &lt;, so a &amp; passed through a parameter will indeed " +
+			"be treated as an &amp; character. If the  $c (failonerror) value is set to false, then the function " +
+			"will not fail if there is no request in scope.",
+			new SequenceType[] {
+				new SequenceType(Type.STRING, Cardinality.EXACTLY_ONE),
+				new SequenceType(Type.ITEM, Cardinality.ZERO_OR_MORE),
+				new SequenceType( Type.BOOLEAN, Cardinality.ZERO_OR_MORE )},
+			new SequenceType(Type.STRING, Cardinality.ZERO_OR_MORE))
+		
+	};
 	
 	public final static FunctionSignature deprecated =
 		new FunctionSignature(
-			new QName(
-				"request-parameter",
-				RequestModule.NAMESPACE_URI,
-				RequestModule.PREFIX),
-			"Returns the HTTP request parameter identified by $a. If the parameter could not be found, " +
-			"the default value specified in $b is returned instead. Note: this function will not try to expand " +
-			"predefined entities like &amp; or &lt;, so a &amp; passed through a parameter will indeed " +
-			"be treated as an &amp; character.",
-			new SequenceType[] {
-				new SequenceType(Type.STRING, Cardinality.EXACTLY_ONE),
-				new SequenceType(Type.ITEM, Cardinality.ZERO_OR_MORE)},
-			new SequenceType(Type.STRING, Cardinality.ZERO_OR_MORE),
-			"Renamed to request:get-parameter.");
-
-	public GetParameter(XQueryContext context) {
+		new QName(
+		"request-parameter",
+		RequestModule.NAMESPACE_URI,
+		RequestModule.PREFIX),
+		"Returns the HTTP request parameter identified by $a. If the parameter could not be found, " +
+		"the default value specified in $b is returned instead. Note: this function will not try to expand " +
+		"predefined entities like &amp; or &lt;, so a &amp; passed through a parameter will indeed " +
+		"be treated as an &amp; character.",
+		new SequenceType[] {
+			new SequenceType(Type.STRING, Cardinality.EXACTLY_ONE),
+			new SequenceType(Type.ITEM, Cardinality.ZERO_OR_MORE)},
+		new SequenceType(Type.STRING, Cardinality.ZERO_OR_MORE),
+		"Renamed to request:get-parameter.");
+	
+	public GetParameter(XQueryContext context, FunctionSignature signature) {
 		super(context, signature);
 	}
-
+	
 	
 	/* (non-Javadoc)
 	 * @see org.exist.xquery.BasicFunction#eval(org.exist.xquery.value.Sequence[], org.exist.xquery.value.Sequence)
 	 */
 	public Sequence eval(Sequence[] args, Sequence contextSequence)
-			throws XPathException {
+		throws XPathException {
 		RequestModule myModule =
 			(RequestModule) context.getModule(RequestModule.NAMESPACE_URI);
-
+		
+		boolean failOnError = true;
+		
+		if( getSignature().getArgumentCount() == 3 ) {
+			failOnError = args[2].effectiveBooleanValue();
+		}
+		
 		// request object is read from global variable $request
 		Variable var = myModule.resolveVariable(RequestModule.REQUEST_VAR);
-		if (var == null || var.getValue() == null || var.getValue().getItemType() != Type.JAVA_OBJECT)
-			throw new XPathException(getASTNode(), "Variable $request is not bound to an Java object.");
-
+		if (var == null || var.getValue() == null || var.getValue().getItemType() != Type.JAVA_OBJECT) {
+			if( failOnError ) {
+				throw new XPathException(getASTNode(), "Variable $request is not bound to an Java object.");
+			} else {
+				return args[1];
+			}
+		}
+		
 		// get parameters
 		String param = args[0].getStringValue();
-
+		
 		JavaObjectValue value = (JavaObjectValue) var.getValue().itemAt(0);
 		if (value.getObject() instanceof RequestWrapper) {
 			String[] values = ((RequestWrapper)value.getObject()).getParameterValues(param);
-			if (values == null || values.length == 0)
+			if (values == null || values.length == 0) {
 				return args[1];
-			if (values.length == 1)
+			}
+			if (values.length == 1) {
 				return XPathUtil.javaObjectToXPath(values[0], null, false);
-			else
+			} else {
 				return XPathUtil.javaObjectToXPath(values, null, false);
-		} else
-			throw new XPathException("Variable $request is not bound to a Request object.");
+			}
+		} else {
+			if( failOnError ) {
+				throw new XPathException("Variable $request is not bound to a Request object.");
+			} else {
+				return args[1];				
+			}
+		}
 	}
 }
