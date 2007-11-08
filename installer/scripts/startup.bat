959247d1a0166bbbe58cc29f972db36b214f4d9d
Wolfgang Meier <wolfgangmm@gmail.com>
2007-11-08 17:45:59 +0000

    Use launch4j to generate a native windows .exe wrapper around the generated installer. Without this, eXist will not install properly on windows vista, due to missing administrator rights. Also, the installer needs to have 'setup' in its name.
    svn path=/trunk/eXist/; revision=6890
    
    
    Former-commit-id: 17aff052ad8b9457bc0528023b734a5a6cb226a5

--- installer/scripts/startup.bat
+++ installer/scripts/startup.bat
@@ -19,25 +19,14 @@ if not "%EXIST_HOME%" == "" goto gotExistHome
 rem will be set by the installer
 set EXIST_HOME=$INSTALL_PATH
 
-rem try to guess
-set EXIST_HOME=.
-if exist %EXIST_HOME%\start.jar goto gotExistHome
-set EXIST_HOME=..
-if exist %EXIST_HOME%\start.jar goto gotExistHome
-
-echo EXIST_HOME not found. Please set your
-echo EXIST_HOME environment variable to the
-echo home directory of eXist.
-goto :eof
-
 :gotExistHome
-set JAVA_ENDORSED_DIRS="%EXIST_HOME%"\lib\endorsed
-set JAVA_OPTS="-Xms16000k -Xmx128000k -Dfile.encoding=UTF-8 -Djava.endorsed.dirs=%JAVA_ENDORSED_DIRS%"
+set JAVA_ENDORSED_DIRS=%EXIST_HOME%\lib\endorsed
+set JAVA_OPTS=-Xms16000k -Xmx128000k -Dfile.encoding=UTF-8 -Djava.endorsed.dirs="%JAVA_ENDORSED_DIRS%"
 
-set BATCH.D=%EXIST_HOME%/bin/batch.d
-call %BATCH.D%/get_opts.bat %*
-call %BATCH.D%/check_jmx_status.bat
+set BATCH.D="%EXIST_HOME%\bin\batch.d"
+call %BATCH.D%\get_opts.bat %*
+call %BATCH.D%\check_jmx_status.bat
 
-"%JAVA_HOME%\bin\java" "%JAVA_OPTS%"  -Dexist.home="%EXIST_HOME%" -jar "%EXIST_HOME%\start.jar" jetty %JAVA_ARGS%
+"%JAVA_HOME%\bin\java" %JAVA_OPTS%  -Dexist.home="%EXIST_HOME%" -jar "%EXIST_HOME%\start.jar" jetty %JAVA_ARGS%
 :eof
 
