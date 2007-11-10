ebf497f8171648fb6937560449694642ec824775
Piotr Kaminski <piotr@ideanest.com>
2007-11-10 23:54:10 +0000

    Added equals/hashCode to ItemList and facets. Tweaked QueryService comment.
    svn path=/trunk/eXist/; revision=6901

--- extensions/fluent/test/src/org/exist/fluent/ItemListTest.java
+++ extensions/fluent/test/src/org/exist/fluent/ItemListTest.java
@@ -4,6 +4,42 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ItemListTest extends DatabaseTestCase {
+	@Test public void equals1() {
+		ItemList list1 = db.query().all("(1, 2, 3)"), list2 = db.query().all("(1, 2, 3)");
+		assertTrue(list1.equals(list2));
+		assertEquals(list1.hashCode(), list2.hashCode());
+	}
+	
+	@Test public void equals2() {
+		ItemList list1 = db.query().all("(1, 2, 3)"), list2 = db.query().all("(1, 2, 4)");
+		assertFalse(list1.equals(list2));
+		// can't assert anything about their hashcodes
+	}
+	
+	@Test public void equals3() {
+		ItemList list1 = db.query().all("(1, 2, 3)"), list2 = db.query().all("(1, 2)");
+		assertFalse(list1.equals(list2));
+		// can't assert anything about their hashcodes
+	}
+	
+	@Test public void equals4() {
+		ItemList list1 = db.query().all("(1, 2)"), list2 = db.query().all("(1, 2, 3)");
+		assertFalse(list1.equals(list2));
+		// can't assert anything about their hashcodes
+	}
+
+	@Test public void nodesEquals1() {
+		ItemList.NodesFacet list1 = db.query().all("(1, 2, 3)").nodes(), list2 = db.query().all("(1, 2, 3)").nodes();
+		assertTrue(list1.equals(list2));
+		assertEquals(list1.hashCode(), list2.hashCode());
+	}
+
+	@Test public void valuesEquals1() {
+		ItemList.ValuesFacet list1 = db.query().all("(1, 2, 3)").values(), list2 = db.query().all("(1, 2, 3)").values();
+		assertTrue(list1.equals(list2));
+		assertEquals(list1.hashCode(), list2.hashCode());
+	}
+
 	@Test
 	public void convertToSequence() {
 		XMLDocument doc = db.createFolder("/top").documents().build(Name.create("test"))
