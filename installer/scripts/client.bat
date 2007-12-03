c0b95d17c4167f6163e1a3ebccb0f03af8b43225
Dannes Wessels <dannes@exist-db.org>
2007-12-03 10:24:32 +0000

    Made installer script in sync with bin/client.bat - repaired forward slash - made " (double quotes) consistent
    svn path=/trunk/eXist/; revision=7017

--- installer/scripts/client.bat
+++ installer/scripts/client.bat
@@ -11,21 +11,23 @@ set JMX_PORT=1099
 set JAVA_ARGS=
 
 if not "%JAVA_HOME%" == "" goto gotJavaHome
+
 rem will be set by the installer
 set JAVA_HOME=$JAVA_HOME
 
 :gotJavaHome
 if not "%EXIST_HOME%" == "" goto gotExistHome
+
 rem will be set by the installer
 set EXIST_HOME=$INSTALL_PATH
 
 :gotExistHome
-set JAVA_ENDORSED_DIRS=%EXIST_HOME%\lib\endorsed
-set JAVA_OPTS=-Xms16000k -Xmx256000k -Dfile.encoding=UTF-8 -Djava.endorsed.dirs="%JAVA_ENDORSED_DIRS%"
+set JAVA_ENDORSED_DIRS="%EXIST_HOME%"\lib\endorsed
+set JAVA_OPTS="-Xms16000k -Xmx256000k -Dfile.encoding=UTF-8 -Djava.endorsed.dirs=%JAVA_ENDORSED_DIRS%"
 
-set BATCH.D="%EXIST_HOME%\bin/batch.d"
+set BATCH.D="%EXIST_HOME%\bin\batch.d"
 call %BATCH.D%\get_opts.bat %*
 call %BATCH.D%\check_jmx_status.bat
 
-"%JAVA_HOME%\bin\java" %JAVA_OPTS%  -Dexist.home="%EXIST_HOME%" -jar "%EXIST_HOME%\start.jar" client %JAVA_ARGS%
+"%JAVA_HOME%\bin\java" "%JAVA_OPTS%"  -Dexist.home="%EXIST_HOME%" -jar "%EXIST_HOME%\start.jar" client %JAVA_ARGS%
 :eof
