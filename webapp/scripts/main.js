662636a4fb9916143b5e09749f42ee9f4c2fe148
Wolfgang Meier <wolfgangmm@gmail.com>
2007-12-03 11:27:24 +0000

    Switching the "news" box on the homepage to the new blog.
    svn path=/trunk/eXist/; revision=7020

--- webapp/scripts/main.js
+++ webapp/scripts/main.js
@@ -1,26 +1,24 @@
-Event.observe(window, 'load', initialize, false);
-
-function initialize() {
-	Nifty("h1.chaptertitle", "transparent");
+YAHOO.util.Event.onDOMReady(function () {
+    Nifty("h1.chaptertitle", "transparent");
     Nifty("div.note", "transparent");
     Nifty("div.example", "transparent");
     Nifty("div.important", "transparent");
     Nifty("div.block div.head", "top");
     Nifty("div.block ul", "bottom");
-    
+
     loadRss();
-}
+});
 
 function loadRss() {
-	var div = $('news_content');
+    var div = document.getElementById('news_content');
 	if (!div) return;
-	var ajax = new Ajax.Request("rss_import.xql", {
-		method: 'get',
-		onFailure: function (response) {
+    var callback = {
+        success: function (response) {
+            div.innerHTML = response.responseText;
+        },
+        failure: function (response) {
 			div.innerHTML = 'Failed to load news.';
-		},
-		onComplete: function (response) {
-			div.innerHTML = response.responseText;
 		}
-	});
+    }
+    YAHOO.util.Connect.asyncRequest('GET', 'feed.xql', callback);
 }
