75025efbaac3aa12b6968f9ac18a524a54d2afaf
Dannes Wessels <dannes@exist-db.org>
2008-01-06 17:23:17 +0000

    Added comments to SOAP tests.
    svn path=/trunk/eXist/; revision=7127

--- samples/src/org/exist/examples/soap/GetDocument.java
+++ samples/src/org/exist/examples/soap/GetDocument.java
@@ -1,3 +1,24 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2001-07 The eXist Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * $Id$
+ */
 package org.exist.examples.soap;
 
 import org.exist.soap.Query;
@@ -5,6 +26,11 @@ import org.exist.soap.QueryService;
 import org.exist.soap.QueryServiceLocator;
 import org.exist.storage.DBBroker;
 
+/**
+ * Get document via SOAP. First insert shakespeare example documents.
+ *
+ * Execute: bin\run.bat org.exist.examples.soap.GetDocument
+ */
 public class GetDocument {
 
     public static void main( String[] args ) throws Exception {
