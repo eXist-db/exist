41a1afe1135e8466414a11684276a6dde1e8f61b
Dannes Wessels <dannes@exist-db.org>
2008-01-10 18:54:47 +0000

    PUT example works now
    svn path=/trunk/eXist/; revision=7178
    
    
    Former-commit-id: ce799e747205799885772aa195846f938f3f5332

--- samples/src/org/exist/examples/http/PutExample.java
+++ samples/src/org/exist/examples/http/PutExample.java
@@ -42,50 +42,57 @@ import java.net.URL;
  */
 public class PutExample {
 
-	public static void main(String[] args) {
-	    
-        if(args.length != 1) {
+    public static void main(String[] args) {
+
+        if (args.length != 1) {
             System.out.println("Usage: bin/run.sh org.exist.examples.http.PutExample <fileid>");
             System.exit(0);
         }
-        
-		String fileName = args[0];
-		File file = new File(fileName);
-		if(!file.canRead()) {
-			System.err.println("Cannot read file " + file);
-			return;
-		}
-		String docName =file.getName();
-		
-		try {
-            URL url = new URL("http://localhost:8080/exist/rest" + DBBroker.ROOT_COLLECTION + "/test/" + 
-                docName);
-			HttpURLConnection connect = (HttpURLConnection) url.openConnection();
-			connect.setRequestMethod("PUT");
-			connect.setDoOutput(true);
-			connect.setRequestProperty("ContentType", "text/xml");
-			
-			OutputStream os = connect.getOutputStream();
-			InputStream is = new FileInputStream(file);
-			byte[] buf = new byte[1024];
-			int c;
-			while((c = is.read(buf)) > -1)
-				os.write(buf, 0, c);
-			
-			System.out.println("Connecting to " + url.toString());
-			connect.connect();
+
+        String fileName = args[0];
+        File file = new File(fileName);
+        if (!file.canRead()) {
+            System.err.println("Cannot read file " + file);
+            return;
+        }
+        String docName = file.getName();
+
+        try {
+            URL url = new URL("http://localhost:8080/exist/rest" 
+                    + DBBroker.ROOT_COLLECTION + "/test/" + docName);
+            
+            System.out.println("PUT file to "+url.toString());
+            HttpURLConnection connect = (HttpURLConnection) url.openConnection();
+            connect.setRequestMethod("PUT");
+            connect.setDoOutput(true);
+            connect.setRequestProperty("ContentType", "text/xml");
+
+            OutputStream os = connect.getOutputStream();
+            InputStream is = new FileInputStream(file);
+            byte[] buf = new byte[1024];
+            int c;
+            while ((c = is.read(buf)) > -1) {
+                os.write(buf, 0, c);
+            }
+            os.flush();
+            os.close();
+            System.out.println("Statuscode "+connect.getResponseCode() 
+                    +" ("+ connect.getResponseMessage() +")");
+
+            System.out.println("GET file from " + url.toString());
 
             connect = (HttpURLConnection) url.openConnection();
-			connect.setRequestMethod("GET");
-			connect.connect();
-			System.out.println("Result:");
-			BufferedReader bis = new BufferedReader(new InputStreamReader(connect.getInputStream()));
-			String line;
-			while((line = bis.readLine()) != null)
-				System.out.println(line);
-		} catch (Exception e) {
-			System.err.println("An exception occurred: " + e.getMessage());
-			e.printStackTrace();
-		}
-	}
+            connect.setRequestMethod("GET");
+            connect.connect();
+            System.out.println("Result:");
+            BufferedReader bis = new BufferedReader(new InputStreamReader(connect.getInputStream()));
+            String line;
+            while ((line = bis.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (Exception e) {
+            System.err.println("An exception occurred: " + e.getMessage());
+            e.printStackTrace();
+        }
+    }
 }
