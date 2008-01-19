42545ad4e93aed25b7acacb657a469ae3a8d1bda
Pierrick Brihaye <pierrick.brihaye@free.fr>
2008-01-19 21:04:28 +0000

    Getting rid of 2 more : if (contextItem != null)   contextSequence = contextItem.toSequence();
    svn path=/trunk/eXist/; revision=7265

--- src/org/exist/xquery/functions/FunExists.java
+++ src/org/exist/xquery/functions/FunExists.java
@@ -73,10 +73,10 @@ public class FunExists extends Function {
                 context.getProfiler().message(this, Profiler.START_SEQUENCES, "CONTEXT ITEM", contextItem.toSequence());
         }
         
-        if(contextItem != null)
-			contextSequence = contextItem.toSequence();
+        //if(contextItem != null)
+			//contextSequence = contextItem.toSequence();
         
-		Sequence seq = getArgument(0).eval(contextSequence);
+		Sequence seq = getArgument(0).eval(contextSequence, contextItem);
 		Sequence result = seq.isEmpty() ? BooleanValue.FALSE : BooleanValue.TRUE;
         
         if (context.getProfiler().isEnabled()) 
