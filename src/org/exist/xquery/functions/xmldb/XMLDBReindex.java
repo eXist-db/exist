c8a8476b1dec219897713b670e0e25ed385202f6
Dannes Wessels <dannes@exist-db.org>
2008-01-27 20:40:55 +0000

    [feature] added xmldb:reindex(collection-uri) part 2
    svn path=/trunk/eXist/; revision=7312

--- src/org/exist/xquery/functions/xmldb/XMLDBReindex.java
+++ src/org/exist/xquery/functions/xmldb/XMLDBReindex.java
@@ -18,7 +18,7 @@
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *  
- *  $Id: XMLDBRegisterDatabase.java 4403 2006-09-27 12:27:37Z wolfgang_m $
+ *  $Id$
  */
 package org.exist.xquery.functions.xmldb;
 
