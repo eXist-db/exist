91d1830aea62f77fb97790fba4aa002b5e40cdac
Wolfgang Meier <wolfgangmm@gmail.com>
2008-02-16 14:49:12 +0000

    [feature] improved URL rewrite filter: XQueryURLRewrite. This works similar to the popular UrlRewriteFilter, but uses an XQuery to determine how an HTTP request has to be handled.
    svn path=/trunk/eXist/; revision=7412

---
+++ src/org/exist/http/urlrewrite/Forward.java
@@ -0,0 +1,42 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2001-07 The eXist Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * $Id$
+ */
+package org.exist.http.urlrewrite;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public abstract class Forward extends URLRewrite {
+
+    protected Forward(String uri, String target) {
+        super(uri, target);
+    }
+
+    public void doRewrite(HttpServletRequest request, HttpServletResponse response, FilterChain chain) throws ServletException, IOException {
+        getRequestDispatcher(request).forward(request, response);
+    }
+
+    protected abstract RequestDispatcher getRequestDispatcher(HttpServletRequest request);
+}
---
+++ src/org/exist/http/urlrewrite/URLRewrite.java
@@ -0,0 +1,49 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2001-07 The eXist Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * $Id$
+ */
+package org.exist.http.urlrewrite;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import java.io.IOException;
+
+/**
+ * Base class for all rewritten URLs.
+ */
+public abstract class URLRewrite {
+
+    protected String uri;
+    protected String target;
+
+    protected URLRewrite(String uri, String target) {
+        this.uri = uri;
+        this.target = target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
+    }
+
+    public abstract void doRewrite(HttpServletRequest request, HttpServletResponse response,
+                                   FilterChain chain) throws ServletException, IOException;
+}
