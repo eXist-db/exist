0a1047c528f59b181583ef8853660231c1af2aac
Wolfgang Meier <wolfgangmm@gmail.com>
2008-03-02 19:58:34 +0000

    [bugfix] complete rev. 7467: to preserve transactional integrity, all permissions required for removing a collection have to be checked before any data is removed.
    svn path=/trunk/eXist/; revision=7468
    
    
    Former-commit-id: e7e3fe9d03e79a50c556eaca7f2c74ac429d3b7e

---
+++ test/src/org/exist/collections/AllCollectionTests.java
@@ -0,0 +1,33 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2001-07 The eXist Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * $Id$
+ */
+package org.exist.collections;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        CollectionURITest.class,
+        CollectionRemovalTest.class
+})
+public class AllCollectionTests {
+}
