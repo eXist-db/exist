9646402439427e58c21e13549630bc816787179a
Wolfgang Meier <wolfgangmm@gmail.com>
2008-04-06 21:31:04 +0000

    [ignore] removed group layout: it is Java6-only. netbeans was using this by default.
    svn path=/trunk/eXist/; revision=7616
    
    
    Former-commit-id: c24c3e07a03e4d6dc8cfb6689b0345d813d5033e

--- src/org/exist/storage/repair/CheckerGUI.java
+++ src/org/exist/storage/repair/CheckerGUI.java
@@ -49,64 +49,83 @@ public class CheckerGUI extends javax.swing.JFrame {
      */
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
 
-        progress = new javax.swing.JProgressBar();
+        startBtn = new javax.swing.JButton();
         currentDoc = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
-        jScrollPane1 = new javax.swing.JScrollPane();
+        progress = new javax.swing.JProgressBar();
+        jScrollPane2 = new javax.swing.JScrollPane();
         messages = new javax.swing.JTextArea();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setMinimumSize(new java.awt.Dimension(320, 200));
         addWindowListener(new java.awt.event.WindowAdapter() {
             public void windowClosed(java.awt.event.WindowEvent evt) {
                 formWindowClosed(evt);
             }
         });
+        getContentPane().setLayout(new java.awt.GridBagLayout());
 
-        currentDoc.setText("Processing");
-
-        jButton1.setText("Start");
-        jButton1.addActionListener(new java.awt.event.ActionListener() {
+        startBtn.setText("Start");
+        startBtn.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
-                check(evt);
+                startBtncheck(evt);
             }
         });
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.SOUTHWEST;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        getContentPane().add(startBtn, gridBagConstraints);
+
+        currentDoc.setText("Processing");
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 1;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTH;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        getContentPane().add(currentDoc, gridBagConstraints);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 2;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.HORIZONTAL;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.NORTH;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        getContentPane().add(progress, gridBagConstraints);
+
+        jScrollPane2.setMinimumSize(null);
+        jScrollPane2.setPreferredSize(new java.awt.Dimension(400, 200));
 
         messages.setColumns(20);
         messages.setRows(5);
         messages.setBorder(javax.swing.BorderFactory.createTitledBorder("Messages"));
-        jScrollPane1.setViewportView(messages);
+        messages.setMinimumSize(null);
+        jScrollPane2.setViewportView(messages);
 
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, layout.createSequentialGroup()
-                .addContainerGap()
-                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 376, Short.MAX_VALUE)
-                    .addComponent(currentDoc, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 376, Short.MAX_VALUE)
-                    .addComponent(progress, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.DEFAULT_SIZE, 376, Short.MAX_VALUE)
-                    .addComponent(jButton1, javax.swing.GroupLayout.Alignment.LEADING, javax.swing.GroupLayout.PREFERRED_SIZE, 66, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap())
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGroup(layout.createSequentialGroup()
-                .addContainerGap()
-                .addComponent(jButton1)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(currentDoc, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(progress, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 169, Short.MAX_VALUE)
-                .addContainerGap())
-        );
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.gridx = 0;
+        gridBagConstraints.gridy = 3;
+        gridBagConstraints.gridwidth = 2;
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.anchor = java.awt.GridBagConstraints.SOUTHWEST;
+        gridBagConstraints.weightx = 1.0;
+        gridBagConstraints.weighty = 1.0;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        getContentPane().add(jScrollPane2, gridBagConstraints);
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
-    private void check(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_check
+
+    private void formWindowClosed(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosed
+        BrokerPool.stopAll(false);
+    }//GEN-LAST:event_formWindowClosed
+
+    private void startBtncheck(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_startBtncheck
         Runnable checkRun = new Runnable() {
 
             public void run() {
@@ -131,13 +150,14 @@ public class CheckerGUI extends javax.swing.JFrame {
                     progress.setIndeterminate(false);
                     progress.setMinimum(0);
                     progress.setMaximum(count);
-                    
+
                     displayMessage("Start scanning documents ...");
                     List errors = checker.checkDocuments(cb);
-                    if (errors.size() == 0)
+                    if (errors.size() == 0) {
                         displayMessage("No errors found.");
-                    else
+                    } else {
                         displayMessage("Errors found.");
+                    }
                 } catch (EXistException e) {
                     System.err.println("ERROR: Failed to retrieve database broker: " + e.getMessage());
                 } finally {
@@ -147,11 +167,7 @@ public class CheckerGUI extends javax.swing.JFrame {
             }
         };
         new Thread(checkRun).start();
-    }//GEN-LAST:event_check
-
-    private void formWindowClosed(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowClosed
-        BrokerPool.stopAll(false);
-    }//GEN-LAST:event_formWindowClosed
+}//GEN-LAST:event_startBtncheck
 
     public void displayMessage(String message) {
         messages.append(message + '\n');
@@ -171,9 +187,9 @@ public class CheckerGUI extends javax.swing.JFrame {
     }
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JLabel currentDoc;
-    private javax.swing.JButton jButton1;
-    private javax.swing.JScrollPane jScrollPane1;
+    private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JTextArea messages;
     private javax.swing.JProgressBar progress;
+    private javax.swing.JButton startBtn;
     // End of variables declaration//GEN-END:variables
 }
