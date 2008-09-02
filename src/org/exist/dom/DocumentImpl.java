38899dedcfced443702ddd20fc2b36ec250db379
Wolfgang Meier <wolfgangmm@gmail.com>
2008-09-02 13:18:10 +0000

    [ignore] make sure a stack trace is printed if a lock issue is detected
    svn path=/trunk/eXist/; revision=8138

--- src/org/exist/dom/DocumentImpl.java
+++ src/org/exist/dom/DocumentImpl.java
@@ -463,7 +463,7 @@ public class DocumentImpl extends NodeImpl implements Document, Comparable {
     public void write(VariableByteOutputStream ostream) throws IOException {
 	try {
 	    if (!getCollection().isTempCollection() && !getUpdateLock().isLockedForWrite()) {
-		LOG.warn("document not locked for write !");
+		LOG.warn("document not locked for write !", new Throwable());
 	    }
             ostream.writeInt(docId);
             ostream.writeUTF(fileURI.toString());
