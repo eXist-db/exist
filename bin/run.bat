bdd3dc3c50188c7701cc20d527f95a4f4fb1f6e8
chaeron <andrzej@chaeron.com>
2008-10-09 00:40:37 +0000

    Fixed windows scripts so that they can handle more than 9 command line parameters! #$)(*&%@ing Windows! ;-)
    svn path=/trunk/eXist/; revision=8226

--- bin/run.bat
+++ bin/run.bat
@@ -2,6 +2,24 @@
 
 rem $Id$
 
+rem Slurp the command line arguments. This loop allows for an unlimited number
+rem of arguments (up to the command line limit, anyway).
+
+set CMD_LINE_ARGS=%1
+if ""%1""=="""" goto doneStart
+shift
+:setupArgs
+if ""%1""=="""" goto doneStart
+set CMD_LINE_ARGS=%CMD_LINE_ARGS% %1
+shift
+goto setupArgs
+
+rem This label provides a place for the argument list loop to break out
+rem and for NT handling to skip to.
+
+:doneStart
+
+
 if not "%JAVA_HOME%" == "" goto gotJavaHome
 echo Java environment not found. Please set
 echo your JAVA_HOME environment variable to
@@ -27,5 +45,5 @@ set JAVA_ENDORSED_DIRS="%EXIST_HOME%"\lib\endorsed
 set JAVA_OPTS="-Xms16000k -Xmx256000k -Dfile.encoding=UTF-8 -Djava.endorsed.dirs=%JAVA_ENDORSED_DIRS%"
 
 :gotJavaOpts
-"%JAVA_HOME%\bin\java" "%JAVA_OPTS%"  -Dexist.home="%EXIST_HOME%" -jar "%EXIST_HOME%\start.jar" %1 %2 %3 %4 %5 %6 %7 %8 %9
+"%JAVA_HOME%\bin\java" "%JAVA_OPTS%"  -Dexist.home="%EXIST_HOME%" -jar "%EXIST_HOME%\start.jar" %CMD_LINE_ARGS%
 :eof
