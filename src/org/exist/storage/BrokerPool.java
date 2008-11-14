1de25b867a449e72c8e9c0bc12c6d900b3358760
Wolfgang Meier <wolfgangmm@gmail.com>
2008-11-14 11:38:16 +0000

    [bugfix] fixed issue introduced by rev 8298: user identity not correctly set.
    svn path=/trunk/eXist/; revision=8319

--- src/org/exist/storage/BrokerPool.java
+++ src/org/exist/storage/BrokerPool.java
@@ -1111,7 +1111,8 @@ public class BrokerPool {
 		if(broker != null) {
 			//increase its number of uses
 			broker.incReferenceCount();
-//            broker.setUser(user);
+            if (user != null)
+                broker.setUser(user);
             return broker;
 			//TODO : share the code with what is below (including notifyAll) ?
             // WM: notifyAll is not necessary if we don't have to wait for a broker.
