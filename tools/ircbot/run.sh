2c946b005b0c83c0e2cfed942b204481d0e3a8a1
Leif-Jöran Olsson <ljo@fripost.org>
2008-11-19 00:54:04 +0000

    [ignore] Added some path indirection to the  classpath for ircbot's run.sh script.
    svn path=/trunk/eXist/; revision=8325
    
    
    Former-commit-id: 626a7b69d74820383dbdec48c38b78a4f664b103

--- tools/ircbot/run.sh
+++ tools/ircbot/run.sh
@@ -1,9 +1,12 @@
 #!/bin/sh
 
-EXIST=../..
+if [ -z "${EXIST}" ]; then
+    EXIST="../..";
+fi
 
-CP="$EXIST/exist.jar:lib/pircbot.jar:$EXIST/lib/core/log4j-1.2.15.jar:$EXIST/lib/core/xmldb.jar:$EXIST/lib/core/xmlrpc-1.2-patched.jar:classes"
+
+CP="$EXIST/exist.jar:$EXIST/tools/ircbot/lib/pircbot.jar:$EXIST/lib/core/log4j-1.2.15.jar:$EXIST/lib/core/xmldb.jar:$EXIST/lib/core/xmlrpc-1.2-patched.jar:$EXIST/tools/ircbot/classes"
 
 echo $CP
 
-java -classpath $CP org.exist.irc.XBot
+$JAVA_HOME/bin/java -classpath $CP org.exist.irc.XBot
