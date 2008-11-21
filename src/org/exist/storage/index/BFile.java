b2a7c37ed7d880c4e079d21a48ae342a0579d4dc
Wolfgang Meier <wolfgangmm@gmail.com>
2008-11-21 18:36:19 +0000

    [bugfix] fixed bug in crash recovery. The bug caused the crash recovery process to sometimes fail while redoing transactions, leaving the database in a corrupted state.
    svn path=/branches/eXist-stable-1.2/; revision=8333
    
    
    Former-commit-id: b5384ccff88278af7245a652c297548e99f50852

--- src/org/exist/storage/index/BFile.java
+++ src/org/exist/storage/index/BFile.java
@@ -1424,7 +1424,7 @@ public class BFile extends BTree {
                     dp = new SinglePage(page, data, true);
                 }
             }
-            if ((dp.getPageHeader().getLsn() != Page.NO_PAGE && requiresRedo(loggable, dp)) && loggable != null)
+//            if ((dp.getPageHeader().getLsn() != Page.NO_PAGE && requiresRedo(loggable, dp)) && loggable != null)
                 dp.getPageHeader().setLsn(loggable.getLsn());
             dp.setDirty(true);
             dataCache.add(dp);
