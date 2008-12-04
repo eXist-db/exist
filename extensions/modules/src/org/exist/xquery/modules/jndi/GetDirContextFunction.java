d676986cfbe55a31219f5734d168c3e8c5c07df2
chaeron <andrzej@chaeron.com>
2008-12-04 01:28:36 +0000

    New JNDI eXist XQuery extension module.
    This extension module allows you to access and manipulate JNDI-based directories, such as LDAP, using XQuery functions.
    It can be very useful if you want to integration and LDAP directory into an eXist/XQuery based application.
    
    It is not compiled nor included by default.
    
    To compile it, set the parameter include.module.jndi = true in $EXIST_HOME/extensions/modules/build.properties file.
    
    To enable it, edit the appropriate module entry in conf.xml
    
    Enjoy!
    
    svn path=/trunk/eXist/; revision=8362

---
+++ extensions/modules/src/org/exist/xquery/modules/jndi/GetDirContextFunction.java
@@ -0,0 +1,124 @@
+/*
+ *  eXist SQL Module Extension GetConnectionFunction
+ *  Copyright (C) 2008 Adam Retter <adam@exist-db.org>
+ *  www.adamretter.co.uk
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  
+ *  $Id: GetConnectionFunction.java 4126 2006-09-18 21:20:17 +0000 (Mon, 18 Sep 2006) deliriumsky $
+ */
+
+package org.exist.xquery.modules.jndi;
+
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+
+import java.util.Properties;
+
+import org.exist.dom.QName;
+import org.exist.xquery.BasicFunction;
+import org.exist.xquery.Cardinality;
+import org.exist.xquery.FunctionSignature;
+import org.exist.xquery.XPathException;
+import org.exist.xquery.XQueryContext;
+import org.exist.xquery.modules.ModuleUtils;
+import org.exist.xquery.value.IntegerValue;
+import org.exist.xquery.value.NodeValue;
+import org.exist.xquery.value.Sequence;
+import org.exist.xquery.value.SequenceType;
+import org.exist.xquery.value.Type;
+
+/**
+ * eXist JNDI Module Extension GetDirContextFunction
+ * 
+ * Get a connection to a JNDI Directory
+ * 
+ * @author Andrzej Taramina <andrzej@chaeron.com>
+ * @serial 2008-12-02
+ * @version 1.0
+ * 
+ * @see org.exist.xquery.BasicFunction#BasicFunction(org.exist.xquery.XQueryContext,
+ *      org.exist.xquery.FunctionSignature)
+ */
+
+public class GetDirContextFunction extends BasicFunction 
+{
+	public final static FunctionSignature[] signatures = {
+			
+			new FunctionSignature(
+					new QName( "get-dir-context", JNDIModule.NAMESPACE_URI, JNDIModule.PREFIX ),
+					"Open's a JNDI Directory Context. Expects "
+							+ " JNDI Directory Context environment properties to be set in $a in the"
+							+ " form <properties><property name=\"\" value=\"\"/></properties>. "
+							+ "Returns an xs:long representing the directory context handle.",
+					new SequenceType[] {
+							new SequenceType( Type.ELEMENT, Cardinality.ZERO_OR_ONE ) 
+					},
+					new SequenceType( Type.LONG, Cardinality.ZERO_OR_ONE ) )
+			};
+
+	/**
+	 * GetDirContextFunction Constructor
+	 * 
+	 * @param context
+	 *            The Context of the calling XQuery
+	 */
+	
+	public GetDirContextFunction( XQueryContext context, FunctionSignature signature ) 
+	{
+		super( context, signature );
+	}
+
+	
+	/**
+	 * evaluate the call to the xquery get-dir-context() function, it is really
+	 * the main entry point of this class
+	 * 
+	 * @param args				arguments from the get-connection() function call
+	 * @param contextSequence 	the Context Sequence to operate on (not used here internally!)
+	 * @return 					A xs:long representing a handle to the connection
+	 * 
+	 * @see org.exist.xquery.BasicFunction#eval(org.exist.xquery.value.Sequence[],
+	 *      org.exist.xquery.value.Sequence)
+	 */
+	
+	public Sequence eval( Sequence[] args, Sequence contextSequence ) throws XPathException 
+	{
+		// Were properties specified
+		if( args[0].isEmpty() ) {
+			return( Sequence.EMPTY_SEQUENCE );
+		}
+
+		try {
+			DirContext dirCtx = null;
+
+			Properties env = ModuleUtils.parseProperties( ( (NodeValue)args[0].itemAt( 0 ) ).getNode() );
+			
+			dirCtx = new InitialDirContext( env );
+
+			// store the JNDI Directory Context and return the uid handle of the context
+			
+			return( new IntegerValue( JNDIModule.storeJNDIContext( context, dirCtx ) ) );
+		}
+		catch( NamingException ne ) {
+			LOG.error( "jndi:get-dir-context() Cannot get JNDI directory context: " + ne );
+			throw( new XPathException( getASTNode(), "jndi:get-dir-context() Cannot get JNDI directory context: " + ne ) );
+		}
+	}
+}
---
+++ extensions/modules/src/org/exist/xquery/modules/jndi/SearchFunction.java
@@ -0,0 +1,264 @@
+/*
+ *  eXist SQL Module Extension GetConnectionFunction
+ *  Copyright (C) 2008 Adam Retter <adam@exist-db.org>
+ *  www.adamretter.co.uk
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  
+ *  $Id: GetConnectionFunction.java 4126 2006-09-18 21:20:17 +0000 (Mon, 18 Sep 2006) deliriumsky $
+ */
+
+package org.exist.xquery.modules.jndi;
+
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingEnumeration;
+import javax.naming.NamingException;
+import javax.naming.NameNotFoundException;
+import javax.naming.directory.Attribute;
+import javax.naming.directory.BasicAttribute;
+import javax.naming.directory.BasicAttributes;
+import javax.naming.directory.DirContext;
+import javax.naming.directory.InitialDirContext;
+import javax.naming.directory.SearchResult;
+import javax.naming.directory.SearchControls;
+
+
+import java.util.Properties;
+
+import org.exist.dom.QName;
+import org.exist.memtree.MemTreeBuilder;
+import org.exist.util.Base64Encoder;
+import org.exist.xquery.BasicFunction;
+import org.exist.xquery.Cardinality;
+import org.exist.xquery.FunctionSignature;
+import org.exist.xquery.XPathException;
+import org.exist.xquery.XQueryContext;
+import org.exist.xquery.modules.ModuleUtils;
+import org.exist.xquery.value.IntegerValue;
+import org.exist.xquery.value.NodeValue;
+import org.exist.xquery.value.Sequence;
+import org.exist.xquery.value.SequenceType;
+import org.exist.xquery.value.Type;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+
+/**
+ * eXist JNDI Module Extension SearchFunction
+ * 
+ * Search a JNDI Directory
+ * 
+ * @author Andrzej Taramina <andrzej@chaeron.com>
+ * @serial 2008-12-02
+ * @version 1.0
+ * 
+ * @see org.exist.xquery.BasicFunction#BasicFunction(org.exist.xquery.XQueryContext,
+ *      org.exist.xquery.FunctionSignature)
+ */
+
+public class SearchFunction extends BasicFunction 
+{
+	
+	public final static String DSML_NAMESPACE = "http://www.dsml.org/DSML";
+
+	public final static String DSML_PREFIX = "dsml";
+
+	public final static FunctionSignature[] signatures = {
+			
+			new FunctionSignature(
+					new QName( "search", JNDIModule.NAMESPACE_URI, JNDIModule.PREFIX ),
+							"Searches a JNDI Directory by attributes. $a is the directory context handle from a jndi:get-dir-context() call. $b is the DN. Expects "
+							+ " search attributes to be set in $c in the"
+							+ " form <attributes><attribute name=\"\" value=\"\"/></attributes>. "
+							+ "Search results are returned in DSML format",
+					new SequenceType[] {
+							new SequenceType( Type.INTEGER, Cardinality.EXACTLY_ONE ), 
+							new SequenceType( Type.STRING, Cardinality.EXACTLY_ONE ), 
+							new SequenceType( Type.ELEMENT, Cardinality.EXACTLY_ONE ) 
+					},
+					new SequenceType( Type.NODE, Cardinality.ZERO_OR_ONE ) ),
+			
+			new FunctionSignature(
+					new QName( "search", JNDIModule.NAMESPACE_URI, JNDIModule.PREFIX ),
+							"Searches a JNDI Directory by filter. $a is the directory context handle from a jndi:get-dir-context() call. "
+							+ "$b is the DN. $c is the filter string. $d is the scope, which has a value of 'object', 'onelevel' or 'subtree'."
+							+ "Search results are returned in DSML format",
+					new SequenceType[] {
+							new SequenceType( Type.INTEGER, Cardinality.EXACTLY_ONE ), 
+							new SequenceType( Type.STRING, Cardinality.EXACTLY_ONE ), 
+							new SequenceType( Type.STRING, Cardinality.EXACTLY_ONE ),
+							new SequenceType( Type.STRING, Cardinality.EXACTLY_ONE ) 
+					},
+					new SequenceType( Type.NODE, Cardinality.ZERO_OR_ONE ) )
+			};
+
+	/**
+	 * SearchFunction Constructor
+	 * 
+	 * @param context 	The Context of the calling XQuery
+	 */
+	
+	public SearchFunction( XQueryContext context, FunctionSignature signature ) 
+	{
+		super( context, signature );
+	}
+
+	
+	/**
+	 * evaluate the call to the xquery search() function, it is really
+	 * the main entry point of this class
+	 * 
+	 * @param args				arguments from the get-connection() function call
+	 * @param contextSequence 	the Context Sequence to operate on (not used here internally!)
+	 * @return 					A xs:long representing a handle to the connection
+	 * 
+	 * @see org.exist.xquery.BasicFunction#eval(org.exist.xquery.value.Sequence[],
+	 *      org.exist.xquery.value.Sequence)
+	 */
+	
+	public Sequence eval( Sequence[] args, Sequence contextSequence ) throws XPathException 
+	{
+		Sequence    xmlResult     = Sequence.EMPTY_SEQUENCE;
+		
+		// Was context handle or DN specified?
+		if( !( args[0].isEmpty() ) && !( args[1].isEmpty() ) ) {
+			
+			String dn = args[1].getStringValue();
+			
+			try {
+				long ctxID = ((IntegerValue)args[0].itemAt(0)).getLong();
+				
+				DirContext ctx = (DirContext)JNDIModule.retrieveJNDIContext( context, ctxID );
+				
+				if( ctx == null ) {
+					LOG.error( "jndi:search() - Invalid JNDI context handle provided: " + ctxID );
+				} else {
+					NamingEnumeration results = null;
+					
+					if( args.length == 3 ) {                									// Attributes search			
+						BasicAttributes attributes = JNDIModule.parseAttributes( args[ 2 ] );
+						
+						results = ctx.search( dn, attributes );
+					} else {                													// Filter search
+						int 	scopeCode 	= 0;
+						
+						String 	filter 		= args[2].getStringValue();
+						String 	scope 		= args[3].getStringValue();
+						
+						if(  scope.equalsIgnoreCase( "object" ) ) {
+							scopeCode = 0;
+						} else if(  scope.equalsIgnoreCase( "onelevel" ) ) {
+							scopeCode = 1;
+						} else if(  scope.equalsIgnoreCase( "subtree" ) ) {
+							scopeCode = 2;
+						}
+						
+						results = ctx.search( dn, filter, new SearchControls( scopeCode, 0, 0, null, false, false ) );
+					}
+					
+					xmlResult = renderSearchResultsAsDSML( results, dn );
+				}
+			}
+			catch( NameNotFoundException nf ) {
+				LOG.warn( "jndi:search() Not found for dn [" + dn + "]" );
+			}
+			catch( NamingException ne ) {
+				LOG.error( "jndi:search() Search failed for dn [" + dn + "]: " + ne );
+				throw( new XPathException( getASTNode(), "jndi:search() Search failed for dn [" + dn + "]: " + ne ) );
+			}
+		}
+		
+		return( xmlResult );
+	}
+
+	
+	
+	private Sequence renderSearchResultsAsDSML( NamingEnumeration results, String dn ) throws NamingException
+	{
+		Sequence    xmlResult     = Sequence.EMPTY_SEQUENCE;
+		
+		MemTreeBuilder builder = context.getDocumentBuilder();
+					
+		builder.startDocument();
+    	builder.startElement( new QName( "dsml", DSML_NAMESPACE, DSML_PREFIX ), null );
+		builder.addAttribute( new QName( "dn", null, null ), dn );
+		builder.startElement( new QName( "directory-entries", DSML_NAMESPACE, DSML_PREFIX ), null );
+		
+		while( results.hasMore() ) {
+			SearchResult result = (SearchResult)results.next();
+			
+			builder.startElement( new QName( "entry", DSML_NAMESPACE, DSML_PREFIX ), null );
+			builder.addAttribute( new QName( "dn", null, null ), result.getName() );
+
+			// Handle objectClass attributes
+			Attribute ocattr = result.getAttributes().get( "objectClass" );
+			
+			if( ocattr != null ) {
+				builder.startElement( new QName( "objectclass", DSML_NAMESPACE, DSML_PREFIX ), null );
+				
+				for( int i = 0; i < ocattr.size(); i++ ) {
+						Object value = ocattr.get( i );
+						
+						builder.startElement( new QName( "oc-value", DSML_NAMESPACE, DSML_PREFIX ), null );
+						builder.characters( value.toString() );
+						builder.endElement();
+					}
+				
+				builder.endElement();
+			}
+			
+			NamingEnumeration attrs = result.getAttributes().getAll();
+			
+			// Handle all other attributes
+			while( attrs.hasMore() ) {
+				Attribute attr = (Attribute)attrs.next();
+				
+				String name = attr.getID();
+				
+				if( !name.equals( "objectClass" ) ) {
+				
+					builder.startElement( new QName( "attr", DSML_NAMESPACE, DSML_PREFIX ), null );
+					builder.addAttribute( new QName( "name", null, null ), name );
+					
+					for( int i = 0; i < attr.size(); i++ ) {
+						Object value = attr.get( i );
+						
+						builder.startElement( new QName( "value", DSML_NAMESPACE, DSML_PREFIX ), null );
+						if( name.equals( "userPassword" ) ) {
+							builder.characters( new String( (byte[])value ) );
+						} else {
+							builder.characters( value.toString() );
+						}
+						builder.endElement();
+					}
+					
+					builder.endElement();
+				}
+			}
+			
+			builder.endElement();
+		}
+		
+		builder.endElement();
+		builder.endElement();
+    
+    	xmlResult = (NodeValue)builder.getDocument().getDocumentElement();
+		
+		return( xmlResult );
+	}
+}
