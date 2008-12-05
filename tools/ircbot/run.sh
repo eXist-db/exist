623813dcba315eb51c56ca2b9109d72c786f21db
Leif-Jöran Olsson <ljo@fripost.org>
2008-12-05 13:09:50 +0000

    Added new xmlprc jars to ircbot cp.
    svn path=/trunk/eXist/; revision=8367
    
    
    Former-commit-id: 626a38670d74bdad3515c140960ea65dca97f828

--- tools/ircbot/run.sh
+++ tools/ircbot/run.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+# $Id$
 
 if [ -z "${EXIST}" ]; then
-    EXIST="../..";
+    EXIST="/home/existdb/exist-db.org/eXist/";
 fi
 
 
-CP="$EXIST/exist.jar:$EXIST/tools/ircbot/lib/pircbot.jar:$EXIST/lib/core/log4j-1.2.15.jar:$EXIST/lib/core/xmldb.jar:$EXIST/lib/core/xmlrpc-1.2-patched.jar:$EXIST/tools/ircbot/classes"
+CP="$EXIST/exist.jar:$EXIST/tools/ircbot/lib/pircbot.jar:$EXIST/lib/core/log4j-1.2.15.jar:$EXIST/lib/core/xmldb.jar:$EXIST/lib/core/xmlrpc-client-3.1.1.jar:$EXIST/lib/core/xmlrpc-server-3.1.1.jar:$EXIST/lib/core/xmlrpc-common-3.1.1.jar:$EXIST/lib/core/ws-commons-util-1.0.2.jar:$EXIST/tools/ircbot/classes"
 
 echo $CP
 
