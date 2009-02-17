f72b82a7b6d9056fe0e8119dbe9a55d0d38b6ca6
Dannes Wessels <dannes@exist-db.org>
2009-02-17 10:51:36 +0000

    [ignore] we live in 2009 right now
    svn path=/trunk/eXist/; revision=8606
    
    
    Former-commit-id: 3edcdebe864c2c4a0b3e538e649e6959707a9eac

--- src/org/exist/client/InteractiveClient.java
+++ src/org/exist/client/InteractiveClient.java
@@ -2612,7 +2612,7 @@ public class InteractiveClient {
         
         messageln(sysProperties.getProperty("product-name") + " version " 
                 + sysProperties.getProperty("product-version") 
-                +", Copyright (C) 2001-2008 Wolfgang Meier");
+                +", Copyright (C) 2001-2009 Wolfgang Meier");
         messageln("eXist comes with ABSOLUTELY NO WARRANTY.");
         messageln("This is free software, and you are welcome to "
                 + "redistribute it\nunder certain conditions; "
