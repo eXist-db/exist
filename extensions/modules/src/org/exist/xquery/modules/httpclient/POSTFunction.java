87faf8474810d08dca718ab29ca44109a46ad423
Adam Retter <adam.retter@googlemail.com>
2009-02-19 21:52:32 +0000

    New method for POST of form data. Needed for things like reCaptcha where the content type MUST be application/x-www-form-urlencoded
    svn path=/trunk/eXist/; revision=8616

--- extensions/modules/src/org/exist/xquery/modules/httpclient/POSTFunction.java
+++ extensions/modules/src/org/exist/xquery/modules/httpclient/POSTFunction.java
@@ -22,9 +22,11 @@
  */
 package org.exist.xquery.modules.httpclient;
 
+import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.ByteArrayRequestEntity;
 import org.apache.commons.httpclient.methods.PostMethod;
 import org.apache.commons.httpclient.methods.RequestEntity;
+import org.apache.commons.httpclient.methods.StringRequestEntity;
 import org.exist.dom.QName;
 import org.exist.util.serializer.SAXSerializer;
 import org.exist.xquery.Cardinality;
@@ -32,6 +34,9 @@ import org.exist.xquery.FunctionSignature;
 import org.exist.xquery.XPathException;
 import org.exist.xquery.XQueryContext;
 import org.exist.xquery.value.*;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -40,105 +45,176 @@ import java.io.UnsupportedEncodingException;
 import java.util.Properties;
 
 /**
- * @author Adam Retter <adam.retter@devon.gov.uk>
+ * @author Adam Retter <adam@exist-db.org>
  * @author Andrzej Taramina <andrzej@chaeron.com>
- * @serial 20070905
- * @version 1.2
+ * @serial 20090219
+ * @version 1.3
  */
 public class POSTFunction extends BaseHTTPClientFunction
 {
-    public final static FunctionSignature signature =
+    public final static FunctionSignature signatures[] = {
         new FunctionSignature(
-        new QName( "post", NAMESPACE_URI, PREFIX ),
-        "Performs a HTTP POST request. $a is the URL, $b is the POST payload/content. If $b is an XML Node it will be serialized, any other type will be atomized into a string. $c determines if cookies persist for the query lifetime. $d defines any HTTP Request Headers to set in the form <headers><header name=\"\" value=\"\"/></headers>."
-        + " This method returns the HTTP response encoded as an XML fragment, that looks as follows: <httpclient:response xmlns:httpclient=\"http://exist-db.org/xquery/httpclient\" statusCode=\"200\"><httpclient:headers><httpclient:header name=\"name\" value=\"value\"/>...</httpclient:headers><httpclient:body type=\"xml|xhtml|text|binary\" mimetype=\"returned content mimetype\">body content</httpclient:body></httpclient:response>"
-        + " where XML body content will be returned as a Node, HTML body content will be tidied into an XML compatible form, a body with mime-type of \"text/...\" will be returned as a URLEncoded string, and any other body content will be returned as xs:base64Binary encoded data.",
-        new SequenceType[] {
-            new SequenceType( Type.ANY_URI, Cardinality.EXACTLY_ONE ),
-            new SequenceType( Type.ITEM, Cardinality.EXACTLY_ONE ),
-            new SequenceType( Type.BOOLEAN, Cardinality.EXACTLY_ONE ),
-            new SequenceType( Type.ELEMENT, Cardinality.ZERO_OR_ONE )
-            },
-        new SequenceType( Type.ITEM, Cardinality.EXACTLY_ONE )
-        );
+	        new QName( "post", NAMESPACE_URI, PREFIX ),
+	        "Performs a HTTP POST request. $a is the URL, $b is the POST payload/content. If $b is an XML Node it will be serialized, any other type will be atomized into a string. $c determines if cookies persist for the query lifetime. $d defines any HTTP Request Headers to set in the form <headers><header name=\"\" value=\"\"/></headers>."
+	        + " This method returns the HTTP response encoded as an XML fragment, that looks as follows: <httpclient:response xmlns:httpclient=\"http://exist-db.org/xquery/httpclient\" statusCode=\"200\"><httpclient:headers><httpclient:header name=\"name\" value=\"value\"/>...</httpclient:headers><httpclient:body type=\"xml|xhtml|text|binary\" mimetype=\"returned content mimetype\">body content</httpclient:body></httpclient:response>"
+	        + " where XML body content will be returned as a Node, HTML body content will be tidied into an XML compatible form, a body with mime-type of \"text/...\" will be returned as a URLEncoded string, and any other body content will be returned as xs:base64Binary encoded data.",
+	        new SequenceType[] {
+	            new SequenceType( Type.ANY_URI, Cardinality.EXACTLY_ONE ),
+	            new SequenceType( Type.ITEM, Cardinality.EXACTLY_ONE ),
+	            new SequenceType( Type.BOOLEAN, Cardinality.EXACTLY_ONE ),
+	            new SequenceType( Type.ELEMENT, Cardinality.ZERO_OR_ONE )
+	            },
+	        new SequenceType( Type.ITEM, Cardinality.EXACTLY_ONE )
+        ),
+        new FunctionSignature(
+	        new QName( "post-form", NAMESPACE_URI, PREFIX ),
+	        "Performs a HTTP POST request for a form. $a is the URL, $b is the form data in the format <httpclient:fields><httpclient:field name=\"\" value=\"\"/>...</httpclient:fields>. If the field values in $b will be suitably URLEncoded and sent with the mime type application/x-www-form-urlencoded. $c determines if cookies persist for the query lifetime. $d defines any HTTP Request Headers to set in the form <headers><header name=\"\" value=\"\"/></headers>."
+	        + " This method returns the HTTP response encoded as an XML fragment, that looks as follows: <httpclient:response xmlns:httpclient=\"http://exist-db.org/xquery/httpclient\" statusCode=\"200\"><httpclient:headers><httpclient:header name=\"name\" value=\"value\"/>...</httpclient:headers><httpclient:body type=\"xml|xhtml|text|binary\" mimetype=\"returned content mimetype\">body content</httpclient:body></httpclient:response>"
+	        + " where XML body content will be returned as a Node, HTML body content will be tidied into an XML compatible form, a body with mime-type of \"text/...\" will be returned as a URLEncoded string, and any other body content will be returned as xs:base64Binary encoded data.",
+	        new SequenceType[] {
+	            new SequenceType( Type.ANY_URI, Cardinality.EXACTLY_ONE),
+	            new SequenceType( Type.ELEMENT, Cardinality.EXACTLY_ONE ),
+	            new SequenceType( Type.BOOLEAN, Cardinality.EXACTLY_ONE ),
+	            new SequenceType( Type.ELEMENT, Cardinality.ZERO_OR_ONE )
+	            },
+	        new SequenceType( Type.ITEM, Cardinality.EXACTLY_ONE )
+        )
+    };
     
     
-    public POSTFunction( XQueryContext context )
+    public POSTFunction(XQueryContext context, FunctionSignature signature)
     {
-        super( context, signature );
+        super(context, signature);
     }
     
     
-    public Sequence eval( Sequence[] args, Sequence contextSequence ) throws XPathException
+    public Sequence eval(Sequence[] args, Sequence contextSequence) throws XPathException
     {
-        Sequence    response   = null;
-		byte[]      reqPayload = null;
-		String		mime	   = "text/xml; charset=utf-8";
+        Sequence response = null;
         
         // must be a URL
-        if( args[0].isEmpty() ) {
-            return( Sequence.EMPTY_SEQUENCE );
+        if(args[0].isEmpty())
+        {
+            return(Sequence.EMPTY_SEQUENCE);
         }
         
         //get the url
-        String url                  = args[0].itemAt( 0 ).getStringValue();
+        String url = args[0].itemAt(0).getStringValue();
         
         //get the payload
-        Item payload                = args[1].itemAt( 0 );
+        
+        Item payload = args[1].itemAt(0);
         
         //get the persist cookies
-        boolean persistCookies      = args[2].effectiveBooleanValue();
+        boolean persistCookies = args[2].effectiveBooleanValue();
 		
-		if( Type.subTypeOf( payload.getType(), Type.NODE ) ) {
-        
-	        //serialize the node to SAX
-	        ByteArrayOutputStream baos  = new ByteArrayOutputStream();
-	        OutputStreamWriter osw      = null;
-	        try {
-	            osw = new OutputStreamWriter( baos, "UTF-8" );
-	        } catch (UnsupportedEncodingException e) {
-	            throw new XPathException(getASTNode(), "Internal error");
-	        }
-	        SAXSerializer sax           = new SAXSerializer(osw, new Properties());
-	        
-	        try {
-	            payload.toSAX( context.getBroker(), sax, new Properties() );
-	            osw.flush();
-	            osw.close();
-	        }
-	        catch( Exception e ) {
-	            e.printStackTrace();
-	        } 
-			
-			reqPayload = baos.toByteArray();
-		} else {
-			reqPayload = payload.getStringValue().getBytes();
-			mime = "text/text; charset=utf-8";
-		}
-		
-        //setup POST request
-        PostMethod post         = new PostMethod( url );
-        RequestEntity entity    = new ByteArrayRequestEntity( reqPayload, mime );
-        
-        post.setRequestEntity( entity );
-        
-        //setup POST Request Headers
-        if( !args[3].isEmpty() ) {
-            setHeaders( post, ((NodeValue)args[3].itemAt(0)).getNode() );
+        PostMethod post = new PostMethod(url);
+        if(isCalledAs("post"))
+        {
+        	RequestEntity entity = null;
+    		if(Type.subTypeOf(payload.getType(), Type.NODE))
+    		{
+    	        //serialize the node to SAX
+    	        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    	        OutputStreamWriter osw = null;
+    	        try
+    	        {
+    	            osw = new OutputStreamWriter(baos, "UTF-8");
+    	        }
+    	        catch(UnsupportedEncodingException e)
+    	        {
+    	            throw new XPathException(getASTNode(), e.getMessage());
+    	        }
+    	        
+    	        SAXSerializer sax = new SAXSerializer(osw, new Properties());
+    	        
+    	        try
+    	        {
+    	            payload.toSAX(context.getBroker(), sax, new Properties());
+    	            osw.flush();
+    	            osw.close();
+    	        }
+    	        catch(Exception e)
+    	        {
+    	        	throw new XPathException(getASTNode(), e.getMessage());
+    	        } 
+    			
+    			byte reqPayload[] = baos.toByteArray();
+    			entity = new ByteArrayRequestEntity(reqPayload, "text/xml; charset=utf-8");
+    		}
+    		else
+    		{
+    			try
+    			{
+    				entity = new StringRequestEntity(payload.getStringValue(), "text/text; charset=utf-8", "UTF-8");
+    			}
+    			catch(UnsupportedEncodingException uee)
+    			{
+    				uee.printStackTrace();
+    			}
+    		}
+    		
+    		post.setRequestEntity(entity);
         }
-        
-        try {
-            //execute the request
-            response = doRequest( context, post, persistCookies );
+        else if(isCalledAs("post-form"))
+        {
+			Node nPayload = ((NodeValue)payload).getNode();
+			if (nPayload instanceof Element && nPayload.getNamespaceURI().equals(HTTPClientModule.NAMESPACE_URI) && nPayload.getLocalName().equals("fields"))
+			{
+				NameValuePair[] nvPairs = parseFields((Element)nPayload);
+				post.setRequestBody(nvPairs);
+			}
+			else
+			{
+				throw new XPathException(getASTNode(), "fields must be provided");
+			}
 
         }
-        catch( IOException ioe ) {
-            throw( new XPathException(getASTNode(), ioe ) );
+        else
+        {
+        	return(Sequence.EMPTY_SEQUENCE);
         }
-        finally {
+        
+        
+        //setup POST Request Headers
+        if(!args[3].isEmpty())
+        {
+            setHeaders(post, ((NodeValue)args[3].itemAt(0)).getNode());
+        }
+        
+        try
+        {
+            //execute the request
+            response = doRequest(context, post, persistCookies);
+
+        }
+        catch(IOException ioe)
+        {
+            throw new XPathException(getASTNode(), ioe);
+        }
+        finally
+        {
             post.releaseConnection();
         }
         
-        return( response );
+        return(response);
     }
+    
+    private NameValuePair[] parseFields(Element fields)
+    {
+    	NodeList nlField = fields.getElementsByTagNameNS(HTTPClientModule.NAMESPACE_URI, "field");
+    	
+    	NameValuePair[] nameValuePairs = new NameValuePair[nlField.getLength()];
+    	
+    	for(int i = 0; i < nlField.getLength(); i++)
+    	{
+    		Element field = (Element)nlField.item(i);
+    		
+    		nameValuePairs[i] = new NameValuePair(field.getAttribute("name"), field.getAttribute("value"));
+    		
+    	}
+    	
+    	return nameValuePairs;
+    }
+    
 }
