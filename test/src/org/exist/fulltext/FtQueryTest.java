2234f872384d7c6c025a77556579b802ced1a8b6
Wolfgang Meier <wolfgangmm@gmail.com>
2009-02-23 12:49:51 +0000

    [testsuite] some tests were failing because they use the MODS sample files and we added some more MODS entries for the eXist articles.
    svn path=/branches/eXist-stable-1.2/; revision=8643

--- test/src/org/exist/fulltext/FtQueryTest.java
+++ test/src/org/exist/fulltext/FtQueryTest.java
@@ -204,7 +204,7 @@ public class FtQueryTest extends XMLTestCase {
 
 	        query = queryBody + "t:index-terms(collection('" + TEST_COLLECTION_PATH + "')//mods:title, \'s\', util:function(\'f:term-callback\', 2), 1000)";
 	        result = service.query(query);
-	        assertEquals(20, result.getSize());
+	        assertEquals(30, result.getSize());
 		} catch (XMLDBException e) {
 			e.printStackTrace();
 			fail(e.getMessage());
