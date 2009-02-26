4fcb1f2ed2299186fa95e513702d79e666501782
Piotr Kaminski <piotr@ideanest.com>
2009-02-26 01:14:29 +0000

    Bug fix:  a query now holds a unique copy of every needed module, resolving some false variable duplication errors and NPEs.
    svn path=/trunk/eXist/; revision=8677

--- src/org/exist/xquery/FunctionCall.java
+++ src/org/exist/xquery/FunctionCall.java
@@ -101,8 +101,8 @@ public class FunctionCall extends Function {
 			if (functionDef.getName().getNamespaceURI().equals(modContext.getModuleNamespace()) &&
 					modContext.getParentContext() != context) {
 				UserDefinedFunction replacementFunctionDef =
-					((ExternalModule) context.getModule(functionDef.getName().getNamespaceURI()))
-					.getFunction(functionDef.getName(), getArgumentCount());
+					((ExternalModule) context.getRootModule(functionDef.getName().getNamespaceURI()))
+							.getFunction(functionDef.getName(), getArgumentCount());
 				if (replacementFunctionDef == null) throw new NoSuchElementException("internal error:  unable to rebind cached function reference " + functionDef.getName().getStringValue() + "/" + getArgumentCount() + " to new instance of module");
 				expression = functionDef = replacementFunctionDef;
 			}
