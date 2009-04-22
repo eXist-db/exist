46bea894caa6d3a1db5f24d3146ec3c5df8cd5c0
Adam Retter <adam.retter@googlemail.com>
2009-04-22 21:46:22 +0000

    Removed dependency on other tests and upgraded to JUnit 4
    svn path=/trunk/eXist/; revision=8873

--- test/src/org/exist/validation/DatabaseInsertResources_NoValidation_Test.java
+++ test/src/org/exist/validation/DatabaseInsertResources_NoValidation_Test.java
@@ -21,124 +21,146 @@
  */
 package org.exist.validation;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
-
 import org.apache.log4j.Logger;
+import org.exist.collections.Collection;
+import org.exist.security.User;
 import org.exist.storage.BrokerPool;
+import org.exist.storage.DBBroker;
 import org.exist.storage.io.ExistIOException;
+import org.exist.storage.txn.TransactionManager;
+import org.exist.storage.txn.Txn;
 import org.exist.util.Configuration;
 import org.exist.util.ConfigurationHelper;
 import org.exist.util.XMLReaderObjectFactory;
+import org.exist.xmldb.XmldbURI;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.fail;
 
 /**
  *  Insert documents for validation tests.
  *
  * @author Dannes Wessels (dizzzz@exist-db.org)
  */
-public class DatabaseInsertResources_NoValidation_Test extends TestCase {
-    
+public class DatabaseInsertResources_NoValidation_Test {
+
+
     private final static Logger logger = Logger.getLogger(DatabaseInsertResources_NoValidation_Test.class);
-    
-    private static BrokerPool pool;
-    private static String eXistHome;
-    private static Configuration config;
-    
-    public DatabaseInsertResources_NoValidation_Test(String testName) {
-        super(testName);
-    }
-    
-    public static Test suite() {
-        TestSuite suite = new TestSuite(DatabaseInsertResources_NoValidation_Test.class);
-        return suite;
-    }
-    
-    protected BrokerPool startDB() {
-        try {
+
+    private static String eXistHome = ConfigurationHelper.getExistHome().getAbsolutePath();
+    private static BrokerPool pool = null;
+    private static Configuration config = null;
+
+
+
+
+
+
+    // ---------------------------------------------------
+
+    @BeforeClass
+    public static void startup()
+    {
+        DBBroker broker = null;
+        TransactionManager transact = null;
+        Txn txn = null;
+        try
+        {
             config = new Configuration();
             config.setProperty(XMLReaderObjectFactory.PROPERTY_VALIDATION_MODE, "auto");
             BrokerPool.configure(1, 5, config);
-            return BrokerPool.getInstance();
-        } catch (Exception e) {
+            pool = BrokerPool.getInstance();
+
+            broker = pool.get(User.DEFAULT);
+            transact = pool.getTransactionManager();
+            txn = transact.beginTransaction();
+
+            /** create nessecary collections if they dont exist */
+
+            Collection col = broker.getOrCreateCollection(txn, XmldbURI.create(TestTools.VALIDATION_DTD));
+            broker.saveCollection(txn, col);
+
+            col = broker.getOrCreateCollection(txn, XmldbURI.create(TestTools.VALIDATION_XSD));
+            broker.saveCollection(txn, col);
+
+            col = broker.getOrCreateCollection(txn, XmldbURI.create(TestTools.VALIDATION_TMP));
+            broker.saveCollection(txn, col);
+
+            transact.commit(txn);
+        }
+        catch(Exception e)
+        {
+            if(transact != null && txn != null)
+                transact.abort(txn);
+
             e.printStackTrace();
             fail(e.getMessage());
         }
-        return null;
+        finally
+        {
+            if(broker != null)
+                pool.release(broker);
+        }
     }
-    
-    
-    // ---------------------------------------------------
-    
-    public void testStart() {
-        System.out.println(this.getName());
-        eXistHome = ConfigurationHelper.getExistHome().getAbsolutePath();
-        pool = startDB();
-    }
-    
-    
 
-    
     /**
      * Insert all documents into database, switch of validation.
      */
-    public void testInsertValidationResources(){
-        System.out.println(this.getName());
-        
+    @Test
+    public void insertValidationResources(){
         try{
             config.setProperty(XMLReaderObjectFactory.PROPERTY_VALIDATION_MODE, "no");
-            
+
             String addressbook=eXistHome+"/samples/validation/addressbook";
-            
+
             TestTools.insertDocumentToURL(addressbook+"/addressbook.xsd",
                 "xmldb:exist://"+TestTools.VALIDATION_XSD+"/addressbook.xsd");
             TestTools.insertDocumentToURL(addressbook+"/catalog.xml",
                 "xmldb:exist://"+TestTools.VALIDATION_XSD+"/catalog.xml");
-            
+
             TestTools.insertDocumentToURL(addressbook+"/addressbook_valid.xml",
                 "xmldb:exist://"+TestTools.VALIDATION_HOME+"/addressbook_valid.xml");
             TestTools.insertDocumentToURL(addressbook+"/addressbook_invalid.xml",
                 "xmldb:exist://"+TestTools.VALIDATION_HOME+"/addressbook_invalid.xml");
-            
+
             // ----------------------
-            
+
             String hamlet=eXistHome+"/samples/validation/dtd";
-            
+
             TestTools.insertDocumentToURL(hamlet+"/hamlet.dtd",
                 "xmldb:exist://"+TestTools.VALIDATION_DTD+"/hamlet.dtd");
             TestTools.insertDocumentToURL(hamlet+"/catalog.xml",
                 "xmldb:exist://"+TestTools.VALIDATION_DTD+"/catalog.xml");
-            
+
             TestTools.insertDocumentToURL(hamlet+"/hamlet_valid.xml",
                 "xmldb:exist://"+TestTools.VALIDATION_HOME+"/hamlet_valid.xml");
             TestTools.insertDocumentToURL(hamlet+"/hamlet_invalid.xml",
                 "xmldb:exist://"+TestTools.VALIDATION_HOME+"/hamlet_invalid.xml");
-            
+
             // ----------------------
-            
+
             TestTools.insertDocumentToURL(hamlet+"/hamlet_nodoctype.xml",
                 "xmldb:exist://"+TestTools.VALIDATION_HOME+"/hamlet_nodoctype.xml");
             TestTools.insertDocumentToURL(hamlet+"/hamlet_wrongdoctype.xml",
                 "xmldb:exist://"+TestTools.VALIDATION_HOME+"/hamlet_wrongdoctype.xml");
-            
+
         } catch (ExistIOException ex) {
-            
+
             ex.getCause().printStackTrace();
             logger.error(ex.getCause());
             fail(ex.getCause().getMessage());
-            
+
         } catch (Exception ex) {
             ex.printStackTrace();
             logger.error(ex);
             fail(ex.getMessage());
         }
     }
-    
-    
-    public void testShutdown() {
-        System.out.println(this.getName());
+
+
+    @AfterClass
+    public static void shutdown() {
         BrokerPool.stopAll(true);
-        
     }
-    
 }
