a9ca7f78ffaf17fc258fca4026eb290f26ef3000
Evgeny Gazdovsky <gazdovsky@gmail.com>
2009-04-29 06:01:39 +0000

    [feature] Now possible tar/zip resources/collections not only via URI, but also via passed entry element, like <entry name="entrie's path" type="type of resorce">some XML or binary content or empty</entry>. If entrie's name ends with "/" or type is "collection", entrie's content will be ignored and directory entry in archive will be created. if type is "binary" content will be decoded from BASE64_BINARY. In other cases content will be serialize as text or XML fragment. See also description of compression:unzip($a, $b) and compression:untar($a, $b).
    svn path=/trunk/eXist/; revision=8941

--- extensions/modules/src/org/exist/xquery/modules/compression/ZipFunction.java
+++ extensions/modules/src/org/exist/xquery/modules/compression/ZipFunction.java
@@ -22,35 +22,17 @@
 package org.exist.xquery.modules.compression;
 
 import java.io.ByteArrayOutputStream;
-import java.io.InputStream;
 import java.io.IOException;
-import java.util.Iterator;
+import java.io.OutputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipOutputStream;
 
-import org.exist.collections.Collection;
-import org.exist.dom.BinaryDocument;
-import org.exist.dom.DefaultDocumentSet;
-import org.exist.dom.DocumentImpl;
-import org.exist.dom.MutableDocumentSet;
 import org.exist.dom.QName;
-import org.exist.security.PermissionDeniedException;
-import org.exist.storage.lock.Lock;
-import org.exist.storage.serializers.Serializer;
-import org.exist.util.LockException;
-import org.exist.xmldb.XmldbURI;
-import org.exist.xquery.BasicFunction;
 import org.exist.xquery.Cardinality;
 import org.exist.xquery.FunctionSignature;
-import org.exist.xquery.XPathException;
 import org.exist.xquery.XQueryContext;
-import org.exist.xquery.value.AnyURIValue;
-import org.exist.xquery.value.Base64Binary;
-import org.exist.xquery.value.Sequence;
-import org.exist.xquery.value.SequenceIterator;
 import org.exist.xquery.value.SequenceType;
 import org.exist.xquery.value.Type;
-import org.xml.sax.SAXException;
 
 /**
  * Compresses a sequence of resources and/or collections into a Zip file
@@ -58,17 +40,19 @@ import org.xml.sax.SAXException;
  * @author Adam Retter <adam@exist-db.org>
  * @version 1.0
  */
-public class ZipFunction extends BasicFunction {
+public class ZipFunction extends AbstractCompressFunction {
 
 	public final static FunctionSignature signatures[] = {
 			new FunctionSignature(
 					new QName("zip", CompressionModule.NAMESPACE_URI,
 							CompressionModule.PREFIX),
-					"Zip's resources and/or collections. $a is a sequence of URI's, if a URI points to a collection"
+							"Zip's resources and/or collections. $a is a sequence of URI's and/or entries, if a URI points to a collection"
 							+ "then the collection, its resources and sub-collections are zipped recursively. "
+							+ "Entry is a XML fragment that can contain xml or binary content. "
+							+ "More detailed for entry look compression:unzip($a, $b). "
 							+ "$b indicates whether to use the collection hierarchy in the zip file.",
 					new SequenceType[] {
-							new SequenceType(Type.ANY_URI,
+							new SequenceType(Type.ANY_TYPE,
 									Cardinality.ONE_OR_MORE),
 							new SequenceType(Type.BOOLEAN,
 									Cardinality.EXACTLY_ONE) },
@@ -77,12 +61,14 @@ public class ZipFunction extends BasicFunction {
 			new FunctionSignature(
 					new QName("zip", CompressionModule.NAMESPACE_URI,
 							CompressionModule.PREFIX),
-					"Zip's resources and/or collections. $a is a sequence of URI's, if a URI points to a collection"
+							"Zip's resources and/or collections. $a is a sequence of URI's and/or entries, if a URI points to a collection"
 							+ "then the collection, its resources and sub-collections are zipped recursively. "
+							+ "Entry is a XML fragment that can contain xml or binary content. "
+							+ "More detailed for entry look compression:unzip($a, $b). "
 							+ "$b indicates whether to use the collection hierarchy in the zip file."
 							+ "$c is removed from the beginning of each file path.",
 					new SequenceType[] {
-							new SequenceType(Type.ANY_URI,
+							new SequenceType(Type.ANY_TYPE,
 									Cardinality.ONE_OR_MORE),
 							new SequenceType(Type.BOOLEAN,
 									Cardinality.EXACTLY_ONE),
@@ -95,176 +81,20 @@ public class ZipFunction extends BasicFunction {
 		super(context, signature);
 	}
 
-	public Sequence eval(Sequence[] args, Sequence contextSequence)
-			throws XPathException {
-		// are there some uri's to zip?
-		if (args[0].isEmpty()) {
-			return Sequence.EMPTY_SEQUENCE;
-		}
-
-		// use a hierarchy in the zip file?
-		boolean useHierarchy = args[1].effectiveBooleanValue();
-
-		// Get offset
-		String stripOffset = "";
-		if (args.length == 3) {
-			stripOffset = args[2].getStringValue();
-		}
-
-		ByteArrayOutputStream baos = new ByteArrayOutputStream();
-		ZipOutputStream zos = new ZipOutputStream(baos);
-
-		// iterate through the argument sequence
-		for (SequenceIterator i = args[0].iterate(); i.hasNext();) {
-			AnyURIValue uri = (AnyURIValue) i.nextItem();
-			DocumentImpl doc = null;
-			try {
-				// try for a doc
-				doc = context.getBroker().getXMLResource(uri.toXmldbURI(),
-						Lock.READ_LOCK);
-
-				if (doc == null) {
-					// no doc, try for a collection
-					Collection col = context.getBroker().getCollection(
-							uri.toXmldbURI());
-
-					if (col != null) {
-						// got a collection
-						zipCollection(zos, col, useHierarchy, stripOffset);
-					} else {
-						// no doc or collection
-						throw new XPathException(getASTNode(), "Invalid URI: "
-								+ uri.toString());
-					}
-				} else {
-					// got a doc
-					zipResource(zos, doc, useHierarchy, stripOffset);
-				}
-			} catch (PermissionDeniedException pde) {
-				throw new XPathException(getASTNode(), pde.getMessage());
-			} catch (IOException ioe) {
-				throw new XPathException(getASTNode(), ioe.getMessage());
-			} catch (SAXException se) {
-				throw new XPathException(getASTNode(), se.getMessage());
-			} catch (LockException le) {
-				throw new XPathException(getASTNode(), le.getMessage());
-			} finally {
-				if (doc != null) {
-					doc.getUpdateLock().release(Lock.READ_LOCK);
-				}
-			}
-		}
-
-		try {
-			zos.close();
-		} catch (IOException ioe) {
-			throw new XPathException(getASTNode(), ioe.getMessage());
-		}
-
-		return new Base64Binary(baos.toByteArray());
+	protected void closeEntry(Object os) throws IOException {
+		((ZipOutputStream) os).closeEntry();
 	}
 
-	/**
-	 * Adds a document to a Zip
-	 * 
-	 * @param zos
-	 *            The Zip Output Stream to add the document to
-	 * @param doc
-	 *            The document to add to the Zip
-	 * @param useHierarchy
-	 *            Whether to use a folder hierarchy in the Zip file that
-	 *            reflects the collection hierarchy
-	 */
-	private void zipResource(ZipOutputStream zos, DocumentImpl doc,
-			boolean useHierarchy, String stripOffset) throws IOException,
-			SAXException {
-		// create an entry in the Zip for the document
-		ZipEntry entry = null;
-		if (useHierarchy) {
-			String docCollection = doc.getCollection().getURI().toString();
-
-			// remove leading offset
-			if (docCollection.startsWith(stripOffset)) {
-				docCollection = docCollection.substring(stripOffset.length());
-			}
-
-			// remove leading /
-			if (docCollection.startsWith("/")) {
-				docCollection = docCollection.substring(1);
-			}
-
-			XmldbURI collection = XmldbURI.create(docCollection);
-
-			entry = new ZipEntry(collection.append(doc.getFileURI()).toString());
-		} else {
-			entry = new ZipEntry(doc.getFileURI().toString());
-		}
-		zos.putNextEntry(entry);
-
-		// add the document to the Zip
-		if (doc.getResourceType() == DocumentImpl.XML_FILE) {
-			// xml file
-			Serializer serializer = context.getBroker().getSerializer();
-			serializer.setUser(context.getUser());
-			serializer.setProperty("omit-xml-declaration", "no");
-			String strDoc = serializer.serialize(doc);
-			zos.write(strDoc.getBytes());
-		} else if (doc.getResourceType() == DocumentImpl.BINARY_FILE) {
-			// binary file
-			InputStream is = context.getBroker().getBinaryResource(
-					(BinaryDocument) doc);
-                        byte [] data = new byte[16384];
-                        int len;
-                        while ((len=is.read(data))>0) {
-			   zos.write(data,0,len);
-                        }
-                        is.close();
-		}
-
-		// close the entry in the Zip
-		zos.closeEntry();
+	protected Object newEntry(String name) {
+		return new ZipEntry(name);
 	}
 
-	/**
-	 * Adds a Collection and its child collections and resources recursively to
-	 * a Zip
-	 * 
-	 * @param zos
-	 *            The Zip Output Stream to add the document to
-	 * @param col
-	 *            The Collection to add to the Zip
-	 * @param useHierarchy
-	 *            Whether to use a folder hierarchy in the Zip file that
-	 *            reflects the collection hierarchy
-	 */
-	private void zipCollection(ZipOutputStream zos, Collection col,
-			boolean useHierarchy, String stripOffset) throws IOException,
-			SAXException, LockException {
-		// iterate over child documents
-		MutableDocumentSet childDocs = new DefaultDocumentSet();
-		col.getDocuments(context.getBroker(), childDocs, true);
-		for (Iterator itChildDocs = childDocs.getDocumentIterator(); itChildDocs
-				.hasNext();) {
-			DocumentImpl childDoc = (DocumentImpl) itChildDocs.next();
-			childDoc.getUpdateLock().acquire(Lock.READ_LOCK);
-			try {
-				// zip the resource
-				zipResource(zos, childDoc, useHierarchy, stripOffset);
-			} finally {
-				childDoc.getUpdateLock().release(Lock.READ_LOCK);
-			}
-		}
-
-		// iterate over child collections
-		for (Iterator itChildCols = col.collectionIterator(); itChildCols
-				.hasNext();) {
-			// get the child collection
-			XmldbURI childColURI = (XmldbURI) itChildCols.next();
-			Collection childCol = context.getBroker().getCollection(
-					col.getURI().append(childColURI));
-
-			// recurse
-			zipCollection(zos, childCol, useHierarchy, stripOffset);
-		}
+	protected void putEntry(Object os, Object entry) throws IOException {
+		((ZipOutputStream) os).putNextEntry((ZipEntry) entry);
 	}
+
+	protected OutputStream stream(ByteArrayOutputStream baos) {
+		return new ZipOutputStream(baos);
+	}
+
 }
