f3c90a83bb3a2ff75eacbabed97c733fe95e60a2
James Fuller <jim.fuller@webcomposite.com>
2009-06-10 13:06:13 +0000

    [test.antunit] antunit tests now being run via JUNIT
    svn path=/trunk/eXist/; revision=9113
    
    
    Former-commit-id: a5e8ae44cb01f2ff6f49c7f8e3eafabb32a0ebdb

---
+++ test/src/org/exist/ant/AntUnitTestRunner.java
@@ -0,0 +1,82 @@
+package org.exist.ant;
+
+import org.exist.storage.DBBroker;
+import org.exist.xmldb.DatabaseInstanceManager;
+import org.exist.xquery.XPathException;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.fail;
+import org.xmldb.api.DatabaseManager;
+import org.xmldb.api.base.Collection;
+import org.xmldb.api.base.Database;
+import org.xmldb.api.modules.XPathQueryService;
+import org.apache.tools.ant.Project;
+import org.apache.tools.ant.ProjectHelper;
+import org.apache.tools.ant.BuildException;
+import org.apache.tools.ant.DefaultLogger;
+
+import java.io.File;
+
+/**
+ *
+ * @author jimfuller
+ */
+public class AntUnitTestRunner {
+
+    private XPathQueryService service;
+    private Collection root = null;
+    private Database database = null;
+
+    public AntUnitTestRunner() {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+        // initialize driver
+        Class cl = Class.forName("org.exist.xmldb.DatabaseImpl");
+        database = (Database) cl.newInstance();
+        database.setProperty("create-database", "true");
+        DatabaseManager.registerDatabase(database);
+        root = DatabaseManager.getCollection("xmldb:exist://" + DBBroker.ROOT_COLLECTION, "admin", null);
+        service = (XPathQueryService) root.getService("XQueryService", "1.0");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        DatabaseManager.deregisterDatabase(database);
+        DatabaseInstanceManager dim = (DatabaseInstanceManager) root.getService("DatabaseInstanceManager", "1.0");
+        dim.shutdown();
+
+        // clear instance variables
+        service = null;
+        root = null;
+    //System.out.println("tearDown PASSED");
+    }
+
+    @Test
+    public void testAntUnit() throws BuildException {
+
+        File buildFile = new File("test/ant-tasks/build.xml");
+        Project p = new Project();
+        p.setUserProperty("ant.file", buildFile.getAbsolutePath());
+        DefaultLogger consoleLogger = new DefaultLogger();
+        consoleLogger.setErrorPrintStream(System.err);
+        consoleLogger.setOutputPrintStream(System.out);
+        consoleLogger.setMessageOutputLevel(Project.MSG_INFO);
+        p.addBuildListener(consoleLogger);
+        try {
+            p.fireBuildStarted();
+            p.init();
+            ProjectHelper helper = ProjectHelper.getProjectHelper();
+            p.addReference("ant.projectHelper", helper);
+            helper.parse(p, buildFile);
+            p.executeTarget(p.getDefaultTarget());
+            p.fireBuildFinished(null);
+        } catch (BuildException e) {
+            p.fireBuildFinished(e);
+            fail(e.getMessage());            
+        }
+    }
+
+}
