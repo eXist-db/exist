04a3e25cfdecdbc20d94d5ece99088c8940cae47
chaeron <andrzej@chaeron.com>
2009-07-04 19:01:07 +0000

    [ignore] Changed log message to only show when the db driver is registered (rather than when it was already registered).
    This cleans up the ant log output a lot on big jobs.
    
    svn path=/trunk/eXist/; revision=9258

--- src/org/exist/ant/AbstractXMLDBTask.java
+++ src/org/exist/ant/AbstractXMLDBTask.java
@@ -30,90 +30,94 @@ import org.xmldb.api.base.Database;
 
 /**
  * @author wolf
+ * @author andrzej@chaeron.com
  */
 public abstract class AbstractXMLDBTask extends Task
 {
-
-  protected String driver = "org.exist.xmldb.DatabaseImpl";
-  protected String user = "guest";
-  protected String password = "guest";
-  protected String uri = null;
-  protected boolean createDatabase = false;
-  protected String configuration = null;
-  protected boolean failonerror = true;
-
-  /**
+	
+	protected String driver = "org.exist.xmldb.DatabaseImpl";
+	protected String user = "guest";
+	protected String password = "guest";
+	protected String uri = null;
+	protected boolean createDatabase = false;
+	protected String configuration = null;
+	protected boolean failonerror = true;
+	
+	/**
    * @param driver
    */
-  public void setDriver(String driver)
-  {
-    this.driver = driver;
-  }
-
-  /**
+	public void setDriver( String driver )
+	{
+		this.driver = driver;
+	}
+	
+	/**
    * @param password
    */
-  public void setPassword(String password)
-  {
-    this.password = password;
-  }
-
-  /**
+	public void setPassword( String password )
+	{
+		this.password = password;
+	}
+	
+	/**
    * @param user
    */
-  public void setUser(String user)
-  {
-    this.user = user;
-  }
-
-  /**
+	public void setUser(String user)
+	{
+		this.user = user;
+	}
+	
+	/**
    * @param uri
    */
-  public void setUri(String uri)
-  {
-    this.uri = uri;
-  }
-
-  /**
+	public void setUri(String uri)
+	{
+		this.uri = uri;
+	}
+	
+	/**
    * @param create
    */
-  public void setInitdb(boolean create)
-  {
-    this.createDatabase = create;
-  }
-
-  public void setConfiguration(String config) {
-      this.configuration = config;
-  }
-
-  public void setFailonerror(boolean failonerror)
-  {
-    this.failonerror = failonerror;
-  }
-
-  protected void registerDatabase() throws BuildException
-  {
-    try
-    {
-      log("Registering database", Project.MSG_DEBUG);
-      Database dbs[] = DatabaseManager.getDatabases();
-      for (int i = 0; i < dbs.length; i++)
-      {
-        if (dbs[i].acceptsURI(uri))
-        {
-          log("Database driver already registered.");
-          return;
-        }
-      }
-      Class clazz = Class.forName(driver);
-      Database database = (Database) clazz.newInstance();
-      database.setProperty("create-database", createDatabase ? "true" : "false");
-      if (configuration != null)
-        database.setProperty("configuration", configuration);
-      DatabaseManager.registerDatabase(database);
-    } catch (Exception e)
-    {
-      throw new BuildException("failed to initialize XMLDB database driver");
-    }
-  }
+	public void setInitdb(boolean create)
+	{
+		this.createDatabase = create;
+	}
+	
+	public void setConfiguration(String config) {
+		this.configuration = config;
+	}
+	
+	public void setFailonerror(boolean failonerror)
+	{
+		this.failonerror = failonerror;
+	}
+	
+	protected void registerDatabase() throws BuildException
+	{
+		try {
+			log( "Registering database", Project.MSG_DEBUG );
+			Database dbs[] = DatabaseManager.getDatabases();
+			
+			for( int i = 0; i < dbs.length; i++ ) {
+				if( dbs[i].acceptsURI( uri ) ) {
+					return;	
+				}
+			}
+			
+			Class clazz = Class.forName( driver );	
+			Database database = (Database)clazz.newInstance();
+			database.setProperty( "create-database", createDatabase ? "true" : "false" );
+			
+			if( configuration != null ) {
+				database.setProperty( "configuration", configuration );
+			}
+			
+			DatabaseManager.registerDatabase( database );
+			
+			log( "Database driver registered." );
+		} 
+		catch ( Exception e ) {
+			throw( new BuildException( "failed to initialize XMLDB database driver" ) );
+		}
+	}
 }
