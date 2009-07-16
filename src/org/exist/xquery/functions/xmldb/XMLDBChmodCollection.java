6511fb19cbe319d1f013b2f2ebf4ccf6ab9a180c
Leif-Jöran Olsson <ljo@fripost.org>
2009-07-16 21:07:54 +0000

    [documentation] Function documentation changes and adding logging support for the xmldb module. More to come.
    svn path=/trunk/eXist/; revision=9386
    
    
    Former-commit-id: 948026cb5ed9ce82f900d459cb6f976ce49ad120

--- src/org/exist/xquery/functions/xmldb/XMLDBChmodCollection.java
+++ src/org/exist/xquery/functions/xmldb/XMLDBChmodCollection.java
@@ -1,29 +1,27 @@
 /*
- *  eXist Open Source Native XML Database
- *  Copyright (C) 2001-06 Wolfgang M. Meier
- *  wolfgang@exist-db.org
- *  http://exist.sourceforge.net
+ * eXist Open Source Native XML Database
+ * Copyright (C) 2001-2009 The eXist Project
+ * http://exist-db.org
  *
- *  Modifications Copyright (C) 2004 Luigi P. Bai
- *  finder@users.sf.net
- *  Licensed as below under the LGPL.
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *  
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public License
- *  as published by the Free Software Foundation; either version 2
- *  of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *  
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *  
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *  
+ *  $Id$
  */
 package org.exist.xquery.functions.xmldb;
+
+import org.apache.log4j.Logger;
 
 import org.exist.dom.QName;
 import org.exist.xmldb.UserManagementService;
@@ -31,6 +29,7 @@ import org.exist.xquery.Cardinality;
 import org.exist.xquery.FunctionSignature;
 import org.exist.xquery.XPathException;
 import org.exist.xquery.XQueryContext;
+import org.exist.xquery.value.FunctionParameterSequenceType;
 import org.exist.xquery.value.IntegerValue;
 import org.exist.xquery.value.Sequence;
 import org.exist.xquery.value.SequenceType;
@@ -38,15 +37,23 @@ import org.exist.xquery.value.Type;
 import org.xmldb.api.base.Collection;
 import org.xmldb.api.base.XMLDBException;
 
-public class XMLDBChmodCollection extends XMLDBAbstractCollectionManipulator
-{
+/**
+ * Implements eXist's xmldb:chmod-collection() function.
+ * 
+ * @author wolf
+ * @author Luigi P. Bai, finder@users.sf.net, 2004
+ *
+ */
+
+public class XMLDBChmodCollection extends XMLDBAbstractCollectionManipulator {
+    private static final Logger logger = Logger.getLogger(XMLDBChmodCollection.class);
 	public final static FunctionSignature signature = new FunctionSignature(
 			new QName("chmod-collection", XMLDBModule.NAMESPACE_URI, XMLDBModule.PREFIX),
-                        "Sets the mode of the specified Collection. $a is the Collection path, $b is the mode (as xs:integer). "+
+                        "Sets the mode of the specified Collection. $collection-path is the collection path, $mode is the mode as xs:integer. "+
                         "PLEASE REMEMBER that 0755 is 7*64+5*8+5, NOT decimal 755.",
 			new SequenceType[] {
-					new SequenceType(Type.STRING, Cardinality.EXACTLY_ONE),
-					new SequenceType(Type.INTEGER, Cardinality.EXACTLY_ONE),
+			    new FunctionParameterSequenceType("collection-path", Type.STRING, Cardinality.EXACTLY_ONE, "the collection path"),
+			    new FunctionParameterSequenceType("mode", Type.INTEGER, Cardinality.EXACTLY_ONE, "the mode as xs:integer"),
 			},
             new SequenceType(Type.ITEM, Cardinality.EMPTY)
 	);
@@ -57,23 +64,23 @@ public class XMLDBChmodCollection extends XMLDBAbstractCollectionManipulator
 		super(context, signature);
 	}
 	
-	/* (non-Javadoc)
-	 * @see org.exist.xquery.BasicFunction#eval(org.exist.xquery.value.Sequence[], org.exist.xquery.value.Sequence)
-	 *
-	 */
-	public Sequence evalWithCollection(Collection collection, Sequence[] args, Sequence contextSequence) throws XPathException
-	{
-        try
-        {
+    /* (non-Javadoc)
+     * @see org.exist.xquery.BasicFunction#eval(org.exist.xquery.value.Sequence[], org.exist.xquery.value.Sequence)
+     *
+     */
+    public Sequence evalWithCollection(Collection collection, Sequence[] args, Sequence contextSequence) 
+	throws XPathException {
+	logger.info("Entering " + XMLDBModule.PREFIX + ":" + getName().getLocalName());
+	try {
             UserManagementService ums = (UserManagementService) collection.getService("UserManagementService", "1.0");
             ums.chmod(((IntegerValue) args[1].convertTo(Type.INTEGER)).getInt());
-        }
-        catch(XMLDBException xe)
-        {
-            throw new XPathException(this, "Unable to change collection mode", xe);
-        }
-
-		return Sequence.EMPTY_SEQUENCE;
+        } catch (XMLDBException xe) {
+	    logger.error("Unable to change collection mode", xe);
+	    logger.info("Exiting " + XMLDBModule.PREFIX + ":" + getName().getLocalName());
+	    throw new XPathException(this, "Unable to change collection mode", xe);
 	}
+	logger.info("Exiting " + XMLDBModule.PREFIX + ":" + getName().getLocalName());	
+	return Sequence.EMPTY_SEQUENCE;
+    }
 
 }
