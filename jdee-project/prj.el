e44787c1e8f5436acfdf5a1a1ee4ae3f7ff63dd2
Leif-Jöran Olsson <ljo@fripost.org>
2009-07-21 00:35:35 +0000

    [ignore] jde project settings update.
    svn path=/trunk/eXist/; revision=9431
    
    
    Former-commit-id: f6368e7e2cb430c983d351fc5215635ab1108ed4

--- jdee-project/prj.el
+++ jdee-project/prj.el
@@ -1,3 +1,4 @@
+;; $Id$
 (jde-project-file-version "1.0")
 (jde-set-variables
  '(jde-project-name "eXist Free Software Native XML Database")
@@ -7,8 +8,14 @@
  '(jde-javadoc-author-tag-template "\"* @author \" user-full-name \" <\" user-mail-address \"> \"")
  '(jde-javadoc-describe-field-template "\"* The templates \" (jde-javadoc-field-type modifiers)
  \" \" (jde-javadoc-code name) ")
- '(jde-sourcepath (quote ("./src" "./extensions/modules/src" "./extensions/indexes/ngram/src" "./extensions/indexes/spatial/src" "./extensions/indexes/concordance/src" "./test/src" "./extensions/indexes/test/src" "./tools/wrapper/src" "./tools/requestlog/src" "./tools/ircbot/src")))
+ '(jde-sourcepath (quote ("./src" "./test/src" "./tools/XFormsFilter/src" "./tools/ircbot/src" "./tools/izpack/src" "./tools/requestlog/src" "./tools/wrapper/src" "./samples/src" "./thirdparty/jboss/src" "./extensions/xprocxq/main/src" "./extensions/modules/src" "./extensions/fluent/src" "./extensions/fluent/test/src" "./extensions/indexes/ngram/src" "./extensions/indexes/ngram/test/src" "./extensions/indexes/spatial/src" "./extensions/indexes/spatial/test/src" "./extensions/indexes/lucene/src" "./extensions/indexes/lucene/test/src" "./extensions/versioning/src")))
  '(jde-run-application-class "org.exist.start.Main.class")
- '(jde-built-class-path (quote ("./classes" "./extensions/modules/classes" "./extensions/indexes/classes" "./test/classes" "./extensions/indexes/test/classes" "./tools/wrapper/classes" "./tools/requestlog/classes" "./tools/ircbot/classes")))
+ '(jde-built-class-path (quote ("./build/classes" "./test/classes" "./tools/wrapper/classes" "./webapp/WEB-INF/classes" "./extensions/modules/classes" "./extensions/fluent/test/classes" "./extensions/fluent/classes" "./extensions/indexes/ngram/test/classes" "./extensions/indexes/ngram/classes" "./extensions/indexes/spatial/test/classes" "./extensions/indexes/spatial/classes" "./extensions/indexes/lucene/test/classes" "./extensions/indexes/lucene/classes" "./extensions/versioning/classes" "./tools/requestlog/classes" "./tools/ircbot/classes")))
  '(jde-xref-store-prefixes (quote ("org.exist" "org.apache" "org.xmldb")))
- '(jde-javadoc-describe-class-template "\"* The class \" (jde-javadoc-code name) "))
+ '(jde-javadoc-describe-class-template "\"* The class \" (jde-javadoc-code name) ")
+ '(jde-db-option-connect-socket (quote ("localhost" "4444")))
+ '(jde-enable-abbrev-mode t)
+ '(jde-findbugs-directory "~/bin/findbugs-1.1.1")
+ '(jde-jdk-registry (quote (("1.6.0" . "/opt/java/"))))
+ '(jswat-path "~/bin/jswat-3.16")
+)
