5d637ba1b796f99232311f764ca6279d3a929818
Dannes Wessels <dannes@exist-db.org>
2009-07-28 14:38:07 +0000

    [ignore] added/copied additional tests
    svn path=/trunk/eXist/; revision=9555
    
    
    Former-commit-id: cf25e2b63b18f897f4c2f72194d91a4b8f291094

--- test/src/org/exist/xquery/functions/validate/ValidateJingTest.java
+++ test/src/org/exist/xquery/functions/validate/ValidateJingTest.java
@@ -90,7 +90,42 @@ public class ValidateJingTest {
 
     }
 
+    @Test
+    public void testValidateXSDwithJing_invalid() throws XPathException {
 
+        ResourceSet result = null;
+        String r = "";
+        try {
+            String query = "let $v := <doc>\n" +
+                    "\t<title1>Title</title1>\n" +
+                    "\t<p>Some paragraph.</p>\n" +
+                    "      </doc>\n" +
+                    "let $schema := <xs:schema xmlns:xs=\"http://www.w3.org/2001/XMLSchema\"\n" +
+                    "\t\t elementFormDefault=\"qualified\">\n" +
+                    "\t<xs:element name=\"doc\">\n" +
+                    "\t  <xs:complexType>\n" +
+                    "\t    <xs:sequence>\n" +
+                    "\t      <xs:element minOccurs=\"0\" ref=\"title\"/>\n" +
+                    "\t      <xs:element minOccurs=\"0\" maxOccurs=\"unbounded\" ref=\"p\"/>\n" +
+                    "\t    </xs:sequence>\n" +
+                    "\t  </xs:complexType>\n" +
+                    "\t</xs:element>\n" +
+                    "\t<xs:element name=\"title\" type=\"xs:string\"/>\n" +
+                    "\t<xs:element name=\"p\" type=\"xs:string\"/>\n" +
+                    "      </xs:schema>\n" +
+                    "return\n" +
+                    "\n" +
+                    "\tvalidation:jing($v,$schema)";
+
+            result = service.query(query);
+            r = (String) result.getResource(0).getContent();
+            assertEquals("false", r);
+        } catch (XMLDBException e) {
+            System.out.println("testValidateXSDwithJing(): " + e);
+            fail(e.getMessage());
+        }
+
+    }
 
     @Test
     public void testValidateRNGwithJing() throws XPathException {
@@ -142,5 +177,53 @@ public class ValidateJingTest {
 
     }
 
+    @Test
+    public void testValidateRNGwithJing_invalid() throws XPathException {
 
+        ResourceSet result = null;
+        String r = "";
+        try {
+            String query = "let $v := <doc>\n" +
+                    "\t<title1>Title</title1>\n" +
+                    "\t<p>Some paragraph.</p>\n" +
+                    "      </doc>\n" +
+                    "let $schema := <grammar xmlns=\"http://relaxng.org/ns/structure/1.0\">\n" +
+                    "  <start>\n" +
+                    "    <ref name=\"doc\"/>\n" +
+                    "  </start>\n" +
+                    "  <define name=\"doc\">\n" +
+                    "    <element name=\"doc\">\n" +
+                    "      <optional>\n" +
+                    "        <ref name=\"title\"/>\n" +
+                    "      </optional>\n" +
+                    "      <zeroOrMore>\n" +
+                    "        <ref name=\"p\"/>\n" +
+                    "      </zeroOrMore>\n" +
+                    "    </element>\n" +
+                    "  </define>\n" +
+                    "  <define name=\"title\">\n" +
+                    "    <element name=\"title\">\n" +
+                    "      <text/>\n" +
+                    "    </element>\n" +
+                    "  </define>\n" +
+                    "  <define name=\"p\">\n" +
+                    "    <element name=\"p\">\n" +
+                    "      <text/>\n" +
+                    "    </element>\n" +
+                    "  </define>\n" +
+                    "</grammar>\n" +
+
+                    "return\n" +
+                    "\n" +
+                    "\tvalidation:jing($v,$schema)";
+
+            result = service.query(query);
+            r = (String) result.getResource(0).getContent();
+            assertEquals("false", r);
+        } catch (XMLDBException e) {
+            System.out.println("testValidateRNGwithJing(): " + e);
+            fail(e.getMessage());
+        }
+
+    }
 }
