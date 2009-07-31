11eb39fcee888eb7d14b9c6392c952550e889a2a
Leif-Jöran Olsson <ljo@fripost.org>
2009-07-31 23:43:05 +0000

    [documentation] enhancing function documentation
    svn path=/trunk/eXist/; revision=9611
    
    
    Former-commit-id: b8595f9b71cb8be3598f854dd67891b40d8abc4c

--- src/org/exist/xquery/functions/FunEndsWith.java
+++ src/org/exist/xquery/functions/FunEndsWith.java
@@ -1,23 +1,24 @@
-/* eXist Native XML Database
- * Copyright (C) 2000-2006, The eXist team
+/*
+ * eXist Open Source Native XML Database
+ * Copyright (C) 2001-2009 The eXist Project
+ * http://exist-db.org
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public License
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
+ *  
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Library General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software Foundation,
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * GNU Lesser General Public License for more details.
  * 
- * $Id$
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *  
+ *  $Id$
  */
-
 package org.exist.xquery.functions;
 
 import java.text.Collator;
@@ -32,6 +33,8 @@ import org.exist.xquery.Profiler;
 import org.exist.xquery.XPathException;
 import org.exist.xquery.XQueryContext;
 import org.exist.xquery.value.BooleanValue;
+import org.exist.xquery.value.FunctionReturnSequenceType;
+import org.exist.xquery.value.FunctionParameterSequenceType;
 import org.exist.xquery.value.Item;
 import org.exist.xquery.value.Sequence;
 import org.exist.xquery.value.SequenceType;
@@ -43,24 +46,26 @@ public class FunEndsWith extends CollatingFunction {
 	new FunctionSignature(
 			      new QName("ends-with", Function.BUILTIN_FUNCTION_NS),
 
-			      "Returns true if the string value of $b is a suffix of the " +
-			      "string value of $a, false otherwise. If either $a or $b is the empty " +
-			      "sequence, the empty sequence is returned.",			new SequenceType[] {
-				  new SequenceType(Type.STRING, Cardinality.ZERO_OR_ONE),
-				  new SequenceType(Type.STRING, Cardinality.ZERO_OR_ONE)},
-			      new SequenceType(Type.BOOLEAN, Cardinality.ONE)),
+			      "Returns true if the string value of $suffix is a suffix of the " +
+			      "string value of $source-string, false otherwise. If either $source-string or $suffix is the empty " +
+			      "sequence, the empty sequence is returned.",
+                  new SequenceType[] {
+                      new FunctionParameterSequenceType("source-string", Type.STRING, Cardinality.ZERO_OR_ONE, "the source-string"),
+                      new FunctionParameterSequenceType("suffix", Type.STRING, Cardinality.ZERO_OR_ONE, "the suffix")
+                  },
+			      new FunctionReturnSequenceType(Type.BOOLEAN, Cardinality.ONE, "true() if $suffix is suffix of $source-string, false() otherwise")),
 	new FunctionSignature (
 			       new QName("ends-with", Function.BUILTIN_FUNCTION_NS),
-			       "Returns true if the string value of $b is a suffix of the " +
-			       "string value of $a using collation $c, " + " false otherwise. If " +
-			       "either $a or $b is the empty sequence, the empty sequence" +
-			       " is returned.",
+			       "Returns true if the string value of $suffix is a suffix of the " +
+			       "string value of $source-string using collation $collation-uri, " + " false otherwise. If " +
+			       "either $source-string or $suffix is the empty sequence, the empty sequence" +
+			       " is returned." + THIRD_REL_COLLATION_ARG_EXAMPLE,
 			       new SequenceType[] {
-				   new SequenceType(Type.STRING, Cardinality.ZERO_OR_ONE),
-				   new SequenceType(Type.STRING, Cardinality.ZERO_OR_ONE),
-				   new SequenceType(Type.STRING, Cardinality.EXACTLY_ONE)
+                       new FunctionParameterSequenceType("source-string", Type.STRING, Cardinality.ZERO_OR_ONE, "the source-string"),
+                       new FunctionParameterSequenceType("suffix", Type.STRING, Cardinality.ZERO_OR_ONE, "the suffix"),
+				   new FunctionParameterSequenceType("collation-uri", Type.STRING, Cardinality.EXACTLY_ONE, "the collation-uri")
 			       },
-			       new SequenceType(Type.BOOLEAN, Cardinality.ZERO_OR_ONE))			
+			       new FunctionReturnSequenceType(Type.BOOLEAN, Cardinality.ZERO_OR_ONE, "true() if $suffix is suffix of $source-string, false() otherwise"))			
     };
 
     public FunEndsWith(XQueryContext context, FunctionSignature signature) {
