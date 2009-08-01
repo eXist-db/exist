0bb62775e27457563730117304fdd1c92e3d002f
Leif-Jöran Olsson <ljo@fripost.org>
2009-08-01 00:32:28 +0000

    [documentation] enhancing function documentation
    svn path=/trunk/eXist/; revision=9613
    
    
    Former-commit-id: 8600785854fda7c7e254a78f420f7dc9fa39acc2

--- src/org/exist/xquery/functions/FunEscapeURI.java
+++ src/org/exist/xquery/functions/FunEscapeURI.java
@@ -1,21 +1,21 @@
 /*
- *  eXist Open Source Native XML Database
- *  Copyright (C) 2001-04 The eXist Project
- *  http://exist-db.org
+ * eXist Open Source Native XML Database
+ * Copyright (C) 2001-2009 The eXist Project
+ * http://exist-db.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
  *  
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public License
- *  as published by the Free Software Foundation; either version 2
- *  of the License, or (at your option) any later version.
- *  
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *  
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  *  
  *  $Id$
  */
@@ -31,6 +31,8 @@ import org.exist.xquery.FunctionSignature;
 import org.exist.xquery.Profiler;
 import org.exist.xquery.XPathException;
 import org.exist.xquery.XQueryContext;
+import org.exist.xquery.value.FunctionReturnSequenceType;
+import org.exist.xquery.value.FunctionParameterSequenceType;
 import org.exist.xquery.value.Sequence;
 import org.exist.xquery.value.SequenceType;
 import org.exist.xquery.value.StringValue;
@@ -47,18 +49,18 @@ public class FunEscapeURI extends BasicFunction {
             new QName("escape-uri", Function.BUILTIN_FUNCTION_NS),
             "This function applies the URI escaping rules defined in section 2 " +
             "of [RFC 2396] as amended by [RFC 2732], with one exception, to " +
-            "the string supplied as $a, which typically represents all or part " +
+            "the string supplied as $uri, which typically represents all or part " +
             "of a URI. The effect of the function is to escape a set of identified " +
             "characters in the string. Each such character is replaced in the string " +
             "by an escape sequence, which is formed by encoding the character " +
             "as a sequence of octets in UTF-8, and then representing each of these " +
             "octets in the form %HH, where HH is the hexadecimal representation " +
-            "of the octet. $b indicates whether to escape reserved characters.",
+            "of the octet. $escape-reserved indicates whether to escape reserved characters.",
             new SequenceType[] {
-                 new SequenceType(Type.STRING, Cardinality.ZERO_OR_ONE),
-                 new SequenceType(Type.BOOLEAN, Cardinality.EXACTLY_ONE)
+                new FunctionParameterSequenceType("uri", Type.STRING, Cardinality.ZERO_OR_ONE, "the uri"),
+                 new FunctionParameterSequenceType("escape-reserved", Type.BOOLEAN, Cardinality.EXACTLY_ONE, "the escaped-reserved")
             },
-            new SequenceType(Type.STRING, Cardinality.EXACTLY_ONE));
+            new FunctionReturnSequenceType(Type.STRING, Cardinality.EXACTLY_ONE, "identified characters in $uri encoded with escape sequences"));
     
     public FunEscapeURI(XQueryContext context) {
         super(context, signature);
