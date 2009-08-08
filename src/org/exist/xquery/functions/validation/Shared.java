189c9f13e225850dc1aebe33668fe55287aa3666
Dannes Wessels <dannes@exist-db.org>
2009-08-08 09:30:26 +0000

    [ignore] fixed compile error
    svn path=/trunk/eXist/; revision=9699
    
    
    Former-commit-id: c870873de404bcd086738ae70acdf9807484481f

--- src/org/exist/xquery/functions/validation/Shared.java
+++ src/org/exist/xquery/functions/validation/Shared.java
@@ -21,7 +21,6 @@
  */
 package org.exist.xquery.functions.validation;
 
-import com.sun.xml.internal.messaging.saaj.util.ByteInputStream;
 import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.FileInputStream;
@@ -155,7 +154,7 @@ public class Shared {
             Serializer serializer = context.getBroker().newSerializer();
 
             NodeValue node = (NodeValue) s;
-            InputStream is = new NodeInputStream(serializer, node); // new NodeInputStream()
+            InputStream is = new NodeInputStream(serializer, node); 
             streamSource.setInputStream(is);
 
         } else if (s.getType() == Type.BASE64_BINARY) {
