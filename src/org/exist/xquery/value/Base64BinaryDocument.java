0e135789ce96a9299b53aa0b53713a7efe4657bc
Dannes Wessels <dannes@exist-db.org>
2009-08-08 11:01:49 +0000

    [ingore] now rnc's (relaxng compact) can be used with jing. base64binaryDocument name and location under discussion
    svn path=/trunk/eXist/; revision=9700

---
+++ src/org/exist/xquery/value/Base64BinaryDocument.java
@@ -0,0 +1,49 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2009 The eXist Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * $Id$
+ */
+package org.exist.xquery.value;
+
+import org.exist.xquery.XPathException;
+
+/**
+ * Wrapper around Base64Binary.
+ * @author dizzzzz
+ */
+public class Base64BinaryDocument extends Base64Binary {
+
+    private String url = null;
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+
+    public Base64BinaryDocument(byte[] data) {
+        super(data);
+    }
+
+    public Base64BinaryDocument(String str) throws XPathException {
+    	super(str);
+    }
+}
