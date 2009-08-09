f8d80dcd47c7b504e6325e5b79cf72e09ea405cf
Loren Cahlander <loren.cahlander@gmail.com>
2009-08-09 22:18:07 +0000

    [documentation] final edits to the function docs.
    svn path=/trunk/eXist/; revision=9748

--- extensions/indexes/ngram/src/org/exist/xquery/modules/ngram/HighlightMatches.java
+++ extensions/indexes/ngram/src/org/exist/xquery/modules/ngram/HighlightMatches.java
@@ -1,3 +1,24 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2007-09 The eXist Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ *  $Id$
+ */
 package org.exist.xquery.modules.ngram;
 
 import org.exist.dom.NodeProxy;
@@ -46,8 +67,8 @@ public class HighlightMatches extends BasicFunction {
             "Note: a ngram match on mixed content may span multiple nodes. In this case, the callback function is called " +
             "once for every text node which is part of the matching text sequence.",
             new SequenceType[] {
-                    new FunctionParameterSequenceType("nodes", Type.NODE, Cardinality.ZERO_OR_MORE, "a sequence of nodes"),
-                    new FunctionParameterSequenceType("function-reference", Type.FUNCTION_REFERENCE, Cardinality.EXACTLY_ONE, "a callback function")
+                    new FunctionParameterSequenceType("nodes", Type.NODE, Cardinality.ZERO_OR_MORE, "The sequence of nodes"),
+                    new FunctionParameterSequenceType("function-reference", Type.FUNCTION_REFERENCE, Cardinality.EXACTLY_ONE, "The callback function")
             },
             new FunctionReturnSequenceType(Type.NODE, Cardinality.ZERO_OR_MORE, "a resulting node set"));
 
