bc625288dae987e7570b616f0e40590f5bed1955
Dmitriy Shabanov <shabanovd@gmail.com>
2009-09-15 13:45:30 +0000

    [ignore] Debuggee joint draft
    svn path=/trunk/eXist/; revision=10038
    
    
    Former-commit-id: 98b7c182d453b07c87a4fa3f3c22f93962d68cc7

---
+++ src/org/exist/debuggee/DebuggeeJoint.java
@@ -0,0 +1,38 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2009 The eXist Project
+ *  http://exist-db.org
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  
+ *  $Id:$
+ */
+package org.exist.debuggee;
+
+import org.exist.xquery.Expression;
+import org.exist.xquery.XQueryContext;
+
+/**
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ *
+ */
+public interface DebuggeeJoint {
+	
+	public XQueryContext getContext();
+	
+	public void expressionStart(Expression expr);
+	public void expressionEnd(Expression expr);
+
+}
