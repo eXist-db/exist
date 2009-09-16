af6bc1a6c93e709068abc0e75f8ba45e9bc066ab
Dmitriy Shabanov <shabanovd@gmail.com>
2009-09-16 13:29:31 +0000

    [ignore] debuggee establish TCP connection, next will be hand shaking
    svn path=/trunk/eXist/; revision=10052
    
    
    Former-commit-id: ff4fc675716a350450d530d44680512ab7e008db

--- extensions/debuggee/src/org/exist/debuggee/DebuggeeConnectionTCP.java
+++ extensions/debuggee/src/org/exist/debuggee/DebuggeeConnectionTCP.java
@@ -23,7 +23,9 @@ package org.exist.debuggee;
 
 import java.net.InetSocketAddress;
 
+import org.apache.mina.core.RuntimeIoException;
 import org.apache.mina.core.future.ConnectFuture;
+import org.apache.mina.core.session.IoSession;
 import org.apache.mina.transport.socket.nio.NioSocketConnector;
 import org.exist.debuggee.dgbp.DGBPProtocolHandler;
 
@@ -31,39 +33,70 @@ import org.exist.debuggee.dgbp.DGBPProtocolHandler;
  * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
  *
  */
-public class DebuggeeConnectionTCP implements DebuggeeConnection, Runnable {
+public class DebuggeeConnectionTCP extends Thread implements DebuggeeConnection, Runnable {
 	
 	private String host = "127.0.0.1";
 	private int port = 9000;
 	
-	private NioSocketConnector connector = null;
-	private ConnectFuture cf = null;
+	private NioSocketConnector connector;
+	private IoSession session;
+	
+	private int status = 0;
+	private Object lock = new Object();
 	
 	public DebuggeeConnectionTCP() {
-		Thread thread = new Thread(this);
-		thread.start();
-	}
-	
-	public void run() {
 		// Create TCP/IP connector.
 		connector = new NioSocketConnector();
 		
-		// Set connect timeout for 5 seconds.
-		connector.setConnectTimeoutMillis(5*1000L);
+		// Set connect timeout for 30 seconds.
+		connector.setConnectTimeoutMillis(30*1000L);
 
 		// Start communication.
 		connector.setHandler(new DGBPProtocolHandler());
-		cf = connector.connect(new InetSocketAddress(host, port));
+	}
+	
+	public boolean connect() {
+		synchronized (lock) {
+			if (status == 2)
+				return true;
+				
+			try {
+				ConnectFuture future = connector.connect(new InetSocketAddress(host, port));
+				future.awaitUninterruptibly();
+				session = future.getSession();
+			} catch (RuntimeIoException e) {
+				System.err.println("Failed to connect.");
+				status = 0;
+				return false;
+			}
 
-		// Wait for the connection attempt to be finished.
-		cf.awaitUninterruptibly();
-		cf.getSession().getCloseFuture().awaitUninterruptibly();
+			System.out.println("connected");
+			status = 1;
+			
+			start();
+			return true;
+		}
+	}
+	
+	public void run() {
+		try {
 		
-		connector.dispose();
+			// wait until the summation is done
+			session.getCloseFuture().awaitUninterruptibly();
+
+			connector.dispose();
+
+			System.out.println("disconnected");
+
+		} finally {
+			synchronized (lock) {
+				status = 0; 
+			}
+		}
 	}
 
 	public boolean isConnected() {
 		//XXX: resolve concurrency problem here!!!
-		return (cf != null);
+		return (status == 2);
 	}
 }
