e2af67a69d1a938f3caefd92beee1a0a24bd6b5e
Dmitriy Shabanov <shabanovd@gmail.com>
2009-09-26 05:47:26 +0000

    [ignore] adding debugger site decoder & encode
    svn path=/trunk/eXist/; revision=10133
    
    
    Former-commit-id: f8959fe83c1b76075935761ac57d8a71e5a0193f

---
+++ extensions/debuggee/src/org/exist/debugger/dgbp/DGBPRequestDecoder.java
@@ -0,0 +1,65 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2009 The eXist Project
+ *  http://exist-db.org
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  
+ *  $Id:$
+ */
+package org.exist.debugger.dgbp;
+
+import org.apache.mina.core.buffer.IoBuffer;
+import org.apache.mina.core.session.IoSession;
+import org.apache.mina.filter.codec.CumulativeProtocolDecoder;
+import org.apache.mina.filter.codec.ProtocolDecoderOutput;
+import org.exist.debuggee.dgbp.packets.Command;
+
+/**
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ *
+ */
+public class DGBPRequestDecoder extends CumulativeProtocolDecoder {
+
+	private String sLength = "";
+	private Integer length;
+	
+	@Override
+	protected boolean doDecode(IoSession session, IoBuffer in,
+			ProtocolDecoderOutput out) throws Exception {
+		
+		byte b;
+		while (true) {
+			if (length == null)
+				if (in.remaining() > 0) {
+					b = in.get();
+					if (b == (byte)0) {
+						try {
+							length = Integer.valueOf(sLength);
+						} finally {
+						}
+						continue;
+					}
+					sLength += (char)b; 
+				} else {
+					return false;
+				}
+			
+			if (in.remaining() >= length) {
+				//TODO: parsing response
+			}
+		}
+	}
+}
