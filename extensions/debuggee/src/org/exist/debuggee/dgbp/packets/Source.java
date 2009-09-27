bfe5726fffa24e23b2a345ed3a915e0422a60b14
Dmitriy Shabanov <shabanovd@gmail.com>
2009-09-27 09:13:10 +0000

    [ignore] adding support REST's URI for source command. Response is correct, but emacs don't want to show the source :(
    svn path=/trunk/eXist/; revision=10150

--- extensions/debuggee/src/org/exist/debuggee/dgbp/packets/Source.java
+++ extensions/debuggee/src/org/exist/debuggee/dgbp/packets/Source.java
@@ -23,12 +23,22 @@
 package org.exist.debuggee.dgbp.packets;
 
 import org.apache.mina.core.session.IoSession;
+import org.exist.EXistException;
 import org.exist.debuggee.dgbp.Errors;
+import org.exist.dom.BinaryDocument;
+import org.exist.dom.DocumentImpl;
+import org.exist.security.PermissionDeniedException;
+import org.exist.security.User;
+import org.exist.storage.BrokerPool;
+import org.exist.storage.DBBroker;
+import org.exist.storage.lock.Lock;
 import org.exist.util.Base64Encoder;
+import org.exist.xmldb.XmldbURI;
 
 import java.net.URL;
 import java.net.MalformedURLException;
 import java.net.URLConnection;
+import java.net.URLDecoder;
 import java.io.InputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -43,11 +53,13 @@ public class Source extends Command {
 	/**
 	 * file URI 
 	 */
-	String fileURI;
+	private String fileURI;
 
-    boolean success = false;
+	private boolean success = false;
+    private Exception exception = null;
 
-    byte[] source;
+    private byte[] source;
+    private byte[] response = null;
     
     public Source(IoSession session, String args) {
         super(session, args);
@@ -63,53 +75,87 @@ public class Source extends Command {
 
     @Override
     public void exec() {
+    	if (fileURI == null)
+    		return;
+    	
+    	InputStream is = null;
         try {
-        	//TODO: rewrite to handle REST source url (dbgp:database or something)
-            URL url = new URL(fileURI);
-            URLConnection conn = url.openConnection();
-            InputStream is = conn.getInputStream();
-            ByteArrayOutputStream baos = new ByteArrayOutputStream();
-            byte[] buf = new byte[256];
-            int c;
-            while ((c = is.read(buf)) > -1) {
-                baos.write(buf, 0, c);
-            }
-            source = baos.toByteArray();
-            
-            success = true;
-        
+        	
+        	if (fileURI.toLowerCase().startsWith("dbgp:database")) {
+        		XmldbURI pathUri = XmldbURI.create( URLDecoder.decode( fileURI.substring(16) , "UTF-8" ) );
+
+        		//TODO: authorization ??? add command to DBGP?
+        		DBBroker broker = BrokerPool.getInstance().get(User.DEFAULT);
+    			DocumentImpl resource = broker.getXMLResource(pathUri, Lock.READ_LOCK);
+
+    			if (resource.getResourceType() == DocumentImpl.BINARY_FILE) {
+    				is = broker.getBinaryResource((BinaryDocument) resource);
+    			} else {
+    				//TODO: xml source???
+    				return;
+    			}
+
+        	} else {
+        		URL url = new URL(fileURI);
+        		URLConnection conn = url.openConnection();
+        		is = conn.getInputStream();
+        	}
+    		ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    		byte[] buf = new byte[256];
+    		int c;
+    		while ((c = is.read(buf)) > -1) {
+    			baos.write(buf, 0, c);
+    		}
+    		
+    		source = baos.toByteArray();
+    		success = true;
+
         } catch (MalformedURLException e) {
-            e.printStackTrace();
+            exception = e;
         } catch (IOException e) {
-            e.printStackTrace();
-        }
+            exception = e;
+        } catch (PermissionDeniedException e) {
+            exception = e;
+		} catch (EXistException e) {
+            exception = e;
+        } finally {
+        	if (is != null)
+				try {
+					is.close();
+				} catch (IOException e) {
+					if (exception == null)
+						exception = e;
+				}
+		}
     }
 
     @Override
     public byte[] toBytes() {
-        if (source != null) {
-            try {
-                String head = "<response " +
-                    "command=\"source\" " +
-                    "success=\""+getSuccessString()+"\" " +
-                    "transaction_id=\""+transactionID+"\">";
-                String tail = "</response>";
+    	if (response == null) {
+    		if (source != null) {
+    			try {
+    				String head = "<response " +
+    					"command=\"source\" " +
+                    	"success=\""+getSuccessString()+"\" " +
+                    	"transaction_id=\""+transactionID+"\">";
+    				String tail = "</response>";
 
-                Base64Encoder enc = new Base64Encoder();
-        	    enc.translate(source);
+    				Base64Encoder enc = new Base64Encoder();
+    				enc.translate(source);
 
-                ByteArrayOutputStream baos = new ByteArrayOutputStream();
-                baos.write(head.getBytes());
-                baos.write(new String(enc.getCharArray()).getBytes());
-                baos.write(tail.getBytes());
-                return baos.toByteArray();
-            } catch (IOException e) {
-                e.printStackTrace();
-                return errorBytes("source");
-            }
-        } else {
-            return errorBytes("source", Errors.ERR_100, Errors.ERR_100_STR);
-        }
+    				ByteArrayOutputStream baos = new ByteArrayOutputStream();
+    				baos.write(head.getBytes());
+    				baos.write(new String(enc.getCharArray()).getBytes());
+    				baos.write(tail.getBytes());
+    				response = baos.toByteArray();
+    			} catch (IOException e) {
+    				response = errorBytes("source");
+    			}
+    		} else {
+    			response = errorBytes("source", Errors.ERR_100, Errors.ERR_100_STR);
+    		}
+    	}
+    	return response;
     }
     
     private String getSuccessString() {
