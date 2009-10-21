73383a2779433acbef4801e6fe224ae47bb409c8
Dmitriy Shabanov <shabanovd@gmail.com>
2009-10-21 16:43:14 +0000

    [ignore] refactoring & typos
    svn path=/trunk/eXist/; revision=10242

--- extensions/debuggee/src/org/exist/debugger/DebuggerImpl.java
+++ extensions/debuggee/src/org/exist/debugger/DebuggerImpl.java
@@ -21,14 +21,20 @@
  */
 package org.exist.debugger;
 
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.util.HashMap;
 import java.util.Map;
 
+import org.apache.log4j.Logger;
 import org.apache.mina.core.session.IoSession;
-import org.exist.debuggee.dbgp.packets.ResponseImpl;
+import org.apache.mina.filter.codec.ProtocolCodecFilter;
+import org.apache.mina.transport.socket.nio.NioSocketAcceptor;
+import org.exist.debuggee.dbgp.CodecFactory;
 import org.exist.debuggee.dbgp.packets.Source;
 import org.exist.debugger.Debugger;
 import org.exist.debugger.DebuggingSource;
+import org.exist.debugger.dbgp.ProtocolHandler;
 import org.exist.debugger.model.Breakpoint;
 
 /**
@@ -37,23 +43,46 @@ import org.exist.debugger.model.Breakpoint;
  */
 public class DebuggerImpl implements Debugger {
 	
-	private IoSession session;
+    protected final static Logger LOG = Logger.getLogger(DebuggerImpl.class);
+
+    private NioSocketAcceptor acceptor;
+
+    private int eventPort = 9000;
+
+    private IoSession session;
 	
-	//uri, source
+	//uri -> source
 	private Map<String, DebuggingSource> sources = new HashMap<String, DebuggingSource>();
 	
+
 	public DebuggerImpl() {
-		
 	}
 	
 	protected void setSession(IoSession session) {
 		this.session = session;
 	}
 
+	public DebuggingSource init(String url) throws IOException, ExceptionTimeout {
+		acceptor = new NioSocketAcceptor();
+		acceptor.getFilterChain().addLast(
+				"protocol", new ProtocolCodecFilter(new CodecFactory()));
+		acceptor.setHandler(new ProtocolHandler(this));
+		acceptor.bind(new InetSocketAddress(eventPort));
+
+		LOG.info("Debugger is listening at port "+eventPort);
+
+		Thread session = new Thread(new HttpSession(url));
+		session.run();
+		
+		Response response = getResponse("init", 30 * 1000); // 30s timeout
+		
+		return getSource(response.getAttribute("fileuri")); //TODO: fileuri as constant???
+	}
+	
 	/* (non-Javadoc)
 	 * @see org.exist.debugger.Debugger#source(java.lang.String)
 	 */
-	public DebuggingSource source(String fileURI) {
+	public DebuggingSource getSource(String fileURI) {
 		if (sources.containsKey(fileURI))
 			return sources.get(fileURI);
 
@@ -62,8 +91,8 @@ public class DebuggerImpl implements Debugger {
 		Response response = command.toDebuggee();
 		
 		if ("1".equals(response.getAttribute("success"))) {
-			DebuggingSource source = new DebuggingSourceImpl(this, fileURI);
-			source.setData(response.getText());
+			DebuggingSourceImpl source = new DebuggingSourceImpl(this, fileURI);
+			source.setText(response.getText());
 			
 			sources.put(fileURI, source);
 			
@@ -83,28 +112,42 @@ public class DebuggerImpl implements Debugger {
 		
 	}
 	
-	private Map<String, ResponseImpl> responses = new HashMap<String, ResponseImpl>();
+	//weak map???
+	private Map<String, Response> responses = new HashMap<String, Response>();
 	
-	protected synchronized void addResponse(ResponseImpl response) {
+	protected synchronized void addResponse(Response response) {
 		responses.put(response.getTransactionID(), response);
 		notifyAll();
 	}
 
-	public synchronized ResponseImpl getResponse(String transactionID) {
+	public Response getResponse(String transactionID) {
+		try {
+			return getResponse(transactionID, 0);
+		} catch (ExceptionTimeout e) {
+			return null;
+		}
+	}
+	public synchronized Response getResponse(String transactionID, int timeout) throws ExceptionTimeout {
+		long sTime = System.currentTimeMillis();
+		
 		while (!responses.containsKey(transactionID)) {
 			try {
-				wait(30 * 1000); //30s
+				wait(timeout);
+				
+				if (timeout != 0 && (System.currentTimeMillis() - sTime) > timeout)
+					throw new ExceptionTimeout();
 			} catch (InterruptedException e) {
 			} 
 		}
 		
 		if (responses.containsKey(transactionID)) {
-			ResponseImpl response = responses.get(transactionID);
+			Response response = responses.get(transactionID);
 			responses.remove(transactionID);
 			
 			return response;
 		}
 
+		//throw error???
 		return null;
 	}
 
