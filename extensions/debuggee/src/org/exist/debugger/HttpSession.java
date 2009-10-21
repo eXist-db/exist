e6957ad4511faa9485f13493d67ddcb210f40248
Dmitriy Shabanov <shabanovd@gmail.com>
2009-10-21 16:27:25 +0000

    [ignore] HttpSession helpful for initializing debugging session
    svn path=/trunk/eXist/; revision=10239
    
    
    Former-commit-id: 300108c745ea38832dfd4af46cda0f09107dd6c6

---
+++ extensions/debuggee/src/org/exist/debugger/HttpSession.java
@@ -0,0 +1,56 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2009 The eXist Project
+ *  http://exist-db.org
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  
+ *  $Id:$
+ */
+package org.exist.debugger;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.PostMethod;
+
+/**
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ *
+ */
+public class HttpSession implements Runnable {
+	
+	private String url;
+	
+	protected HttpSession(String url) {
+		this.url = url;
+	}
+
+	/* (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	public void run() {
+		HttpClient client = new HttpClient();
+
+		PostMethod method = new PostMethod(url);
+
+		try {
+			System.out.println("sending http request with debugging flag");
+			client.executeMethod(method);
+
+			System.out.println("get http response");
+		} catch (Exception e) {
+		}
+	}
+
+}
