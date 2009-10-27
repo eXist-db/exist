7da35869d384e71257dfb060eafd33f0c5222f5b
Dmitriy Shabanov <shabanovd@gmail.com>
2009-10-27 18:54:51 +0000

    [ignore] interface for interpreter context
    svn path=/trunk/eXist/; revision=10273
    
    
    Former-commit-id: cc9e6e2e1ff26f8158041ace424ebe09b94ada4c

---
+++ extensions/xslt/src/org/exist/interpreter/ContextAtExist.java
@@ -0,0 +1,54 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2008-2009 The eXist Project
+ *  http://exist-db.org
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  
+ *  $Id$
+ */
+
+package org.exist.interpreter;
+
+import org.exist.dom.DocumentImpl;
+import org.exist.storage.DBBroker;
+import org.exist.util.hashtable.NamePool;
+import org.exist.xquery.XPathException;
+import org.exist.xquery.value.AnyURIValue;
+
+/**
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ *
+ */
+public interface ContextAtExist {
+	
+	public DBBroker getBroker();
+	public NamePool getSharedNamePool();
+
+	public String getModuleLoadPath();
+
+	public String getPrefixForURI(String namespaceURI);
+	public void declareInScopeNamespace(String string, String namespaceURI);
+
+	//TODO: change to DocumentAtExist
+	public DocumentImpl storeTemporaryDoc(org.exist.memtree.DocumentImpl doc) throws XPathException;
+
+	public boolean isBaseURIDeclared();
+	public AnyURIValue getBaseURI() throws XPathException;
+	
+//	public TraceListener getTraceListener();
+//	public void getTraceListener(TraceListener listener);
+
+}
