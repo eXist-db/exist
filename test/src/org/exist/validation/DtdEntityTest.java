f61985152800e618ee872ae82c90aee082b441c9
Dannes Wessels <dannes@exist-db.org>
2009-11-18 19:52:06 +0000

    [ignore] added enity load test. did expect to see a fail but....
    svn path=/trunk/eXist/; revision=10518
    
    
    Former-commit-id: b30b973a0387d50d4c17348500488e855ec6300a

---
+++ test/src/org/exist/validation/DtdEntityTest.java
@@ -0,0 +1,60 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2009 The eXist Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * $Id$
+ */
+package org.exist.validation;
+
+import org.exist.test.EmbeddedExistTester;
+import static org.junit.Assert.*;
+import org.junit.*;
+import org.xmldb.api.base.Collection;
+import org.xmldb.api.base.XMLDBException;
+
+/**
+ * Small test to show than entities are required to be resolved.
+ * 
+ * @author wessels
+ */
+public class DtdEntityTest extends EmbeddedExistTester {
+
+    @Test
+    public void loadWithEntities() {
+
+        String input = "<a>first empty: &empty; then trade: &trade; </a>";
+        try {
+            Collection col = createCollection(rootCollection, "entitiy");
+            storeResource(col, "docname.xml", input.getBytes());
+
+            String result = getXMLResource(col, "docname.xml");
+            System.out.println(result);
+            fail("Exception expected");
+
+        } catch (XMLDBException ex) {
+            assertTrue(ex.getMessage()
+                    .contains("The entity \"empty\" was referenced, but not declared"));
+
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+            fail(ex.getMessage());
+        }
+
+    }
+}
