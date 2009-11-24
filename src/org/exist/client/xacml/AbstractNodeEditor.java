e86506b3b0dc540a1151b4676abd26a12e2d9063
Dmitriy Shabanov <shabanovd@gmail.com>
2009-11-24 08:50:08 +0000

    [ignore] tidy up eXist (generics)
    svn path=/trunk/eXist/; revision=10563
    
    
    Former-commit-id: e05274d700d845da1170168cd507764ba8f381f3

--- src/org/exist/client/xacml/AbstractNodeEditor.java
+++ src/org/exist/client/xacml/AbstractNodeEditor.java
@@ -1,29 +1,26 @@
 package org.exist.client.xacml;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 
 import javax.swing.event.ChangeEvent;
 import javax.swing.event.ChangeListener;
 
-public abstract class AbstractNodeEditor implements NodeEditor
-{
-	private List listeners = new ArrayList(2);
+public abstract class AbstractNodeEditor implements NodeEditor {
+	private List<ChangeListener> listeners = new ArrayList<ChangeListener>(2);
 
-	protected void fireChanged()
-	{
+	protected void fireChanged() {
 		ChangeEvent event = new ChangeEvent(this);
-		for(Iterator it = listeners.iterator(); it.hasNext();)
-			((ChangeListener)it.next()).stateChanged(event);
+		for(ChangeListener listener : listeners)
+			listener.stateChanged(event);
 	}
-	public void addChangeListener(ChangeListener listener)
-	{
+	
+	public void addChangeListener(ChangeListener listener) {
 		if(listener != null)
 			listeners.add(listener);
 	}
-	public void removeChangeListener(ChangeListener listener)
-	{
+	
+	public void removeChangeListener(ChangeListener listener) {
 		if(listeners != null)
 			listeners.remove(listeners);
 	}
