276dc3c0ecc9a315cd9b5534ae8de6a1cb19f0cf
Dmitriy Shabanov <shabanovd@gmail.com>
2009-12-26 05:29:45 +0000

    [test] Configurable interface
    svn path=/trunk/eXist/; revision=10880
    
    
    Former-commit-id: e3a0172ad997cf0d03f0da507f728372999a07f4

---
+++ test/src/org/exist/config/ConfigurableObject.java
@@ -0,0 +1,53 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2009 The eXist Project
+ *  http://exist-db.org
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  
+ *  $Id:$
+ */
+package org.exist.config;
+
+import org.exist.config.annotation.ConfigurationClass;
+import org.exist.config.annotation.ConfigurationField;
+
+/**
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ *
+ */
+@ConfigurationClass("instance")
+public class ConfigurableObject implements Configurable {
+	
+	@ConfigurationField("value1")
+	protected String some = "default";
+
+	@ConfigurationField("value2")
+	protected Integer someInteger = 7;
+
+	public ConfigurableObject(ConfigElementImpl config) {
+		Configurater.configure(this, config);
+	}
+
+	/* (non-Javadoc)
+	 * @see org.exist.config.Configurable#isConfigured()
+	 */
+	@Override
+	public boolean isConfigured() {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
---
+++ test/src/org/exist/config/ConfigurableTest.java
@@ -0,0 +1,73 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2009 The eXist Project
+ *  http://exist-db.org
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  
+ *  $Id:$
+ */
+package org.exist.config;
+
+import static org.junit.Assert.*;
+
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.exist.dom.ElementAtExist;
+import org.exist.memtree.SAXAdapter;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.xml.sax.InputSource;
+import org.xml.sax.XMLReader;
+
+
+/**
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ *
+ */
+public class ConfigurableTest {
+
+	String config = "<instance value1=\"a\" value2=\"5\"></instance>";
+	
+	@Test
+	public void simple() throws Exception {
+		InputStream is = new ByteArrayInputStream(config.getBytes("UTF-8"));
+		
+        // initialize xml parser
+        // we use eXist's in-memory DOM implementation to work
+        // around a bug in Xerces
+        SAXParserFactory factory = SAXParserFactory.newInstance();
+        factory.setNamespaceAware(true);
+        InputSource src = new InputSource(is);
+        SAXParser parser = factory.newSAXParser();
+        XMLReader reader = parser.getXMLReader();
+        SAXAdapter adapter = new SAXAdapter();
+        reader.setContentHandler(adapter);
+        reader.parse(src);
+        
+        ConfigElementImpl config = new ConfigElementImpl((ElementAtExist) adapter.getDocument().getDocumentElement());
+        
+        ConfigurableObject object = new ConfigurableObject(config);
+        
+        assertEquals("a", object.some);
+        
+        assertEquals(Integer.valueOf(5), object.someInteger);
+	}
+
+}
