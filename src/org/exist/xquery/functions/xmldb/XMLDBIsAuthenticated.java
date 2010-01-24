38168b2afbd67f2cafffa4869a5493920feb51a9
Dmitriy Shabanov <shabanovd@gmail.com>
2010-01-24 08:15:02 +0000

    [function] xmldb:is-authenticated returns the true() if current user from the xquery context is authenticated, false() otherwise.
    svn path=/trunk/eXist/; revision=11065

---
+++ src/org/exist/xquery/functions/xmldb/XMLDBIsAuthenticated.java
@@ -0,0 +1,62 @@
+/*
+ * eXist Open Source Native XML Database
+ * Copyright (C) 2010 The eXist Project
+ * http://exist-db.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *  
+ *  $Id$
+ */
+package org.exist.xquery.functions.xmldb;
+
+import org.apache.log4j.Logger;
+
+import org.exist.dom.QName;
+import org.exist.xquery.BasicFunction;
+import org.exist.xquery.Cardinality;
+import org.exist.xquery.FunctionSignature;
+import org.exist.xquery.XPathException;
+import org.exist.xquery.XQueryContext;
+import org.exist.xquery.value.BooleanValue;
+import org.exist.xquery.value.FunctionReturnSequenceType;
+import org.exist.xquery.value.Sequence;
+import org.exist.xquery.value.Type;
+
+/**
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ *
+ */
+public class XMLDBIsAuthenticated extends BasicFunction
+{
+	protected static final Logger logger = Logger.getLogger(XMLDBIsAuthenticated.class);
+	
+	public final static FunctionSignature signature =
+		new FunctionSignature(
+			new QName("is-authenticated", XMLDBModule.NAMESPACE_URI, XMLDBModule.PREFIX),
+			"Returns the true() if current user from the xquery context is authenticated, false() otherwise.",
+			null,
+			new FunctionReturnSequenceType(Type.STRING, Cardinality.EXACTLY_ONE, "true() if user from the xquery context is authenticated, false() otherwise")
+		);
+	
+	public XMLDBIsAuthenticated(XQueryContext context, FunctionSignature signature)
+	{
+		super(context, signature);
+	}
+	
+	public Sequence eval(Sequence args[], Sequence contextSequence) throws XPathException
+	{
+		return new BooleanValue(context.getUser().isAuthenticated());
+	}
+}
