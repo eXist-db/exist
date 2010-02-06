6c60ac474feaf475cb20370b1f9cc4db995c438b
Dannes Wessels <dannes@exist-db.org>
2010-02-06 21:22:47 +0000

    [ignore] added comment, classcast exception. Pierrick, please help?
    java.lang.ClassCastException: org.exist.dom.DocumentImpl cannot be cast to org.exist.dom.StoredNode
    	at org.exist.indexing.spatial.AbstractGMLJDBCIndexWorker.getReindexRoot(AbstractGMLJDBCIndexWorker.java:231)
    	at org.exist.indexing.IndexController.getReindexRoot(IndexController.java:253)
    	at org.exist.dom.ElementImpl.update(ElementImpl.java:1272)
    
    
    svn path=/trunk/eXist/; revision=11138

--- extensions/indexes/spatial/src/org/exist/indexing/spatial/AbstractGMLJDBCIndexWorker.java
+++ extensions/indexes/spatial/src/org/exist/indexing/spatial/AbstractGMLJDBCIndexWorker.java
@@ -228,7 +228,7 @@ public abstract class AbstractGMLJDBCIndexWorker implements IndexWorker {
     	StoredNode topMost = node;
     	StoredNode currentNode = node;
     	for (int i = path.length() ; i > 0; i--) {
-    		currentNode = (StoredNode)currentNode.getParentNode();
+    		currentNode = (StoredNode)currentNode.getParentNode(); // CCE org.exist.dom.DocumentImpl
     		if (GML_NS.equals(currentNode.getNamespaceURI()))
     			//TODO : retain only geometries
     			topMost = currentNode;
