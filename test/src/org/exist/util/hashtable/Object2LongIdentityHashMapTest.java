ef0a10be3b697e63642e8bd4e084674ef7dcd40b
Dmitriy Shabanov <shabanovd@gmail.com>
2010-03-01 17:18:48 +0000

    [test] test cases for the various org.exist.util.hashtable classes
    Many thanks to Paul Murray
    
    svn path=/trunk/eXist/; revision=11347

---
+++ test/src/org/exist/util/hashtable/Object2LongIdentityHashMapTest.java
@@ -0,0 +1,86 @@
+package org.exist.util.hashtable;
+
+import java.util.Iterator;
+
+public class Object2LongIdentityHashMapTest extends AbstractHashtableTest<Object2LongIdentityHashMap, Object, Long> {
+	
+	java.util.HashMap<Integer, Integer> canonicalObject = new java.util.HashMap<Integer, Integer>();
+	
+	protected Object2LongIdentityHashMap newT() {
+		return new Object2LongIdentityHashMap();
+	}
+	
+	@SuppressWarnings("unchecked")
+	protected Iterator<? extends Object> simpleKeyIterator() {
+		return map.iterator();
+	}
+
+	@SuppressWarnings("unchecked")
+	protected Iterator<? extends Long> simpleValueIterator() {
+		return map.valueIterator();
+	}
+
+	protected Long simpleGet(Object k) {
+		long foo = map.get(k);
+		return foo == -1 ? null : foo;
+	}
+
+	protected void simplePut(Object k, Long v) {
+		map.put(k, v);
+	}
+
+	protected void simpleRemove(Object k) {
+		map.remove(k);
+	}
+
+	protected boolean simpleContainsKey(int k) {
+		return map.containsKey(keyEquiv(k));
+	}
+
+	protected Long valEquiv(int v) {
+		return (long) v;
+	}
+
+	protected int valEquiv(Long v) {
+		return v.intValue();
+	}
+
+	protected Object keyEquiv(int k) {
+		if(!canonicalObject.containsKey(k)) {
+			canonicalObject.put(k,k);
+		}
+		return canonicalObject.get(k);
+	}
+
+	protected int keyEquiv(Object k) {
+		return (Integer) k;
+	}
+	
+	protected Integer keyEquiv_newObject(int k) {
+		return new Integer(k);
+	}
+
+	/**
+	 * We override this, because the identity hash map specifically behaves differently
+	 * with respect to equality.
+	 */
+	public void testPutDuplicates() {
+		for (int i = 0; i < 10; i++)
+			for (int j = 0; j < 10; j++) {
+				simpleAdd(keyEquiv_newObject(j));
+			}
+
+		int[] test = new int[10];
+		for (Iterator<? extends Object> ki = simpleKeyIterator(); ki.hasNext();) {
+			Object k = ki.next();
+			int kk = keyEquiv(k);
+			test[kk]++;
+		}
+
+		for (int i = 0; i < 10; i++) {
+			assertEquals("key " + i + " appeared 10 times", 10, test[i]);
+		}
+	}
+
+	
+}
---
+++ test/src/org/exist/util/hashtable/SequencedLongHashMapTest.java
@@ -0,0 +1,91 @@
+package org.exist.util.hashtable;
+
+import java.util.Iterator;
+
+public class SequencedLongHashMapTest extends AbstractHashtableTest<SequencedLongHashMap, Long, Object> {
+	
+	protected SequencedLongHashMap newT() {
+		return new SequencedLongHashMap();
+	}
+
+	protected Object simpleGet(Long k) {
+		return map.get(k);
+	}
+
+	protected void simplePut(Long k, Object v) {
+		map.put(k, v);
+	}
+
+	protected void simpleRemove(Long k) {
+		map.remove(k);
+	}
+
+	protected boolean simpleContainsKey(int k) {
+		return map.get(keyEquiv(k))!=null;
+	}
+
+
+	@SuppressWarnings("unchecked")
+	protected Iterator<? extends Object> simpleValueIterator() {
+		return map.valueIterator();
+	}
+
+	protected Object valEquiv(int v) {
+		return v;
+	}
+
+	protected int valEquiv(Object v) {
+		return (Integer) v;
+	}
+
+	protected Long keyEquiv(int k) {
+		return (long) k;
+	}
+
+	protected Long keyEquiv_newObject(int k) {
+		return new Long(k);
+	}
+
+	protected int keyEquiv(Long k) {
+		return k.intValue();
+	}
+
+	@SuppressWarnings("unchecked")
+	protected Iterator<? extends Long> simpleKeyIterator() {
+		return map.iterator();
+	}
+	
+	@SuppressWarnings("unchecked")
+	public void testSequenceProperty() throws Exception {
+		map.put(1, 2);
+		map.put(3, 4);
+		map.put(11, 12);
+		map.put(5, 6);
+		map.put(7, 8);
+		map.put(9, 10);
+		
+		map.put(3, 2);
+		map.put(11, 10);
+		map.put(5, 4);
+		
+		Iterator<Long> ki = map.iterator();
+		assertEquals(1, (int) (long)ki.next());
+		assertEquals(7, (int) (long)ki.next());
+		assertEquals(9, (int) (long)ki.next());
+		assertEquals(3, (int) (long)ki.next());
+		assertEquals(11, (int) (long)ki.next());
+		assertEquals(5, (int) (long)ki.next());
+		assertFalse(ki.hasNext());
+		
+		Iterator<Integer> vi = map.valueIterator();
+		assertEquals(2, (int) vi.next());
+		assertEquals(8, (int) vi.next());
+		assertEquals(10, (int) vi.next());
+		assertEquals(2, (int) vi.next());
+		assertEquals(10, (int) vi.next());
+		assertEquals(4, (int) vi.next());
+		assertFalse(vi.hasNext());
+	}
+	
+	
+}
