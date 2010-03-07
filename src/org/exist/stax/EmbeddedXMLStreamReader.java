c7d27d1b124a8bc10c3c20f7e4ac78e33c05f442
Wolfgang Meier <wolfgangmm@gmail.com>
2010-03-07 19:06:30 +0000

    [bugfix] XMLStreamReader.getElementText() was not implemented.
    svn path=/trunk/eXist/; revision=11414

--- src/org/exist/stax/EmbeddedXMLStreamReader.java
+++ src/org/exist/stax/EmbeddedXMLStreamReader.java
@@ -283,7 +283,33 @@ public class EmbeddedXMLStreamReader implements ExtendedXMLStreamReader {
     }
 
     public String getElementText() throws XMLStreamException {
-        throw new UnsupportedOperationException();
+        if(getEventType() != START_ELEMENT) {
+            throw new XMLStreamException(
+                "parser must be on START_ELEMENT to read next text");
+        }
+        int eventType = next();
+        StringBuffer content = new StringBuffer();
+        while(eventType != END_ELEMENT ) {
+            if(eventType == CHARACTERS
+                || eventType == CDATA
+                || eventType == SPACE
+                || eventType == ENTITY_REFERENCE) {
+                content.append(getText());
+            } else if(eventType == PROCESSING_INSTRUCTION
+                || eventType == COMMENT) {
+                // skipping
+            } else if(eventType == END_DOCUMENT) {
+                throw new XMLStreamException("unexpected end of document when reading element text content");
+            } else if(eventType == START_ELEMENT) {
+                throw new XMLStreamException(
+                    "element text content may not contain START_ELEMENT");
+            } else {
+                throw new XMLStreamException(
+                    "Unexpected event type "+eventType);
+            }
+            eventType = next();
+        }
+        return content.toString();
     }
 
     public Object getProperty(String string) throws IllegalArgumentException {
