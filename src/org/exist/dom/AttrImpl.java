366013ca82c5432275fa134323d55038ff2a5087
Dmitriy Shabanov <shabanovd@gmail.com>
2010-03-10 09:06:27 +0000

    [bugfix] attributes have base-uri
    svn path=/trunk/eXist/; revision=11423

--- src/org/exist/dom/AttrImpl.java
+++ src/org/exist/dom/AttrImpl.java
@@ -1,22 +1,22 @@
-
 /*
  *  eXist Open Source Native XML Database
- *  Copyright (C) 2000-04,  Wolfgang Meier (wolfgang@exist-db.org)
+ *  Copyright (C) 2000-2010 The eXist Project
+ *  http://exist-db.org
  *
  *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU General Public License
+ *  modify it under the terms of the GNU Lesser General Public License
  *  as published by the Free Software Foundation; either version 2
  *  of the License, or (at your option) any later version.
  *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ *  GNU Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- * 
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
  *  $Id$
  */
 package org.exist.dom;
@@ -319,10 +319,11 @@ public class AttrImpl extends NamedNode implements Attr {
 		return this.getType() == ID;
 	}
 
-	/** ? @see org.w3c.dom.Node#getBaseURI()
-	 */
 	public String getBaseURI() {
-		// maybe TODO - new DOM interfaces - Java 5.0
+		Element e = getOwnerElement();
+		if (e != null)
+			return e.getBaseURI();
+		
 		return null;
 	}
 
