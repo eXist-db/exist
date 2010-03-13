2caf8c33e53373ddf286d95465f0ce285603fa9d
chaeron <andrzej@chaeron.com>
2010-03-13 00:43:19 +0000

    Added base64 encode/decode functions to util along iwth a unit test.
    These functions operate on strings and thus are easier and more intuitive  to use than the string-to-binary function, which relies on a side effect to accomplish base64 encoding and returns a base64binary item.
    
    
    svn path=/trunk/eXist/; revision=11441
    
    
    Former-commit-id: 32e3b196077509e6a724b77f2191ec9ea993f894

---
+++ src/org/exist/xquery/functions/util/Base64Functions.java
@@ -0,0 +1,125 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2001-09 Wolfgang M. Meier
+ *  wolfgang@exist-db.org
+ *  http://exist.sourceforge.net
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *  $Id: BuiltinFunctions.java 9598 2009-07-31 05:45:57Z ixitar $
+ */
+package org.exist.xquery.functions.util;
+
+import org.apache.log4j.Logger;
+
+import org.exist.dom.QName;
+import org.exist.xquery.BasicFunction;
+import org.exist.xquery.Cardinality;
+import org.exist.xquery.FunctionSignature;
+import org.exist.xquery.XPathException;
+import org.exist.xquery.XQueryContext;
+import org.exist.util.Base64Decoder;
+import org.exist.util.Base64Encoder;
+import org.exist.xquery.value.FunctionParameterSequenceType;
+import org.exist.xquery.value.FunctionReturnSequenceType;
+import org.exist.xquery.value.Sequence;
+import org.exist.xquery.value.SequenceType;
+import org.exist.xquery.value.StringValue;
+import org.exist.xquery.value.Type;
+
+import java.io.UnsupportedEncodingException;
+
+/**
+ * Base64 String conversion functions.
+ *
+ * @author  Andrzej Taramina (andrzej@chaeron.com)
+ */
+
+public class Base64Functions extends BasicFunction
+{
+    protected static final Logger           logger       = Logger.getLogger( Base64Functions.class );
+
+   public final static FunctionSignature signatures[] = {
+        new FunctionSignature(
+            new QName( "base64-encode", UtilModule.NAMESPACE_URI, UtilModule.PREFIX ),
+            "Encodes the given string as Base64",
+            new SequenceType[] {
+                new FunctionParameterSequenceType( "string", Type.STRING, Cardinality.ZERO_OR_ONE, "The string to be Base64 encoded" )
+            },
+            new FunctionReturnSequenceType( Type.STRING, Cardinality.ZERO_OR_ONE, "the Base64 encoded output, with trailing newlines trimmed" )
+        ),
+		
+		 new FunctionSignature(
+            new QName( "base64-encode", UtilModule.NAMESPACE_URI, UtilModule.PREFIX ),
+            "Encodes the given string as Base64",
+            new SequenceType[] {
+                new FunctionParameterSequenceType( "string", Type.STRING, Cardinality.ZERO_OR_ONE, "The string to be Base64 encoded" ),
+				new FunctionParameterSequenceType( "trim", Type.BOOLEAN, Cardinality.EXACTLY_ONE, "Trim trailing newlines?" )
+            },
+            new FunctionReturnSequenceType( Type.STRING, Cardinality.ZERO_OR_ONE, "the Base64 encoded output" )
+        ),
+		
+         new FunctionSignature(
+            new QName( "base64-decode", UtilModule.NAMESPACE_URI, UtilModule.PREFIX ),
+            "Decode the given Base64 encoded string back to clear text",
+            new SequenceType[] {
+                new FunctionParameterSequenceType( "string", Type.STRING, Cardinality.ZERO_OR_ONE, "The Base64 string to be decoded" )
+            },
+            new FunctionReturnSequenceType( Type.STRING, Cardinality.ZERO_OR_ONE, "the decoded output" )
+        )
+    };
+	
+
+    public Base64Functions( XQueryContext context, FunctionSignature signature )
+    {
+        super( context, signature );
+    }
+	
+
+    public Sequence eval( Sequence[] args, Sequence contextSequence ) throws XPathException
+    {
+		Sequence value	= Sequence.EMPTY_SEQUENCE;
+		boolean  trim	= true;
+		
+        if( !args[0].isEmpty() ) {       
+			String str = args[0].getStringValue();
+			
+			if( args.length == 2 ) {
+				trim = args[1].effectiveBooleanValue();
+			}
+	
+	        if( isCalledAs( "base64-encode" ) ) {
+	           	Base64Encoder enc = new Base64Encoder();
+					
+	        	enc.translate( str.getBytes()  );
+				
+				if( trim ) {
+	        		value = new StringValue( new String( enc.getCharArray() ).trim() );
+				} else {
+					value = new StringValue( new String( enc.getCharArray() ) );
+				}
+	        } else {
+	            Base64Decoder dec = new Base64Decoder();
+				
+	            dec.translate( str );
+				
+				value = new StringValue( new String( dec.getByteArray() ) );
+	        }
+		}
+		
+		return( value );
+    }
+
+}
--- src/org/exist/xquery/functions/util/UtilModule.java
+++ src/org/exist/xquery/functions/util/UtilModule.java
@@ -7,158 +7,169 @@
  * modify it under the terms of the GNU Lesser General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- *  
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- *  
+ *
  *  $Id$
  */
 package org.exist.xquery.functions.util;
-
-import java.util.Arrays;
 
 import org.exist.dom.QName;
 import org.exist.xquery.AbstractInternalModule;
 import org.exist.xquery.FunctionDef;
 import org.exist.xquery.XPathException;
 
+import java.util.Arrays;
+
+
 /**
  * Module function definitions for util module.
  *
- * @author Wolfgang Meier (wolfgang@exist-db.org)
- * @author ljo
+ * @author  Wolfgang Meier (wolfgang@exist-db.org)
+ * @author  ljo
+ * @author  Andrzej Taramina (andrzej@chaeron.com)
  */
-public class UtilModule extends AbstractInternalModule {
+public class UtilModule extends AbstractInternalModule
+{
+    public final static String        NAMESPACE_URI       = "http://exist-db.org/xquery/util";
 
-	public final static String NAMESPACE_URI = "http://exist-db.org/xquery/util";
-	
-	public final static String PREFIX = "util";
-    public final static String INCLUSION_DATE = "2004-09-12";
-    public final static String RELEASED_IN_VERSION = "&lt; eXist-1.0";
+    public final static String        PREFIX              = "util";
+    public final static String        INCLUSION_DATE      = "2004-09-12";
+    public final static String        RELEASED_IN_VERSION = "&lt; eXist-1.0";
 
-	public final static FunctionDef[] functions = {
-		new FunctionDef(BuiltinFunctions.signatures[0], BuiltinFunctions.class),
-		new FunctionDef(BuiltinFunctions.signatures[1], BuiltinFunctions.class),
-		new FunctionDef(ModuleInfo.moduleDescriptionSig, ModuleInfo.class),
-		new FunctionDef(ModuleInfo.registeredModuleSig, ModuleInfo.class),
-        new FunctionDef(ModuleInfo.mappedModulesSig, ModuleInfo.class),
-		new FunctionDef(ModuleInfo.registeredModulesSig, ModuleInfo.class),
-        new FunctionDef(Expand.signatures[0], Expand.class),
-        new FunctionDef(Expand.signatures[1], Expand.class),
-        new FunctionDef(DescribeFunction.signature, DescribeFunction.class),
-		new FunctionDef(FunDoctype.signature, FunDoctype.class),
-		new FunctionDef(Eval.signatures[0], Eval.class),
-		new FunctionDef(Eval.signatures[1], Eval.class),
-		new FunctionDef(Eval.signatures[2], Eval.class),
-		new FunctionDef(Eval.signatures[3], Eval.class),
-		new FunctionDef(Eval.signatures[4], Eval.class),
-		new FunctionDef(Compile.signature, Compile.class),
-		new FunctionDef(DocumentNameOrId.docIdSignature, DocumentNameOrId.class),
-		new FunctionDef(DocumentNameOrId.docNameSignature, DocumentNameOrId.class),
-		new FunctionDef(CollectionName.signature, CollectionName.class),
-		new FunctionDef(LogFunction.signatures[0], LogFunction.class),
-		new FunctionDef(LogFunction.signatures[1], LogFunction.class),
-		new FunctionDef(LogFunction.signatures[2], LogFunction.class),
-		new FunctionDef(LogFunction.signatures[3], LogFunction.class),
-		new FunctionDef(CatchFunction.signature, CatchFunction.class),
-		new FunctionDef(ExclusiveLockFunction.signature, ExclusiveLockFunction.class),
-		new FunctionDef(SharedLockFunction.signature, SharedLockFunction.class),
-		new FunctionDef(Collations.signature, Collations.class),
-		new FunctionDef(SystemProperty.signature, SystemProperty.class),
-        new FunctionDef(FunctionFunction.signature, FunctionFunction.class),
-        new FunctionDef(CallFunction.signature, CallFunction.class),
-        new FunctionDef(NodeId.signature, NodeId.class),
-        new FunctionDef(GetNodeById.signature, GetNodeById.class),
-        new FunctionDef(IndexKeys.signatures[0], IndexKeys.class),
-        new FunctionDef(IndexKeys.signatures[1], IndexKeys.class),
-        new FunctionDef(IndexKeys.signatures[2], IndexKeys.class),
-        new FunctionDef(IndexKeyOccurrences.signatures[0], IndexKeyOccurrences.class),
-        new FunctionDef(IndexKeyOccurrences.signatures[1], IndexKeyOccurrences.class),
-        new FunctionDef(IndexKeyDocuments.signatures[0], IndexKeyDocuments.class),
-        new FunctionDef(IndexKeyDocuments.signatures[1], IndexKeyDocuments.class),
-        new FunctionDef(IndexType.signature, IndexType.class),
-        new FunctionDef(QNameIndexLookup.signature, QNameIndexLookup.class),
-        new FunctionDef(Serialize.signatures[0], Serialize.class),
-        new FunctionDef(Serialize.signatures[1], Serialize.class),
-        new FunctionDef(BinaryDoc.signatures[0], BinaryDoc.class),
-        new FunctionDef(BinaryDoc.signatures[1], BinaryDoc.class),
-        new FunctionDef(BinaryDoc.signatures[2], BinaryDoc.class),
-        new FunctionDef(BinaryToString.signatures[0], BinaryToString.class),
-        new FunctionDef(BinaryToString.signatures[1], BinaryToString.class),
-        new FunctionDef(BinaryToString.signatures[2], BinaryToString.class),
-        new FunctionDef(BinaryToString.signatures[3], BinaryToString.class),
-        new FunctionDef(Profile.signatures[0], Profile.class),
-        new FunctionDef(Profile.signatures[1], Profile.class),
-        new FunctionDef(PrologFunctions.signatures[0], PrologFunctions.class),
-        new FunctionDef(PrologFunctions.signatures[1], PrologFunctions.class),
-        new FunctionDef(PrologFunctions.signatures[2], PrologFunctions.class),
-        new FunctionDef(SystemTime.signatures[0], SystemTime.class),
-        new FunctionDef(SystemTime.signatures[1], SystemTime.class),
-        new FunctionDef(SystemTime.signatures[2], SystemTime.class),
-        new FunctionDef(RandomFunction.signatures[0], RandomFunction.class),
-        new FunctionDef(RandomFunction.signatures[1], RandomFunction.class),
-        new FunctionDef(FunUnEscapeURI.signature, FunUnEscapeURI.class),
-        new FunctionDef(UUID.signatures[0], UUID.class),
-        new FunctionDef(UUID.signatures[1], UUID.class),
-        new FunctionDef(DeepCopyFunction.signature, DeepCopyFunction.class),
-        new FunctionDef(GetSequenceType.signature, GetSequenceType.class),
-        new FunctionDef(Parse.signatures[0], Parse.class),
-        new FunctionDef(Parse.signatures[1], Parse.class),
-        new FunctionDef(ExtractDocs.signature, ExtractDocs.class),
-        new FunctionDef(NodeXPath.signature, NodeXPath.class),
-        new FunctionDef(Hash.signatures[0], Hash.class),
-        new FunctionDef(Hash.signatures[1], Hash.class),
-        new FunctionDef(GetFragmentBetween.signature, GetFragmentBetween.class),
-        new FunctionDef(BaseConverter.signatures[0], BaseConverter.class),
-        new FunctionDef(BaseConverter.signatures[1], BaseConverter.class),
-		new FunctionDef(Wait.signatures[0], Wait.class)
+    public final static FunctionDef[] functions           = {
+        new FunctionDef( BuiltinFunctions.signatures[0], BuiltinFunctions.class ),
+        new FunctionDef( BuiltinFunctions.signatures[1], BuiltinFunctions.class ),
+        new FunctionDef( ModuleInfo.moduleDescriptionSig, ModuleInfo.class ),
+        new FunctionDef( ModuleInfo.registeredModuleSig, ModuleInfo.class ),
+        new FunctionDef( ModuleInfo.mappedModulesSig, ModuleInfo.class ),
+        new FunctionDef( ModuleInfo.registeredModulesSig, ModuleInfo.class ),
+        new FunctionDef( Expand.signatures[0], Expand.class ),
+        new FunctionDef( Expand.signatures[1], Expand.class ),
+        new FunctionDef( DescribeFunction.signature, DescribeFunction.class ),
+        new FunctionDef( FunDoctype.signature, FunDoctype.class ),
+        new FunctionDef( Eval.signatures[0], Eval.class ),
+        new FunctionDef( Eval.signatures[1], Eval.class ),
+        new FunctionDef( Eval.signatures[2], Eval.class ),
+        new FunctionDef( Eval.signatures[3], Eval.class ),
+        new FunctionDef( Eval.signatures[4], Eval.class ),
+        new FunctionDef( Compile.signature, Compile.class ),
+        new FunctionDef( DocumentNameOrId.docIdSignature, DocumentNameOrId.class ),
+        new FunctionDef( DocumentNameOrId.docNameSignature, DocumentNameOrId.class ),
+        new FunctionDef( CollectionName.signature, CollectionName.class ),
+        new FunctionDef( LogFunction.signatures[0], LogFunction.class ),
+        new FunctionDef( LogFunction.signatures[1], LogFunction.class ),
+        new FunctionDef( LogFunction.signatures[2], LogFunction.class ),
+        new FunctionDef( LogFunction.signatures[3], LogFunction.class ),
+        new FunctionDef( CatchFunction.signature, CatchFunction.class ),
+        new FunctionDef( ExclusiveLockFunction.signature, ExclusiveLockFunction.class ),
+        new FunctionDef( SharedLockFunction.signature, SharedLockFunction.class ),
+        new FunctionDef( Collations.signature, Collations.class ),
+        new FunctionDef( SystemProperty.signature, SystemProperty.class ),
+        new FunctionDef( FunctionFunction.signature, FunctionFunction.class ),
+        new FunctionDef( CallFunction.signature, CallFunction.class ),
+        new FunctionDef( NodeId.signature, NodeId.class ),
+        new FunctionDef( GetNodeById.signature, GetNodeById.class ),
+        new FunctionDef( IndexKeys.signatures[0], IndexKeys.class ),
+        new FunctionDef( IndexKeys.signatures[1], IndexKeys.class ),
+        new FunctionDef( IndexKeys.signatures[2], IndexKeys.class ),
+        new FunctionDef( IndexKeyOccurrences.signatures[0], IndexKeyOccurrences.class ),
+        new FunctionDef( IndexKeyOccurrences.signatures[1], IndexKeyOccurrences.class ),
+        new FunctionDef( IndexKeyDocuments.signatures[0], IndexKeyDocuments.class ),
+        new FunctionDef( IndexKeyDocuments.signatures[1], IndexKeyDocuments.class ),
+        new FunctionDef( IndexType.signature, IndexType.class ),
+        new FunctionDef( QNameIndexLookup.signature, QNameIndexLookup.class ),
+        new FunctionDef( Serialize.signatures[0], Serialize.class ),
+        new FunctionDef( Serialize.signatures[1], Serialize.class ),
+        new FunctionDef( BinaryDoc.signatures[0], BinaryDoc.class ),
+        new FunctionDef( BinaryDoc.signatures[1], BinaryDoc.class ),
+        new FunctionDef( BinaryDoc.signatures[2], BinaryDoc.class ),
+        new FunctionDef( BinaryToString.signatures[0], BinaryToString.class ),
+        new FunctionDef( BinaryToString.signatures[1], BinaryToString.class ),
+        new FunctionDef( BinaryToString.signatures[2], BinaryToString.class ),
+        new FunctionDef( BinaryToString.signatures[3], BinaryToString.class ),
+        new FunctionDef( Profile.signatures[0], Profile.class ),
+        new FunctionDef( Profile.signatures[1], Profile.class ),
+        new FunctionDef( PrologFunctions.signatures[0], PrologFunctions.class ),
+        new FunctionDef( PrologFunctions.signatures[1], PrologFunctions.class ),
+        new FunctionDef( PrologFunctions.signatures[2], PrologFunctions.class ),
+        new FunctionDef( SystemTime.signatures[0], SystemTime.class ),
+        new FunctionDef( SystemTime.signatures[1], SystemTime.class ),
+        new FunctionDef( SystemTime.signatures[2], SystemTime.class ),
+        new FunctionDef( RandomFunction.signatures[0], RandomFunction.class ),
+        new FunctionDef( RandomFunction.signatures[1], RandomFunction.class ),
+        new FunctionDef( FunUnEscapeURI.signature, FunUnEscapeURI.class ),
+        new FunctionDef( UUID.signatures[0], UUID.class ),
+        new FunctionDef( UUID.signatures[1], UUID.class ),
+        new FunctionDef( DeepCopyFunction.signature, DeepCopyFunction.class ),
+        new FunctionDef( GetSequenceType.signature, GetSequenceType.class ),
+        new FunctionDef( Parse.signatures[0], Parse.class ),
+        new FunctionDef( Parse.signatures[1], Parse.class ),
+        new FunctionDef( ExtractDocs.signature, ExtractDocs.class ),
+        new FunctionDef( NodeXPath.signature, NodeXPath.class ),
+        new FunctionDef( Hash.signatures[0], Hash.class ),
+        new FunctionDef( Hash.signatures[1], Hash.class ),
+        new FunctionDef( GetFragmentBetween.signature, GetFragmentBetween.class ),
+        new FunctionDef( BaseConverter.signatures[0], BaseConverter.class ),
+        new FunctionDef( BaseConverter.signatures[1], BaseConverter.class ),
+        new FunctionDef( Wait.signatures[0], Wait.class ),
+		new FunctionDef( Base64Functions.signatures[0], Base64Functions.class ),
+        new FunctionDef( Base64Functions.signatures[1], Base64Functions.class ),
+		new FunctionDef( Base64Functions.signatures[2], Base64Functions.class )     
     };
 
     static {
-        Arrays.sort(functions, new FunctionComparator());
+        Arrays.sort( functions, new FunctionComparator() );
     }
 
-    public final static QName EXCEPTION_QNAME =
-	    new QName("exception", UtilModule.NAMESPACE_URI, UtilModule.PREFIX);
+    public final static QName EXCEPTION_QNAME         = new QName( "exception", UtilModule.NAMESPACE_URI, UtilModule.PREFIX );
 
-    public final static QName EXCEPTION_MESSAGE_QNAME = 
-        new QName("exception-message", UtilModule.NAMESPACE_URI, UtilModule.PREFIX);
-    
-	public UtilModule() throws XPathException {
-		super(functions, true);
-		declareVariable(EXCEPTION_QNAME, null);
-        declareVariable(EXCEPTION_MESSAGE_QNAME, null);
-	}
+    public final static QName EXCEPTION_MESSAGE_QNAME = new QName( "exception-message", UtilModule.NAMESPACE_URI, UtilModule.PREFIX );
 
-	/* (non-Javadoc)
-	 * @see org.exist.xquery.Module#getDescription()
-	 */
-	public String getDescription() {
-		return "A module for various utility extension functions.";
-	}
-	
-	/* (non-Javadoc)
-	 * @see org.exist.xquery.Module#getNamespaceURI()
-	 */
-	public String getNamespaceURI() {
-		return NAMESPACE_URI;
-	}
+    public UtilModule() throws XPathException
+    {
+        super( functions, true );
+        declareVariable( EXCEPTION_QNAME, null );
+        declareVariable( EXCEPTION_MESSAGE_QNAME, null );
+    }
 
-	/* (non-Javadoc)
-	 * @see org.exist.xquery.Module#getDefaultPrefix()
-	 */
-	public String getDefaultPrefix() {
-		return PREFIX;
-	}
+    /* (non-Javadoc)
+     * @see org.exist.xquery.Module#getDescription()
+     */
+    public String getDescription()
+    {
+        return( "A module for various utility extension functions." );
+    }
 
-    public String getReleaseVersion() {
-        return RELEASED_IN_VERSION;
+
+    /* (non-Javadoc)
+     * @see org.exist.xquery.Module#getNamespaceURI()
+     */
+    public String getNamespaceURI()
+    {
+        return( NAMESPACE_URI );
+    }
+
+
+    /* (non-Javadoc)
+     * @see org.exist.xquery.Module#getDefaultPrefix()
+     */
+    public String getDefaultPrefix()
+    {
+        return( PREFIX );
+    }
+
+
+    public String getReleaseVersion()
+    {
+        return( RELEASED_IN_VERSION );
     }
 
 }
