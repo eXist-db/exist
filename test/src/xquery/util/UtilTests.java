ca335bd3a03d3b8adb8aeb205d7c6c1debd9cef4
James Fuller <jim.fuller@webcomposite.com>
2010-03-16 13:48:09 +0000

    [testing] started new internal xquery unit test for util modules
    svn path=/trunk/eXist/; revision=11455

---
+++ test/src/xquery/util/UtilTests.java
@@ -0,0 +1,11 @@
+package xquery.util;
+
+import xquery.TestRunner;
+
+public class UtilTests extends TestRunner {
+
+    @Override
+    protected String getDirectory() {
+        return "test/src/xquery/util";
+    }
+}
