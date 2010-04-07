27390ac2772f8ccf427ffb2e69419fba1948a2b2
Dannes Wessels <dannes@exist-db.org>
2010-04-07 19:48:43 +0000

    [bugfix] catch bad collection URIs while scanning the controller hierarchy in XQueryURLRewrite.  port of rev 10963
    svn path=/stable/eXist-1.4.x/; revision=11638

--- src/org/exist/http/urlrewrite/XQueryURLRewrite.java
+++ src/org/exist/http/urlrewrite/XQueryURLRewrite.java
@@ -584,9 +584,9 @@ public class XQueryURLRewrite implements Filter {
                 Collection subColl = collection;
                 DocumentImpl controllerDoc = null;
                 for (int i = 0; i < components.length; i++) {
-                    if (components[i].length() > 0 && subColl.hasChildCollection(XmldbURI.createInternal(components[i]))) {
-                        DocumentImpl doc = null;
-                        try {
+                    DocumentImpl doc = null;
+                    try {
+                        if (components[i].length() > 0 && subColl.hasChildCollection(XmldbURI.createInternal(components[i]))) {
                             subColl = broker.openCollection(subColl.getURI().append(components[i]), Lock.READ_LOCK);
                             if (subColl != null) {
                                 XmldbURI docUri = subColl.getURI().append("controller.xql");
@@ -595,17 +595,21 @@ public class XQueryURLRewrite implements Filter {
                                     controllerDoc = doc;
                             } else
                                 break;
-                        } catch (PermissionDeniedException e) {
-                            LOG.debug("Permission denied while scanning for XQueryURLRewrite controllers: " +
-                                e.getMessage(), e);
-                        } finally {
-                            if (doc != null)
-                                doc.getUpdateLock().release(Lock.READ_LOCK);
-                            if (subColl != null)
-                                subColl.getLock().release(Lock.READ_LOCK);
+                        } else {
+                            break;
                         }
-                    } else {
+                    } catch (PermissionDeniedException e) {
+                        LOG.debug("Permission denied while scanning for XQueryURLRewrite controllers: " +
+                            e.getMessage(), e);
                         break;
+                    } catch (Exception e) {
+                        LOG.debug("Bad collection URI: " + path);
+                        break;
+                    } finally {
+                        if (doc != null)
+                            doc.getUpdateLock().release(Lock.READ_LOCK);
+                        if (subColl != null)
+                            subColl.getLock().release(Lock.READ_LOCK);
                     }
                 }
                 collection.getLock().release(Lock.READ_LOCK);
