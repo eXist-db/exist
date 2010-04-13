6678eedf64137ca9b0b91e64b78a2050af3aea63
Dannes Wessels <dannes@exist-db.org>
2010-04-13 18:07:17 +0000

    [bugfix]  Get rid of that long standing exception in spatial stuff. ; port of rev11214
    svn path=/stable/eXist-1.4.x/; revision=11675
    
    
    Former-commit-id: 9621c4c655fecacd59b7b56b2ef5090f9a828c21

--- extensions/indexes/spatial/src/org/exist/indexing/spatial/AbstractGMLJDBCIndexWorker.java
+++ extensions/indexes/spatial/src/org/exist/indexing/spatial/AbstractGMLJDBCIndexWorker.java
@@ -228,6 +228,9 @@ public abstract class AbstractGMLJDBCIndexWorker implements IndexWorker {
     	StoredNode topMost = node;
     	StoredNode currentNode = node;
     	for (int i = path.length() ; i > 0; i--) {
+    		//Stop below root
+    		if (currentNode.getParentNode() instanceof DocumentImpl)
+    			break;    			
     		currentNode = (StoredNode)currentNode.getParentNode();
     		if (GML_NS.equals(currentNode.getNamespaceURI()))
     			//TODO : retain only geometries
