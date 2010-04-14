cec73a64c97ef83db8713bf9e66b0347cba6328b
Dannes Wessels <dannes@exist-db.org>
2010-04-14 17:47:06 +0000

    [lib-upgrade] upgrade to log4j 1.2.16 ; with this update the log4j initialization can be made more simple.
    svn path=/trunk/eXist/; revision=11677

--- tools/ircbot/run.bat
+++ tools/ircbot/run.bat
@@ -2,6 +2,6 @@
 
 set EXIST=..\..
 
-set CP="%EXIST%\exist.jar;lib\pircbot.jar;%EXIST%\lib\core\log4j-1.2.15.jar;%EXIST%\lib\core\xmldb.jar;%EXIST%\lib\core\xmlrpc-1.2-patched.jar;classes"
+set CP="%EXIST%\exist.jar;lib\pircbot.jar;%EXIST%\lib\core\log4j-1.2.16.jar;%EXIST%\lib\core\xmldb.jar;%EXIST%\lib\core\xmlrpc-1.2-patched.jar;classes"
 
 java -classpath %CP% org.exist.irc.XBot %1 %2
