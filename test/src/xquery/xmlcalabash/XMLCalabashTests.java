88460d61b9025151f978b462d8363b97290e14c3
James Fuller <jim.fuller@webcomposite.com>
2010-05-03 07:14:52 +0000

    XMLCalabash xproc extensions
    svn path=/trunk/eXist/; revision=11740
    
    
    Former-commit-id: 4b13209d693e085d7d89f3331c647b01292d7dba

---
+++ test/src/xquery/xmlcalabash/XMLCalabashTests.java
@@ -0,0 +1,11 @@
+package xquery.xmlcalabash;
+
+import xquery.TestRunner;
+
+public class XMLCalabashTests extends TestRunner {
+
+    @Override
+    protected String getDirectory() {
+        return "test/src/xquery/xmlcalabash";
+    }
+}
