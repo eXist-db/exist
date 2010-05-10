78878248df9c0c1bdbaaa663460c7c4dd78c5dcd
Pierrick Brihaye <pierrick.brihaye@free.fr>
2010-05-10 17:24:24 +0000

    [ignore] removed annotations
    svn path=/trunk/eXist/; revision=11780

--- test/src/org/exist/xquery/SpecialNamesTest.java
+++ test/src/org/exist/xquery/SpecialNamesTest.java
@@ -54,7 +54,6 @@ public class SpecialNamesTest extends XMLTestCase {
         }
     }
     
-    @SuppressWarnings("unchecked")
 	private void initServer() {
         try {
             if (server == null) {
