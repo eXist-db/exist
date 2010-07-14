45fcd7a1314f6869b9e017d0cbe7db3386ac127c
Wolfgang Meier <wolfgangmm@gmail.com>
2010-07-14 17:32:44 +0000

    [testsuite] added some tests regarding index use
    svn path=/trunk/eXist/; revision=11964

---
+++ test/src/xquery/indexing/IndexingTests.java
@@ -0,0 +1,11 @@
+package xquery.indexing;
+
+import xquery.TestRunner;
+
+public class IndexingTests extends TestRunner {
+
+    @Override
+    protected String getDirectory() {
+        return "test/src/xquery/indexing";
+    }
+}
