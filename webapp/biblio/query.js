fb74e1612886b5cb30a30a2118edf180e95b144c
Wolfgang Meier <wolfgangmm@gmail.com>
2010-07-23 20:21:05 +0000

    [ignore] bibliographic demo: fixed create collection dialog
    svn path=/trunk/eXist/; revision=12032
    
    
    Former-commit-id: 27402b3b3df196d54a2aa3365107d6b1910457e5

--- webapp/biblio/query.js
+++ webapp/biblio/query.js
@@ -6,6 +6,99 @@ $(function() {
     initCollectionTree();
 });
 
+/*
+    Initialize the collection tree. Connect toolbar button events.
+ */
+function initCollectionTree() {
+    var dynaTree = $('#collection-tree-tree');
+    var treeDiv = $('#collection-tree-main').css('display', 'none');
+    dynaTree.dynatree({
+        persist: true,
+        rootVisible: false,
+        initAjax: {url: "collections.xql" },
+        onActivate: function (dtnode) {
+            var key = dtnode.data.key;
+            var form = $('#simple-search-form');
+            $('input[name = collection]', form).val(key);
+            form.submit();
+        }
+    });
+    $('#toggle-collection-tree').click(function () {
+        if (treeDiv.css('display') == 'none') {
+            $('#collection-tree').css({width: '300px', height: 'auto', 'background-color': 'transparent'});
+            $('#main-content').css('margin-left', '310px');
+            treeDiv.css('display', '');
+        } else {
+            $('#collection-tree').css({width: '40px', height: '400px', 'background-color': '#CCC'});
+            $('#main-content').css('margin-left', '50px');
+            treeDiv.css('display', 'none');
+        }
+    });
+    $('#collection-expand-all').click(function () {
+        $("#collection-tree-tree").dynatree("getRoot").visit(function(dtnode){
+            dtnode.expand(true);
+        });
+        return false;
+    });
+    $('#collection-collapse-all').click(function () {
+        $("#collection-tree-tree").dynatree("getRoot").visit(function(dtnode){
+            dtnode.expand(false);
+        });
+        return false;
+    });
+    $('#collection-reload').click(function () {
+        $("#collection-tree-tree").dynatree("getRoot").reload();
+        return false;
+    });
+}
+
+/*
+    Called when the user clicks on the "create" button in the create collection dialog.
+ */
+function createCollection(dialog) {
+    var name = $('#create-collection-form input[name = name]').val();
+    var collection = $('#simple-search-form input[name = collection]').val();
+    var params = { action: 'create-collection', name: name, collection: collection };
+    $.get("operations.xql", params, function (data) {
+        $("#collection-tree-tree").dynatree("getRoot").reload();
+        dialog.dialog("close");
+    });
+}
+
+/**
+ * Called after the user clicked "Login" on the login form.
+ * Checks if the supplied credentials are valid. If yes, submit
+ * the form to reload the page.
+ */
+function login() {
+    var user = $('#login-dialog input[name = user]');
+    var password = $('#login-dialog input[name = password]');
+    $('#login-message').text('Checking ...');
+    $.ajax({
+        url: "checkuser.xql",
+        data: "user=" + user.val() + "&amp;password=" + password.val(),
+        type: 'POST',
+        success:
+            function(data, message) { $('#login-form').submit(); },
+        error: function (response, message) { $('#login-message').html('Login failed: ' + response.responseText); }
+    });
+}
+
+/**
+ * Called from the create indexes dialog if the user clicks on "Start".
+ */
+function createIndexes() {
+    var pass = $('#optimize-dialog input[name = password]');
+    $('#optimize-message').text('Running ...');
+    $.get('optimize.xql?pass=' + pass.val(),
+        function (data, status) {
+            if (status != "success")
+                $('#optimize-message').text('Error during optimize!');
+            else
+                $('#optimize-dialog').dialog("close");
+    });
+}
+
 function loadIndexTerms() {
     var input = $('input[name = input-keyword-prefix]');
     $('#keywords-result').load("filters.xql?type=keywords&prefix=" + input.val(), function () {
@@ -71,61 +164,4 @@ function searchTabSelected(ev, ui) {
     if (ui.index == 3) {
         $('#personal-list-size').load('user.xql', { action: 'count' });
     }
-}
-
-function initCollectionTree() {
-    var dynaTree = $('#collection-tree-tree');
-    var treeDiv = $('#collection-tree-main').css('display', 'none');
-    dynaTree.dynatree({
-        persist: true,
-        rootVisible: false,
-        initAjax: {url: "collections.xql" },
-        onActivate: function (dtnode) {
-            var key = dtnode.data.key;
-            var form = $('#simple-search-form');
-            $('input[name = collection]', form).val(key);
-            form.submit();
-        }
-    });
-    $('#toggle-collection-tree').click(function () {
-        if (treeDiv.css('display') == 'none') {
-            $('#collection-tree').css({width: '300px', height: 'auto', 'background-color': 'transparent'});
-            $('#main-content').css('margin-left', '310px');
-            treeDiv.css('display', '');
-        } else {
-            $('#collection-tree').css({width: '40px', height: '400px', 'background-color': '#CCC'});
-            $('#main-content').css('margin-left', '50px');
-            treeDiv.css('display', 'none');
-        }
-    });
-    $('#collection-expand-all').click(function () {
-        $("#collection-tree-tree").dynatree("getRoot").visit(function(dtnode){
-            dtnode.expand(true);
-        });
-        return false;
-    });
-    $('#collection-collapse-all').click(function () {
-        $("#collection-tree-tree").dynatree("getRoot").visit(function(dtnode){
-            dtnode.expand(false);
-        });
-        return false;
-    });
-    $('#collection-reload').click(function () {
-        $("#collection-tree-tree").dynatree("getRoot").reload();
-        return false;
-    });
-}
-
-function createCollection() {
-    var name = $('#create-collection-form input[name = name]').val();
-    var collection = $('#simple-search-form input[name = collection]').val();
-    var data = { action: 'create-collection', name: name, collection: collection };
-    $.ajax({
-        url: "operations.xql",
-        dataType: "xml",
-        data: data,
-        success: function(data) {
-            $("#collection-tree-tree").dynatree("getRoot").reload();
-        }
-    });
 }
