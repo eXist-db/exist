ac8d3a66906802682df1996922ae717fcd27b678
James Fuller <jim.fuller@webcomposite.com>
2010-07-29 16:33:05 +0000

    [test] adding package management repo xquery smoke tests, disabled at the moment
    svn path=/trunk/eXist/; revision=12098

---
+++ test/src/xquery/expathrepo/ExpathRepoTests.java
@@ -0,0 +1,12 @@
+package xquery.expathrepo;
+
+import xquery.TestRunner;
+
+public class ExpathRepoTests extends TestRunner {
+
+    @Override
+    protected String getDirectory() {
+        return "test/src/xquery/expathrepo";
+    }
+}
+
