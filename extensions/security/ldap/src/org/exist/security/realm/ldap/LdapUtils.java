35ce59fbb9ae4b90a11f1537f5b46a8e18632a77
Dmitriy Shabanov <shabanovd@gmail.com>
2010-07-31 17:46:32 +0000

    [ignore] LDAP realm authentication
    svn path=/trunk/eXist/; revision=12168
    
    
    Former-commit-id: 6fbe3db52c6bef996f15d8df79a8f0627f5b8d22

---
+++ extensions/security/ldap/src/org/exist/security/realm/ldap/LdapUtils.java
@@ -0,0 +1,48 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2010 The eXist Project
+ *  http://exist-db.org
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  
+ *  $Id$
+ */
+package org.exist.security.realm.ldap;
+
+import javax.naming.NamingException;
+import javax.naming.ldap.LdapContext;
+
+import org.apache.log4j.Logger;
+
+/**
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ *
+ */
+public class LdapUtils {
+
+	protected final static Logger LOG = Logger.getLogger(LdapUtils.class);
+
+    public static void closeContext(LdapContext ctx) {
+        try {
+            if (ctx != null) {
+                ctx.close();
+            }
+        } catch (NamingException e) {
+            if (LOG.isDebugEnabled()) {
+            	LOG.error("Exception while closing LDAP context. ", e);
+            }
+        }
+    }
+}
