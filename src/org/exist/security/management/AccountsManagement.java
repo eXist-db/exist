619785cd40cbb1e08e3bfc55e70b2b405a983ddb
Dmitriy Shabanov <shabanovd@gmail.com>
2010-08-02 18:48:53 +0000

    [API] Accounts & Users management interfaces
    svn path=/trunk/eXist/; revision=12198
    
    
    Former-commit-id: 7287d604a49b7bc6ec5344e0729a8e5f0cc70adc

---
+++ src/org/exist/security/management/AccountsManagement.java
@@ -0,0 +1,45 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2010 The eXist Project
+ *  http://exist-db.org
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  
+ *  $Id$
+ */
+package org.exist.security.management;
+
+import org.exist.EXistException;
+import org.exist.security.PermissionDeniedException;
+import org.exist.security.User;
+
+/**
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ *
+ */
+public interface AccountsManagement {
+	
+	public User addAccount(User account) throws PermissionDeniedException, EXistException;
+	
+	public User getAccount(String name);
+
+	public boolean hasAccount(User account);
+	public boolean hasAccount(String name);
+
+	public boolean updateAccount(User account) throws PermissionDeniedException, EXistException;
+	
+	public boolean deleteAccount(User account) throws PermissionDeniedException, EXistException;
+
+}
