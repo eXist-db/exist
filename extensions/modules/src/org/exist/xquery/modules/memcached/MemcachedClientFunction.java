8bd6b83fb56b2842eec070900c6165897de18b1c
Evgeny Gazdovsky <gazdovsky@gmail.com>
2010-08-03 21:19:08 +0000

    [feature] memcached module. To be continue...
    svn path=/trunk/eXist/; revision=12209
    
    
    Former-commit-id: dfc2fe5b8b2616186c4140ccd875f3f1ee9e67fa

---
+++ extensions/modules/src/org/exist/xquery/modules/memcached/MemcachedClientFunction.java
@@ -0,0 +1,100 @@
+/*
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software Foundation
+ *  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *  
+ *  $Id: MailSessionFunctions.java 9745 2009-08-09 21:37:29Z ixitar $
+ */
+
+package org.exist.xquery.modules.memcached;
+
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import net.spy.memcached.BinaryConnectionFactory;
+import net.spy.memcached.MemcachedClient;
+
+import org.apache.log4j.Logger;
+import org.exist.dom.QName;
+import org.exist.xquery.BasicFunction;
+import org.exist.xquery.Cardinality;
+import org.exist.xquery.FunctionSignature;
+import org.exist.xquery.XPathException;
+import org.exist.xquery.XQueryContext;
+import org.exist.xquery.modules.ModuleUtils;
+import org.exist.xquery.value.FunctionParameterSequenceType;
+import org.exist.xquery.value.FunctionReturnSequenceType;
+import org.exist.xquery.value.IntegerValue;
+import org.exist.xquery.value.NodeValue;
+import org.exist.xquery.value.Sequence;
+import org.exist.xquery.value.SequenceIterator;
+import org.exist.xquery.value.SequenceType;
+import org.exist.xquery.value.Type;
+
+public class MemcachedClientFunction extends BasicFunction
+{
+	protected static final Logger logger = Logger.getLogger(MemcachedClientFunction.class);
+
+	public final static FunctionSignature signatures[] = {
+		new FunctionSignature(
+			new QName( "create-client", MemcachedModule.NAMESPACE_URI, MemcachedModule.PREFIX ),
+			"Create a Memcached client.",
+			new SequenceType[]
+			{
+				new FunctionParameterSequenceType( "properties", Type.ELEMENT, Cardinality.ONE_OR_MORE, 
+						"A client properties in the form <properties><property name=\"\" value=\"\"/></properties>. " +
+						"Properties names are \"host\" and optional \"port\" (default port is 11211)."),
+				new FunctionParameterSequenceType( "type", Type.BOOLEAN, Cardinality.ZERO_OR_ONE, 
+						"A type of memcached protocol, if true then binary version of protocol " +
+						"will be used. Default value is false.")
+			},
+			new FunctionReturnSequenceType( Type.LONG, Cardinality.EXACTLY_ONE, "an xs:long representing the client handle." )
+			)
+		};
+
+	public MemcachedClientFunction( XQueryContext context, FunctionSignature signature )
+	{
+		super( context, signature );
+    }
+
+	public Sequence eval( Sequence[] args, Sequence contextSequence ) throws XPathException
+	{
+		List <InetSocketAddress> ialist = new ArrayList <InetSocketAddress>(); 
+		SequenceIterator i = args[0].iterate();
+		do {
+			Properties props = ModuleUtils.parseProperties( ((NodeValue) i.nextItem()).getNode() );
+			String host = props.getProperty("host");
+			String tmp	= props.getProperty("port");
+			int    port	= tmp == null ? 11211 : new Integer(tmp).intValue();   
+			InetSocketAddress ia = new InetSocketAddress(host, port);
+			ialist.add(ia);
+		} while (i.hasNext());
+		
+		boolean isbinary = args[1].isEmpty() ? false : new Boolean(args[1].itemAt(0).getStringValue());
+		
+		MemcachedClient client;
+		try {
+			client = isbinary ? new MemcachedClient(new BinaryConnectionFactory(), ialist) : new MemcachedClient(ialist);
+		} catch (IOException e) {
+			throw new XPathException("Can't connect to memcahed server(s)");
+		}
+		
+		// store the client and return the handle of the one.
+		IntegerValue integerValue = new IntegerValue( MemcachedModule.storeClient(client));
+		return integerValue;
+	}
+}
---
+++ extensions/modules/src/org/exist/xquery/modules/memcached/MemcachedModule.java
@@ -0,0 +1,122 @@
+/*
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software Foundation
+ *  Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *  
+ *  $Id: MailModule.java 10647 2009-11-26 17:27:50Z shabanovd $
+ */
+
+package org.exist.xquery.modules.memcached;
+
+import java.util.HashMap;
+
+import net.spy.memcached.MemcachedClient;
+
+import org.apache.log4j.Logger;
+import org.exist.xquery.AbstractInternalModule;
+import org.exist.xquery.FunctionDef;
+
+/**
+ * eXist Memcached Module Extension
+ * 
+ * An extension module for the eXist Native XML Database that allows
+ * getting and setting stuff via Memcached protocol.
+ *   
+ * @author Evgeny Gazdovsky <gazdovsky@gmail.com>
+ * @version 1.5
+ *
+ */
+public class MemcachedModule extends AbstractInternalModule
+{ 
+	protected final static Logger LOG = Logger.getLogger( MemcachedModule.class );
+	
+	public final static String NAMESPACE_URI = "http://exist-db.org/xquery/memcached";
+	
+	public final static String PREFIX = "mcache";
+    public final static String INCLUSION_DATE = "2010-08-01";
+    public final static String RELEASED_IN_VERSION = "eXist-1.5 (spymemcached in trunk)";
+    
+    private static HashMap<Long, MemcachedClient> clients = new HashMap<Long, MemcachedClient>();
+	
+	private final static FunctionDef[] functions = {
+		
+		new FunctionDef( MemcachedClientFunction.signatures[0], MemcachedClientFunction.class ),
+		new FunctionDef( MemcachedGetFunction.signatures[0], MemcachedGetFunction.class ),
+		new FunctionDef( MemcachedStoreFunction.signatures[0], MemcachedStoreFunction.class ),
+		new FunctionDef( MemcachedStoreFunction.signatures[1], MemcachedStoreFunction.class ),
+		new FunctionDef( MemcachedStoreFunction.signatures[2], MemcachedStoreFunction.class ),
+		new FunctionDef( MemcachedFlushFunction.signatures[0], MemcachedFlushFunction.class ),
+		new FunctionDef( MemcachedDeleteFunction.signatures[0], MemcachedDeleteFunction.class ),
+		new FunctionDef( MemcachedShutdownFunction.signatures[0], MemcachedShutdownFunction.class )
+				
+	};
+	
+	public final static String CLIENTS_CONTEXTVAR		= "_eXist_memcached_clients";
+
+	private static long currentSessionHandle = System.currentTimeMillis();
+	
+	
+	public MemcachedModule()
+	{
+		super( functions );
+	}
+	
+
+	public String getNamespaceURI()
+	{
+		return( NAMESPACE_URI );
+	}
+	
+
+	public String getDefaultPrefix()
+	{
+		return( PREFIX );
+	}
+	
+
+	public String getDescription()
+	{
+		return( "A module for getting and setting stuff in memcached" );
+	}
+	
+    public String getReleaseVersion() {
+        return RELEASED_IN_VERSION;
+    }
+
+	
+	final static MemcachedClient retrieveClient( long clientHandle ) 
+	{
+		return ( clients.get( new Long( clientHandle ) ) );
+	}
+
+
+	final static synchronized long storeClient(MemcachedClient client ) 
+	{
+		long clientHandle = getHandle();
+		clients.put( new Long( clientHandle ), client );
+		return( clientHandle );
+	}
+
+	final static synchronized  void shutdownClient( long clientHandle ) 
+	{
+		MemcachedClient client = clients.get( clientHandle );
+		client.shutdown();
+		clients.remove( new Long( clientHandle ) ) ;
+	}
+	
+	protected static synchronized long getHandle() 
+	{
+		return( currentSessionHandle++ );
+	}
+	
+}
