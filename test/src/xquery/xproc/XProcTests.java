7b1a020bbd03932ae2ff3116dd346a80096ba776
James Fuller <jim.fuller@webcomposite.com>
2010-08-04 15:58:22 +0000

    [xproc] adding new xquery smoke test for xproc
    svn path=/trunk/eXist/; revision=12216

---
+++ test/src/xquery/xproc/XProcTests.java
@@ -0,0 +1,11 @@
+package xquery.xproc;
+
+import xquery.TestRunner;
+
+public class XProcTests extends TestRunner {
+
+    @Override
+    protected String getDirectory() {
+        return "test/src/xquery/xproc";
+    }
+}
