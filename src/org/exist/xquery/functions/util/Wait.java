bbb06ac368f333641cf4f083879977c89f3d6970
Dmitriy Shabanov <shabanovd@gmail.com>
2010-08-10 06:33:05 +0000

    [ignore] clean-up imports
    svn path=/trunk/eXist/; revision=12366

--- src/org/exist/xquery/functions/util/Wait.java
+++ src/org/exist/xquery/functions/util/Wait.java
@@ -21,7 +21,6 @@
  */
 package org.exist.xquery.functions.util;
 
-import org.apache.log4j.Logger;
 import org.exist.dom.QName;
 import org.exist.xquery.BasicFunction;
 import org.exist.xquery.Cardinality;
@@ -40,8 +39,6 @@ import org.exist.xquery.value.Type;
  */
 public class Wait extends BasicFunction 
 {
-    private static final Logger logger = Logger.getLogger( Wait.class );
-    
     public final static FunctionSignature signatures[] = {
             new FunctionSignature (
                     new QName( "wait", UtilModule.NAMESPACE_URI, UtilModule.PREFIX ),
--- src/org/exist/xquery/functions/xmldb/XMLDBCreateUser.java
+++ src/org/exist/xquery/functions/xmldb/XMLDBCreateUser.java
@@ -23,7 +23,6 @@ package org.exist.xquery.functions.xmldb;
 
 import org.apache.log4j.Logger;
 import org.exist.dom.QName;
-import org.exist.security.UserImpl;
 import org.exist.security.internal.aider.UserAider;
 import org.exist.xmldb.LocalCollection;
 import org.exist.xmldb.UserManagementService;
