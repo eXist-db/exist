4594cca6582e2aae98533c5e517fc8181dd86402
Adam Retter <adam.retter@googlemail.com>
2010-08-11 16:47:14 +0000

    Switched to Saxon-B 9 as the default XSLT processor.
    NOTE - Cannot yet move to SaxonHE due to a bug in SaxonHE Configuration whereby it cannot load edition.properties - AllConcurrentTests shows this! As does the Ant xslt task.
    
    svn path=/trunk/eXist/; revision=12387

--- build.bat
+++ build.bat
@@ -17,7 +17,12 @@ set ANT_HOME=%EXIST_HOME%\tools\ant
 set _LIBJARS=%CLASSPATH%;%ANT_HOME%\lib\ant-launcher.jar;%EXIST_HOME%\lib\test\junit-4.8.2.jar;%JAVA_HOME%\lib\tools.jar;%EXIST_HOME%\lib\user\svnkit.jar:%EXIST_HOME%\lib\user\svnkit-cli.jar
 
 set JAVA_ENDORSED_DIRS=%EXIST_HOME%\lib\endorsed
-set JAVA_OPTS=-Djava.endorsed.dirs="%JAVA_ENDORSED_DIRS%" -Dant.home="%ANT_HOME%" -Dexist.home="%EXIST_HOME%"
+
+rem You must set
+rem -Djavax.xml.transform.TransformerFactory=org.apache.xalan.processor.TransformerFactoryImpl
+rem Otherwise Ant will fail to do junitreport with Saxon, as it has a direct dependency on Xalan.
+
+set JAVA_OPTS=-Djava.endorsed.dirs="%JAVA_ENDORSED_DIRS%" -Dant.home="%ANT_HOME%" -Dexist.home="%EXIST_HOME%" -Djavax.xml.transform.TransformerFactory="org.apache.xalan.processor.TransformerFactoryImpl"
 
 echo eXist Build
 echo -------------------
