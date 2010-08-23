2e1c6c8ebd36e2e2f23f998aa21db68cda3ac405
Loren Cahlander <loren.cahlander@gmail.com>
2010-08-23 20:50:17 +0000

    [feature] Adding an optional security filter to the system that can be called before the XQueryURLRewrite filter.
    Add these into webapp/WEB-INF/web.xml before the filter for XQueryURLRewrite
    
    
        <filter>
            <filter-name>DumpFilter</filter-name>
            <filter-class>org.exist.http.filter.DumpFilter</filter-class>
            <init-param>
                <param-name>dumpRequest</param-name>
                <param-value>false</param-value>
            </init-param>
            <init-param>
                <param-name>dumpResponse</param-name>
                <param-value>false</param-value>
            </init-param>
         </filter>
    
        <filter>
            <filter-name>URLExcludes</filter-name>
            <filter-class>org.exist.http.filter.PathFilter</filter-class>
    
            <init-param>
                <param-name>order</param-name>
                <param-value>allow,deny</param-value>
            </init-param>
            
    	<!-- This will return a status of 403 Forbidden if the HTTP GET URL contains /rest/ -->
            <init-param>
                <param-name>type.1</param-name>
                <param-value>HTTP GET /rest/</param-value>
            </init-param>
            
    	<!-- This will return a status of 403 Forbidden if the HTTP GET URL contains the query parameter _query= -->
            <init-param>
                <param-name>type.2</param-name>
                <param-value>HTTP GET ?_query=</param-value>
            </init-param>
            
    	<!-- This will return a status of 403 Forbidden if the HTTP POST URL contains XUpdate -->
            <init-param>
                <param-name>type.3</param-name>
                <param-value>HTTP POST XUpdate</param-value>
            </init-param>
            
    	<!-- This will return a status of 403 Forbidden if the HTTP POST URL contains Query -->
            <init-param>
                <param-name>type.4</param-name>
                <param-value>HTTP POST Query Document</param-value>
            </init-param>
            
    	<!-- This will return a status of 403 Forbidden for the HTTP DELETE URL -->
            <init-param>
                <param-name>type.5</param-name>
                <param-value>HTTP DELETE</param-value>
            </init-param>
            
    	<!-- This will return a status of 403 Forbidden for the HTTP PUT URL -->
            <init-param>
                <param-name>type.6</param-name>
                <param-value>HTTP PUT</param-value>
            </init-param>
            
            
            <init-param>
                <param-name>include.2</param-name>
                <param-value>localhost</param-value>
            </init-param>
    
            <init-param>
                <param-name>include.3</param-name>
                <param-value>127.0.0.1</param-value>
            </init-param>
        </filter>
        
    
    
        <filter-mapping>
            <filter-name>DumpFilter</filter-name>
            <url-pattern>/*</url-pattern>
        </filter-mapping>
    
        <filter-mapping>
            <filter-name>URLExcludes</filter-name>
            <url-pattern>/*</url-pattern>
        </filter-mapping>
    
    
    
    svn path=/trunk/eXist/; revision=12550
    
    
    Former-commit-id: 479d262800d3aeb03231fb83fef85022a92a59b7

---
+++ src/org/exist/http/filter/DumpFilter.java
@@ -0,0 +1,185 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2010 The eXist Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * $Id$
+ */
+package org.exist.http.filter;
+
+import org.apache.log4j.Logger;
+
+import javax.servlet.*;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import java.io.IOException;
+import java.sql.Timestamp;
+import java.util.Enumeration;
+import java.util.Locale;
+
+public class DumpFilter implements Filter {
+
+    private final static Logger LOG = Logger.getLogger(DumpFilter.class);
+
+
+    // ----------------------------------------------------- Instance Variables
+
+
+    /**
+     * The filter configuration object we are associated with.  If this value
+     * is null, this filter instance is not currently configured.
+     */
+    private FilterConfig filterConfig = null;
+
+
+    // --------------------------------------------------------- Public Methods
+
+
+    /**
+     * Take this filter out of service.
+     */
+    public void destroy() {
+
+        this.filterConfig = null;
+
+    }
+
+
+    /**
+     * Time the processing that is performed by all subsequent filters in the
+     * current filter stack, including the ultimately invoked servlet.
+     *
+     * @param request  The servlet request we are processing
+     * @param response The servlet response we are creating
+     * @param chain    The filter chain we are processing
+     * @throws IOException      if an input/output error occurs
+     * @throws ServletException if a servlet error occurs
+     */
+    public void doFilter(ServletRequest request, ServletResponse response,
+                         FilterChain chain)
+            throws IOException, ServletException {
+
+        if (!LOG.isInfoEnabled()) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        LOG.info("== START ====================================");
+        LOG.info("Request Received at " +
+                (new Timestamp(System.currentTimeMillis())));
+        LOG.info("=============================================");
+        LOG.info(" characterEncoding=" + request.getCharacterEncoding());
+        LOG.info("     contentLength=" + request.getContentLength());
+        LOG.info("       contentType=" + request.getContentType());
+        LOG.info("            locale=" + request.getLocale());
+        StringBuffer buffer = new StringBuffer();
+        buffer.append("           locales=");
+        Enumeration locales = request.getLocales();
+        boolean first = true;
+        while (locales.hasMoreElements()) {
+            Locale locale = (Locale) locales.nextElement();
+            if (first)
+                first = false;
+            else
+                buffer.append(", ");
+            buffer.append(locale.toString());
+        }
+        LOG.info(buffer.toString());
+        Enumeration names = request.getParameterNames();
+        while (names.hasMoreElements()) {
+            String name = (String) names.nextElement();
+            buffer = new StringBuffer();
+            buffer.append("         parameter=" + name + "=");
+            String values[] = request.getParameterValues(name);
+            for (int i = 0; i < values.length; i++) {
+                if (i > 0)
+                    buffer.append(", ");
+                buffer.append(values[i]);
+            }
+            LOG.info(buffer.toString());
+        }
+        LOG.info("          protocol=" + request.getProtocol());
+        LOG.info("        remoteAddr=" + request.getRemoteAddr());
+        LOG.info("        remoteHost=" + request.getRemoteHost());
+        LOG.info("            scheme=" + request.getScheme());
+        LOG.info("        serverName=" + request.getServerName());
+        LOG.info("        serverPort=" + request.getServerPort());
+        LOG.info("          isSecure=" + request.isSecure());
+
+        // Render the HTTP servlet request properties
+        if (request instanceof HttpServletRequest) {
+            LOG.info("---------------------------------------------");
+            HttpServletRequest hrequest = (HttpServletRequest) request;
+            LOG.info("       contextPath=" + hrequest.getContextPath());
+            Cookie cookies[] = hrequest.getCookies();
+            if (cookies == null)
+                cookies = new Cookie[0];
+            for (int i = 0; i < cookies.length; i++) {
+                LOG.info("            cookie=" + cookies[i].getName() +
+                        "=" + cookies[i].getValue());
+            }
+            names = hrequest.getHeaderNames();
+            while (names.hasMoreElements()) {
+                String name = (String) names.nextElement();
+                String value = hrequest.getHeader(name);
+                LOG.info("            header=" + name + "=" + value);
+            }
+            LOG.info("            method=" + hrequest.getMethod());
+            LOG.info("          pathInfo=" + hrequest.getPathInfo());
+            LOG.info("       queryString=" + hrequest.getQueryString());
+            LOG.info("        remoteUser=" + hrequest.getRemoteUser());
+            LOG.info("requestedSessionId=" +
+                    hrequest.getRequestedSessionId());
+            LOG.info("        requestURI=" + hrequest.getRequestURI());
+            LOG.info("       servletPath=" + hrequest.getServletPath());
+        }
+        LOG.info("== END ======================================");
+
+        // Pass control on to the next filter
+        chain.doFilter(request, response);
+
+    }
+
+
+    /**
+     * Place this filter into service.
+     *
+     * @param filterConfig The filter configuration object
+     */
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+        this.filterConfig = filterConfig;
+
+    }
+
+
+    /**
+     * Return a String representation of this object.
+     */
+    public String toString() {
+
+        if (filterConfig == null)
+            return ("RequestDumperFilter()");
+        StringBuffer sb = new StringBuffer("RequestDumperFilter(");
+        sb.append(filterConfig);
+        sb.append(")");
+        return (sb.toString());
+
+    }
+
+
+}
---
+++ src/org/exist/http/filter/PathFilter.java
@@ -0,0 +1,204 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2010 The eXist Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ * $Id$
+ */
+package org.exist.http.filter;
+
+import org.apache.log4j.Logger;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Enumeration;
+import java.util.HashSet;
+
+/**
+ *
+ * HTTP GET /rest/
+ * HTTP GET ?_query=
+ * HTTP POST XUpdate
+ * HTTP POST Query Document
+ * HTTP DELETE
+ * HTTP PUT
+
+ * Created by IntelliJ IDEA.
+ * User: lcahlander
+ * Date: Aug 18, 2010
+ * Time: 2:03:11 PM
+ * To change this template use File | Settings | File Templates.
+ */
+public class PathFilter implements Filter {
+
+    private final static Logger LOG = Logger.getLogger(PathFilter.class);
+
+    private FilterConfig filterConfig;
+
+    private static final String TEST_REST = "HTTP GET /rest/";
+    private static final String TEST_GET_QUERY = "HTTP GET ?_query=";
+    private static final String TEST_POST_XUPDATE = "HTTP POST XUpdate";
+    private static final String TEST_POST_QUERY = "HTTP POST Query Document";
+    private static final String TEST_DELETE = "HTTP DELETE";
+    private static final String TEST_PUT = "HTTP PUT";
+
+    private boolean allowFirst = false;
+    private HashSet<String> allows = new HashSet<String>();
+    private HashSet<String> denys = new HashSet<String>();
+    private HashSet<String> filterNames = new HashSet<String>();
+
+    public void init(FilterConfig filterConfig) throws ServletException {
+        setFilterConfig(filterConfig);
+    }
+
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain)
+            throws IOException, ServletException {
+
+        if (filterConfig == null) {
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+
+        HttpServletRequest httpServletRequest;
+        HttpServletResponse httpServletResponse;
+
+        if (servletRequest instanceof HttpServletRequest) {
+            httpServletRequest = (HttpServletRequest)servletRequest;
+            httpServletResponse = (HttpServletResponse)servletResponse;
+        }
+        else {
+            filterChain.doFilter(servletRequest, servletResponse);
+            return;
+        }
+
+        boolean conditionMet = false;
+
+        String queryString = httpServletRequest.getQueryString();
+        String requestURI = httpServletRequest.getRequestURI();
+
+        LOG.info("requestURI = [" + requestURI + "]");
+        LOG.info("queryString = [" + queryString + "]");
+        LOG.info("method = [" + httpServletRequest.getMethod() + "]");
+
+        if ((queryString != null && queryString.indexOf("_query=") >= 0) && filterNames.contains(TEST_GET_QUERY)) {
+            LOG.info(TEST_GET_QUERY + " met");
+            conditionMet = true;
+        } else if (requestURI != null && requestURI.indexOf("/rest/") >= 0 && filterNames.contains(TEST_REST)) {
+            conditionMet = true;
+            LOG.info(TEST_REST + " met");
+        } else if (httpServletRequest.getMethod().equalsIgnoreCase("PUT") && filterNames.contains(TEST_PUT)) {
+            conditionMet = true;
+            LOG.info(TEST_PUT + " met");
+        } else if (httpServletRequest.getMethod().equalsIgnoreCase("DELETE") && filterNames.contains(TEST_DELETE)) {
+            conditionMet = true;
+            LOG.info(TEST_DELETE + " met");
+        } else if (httpServletRequest.getMethod().equalsIgnoreCase("POST")) {
+
+        }
+
+        if (!conditionMet) {
+            filterChain.doFilter(httpServletRequest, httpServletResponse);
+            return;
+        }
+
+        if (allowFirst) {
+            if (allowMatch(httpServletRequest)) {
+                filterChain.doFilter(httpServletRequest, httpServletResponse);
+            }
+        } else {
+            if (denyMatch(httpServletRequest)) {
+                httpServletResponse.sendError(HttpServletResponse.SC_FORBIDDEN);
+            } else if (allowMatch(httpServletRequest)) {
+                filterChain.doFilter(httpServletRequest, httpServletResponse);
+            }
+        }
+
+        httpServletResponse.sendError(HttpServletResponse.SC_FORBIDDEN);
+    }
+
+    private String validName(ServletRequest servletRequest) {
+        String name = servletRequest.getRemoteHost();
+        String address = servletRequest.getRemoteAddr();
+
+        if (name.equalsIgnoreCase(address)) {
+            try {
+                name = InetAddress.getByName(address).getCanonicalHostName();
+            } catch (UnknownHostException e) {
+                name = null;
+            }
+        }
+        return name;
+    }
+
+    private boolean denyMatch(ServletRequest servletRequest) {
+
+        String name = validName(servletRequest);
+        String address = servletRequest.getRemoteAddr();
+        return denys.contains(address) || denys.contains(name);
+    }
+
+    private boolean allowMatch(ServletRequest servletRequest) {
+
+        String name = validName(servletRequest);
+        String address = servletRequest.getRemoteAddr();
+
+        return allows.contains(address) || allows.contains(name);
+    }
+
+    public void destroy() {
+        allows = null;
+        denys = null;
+        filterNames = null;
+        filterConfig = null;
+    }
+
+    public FilterConfig getFilterConfig() {
+        return filterConfig;
+    }
+
+    public void setFilterConfig(FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
+        Enumeration initParams = filterConfig.getInitParameterNames();
+
+        // no initial parameters, so invoke the next filter in the chain
+        if (initParams != null) {
+            allows.clear();
+            denys.clear();
+            filterNames.clear();
+            while (initParams.hasMoreElements()) {
+                String name = (String) initParams.nextElement();
+                String value = filterConfig.getInitParameter(name);
+
+                LOG.info("Parameter [" + name + "][" + value + "]");
+
+                if (name.startsWith("exclude")) {
+                    denys.add(value);
+                } else if (name.startsWith("include")) {
+                    allows.add(value);
+                } else if (name.startsWith("type")) {
+                    filterNames.add(value);
+                } else if (name.equalsIgnoreCase("order")) {
+                    allowFirst = value.equalsIgnoreCase("allow,deny");
+                }
+            }
+        }
+    }
+}
