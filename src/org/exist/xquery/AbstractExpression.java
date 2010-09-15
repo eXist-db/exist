0fd2f31779340170e2d1693d8b5cf23a094236aa
Adam Retter <adam.retter@googlemail.com>
2010-09-15 10:45:13 +0000

    [ignore] Reformated code. Added @Override. Updated licence
    svn path=/trunk/eXist/; revision=12715
    
    
    Former-commit-id: 25f27b981e8ed653b289856adb634513c22741d8

--- src/org/exist/xquery/AbstractExpression.java
+++ src/org/exist/xquery/AbstractExpression.java
@@ -1,24 +1,24 @@
-
-/* eXist Native XML Database
- * Copyright (C) 2000-03,  Wolfgang M. Meier (wolfgang@exist-db.org)
+/*
+ * eXist Open Source Native XML Database
+ * Copyright (C) 2001-2010 The eXist Project
+ * http://exist-db.org
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Library General Public License
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Library General Public License for more details.
+ * GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
- * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *
  * $Id$
  */
-
 package org.exist.xquery;
 
 import org.exist.dom.DocumentSet;
@@ -29,77 +29,79 @@ import org.exist.security.xacml.XACMLSource;
 
 public abstract class AbstractExpression implements Expression {
 
-	private int expressionId = EXPRESSION_ID_INVALID;
-		
-	protected XQueryContext context;
-
+    private int expressionId = EXPRESSION_ID_INVALID;
+    protected XQueryContext context;
     protected int line = -1;
     protected int column = -1;
-    
-	protected DocumentSet contextDocSet = null;
-    
-	/**
-	 * Holds the context id for the context of this expression.
-	 */
+    protected DocumentSet contextDocSet = null;
+    /**
+     * Holds the context id for the context of this expression.
+     */
     protected int contextId = Expression.NO_CONTEXT_ID;
-	
-	public AbstractExpression(XQueryContext context) {
-		this.context = context;
-		this.expressionId = context.nextExpressionId();
-	}
 
+    public AbstractExpression(XQueryContext context) {
+        this.context = context;
+        this.expressionId = context.nextExpressionId();
+    }
+
+    @Override
     public int getExpressionId() {
-		return expressionId;
-	}
-    
+        return expressionId;
+    }
+
+    @Override
     public int getContextId() {
         return contextId;
     }
-	
-	public Sequence eval(Sequence contextSequence)
-		throws XPathException {
-		return eval(contextSequence, null);
-	}
 
-	/* (non-Javadoc)
-	 * @see org.exist.xquery.Expression#eval(org.exist.xquery.value.Sequence, org.exist.xquery.value.Item)
-	 */
-	public abstract Sequence eval(
-		Sequence contextSequence,
-		Item contextItem)
-		throws XPathException;
+    @Override
+    public Sequence eval(Sequence contextSequence) throws XPathException {
+        return eval(contextSequence, null);
+    }
 
-	/* (non-Javadoc)
-	 * @see org.exist.xquery.Expression#returnsType()
-	 */
-	public abstract int returnsType();
+    /* (non-Javadoc)
+     * @see org.exist.xquery.Expression#eval(org.exist.xquery.value.Sequence, org.exist.xquery.value.Item)
+     */
+    @Override
+    public abstract Sequence eval(Sequence contextSequence, Item contextItem) throws XPathException;
 
-	/* (non-Javadoc)
-	 * @see org.exist.xquery.Expression#resetState()
-	 */
-	public void resetState(boolean postOptimization) {
-		contextDocSet = null;
-	}
+    /* (non-Javadoc)
+     * @see org.exist.xquery.Expression#returnsType()
+     */
+    @Override
+    public abstract int returnsType();
 
-	/**
-	 * The default cardinality is {@link Cardinality#EXACTLY_ONE}.
-	 */
-	public int getCardinality() {
-		return Cardinality.EXACTLY_ONE; // default cardinality
-	}
+    /* (non-Javadoc)
+     * @see org.exist.xquery.Expression#resetState()
+     */
+    @Override
+    public void resetState(boolean postOptimization) {
+        contextDocSet = null;
+    }
 
-	/**
-	 * Returns {@link Dependency#DEFAULT_DEPENDENCIES}.
-	 * 
-	 * @see org.exist.xquery.Expression#getDependencies()
-	 */
-	public int getDependencies() {
-		return Dependency.DEFAULT_DEPENDENCIES;
-	}
-	
-	public void setPrimaryAxis(int axis) {
-	}
+    /**
+     * The default cardinality is {@link Cardinality#EXACTLY_ONE}.
+     */
+    @Override
+    public int getCardinality() {
+        return Cardinality.EXACTLY_ONE; // default cardinality
+    }
 
+    /**
+     * Returns {@link Dependency#DEFAULT_DEPENDENCIES}.
+     *
+     * @see org.exist.xquery.Expression#getDependencies()
+     */
+    @Override
+    public int getDependencies() {
+        return Dependency.DEFAULT_DEPENDENCIES;
+    }
+
+    @Override
+    public void setPrimaryAxis(int axis) {
+    }
+
+    @Override
     public int getPrimaryAxis() {
         return Constants.UNKNOWN_AXIS;
     }
@@ -107,62 +109,74 @@ public abstract class AbstractExpression implements Expression {
     /* (non-Javadoc)
      * @see org.exist.xquery.Expression#setContextDocSet(org.exist.dom.DocumentSet)
      */
+    @Override
     public void setContextDocSet(DocumentSet contextSet) {
         this.contextDocSet = contextSet;
     }
-    
+
+    @Override
     public DocumentSet getContextDocSet() {
         return contextDocSet;
     }
-    
+
+    @Override
     public void accept(ExpressionVisitor visitor) {
-    	visitor.visit(this);    	
+        visitor.visit(this);
     }
-    
-	public void setASTNode(XQueryAST ast) {
-        if (ast != null) {
+
+    @Override
+    public void setASTNode(XQueryAST ast) {
+        if(ast != null) {
             line = ast.getLine();
             column = ast.getColumn();
         }
-	}
+    }
 
+    @Override
     public void setLocation(int line, int column) {
         this.line = line;
         this.column = column;
     }
 
+    @Override
     public int getLine() {
         return line;
     }
 
+    @Override
     public int getColumn() {
         return column;
     }
 
+    @Override
     public XACMLSource getSource() {
         return context.getSource();
     }
 
+    @Override
     public XQueryContext getContext() {
         return context;
     }
 
+    @Override
     public int getSubExpressionCount() {
-    	//default value
-    	return 0;
+        //default value
+        return 0;
     }
-    
+
+    @Override
     public Expression getSubExpression(int index) {
-	    throw new IndexOutOfBoundsException("Index: "+index+", Size: "+getSubExpressionCount());
+        throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + getSubExpressionCount());
     }
 
-	public Boolean match(Sequence contextSequence, Item item) throws XPathException {
-		//default
-		return false;
-	}
+    @Override
+    public Boolean match(Sequence contextSequence, Item item) throws XPathException {
+        //default
+        return false;
+    }
 
-	@Override
-	public boolean allowMixNodesInReturn() {
-		return false;
-	}
+    @Override
+    public boolean allowMixNodesInReturn() {
+        return false;
+    }
 }
