c71298f3ce9bb883f736962388cecf10e5b4a20e
chaeron <andrzej@chaeron.com>
2010-10-29 21:03:38 +0000

    Ignore: Code reformat in preparation for bug fix
    svn path=/trunk/eXist/; revision=13054
    
    
    Former-commit-id: 3fc86d13678e858e75961fc5fde57e4556236777

--- src/org/exist/xquery/functions/response/SetCookie.java
+++ src/org/exist/xquery/functions/response/SetCookie.java
@@ -7,26 +7,23 @@
  * modify it under the terms of the GNU Lesser General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- *  
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- *  
+ *
  *  $Id$
  */
 
 package org.exist.xquery.functions.response;
 
-import java.util.Date;
-
-import javax.xml.datatype.Duration;
-
 import org.apache.log4j.Logger;
+
 import org.exist.dom.QName;
 import org.exist.http.servlets.ResponseWrapper;
 import org.exist.xquery.Cardinality;
@@ -37,8 +34,8 @@ import org.exist.xquery.Profiler;
 import org.exist.xquery.Variable;
 import org.exist.xquery.XPathException;
 import org.exist.xquery.XQueryContext;
-import org.exist.xquery.value.DurationValue;
 import org.exist.xquery.value.BooleanValue;
+import org.exist.xquery.value.DurationValue;
 import org.exist.xquery.value.FunctionParameterSequenceType;
 import org.exist.xquery.value.Item;
 import org.exist.xquery.value.JavaObjectValue;
@@ -46,17 +43,21 @@ import org.exist.xquery.value.Sequence;
 import org.exist.xquery.value.SequenceType;
 import org.exist.xquery.value.Type;
 
+import java.util.Date;
+
+import javax.xml.datatype.Duration;
+
+
 /**
- * Set's a HTTP Cookie on the HTTP Response
- * 
- * @author Adam Retter <adam.retter@devon.gov.uk>
- * @author José María Fernández (jmfg@users.sourceforge.net)
- * 
- * @see org.exist.xquery.Function
+ * Set's a HTTP Cookie on the HTTP Response.
+ *
+ * @author  Adam Retter <adam.retter@devon.gov.uk>
+ * @author  José María Fernández (jmfg@users.sourceforge.net)
+ * @see     org.exist.xquery.Function
  */
-public class SetCookie extends Function {
-	
-	protected static final Logger logger = Logger.getLogger(SetCookie.class);
+public class SetCookie extends Function
+{
+    protected static final Logger logger = Logger.getLogger(SetCookie.class);
 	protected static final FunctionParameterSequenceType NAME_PARAM = new FunctionParameterSequenceType("name", Type.STRING, Cardinality.EXACTLY_ONE, "The cookie name");
 	protected static final FunctionParameterSequenceType VALUE_PARAM = new FunctionParameterSequenceType("value", Type.STRING, Cardinality.EXACTLY_ONE, "The cookie value");
 	protected static final FunctionParameterSequenceType MAX_AGE_PARAM = new FunctionParameterSequenceType("max-age", Type.DURATION, Cardinality.EXACTLY_ONE, "The xs:duration of the cookie");
@@ -81,93 +82,109 @@ public class SetCookie extends Function {
 			new SequenceType[] { NAME_PARAM, VALUE_PARAM, MAX_AGE_PARAM, SECURE_PARAM, DOMAIN_PARAM, PATH_PARAM },
 			new SequenceType(Type.ITEM, Cardinality.EMPTY))
 		};
-	
-	public SetCookie(XQueryContext context, FunctionSignature signature) {
-		super(context, signature);
-	}
-	
-	/* (non-Javadoc)
-	 * @see org.exist.xquery.Expression#eval(org.exist.dom.DocumentSet, org.exist.xquery.value.Sequence, org.exist.xquery.value.Item)
-	 */
-	public Sequence eval(Sequence contextSequence, Item contextItem) throws XPathException {
-		
-		if (context.getProfiler().isEnabled()) {
-			context.getProfiler().start(this);       
-			context.getProfiler().message(this, Profiler.DEPENDENCIES, "DEPENDENCIES", Dependency.getDependenciesName(this.getDependencies()));
-			if (contextSequence != null) {
-				context.getProfiler().message(this, Profiler.START_SEQUENCES, "CONTEXT SEQUENCE", contextSequence);
-			}
-			if (contextItem != null) {
-				context.getProfiler().message(this, Profiler.START_SEQUENCES, "CONTEXT ITEM", contextItem.toSequence());	
-			}
-		}
-		
-		ResponseModule myModule = (ResponseModule)context.getModule(ResponseModule.NAMESPACE_URI);
-		
-		// response object is read from global variable $response
-		Variable var = myModule.resolveVariable(ResponseModule.RESPONSE_VAR);
-		if(var == null || var.getValue() == null) {
-			throw new XPathException(this, "Response not set");
-		}
-		if(var.getValue().getItemType() != Type.JAVA_OBJECT) {
-			throw new XPathException(this, "Variable $response is not bound to a Java object.");
-		}
-		JavaObjectValue response = (JavaObjectValue)var.getValue().itemAt(0);
-		
-		//get parameters
-		String name = getArgument(0).eval(contextSequence, contextItem).getStringValue();
-		String value = getArgument(1).eval(contextSequence, contextItem).getStringValue();
-		
-		Sequence ageSeq = Sequence.EMPTY_SEQUENCE;
-		Sequence secureSeq = Sequence.EMPTY_SEQUENCE;
-		Sequence domainSeq = Sequence.EMPTY_SEQUENCE;
-		Sequence pathSeq = Sequence.EMPTY_SEQUENCE;
-		int maxAge = -1;
-		if(getArgumentCount()>2){
-			ageSeq = getArgument(2).eval(contextSequence, contextItem);
-			secureSeq = getArgument(3).eval(contextSequence, contextItem);
-			if(!ageSeq.isEmpty()) {
-				Duration duration = ((DurationValue) ageSeq.itemAt(0)).getCanonicalDuration();
-				maxAge = (int) (duration.getTimeInMillis(new Date(System.currentTimeMillis())) / 1000L);
-			}
-			if(getArgumentCount()>4) {
-				domainSeq = getArgument(4).eval(contextSequence, contextItem);
-				pathSeq = getArgument(5).eval(contextSequence, contextItem);
-			}
-		}
 
-		//set response header
-		if(response.getObject() instanceof ResponseWrapper) {
-			switch(getArgumentCount()) {
-				case 2:
-					((ResponseWrapper) response.getObject()).addCookie(name, value);
-					break;
-				case 4:
-					if (secureSeq.isEmpty()) {
-						((ResponseWrapper) response.getObject()).addCookie(name, value, maxAge);
-					} else {
-						((ResponseWrapper) response.getObject()).addCookie(name, value, maxAge, ((BooleanValue)secureSeq.itemAt(0)).effectiveBooleanValue() );
-					}
-					break;
-				case 6:
-					boolean secure = false;
-					String domain = null;
-					String path = null;
-					if(!secureSeq.isEmpty()) {
-						secure=((BooleanValue)secureSeq.itemAt(0)).effectiveBooleanValue();
-					}
-					if(!domainSeq.isEmpty()) {
-						domain=domainSeq.itemAt(0).getStringValue();
-					}
-					if(!pathSeq.isEmpty()) {
-						path=pathSeq.itemAt(0).getStringValue();
-					}
-					((ResponseWrapper) response.getObject()).addCookie(name, value, maxAge, secure, domain, path);
-					break;
-			}
-		} else {
-			throw new XPathException(this, "Type error: variable $response is not bound to a response object");
-		}
-		return Sequence.EMPTY_SEQUENCE;
-	}
+    public SetCookie( XQueryContext context, FunctionSignature signature )
+    {
+        super( context, signature );
+    }
+
+    /* (non-Javadoc)
+     * @see org.exist.xquery.Expression#eval(org.exist.dom.DocumentSet, org.exist.xquery.value.Sequence, org.exist.xquery.value.Item)
+     */
+    public Sequence eval( Sequence contextSequence, Item contextItem ) throws XPathException
+    {
+        if( context.getProfiler().isEnabled() ) {
+            context.getProfiler().start( this );
+            context.getProfiler().message( this, Profiler.DEPENDENCIES, "DEPENDENCIES", Dependency.getDependenciesName( this.getDependencies() ) );
+
+            if( contextSequence != null ) {
+                context.getProfiler().message( this, Profiler.START_SEQUENCES, "CONTEXT SEQUENCE", contextSequence );
+            }
+
+            if( contextItem != null ) {
+                context.getProfiler().message( this, Profiler.START_SEQUENCES, "CONTEXT ITEM", contextItem.toSequence() );
+            }
+        }
+
+        ResponseModule myModule = (ResponseModule)context.getModule( ResponseModule.NAMESPACE_URI );
+
+        // response object is read from global variable $response
+        Variable       var      = myModule.resolveVariable( ResponseModule.RESPONSE_VAR );
+
+        if( ( var == null ) || ( var.getValue() == null ) ) {
+            throw( new XPathException( this, "Response not set" ) );
+        }
+
+        if( var.getValue().getItemType() != Type.JAVA_OBJECT ) {
+            throw( new XPathException( this, "Variable $response is not bound to a Java object." ) );
+        }
+        JavaObjectValue response  = (JavaObjectValue)var.getValue().itemAt( 0 );
+
+        //get parameters
+        String          name      = getArgument( 0 ).eval( contextSequence, contextItem ).getStringValue();
+        String          value     = getArgument( 1 ).eval( contextSequence, contextItem ).getStringValue();
+
+        Sequence        ageSeq    = Sequence.EMPTY_SEQUENCE;
+        Sequence        secureSeq = Sequence.EMPTY_SEQUENCE;
+        Sequence        domainSeq = Sequence.EMPTY_SEQUENCE;
+        Sequence        pathSeq   = Sequence.EMPTY_SEQUENCE;
+        int             maxAge    = -1;
+
+        if( getArgumentCount() > 2 ) {
+            ageSeq    = getArgument( 2 ).eval( contextSequence, contextItem );
+            secureSeq = getArgument( 3 ).eval( contextSequence, contextItem );
+
+            if( !ageSeq.isEmpty() ) {
+                Duration duration = ( (DurationValue)ageSeq.itemAt( 0 ) ).getCanonicalDuration();
+                maxAge = (int)( duration.getTimeInMillis( new Date( System.currentTimeMillis() ) ) / 1000L );
+            }
+
+            if( getArgumentCount() > 4 ) {
+                domainSeq = getArgument( 4 ).eval( contextSequence, contextItem );
+                pathSeq   = getArgument( 5 ).eval( contextSequence, contextItem );
+            }
+        }
+
+        //set response header
+        if( response.getObject() instanceof ResponseWrapper ) {
+
+            switch( getArgumentCount() ) {
+
+                case 2: {
+                    ( (ResponseWrapper)response.getObject() ).addCookie( name, value );
+                    break;
+                }
+
+                case 4: {
+                    if( secureSeq.isEmpty() ) {
+                        ( (ResponseWrapper)response.getObject() ).addCookie( name, value, maxAge );
+                    } else {
+                        ( (ResponseWrapper)response.getObject() ).addCookie( name, value, maxAge, ( (BooleanValue)secureSeq.itemAt( 0 ) ).effectiveBooleanValue() );
+                    }
+                    break;
+                }
+
+                case 6: {
+                    boolean secure = false;
+                    String  domain = null;
+                    String  path   = null;
+                    if( !secureSeq.isEmpty() ) {
+                        secure = ( (BooleanValue)secureSeq.itemAt( 0 ) ).effectiveBooleanValue();
+                    }
+                    if( !domainSeq.isEmpty() ) {
+                        domain = domainSeq.itemAt( 0 ).getStringValue();
+                    }
+                    if( !pathSeq.isEmpty() ) {
+                        path = pathSeq.itemAt( 0 ).getStringValue();
+                    }
+                    ( (ResponseWrapper)response.getObject() ).addCookie( name, value, maxAge, secure, domain, path );
+                    break;
+                }
+            }
+        } else {
+            throw( new XPathException( this, "Type error: variable $response is not bound to a response object" ) );
+        }
+        
+        return( Sequence.EMPTY_SEQUENCE );
+    }
 }
--- src/org/exist/xquery/functions/response/StreamBinary.java
+++ src/org/exist/xquery/functions/response/StreamBinary.java
@@ -2,30 +2,27 @@
  *  eXist Open Source Native XML Database
  *  Copyright (C) 2001-09 The eXist Project
  *  http://exist-db.org
- *  
+ *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License
  *  as published by the Free Software Foundation; either version 2
  *  of the License, or (at your option) any later version.
- *  
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Lesser General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *  
+ *
  *  $Id$
  */
 package org.exist.xquery.functions.response;
 
-import java.io.BufferedOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-
 import org.apache.log4j.Logger;
+
 import org.exist.dom.QName;
 import org.exist.http.servlets.ResponseWrapper;
 import org.exist.xquery.BasicFunction;
@@ -41,9 +38,14 @@ import org.exist.xquery.value.Sequence;
 import org.exist.xquery.value.SequenceType;
 import org.exist.xquery.value.Type;
 
-public class StreamBinary extends BasicFunction {
-	
-	protected static final Logger logger = Logger.getLogger(StreamBinary.class);
+import java.io.BufferedOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+
+
+public class StreamBinary extends BasicFunction
+{
+    protected static final Logger logger = Logger.getLogger(StreamBinary.class);
 	protected static final FunctionParameterSequenceType BINARY_DATA_PARAM = new FunctionParameterSequenceType("binary-data", Type.BASE64_BINARY, Cardinality.EXACTLY_ONE, "The binary data to stream");
 	protected static final FunctionParameterSequenceType CONTENT_TYPE_PARAM = new FunctionParameterSequenceType("content-type", Type.STRING, Cardinality.EXACTLY_ONE, "The ContentType HTTP header value");
 	protected static final FunctionParameterSequenceType FILENAME_PARAM = new FunctionParameterSequenceType("filename", Type.STRING, Cardinality.ZERO_OR_ONE, "The filename.  If no filename is given, then the current request name is used");
@@ -60,52 +62,65 @@ public class StreamBinary extends BasicFunction {
             new SequenceType(Type.ITEM, Cardinality.EMPTY),
             true
         );
-    
-    public StreamBinary(XQueryContext context) {
-        super(context, signature);
+        
+    public StreamBinary( XQueryContext context )
+    {
+        super( context, signature );
     }
 
-    public Sequence eval(Sequence[] args, Sequence contextSequence)
-            throws XPathException {
-        if(args[0].isEmpty() || args[1].isEmpty()) {
-            return Sequence.EMPTY_SEQUENCE;
-        }
-        Base64Binary binary = (Base64Binary) args[0].itemAt(0);
-        String contentType = args[1].getStringValue();
-        String filename = null;
-        if(args.length > 2 && !args[2].isEmpty())
-        {
-        	filename = args[2].getStringValue();
+    public Sequence eval( Sequence[] args, Sequence contextSequence ) throws XPathException
+    {
+        if( args[0].isEmpty() || args[1].isEmpty() ) {
+            return( Sequence.EMPTY_SEQUENCE );
         }
         
-        ResponseModule myModule = (ResponseModule)context.getModule(ResponseModule.NAMESPACE_URI);
-        // response object is read from global variable $response
-        Variable respVar = myModule.resolveVariable(ResponseModule.RESPONSE_VAR);
-        if(respVar == null || respVar.getValue() == null)
-            throw new XPathException(this, "No response object found in the current XQuery context.");
-        if(respVar.getValue().getItemType() != Type.JAVA_OBJECT)
-            throw new XPathException(this, "Variable $response is not bound to an Java object.");
-        JavaObjectValue respValue = (JavaObjectValue)
-            respVar.getValue().itemAt(0);
-        if (!"org.exist.http.servlets.HttpResponseWrapper".equals(respValue.getObject().getClass().getName()))
-            throw new XPathException(this, signature.toString() +
-                    " can only be used within the EXistServlet or XQueryServlet");
-        ResponseWrapper response = (ResponseWrapper) respValue.getObject();
-        response.setHeader("Content-Type", contentType);
-        if(filename != null)
-        {
-        	response.setHeader("Content-Disposition","inline; filename=" + filename);
+        Base64Binary binary      = (Base64Binary)args[0].itemAt( 0 );
+        String       contentType = args[1].getStringValue();
+        String       filename    = null;
+
+        if( ( args.length > 2 ) && !args[2].isEmpty() ) {
+            filename = args[2].getStringValue();
         }
+
+        ResponseModule myModule = (ResponseModule)context.getModule( ResponseModule.NAMESPACE_URI );
+
+        // response object is read from global variable $response
+        Variable       respVar  = myModule.resolveVariable( ResponseModule.RESPONSE_VAR );
+
+        if( ( respVar == null ) || ( respVar.getValue() == null ) ) {
+            throw( new XPathException( this, "No response object found in the current XQuery context." ) );
+        }
+
+        if( respVar.getValue().getItemType() != Type.JAVA_OBJECT ) {
+            throw( new XPathException( this, "Variable $response is not bound to an Java object." ) );
+        }
+        
+        JavaObjectValue respValue = (JavaObjectValue)respVar.getValue().itemAt( 0 );
+
+        if( !"org.exist.http.servlets.HttpResponseWrapper".equals( respValue.getObject().getClass().getName() ) ) {
+            throw( new XPathException( this, signature.toString() + " can only be used within the EXistServlet or XQueryServlet" ) );
+        }
+        
+        ResponseWrapper response = (ResponseWrapper)respValue.getObject();
+        response.setHeader( "Content-Type", contentType );
+
+        if( filename != null ) {
+            response.setHeader( "Content-Disposition", "inline; filename=" + filename );
+        }
+
         try {
-            OutputStream os = new BufferedOutputStream(response.getOutputStream());
-            os.write(binary.getBinaryData());
+            OutputStream os = new BufferedOutputStream( response.getOutputStream() );
+            os.write( binary.getBinaryData() );
             os.close();
+
             //commit the response
             response.flushBuffer();
-        } catch (IOException e) {
-            throw new XPathException(this, "IO exception while streaming data: " + e.getMessage(), e);
         }
-        return Sequence.EMPTY_SEQUENCE;
+        catch( IOException e ) {
+            throw( new XPathException( this, "IO exception while streaming data: " + e.getMessage(), e ) );
+        }
+        
+        return( Sequence.EMPTY_SEQUENCE );
     }
 
 }
