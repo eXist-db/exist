a44cd24daf11186412b3d9050e5a3bf96afb6179
Dannes Wessels <dannes@exist-db.org>
2010-11-07 21:03:08 +0000

    [ignore] Added some interesting testcases. More is wrong here
    svn path=/trunk/eXist/; revision=13133

--- test/src/org/exist/storage/ValueIndexFactoryTest.java
+++ test/src/org/exist/storage/ValueIndexFactoryTest.java
@@ -8,6 +8,7 @@ import static org.junit.Assert.assertTrue;
 import org.exist.storage.btree.Value;
 import org.exist.util.ByteConversion;
 import org.exist.xquery.value.DecimalValue;
+import org.junit.Ignore;
 import org.junit.Test;
 
 
@@ -15,19 +16,90 @@ public class ValueIndexFactoryTest {
 	
     @Test
     public void negativeNumbersComparison() {
-        byte[] data1 = encode(-8.612328);
 
+        // -8.6...
+        byte[] data1 = encode(-8.612328);
         long v1 = ByteConversion.byteToLong(data1, 0);
 
+        // 1.0
         byte[] data2 = encode(1.0);
-
         long v2 = ByteConversion.byteToLong(data2, 0);
 
-        assertTrue("v1= " + v1 + " v2 = " + v2, v1 < v2);
-        assertEquals("v1 < v2", -1, (new Value(data1).compareTo(new Value(data2))));
-
+        // print data
         print(data1);
         print(data2);
+
+        // -8.6 < 1.0
+        assertTrue("v1= " + v1 + " v2 = " + v2, v1 < v2);
+
+        // -8.6 < 1.0
+        assertEquals("v1 < v2", -1, (new Value(data1).compareTo(new Value(data2))));
+    }
+
+    @Test @Ignore
+    public void negativeNumbersComparison2() {
+
+        // -8.6...
+        byte[] data1 = encode(-8.612328);
+        long v1 = ByteConversion.byteToLong(data1, 0);
+
+        // 1.0
+        byte[] data2 = encode(-1.0);
+        long v2 = ByteConversion.byteToLong(data2, 0);
+
+        // print data
+        print(data1);
+        print(data2);
+
+        // -8.6 < 1.0
+        assertTrue("v1= " + v1 + " v2 = " + v2, v1 < v2);
+
+        // -8.6 < 1.0
+        assertEquals("v1 < v2", -1, (new Value(data1).compareTo(new Value(data2))));
+    }
+
+    @Test @Ignore
+    public void negativeNumbersComparison3() {
+
+        // -8.6...
+        byte[] data1 = encode(8.612328);
+        long v1 = ByteConversion.byteToLong(data1, 0);
+
+        // 1.0
+        byte[] data2 = encode(1.0);
+        long v2 = ByteConversion.byteToLong(data2, 0);
+
+        // print data
+        print(data1);
+        print(data2);
+
+        // -8.6 < 1.0
+        assertTrue("v1= " + v1 + " v2 = " + v2, v1 > v2);
+
+        // -8.6 < 1.0
+        assertEquals("v1 < v2", 1, (new Value(data1).compareTo(new Value(data2))));
+    }
+
+    @Test
+    public void negativeNumbersComparison4() {
+
+        // -8.6...
+        byte[] data1 = encode(8.612328);
+        long v1 = ByteConversion.byteToLong(data1, 0);
+
+        // 1.0
+        byte[] data2 = encode(-1.0);
+        long v2 = ByteConversion.byteToLong(data2, 0);
+
+        // print data
+        print(data1);
+        print(data2);
+
+        // -8.6 < 1.0
+        assertTrue("v1= " + v1 + " v2 = " + v2, v1 > v2);
+
+        // -8.6 < 1.0
+        assertEquals("v1 < v2", 1, (new Value(data1).compareTo(new Value(data2))));
     }
 
     @Test
