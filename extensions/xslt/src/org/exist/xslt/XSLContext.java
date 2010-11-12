eef32a003fb653dd09c09f7b2db975af233751d5
Dmitriy Shabanov <shabanovd@gmail.com>
2010-11-12 21:03:55 +0000

    [xslt] select root template base on priority
    svn path=/trunk/eXist/; revision=13198

--- extensions/xslt/src/org/exist/xslt/XSLContext.java
+++ extensions/xslt/src/org/exist/xslt/XSLContext.java
@@ -21,11 +21,18 @@
  */
 package org.exist.xslt;
 
+import java.util.Map;
+
 import org.exist.interpreter.ContextAtExist;
 import org.exist.security.xacml.AccessContext;
 import org.exist.storage.DBBroker;
+import org.exist.util.Configuration;
+import org.exist.xquery.Module;
+import org.exist.xquery.XPathException;
 import org.exist.xquery.XQueryContext;
 import org.exist.xquery.XQueryWatchDog;
+import org.exist.xslt.functions.XSLModule;
+
 import javax.xml.transform.Transformer;
 
 /**
@@ -45,17 +52,56 @@ public class XSLContext extends XQueryContext implements ContextAtExist {
 	private void init() {
     	setWatchDog(new XQueryWatchDog(this));
     	
-    	//UNDERSTAND: what to do?
-    	/*
+        loadDefaultNS();
+        
+
+        Configuration config = broker.getConfiguration();
+        // Get map of built-in modules
+        Map<String, Class<Module>> builtInModules = (Map)config.getProperty( PROPERTY_BUILT_IN_MODULES );
+
+        if( builtInModules != null ) {
+
+            // Iterate on all map entries
+            for( Map.Entry<String, Class<Module>> entry : builtInModules.entrySet() ) {
+
+                // Get URI and class
+                String        namespaceURI = entry.getKey();
+                Class<Module> moduleClass  = entry.getValue();
+                
+                // first check if the module has already been loaded in the parent context
+                Module        module       = getModule( namespaceURI );
+
+                if( module == null ) {
+                    // Module does not exist yet, instantiate
+                    instantiateModule( namespaceURI, moduleClass );
+
+                } else if( ( getPrefixForURI( module.getNamespaceURI() ) == null )
+                           && ( module.getDefaultPrefix().length() > 0 ) ) {
+
+                    // make sure the namespaces of default modules are known,
+                    // even if they were imported in a parent context
+                    try {
+                        declareNamespace( module.getDefaultPrefix(), module.getNamespaceURI() );
+                        
+                    } catch( XPathException e ) {
+                        LOG.warn( "Internal error while loading default modules: " + e.getMessage(), e );
+                    }
+                }
+            }
+        }
+        //UNDERSTAND: what to do?
     	try {
-			importModule(XSLTModule.NAMESPACE_URI,
-					XSLTModule.PREFIX,
-					"java:org.exist.xslt.functions.XSLTModule");
+			importModule(XSLModule.NAMESPACE_URI,
+					XSLModule.PREFIX,
+					"java:org.exist.xslt.functions.XSLModule");
 		} catch (XPathException e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		*/
+	}
+	
+	protected void loadDefaultNS() {
+		super.loadDefaultNS();
+		
 	}
 	
 	public void setXSLStylesheet(XSLStylesheet xsl) {
