fd23ec768070c0bfe7657b058bd9e69e3af92128
Dmitriy Shabanov <shabanovd@gmail.com>
2010-11-16 17:15:23 +0000

    [ignore] add xsl namespace
    svn path=/trunk/eXist/; revision=13235
    
    
    Former-commit-id: bbaf999c5dff70e7b7ef3dbaf09027a0af155c9d

--- src/org/exist/Namespaces.java
+++ src/org/exist/Namespaces.java
@@ -40,10 +40,12 @@ public interface Namespaces {
     public final static String XQUERY_LOCAL_NS = "http://www.w3.org/2005/xquery-local-functions";
 	public final static String XPATH_DATATYPES_NS = "http://www.w3.org/2003/05/xpath-datatypes";
 
-        public final static String XQUERY_XPATH_ERROR_NS = "http://www.w3.org/2005/xqt-errors/";
-        public final static String XQUERY_XPATH_ERROR_PREFIX = "err";
+    public final static String XQUERY_XPATH_ERROR_NS = "http://www.w3.org/2005/xqt-errors/";
+    public final static String XQUERY_XPATH_ERROR_PREFIX = "err";
 
-	public final static String EXIST_NS = "http://exist.sourceforge.net/NS/exist";
+	public final static String XSL_NS = "http://www.w3.org/1999/XSL/Transform";
+
+    public final static String EXIST_NS = "http://exist.sourceforge.net/NS/exist";
 	public final static String RDF_NS = "http://www.w3.org/1999/02/22-rdf-syntax-ns#";
 	public final static String DC_NS = "http://purl.org/dc/elements/1.1/";
 
