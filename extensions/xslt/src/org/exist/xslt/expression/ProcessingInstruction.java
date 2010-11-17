10f99908ba342d26fb8649c4c2b5ada2be095e08
Dmitriy Shabanov <shabanovd@gmail.com>
2010-11-17 07:52:13 +0000

    [xslt] document, attribute, comment and pi constructors
    svn path=/trunk/eXist/; revision=13247

--- extensions/xslt/src/org/exist/xslt/expression/ProcessingInstruction.java
+++ extensions/xslt/src/org/exist/xslt/expression/ProcessingInstruction.java
@@ -22,10 +22,19 @@
 package org.exist.xslt.expression;
 
 import org.exist.interpreter.ContextAtExist;
+import org.exist.memtree.DocumentImpl;
+import org.exist.memtree.MemTreeBuilder;
+import org.exist.util.XMLChar;
+import org.exist.xquery.Constants;
+import org.exist.xquery.Dependency;
+import org.exist.xquery.ErrorCodes;
+import org.exist.xquery.PathExpr;
+import org.exist.xquery.Profiler;
 import org.exist.xquery.XPathException;
 import org.exist.xquery.util.ExpressionDumper;
 import org.exist.xquery.value.Item;
 import org.exist.xquery.value.Sequence;
+import org.exist.xquery.value.SequenceIterator;
 import org.exist.xslt.XSLContext;
 import org.w3c.dom.Attr;
 
@@ -42,30 +51,110 @@ import org.w3c.dom.Attr;
  */
 public class ProcessingInstruction extends Declaration {
 
-    private String name = null;
-    private String select = null;
+    private String attr_name = null;
+    private String attr_select = null;
 
-    public ProcessingInstruction(XSLContext context) {
+	private PathExpr select = null;
+
+	public ProcessingInstruction(XSLContext context) {
 		super(context);
 	}
 
 	public void setToDefaults() {
-	    name = null;
+	    attr_name = null;
+	    attr_select = null;
+
 	    select = null;
 	}
 
 	public void prepareAttribute(ContextAtExist context, Attr attr) throws XPathException {
-		String attr_name = attr.getLocalName();
+		String localName = attr.getLocalName();
 			
-		if (attr_name.equals(NAME)) {
-			name = attr.getValue();
-		} else if (attr_name.equals(SELECT)) {
-			select = attr.getValue();
+		if (localName.equals(NAME)) {
+			attr_name = attr.getValue();
+		} else if (localName.equals(SELECT)) {
+			attr_select = attr.getValue();
 		}
 	}
 	
 	public Sequence eval(Sequence contextSequence, Item contextItem) throws XPathException {
-    	throw new RuntimeException("eval(Sequence contextSequence, Item contextItem) at "+this.getClass());
+        if (context.getProfiler().isEnabled()) {
+            context.getProfiler().start(this);       
+            context.getProfiler().message(this, Profiler.DEPENDENCIES, "DEPENDENCIES", Dependency.getDependenciesName(this.getDependencies()));
+            if (contextSequence != null)
+                context.getProfiler().message(this, Profiler.START_SEQUENCES, "CONTEXT SEQUENCE", contextSequence);
+            if (contextItem != null)
+                context.getProfiler().message(this, Profiler.START_SEQUENCES, "CONTEXT ITEM", contextItem.toSequence());
+        }
+
+//        if (newDocumentContext)
+//            context.pushDocumentContext();
+        try {
+            MemTreeBuilder builder = context.getDocumentBuilder();
+            context.proceed(this, builder);
+
+//            Sequence nameSeq = name.eval(contextSequence, contextItem);
+//            if(!nameSeq.hasOne())
+//            	throw new XPathException(this, "The name expression should evaluate to a single value");
+
+//            Item nameItem = nameSeq.itemAt(0);
+//            if(!(nameItem.getType() == Type.STRING || nameItem.getType() == Type.NCNAME ||
+//                    nameItem.getType() == Type.UNTYPED_ATOMIC))
+//                throw new XPathException(this, "The name expression should evaluate to a " +
+//                        Type.getTypeName(Type.STRING) + " or a " + Type.getTypeName(Type.NCNAME) +
+//                        " or a " + Type.getTypeName(Type.UNTYPED_ATOMIC) + ". Got: " +
+//                        Type.getTypeName(nameItem.getType()));
+
+//            if(!XMLChar.isValidNCName(nameSeq.getStringValue()))
+//			throw new XPathException(this, ErrorCodes.XQDY0041, nameSeq.getStringValue() + "' is not a valid processing instruction name", nameSeq);
+
+//            if (nameSeq.getStringValue().equalsIgnoreCase("XML"))
+//                throw new XPathException(this, ErrorCodes.XQDY0064, nameSeq.getStringValue() + "' is not a valid processing instruction name", nameSeq);
+
+            if(!XMLChar.isValidNCName(attr_name))
+            	throw new XPathException(this, ErrorCodes.XQDY0041, "'"+attr_name + "' is not a valid processing instruction name");
+
+            if (attr_name.equalsIgnoreCase("XML"))
+                throw new XPathException(this, ErrorCodes.XQDY0064, "'"+attr_name+"' is not a valid processing instruction name");
+
+            String contentString;
+            Sequence contentSeq;
+            if (select != null)
+            	contentSeq = select.eval(contextSequence, contextItem);
+            else
+            	contentSeq = super.eval(contextSequence, contextItem);
+            
+            if(contentSeq.isEmpty())
+        	contentString = "";
+            else {
+                StringBuilder buf = new StringBuilder();
+                for(SequenceIterator i = contentSeq.iterate(); i.hasNext(); ) {
+                    context.proceed(this, builder);
+                    Item next = i.nextItem();
+                    if(buf.length() > 0)
+                        buf.append(' ');
+                    buf.append(next.getStringValue());
+                }
+                while (buf.length() > 0 && Character.isWhitespace(buf.charAt(0)))
+                    buf.deleteCharAt(0);
+                contentString = buf.toString();
+            }
+
+            if (contentString.indexOf("?>") != Constants.STRING_NOT_FOUND)
+                throw new XPathException(this, ErrorCodes.XQDY0026, contentString + "' is not a valid processing intruction content", contentSeq);
+
+            int nodeNr = builder.processingInstruction(attr_name, contentString);
+
+            Sequence result = ((DocumentImpl)builder.getDocument()).getNode(nodeNr);
+
+            if (context.getProfiler().isEnabled())
+                context.getProfiler().end(this, "", result);
+
+            return result;
+        } finally {
+//            if (newDocumentContext)
+//                context.popDocumentContext();
+        }
 	}
 
 	/* (non-Javadoc)
@@ -74,13 +163,13 @@ public class ProcessingInstruction extends Declaration {
     public void dump(ExpressionDumper dumper) {
         dumper.display("<xsl:processing-instruction");
 
-        if (name != null) {
+        if (attr_name != null) {
         	dumper.display(" name = ");
-        	dumper.display(name);
+        	dumper.display(attr_name);
         }
-        if (select != null) {
+        if (attr_select != null) {
         	dumper.display(" select = ");
-        	dumper.display(select);
+        	dumper.display(attr_select);
         }
 
         super.dump(dumper);
@@ -92,10 +181,10 @@ public class ProcessingInstruction extends Declaration {
     	StringBuilder result = new StringBuilder();
     	result.append("<xsl:processing-instruction");
         
-    	if (name != null)
-        	result.append(" name = "+name.toString());    
-    	if (select != null)
-        	result.append(" select = "+select.toString());    
+    	if (attr_name != null)
+        	result.append(" name = "+attr_name.toString());    
+    	if (attr_select != null)
+        	result.append(" select = "+attr_select.toString());    
 
         result.append("> ");
         
