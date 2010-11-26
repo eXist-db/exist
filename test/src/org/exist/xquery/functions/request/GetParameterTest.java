143321ee1cc88b6f7f124fdd67131e44aae8fefe
Adam Retter <adam.retter@googlemail.com>
2010-11-26 21:57:07 +0000

    Moved query into the database, added post requests. More tests to come
    svn path=/trunk/eXist/; revision=13319
    
    
    Former-commit-id: a9ac293746a96dd683f757fbbbfbb71738c91118

--- test/src/org/exist/xquery/functions/request/GetParameterTest.java
+++ test/src/org/exist/xquery/functions/request/GetParameterTest.java
@@ -1,19 +1,30 @@
 package org.exist.xquery.functions.request;
 
-import static org.custommonkey.xmlunit.XMLAssert.assertXMLEqual;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.apache.commons.httpclient.HttpException;
+import org.apache.commons.httpclient.HttpMethod;
 import org.apache.commons.httpclient.HttpStatus;
 import org.apache.commons.httpclient.NameValuePair;
 import org.apache.commons.httpclient.methods.GetMethod;
+import org.apache.commons.httpclient.methods.PostMethod;
 import org.exist.http.RESTTest;
+import org.exist.xmldb.EXistResource;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.xml.sax.SAXException;
+import org.xmldb.api.DatabaseManager;
+import org.xmldb.api.base.Collection;
+import org.xmldb.api.base.Resource;
+import org.xmldb.api.base.XMLDBException;
+import org.xmldb.api.modules.BinaryResource;
 
 /**
  * Tests expected behaviour of request:get-parameter() XQuery function
@@ -23,85 +34,182 @@ import org.xml.sax.SAXException;
  */
 public class GetParameterTest extends RESTTest {
 
-    private final static String HTTP_REQUEST_PARAM_NAME = "param1";
-    private final static String xquery = "<request-param name=\""
-            + HTTP_REQUEST_PARAM_NAME
-            + "\">{for $value in request:get-parameter(\""
-            + HTTP_REQUEST_PARAM_NAME
-            + "\", ()) return <value>{$value}</value>}</request-param>";
+    private final static String XQUERY = "for $param-name in request:get-parameter-names() return for $param-value in request:get-parameter($param-name, ()) return fn:concat($param-name, '=', $param-value)";
+    private final static String XQUERY_FILENAME = "test-get-parameter.xql";
 
-    @Test
-    public void testNoParameter() {
-        testGetParameter(null);
+    private static Collection root;
+
+    
+    @BeforeClass
+    public static void beforeClass() throws XMLDBException {
+        root = DatabaseManager.getCollection("xmldb:exist://localhost:8088/xmlrpc/db", "admin", "");
+        BinaryResource res = (BinaryResource)root.createResource(XQUERY_FILENAME, "BinaryResource");
+        ((EXistResource) res).setMimeType("application/xquery");
+        res.setContent(XQUERY);
+        root.storeResource(res);
+    }
+
+    @AfterClass
+    public static void afterClass() throws XMLDBException {
+        BinaryResource res = (BinaryResource)root.getResource(XQUERY_FILENAME);
+        root.removeResource(res);
     }
 
     @Test
-    public void testEmptyParameter() {
-        testGetParameter(new String[]{""});
+    public void testGetNoParameter() throws XMLDBException {
+        testGet(null);
     }
 
     @Test
-    public void testSingleValueParameter() {
-        testGetParameter(new String[]{"value1"});
+    public void testPostNoParameter() throws XMLDBException {
+        testPost(null);
     }
 
     @Test
-    public void testMultiValueParameter() {
-        testGetParameter(new String[]{"value1", "value2", "value3", "value4"});
+    public void testGetEmptyParameter() {
+        testGet(new NameValues[] {
+            new NameValues("param1", new String[]{})
+        });
     }
 
-    private void testGetParameter(String paramValues[]) {
-        GetMethod get = new GetMethod(COLLECTION_ROOT_URL);
+    @Test
+    public void testPostEmptyParameter() {
+        testPost(new NameValues[] {
+            new NameValues("param1", new String[]{})
+        });
+    }
 
-        NameValuePair qsParams[] = null;
-        NameValuePair qsXQueryParam = new NameValuePair("_query", xquery);
-        NameValuePair qsIndentParam = new NameValuePair("_indent", "no");
+    @Test
+    public void testGetSingleValueParameter() {
+        testGet(new NameValues[] {
+            new NameValues("param1", new String[] {
+                "value1"
+            })
+        });
+    }
 
-        StringBuilder xmlExpectedResponse = new StringBuilder(
-                "<exist:result xmlns:exist=\"http://exist.sourceforge.net/NS/exist\" exist:hits=\"1\" exist:start=\"1\" exist:count=\"1\"><request-param name=\""
-                + HTTP_REQUEST_PARAM_NAME + "\">");
-        if(paramValues == null || paramValues.length == 0) {
-            qsParams = new NameValuePair[2];
-            qsParams[0] = qsXQueryParam;
-            qsParams[1] = qsIndentParam;
-        } else {
-            qsParams = new NameValuePair[paramValues.length + 2];
-            qsParams[0] = qsXQueryParam;
-            qsParams[1] = qsIndentParam;
-            for(int i = 0; i < paramValues.length; i++) {
-                qsParams[i + 2] = new NameValuePair(HTTP_REQUEST_PARAM_NAME,
-                        paramValues[i]);
-                xmlExpectedResponse.append("<value>" + paramValues[i]
-                        + "</value>");
-            }
+    @Test
+    public void testPostSingleValueParameter() {
+        testPost(new NameValues[] {
+            new NameValues("param1", new String[] {
+                "value1"
+            })
+        });
+    }
+
+    @Test
+    public void testGetMultiValueParameter() {
+        testGet(new NameValues[]{
+            new NameValues("param1", new String[] {
+                "value1",
+                "value2",
+                "value3",
+                "value4"
+            })
+        });
+    }
+
+    @Test
+    public void testPostMultiValueParameter() {
+        testPost(new NameValues[]{
+            new NameValues("param1", new String[] {
+                "value1",
+                "value2",
+                "value3",
+                "value4"
+            })
+        });
+    }
+
+    private void testGet(NameValues queryStringParams[]) {
+
+        StringBuilder expectedResponse = new StringBuilder();
+        NameValuePair qsParams[] = convertNameValuesToNameValuePairs(queryStringParams, expectedResponse);
+
+        GetMethod get = new GetMethod(COLLECTION_ROOT_URL + "/" + XQUERY_FILENAME);
+        if(qsParams.length > 0) {
+            get.setQueryString(qsParams);
         }
-        xmlExpectedResponse.append("</request-param></exist:result>");
 
-        get.setQueryString(qsParams);
+        testRequest(get, expectedResponse);
+    }
+
+    private void testPost(NameValues formParams[]) {
+
+        StringBuilder expectedResponse = new StringBuilder();
+        NameValuePair fParams[] = convertNameValuesToNameValuePairs(formParams, expectedResponse);
+
+        PostMethod post = new PostMethod(COLLECTION_ROOT_URL + "/" + XQUERY_FILENAME);
+
+        if(fParams.length > 0) {
+            post.setRequestBody(fParams);
+        }
+
+        testRequest(post, expectedResponse);
+    }
+
+    private void testRequest(HttpMethod method, StringBuilder expectedResponse) {
 
         try {
-            int httpResult = client.executeMethod(get);
+            int httpResult = client.executeMethod(method);
 
             byte buf[] = new byte[1024];
             int read = -1;
-            StringBuilder xmlActualResponse = new StringBuilder();
-            InputStream is = get.getResponseBodyAsStream();
+            StringBuilder responseBody = new StringBuilder();
+            InputStream is = method.getResponseBodyAsStream();
             while((read = is.read(buf)) > -1) {
-                xmlActualResponse.append(new String(buf, 0, read));
+                responseBody.append(new String(buf, 0, read));
             }
 
-            assertEquals(httpResult, HttpStatus.SC_OK);
+            assertEquals(HttpStatus.SC_OK, httpResult);
 
-            assertXMLEqual(xmlActualResponse.toString(), xmlExpectedResponse.toString());
+            assertEquals(expectedResponse.toString(), responseBody.toString());
 
         } catch(HttpException he) {
             fail(he.getMessage());
         } catch(IOException ioe) {
             fail(ioe.getMessage());
-        } catch(SAXException sae) {
-            fail(sae.getMessage());
         } finally {
-            get.releaseConnection();
+            method.releaseConnection();
         }
+    }
+
+    private NameValuePair[] convertNameValuesToNameValuePairs(NameValues nameValues[], StringBuilder expectedResponse) {
+
+        List<NameValuePair> nameValuePairs = new ArrayList<NameValuePair>();
+
+        if(nameValues != null) {
+            for(NameValues param : nameValues) {
+                for(String paramValue : param.getValues()) {
+                    nameValuePairs.add(new NameValuePair(param.getName(), paramValue));
+
+                    expectedResponse.append(param.getName());
+                    expectedResponse.append("=");
+                    expectedResponse.append(paramValue);
+                }
+            }
+        }
+
+        return nameValuePairs.toArray(new NameValuePair[nameValuePairs.size()]);
+    }
+
+    public class NameValues {
+
+        final String name;
+        final String values[];
+
+        public NameValues(String name, String values[]) {
+            this.name = name;
+            this.values = values;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public String[] getValues() {
+            return values;
+        }
+
     }
 }
