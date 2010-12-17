38574f13b139392ca3c9b0f831db99c53d843f1f
Wolfgang Meier <wolfgangmm@gmail.com>
2010-12-17 19:50:40 +0000

    [bugfix] Fixed a few cases in which a temporary fragment was generated.
    svn path=/stable/eXist-1.4.x/; revision=13410
    
    
    Former-commit-id: 5c871f2931a2adefc3d7c34c1b67503a51b37158

--- src/org/exist/xquery/functions/DeprecatedExtRegexp.java
+++ src/org/exist/xquery/functions/DeprecatedExtRegexp.java
@@ -160,6 +160,9 @@ public class DeprecatedExtRegexp extends Function implements Optimizable {
     }
     
     public NodeSet preSelect(Sequence contextSequence, boolean useContext) throws XPathException {
+    	if (contextSequence != null && !contextSequence.isPersistentSet())
+    		return NodeSet.EMPTY_SET;
+    	
         // the expression can be called multiple times, so we need to clear the previous preselectResult
         preselectResult = null;
         
@@ -200,6 +203,9 @@ public class DeprecatedExtRegexp extends Function implements Optimizable {
         if (contextItem != null)
 			contextSequence = contextItem.toSequence();
 
+        if (contextSequence != null && !contextSequence.isPersistentSet())
+        	return Sequence.EMPTY_SEQUENCE;
+        
         if (preselectResult == null && !checkForQNameIndex(contextSequence))
             contextQName = null;
 
