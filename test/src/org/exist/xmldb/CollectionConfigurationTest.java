81c75e8b166e3d717e58f0e8b2003b2c64d3eb36
Wolfgang Meier <wolfgangmm@gmail.com>
2011-01-07 23:14:38 +0000

    [ignore] Fixed wrong range index tests.
    svn path=/trunk/eXist/; revision=13517

--- test/src/org/exist/xmldb/CollectionConfigurationTest.java
+++ test/src/org/exist/xmldb/CollectionConfigurationTest.java
@@ -916,42 +916,42 @@ public class CollectionConfigurationTest extends TestCase {
 
            result = service.query("util:index-key-occurrences(/test/c, xs:dateTime(\"2002-12-07T12:20:46.275+01:00\") )");
            assertEquals(1, result.getSize());
-           assertEquals("2", result.getResource(0).getContent());
+           assertEquals("1", result.getResource(0).getContent());
 
            result = service.query("util:index-type(/test/c)");
            assertEquals("xs:dateTime", result.getResource(0).getContent());
 
            result = service.query("util:index-key-occurrences(/test/d, xs:double(1) )");
            assertEquals(1, result.getSize());
-           assertEquals("2", result.getResource(0).getContent());
+           assertEquals("1", result.getResource(0).getContent());
 
            result = service.query("util:index-type(/test/d)");
            assertEquals("xs:double", result.getResource(0).getContent());
 
            result = service.query("util:index-key-occurrences(/test/e, xs:float(1) )");
            assertEquals(1, result.getSize());
-           assertEquals("2", result.getResource(0).getContent());
+           assertEquals("1", result.getResource(0).getContent());
 
            result = service.query("util:index-type(/test/e)");
            assertEquals("xs:float", result.getResource(0).getContent());
 
            result = service.query("util:index-key-occurrences(/test/f, true())");
            assertEquals(1, result.getSize());
-           assertEquals("2", result.getResource(0).getContent());
+           assertEquals("1", result.getResource(0).getContent());
 
            result = service.query("util:index-type(/test/f)");
            assertEquals("xs:boolean", result.getResource(0).getContent());
 
            result = service.query("util:index-key-occurrences(/test/g, xs:integer(1))");
            assertEquals(1, result.getSize());
-           assertEquals("2", result.getResource(0).getContent());
+           assertEquals("1", result.getResource(0).getContent());
 
            result = service.query("util:index-type(/test/g)");
            assertEquals("xs:integer", result.getResource(0).getContent());
 
            result = service.query("util:index-key-occurrences(/test/h, '1')");
            assertEquals(1, result.getSize());
-           assertEquals("2", result.getResource(0).getContent());
+           assertEquals("1", result.getResource(0).getContent());
 
            result = service.query("util:index-type(/test/h)");
            assertEquals("xs:string", result.getResource(0).getContent());
