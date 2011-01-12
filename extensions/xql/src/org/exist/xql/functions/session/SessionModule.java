5c4b29d59a5d0a930baa3bb1fcebcce74267e46c
Dmitriy Shabanov <shabanovd@gmail.com>
2011-01-12 20:41:34 +0000

    [ignore] copy xquery engine (rev 13528)
    svn path=/trunk/eXist/; revision=13536

---
+++ extensions/xql/src/org/exist/xql/functions/session/SessionModule.java
@@ -0,0 +1,144 @@
+/*
+ * eXist Open Source Native XML Database
+ * Copyright (C) 2006-09 The eXist Project
+ * http://exist-db.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *  
+ *  $Id$
+ */
+package org.exist.xquery.functions.session;
+
+import org.exist.dom.QName;
+import org.exist.http.servlets.RequestWrapper;
+import org.exist.http.servlets.SessionWrapper;
+import org.exist.xquery.AbstractInternalModule;
+import org.exist.xquery.Function;
+import org.exist.xquery.FunctionDef;
+import org.exist.xquery.Variable;
+import org.exist.xquery.XPathException;
+import org.exist.xquery.XQueryContext;
+import org.exist.xquery.functions.request.RequestModule;
+import org.exist.xquery.value.JavaObjectValue;
+import org.exist.xquery.value.Type;
+
+/**
+ * Module function definitions for transform module.
+ *
+ * @author Wolfgang Meier (wolfgang@exist-db.org)
+ * @author Adam Retter (adam.retter@devon.gov.uk)
+ * @author Loren Cahlander
+ * @author ljo
+ */
+public class SessionModule extends AbstractInternalModule 
+{
+	public static final String NAMESPACE_URI = "http://exist-db.org/xquery/session";
+	public static final String PREFIX = "session";
+    public final static String INCLUSION_DATE = "2006-04-09";
+    public final static String RELEASED_IN_VERSION = "eXist-1.0";
+
+	public static final QName SESSION_VAR = new QName("session", NAMESPACE_URI, PREFIX);
+	
+	public static final FunctionDef[] functions = {
+		new FunctionDef( Create.signature, Create.class ),
+		new FunctionDef( Clear.signature, Clear.class ),
+		new FunctionDef( EncodeURL.signature, EncodeURL.class ),
+		new FunctionDef( GetID.signature, GetID.class ),
+		new FunctionDef( GetAttribute.signature, GetAttribute.class ),
+		new FunctionDef( RemoveAttribute.signature, RemoveAttribute.class ),
+		new FunctionDef( GetAttributeNames.signature, GetAttributeNames.class ),
+		new FunctionDef( GetCreationTime.signature, GetCreationTime.class ),
+		new FunctionDef( GetLastAccessedTime.signature, GetLastAccessedTime.class ),
+		new FunctionDef( GetMaxInactiveInterval.signature, GetMaxInactiveInterval.class ),
+		new FunctionDef( SetMaxInactiveInterval.signature, SetMaxInactiveInterval.class ),
+		new FunctionDef( Invalidate.signature, Invalidate.class ),
+		new FunctionDef( SetAttribute.signature, SetAttribute.class ) ,
+		new FunctionDef( SetCurrentUser.signature, SetCurrentUser.class ),
+		new FunctionDef( GetExists.signature, GetExists.class )
+	};
+	
+	public SessionModule() throws XPathException 
+	{
+		super( functions );
+		// predefined module global variables:
+		declareVariable( SESSION_VAR, null );
+	}
+
+	/* (non-Javadoc)
+	 * @see org.exist.xquery.Module#getDescription()
+	 */
+	public String getDescription() 
+	{
+		return "A module for dealing with the HTTP session."; 
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.exist.xquery.Module#getNamespaceURI()
+	 */
+	public String getNamespaceURI() 
+	{
+		return( NAMESPACE_URI );
+	}
+
+	/* (non-Javadoc)
+	 * @see org.exist.xquery.Module#getDefaultPrefix()
+	 */
+	public String getDefaultPrefix() 
+	{
+		return( PREFIX );
+	}
+
+    public String getReleaseVersion() {
+        return RELEASED_IN_VERSION;
+    }
+	
+	/**
+	 * Utility method to create a session and store it in the context as a variable
+	 
+	 * @param context
+	 */
+	static JavaObjectValue createSession( XQueryContext context, Function fn ) throws XPathException 
+	{
+		JavaObjectValue ret = null;
+		
+		RequestModule myModule = (RequestModule)context.getModule( RequestModule.NAMESPACE_URI );
+		
+		// request object is read from global variable $request
+		Variable var = myModule.resolveVariable( RequestModule.REQUEST_VAR );
+		
+		if( var == null || var.getValue() == null ) {
+			throw( new XPathException( fn, "No request object found in the current XQuery context." ) );
+		}
+	
+		if( var.getValue().getItemType() != Type.JAVA_OBJECT ) {
+			throw( new XPathException( fn, "Variable $request is not bound to an Java object." ) );
+		}
+
+		JavaObjectValue value = (JavaObjectValue)var.getValue().itemAt( 0 );
+		
+		if( value.getObject() instanceof RequestWrapper ) {
+			SessionModule  sessionModule 	= (SessionModule)context.getModule( SessionModule.NAMESPACE_URI );
+			SessionWrapper session 			= ((RequestWrapper)value.getObject()).getSession( true );
+			
+			sessionModule.declareVariable( SessionModule.SESSION_VAR, session );
+			ret = (JavaObjectValue)sessionModule.resolveVariable( SessionModule.SESSION_VAR ).getValue().itemAt( 0 );
+		} else {
+			throw( new XPathException( fn, "Variable $request is not bound to a Request object." ) );
+		}
+		
+		return( ret );
+	}
+
+}
---
+++ extensions/xql/src/org/exist/xql/functions/session/SetAttribute.java
@@ -0,0 +1,114 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2001-09 Wolfgang M. Meier
+ *  wolfgang@exist-db.org
+ *  http://exist.sourceforge.net
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  
+ *  $Id$
+ */
+package org.exist.xquery.functions.session;
+
+//import org.apache.log4j.Logger;
+import org.exist.dom.QName;
+import org.exist.http.servlets.SessionWrapper;
+import org.exist.xquery.Cardinality;
+import org.exist.xquery.Dependency;
+import org.exist.xquery.Function;
+import org.exist.xquery.FunctionSignature;
+import org.exist.xquery.Profiler;
+import org.exist.xquery.Variable;
+import org.exist.xquery.XPathException;
+import org.exist.xquery.XQueryContext;
+import org.exist.xquery.value.FunctionParameterSequenceType;
+import org.exist.xquery.value.Item;
+import org.exist.xquery.value.JavaObjectValue;
+import org.exist.xquery.value.Sequence;
+import org.exist.xquery.value.SequenceType;
+import org.exist.xquery.value.Type;
+
+/**
+ * @author Wolfgang Meier
+ * @author Loren Cahlander
+ */
+public class SetAttribute extends Function 
+{
+//	private static final Logger logger = Logger.getLogger(SetAttribute.class);
+	
+	public final static FunctionSignature signature =
+		new FunctionSignature(
+			new QName( "set-attribute", SessionModule.NAMESPACE_URI, SessionModule.PREFIX ),
+			"Stores a value in the current session using the supplied attribute name. If no session exists, then one will be created.",
+			new SequenceType[] {
+				new FunctionParameterSequenceType( "name", Type.STRING, Cardinality.EXACTLY_ONE, "The attribute name" ),
+				new FunctionParameterSequenceType( "value", Type.ITEM, Cardinality.ZERO_OR_MORE, "The value to be stored in the session by the attribute name" )
+			},
+			new SequenceType( Type.ITEM, Cardinality.EMPTY ) );
+	
+	
+	public SetAttribute( XQueryContext context ) 
+	{
+		super( context, signature );
+	}
+	
+	/* (non-Javadoc)
+	 * @see org.exist.xquery.Expression#eval(org.exist.dom.DocumentSet, org.exist.xquery.value.Sequence, org.exist.xquery.value.Item)
+	 */
+	public Sequence eval( Sequence contextSequence, Item contextItem ) throws XPathException 
+	{
+
+		JavaObjectValue session;
+		
+        if( context.getProfiler().isEnabled() ) {
+            context.getProfiler().start( this );  	
+            context.getProfiler().message( this, Profiler.DEPENDENCIES, "DEPENDENCIES", Dependency.getDependenciesName( this.getDependencies() ) );
+			
+            if( contextSequence != null ) {
+                context.getProfiler().message( this, Profiler.START_SEQUENCES, "CONTEXT SEQUENCE", contextSequence );
+			}
+
+            if( contextItem != null ) {
+                context.getProfiler().message( this, Profiler.START_SEQUENCES, "CONTEXT ITEM", contextItem.toSequence() );
+			}
+        }
+        
+		SessionModule myModule = (SessionModule)context.getModule( SessionModule.NAMESPACE_URI );
+
+		// session object is read from global variable $session
+		Variable var = myModule.resolveVariable( SessionModule.SESSION_VAR );
+		
+		if( var == null || var.getValue() == null ) {
+			// No saved session, so create one
+			session = SessionModule.createSession( context, this );
+		} else if( var.getValue().getItemType() != Type.JAVA_OBJECT ) {
+			throw( new XPathException( this, "Variable $session is not bound to a Java object." ) );
+		} else {
+			session = (JavaObjectValue)var.getValue().itemAt( 0 );
+		}
+		
+		// get attribute name parameter
+		String attribName = getArgument(0).eval( contextSequence, contextItem ).getStringValue();
+		Sequence attribValue = getArgument(1).eval( contextSequence, contextItem) ;
+		
+		if( session.getObject() instanceof SessionWrapper ) {
+			((SessionWrapper)session.getObject()).setAttribute (attribName, attribValue );
+		} else {
+			throw( new XPathException( this, "Type error: variable $session is not bound to a session object" ) );
+		}
+
+		return( Sequence.EMPTY_SEQUENCE );
+	}
+}
