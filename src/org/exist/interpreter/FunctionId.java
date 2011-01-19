cde658492dbcd71466625176e685f47297d49165
Dmitriy Shabanov <shabanovd@gmail.com>
2011-01-19 00:25:13 +0000

    [API] interpreter interfaces
    svn path=/trunk/eXist/; revision=13582
    
    
    Former-commit-id: d83ddfc0a218afb1a5f8061209e129c0d231d6ff

---
+++ src/org/exist/interpreter/FunctionId.java
@@ -0,0 +1,30 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2011 The eXist Project
+ *  http://exist-db.org
+ *  
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *  
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *  
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  
+ *  $Id$
+ */
+package org.exist.interpreter;
+
+/**
+ * Used to uniquely identify a function by its function name and arity.
+ *
+ */
+public interface FunctionId<T> extends Comparable<T> {
+
+}
