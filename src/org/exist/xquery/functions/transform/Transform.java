a273e0f77d6eb22a7cf744a690e3f928bc33d33d
Dmitriy Shabanov <shabanovd@gmail.com>
2011-02-07 07:10:51 +0000

    [ignore] fix NPE on unexist db resource
    svn path=/trunk/eXist/; revision=13734
    
    
    Former-commit-id: 60e7e810c3bbabee3e1086f18188efd444e68e76

--- src/org/exist/xquery/functions/transform/Transform.java
+++ src/org/exist/xquery/functions/transform/Transform.java
@@ -1,23 +1,22 @@
 /*
  *  eXist Open Source Native XML Database
- *  Copyright (C) 2001-09 Wolfgang M. Meier
- *  wolfgang@exist-db.org
- *  http://exist.sourceforge.net
- *  
+ *  Copyright (C) 2001-2011 The eXist Project
+ *  http://exist-db.org
+ *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License
  *  as published by the Free Software Foundation; either version 2
  *  of the License, or (at your option) any later version.
- *  
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Lesser General Public License for more details.
- *  
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *  
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
  *  $Id$
  */
 package org.exist.xquery.functions.transform;
@@ -54,7 +53,6 @@ import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
 
 import org.apache.log4j.Logger;
-import org.exist.collections.Collection;
 import org.exist.dom.DocumentImpl;
 import org.exist.dom.NodeProxy;
 import org.exist.dom.QName;
@@ -703,10 +701,7 @@ public class Transform extends BasicFunction {
 			}
 			if(xslDoc == null) {
 				LOG.debug("Document " + path + " not found");
-				Collection collection = context.getBroker().getCollection(uri);
-				if (collection !=null) {
-					
-				}
+			    throw new TransformerException("Resource " + path + " not found in database.");
 			}
 			if(!xslDoc.getPermissions().validate(context.getSubject(), Permission.READ))
 			    throw new TransformerException("Insufficient privileges to read resource " + path);
