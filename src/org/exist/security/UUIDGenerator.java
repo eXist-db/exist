2fd47d86149d04d9e2e83c02c23d62515e902512
Dmitriy Shabanov <shabanovd@gmail.com>
2011-02-12 18:43:33 +0000

    [ignore] update comments
    svn path=/trunk/eXist/; revision=13770
    
    
    Former-commit-id: ab69ce117f43279a29c0f0365fad8289fcff137d

--- src/org/exist/security/UUIDGenerator.java
+++ src/org/exist/security/UUIDGenerator.java
@@ -1,6 +1,6 @@
 /*
  *  eXist Open Source Native XML Database
- *  Copyright (C) 2001-2010 The eXist Project
+ *  Copyright (C) 2001-2011 The eXist Project
  *  http://exist-db.org
  *  
  *  This program is free software; you can redistribute it and/or
@@ -19,7 +19,6 @@
  *
  *  $Id$
  */
-
 package org.exist.security;
 
 import java.util.UUID;
