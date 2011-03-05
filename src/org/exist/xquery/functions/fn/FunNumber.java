3269f9e0540dad7f6ec81eee87ff70227788b1a8
Adam Retter <adam.retter@googlemail.com>
2011-03-05 16:11:37 +0000

    [ignore] fix formatting
    svn path=/trunk/eXist/; revision=13906

--- src/org/exist/xquery/functions/fn/FunNumber.java
+++ src/org/exist/xquery/functions/fn/FunNumber.java
@@ -38,81 +38,89 @@ import org.exist.xquery.value.SequenceType;
 import org.exist.xquery.value.Type;
 
 /**
- * xpath-library function: number(object)
+ * xpath-library function: number(object) $context/number()
  *
  */
 public class FunNumber extends Function {
 
-	protected static final String FUNCTION_DESCRIPTION_0_PARAM =    
+    protected static final String FUNCTION_DESCRIPTION_0_PARAM =    
         "Returns the value of the context item after atomization, " + 
         "converted to an xs:double.\n\n" +
         "If the context item cannot be " +
-		"converted to an xs:double, the xs:double value NaN is returned. " +
+                "converted to an xs:double, the xs:double value NaN is returned. " +
         "If the context item is undefined an error is raised: " + 
         "[err:XPDY0002]XP.\n\n";	
-	protected static final String FUNCTION_DESCRIPTION_1_PARAM =
-		"Returns the value indicated by $arg or, if $arg is not specified, " +
-		"the context item after atomization, converted to an xs:double.\n\n" +
-		"Calling the zero-argument version of the function is defined to " +
-		"give the same result as calling the single-argument version with " +
-		"the context item (.). That is, fn:number() is equivalent to fn:number(.).\n\n" +
-		"If $arg is the empty sequence or if $arg or the context item cannot be " +
-		"converted to an xs:double, the xs:double value NaN is returned. " +
-        "If the " +
-		"context item is undefined an error is raised: [err:XPDY0002]XP.\n\n" +
-		"If $arg is the empty sequence, NaN is returned. Otherwise, $arg, or " +
-		"the context item after atomization, is converted to an xs:double " +
-		"following the rules of 17.1.3.2 Casting to xs:double. If the conversion " +
-		"to xs:double fails, the xs:double value NaN is returned.";
-
-	protected static final FunctionParameterSequenceType ARG_PARAM = new FunctionParameterSequenceType("arg", Type.ATOMIC, Cardinality.ZERO_OR_ONE, "The input item");
-	protected static final FunctionReturnSequenceType RETURN_TYPE = new FunctionReturnSequenceType(Type.DOUBLE, Cardinality.EXACTLY_ONE, "the numerical value");
 	
-	public final static FunctionSignature signatures[] = {
-		new FunctionSignature(
-				new QName("number", Function.BUILTIN_FUNCTION_NS),
-				FUNCTION_DESCRIPTION_0_PARAM,
-				new SequenceType[0],
-				RETURN_TYPE
-		),
-		new FunctionSignature(
-			new QName("number", Function.BUILTIN_FUNCTION_NS),
-			FUNCTION_DESCRIPTION_1_PARAM,
-			new SequenceType[] { ARG_PARAM },
-			RETURN_TYPE
-		)
-	};
+    protected static final String FUNCTION_DESCRIPTION_1_PARAM =
+        "Returns the value indicated by $arg or, if $arg is not specified, " +
+        "the context item after atomization, converted to an xs:double.\n\n" +
+        "Calling the zero-argument version of the function is defined to " +
+        "give the same result as calling the single-argument version with " +
+        "the context item (.). That is, fn:number() is equivalent to fn:number(.).\n\n" +
+        "If $arg is the empty sequence or if $arg or the context item cannot be " +
+        "converted to an xs:double, the xs:double value NaN is returned. " +
+        "If the " +
+        "context item is undefined an error is raised: [err:XPDY0002]XP.\n\n" +
+        "If $arg is the empty sequence, NaN is returned. Otherwise, $arg, or " +
+        "the context item after atomization, is converted to an xs:double " +
+        "following the rules of 17.1.3.2 Casting to xs:double. If the conversion " +
+        "to xs:double fails, the xs:double value NaN is returned.";
 
-	public FunNumber(XQueryContext context, FunctionSignature signature) {
-		super(context, signature);
-	}
+    protected static final FunctionParameterSequenceType ARG_PARAM = new FunctionParameterSequenceType("arg", Type.ATOMIC, Cardinality.ZERO_OR_ONE, "The input item");
+    protected static final FunctionReturnSequenceType RETURN_TYPE = new FunctionReturnSequenceType(Type.DOUBLE, Cardinality.EXACTLY_ONE, "the numerical value");
 
-	public Sequence eval(Sequence contextSequence, Item contextItem) throws XPathException {
-        if (context.getProfiler().isEnabled()) {
+    public final static FunctionSignature signatures[] = {
+        new FunctionSignature(
+            new QName("number", Function.BUILTIN_FUNCTION_NS),
+            FUNCTION_DESCRIPTION_0_PARAM,
+            new SequenceType[0],
+            RETURN_TYPE
+        ),
+        new FunctionSignature(
+            new QName("number", Function.BUILTIN_FUNCTION_NS),
+            FUNCTION_DESCRIPTION_1_PARAM,
+            new SequenceType[] { ARG_PARAM },
+            RETURN_TYPE
+        )
+    };
+
+    public FunNumber(XQueryContext context, FunctionSignature signature) {
+        super(context, signature);
+    }
+
+    @Override
+    public Sequence eval(Sequence contextSequence, Item contextItem) throws XPathException {
+        if(context.getProfiler().isEnabled()) {
             context.getProfiler().start(this);       
             context.getProfiler().message(this, Profiler.DEPENDENCIES, "DEPENDENCIES", Dependency.getDependenciesName(this.getDependencies()));
-            if (contextSequence != null)
+            if(contextSequence != null) {
                 context.getProfiler().message(this, Profiler.START_SEQUENCES, "CONTEXT SEQUENCE", contextSequence);
-            if (contextItem != null)
+            }
+            if(contextItem != null) {
                 context.getProfiler().message(this, Profiler.START_SEQUENCES, "CONTEXT ITEM", contextItem.toSequence());
+            }
         }
         
-		if (contextItem != null)
-			contextSequence = contextItem.toSequence();
+        if(contextItem != null) {
+            contextSequence = contextItem.toSequence();
+        }
         
         Sequence result;
-		Sequence arg = null;
-		if(getSignature().getArgumentCount() == 1)
-			arg = getArgument(0).eval(contextSequence);
-		else
-			arg = contextSequence;
-		
-		if (arg == null)
-			throw new XPathException(this, "XPDY0002: Undefined context item");
-		        
-		if(arg.isEmpty())
-			result = DoubleValue.NaN;
-		else {
+        Sequence arg = null;
+        
+        if(getSignature().getArgumentCount() == 1) {
+            arg = getArgument(0).eval(contextSequence);
+        } else {
+            arg = contextSequence;
+        }
+
+        if(arg == null) {
+            throw new XPathException(this, "XPDY0002: Undefined context item");
+        }
+
+        if(arg.isEmpty()) {
+            result = DoubleValue.NaN;
+        } else {
             try {
                 result = arg.convertTo(Type.DOUBLE);
             } catch(XPathException e) {
@@ -120,10 +128,11 @@ public class FunNumber extends Function {
             }
         }
         
-        if (context.getProfiler().isEnabled()) 
+        if(context.getProfiler().isEnabled()) {
             context.getProfiler().end(this, "", result); 
+        }
         
         return result; 
         
-	}
+    }
 }
