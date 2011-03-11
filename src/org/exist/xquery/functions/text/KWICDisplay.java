7cdbcb20ed176e0cbbae0e5d7e26f56b518c57de
Adam Retter <adam.retter@googlemail.com>
2011-03-11 13:33:50 +0000

    [ignore] Reverted some comments changes in rev 13974. Somehow the NetBeans refactor tool was a bit over-zealous
    svn path=/trunk/eXist/; revision=13975
    
    
    Former-commit-id: a979c118c9a16102e7df04376104dba9df205eda

--- src/org/exist/xquery/functions/text/KWICDisplay.java
+++ src/org/exist/xquery/functions/text/KWICDisplay.java
@@ -198,7 +198,7 @@ public class KWICDisplay extends BasicFunction {
             int pos = 0;
             int lastNodeNr = -1;
             
-            // prepareForExecution array for callback function arguments
+            // prepare array for callback function arguments
             Sequence params[] = new Sequence[callback.getSignature().getArgumentCount()];
             params[1] = firstProxy;
             params[2] = extraArgs;
