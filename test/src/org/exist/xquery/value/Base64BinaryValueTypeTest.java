27c9bf5a61e51c2be5f477f8be20fde1ca39eb84
Adam Retter <adam.retter@googlemail.com>
2011-03-11 09:49:38 +0000

    [bugfix] Verifying the format of pre-encoded base64 binary string values did not work on large strings. Corrected the Regular Expression used for verification.
    However text() node handling from persistent storage in XQuery could be optimised...
    
    svn path=/trunk/eXist/; revision=13972
    
    
    Former-commit-id: 1391a93b7816f095ac790d2424968090cc8c82c2

--- test/src/org/exist/xquery/value/Base64BinaryValueTypeTest.java
+++ test/src/org/exist/xquery/value/Base64BinaryValueTypeTest.java
@@ -1,7 +1,15 @@
 package org.exist.xquery.value;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import org.apache.commons.codec.binary.Base64InputStream;
+import org.exist.util.ConfigurationHelper;
 import org.exist.xquery.XPathException;
 import org.junit.Test;
+import static org.junit.Assert.assertNotNull;
 
 /**
  *
@@ -31,6 +39,41 @@ public class Base64BinaryValueTypeTest {
     public void verify_validBase64_passes_2() throws XPathException {
         TestableBase64BinaryValueType base64Type = new TestableBase64BinaryValueType();
         base64Type.verifyString("dGVzdCBkYXRh");
+    }
+
+    @Test
+    public void verify_validBase64_passes_3() throws XPathException {
+        TestableBase64BinaryValueType base64Type = new TestableBase64BinaryValueType();
+        base64Type.verifyString("aaa a");
+    }
+
+    @Test
+    public void verify_validBase64_passes_large_string() throws XPathException, IOException {
+        File home = ConfigurationHelper.getExistHome();
+        File binaryFile = new File(home, "webapp/logo.jpg");
+
+        InputStream is = null;
+        ByteArrayOutputStream baos = null;
+        String base64data = null;
+        try {
+            is = new Base64InputStream(new FileInputStream(binaryFile), true, -1, null);
+            baos  = new ByteArrayOutputStream();
+            byte buf[] = new byte[1024];
+            int read = -1;
+            while((read = is.read(buf)) > -1) {
+                baos.write(buf, 0, read);
+            }
+            base64data = new String(baos.toByteArray());
+        } finally {
+            if(is != null) { is.close(); }
+            if(baos != null) { baos.close(); }
+        }
+
+        assertNotNull(base64data);
+
+        TestableBase64BinaryValueType base64Type = new TestableBase64BinaryValueType();
+
+        base64Type.verifyString(base64data);
     }
 
     public class TestableBase64BinaryValueType extends Base64BinaryValueType {
