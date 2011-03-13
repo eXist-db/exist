3123106b3aaa8050014b74a34df64b6fa6c8cc1a
Dannes Wessels <dannes@exist-db.org>
2011-03-13 12:32:31 +0000

    [ignore] Added deprecation message to util:catch() function
    svn path=/trunk/eXist/; revision=13992
    
    
    Former-commit-id: 9a71eca42b15fc52ac3dd158c03a516f72cd367b

--- src/org/exist/xquery/functions/util/CatchFunction.java
+++ src/org/exist/xquery/functions/util/CatchFunction.java
@@ -63,7 +63,9 @@ public class CatchFunction extends Function {
 					new FunctionParameterSequenceType("try-code-blocks", Type.ITEM, Cardinality.ZERO_OR_MORE, "The code blocks that will be put inside of a the try part of the try-catch statement."),
 					new FunctionParameterSequenceType("catch-code-blocks", Type.ITEM, Cardinality.ZERO_OR_MORE, "The code blocks that will be will called if the catch matches one of the $java-classnames")
 			},
-			new FunctionReturnSequenceType(Type.ITEM, Cardinality.ZERO_OR_MORE, "the results from the try-catch"));
+			new FunctionReturnSequenceType(Type.ITEM, Cardinality.ZERO_OR_MORE, "the results from the try-catch"),
+            "Use the XQuery 3.0 try/catch expression in stead."
+            );
     
     /**
      * @param context
