5f823083ca929aa5eac7cf88eaa7ed20b8ac5273
Adam Retter <adam.retter@googlemail.com>
2011-04-07 13:20:31 +0000

    Merged JSON Serializer fixes from trunk
    svn path=/stable/eXist-1.4.x/; revision=14116
    
    
    Former-commit-id: aff4a8dbc9fa7b2ae77e9ec140637d01fe0483c1

--- src/org/exist/util/serializer/json/JSONValue.java
+++ src/org/exist/util/serializer/json/JSONValue.java
@@ -7,28 +7,67 @@ public class JSONValue extends JSONNode {
 	
 	public final static String NAME_VALUE = "#text";
 	
-	private String content;
+	private String content = null;
 	
 	public JSONValue(String content) {
 		super(Type.VALUE_TYPE, NAME_VALUE);
-		this.content = content;
+		this.content = escape(content);
 	}
 
+	public JSONValue() {
+		super(Type.VALUE_TYPE, NAME_VALUE);
+	}
+
+	public void addContent(String str) {
+		if (content == null)
+			content = str;
+		else
+			content += str;
+	}
+	
 	@Override
 	public void serialize(Writer writer, boolean isRoot) throws IOException {
-		serializeContent(writer);		
+		if (getNextOfSame() != null) {
+			writer.write("[");
+			JSONNode next = this;
+			while (next != null) {
+				next.serializeContent(writer);
+				next = next.getNextOfSame();
+				if (next != null)
+					writer.write(", ");
+			}
+			writer.write("]");
+		} else
+			serializeContent(writer);		
 	}
 
 	@Override
 	public void serializeContent(Writer writer) throws IOException {
-		if (getSerializationType() == SerializationType.AS_STRING)
+		if (getSerializationType() != SerializationType.AS_LITERAL)
 			writer.write('"');
-		JSONNode next = this;
-		while (next != null) {
-			writer.write(content);
-			next = next.getNextOfSame();
+		writer.write(content);
+		if (getSerializationType() != SerializationType.AS_LITERAL)
+			writer.write('"');
+	}
+	
+	protected static String escape(String str) {
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < str.length(); i++) {
+			char ch = str.charAt(i);
+			switch (ch) {
+			case '\n':
+				builder.append("\\n");
+				break;
+			case '\r':
+				break;
+			case '"':
+				builder.append("\\\"");
+				break;
+			default:
+				builder.append(ch);
+				break;
+			}
 		}
-		if (getSerializationType() == SerializationType.AS_STRING)
-			writer.write('"');
+		return builder.toString();
 	}
 }
