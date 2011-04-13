6d19d6f52380d854779ab26dd85053a4ba14984e
Dmitriy Shabanov <shabanovd@gmail.com>
2011-04-13 03:13:22 +0000

    [test] this test case reported by Csaba Dery
    svn path=/trunk/eXist/; revision=14187

---
+++ test/src/xquery/or/OrTests.java
@@ -0,0 +1,11 @@
+package xquery.or;
+
+import xquery.TestRunner;
+
+public class OrTests extends TestRunner {
+
+    @Override
+    protected String getDirectory() {
+        return "test/src/xquery/or";
+    }
+}
