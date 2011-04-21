e21379bfed4ca692065d8f413bb9f281e97b7a8d
Claudius Teodorescu <claudius108>
2011-04-21 06:58:19 +0000

    Included encrypt-string() and decrypt-string() functions.
    svn path=/trunk/eXist/; revision=14258
    
    
    Former-commit-id: 4367e4a21b852dd054f76410fbf9f45c3a55ad26

--- extensions/modules/src/ro/kuberam/xcrypt/ValidateSignatureFunction.java
+++ extensions/modules/src/ro/kuberam/xcrypt/ValidateSignatureFunction.java
@@ -7,11 +7,10 @@
  */
 package ro.kuberam.xcrypt;
 
+import java.util.logging.Level;
 import org.apache.log4j.Logger;
 import java.io.IOException;
 import java.io.StringReader;
-import java.io.StringWriter;
-import java.util.HashMap;
 import java.util.Properties;
 import javax.xml.parsers.DocumentBuilderFactory;
 import javax.xml.parsers.DocumentBuilder;
@@ -31,11 +30,10 @@ import org.xml.sax.InputSource;
 import org.xml.sax.SAXException;
 import org.exist.xquery.value.FunctionReturnSequenceType;
 import org.exist.storage.serializers.Serializer;
-import org.exist.util.serializer.SAXSerializer;
-import org.exist.util.serializer.SerializerPool;
 import org.exist.xquery.value.BooleanValue;
 import org.exist.xquery.value.NodeValue;
-import org.exist.xquery.value.SequenceIterator;
+import org.xml.sax.SAXNotRecognizedException;
+import org.xml.sax.SAXNotSupportedException;
 
 /**
  * Cryptographic extension functions.
@@ -51,21 +49,55 @@ public class ValidateSignatureFunction extends BasicFunction {
 			new QName("validate-signature", XcryptModule.NAMESPACE_URI, XcryptModule.PREFIX),
 			"Validate an XML digital signature.",
 			new SequenceType[] {
-                            new FunctionParameterSequenceType("input-doc", Type.NODE, Cardinality.EXACTLY_ONE, "The signed document.")                        },
+                            new FunctionParameterSequenceType("input-doc", Type.NODE, Cardinality.EXACTLY_ONE, "The enveloped, enveloping, or detached signature.")                        },
 			new FunctionReturnSequenceType(Type.BOOLEAN, Cardinality.EXACTLY_ONE, "boolean value true() if the signature is valid, otherwise return value false()."));
 
 	public ValidateSignatureFunction(XQueryContext context, FunctionSignature signature) {
 		super(context, signature);
 	}
 
+	protected final static Properties defaultOutputKeysProperties = new Properties();
+
+	static {
+		defaultOutputKeysProperties.setProperty(OutputKeys.INDENT, "no");
+		defaultOutputKeysProperties.setProperty(OutputKeys.OMIT_XML_DECLARATION, "yes");
+		defaultOutputKeysProperties.setProperty(OutputKeys.ENCODING, "UTF-8");
+	}
+
 	public Sequence eval(Sequence[] args, Sequence contextSequence) throws XPathException {
             if( args[0].isEmpty() ){
                 return Sequence.EMPTY_SEQUENCE;
             }
-            //get  and process the input document or node to string
+
             String inputNodeSerialized = "";
             try {
                 inputNodeSerialized = SerializeXML.serialize( args[0].iterate(), context );
+            } catch (SAXException ex) {
+                throw new XPathException( ex.getMessage() );
+            }
+
+
+            //get  and process the input document or node to InputStream, in order to be transformed into DOM Document
+            Serializer serializer = context.getBroker().getSerializer();
+            serializer.reset();
+            Properties outputProperties = new Properties( defaultOutputKeysProperties );
+            try {
+                serializer.setProperties(outputProperties);
+            } catch (SAXNotRecognizedException ex) {
+            java.util.logging.Logger.getLogger(ValidateSignatureFunction.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (SAXNotSupportedException ex) {
+            java.util.logging.Logger.getLogger(ValidateSignatureFunction.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            //NodeValue inputNode = (NodeValue)args[0].itemAt(0);
+            //InputStream inputNodeStream = new NodeInputStream( serializer, inputNode );
+
+
+
+
+            //System.out.println( "inputNodeSerialized: " + inputNodeSerialized + "\n" );
+
+            try {
+                System.out.println("inputDOMDoc for validating: " + serializer.serialize( (NodeValue)args[0].itemAt(0) ) + "\n");
             } catch (SAXException ex) {}
 
             //initialize the document builder
@@ -79,15 +111,17 @@ public class ValidateSignatureFunction extends BasicFunction {
             //process the input string to DOM document
             org.w3c.dom.Document inputDOMDoc = null;
             try {
-                    inputDOMDoc = db.parse(new InputSource(new StringReader( inputNodeSerialized )));
-                } catch (SAXException ex) {
-                    ex.getMessage();
-                } catch (IOException ex) {
-                    ex.getMessage();
-                }
+                //inputDOMDoc = db.parse( inputNodeStream );
+                //inputDOMDoc = db.parse(new InputSource(new StringReader( inputNodeSerialized )));
+                inputDOMDoc = db.parse( new InputSource( new StringReader( serializer.serialize( (NodeValue)args[0].itemAt(0) ) ) ) );
+            } catch (SAXException ex) {
+                ex.getMessage();
+            } catch (IOException ex) {
+                ex.getMessage();
+            }
 
             //validate the document
-            Boolean isValid = null;
+            Boolean isValid = false;
             try {
                 isValid = ValidateSignature.ValidateDigitalSignature( inputDOMDoc );
             } catch (Exception ex) {
