68cb9d301858a58c2ae9e63bbf69586fb7a21e93
Dmitriy Shabanov <shabanovd@gmail.com>
2011-05-16 13:01:07 +0000

    [ignore] add svn command 'add'  (xquery function)
    svn path=/trunk/eXist/; revision=14439
    
    
    Former-commit-id: 93b5319f6d647fea4d01b3347e0c395ffd61b4ec

---
+++ extensions/svn/src/org/exist/versioning/svn/xquery/SVNAdd.java
@@ -0,0 +1,77 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2010 The eXist Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  $Id$
+ */
+package org.exist.versioning.svn.xquery;
+
+import org.exist.dom.QName;
+import org.exist.versioning.svn.Resource;
+import org.exist.versioning.svn.WorkingCopy;
+import org.exist.xquery.BasicFunction;
+import org.exist.xquery.Cardinality;
+import org.exist.xquery.FunctionSignature;
+import org.exist.xquery.XPathException;
+import org.exist.xquery.XQueryContext;
+import org.exist.xquery.value.FunctionParameterSequenceType;
+import org.exist.xquery.value.FunctionReturnSequenceType;
+import org.exist.xquery.value.Sequence;
+import org.exist.xquery.value.SequenceType;
+import org.exist.xquery.value.Type;
+import org.tmatesoft.svn.core.SVNException;
+import org.tmatesoft.svn.core.wc.SVNRevision;
+
+/**
+ * Collects information on local path(s). Like 'svn add' command.
+ * 
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ */
+public class SVNAdd extends BasicFunction {
+
+	public final static FunctionSignature signature =
+		new FunctionSignature(
+			new QName("add", SVNModule.NAMESPACE_URI, SVNModule.PREFIX), "Puts directories and files under version control scheduling them for addition to a repository.",
+			new SequenceType[] {
+                new FunctionParameterSequenceType("path", Type.ANY_URI, Cardinality.EXACTLY_ONE, "A local entry for which info will be collected")
+            },
+            new FunctionReturnSequenceType(Type.EMPTY, Cardinality.ZERO, ""));
+
+	public SVNAdd(XQueryContext context) {
+		super(context, signature);
+	}
+
+	@Override
+	public Sequence eval(Sequence[] args, Sequence contextSequence) throws XPathException {
+        
+		WorkingCopy wc = new WorkingCopy("", "");
+        
+        String uri = args[0].getStringValue();
+       
+        Resource wcURI = new Resource(uri);
+    	try {
+			wc.addEntry(wcURI);;
+		} catch (SVNException svne) {
+			throw new XPathException(this,
+					"error while adding location '" + uri + "'", svne);
+		}
+		
+		return Sequence.EMPTY_SEQUENCE;
+	}
+
+}
