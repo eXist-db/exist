1900e645d23e6eaf633024e32ffc68225da63e6d
Dmitriy Shabanov <shabanovd@gmail.com>
2011-05-18 04:57:50 +0000

    [ignore] missing 'add' function signature @SVNModule
    svn path=/trunk/eXist/; revision=14473

--- extensions/svn/src/org/exist/versioning/svn/xquery/SVNModule.java
+++ extensions/svn/src/org/exist/versioning/svn/xquery/SVNModule.java
@@ -43,6 +43,7 @@ public class SVNModule extends AbstractInternalModule {
 
 
 	private final static FunctionDef[] functions = {
+        	new FunctionDef(SVNAdd.signature, SVNCleanup.class),
             new FunctionDef(SVNCleanup.signature, SVNCleanup.class),
             new FunctionDef(SVNCheckOut.signature[0], SVNCheckOut.class),
             new FunctionDef(SVNCheckOut.signature[1], SVNCheckOut.class),
