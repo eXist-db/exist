9a3c6c92426cc2cb579509c930051fa25c774b53
Rob Walpole <robkwalpole@gmail.com>
2011-06-13 21:50:09 +0000

    Extension module with functions for encoding and decoding the EXI (Efficient XML Interchange) format [feature]
    svn path=/trunk/eXist/; revision=14680
    
    
    Former-commit-id: 887840ced059bbe635107e612e29f126cf5ccaef

---
+++ extensions/modules/src/org/exist/xquery/modules/exi/EncodeExiFunction.java
@@ -0,0 +1,92 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2011 The eXist Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package org.exist.xquery.modules.exi;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.util.Properties;
+
+import org.exist.dom.QName;
+import org.exist.util.serializer.EXISerializer;
+import org.exist.xquery.BasicFunction;
+import org.exist.xquery.Cardinality;
+import org.exist.xquery.ErrorCodes.JavaErrorCode;
+import org.exist.xquery.FunctionSignature;
+import org.exist.xquery.XPathException;
+import org.exist.xquery.XQueryContext;
+import org.exist.xquery.value.Base64BinaryValueType;
+import org.exist.xquery.value.BinaryValueFromInputStream;
+import org.exist.xquery.value.FunctionParameterSequenceType;
+import org.exist.xquery.value.FunctionReturnSequenceType;
+import org.exist.xquery.value.Item;
+import org.exist.xquery.value.Sequence;
+import org.exist.xquery.value.SequenceType;
+import org.exist.xquery.value.Type;
+import org.xml.sax.SAXException;
+
+import com.siemens.ct.exi.exceptions.EXIException;
+
+/**
+ * eXist EXI Module Extension EncodeExiFunction.
+ * 
+ * Encodes an XML source in Efficient XML Interchange (EXI) format.
+ * 
+ * @author Rob Walpole
+ * @version 1.0
+ *
+ */
+public class EncodeExiFunction extends BasicFunction {
+
+    public final static FunctionSignature signature = 
+		new FunctionSignature(
+				new QName("encode-from-xml", ExiModule.NAMESPACE_URI, ExiModule.PREFIX),
+				"A function which returns encoded EXI from an XML source.",
+				new SequenceType[] {
+					new FunctionParameterSequenceType("source-xml", Type.NODE, Cardinality.EXACTLY_ONE, "the XML source")},
+				new FunctionReturnSequenceType(Type.BASE64_BINARY, Cardinality.ZERO_OR_ONE, "the binary EXI result document"));
+
+    public EncodeExiFunction(XQueryContext context, FunctionSignature signature) {
+		super(context, signature);
+	}
+
+	@Override
+	public Sequence eval(Sequence[] args, Sequence contextSequence)
+			throws XPathException {
+		if(args[0].isEmpty()) {
+            return Sequence.EMPTY_SEQUENCE;
+        }
+		try {
+			Item inputNode = args[0].itemAt(0);
+			ByteArrayOutputStream baos = new ByteArrayOutputStream();
+			EXISerializer exiSerializer = new EXISerializer(baos);
+			exiSerializer.startDocument();
+	        inputNode.toSAX(context.getBroker(), exiSerializer, new Properties());
+	        exiSerializer.endDocument();
+	        return BinaryValueFromInputStream.getInstance(context, new Base64BinaryValueType(), new ByteArrayInputStream(baos.toByteArray()));
+		}
+		catch(EXIException exie) {
+			throw new XPathException(this, new JavaErrorCode(exie.getCause()), exie.getMessage());
+		}
+		catch(SAXException saxe) {
+			throw new XPathException(this, new JavaErrorCode(saxe.getCause()), saxe.getMessage());
+		}
+	}
+
+}
---
+++ extensions/modules/src/org/exist/xquery/modules/exi/ExiModule.java
@@ -0,0 +1,68 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2011 The eXist Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package org.exist.xquery.modules.exi;
+
+import java.util.List;
+import java.util.Map;
+
+import org.exist.xquery.AbstractInternalModule;
+import org.exist.xquery.FunctionDef;
+
+/**
+ * XQuery Extension module for Efficient XML Interchange (EXI) processing.
+ *
+ * @author Rob Walpole
+ * @version 1.0
+ */
+public class ExiModule extends AbstractInternalModule {
+	
+	public final static String NAMESPACE_URI = "http://exist-db.org/xquery/exi";
+	
+	public final static String PREFIX = "exi";
+    public final static String INCLUSION_DATE = "2011-06-09";
+    public final static String RELEASED_IN_VERSION = "eXist-1.5";
+    
+    // TODO add functions for schema encoding/decoding
+    private final static FunctionDef[] functions = {
+		new FunctionDef(EncodeExiFunction.signature, EncodeExiFunction.class),
+		new FunctionDef(DecodeExiFunction.signature, DecodeExiFunction.class)
+	};
+	
+	public ExiModule(Map<String, List<? extends Object>> parameters) {
+		super(functions, parameters);
+	}
+
+	public String getNamespaceURI() {
+		return NAMESPACE_URI;
+	}
+
+	public String getDefaultPrefix() {
+		return PREFIX;
+	}
+
+	public String getDescription() {
+		return "A module for working with the EXI (Efficient XML Interchange) format.";
+	}
+
+    public String getReleaseVersion() {
+        return RELEASED_IN_VERSION;
+    }
+    
+}
