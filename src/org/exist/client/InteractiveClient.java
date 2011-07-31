007392ca2fb5da9d37b78a9c76c7738879214bb2
Leif-Jöran Olsson <ljo@fripost.org>
2011-07-31 10:49:03 +0000

    [ignore] javaclient review changes before 1.4.1 release.
    svn path=/stable/eXist-1.4.x/; revision=15014
    
    
    Former-commit-id: ee330b84cbfdf13644579d4aaca988acccec14c0

--- src/org/exist/client/InteractiveClient.java
+++ src/org/exist/client/InteractiveClient.java
@@ -1,23 +1,23 @@
 /*
- *  eXist Open Source Native XML Database
- *  Copyright (C) 2001-07 The eXist Project
- *  http://exist-db.org
+ * eXist Open Source Native XML Database
+ * Copyright (C) 2001-2011 The eXist Project
+ * http://exist-db.org
  *
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public License
- *  as published by the Free Software Foundation; either version 2
- *  of the License, or (at your option) any later version.
- *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *
- *  You should have received a copy of the GNU Lesser General Public
- *  License along with this library; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
- *
- * $Id$
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *  
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *  
+ *  $Id$
  */
 package org.exist.client;
 
@@ -147,7 +147,7 @@ public class InteractiveClient {
     public static final String DRIVER="driver";
     
     // values
-    protected static String EDIT_CMD = "xemacs $file";
+    protected static String EDIT_CMD = "emacsclient -t $file";
     protected static String ENCODING = "ISO-8859-1";
     protected static String PASS = null;
     protected static String URI_DEFAULT = "xmldb:exist://localhost:8080/exist/xmlrpc";
@@ -2612,8 +2612,8 @@ public class InteractiveClient {
         
         messageln(sysProperties.getProperty("product-name") + " version " 
                 + sysProperties.getProperty("product-version") 
-                +", Copyright (C) 2001-2009 Wolfgang Meier");
-        messageln("eXist comes with ABSOLUTELY NO WARRANTY.");
+                +", Copyright (C) 2001-2011 The eXist Project");
+        messageln("eXist-db comes with ABSOLUTELY NO WARRANTY.");
         messageln("This is free software, and you are welcome to "
                 + "redistribute it\nunder certain conditions; "
                 + "for details read the license file.\n");
