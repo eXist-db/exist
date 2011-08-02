ecf154444b8f332e420ff64ad76f2a9c093951a7
Wolfgang Meier <wolfgangmm@gmail.com>
2011-08-02 08:09:25 +0000

    [ignore] Forgot to add a class, sorry.
    svn path=/trunk/eXist/; revision=15031
    
    
    Former-commit-id: 5bf6635a1c2184656b2a21e3b37e18802b81cdb6

---
+++ extensions/indexes/lucene/src/org/exist/xquery/modules/lucene/GetField.java
@@ -0,0 +1,68 @@
+package org.exist.xquery.modules.lucene;
+
+import java.io.IOException;
+
+import org.exist.dom.DocumentImpl;
+import org.exist.dom.QName;
+import org.exist.indexing.lucene.LuceneIndex;
+import org.exist.indexing.lucene.LuceneIndexWorker;
+import org.exist.security.PermissionDeniedException;
+import org.exist.storage.lock.Lock;
+import org.exist.xmldb.XmldbURI;
+import org.exist.xquery.BasicFunction;
+import org.exist.xquery.Cardinality;
+import org.exist.xquery.ErrorCodes;
+import org.exist.xquery.FunctionSignature;
+import org.exist.xquery.XPathException;
+import org.exist.xquery.XQueryContext;
+import org.exist.xquery.value.FunctionParameterSequenceType;
+import org.exist.xquery.value.FunctionReturnSequenceType;
+import org.exist.xquery.value.Sequence;
+import org.exist.xquery.value.SequenceType;
+import org.exist.xquery.value.Type;
+
+public class GetField extends BasicFunction {
+
+	public final static FunctionSignature signatures[] = {
+        new FunctionSignature(
+            new QName("get-field", LuceneModule.NAMESPACE_URI, LuceneModule.PREFIX),
+            "Retrieve the stored content of a field.",
+            new SequenceType[]{
+                new FunctionParameterSequenceType("path", Type.STRING, Cardinality.ZERO_OR_MORE,
+                "URI paths of documents or collections in database. Collection URIs should end on a '/'."),
+                new FunctionParameterSequenceType("field", Type.STRING, Cardinality.EXACTLY_ONE,
+                "query string")
+            },
+	        new FunctionReturnSequenceType(Type.STRING, Cardinality.ZERO_OR_MORE,
+	    		"All documents that are match by the query"))
+    };
+	
+	public GetField(XQueryContext context, FunctionSignature signature) {
+		super(context, signature);
+	}
+	
+	@Override
+	public Sequence eval(Sequence[] args, Sequence contextSequence)
+			throws XPathException {
+		XmldbURI uri = XmldbURI.createInternal(args[0].getStringValue());
+		String field = args[1].getStringValue();
+		
+		DocumentImpl doc = null;
+		try {
+			doc = context.getBroker().getXMLResource(uri, Lock.READ_LOCK);
+			
+			// Get the lucene worker
+            LuceneIndexWorker index = (LuceneIndexWorker) context.getBroker().getIndexController().getWorkerByIndexId(LuceneIndex.ID);
+            String content = index.getFieldContent(context, doc.getDocId(), field);
+            return content == null ? Sequence.EMPTY_SEQUENCE : new org.exist.xquery.value.StringValue(content);
+		} catch (PermissionDeniedException e) {
+			throw new XPathException(this, ErrorCodes.EXXQDY0004, "Permission denied to read document " + args[0].getStringValue());
+		} catch (IOException e) {
+			throw new XPathException(this, ErrorCodes.EXXQDY0004, "IO error while reading document " + args[0].getStringValue());
+		} finally {
+			if (doc != null)
+				doc.getUpdateLock().release(Lock.READ_LOCK);
+		}
+	}
+
+}
