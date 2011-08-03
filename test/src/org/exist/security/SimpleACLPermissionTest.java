68cea40da872c89cec7b97b00f0be47f7e209819
Adam Retter <adam.retter@googlemail.com>
2011-08-03 19:50:31 +0000

    [bugfix] Only last ACE was preserved, others were zero'd. Also accompanying test case
    svn path=/trunk/eXist/; revision=15055

--- test/src/org/exist/security/SimpleACLPermissionTest.java
+++ test/src/org/exist/security/SimpleACLPermissionTest.java
@@ -1,11 +1,16 @@
 package org.exist.security;
 
+import org.exist.storage.io.VariableByteInputStream;
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import org.exist.storage.io.VariableByteOutputStream;
 import org.exist.Database;
 import org.exist.security.ACLPermission.ACE_TARGET;
 import org.exist.security.ACLPermission.ACE_ACCESS_TYPE;
 import org.exist.security.internal.SecurityManagerImpl;
 import java.util.Random;
 import org.easymock.EasyMock;
+import org.exist.util.ByteArray;
 import org.junit.Test;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
@@ -45,6 +50,7 @@ public class SimpleACLPermissionTest {
         
         assertEquals(1, permission.getACECount());
         assertEquals(ACE_ACCESS_TYPE.ALLOWED, permission.getACEAccessType(0));
+        assertEquals(ACE_TARGET.USER, permission.getACETarget(0));
         assertEquals(userId, permission.getACEId(0));
         assertEquals(mode, permission.getACEMode(0));
     }
@@ -593,5 +599,70 @@ public class SimpleACLPermissionTest {
         verify(mockSecurityManager, mockUser);
 
         assertTrue(isValid);
+    }
+    
+    @Test
+    public void roundtrip_write_read() throws PermissionDeniedException, IOException {
+        
+        final SecurityManager mockSecurityManager = EasyMock.createMock(SecurityManager.class);
+        final Database mockDatabase = EasyMock.createMock(Database.class);
+        final Subject mockCurrentSubject = EasyMock.createMock(Subject.class);
+        
+        replay(mockSecurityManager, mockDatabase, mockCurrentSubject);
+        
+        SimpleACLPermission permission = new SimpleACLPermission(mockSecurityManager);
+        
+        assertEquals(0, permission.getACECount());
+
+        final int userId1 = 1;
+        final int mode1 = ALL;
+        permission.addUserACE(ACE_ACCESS_TYPE.ALLOWED, userId1, mode1);
+        
+        final int groupId2 = 2;
+        final int mode2 = Permission.READ;
+        permission.addGroupACE(ACE_ACCESS_TYPE.DENIED, groupId2, mode2);
+        
+        final VariableByteOutputStream os = new VariableByteOutputStream();
+        
+        //write the acl out
+        permission.write(os);
+        
+        verify(mockSecurityManager, mockDatabase, mockCurrentSubject);
+        
+        assertEquals(2, permission.getACECount());
+        
+        assertEquals(ACE_ACCESS_TYPE.ALLOWED, permission.getACEAccessType(0));
+        assertEquals(userId1, permission.getACEId(0));
+        assertEquals(ACE_TARGET.USER, permission.getACETarget(0));
+        assertEquals(mode1, permission.getACEMode(0));
+        
+        assertEquals(ACE_ACCESS_TYPE.DENIED, permission.getACEAccessType(1));
+        assertEquals(groupId2, permission.getACEId(1));
+        assertEquals(ACE_TARGET.GROUP, permission.getACETarget(1));
+        assertEquals(mode2, permission.getACEMode(1));
+        
+        //get the written acl data
+        ByteArray buf = os.data();
+        byte data[] = new byte[buf.size()];
+        buf.copyTo(data, 0);
+        
+        
+        //create a new permission instance
+        SimpleACLPermission permission2 = new SimpleACLPermission(mockSecurityManager);
+        
+        //read the acl in
+        permission2.read(new VariableByteInputStream(new ByteArrayInputStream(data)));
+        
+        assertEquals(2, permission2.getACECount());
+        
+        assertEquals(ACE_ACCESS_TYPE.ALLOWED, permission2.getACEAccessType(0));
+        assertEquals(userId1, permission2.getACEId(0));
+        assertEquals(ACE_TARGET.USER, permission2.getACETarget(0));
+        assertEquals(mode1, permission2.getACEMode(0));
+        
+        assertEquals(ACE_ACCESS_TYPE.DENIED, permission2.getACEAccessType(1));
+        assertEquals(groupId2, permission2.getACEId(1));
+        assertEquals(ACE_TARGET.GROUP, permission2.getACETarget(1));
+        assertEquals(mode2, permission2.getACEMode(1));
     }
 }
