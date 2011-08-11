1a7a82e63645065a13a99c7b54af5d55717fc5e8
Adam Retter <adam.retter@googlemail.com>
2011-08-11 09:42:16 +0000

    [ignore] Missing licence headers and attribution
    svn path=/trunk/eXist/; revision=15124
    
    
    Former-commit-id: 42950d07941014d2c5a4f591c04b6b400b3da2fc

--- src/org/exist/backup/restore/ResourceDeferredPermission.java
+++ src/org/exist/backup/restore/ResourceDeferredPermission.java
@@ -1,3 +1,24 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2005-2011 The eXist-db Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  $Id: Restore.java 15109 2011-08-09 13:03:09Z deliriumsky $
+ */
 package org.exist.backup.restore;
 
 import org.apache.log4j.Logger;
@@ -8,7 +29,7 @@ import org.xmldb.api.base.Resource;
 import org.xmldb.api.base.XMLDBException;
 
 /**
- *
+ * 
  * @author Adam Retter <adam@exist-db.org>
  */
 
