d2a28aa1ce751e7f684a68df9aa39f948114536c
Wolfgang Meier <wolfgangmm@gmail.com>
2011-09-07 15:56:31 +0000

    [website] Fixed blocking index.xml page if blog and twitter feeds could not be loaded due to firewall.
    svn path=/trunk/eXist/; revision=15316

--- webapp/scripts/main.js
+++ webapp/scripts/main.js
@@ -1,30 +1,9 @@
-YAHOO.util.Event.onDOMReady(function () {
-    loadRss();
-	loadTwitter();
-});
-
-function loadTwitter() {
-    if (document.getElementById('news_content')) {
-    	var script = document.createElement("script");
-    	script.setAttribute('src', 'xquery/twitter.xql?mode=json&user=existdb&max=5');
-        document.getElementsByTagName('head')[0].appendChild(script);
-    }
-}
-
-function loadRss() {
-    if (document.getElementById('news_content')) {
-        var script = document.createElement("script");
-        script.setAttribute('src', 'feed.xql');
-        document.getElementsByTagName('head')[0].appendChild(script);
-    }
-}
-
 function atomCallback(html) {
 	document.getElementById('news_content').innerHTML = html;
-	YAHOO.util.Dom.setStyle('news-box', 'display', '');
+	document.getElementById('news-box').style.display = "";
 }
 
 function twitterCallback(html) {
 	document.getElementById('twitter_content').innerHTML = html;
-	YAHOO.util.Dom.setStyle('twitter-box', 'display', '');
+	document.getElementById('twitter-box').style.display = "";
 }
