8163cab12ecb70b753d2aeb1cd16473a5b4cefcd
Dmitriy Shabanov <shabanovd@gmail.com>
2011-11-15 16:47:12 +0000

    [ignore] svn delete xquery function
    svn path=/trunk/eXist/; revision=15539

---
+++ extensions/svn/src/org/exist/versioning/svn/xquery/SVNDelete.java
@@ -0,0 +1,87 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2010 The eXist Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * $Id: SVNCommit.java 14605 2011-06-01 18:23:40Z shabanovd $
+ */
+package org.exist.versioning.svn.xquery;
+
+import org.exist.dom.QName;
+import org.exist.versioning.svn.Resource;
+import org.exist.versioning.svn.WorkingCopy;
+import org.exist.xquery.Cardinality;
+import org.exist.xquery.FunctionSignature;
+import org.exist.xquery.XPathException;
+import org.exist.xquery.XQueryContext;
+import org.exist.xquery.value.FunctionReturnSequenceType;
+import org.exist.xquery.value.Sequence;
+import org.exist.xquery.value.SequenceType;
+import org.exist.xquery.value.Type;
+import org.tmatesoft.svn.core.SVNException;
+
+/**
+ * Schedules directories and files for deletion from version control upon the next 
+ * commit (locally).
+ * 
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ */
+public class SVNDelete extends AbstractSVNFunction {
+
+    public final static FunctionSignature signature =
+		new FunctionSignature(
+			new QName("delete", SVNModule.NAMESPACE_URI, SVNModule.PREFIX),
+			"Schedules directories and files for deletion from version control upon the next commit (locally).",
+			new SequenceType[] {
+				DB_PATH
+            },
+            new FunctionReturnSequenceType(Type.EMPTY, Cardinality.ZERO, ""));
+
+    /**
+     *
+     * @param context
+     */
+    public SVNDelete(XQueryContext context) {
+        super(context, signature);
+    }
+    
+    /**
+     * Process the function. All arguments are passed in the array args. The number of
+     * arguments, their type and cardinality have already been checked to match
+     * the function signature.
+     *
+     * @param args
+     * @param contextSequence
+     */
+    public Sequence eval(Sequence[] args, Sequence contextSequence) throws XPathException {
+
+        String wcDir = args[0].getStringValue();
+        
+        try {
+    		WorkingCopy wc = new WorkingCopy("", "");
+
+        	wc.delete(new Resource(wcDir), true);
+		} catch (SVNException svne) {
+			svne.printStackTrace();
+			throw new XPathException(this,
+					"error while scheduling deletion from version control of '"
+                    + wcDir + "'", svne);
+		}
+
+		return Sequence.EMPTY_SEQUENCE;
+    }
+}
