d2a9d0fa4a8020f9970bd015a176db79bc1d5dd7
Adam Retter <adam.retter@googlemail.com>
2012-01-18 21:15:37 +0000

    [ignore] cleanup and refactoring
    svn path=/trunk/eXist/; revision=15691
    
    
    Former-commit-id: 78d7c48c91437e0ddf69592b6d9434ab19a86afa

--- src/org/exist/client/MimeTypeFileFilter.java
+++ src/org/exist/client/MimeTypeFileFilter.java
@@ -2,8 +2,8 @@ package org.exist.client;
 
 import java.io.File;
 import java.util.Iterator;
-import java.util.Vector;
 
+import java.util.List;
 import javax.swing.filechooser.FileFilter;
 
 import org.exist.util.MimeTable;
@@ -15,51 +15,50 @@ import org.exist.util.MimeTable;
  * 
  *  Java 6 API has a similar FileNameExtensionFilter
  */
-public class MimeTypeFileFilter extends FileFilter
-{
-	private String description = null;	
-	private Vector<String> extensions = null;
+public class MimeTypeFileFilter extends FileFilter {
+    
+    private String description = null;	
+    private List<String> extensions = null;
+
+    public MimeTypeFileFilter(String mimeType) {
+        description = MimeTable.getInstance().getContentType(mimeType).getDescription();
+        extensions = MimeTable.getInstance().getAllExtensions(mimeType);
+    }
 	
-	public MimeTypeFileFilter(String mimeType)
-	{
-		description = MimeTable.getInstance().getContentType(mimeType).getDescription();
-		extensions = MimeTable.getInstance().getAllExtensions(mimeType);
-	}
-	
-	public boolean accept(File file)
-	{
-		if(file.isDirectory()) //permit directories to be viewed
-			return true;
+    @Override
+    public boolean accept(File file) {
+        if(file.isDirectory()){ //permit directories to be viewed
+            return true;
+        }
+
+        int extensionOffset = file.getName().lastIndexOf('.');	//do-not allow files without an extension
+        if(extensionOffset == -1) {
+            return false;
+        }
 		
-		int extensionOffset = file.getName().lastIndexOf('.');	//do-not allow files without an extension
-		if(extensionOffset == -1)
-			return false;
-		
-		//check the extension is that of a file as defined in mime-types.xml
-		String fileExtension = file.getName().substring(extensionOffset).toLowerCase();
-		
-		for(String extension : extensions)
-		{
-			if(fileExtension.equals(extension))
-			{
-				return true;
-			}
-		}
-		
-		return false;
-	}
-	
-	public String getDescription()
-	{
-		String description = this.description + " ("; 
-		
-		for(Iterator<String> itExtensions = extensions.iterator(); itExtensions.hasNext();)
-		{
-			description += itExtensions.next();
-			if(itExtensions.hasNext())
-				description += ' ';
-		}
-		
-		return description + ")";
-	}
+        //check the extension is that of a file as defined in mime-types.xml
+        String fileExtension = file.getName().substring(extensionOffset).toLowerCase();
+
+        for(String extension : extensions) {
+            if(fileExtension.equals(extension)) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+    
+    @Override
+    public String getDescription() {
+        String description = this.description + " ("; 
+
+        for(Iterator<String> itExtensions = extensions.iterator(); itExtensions.hasNext();) {
+            description += itExtensions.next();
+            if(itExtensions.hasNext()) {
+                description += ' ';
+            }
+        }
+
+        return description + ")";
+    }
 }
