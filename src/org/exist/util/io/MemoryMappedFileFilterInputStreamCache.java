1af9e536d18f5475633a3258741767af971ac6cf
Adam Retter <adam.retter@googlemail.com>
2012-02-25 14:44:28 +0000

    [bugfix] If you cannot delete a temporary file (e.g. on Windows), then re-use it. Delete previous temporary files when eXist-db is restarted.
    svn path=/trunk/eXist/; revision=15976

--- src/org/exist/util/io/MemoryMappedFileFilterInputStreamCache.java
+++ src/org/exist/util/io/MemoryMappedFileFilterInputStreamCache.java
@@ -1,3 +1,29 @@
+/*
+Copyright (c) 2012, Adam Retter
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+    * Redistributions of source code must retain the above copyright
+      notice, this list of conditions and the following disclaimer.
+    * Redistributions in binary form must reproduce the above copyright
+      notice, this list of conditions and the following disclaimer in the
+      documentation and/or other materials provided with the distribution.
+    * Neither the name of Adam Retter Consulting nor the
+      names of its contributors may be used to endorse or promote products
+      derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL Adam Retter BE LIABLE FOR ANY
+DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 package org.exist.util.io;
 
 import java.io.File;
@@ -17,7 +43,6 @@ import java.nio.channels.FileChannel;
  */
 public class MemoryMappedFileFilterInputStreamCache implements FilterInputStreamCache {
 
-    //private final static long DEFAULT_MEMORY_MAP_SIZE = 64 * 1024; //64KB
     private final static long DEFAULT_MEMORY_MAP_SIZE = 64 * 1024 * 1024; //64MB
 
     private final RandomAccessFile raf;
@@ -25,22 +50,26 @@ public class MemoryMappedFileFilterInputStreamCache implements FilterInputStream
     private MappedByteBuffer buf;
     private File tempFile = null;
     private final long memoryMapSize = DEFAULT_MEMORY_MAP_SIZE;
-    //private final ReadOnlyByteBufferAccessor bufAccessor;
+    
+    private boolean externalFile = true;
 
     public MemoryMappedFileFilterInputStreamCache() throws IOException {
         this(null);
     }
-
-    public MemoryMappedFileFilterInputStreamCache(File f) throws FileNotFoundException, IOException {
+    
+    public MemoryMappedFileFilterInputStreamCache(final File f) throws FileNotFoundException, IOException {
+        
         if(f == null) {
-		tempFile = File.createTempFile("MemoryMappedFileFilterInputStreamCache" + "_" + System.currentTimeMillis(), ".tmp");
-		tempFile.deleteOnExit();
-		f = tempFile;
+            tempFile = MemoryMappedTemporaryFileManager.getInstance().getTemporaryFile();
+            externalFile = false;
+        } else {
+            tempFile = f;
+            externalFile = true;
         }
-        this.raf =  new RandomAccessFile(f, "rw");
+        
+        this.raf =  new RandomAccessFile(tempFile, "rw");
         this.channel = raf.getChannel();
         this.buf = channel.map(FileChannel.MapMode.READ_WRITE, 0, getMemoryMapSize());
-        //this.bufAccessor = new ReadOnlyByteBufferAccessor();
     }
 
     private long getMemoryMapSize() {
@@ -133,13 +162,19 @@ public class MemoryMappedFileFilterInputStreamCache implements FilterInputStream
         channel.close();
         raf.close();
         //System.gc();
+        
+        if(tempFile != null && (!externalFile)) {
+           MemoryMappedTemporaryFileManager.getInstance().returnTemporaryFile(tempFile);
+        }
     }
     
+    /*
     @Override
     protected void finalize() throws Throwable {
         super.finalize();
 	if(tempFile != null) {
             boolean deletedFile = tempFile.delete();
+            System.out.println("deleted=" + deletedFile);
         }
-    }
+    }*/
 }
