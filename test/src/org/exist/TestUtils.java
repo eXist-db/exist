78bfc8889efc96e9757982c325b6030a26558b69
Patrick Bosek <pboz>
2012-03-06 23:01:03 +0000

    Improved TestUtils with static functions for setting a temporary test data directory which does not damage the current data directory, replacing the data directory once the test completes and starting and stopping the database
    svn path=/trunk/eXist/; revision=16120

--- test/src/org/exist/TestUtils.java
+++ test/src/org/exist/TestUtils.java
@@ -1,16 +1,24 @@
 package org.exist;
 
+import org.apache.commons.io.FileUtils;
 import org.exist.collections.Collection;
 import org.exist.collections.CollectionConfigurationManager;
 import org.exist.dom.DocumentImpl;
+import org.exist.start.Main;
 import org.exist.storage.BrokerPool;
 import org.exist.storage.DBBroker;
 import org.exist.storage.txn.TransactionManager;
 import org.exist.storage.txn.Txn;
+import org.exist.util.Configuration;
+import org.exist.util.ConfigurationHelper;
+import org.exist.util.DatabaseConfigurationException;
 import org.exist.xmldb.XmldbURI;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.fail;
 
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
 import java.util.Iterator;
 
 /**
@@ -21,7 +29,7 @@ import java.util.Iterator;
  * To change this template use File | Settings | File Templates.
  */
 public class TestUtils {
-
+	
     public static void cleanupDB() {
         BrokerPool pool = null;
         DBBroker broker = null;
@@ -69,4 +77,80 @@ public class TestUtils {
             if (pool != null) pool.release(broker);
         }
     }
+
+
+    public static Main startupDatabase() {
+        Main database = new org.exist.start.Main("jetty");
+        database.run(new String[]{"jetty"});
+        return database;
+    }
+
+    public static void stopDatabase(Main database) {
+       	database.shutdown();
+    }
+    
+    /**
+     * Moves the current data directory to a backup location and places a 
+     * clean data directory in it's place. This way we don't loose data when 
+     * we run a unit test
+     * 
+     * @return the path to the data backup directory
+     * @throws IOException
+     * @author Patrick Bosek <patrick.bosek@jorsek.com>
+     * @throws DatabaseConfigurationException 
+     */
+    public static String moveDataDirToTempAndCreateClean() throws IOException, DatabaseConfigurationException {
+    	
+		Configuration conf = new Configuration();
+		
+		String dataDirPath = (String) conf.getProperty(BrokerPool.PROPERTY_DATA_DIR);
+		
+		if(dataDirPath == null || dataDirPath.equals(""))
+			throw new DatabaseConfigurationException("Could not find configuration for data directory");
+		
+		dataDirPath = dataDirPath.replaceAll("/$", "");
+		
+		java.util.GregorianCalendar cal = new java.util.GregorianCalendar();
+		String dateString = cal.toString();
+		
+		File data = new File(dataDirPath);
+		File dataBackup = new File(dataDirPath + ".temp-test-bak");
+		
+		data.renameTo(dataBackup);
+		
+		data = new File(dataDirPath);
+		data.mkdir();
+		
+		return dataBackup.getAbsolutePath();
+	}
+	
+	/**
+	 * Restores the data directory from before the test run and moves
+	 * the data directory used durring the test to data.last-test-run
+	 * so it can be inspected if desired.
+	 * 
+	 * @param backupDataDirPath
+	 * @throws IOException
+	 * @author Patrick Bosek <patrick.bosek@jorsek.com>
+	 * @throws DatabaseConfigurationException 
+	 */
+	public static void moveDataDirBack(String backupDataDirPath) throws IOException, DatabaseConfigurationException {
+
+		Configuration conf = new Configuration();
+		
+		String dataDirPath = (String) conf.getProperty(BrokerPool.PROPERTY_DATA_DIR);
+		dataDirPath = dataDirPath.replaceAll("/$", "");
+		
+		File lastTestRunDataDir = new File(dataDirPath + ".last-test-run");
+		
+		if(lastTestRunDataDir.exists()) FileUtils.deleteDirectory(lastTestRunDataDir);
+		
+		File data = new File(dataDirPath);
+		
+		data.renameTo(lastTestRunDataDir);
+		
+		File dataBackup = new File(backupDataDirPath);
+		
+		dataBackup.renameTo(data);
+	}
 }
