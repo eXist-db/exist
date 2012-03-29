587c50edca8456f6f03e82637b6fff38d6bc4721
Wolfgang Meier <wolfgangmm@gmail.com>
2012-03-29 13:51:02 +0000

    [bugfix] package repository: added function repo:install-from-db to directly install an app from a .xar stored in the db; avoids downloading from URL which is causing errors on some installations.
    svn path=/trunk/eXist/; revision=16209
    
    
    Former-commit-id: e7212e40acdce7fc898bc911949e5f1f352d31fa

--- extensions/modules/src/org/exist/xquery/modules/expathrepo/InstallFunction.java
+++ extensions/modules/src/org/exist/xquery/modules/expathrepo/InstallFunction.java
@@ -1,42 +1,62 @@
 package org.exist.xquery.modules.expathrepo;
 
+import java.io.File;
+import java.net.URI;
+import java.net.URISyntaxException;
+
 import org.apache.log4j.Logger;
+import org.exist.dom.BinaryDocument;
+import org.exist.dom.DocumentImpl;
 import org.exist.dom.QName;
+import org.exist.repo.ExistRepository;
+import org.exist.security.PermissionDeniedException;
+import org.exist.storage.NativeBroker;
+import org.exist.storage.lock.Lock;
+import org.exist.xmldb.XmldbURI;
 import org.exist.xquery.BasicFunction;
 import org.exist.xquery.Cardinality;
 import org.exist.xquery.FunctionSignature;
 import org.exist.xquery.XPathException;
 import org.exist.xquery.XQueryContext;
-import org.exist.xquery.value.*;
+import org.exist.xquery.value.BooleanValue;
+import org.exist.xquery.value.FunctionParameterSequenceType;
+import org.exist.xquery.value.FunctionReturnSequenceType;
+import org.exist.xquery.value.Sequence;
+import org.exist.xquery.value.SequenceType;
+import org.exist.xquery.value.StringValue;
+import org.exist.xquery.value.Type;
 import org.expath.pkg.repo.PackageException;
 import org.expath.pkg.repo.Repository;
 import org.expath.pkg.repo.UserInteractionStrategy;
 import org.expath.pkg.repo.tui.BatchUserInteraction;
-
-import java.net.URI;
-import java.net.URISyntaxException;
-import org.exist.repo.ExistRepository;
 
 
 /**
  * Install Function: Install package into repository
  *
  * @author James Fuller <jim.fuller@exist-db.org>
- * @author cutlass
+ * @author Wolfgang Meier
  * @version 1.0
  */
 public class InstallFunction extends BasicFunction {
 
 	private final static Logger logger = Logger.getLogger(InstallFunction.class);
 
-    public final static FunctionSignature signature =
+    public final static FunctionSignature signatureInstall =
 		new FunctionSignature(
 			new QName("install", ExpathPackageModule.NAMESPACE_URI, ExpathPackageModule.PREFIX),
 			"Install package from repository.",
-			new SequenceType[] { new FunctionParameterSequenceType("text", Type.STRING, Cardinality.ZERO_OR_MORE, "package name")},
+			new SequenceType[] { new FunctionParameterSequenceType("text", Type.STRING, Cardinality.EXACTLY_ONE, "package name")},
 			new FunctionReturnSequenceType(Type.BOOLEAN, Cardinality.EXACTLY_ONE, "true if successful, false otherwise"));
 
-	public InstallFunction(XQueryContext context) {
+    public final static FunctionSignature signatureInstallFromDB =
+		new FunctionSignature(
+			new QName("install-from-db", ExpathPackageModule.NAMESPACE_URI, ExpathPackageModule.PREFIX),
+			"Install package stored in database.",
+			new SequenceType[] { new FunctionParameterSequenceType("path", Type.STRING, Cardinality.EXACTLY_ONE, "database path to the package archive (.xar file)")},
+			new FunctionReturnSequenceType(Type.BOOLEAN, Cardinality.EXACTLY_ONE, "true if successful, false otherwise"));
+    
+	public InstallFunction(XQueryContext context, FunctionSignature signature) {
 		super(context, signature);
  	}
 
@@ -45,18 +65,30 @@ public class InstallFunction extends BasicFunction {
         Sequence removed = BooleanValue.FALSE;
         boolean force = true;
         UserInteractionStrategy interact = new BatchUserInteraction();
-        String pkg = args[0].getStringValue();
-        URI uri = _getURI(pkg);
+        String pkgOrPath = args[0].getStringValue();
+        
+        ExistRepository repo = getContext().getRepository();
+        Repository parent_repo = repo.getParentRepo();
+        
         try {
-
-            if ( pkg == null ) {
-                System.err.println("Package name required");
-            }
-            else {
-                ExistRepository repo = getContext().getRepository();
-                Repository parent_repo = repo.getParentRepo();
-                parent_repo.installPackage(uri,force,interact);
-            }
+        	if (isCalledAs("install")) {
+        		// download .xar from a URI
+        		URI uri = _getURI(pkgOrPath);
+        		parent_repo.installPackage(uri, force, interact);
+        	} else {
+        		// .xar is stored as a binary resource
+        		BinaryDocument doc = null;
+        		try {
+        			doc = _getDocument(pkgOrPath);
+        			File file = ((NativeBroker)context.getBroker()).getCollectionBinaryFileFsPath(doc.getURI());
+        			LOG.debug("Installing file: " + file.getAbsolutePath());
+        			parent_repo.installPackage(file, force, interact);
+        		} finally {
+        			if (doc != null)
+        				doc.getUpdateLock().release(Lock.READ_LOCK);
+        		}
+        	}
+        
             removed = BooleanValue.TRUE;
         } catch (PackageException ex ) {
         	logger.debug(ex.getMessage(), ex);
@@ -67,21 +99,31 @@ public class InstallFunction extends BasicFunction {
         return removed;
 	}
 
-    private URI _getURI(String s)
+    private URI _getURI(String s) throws XPathException
     {
         URI uri;
         try {
             uri = new URI(s);
         }
         catch ( URISyntaxException ex ) {
-            return null;
+        	throw new XPathException(this, EXPathErrorCode.EXPDY001, s + " is not a valid URI: " + ex.getMessage(), new StringValue(s), ex);
         }
         if ( uri.isAbsolute() ) {
             return uri;
-        }
-        else {
-            return null;
+        } else {
+        	throw new XPathException(this, EXPathErrorCode.EXPDY001, s + " must be an absolute URI", new StringValue(s));
         }
     }
 
+    private BinaryDocument _getDocument(String path) throws XPathException {
+    	try {
+			XmldbURI uri = XmldbURI.createInternal(path);
+			DocumentImpl doc = context.getBroker().getXMLResource(uri, Lock.READ_LOCK);
+			if (doc.getResourceType() != DocumentImpl.BINARY_FILE)
+				throw new XPathException(this, EXPathErrorCode.EXPDY001, path + " is not a valid .xar", new StringValue(path));
+			return (BinaryDocument) doc;
+		} catch (PermissionDeniedException e) {
+			throw new XPathException(this, EXPathErrorCode.EXPDY003, e.getMessage(), new StringValue(path), e);
+		}
+    }
 }
