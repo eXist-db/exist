b9870066348ba7f9f8098ae6693783235876582b
Wolfgang Meier <wolfgangmm@gmail.com>
2012-05-11 14:20:15 +0000

    [distribution] Extended the installer to allow optional application packages to be selected and installed.
    svn path=/trunk/eXist/; revision=16364
    
    
    Former-commit-id: f1b80002266e87b59cd92f2130678e9c89570eee

--- src/org/exist/installer/Setup.java
+++ src/org/exist/installer/Setup.java
@@ -22,13 +22,25 @@
 
 package org.exist.installer;
 
+import org.exist.EXistException;
+import org.exist.repo.ExistRepository;
 import org.exist.security.Account;
+import org.exist.storage.BrokerPool;
 import org.exist.xmldb.DatabaseInstanceManager;
 import org.exist.xmldb.UserManagementService;
+import org.exist.xquery.XPathException;
+import org.expath.pkg.repo.*;
+import org.expath.pkg.repo.Package;
+import org.expath.pkg.repo.tui.BatchUserInteraction;
+import org.jedit.syntax.InputHandler;
 import org.xmldb.api.DatabaseManager;
 import org.xmldb.api.base.Collection;
 import org.xmldb.api.base.Database;
 import org.xmldb.api.base.XMLDBException;
+import org.xmldb.api.modules.XQueryService;
+
+import java.io.File;
+import java.util.Arrays;
 
 /**
  * Initial database setup: called from the installer to set the admin password.
@@ -39,15 +51,100 @@ public class Setup {
     private final static String URI = "xmldb:exist:///db";
 
     public static void main(String[] args) {
-        if (args.length < 1 || args[0].length() == 0) {
+        if (args.length < 1) {
             System.err.println("No password specified. Admin password will be empty.");
             return;
         }
-        initDb(args[0]);
-        shutdown(args[0]);
+        int offset = 0;
+        String passwd = null;
+        if (args[0].startsWith("pass=")) {
+            passwd = args[0].substring(5);
+            offset = 1;
+        }
+        XQueryService query = initDb(passwd);
+        if (query != null) {
+            try {
+                installApps(query, args, offset);
+            } catch (EXistException e) {
+                System.err.println("An error occurred while installing apps: " + e.getMessage());
+            }
+        }
+        shutdown(passwd);
     }
 
-    private static void initDb(String adminPass) {
+    private static void installApps(XQueryService query, String[] args, int offset) throws EXistException {
+        File home = getExistHome();
+        ExistRepository repository = getRepository(home);
+
+        for (int i = offset; i < args.length; i++) {
+            String[] appParams = args[i].split("=");
+            String name = appParams[0];
+            String onOff = appParams[1];
+            if ("on".equals(onOff)) {
+                try {
+                    File xar = findApp(home, name);
+                    if (xar != null) {
+                        System.out.println("Installing app package " + xar.getName());
+                        UserInteractionStrategy interact = new BatchUserInteraction();
+                        Package pkg = repository.getParentRepo().installPackage(xar, true, interact);
+                        String pkgName = pkg.getName();
+                        String xquery =
+                                "import module namespace repo=\"http://exist-db.org/xquery/repo\" " +
+                                "at \"java:org.exist.xquery.modules.expathrepo.ExpathPackageModule\"; " +
+                                "repo:deploy(\"" + pkgName + "\")";
+                        query.query(xquery);
+                    } else {
+                        System.err.println("App package not found: " + name + ". Skipping it.");
+                    }
+                } catch (PackageException e) {
+                    System.err.println("Failed to install application package " + name + ": " + e.getMessage());
+                } catch (XMLDBException e) {
+                    System.err.println("Failed to deploy application package " + name + ": " + e.getMessage());
+                }
+            }
+        }
+    }
+
+    private static File getExistHome() throws EXistException {
+        return BrokerPool.getInstance().getConfiguration().getExistHome();
+    }
+
+    private static File findApp(File home, String app) {
+        File apps = new File(home, "apps");
+        System.out.println("Apps directory: " + apps.getAbsolutePath());
+        if (apps.canRead() && apps.isDirectory()) {
+            File[] files = apps.listFiles();
+            for (File file : files) {
+                if (file.getName().startsWith(app))
+                    return file;
+            }
+        }
+        return null;
+    }
+
+    private static ExistRepository getRepository(File home) throws EXistException {
+        try {
+            if (home != null){
+                File repo_dir = new File(home, "webapp/WEB-INF/expathrepo");
+                // ensure the dir exists
+                repo_dir.mkdir();
+                FileSystemStorage storage = new FileSystemStorage(repo_dir);
+                return new ExistRepository(storage);
+            }else{
+                File repo_dir = new File(System.getProperty("java.io.tmpdir") + "/expathrepo");
+                // ensure the dir exists
+                repo_dir.mkdir();
+                FileSystemStorage storage = new FileSystemStorage(repo_dir);
+                return new ExistRepository(storage);
+            }
+        }
+        catch ( PackageException ex ) {
+            // problem with pkg-repo.jar throwing exception
+            throw new EXistException("Problem setting expath repository", ex);
+        }
+    }
+
+    private static XQueryService initDb(String adminPass) {
         System.out.println("--- Starting embedded database instance ---");
         try {
             Class<?> cl = Class.forName(DRIVER);
@@ -55,16 +152,22 @@ public class Setup {
             database.setProperty("create-database", "true");
             DatabaseManager.registerDatabase(database);
             Collection root = DatabaseManager.getCollection(URI, "admin", "");
-            UserManagementService service =
-                    (UserManagementService) root.getService("UserManagementService", "1.0");
-            Account admin = service.getAccount("admin");
-            admin.setPassword(adminPass);
-            System.out.println("Setting admin user password...");
-            service.updateAccount(admin);
+            if (adminPass != null) {
+                UserManagementService service =
+                        (UserManagementService) root.getService("UserManagementService", "1.0");
+                Account admin = service.getAccount("admin");
+                admin.setPassword(adminPass);
+                System.out.println("Setting admin user password...");
+                service.updateAccount(admin);
+                root = DatabaseManager.getCollection(URI, "admin", adminPass);
+            }
+            XQueryService query = (XQueryService) root.getService("XQueryService", "1.0");
+            return query;
         } catch (Exception e) {
             System.err.println("Caught an exception while initializing db: " + e.getMessage());
             e.printStackTrace();
         }
+        return null;
     }
 
     private static void shutdown(String adminPass) {
