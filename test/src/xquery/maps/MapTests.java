afeede785f1f0708b653e9591f9ddaa40254c72e
Wolfgang Meier <wolfgangmm@gmail.com>
2012-06-01 17:43:45 +0000

    [testsuite] Tests for the map feature.
    svn path=/trunk/eXist/; revision=16536
    
    
    Former-commit-id: 7ee85958b8a1ee7d05521a0974241c4844650049

---
+++ test/src/xquery/maps/MapTests.java
@@ -0,0 +1,11 @@
+package xquery.maps;
+
+import xquery.TestRunner;
+
+public class MapTests extends TestRunner {
+
+    @Override
+    protected String getDirectory() {
+        return "test/src/xquery/maps";
+    }
+}
