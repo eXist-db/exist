f954848b63b4a59baf334d773f9c6165a77c9a6c
Dmitriy Shabanov <shabanovd@gmail.com>
2012-06-15 07:59:09 +0000

    [ignore] metadata storage - extract interfaces & core from implementation
    svn path=/trunk/eXist/; revision=16680
    
    
    Former-commit-id: d2fdd87e3bd4f267b2c0517c9db708046526bef4

--- extensions/metadata/sleepycat/src/main/java/org/exist/storage/md/MetaDataImpl.java
+++ extensions/metadata/sleepycat/src/main/java/org/exist/storage/md/MetaDataImpl.java
@@ -1,3 +1,24 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2012 The eXist Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  $Id$
+ */
 package org.exist.storage.md;
 
 import java.io.File;
@@ -17,6 +38,10 @@ import com.sleepycat.je.Environment;
 import com.sleepycat.je.EnvironmentConfig;
 import com.sleepycat.persist.*;
 
+/**
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ *
+ */
 public class MetaDataImpl extends MetaData {
 	
 	protected static MetaDataImpl _ = null;
