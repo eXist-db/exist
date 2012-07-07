c68cc7115f18dceb3931c4dcd75bb3575007dd31
Dannes Wessels <dannes@exist-db.org>
2012-07-07 15:27:58 +0000

    [ignore] Refactored ContentReceiver to separate class
    svn path=/trunk/eXist/; revision=16808
    
    
    Former-commit-id: 01eeb13a99d9f68dcd2fec358939ad6c0e97ef8a

--- extensions/contentextraction/src/org/exist/contentextraction/xquery/ContentFunctions.java
+++ extensions/contentextraction/src/org/exist/contentextraction/xquery/ContentFunctions.java
@@ -26,20 +26,14 @@ import java.util.HashMap;
 import java.util.Map;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
-import org.apache.log4j.Logger;
 import org.exist.contentextraction.ContentExtraction;
 import org.exist.contentextraction.ContentExtractionException;
+import org.exist.contentextraction.ContentReceiver;
 import org.exist.dom.QName;
-import org.exist.dom.StoredNode;
 import org.exist.memtree.DocumentBuilderReceiver;
-import org.exist.memtree.MemTreeBuilder;
-import org.exist.memtree.NodeImpl;
 import org.exist.storage.NodePath;
-import org.exist.util.serializer.AttrList;
-import org.exist.util.serializer.Receiver;
 import org.exist.xquery.*;
 import org.exist.xquery.value.*;
-import org.w3c.dom.Document;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 
@@ -49,8 +43,6 @@ import org.xml.sax.SAXException;
  */
 public class ContentFunctions extends BasicFunction {
 
-    @SuppressWarnings("unused")
-    private final static Logger logger = Logger.getLogger(ContentFunctions.class);
 
     public final static FunctionSignature getMeatadata = new FunctionSignature(
         new QName("get-metadata", ContentExtractionModule.NAMESPACE_URI, ContentExtractionModule.PREFIX),
@@ -109,41 +101,41 @@ public class ContentFunctions extends BasicFunction {
         ContentExtraction ce = new ContentExtraction();
 
         if (isCalledAs("stream-content")) {
-            
+
             /* binary content */
             BinaryValue binary = (BinaryValue) args[0].itemAt(0);
-            
+
             /* callback function */
             FunctionReference ref = (FunctionReference) args[2].itemAt(0);
-            
-            Map<String, String> mappings = new HashMap<String, String>();         
+
+            Map<String, String> mappings = new HashMap<String, String>();
             if (args[3].hasOne()) {
                 NodeValue namespaces = (NodeValue) args[3].itemAt(0);
                 parseMappings(namespaces, mappings);
             }
-            
+
             return streamContent(ce, binary, args[1], ref, mappings, args[4]);
-            
+
         } else {
-            
+
             try {
                 if (isCalledAs("get-metadata")) {
                     ce.extractMetadata((BinaryValue) args[0].itemAt(0), (ContentHandler) builder);
-                    
+
                 } else {
                     ce.extractContentAndMetadata((BinaryValue) args[0].itemAt(0), (ContentHandler) builder);
                 }
 
                 return (NodeValue) builder.getDocument();
-                
+
             } catch (IOException ex) {
                 LOG.error(ex.getMessage(), ex);
                 throw new XPathException(ex.getMessage(), ex);
-                
+
             } catch (SAXException ex) {
                 LOG.error(ex.getMessage(), ex);
                 throw new XPathException(ex.getMessage(), ex);
-                
+
             } catch (ContentExtractionException ex) {
                 LOG.error(ex.getMessage(), ex);
                 throw new XPathException(ex.getMessage(), ex);
@@ -151,15 +143,14 @@ public class ContentFunctions extends BasicFunction {
         }
     }
 
-    
     private void parseMappings(NodeValue namespaces, Map<String, String> mappings) throws XPathException {
-        
+
         try {
             XMLStreamReader reader = context.getXMLStreamReader(namespaces);
             reader.next();
-            
+
             while (reader.hasNext()) {
-                
+
                 int status = reader.next();
                 if (status == XMLStreamReader.START_ELEMENT && reader.getLocalName().equals("namespace")) {
                     String prefix = reader.getAttributeValue("", "prefix");
@@ -167,212 +158,45 @@ public class ContentFunctions extends BasicFunction {
                     mappings.put(prefix, uri);
                 }
             }
-            
+
         } catch (XMLStreamException e) {
             throw new XPathException(this, "Error while parsing namespace mappings: " + e.getMessage(), e);
-            
+
         } catch (IOException e) {
             throw new XPathException(this, "Error while parsing namespace mappings: " + e.getMessage(), e);
         }
     }
 
-    
     private Sequence streamContent(ContentExtraction ce, BinaryValue binary, Sequence pathSeq,
             FunctionReference ref, Map<String, String> mappings, Sequence data) throws XPathException {
+
         
         NodePath[] paths = new NodePath[pathSeq.getItemCount()];
         int i = 0;
-        
+
         for (SequenceIterator iter = pathSeq.iterate(); iter.hasNext(); i++) {
             String path = iter.nextItem().getStringValue();
             paths[i] = new NodePath(mappings, path, false);
         }
 
-        ContentReceiver receiver = new ContentReceiver(paths, ref, data);
-        
+        ContentReceiver receiver = new ContentReceiver(context, paths, ref, data);
+
         try {
             ce.extractContentAndMetadata(binary, receiver);
-            
+
         } catch (IOException ex) {
             LOG.error(ex.getMessage(), ex);
             throw new XPathException(ex.getMessage(), ex);
-            
+
         } catch (SAXException ex) {
             LOG.error(ex.getMessage(), ex);
             throw new XPathException(ex.getMessage(), ex);
-            
+
         } catch (ContentExtractionException ex) {
             LOG.error(ex.getMessage(), ex);
             throw new XPathException(ex.getMessage(), ex);
         }
-        
+
         return receiver.getResult();
-    }
-	
-    
-    private class ContentReceiver implements Receiver {
-
-        private ValueSequence result = new ValueSequence();
-        private FunctionReference ref;
-        private NodePath currentElementPath = new NodePath();
-        private NodePath[] paths;
-        private DocumentBuilderReceiver docBuilderReceiver = null;
-        private NodePath startElementPath = null;
-        private Sequence userData = null;
-        private Sequence prevReturnData = Sequence.EMPTY_SEQUENCE;
-
-        ContentReceiver(NodePath[] paths, FunctionReference ref, Sequence userData) {
-            this.paths = paths;
-            this.ref = ref;
-            this.userData = userData;
-        }
-
-        protected Sequence getResult() {
-            return result;
-        }
-
-        /**
-         *  Check if content of current (node) path should be retrieved.
-         * 
-         * @param path Xpath to current node
-         * 
-         * @return TRUE if path is in to-be-retrieved paths
-         */
-        private boolean matches(NodePath path) {
-            for (int i = 0; i < paths.length; i++) {
-                if (paths[i].match(path)) {
-                    return true;
-                }
-            }
-            return false;
-        }
-
-        @Override
-        public void startDocument() throws SAXException {
-        }
-
-        @Override
-        public void endDocument() throws SAXException {
-        }
-
-        @Override
-        public void startPrefixMapping(String prefix, String namespaceURI) throws SAXException {
-        }
-
-        @Override
-        public void endPrefixMapping(String prefix) throws SAXException {
-        }
-
-        @Override
-        public void startElement(QName qname, AttrList attribs) throws SAXException {
-            
-            currentElementPath.addComponent(qname);
-            
-            // if current path is one of required paths
-            // and there is a docReceiver (=data must be collected)
-            if (matches(currentElementPath) && docBuilderReceiver == null) {
-                startElementPath = currentElementPath;
-                context.pushDocumentContext();
-                MemTreeBuilder memBuilder = context.getDocumentBuilder();
-                docBuilderReceiver = new DocumentBuilderReceiver(memBuilder);
-            }
-
-            // Add element to result
-            if (docBuilderReceiver != null) {
-                docBuilderReceiver.startElement(qname, attribs);
-            }
-        }
-
-        @Override
-        public void endElement(QName qname) throws SAXException {
-            
-            // not null means data must be collecterd
-            if (docBuilderReceiver != null) {
-
-                // Add end element
-                docBuilderReceiver.endElement(qname);
-
-                // If path was to be matched path
-                if (currentElementPath.match(startElementPath)) {
-                    
-                    // Retrieve result as document
-                    Document doc = docBuilderReceiver.getDocument();
-                    
-                    // Get the root
-                    NodeImpl root = (NodeImpl) doc.getDocumentElement();
-
-                    docBuilderReceiver = null;
-                    startElementPath = null;
-                    context.popDocumentContext();
-
-                    // Construct parameters
-                    Sequence[] params = new Sequence[3];
-                    params[0] = root;
-                    params[1] = userData;
-                    params[2] = prevReturnData;
-
-                    try {
-                        // Send data to callback function
-                        Sequence ret = ref.evalFunction(null, null, params);
-                        prevReturnData = ret;
-                        result.addAll(ret);
-
-                    } catch (XPathException e) {
-                        LOG.warn(e.getMessage(), e);
-                    }
-                }
-            }
-            
-            // reduce path
-            // DW: should be earlier? currentElementPath is one level wrong
-            currentElementPath.removeLastComponent();
-        }
-
-        @Override
-        public void characters(CharSequence seq) throws SAXException {
-            
-            // DW: receiver is null for subsequent <p> elements.
-            // Need to figure out about the design of class
-            
-            if (docBuilderReceiver != null) {
-                docBuilderReceiver.characters(seq);
-            }
-        }
-
-        @Override
-        public void attribute(QName qname, String value) throws SAXException {
-            if (docBuilderReceiver != null) {
-                docBuilderReceiver.attribute(qname, value);
-            }
-        }
-
-        @Override
-        public void comment(char[] ch, int start, int length) throws SAXException {
-        }
-
-        @Override
-        public void cdataSection(char[] ch, int start, int len) throws SAXException {
-        }
-
-        @Override
-        public void processingInstruction(String target, String data) throws SAXException {
-        }
-
-        @Override
-        public void documentType(String name, String publicId, String systemId)  throws SAXException {
-        }
-
-        @Override
-        public void highlightText(CharSequence seq) throws SAXException {
-        }
-
-        @Override
-        public void setCurrentNode(StoredNode node) {
-        }
-
-        @Override
-        public Document getDocument() {
-            return null;
-        }
     }
 }
