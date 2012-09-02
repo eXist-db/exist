283b11e2a8aec70355fe21fa8aaafe142298b067
Dmitriy Shabanov <shabanovd@gmail.com>
2012-09-02 21:32:46 +0000

    [ignore] make new SchedulerManager pluggable. Add xquery jobs type.
    svn path=/trunk/eXist/; revision=17058

--- extensions/scheduler/src/main/java/org/exist/scheduler/Job.java
+++ extensions/scheduler/src/main/java/org/exist/scheduler/Job.java
@@ -23,12 +23,17 @@ package org.exist.scheduler;
 
 import java.text.ParseException;
 
+import org.exist.Database;
 import org.exist.config.Configurable;
 import org.exist.config.Configuration;
 import org.exist.config.ConfigurationException;
 import org.exist.config.Configurator;
 import org.exist.config.annotation.ConfigurationClass;
 import org.exist.config.annotation.ConfigurationFieldAsElement;
+import org.exist.security.AbstractAccount;
+import org.exist.security.SecurityManager;
+import org.exist.security.Subject;
+import org.exist.security.internal.SubjectAccreditedImpl;
 import org.quartz.CronTrigger;
 import org.quartz.JobDetail;
 import org.quartz.SchedulerException;
@@ -52,13 +57,22 @@ public class Job implements Configurable {
 	@ConfigurationFieldAsElement("class")
 	private String clazz;
 	
+	@ConfigurationFieldAsElement("run-as-account")
+	private String account;
+
+	@ConfigurationFieldAsElement("script-uri")
+	protected String scriptURI;
+
 	@ConfigurationFieldAsElement("cron-expression")
 	private String cronExpression;
 	
 	private Configuration configuration = null;
+	private SchedulerManager manager;
 
 	public Job(SchedulerManager manager, Configuration config) throws ConfigurationException {
 
+		this.manager = manager;
+		
         configuration = Configurator.configure(this, config);
 
 		try {
@@ -82,10 +96,27 @@ public class Job implements Configurable {
 	
 	private Class<?> getJobClass() throws ConfigurationException {
 		try {
+			if (scriptURI != null)
+				return Class.forName("org.exist.scheduler.XQueryJob");
+
 			return Class.forName(clazz);
 		} catch (ClassNotFoundException e) {
 			throw new ConfigurationException(e);
 		}
+	}
+
+	protected Subject getSubject() {
+    	SecurityManager sm = getDatabase().getSecurityManager();
+    	AbstractAccount acc = (AbstractAccount) sm.getAccount(account);
+    	if (acc == null)
+    		//UNDERSTAND: error better here?
+    		return sm.getGuestSubject();
+    	
+        return new SubjectAccreditedImpl(acc, this);
+    }
+	
+	protected Database getDatabase() {
+		return manager.db;
 	}
 
 	@Override
