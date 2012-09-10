3f43847bc5cc4f01d261cc60d6f14d5ded6d22d9
Dmitriy Shabanov <shabanovd@gmail.com>
2012-09-10 15:20:51 +0000

    [ignore] restructure tests. Add "AND under two predicates" test
    svn path=/trunk/eXist/; revision=17115

---
+++ test/src/xquery/logicalOp/LogicalOpTests.java
@@ -0,0 +1,11 @@
+package xquery.logicalOp;
+
+import xquery.TestRunner;
+
+public class LogicalOpTests extends TestRunner {
+
+    @Override
+    protected String getDirectory() {
+        return "test/src/xquery/logicalOp";
+    }
+}
