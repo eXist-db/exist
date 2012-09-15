baadd211e89e913dc5b3206db661ae4724dfcd9a
Wolfgang Meier <wolfgangmm@gmail.com>
2012-09-15 21:38:03 +0000

    [feature] Enable dynamic loading of jar files from xar packages. eXist-db no longer needs to be restarted after deployment of a xar.
    svn path=/trunk/eXist/; revision=17145
    
    
    Former-commit-id: 077a3813693b0d2ce2b28c41a8248dbf2c846002

---
+++ src/org/exist/start/EXistClassLoader.java
@@ -0,0 +1,28 @@
+package org.exist.start;
+
+import java.io.File;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+/**
+ * Root class loader when eXist is started via the bootstrap loader. Extends
+ * URLClassLoader to allow dynamic addition of URLs at runtime.
+ *
+ * @author Wolfgang Meier
+ */
+public class EXistClassLoader extends URLClassLoader {
+
+    public EXistClassLoader(URL[] urls, ClassLoader classLoader) {
+        super(urls, classLoader);
+    }
+
+    public void addURLs(Classpath cp) {
+        for (File file : cp) {
+            try {
+                addURL(file.toURI().toURL());
+            } catch (MalformedURLException e) {
+            }
+        }
+    }
+}
