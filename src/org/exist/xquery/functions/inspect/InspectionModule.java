2a87e7c274a1528f7793f04a7042dd86c6c01bef
Wolfgang Meier <wolfgangmm@gmail.com>
2012-09-22 15:09:42 +0000

    [feature] Moved util:inspect-function to inspect:inspect-function in separate module. Added inspect:inspect-module to inspect a module without importing it.
    svn path=/trunk/eXist/; revision=17168

---
+++ src/org/exist/xquery/functions/inspect/InspectionModule.java
@@ -0,0 +1,45 @@
+package org.exist.xquery.functions.inspect;
+
+import org.exist.xquery.AbstractInternalModule;
+import org.exist.xquery.FunctionDef;
+
+import java.util.List;
+import java.util.Map;
+
+public class InspectionModule extends AbstractInternalModule {
+
+    public final static String NAMESPACE_URI = "http://exist-db.org/xquery/inspection";
+
+    public final static String PREFIX = "inspect";
+
+    public final static String RELEASE = "2.0";
+
+    public final static FunctionDef[] functions = {
+        new FunctionDef(InspectFunction.SIGNATURE, InspectFunction.class),
+        new FunctionDef(InspectModule.signature, InspectModule.class)
+    };
+
+    public InspectionModule(Map<String, List<? extends Object>> parameters) {
+        super(functions, parameters, true);
+    }
+
+    @Override
+    public String getNamespaceURI() {
+        return NAMESPACE_URI;
+    }
+
+    @Override
+    public String getDefaultPrefix() {
+        return PREFIX;
+    }
+
+    @Override
+    public String getDescription() {
+        return "Functions for inspecting XQuery modules and functions";
+    }
+
+    @Override
+    public String getReleaseVersion() {
+        return RELEASE;
+    }
+}
