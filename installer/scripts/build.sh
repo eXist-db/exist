dcffdac8e9d6a0fd7e6f9ebfd0c407e890d19050
Leif-Jöran Olsson <ljo@fripost.org>
2012-09-26 23:28:15 +0000

    [ignore] Make installer version of /home/ljo/eXist/build.sh in sync with the svn repos one.
    svn path=/trunk/eXist/; revision=17185
    
    
    Former-commit-id: 46f0c2ff1f30bb617b4882f9651efc094e7674c5

--- installer/scripts/build.sh
+++ installer/scripts/build.sh
@@ -16,9 +16,12 @@ LOCALCLASSPATH=$CLASSPATH:$ANT_HOME/lib/ant-launcher.jar:$EXIST_HOME/lib/test/ju
 
 JAVA_ENDORSED_DIRS="$EXIST_HOME"/lib/endorsed
 
-JAVA_OPTS="-Dant.home=$ANT_HOME -Djava.endorsed.dirs=$JAVA_ENDORSED_DIRS -Dexist.home=$EXIST_HOME"
+# You must set
+# -Djavax.xml.transform.TransformerFactory=org.apache.xalan.processor.TransformerFactoryImpl
+# Otherwise Ant will fail to do junitreport with Saxon, as it has a direct dependency on Xalan.
+JAVA_OPTS="-Dant.home=$ANT_HOME -Djava.endorsed.dirs=$JAVA_ENDORSED_DIRS -Dexist.home=$EXIST_HOME -Djavax.xml.transform.TransformerFactory=org.apache.xalan.processor.TransformerFactoryImpl $JAVA_OPTS"
 
 echo Starting Ant...
 echo
 
-$JAVA_HOME/bin/java -Xms512m -Xmx2048m $JAVA_OPTS -classpath $LOCALCLASSPATH org.apache.tools.ant.launch.Launcher $*
+"$JAVA_HOME/bin/java" -Xms512m -Xmx2048m $JAVA_OPTS -classpath $LOCALCLASSPATH org.apache.tools.ant.launch.Launcher $*
