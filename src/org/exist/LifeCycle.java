054395b9b18e5b119fc31e5ffb5993993c1ca279
Dmitriy Shabanov <shabanovd@gmail.com>
2012-09-30 18:13:00 +0000

    [ignore] API - add LifeCycle interface
    svn path=/trunk/eXist/; revision=17216
    
    
    Former-commit-id: 0ef9f37be6ef1c70e843b481d29ab80736fad5e5

---
+++ src/org/exist/LifeCycle.java
@@ -0,0 +1,48 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2012 The eXist Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  $Id$
+ */
+package org.exist;
+
+import org.exist.config.Startable;
+import org.exist.storage.DBBroker;
+
+/**
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ *
+ */
+public interface LifeCycle extends Startable {
+
+	public void start(DBBroker broker) throws EXistException;
+
+	public void sync(DBBroker broker) throws EXistException;
+
+	public void stop(DBBroker broker) throws EXistException;
+	
+//    public boolean isRunning();
+//
+//    public boolean isStarted();
+//    public boolean isStarting();
+//
+//    public boolean isStopping();
+//    public boolean isStopped();
+//
+//    public boolean isFailed();
+}
