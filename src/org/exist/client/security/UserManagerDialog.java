d8dd77ca5d82052bcf2913bc260238329221a703
Dmitriy Shabanov <shabanovd@gmail.com>
2012-11-15 09:45:50 +0000

    [ignore] cleanup
    svn path=/trunk/eXist/; revision=17596
    
    
    Former-commit-id: f3d158b2a11ff3df5a7974e5bc973820dcaecc9c

--- src/org/exist/client/security/UserManagerDialog.java
+++ src/org/exist/client/security/UserManagerDialog.java
@@ -1,3 +1,24 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2001-2012 The eXist Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  $Id$
+ */
 package org.exist.client.security;
 
 import java.awt.event.WindowAdapter;
@@ -21,7 +42,9 @@ import org.xmldb.api.base.XMLDBException;
  */
 public class UserManagerDialog extends javax.swing.JFrame {
 
-    private final UserManagementService userManagementService;
+	private static final long serialVersionUID = 2091215304766070041L;
+
+	private final UserManagementService userManagementService;
     private final String currentUser;
     
     private DefaultTableModel usersTableModel = null;
