f3d158b2a11ff3df5a7974e5bc973820dcaecc9c
Dmitriy Shabanov <shabanovd@gmail.com>
2012-11-15 09:45:50 +0000

    [ignore] cleanup
    svn path=/trunk/eXist/; revision=17596

--- src/org/exist/client/security/ReadOnlyDefaultTableModel.java
+++ src/org/exist/client/security/ReadOnlyDefaultTableModel.java
@@ -1,3 +1,24 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2001-2012 The eXist Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  $Id$
+ */
 package org.exist.client.security;
 
 import javax.swing.table.DefaultTableModel;
@@ -8,7 +29,9 @@ import javax.swing.table.DefaultTableModel;
  */
 public class ReadOnlyDefaultTableModel extends DefaultTableModel {
 
-    public ReadOnlyDefaultTableModel(final String[][] data, final String[] columnNames) {
+	private static final long serialVersionUID = 8229062312676408884L;
+
+	public ReadOnlyDefaultTableModel(final String[][] data, final String[] columnNames) {
         super(data, columnNames);
     }
 
--- src/org/exist/client/security/SortedListModel.java
+++ src/org/exist/client/security/SortedListModel.java
@@ -1,4 +1,24 @@
-
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2001-2012 The eXist Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  $Id$
+ */
 package org.exist.client.security;
  
 import java.util.Arrays;
@@ -9,7 +29,10 @@ import java.util.TreeSet;
 import javax.swing.AbstractListModel;
 
 public class SortedListModel<T extends Object> extends AbstractListModel {
-    private final SortedSet<T> model;
+
+	private static final long serialVersionUID = 8156990970750901747L;
+
+	private final SortedSet<T> model;
 
     public SortedListModel() {
         model = new TreeSet<T>();
