4e76630b1e685d2262b3a174a50a482e3e961b61
Dmitriy Shabanov <shabanovd@gmail.com>
2012-11-15 09:42:31 +0000

    [ignore] cleanup
    svn path=/trunk/eXist/; revision=17595
    
    
    Former-commit-id: b870fa84e8eb02d6e1556b5a3e06ffbb088cff10

--- src/org/exist/client/ResourceDescriptor.java
+++ src/org/exist/client/ResourceDescriptor.java
@@ -1,8 +1,7 @@
 /*
  *  eXist Open Source Native XML Database
- *  Copyright (C) 2001-06 Wolfgang M. Meier
- *  wolfgang@exist-db.org
- *  http://exist.sourceforge.net
+ *  Copyright (C) 2001-2012 The eXist Project
+ *  http://exist-db.org
  *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License
@@ -14,9 +13,9 @@
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Lesser General Public License for more details.
  *
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
  *
  *  $Id$
  */
