2536a48429b3098b85b1e9f8e016d2154eb1a373
Adam Retter <adam.retter@googlemail.com>
2012-11-24 17:21:13 +0000

    [refactor] externalise sub-classes
    svn path=/trunk/eXist/; revision=17708

---
+++ src/org/exist/client/security/BasicPermissionsTableModel.java
@@ -0,0 +1,64 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2001-2012 The eXist Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  $Id$
+ */
+package org.exist.client.security;
+
+import javax.swing.table.DefaultTableModel;
+import org.exist.security.Permission;
+
+/**
+ *
+ * @author Adam Retter <adam.retter@googlemail.com>
+ */
+public class BasicPermissionsTableModel extends DefaultTableModel {
+    
+    public BasicPermissionsTableModel(final Permission permission) {
+        
+        super(
+            new Object [][] {
+                new Object[] {"Read", (permission.getOwnerMode() & Permission.READ) == Permission.READ, (permission.getGroupMode() & Permission.READ) == Permission.READ, (permission.getOtherMode() & Permission.READ) == Permission.READ},
+                new Object[] {"Write", (permission.getOwnerMode() & Permission.WRITE) == Permission.WRITE, (permission.getGroupMode() & Permission.WRITE) == Permission.WRITE, (permission.getOtherMode() & Permission.WRITE) == Permission.WRITE},
+                new Object[] {"Execute", (permission.getOwnerMode() & Permission.EXECUTE) == Permission.EXECUTE, (permission.getGroupMode() & Permission.EXECUTE) == Permission.EXECUTE, (permission.getOtherMode() & Permission.EXECUTE) == Permission.EXECUTE}
+            },
+            new String [] {
+                "Permission", "User", "Group", "Other"
+            }
+        );
+    }
+           
+    final Class[] types = new Class [] {
+        java.lang.String.class, java.lang.Boolean.class, java.lang.Boolean.class, java.lang.Boolean.class
+    };
+
+    boolean[] canEdit = new boolean [] {
+        false, true, true, true
+    };
+
+    @Override
+    public Class getColumnClass(int columnIndex) {
+        return types [columnIndex];
+    }
+
+    @Override
+    public boolean isCellEditable(int rowIndex, int columnIndex) {
+        return canEdit [columnIndex];
+    }
+}
