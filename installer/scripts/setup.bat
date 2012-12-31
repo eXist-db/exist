b86b93a5a00bd20dd3e96c82b5fa9339515b6a14
Wolfgang Meier <wolfgangmm@gmail.com>
2012-12-31 17:08:48 +0000

    [installer] Fixed setup.bat script to work on winxp.
    svn path=/trunk/eXist/; revision=17986
    
    
    Former-commit-id: 01628ed4acf68b2d2b12b79fe4150364b73e8e7c

--- installer/scripts/setup.bat
+++ installer/scripts/setup.bat
@@ -11,10 +11,11 @@ set JAVA_HOME=$JAVA_HOME
 set JAVA_CMD="%JAVA_HOME%\bin\java"
 
 set JAVA_ENDORSED_DIRS="%EXIST_HOME%"\lib\endorsed
-set JAVA_OPTS="-Xms64m -Xmx768m -Djava.endorsed.dirs=%JAVA_ENDORSED_DIRS%" "%JAVA_OPTS%"
+set JAVA_OPTS="-Xms64m -Xmx768m -Djava.endorsed.dirs=%JAVA_ENDORSED_DIRS%"
 
-echo "JAVA_HOME: %JAVA_HOME%"
-echo "EXIST_HOME: %EXIST_HOME%"
+rem echo "JAVA_HOME: %JAVA_HOME%"
+rem echo "EXIST_HOME: %EXIST_HOME%"
+echo %JAVA_OPTS%
 
 %JAVA_CMD% "%JAVA_OPTS%" -Dexist.home="%EXIST_HOME%" -Duse.autodeploy.feature=false -jar "%EXIST_HOME%\start.jar" org.exist.installer.Setup %1 %2 %3 %4 %5 %6 %7 %8 %9
 
