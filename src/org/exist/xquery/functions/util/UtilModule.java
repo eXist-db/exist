3d65433fd76d6683d1d5592e27865598d2de2e99
Adam Retter <adam.retter@googlemail.com>
2013-02-02 21:53:30 +0000

    [feature] XQuery functions for converting int to/from octal number
    svn path=/trunk/eXist/; revision=18178
    
    
    Former-commit-id: 9c8dcc44dbc8ea43bf79110f923389a180d4cb8a

--- src/org/exist/xquery/functions/util/UtilModule.java
+++ src/org/exist/xquery/functions/util/UtilModule.java
@@ -1,21 +1,21 @@
 /*
- * eXist Open Source Native XML Database
- * Copyright (C) 2001-09 The eXist Project
- * http://exist-db.org
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2001-2013 The eXist-db Project
+ *  http://exist-db.org
  *
- * This program is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public License
- * as published by the Free Software Foundation; either version 2
- * of the License, or (at your option) any later version.
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this program; if not, write to the Free Software Foundation
- * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
  *  $Id$
  */
@@ -144,7 +144,10 @@ public class UtilModule extends AbstractInternalModule {
         new FunctionDef(Wait.signatures[0], Wait.class),
         new FunctionDef(Base64Functions.signatures[0], Base64Functions.class),
         new FunctionDef(Base64Functions.signatures[1], Base64Functions.class),
-        new FunctionDef(Base64Functions.signatures[2], Base64Functions.class)     
+        new FunctionDef(Base64Functions.signatures[2], Base64Functions.class),
+        new FunctionDef(BaseConversionFunctions.FNS_INT_TO_OCTAL, BaseConversionFunctions.class),
+        new FunctionDef(BaseConversionFunctions.FNS_OCTAL_TO_INT, BaseConversionFunctions.class)
+            
     };
 
     static {
