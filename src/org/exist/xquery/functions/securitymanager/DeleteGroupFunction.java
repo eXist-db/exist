063ccfbcbe6085dc3bd1d2e89b398b772e2dd5aa
Adam Retter <adam.retter@googlemail.com>
2013-02-02 19:16:15 +0000

    [ignore] cleanup
    svn path=/trunk/eXist/; revision=18173
    
    
    Former-commit-id: 6ad7b043f0445d49b3b7cb09f600ec654e902e8b

--- src/org/exist/xquery/functions/securitymanager/DeleteGroupFunction.java
+++ src/org/exist/xquery/functions/securitymanager/DeleteGroupFunction.java
@@ -1,9 +1,31 @@
+/*
+ *  eXist-db SecurityManager Module Extension
+ *  Copyright (C) 2013 Adam Retter <adam@existsolutions.com>
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ *  $Id$
+ */
 package org.exist.xquery.functions.securitymanager;
 
 import org.exist.EXistException;
 import org.exist.dom.QName;
 import org.exist.security.Group;
 import org.exist.security.PermissionDeniedException;
+import org.exist.security.SecurityManager;
+import org.exist.security.Subject;
 import org.exist.xquery.BasicFunction;
 import org.exist.xquery.Cardinality;
 import org.exist.xquery.FunctionSignature;
@@ -13,46 +35,44 @@ import org.exist.xquery.value.FunctionParameterSequenceType;
 import org.exist.xquery.value.Sequence;
 import org.exist.xquery.value.SequenceType;
 import org.exist.xquery.value.Type;
-import org.exist.security.SecurityManager;
-import org.exist.security.Subject;
 
 /**
  *
- * @author aretter
+ * @author Adam Retter <adam.retter@googlemail.com>
  */
 public class DeleteGroupFunction extends BasicFunction {
 
-    public DeleteGroupFunction(XQueryContext context, FunctionSignature signature) {
+    public DeleteGroupFunction(final XQueryContext context, final FunctionSignature signature) {
         super(context, signature);
     }
 
-    public final static FunctionSignature signatures[] = {
-        new FunctionSignature(
-            new QName("delete-group", SecurityManagerModule.NAMESPACE_URI, SecurityManagerModule.PREFIX),
-            "Deletes an existing group identified by $group-id. Any resources owned by the group will be moved to the 'guest' group.",
-            new SequenceType[]{
-                new FunctionParameterSequenceType("group-id", Type.STRING, Cardinality.EXACTLY_ONE, "The group-id to delete")
-            },
-            new SequenceType(Type.ITEM, Cardinality.EMPTY)
-        ),
+    
+    public final static FunctionSignature FNS_DELETE_GROUP = new FunctionSignature(
+        new QName("delete-group", SecurityManagerModule.NAMESPACE_URI, SecurityManagerModule.PREFIX),
+        "Deletes an existing group identified by $group-id. Any resources owned by the group will be moved to the 'guest' group.",
+        new SequenceType[]{
+            new FunctionParameterSequenceType("group-id", Type.STRING, Cardinality.EXACTLY_ONE, "The group-id to delete")
+        },
+        new SequenceType(Type.ITEM, Cardinality.EMPTY)
+    );
 
-        new FunctionSignature(
-            new QName("delete-group", SecurityManagerModule.NAMESPACE_URI, SecurityManagerModule.PREFIX),
-            "Deletes an existing group identified by $group-id, any resources owned by the group will be moved to the group indicated by $successor-group-id.",
-            new SequenceType[]{
-                new FunctionParameterSequenceType("group-id", Type.STRING, Cardinality.EXACTLY_ONE, "The group-id to delete"),
-                new FunctionParameterSequenceType("successor-group-id", Type.STRING, Cardinality.EXACTLY_ONE, "The group-id that should take over ownership of any resources")
-            },
-            new SequenceType(Type.ITEM, Cardinality.EMPTY)
-        )
-    };
+    //TODO implement later
+    /* public final static FunctionSignature FNS_DELETE_GROUP_WITH_SUCCESSOR = new FunctionSignature(
+        new QName("delete-group", SecurityManagerModule.NAMESPACE_URI, SecurityManagerModule.PREFIX),
+        "Deletes an existing group identified by $group-id, any resources owned by the group will be moved to the group indicated by $successor-group-id.",
+        new SequenceType[]{
+            new FunctionParameterSequenceType("group-id", Type.STRING, Cardinality.EXACTLY_ONE, "The group-id to delete"),
+            new FunctionParameterSequenceType("successor-group-id", Type.STRING, Cardinality.EXACTLY_ONE, "The group-id that should take over ownership of any resources")
+        },
+        new SequenceType(Type.ITEM, Cardinality.EMPTY)
+    ); */
 
 
     @Override
-    public Sequence eval(Sequence[] args, Sequence contextSequence) throws XPathException {
+    public Sequence eval(final Sequence[] args, final Sequence contextSequence) throws XPathException {
 
-        SecurityManager sm = context.getBroker().getBrokerPool().getSecurityManager();
-        Subject currentSubject = context.getBroker().getSubject();
+        final SecurityManager sm = context.getBroker().getBrokerPool().getSecurityManager();
+        final Subject currentSubject = context.getBroker().getSubject();
 
         try {
             final String name = args[0].itemAt(0).getStringValue();
@@ -69,19 +89,15 @@ public class DeleteGroupFunction extends BasicFunction {
             	successorGroup = sm.getGroup("guest");
             }
 
-            //TODO how to handle user which are members of this group
-            //also how to reassign resources that are allocated to this group to another group
-
             try {
                 sm.deleteGroup(name);
-            } catch(EXistException ee) {
+            } catch(final EXistException ee) {
                 throw new XPathException(this, ee);
             }
 
             return Sequence.EMPTY_SEQUENCE;
-        } catch(PermissionDeniedException pde) {
+        } catch(final PermissionDeniedException pde) {
             throw new XPathException(this, pde);
         }
     }
-
 }
