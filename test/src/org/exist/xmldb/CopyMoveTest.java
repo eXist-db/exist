104531b813b4e77f9387c395ac0b48bd2733eefc
Dmitriy Shabanov <shabanovd@gmail.com>
2013-04-08 05:55:04 +0000

    [bugfix] after copy a resource and set permissions on the copy, original document permissions change to
    svn path=/trunk/eXist/; revision=18352
    
    
    Former-commit-id: 8d36f0fa33a65dc235ea7068196de2ea708fd5e6

--- test/src/org/exist/xmldb/CopyMoveTest.java
+++ test/src/org/exist/xmldb/CopyMoveTest.java
@@ -2,6 +2,7 @@ package org.exist.xmldb;
 
 import org.junit.After;
 import org.exist.security.Account;
+import org.exist.security.Permission;
 import org.junit.Before;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
@@ -44,6 +45,55 @@ public class CopyMoveTest {
         XPathQueryService xq = (XPathQueryService) testCollection.getService("XPathQueryService", "1.0");
         ResourceSet rs = xq.queryResource("duplicate", "/sample");
         assertEquals(1, rs.getSize());
+    }
+    
+    @Test
+    public void changePermissionsAfterCopy() throws XMLDBException {
+        final String collectionURL = ROOT_URI + "/" + TEST_COLLECTION;
+        final String originalResource = "original.xml";
+        final String copyResource = "copy.xml";
+        
+        final String resourceURL = collectionURL + "/" + originalResource;
+        
+        //get collection & services
+        CollectionImpl col = (CollectionImpl)DatabaseManager.getCollection(collectionURL);
+        CollectionManagementServiceImpl service = (CollectionManagementServiceImpl) col.getService("CollectionManagementService", "1.0");
+        UserManagementService ums = (UserManagementService)DatabaseManager.getCollection(collectionURL, ADMIN_UID, ADMIN_PWD).getService("UserManagementService", "1.0");
+        
+        //store xml document
+        XMLResource original = (XMLResource) col.createResource(originalResource, XMLResource.RESOURCE_TYPE);
+        original.setContent("<sample/>");
+        col.storeResource(original);
+
+        //get original resource
+        Resource orgnRes = col.getResource(originalResource);
+
+        //check permission before copy
+        Permission prm = ums.getPermissions(orgnRes);
+        assertEquals("rw-r--r--", prm.toString());
+        
+        //copy
+        service.copyResource(XmldbURI.create(resourceURL), col.getPathURI(), XmldbURI.create(copyResource));
+
+        //check permission after copy
+        prm = ums.getPermissions(orgnRes);
+        assertEquals("rw-r--r--", prm.toString());
+
+        //get copy resource
+        Resource copyRes = col.getResource(copyResource);
+        
+        //change permission on copy
+        Account admin = ums.getAccount(ADMIN_UID);
+        ums.chown(copyRes, admin, admin.getPrimaryGroup());
+        ums.chmod(copyRes, "rwx--x---");
+        
+        //check permission of copy
+        prm = ums.getPermissions(copyRes);
+        assertEquals("rwx--x---", prm.toString());
+
+        //check permission of original
+        prm = ums.getPermissions(orgnRes);
+        assertEquals("rw-r--r--", prm.toString());
     }
 
     @Before
