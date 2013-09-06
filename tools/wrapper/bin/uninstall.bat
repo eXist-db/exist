c167039db0b01b70da313c4fb335439efdea8abd
Dannes Wessels <dannes@exist-db.org>
2013-09-06 17:39:26 +0200

    [lib-upgrade] upgrading to Java Service Wrapper Community Edition 3.5.20 - including 64bit windows support - synced scripts with originals
    release notes on http://wrapper.tanukisoftware.com/doc/english/release-notes.html#3.5.20 (jumped from .17 to .20)

--- tools/wrapper/bin/uninstall.bat
+++ tools/wrapper/bin/uninstall.bat
@@ -2,7 +2,7 @@
 setlocal
 
 rem
-rem Copyright (c) 1999, 2012 Tanuki Software, Ltd.
+rem Copyright (c) 1999, 2013 Tanuki Software, Ltd.
 rem http://www.tanukisoftware.com
 rem All rights reserved.
 rem
@@ -16,15 +16,23 @@ rem
 
 rem -----------------------------------------------------------------------------
 rem These settings can be modified to fit the needs of your application
-rem Optimized for use with version 3.5.17 of the Wrapper.
+rem Optimized for use with version 3.5.20 of the Wrapper.
 
 rem The base name for the Wrapper binary.
 set _WRAPPER_BASE=wrapper
 
+rem The directory where the Wrapper binary (.exe) file is located, this can be
+rem either a absolute or relative path. If the path contains any special characters, 
+rem please make sure to quote the variable.
+set _WRAPPER_DIR=
+
 rem The name and location of the Wrapper configuration file.   This will be used
 rem  if the user does not specify a configuration file as the first parameter to
-rem  this script.
-set _WRAPPER_CONF_DEFAULT=../conf/wrapper.conf
+rem  this script.  It will not be possible to specify a configuration file on the
+rem  command line if _PASS_THROUGH is set. 
+rem If a relative path is specified, please note that the location is based on the 
+rem location.
+set _WRAPPER_CONF_DEFAULT="../conf/%_WRAPPER_BASE%.conf"
 
 rem Makes it possible to override the Wrapper configuration file by specifying it
 rem  as the first parameter.
@@ -46,31 +54,46 @@ echo This script only works with NT-based versions of Windows.
 goto :eof
 
 :nt
-rem
 rem Find the application home.
-rem
-rem %~dp0 is location of current script under NT
-set _REALPATH=%~dp0
+rem if no path path specified do the default action
+IF not DEFINED _WRAPPER_DIR goto dir_undefined
+set _WRAPPER_DIR_QUOTED="%_WRAPPER_DIR:"=%"
+if not "%_WRAPPER_DIR:~-2,1%" == "\" set _WRAPPER_DIR_QUOTED="%_WRAPPER_DIR_QUOTED:"=%\"
+rem check if absolute path
+if "%_WRAPPER_DIR_QUOTED:~2,1%" == ":" goto absolute_path
+if "%_WRAPPER_DIR_QUOTED:~1,1%" == "\" goto absolute_path
+rem everythig else means relative path
+set _REALPATH="%~dp0%_WRAPPER_DIR_QUOTED:"=%"
+goto pathfound
 
+:dir_undefined
+rem Use a relative path to the wrapper %~dp0 is location of current script under NT
+set _REALPATH="%~dp0"
+goto pathfound
+:absolute_path
+rem Use an absolute path to the wrapper
+set _REALPATH="%_WRAPPER_DIR_QUOTED:"=%"
+
+:pathfound
 rem
 rem Decide on the specific Wrapper binary to use (See delta-pack)
 rem
 if "%PROCESSOR_ARCHITEW6432%"=="AMD64" goto amd64
 if "%PROCESSOR_ARCHITECTURE%"=="AMD64" goto amd64
 if "%PROCESSOR_ARCHITECTURE%"=="IA64" goto ia64
-set _WRAPPER_L_EXE=%_REALPATH%%_WRAPPER_BASE%-windows-x86-32.exe
+set _WRAPPER_L_EXE="%_REALPATH:"=%%_WRAPPER_BASE%-windows-x86-32.exe"
 goto search
 :amd64
-set _WRAPPER_L_EXE=%_REALPATH%%_WRAPPER_BASE%-windows-x86-64.exe
+set _WRAPPER_L_EXE="%_REALPATH:"=%%_WRAPPER_BASE%-windows-x86-64.exe"
 goto search
 :ia64
-set _WRAPPER_L_EXE=%_REALPATH%%_WRAPPER_BASE%-windows-ia-64.exe
+set _WRAPPER_L_EXE="%_REALPATH:"=%%_WRAPPER_BASE%-windows-ia-64.exe"
 goto search
 :search
-set _WRAPPER_EXE=%_WRAPPER_L_EXE%
-if exist "%_WRAPPER_EXE%" goto conf
-set _WRAPPER_EXE=%_REALPATH%%_WRAPPER_BASE%.exe
-if exist "%_WRAPPER_EXE%" goto conf
+set _WRAPPER_EXE="%_WRAPPER_L_EXE:"=%"
+if exist %_WRAPPER_EXE% goto conf
+set _WRAPPER_EXE="%_REALPATH:"=%%_WRAPPER_BASE%.exe"
+if exist %_WRAPPER_EXE% goto conf
 echo Unable to locate a Wrapper executable using any of the following names:
 echo %_WRAPPER_L_EXE%
 echo %_WRAPPER_EXE%
@@ -88,13 +111,23 @@ if not [%_WRAPPER_CONF_OVERRIDE%]==[] (
         goto :startup
     )
 )
-set _WRAPPER_CONF="%_WRAPPER_CONF_DEFAULT%"
+set _WRAPPER_CONF="%_WRAPPER_CONF_DEFAULT:"=%"
 
 rem
 rem Start the Wrapper
 rem
 :startup
-"%_WRAPPER_EXE%" -r %_WRAPPER_CONF%
+
+rem Collect an parameters
+:parameters
+set _PARAMETERS=%_PARAMETERS% %1
+shift
+if not [%1]==[] goto :parameters
+
+if [%_PASS_THROUGH%]==[] (
+    %_WRAPPER_EXE% -r %_WRAPPER_CONF%
+) else (
+    %_WRAPPER_EXE% -r %_WRAPPER_CONF% -- %_PARAMETERS%
+)
 if not errorlevel 1 goto :eof
 pause
-
