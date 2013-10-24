bbc475dfb83b49676e16e6a3d3bb18a2937e344a
Dmitriy Shabanov <shabanovd@gmail.com>
2013-10-24 12:24:15 +0400

    refactor configurator
    Former-commit-id: 5e0158b0b8d74184d5ba33f14fdfbcd9e53ee023

--- src/org/exist/config/ProxyElement.java
+++ src/org/exist/config/ProxyElement.java
@@ -1,6 +1,6 @@
 /*
  * eXist Open Source Native XML Database
- * Copyright (C) 2008-2012 The eXist Project
+ * Copyright (C) 2008-2013 The eXist Project
  * http://exist-db.org
  *
  * This program is free software; you can redistribute it and/or
