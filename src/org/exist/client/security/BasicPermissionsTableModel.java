99e60a84808024acfca9038037aea6ed0224dcbc
Adam Retter <adam.retter@googlemail.com>
2013-11-14 15:49:01 +0000

    [feature] SetUID, SetGID and Sticky bit can now be set/unset via the Resource Properties dialog in the Java Admin Client

--- src/org/exist/client/security/BasicPermissionsTableModel.java
+++ src/org/exist/client/security/BasicPermissionsTableModel.java
@@ -1,6 +1,6 @@
 /*
  *  eXist Open Source Native XML Database
- *  Copyright (C) 2001-2012 The eXist Project
+ *  Copyright (C) 2013 The eXist Project
  *  http://exist-db.org
  *
  *  This program is free software; you can redistribute it and/or
@@ -21,7 +21,9 @@
  */
 package org.exist.client.security;
 
+import javax.swing.JCheckBox;
 import javax.swing.table.DefaultTableModel;
+import org.exist.client.LabelledBoolean;
 import org.exist.security.Permission;
 
 /**
@@ -34,36 +36,45 @@ public class BasicPermissionsTableModel extends DefaultTableModel {
         
         super(
             new Object [][] {
-                new Object[] {"User", (permission.getOwnerMode() & Permission.READ) == Permission.READ, (permission.getOwnerMode() & Permission.WRITE) == Permission.WRITE, (permission.getOwnerMode() & Permission.EXECUTE) == Permission.EXECUTE},
-                new Object[] {"Group", (permission.getGroupMode() & Permission.READ) == Permission.READ, (permission.getGroupMode() & Permission.WRITE) == Permission.WRITE, (permission.getGroupMode() & Permission.EXECUTE) == Permission.EXECUTE},
-                new Object[] {"Other", (permission.getOtherMode() & Permission.READ) == Permission.READ, (permission.getOtherMode() & Permission.WRITE) == Permission.WRITE, (permission.getOtherMode() & Permission.EXECUTE) == Permission.EXECUTE}
+                new Object[] {"User", (permission.getOwnerMode() & Permission.READ) == Permission.READ, (permission.getOwnerMode() & Permission.WRITE) == Permission.WRITE, (permission.getOwnerMode() & Permission.EXECUTE) == Permission.EXECUTE, new LabelledBoolean("SetUID", permission.isSetUid())},
+                new Object[] {"Group", (permission.getGroupMode() & Permission.READ) == Permission.READ, (permission.getGroupMode() & Permission.WRITE) == Permission.WRITE, (permission.getGroupMode() & Permission.EXECUTE) == Permission.EXECUTE, new LabelledBoolean("SetGID", permission.isSetGid())},
+                new Object[] {"Other", (permission.getOtherMode() & Permission.READ) == Permission.READ, (permission.getOtherMode() & Permission.WRITE) == Permission.WRITE, (permission.getOtherMode() & Permission.EXECUTE) == Permission.EXECUTE, new LabelledBoolean("Sticky", permission.isSticky())}
             },
             new String [] {
-                "Permission", "Read", "Write", "Execute"
+                "Permission", "Read", "Write", "Execute", "Special"
             }
         );
     }
            
     final Class[] types = new Class [] {
-        java.lang.String.class, java.lang.Boolean.class, java.lang.Boolean.class, java.lang.Boolean.class
+        java.lang.String.class, java.lang.Boolean.class, java.lang.Boolean.class, java.lang.Boolean.class, LabelledBoolean.class
     };
 
     boolean[] canEdit = new boolean [] {
-        false, true, true, true
+        false, true, true, true, true
     };
 
     @Override
     public Class getColumnClass(int columnIndex) {
-        return types [columnIndex];
+        return types[columnIndex];
     }
 
     @Override
     public boolean isCellEditable(int rowIndex, int columnIndex) {
-        return canEdit [columnIndex];
+        return canEdit[columnIndex];
     }
+
+    @Override
+    public void setValueAt(Object aValue, int row, int column) {
+        super.setValueAt(aValue, row, column); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+    
     
     /**
      * Get the Mode described by the table model
+     * 
+     * The Unix mode of the permissions
      */
     public int getMode() {
         int mode = 0;
@@ -81,6 +92,16 @@ public class BasicPermissionsTableModel extends DefaultTableModel {
             if(i != getRowCount() - 1) {
                 mode <<= 3;
             }
+        }
+        
+        if(((LabelledBoolean)getValueAt(0, 4)).isSet()) {
+            mode |= (Permission.SET_UID << 9);
+        }
+        if(((LabelledBoolean)getValueAt(1, 4)).isSet()) {
+            mode |= (Permission.SET_GID << 9);
+        }
+        if(((LabelledBoolean)getValueAt(2, 4)).isSet()) {
+            mode |= (Permission.STICKY << 9);
         }
         
         return mode;
