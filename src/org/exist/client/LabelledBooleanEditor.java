20fc062b4bc195a380380adebccd14e4f6f9d245
Adam Retter <adam.retter@googlemail.com>
2013-11-14 15:49:01 +0000

    [feature] SetUID, SetGID and Sticky bit can now be set/unset via the Resource Properties dialog in the Java Admin Client
    Former-commit-id: 99e60a84808024acfca9038037aea6ed0224dcbc

---
+++ src/org/exist/client/LabelledBooleanEditor.java
@@ -0,0 +1,65 @@
+/*
+ *  eXist Open Source Native XML Database
+ *  Copyright (C) 2013 The eXist Project
+ *  http://exist-db.org
+ *
+ *  This program is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public License
+ *  as published by the Free Software Foundation; either version 2
+ *  of the License, or (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU Lesser General Public License for more details.
+ *
+ *  You should have received a copy of the GNU Lesser General Public
+ *  License along with this library; if not, write to the Free Software
+ *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  $Id$
+ */
+package org.exist.client;
+
+import java.awt.Component;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JTable;
+import javax.swing.table.TableCellEditor;
+import javax.swing.AbstractCellEditor;
+import javax.swing.JCheckBox;
+import javax.swing.SwingConstants;
+
+/**
+ * Editor for a LabelledBoolean using a JCheckBox
+ *
+ * @author Adam Retter <adam@existsolutions.com>
+ */
+public class LabelledBooleanEditor extends AbstractCellEditor implements TableCellEditor {
+
+    private LabelledBoolean current;
+    
+    @Override
+    public Object getCellEditorValue() {
+        return current;
+    }
+
+    @Override
+    public Component getTableCellEditorComponent(final JTable table, final Object value, final boolean isSelected, final int row, final int column) {
+        final LabelledBoolean lb = (LabelledBoolean)value;
+        final JCheckBox chkBox = new JCheckBox(lb.getLabel(), lb.isSet());
+        
+        chkBox.setHorizontalAlignment(SwingConstants.LEFT);
+        chkBox.setHorizontalTextPosition(SwingConstants.RIGHT);
+        
+        chkBox.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(final ActionEvent e) {
+                current = lb.copy(!lb.isSet());
+                fireEditingStopped(); //notify that editing is done!
+            }
+        });
+        
+        return chkBox;
+    }
+}
