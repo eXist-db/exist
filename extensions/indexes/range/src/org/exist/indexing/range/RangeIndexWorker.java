0ebca3a5aee77939298d811160e09b7368c7184f
Wolfgang Meier <wolfgangmm@gmail.com>
2014-01-27 15:34:02 +0100

    [bugfix] Nodes were removed from indexes multiple times when using node-level update expressions.
    Former-commit-id: ef1d13f98ee61d6422990bcfd888055d305c17f5

--- extensions/indexes/range/src/org/exist/indexing/range/RangeIndexWorker.java
+++ extensions/indexes/range/src/org/exist/indexing/range/RangeIndexWorker.java
@@ -255,8 +255,8 @@ public class RangeIndexWorker implements OrderedValuesIndex, QNamedKeysIndex {
 
     @Override
     public StoredNode getReindexRoot(StoredNode node, NodePath path, boolean includeSelf) {
-        if (node.getNodeType() == Node.ATTRIBUTE_NODE)
-            return null;
+//        if (node.getNodeType() == Node.ATTRIBUTE_NODE)
+//            return null;
         if (config == null)
             return null;
         NodePath p = new NodePath(path);
@@ -387,6 +387,7 @@ public class RangeIndexWorker implements OrderedValuesIndex, QNamedKeysIndex {
         } catch (IOException e) {
             LOG.warn("Error while deleting lucene index entries: " + e.getMessage(), e);
         } finally {
+            nodesToRemove = null;
             index.releaseWriter(writer);
         }
     }
