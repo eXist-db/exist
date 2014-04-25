f07b887b60324e46616c5a529db0108fa9a6a97d
Wolfgang Meier <wolfgangmm@gmail.com>
2014-04-25 12:07:04 +0200

    [ignore] range:index-keys-for-field: add parameter to define max returned items to make it consistent with util:index-keys.
    Former-commit-id: 2264e0cc23618187f868f6b6677542699d6e27ce

--- extensions/indexes/range/src/org/exist/xquery/modules/range/IndexKeys.java
+++ extensions/indexes/range/src/org/exist/xquery/modules/range/IndexKeys.java
@@ -23,7 +23,23 @@ public class IndexKeys extends BasicFunction {
                         "c) the current position of the key in the whole list of keys returned."),
                 new FunctionParameterSequenceType("max-number-returned", Type.INT, Cardinality.ZERO_OR_ONE , "The maximum number of returned keys")
             },
-            new FunctionReturnSequenceType(Type.ITEM, Cardinality.ZERO_OR_MORE, "the results of the eval of the $function-reference"))
+            new FunctionReturnSequenceType(Type.ITEM, Cardinality.ZERO_OR_MORE, "the results of the eval of the $function-reference")),
+        new FunctionSignature(
+                new QName("index-keys-for-field", RangeIndexModule.NAMESPACE_URI, RangeIndexModule.PREFIX),
+                "Retrieve all index keys contained in a range index which has been defined with a field name. Similar to" +
+                        "util:index-keys, but works with fields.",
+                new SequenceType[] {
+                        new FunctionParameterSequenceType("field", Type.STRING, Cardinality.EXACTLY_ONE, "The field to use"),
+                        new FunctionParameterSequenceType("start-value", Type.ATOMIC, Cardinality.ZERO_OR_ONE, "Only index keys of the same type but being greater than $start-value will be reported for non-string types. For string types, only keys starting with the given prefix are reported."),
+                        new FunctionParameterSequenceType("function-reference", Type.FUNCTION_REFERENCE, Cardinality.EXACTLY_ONE, "The function reference as created by the util:function function. " +
+                                "It can be an arbitrary user-defined function, but it should take exactly 2 arguments: " +
+                                "1) the current index key as found in the range index as an atomic value, 2) a sequence " +
+                                "containing three int values: a) the overall frequency of the key within the node set, " +
+                                "b) the number of distinct documents in the node set the key occurs in, " +
+                                "c) the current position of the key in the whole list of keys returned."),
+                        new FunctionParameterSequenceType("max-number-returned", Type.INT, Cardinality.ZERO_OR_ONE , "The maximum number of returned keys")
+                },
+                new FunctionReturnSequenceType(Type.ITEM, Cardinality.ZERO_OR_MORE, "the results of the eval of the $function-reference"))
     };
 
     public IndexKeys(XQueryContext context, FunctionSignature signature) {
@@ -32,16 +48,21 @@ public class IndexKeys extends BasicFunction {
 
     @Override
     public Sequence eval(final Sequence[] args, final Sequence contextSequence) throws XPathException {
-        final String field = args[0].getStringValue();
-        final FunctionReference ref = (FunctionReference) args[1].itemAt(0);
+        int arg = 0;
+        final String field = args[arg++].getStringValue();
+        String start = null;
+        if (args.length == 4) {
+            start = args[arg++].getStringValue();
+        }
+        final FunctionReference ref = (FunctionReference) args[arg++].itemAt(0);
         int max = -1;
-        if (!args[2].isEmpty()) {
-            max = ((IntegerValue)args[2].itemAt(0)).getInt();
+        if (!args[arg].isEmpty()) {
+            max = ((IntegerValue)args[arg].itemAt(0)).getInt();
         }
 
         final Sequence result = new ValueSequence();
         final RangeIndexWorker worker = (RangeIndexWorker) context.getBroker().getIndexController().getWorkerByIndexName("range-index");
-        Occurrences[] occur = worker.scanIndexByField(field, contextSequence == null ? context.getStaticallyKnownDocuments() : contextSequence.getDocumentSet(), max);
+        Occurrences[] occur = worker.scanIndexByField(field, contextSequence == null ? context.getStaticallyKnownDocuments() : contextSequence.getDocumentSet(), start, max);
         final int len = (max != -1 && occur.length > max ? max : occur.length);
         final Sequence params[] = new Sequence[2];
         ValueSequence data = new ValueSequence();
