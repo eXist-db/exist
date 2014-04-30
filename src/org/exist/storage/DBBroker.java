f4a984c84557a7c6fdd3aa4b50b625bfd7c96da2
Dmitriy Shabanov <shabanovd@gmail.com>
2014-04-30 15:58:47 +0400

    missing @Override
    Former-commit-id: 3f4acad17d5d738426ff61c4547be56cb0b4c647

--- src/org/exist/storage/DBBroker.java
+++ src/org/exist/storage/DBBroker.java
@@ -840,6 +840,7 @@ public abstract class DBBroker extends Observable implements AutoCloseable {
 
     public abstract void readCollectionEntry(SubCollectionEntry entry);
 
+    @Override
     public void close() throws Exception {
         pool.release(this);
     }
