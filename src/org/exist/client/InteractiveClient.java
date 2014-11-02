ef76413490f9a137263dd3858930f6dfc26f0f1f
Dannes Wessels <dannes@exist-db.org>
2014-11-02 20:13:22 +0100

    Remove helper text from client ; I was convinced the text was removed ages ago. see exist-open http://exist.markmail.org/thread/ena7vtayc52bw24l
    Former-commit-id: e819b95cf05219c99101bfc7df524cf5f397600a

--- src/org/exist/client/InteractiveClient.java
+++ src/org/exist/client/InteractiveClient.java
@@ -252,9 +252,6 @@ public class InteractiveClient {
         messageln("rmcol collection     remove collection");
         messageln("set [key=value]      set property. Calling set without ");
         messageln("                     argument shows current settings.");
-        messageln("validate [document]  validate xml document with system xml catalog.");
-        messageln("validate [document] [grammar]  validate xml document with ");
-        messageln("                     specified grammar document.");
         messageln(EOL + "--- search commands ---");
         messageln("find xpath-expr      execute the given XPath expression.");
         messageln("show [position]      display query result value at position.");
