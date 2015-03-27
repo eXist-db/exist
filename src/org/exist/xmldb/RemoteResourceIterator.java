c9bc5ad65b346819415d8854d51d3a0a9eed6bff
Adam Retter <adam.retter@googlemail.com>
2015-03-27 08:56:32 +0000

    [refactor] Cleaned up the XMLDB Remote API implementation. Fixed several small bugs along the way related to previous coding mistakes and copy-pasta errors
    Former-commit-id: e913415d4be03a3bb5f023d0417061a4d7b32723

--- src/org/exist/xmldb/RemoteResourceIterator.java
+++ src/org/exist/xmldb/RemoteResourceIterator.java
@@ -21,10 +21,11 @@ package org.exist.xmldb;
 
 import java.io.IOException;
 import java.net.URISyntaxException;
-import java.util.Vector;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
 
 import org.apache.xmlrpc.XmlRpcException;
-import org.apache.xmlrpc.client.XmlRpcClient;
 import org.xmldb.api.base.ErrorCodes;
 import org.xmldb.api.base.Resource;
 import org.xmldb.api.base.ResourceIterator;
@@ -33,67 +34,67 @@ import org.xmldb.api.modules.XMLResource;
 
 public class RemoteResourceIterator implements ResourceIterator {
 
-	protected XmlRpcClient rpcClient;
-	protected RemoteCollection collection;
-	protected Vector<Object> resources;
-	protected int pos = 0 ;
-	protected int indentXML;
-	protected String encoding = "UTF-8";
+    private final RemoteCollection collection;
+    private final List resources;
+    private final int indentXML;
+    private final String encoding;
+    private int pos = 0;
 
-	public RemoteResourceIterator(RemoteCollection col, Vector<Object> resources, 
-								int indentXML, String encoding) {
-		this.resources = resources;
-		this.collection = col;
-		this.indentXML = indentXML;
-		this.encoding = encoding;
-	}
+    public RemoteResourceIterator(final RemoteCollection collection, final List resources, final int indentXML, final String encoding) {
+        this.collection = collection;
+        this.resources = resources;
+        this.indentXML = indentXML;
+        this.encoding = encoding;
+    }
 
-	public int getLength() {
-		return resources.size();
-	}
+    public int getLength() {
+        return resources.size();
+    }
 
-	public boolean hasMoreResources() throws XMLDBException {
-		return pos < resources.size();
-	}
+    @Override
+    public boolean hasMoreResources() throws XMLDBException {
+        return pos < resources.size();
+    }
 
     public void setNext(int next) {
         pos = next;
     }
-    
-	@SuppressWarnings("unchecked")
-	public Resource nextResource() throws XMLDBException {
-        if(pos >= resources.size())
-            {return null;}
+
+    @SuppressWarnings("unchecked")
+    @Override
+    public Resource nextResource() throws XMLDBException {
+        if (pos >= resources.size()) {
+            return null;
+        }
         // node or value?
-        if(resources.elementAt(pos) instanceof Vector<?>) {
+        if (resources.get(pos) instanceof List) {
             // node
-            final Vector<String> v = (Vector<String>)resources.elementAt(pos++);
-            final String doc = v.elementAt(0);
-            final String s_id = v.elementAt(1);
-            
-            final Vector<Object> params = new Vector<Object>();
-            params.addElement(doc);
-            params.addElement(s_id);
-            params.addElement(Integer.valueOf(indentXML));
-            params.addElement(encoding);
+            final List<String> v = (List<String>) resources.get(pos++);
+            final String doc = v.get(0);
+            final String s_id = v.get(1);
+
+            final List<Object> params = new ArrayList<>();
+            params.add(doc);
+            params.add(s_id);
+            params.add(indentXML);
+            params.add(encoding);
             try {
-                final byte[] data = (byte[])collection.getClient().execute("retrieve", params);
-                final XMLResource res = new RemoteXMLResource(collection, XmldbURI.xmldbUriFor(doc), doc + "_" + s_id);
+                final byte[] data = (byte[]) collection.getClient().execute("retrieve", params);
+                final XMLResource res = new RemoteXMLResource(collection, XmldbURI.xmldbUriFor(doc), Optional.of(doc + "_" + s_id));
                 res.setContent(new String(data, encoding));
                 return res;
-            } catch(final XmlRpcException xre) {
+            } catch (final XmlRpcException xre) {
                 throw new XMLDBException(ErrorCodes.INVALID_RESOURCE, xre.getMessage(), xre);
-            } catch(final IOException ioe) {
+            } catch (final IOException ioe) {
                 throw new XMLDBException(ErrorCodes.VENDOR_ERROR, ioe.getMessage(), ioe);
-			} catch (final URISyntaxException e) {
+            } catch (final URISyntaxException e) {
                 throw new XMLDBException(ErrorCodes.INVALID_URI, e.getMessage(), e);
-			}
+            }
         } else {
             // value
-            final XMLResource res = new RemoteXMLResource(collection, null, Integer.toString(pos));
-            res.setContent(resources.elementAt(pos++));
+            final XMLResource res = new RemoteXMLResource(collection, null, Optional.of(Integer.toString(pos)));
+            res.setContent(resources.get(pos++));
             return res;
         }
     }
 }
-	
