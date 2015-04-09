176b2fef90464f84b7c702c38dbf4e31db66281b
Adam Retter <adam.retter@googlemail.com>
2015-04-09 15:48:49 +0100

    Test proof for https://github.com/eXist-db/exist/issues/583
    Former-commit-id: f910822e34fc06e25fb4ca3399febd3417324af8

--- test/src/org/exist/security/SecurityManagerRoundtripTest.java
+++ test/src/org/exist/security/SecurityManagerRoundtripTest.java
@@ -1,6 +1,8 @@
 package org.exist.security;
 
 import java.util.Arrays;
+
+import org.exist.TestUtils;
 import org.exist.xmldb.UserManagementService;
 import org.xmldb.api.DatabaseManager;
 import org.xmldb.api.base.Collection;
@@ -59,6 +61,7 @@ public class SecurityManagerRoundtripTest {
 
     @After
     public void stopServer() {
+        TestUtils.cleanupDB();
         server.shutdown();
         server = null;
     }
@@ -106,6 +109,60 @@ public class SecurityManagerRoundtripTest {
             assertEquals(2, groups.length);
             assertEquals(group1Name, groups[0]);
             assertEquals(group2Name, groups[1]);
+
+        } finally {
+            //cleanup
+            try { ums.removeGroup(group1); } catch(Exception e) {}
+            try { ums.removeGroup(group2); } catch(Exception e) {}
+            try { ums.removeAccount(user); } catch(Exception e) {}
+        }
+    }
+
+    @Test
+    public void checkPrimaryGroupRemainsDBA() throws XMLDBException, PermissionDeniedException {
+
+        Collection root = DatabaseManager.getCollection(baseUri + "/db", "admin", "");
+        UserManagementService ums = (UserManagementService)root.getService("UserManagementService", "1.0");
+
+        final String group1Name = "testGroup1";
+        final String group2Name = "testGroup2";
+        final String userName = "testUser";
+        Group group1 = new GroupAider(group1Name);
+        Group group2 = new GroupAider(group2Name);
+        Account user = new UserAider(userName, ums.getGroup(SecurityManager.DBA_GROUP)); //set users primary group as DBA
+
+        try {
+            ums.addGroup(group1);
+            ums.addGroup(group2);
+
+            ums.addAccount(user);
+            ums.getAccount(userName);
+            user.addGroup(group1);
+            user.addGroup(group2);
+
+            ums.updateAccount(user);
+
+            /*** RESTART THE SERVER ***/
+            stopServer();
+            startServer();
+            /**************************/
+
+            root = DatabaseManager.getCollection(baseUri + "/db", "admin", "");
+            ums = (UserManagementService)root.getService("UserManagementService", "1.0");
+
+            user = ums.getAccount("testUser");
+            assertNotNull(user);
+
+            Group defaultGroup = user.getDefaultGroup();
+            assertNotNull(defaultGroup);
+            assertEquals(SecurityManager.DBA_GROUP, defaultGroup.getName());
+
+            String groups[] = user.getGroups();
+            assertNotNull(groups);
+            assertEquals(3, groups.length);
+            assertEquals(SecurityManager.DBA_GROUP, groups[0]);
+            assertEquals(group1Name, groups[1]);
+            assertEquals(group2Name, groups[2]);
 
         } finally {
             //cleanup
