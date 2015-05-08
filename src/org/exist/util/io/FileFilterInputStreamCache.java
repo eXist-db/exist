18274df27664f111add80abcafc8fcc405094692
Tobi Krebs <tobias.krebs@betterform.de>
2015-05-08 13:32:58 +0200

    First Version rewrite InputStreamCache

--- src/org/exist/util/io/FileFilterInputStreamCache.java
+++ src/org/exist/util/io/FileFilterInputStreamCache.java
@@ -1,79 +1,83 @@
 /*
-Copyright (c) 2012, Adam Retter
-All rights reserved.
+ Copyright (c) 2012, Adam Retter
+ All rights reserved.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of Adam Retter Consulting nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of Adam Retter Consulting nor the
+ names of its contributors may be used to endorse or promote products
+ derived from this software without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL Adam Retter BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL Adam Retter BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package org.exist.util.io;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.RandomAccessFile;
 
 /**
- * Cache implementation for CachingFilterInputStream
- * Backed by a Random Access File
- * 
- * Probably slower than MemoryMappedFileFilterInputStreamCache
- * for multiple reads, but uses a fixed small amount of memory.
+ * Cache implementation for CachingFilterInputStream Backed by a Random Access
+ * File
  *
- * @version 1.0
+ * Probably slower than MemoryMappedFileFilterInputStreamCache for multiple
+ * reads, but uses a fixed small amount of memory.
+ *
+ * @version 1.1
  *
  * @author Adam Retter <adam.retter@googlemail.com>
+ * @author Tobi Krebs <tobi.krebs AT gmail.com>
  */
-public class FileFilterInputStreamCache implements FilterInputStreamCache {
+public class FileFilterInputStreamCache extends AbstractFilterInputStreamCache {
+
     private final File tempFile;
     private final boolean externalFile;
     private int length = 0;
     private int offset = 0;
-    
+
     private final RandomAccessFile raf;
 
-    public FileFilterInputStreamCache() throws IOException {
-        this(null);
+    public FileFilterInputStreamCache(InputStream src) throws IOException {
+        this(src, null);
     }
-    
-    public FileFilterInputStreamCache(final File f) throws IOException {
-         if(f == null) {
+
+    public FileFilterInputStreamCache(InputStream src, final File f) throws IOException {
+        super(src);
+
+        if (f == null) {
             tempFile = TemporaryFileManager.getInstance().getTemporaryFile();
             externalFile = false;
         } else {
             tempFile = f;
             externalFile = true;
         }
-         
+
         this.raf = new RandomAccessFile(tempFile, "rw");
     }
-    
-    
+
     @Override
     public void write(final byte[] b, final int off, final int len) throws IOException {
         //force writing to be append only
-        if(offset != length) {
+        if (offset != length) {
             raf.seek(length);
             offset = length;
         }
-        
+
         raf.write(b, off, len);
         length += len;
         offset += len;
@@ -82,11 +86,11 @@ public class FileFilterInputStreamCache implements FilterInputStreamCache {
     @Override
     public void write(final int i) throws IOException {
         //force writing to be append only
-        if(offset != length) {
+        if (offset != length) {
             raf.seek(length);
             offset = length;
         }
-        
+
         raf.write(i);
         length++;
         offset++;
@@ -99,7 +103,7 @@ public class FileFilterInputStreamCache implements FilterInputStreamCache {
 
     @Override
     public byte get(final int off) throws IOException {
-        if(off != offset) {
+        if (off != offset) {
             raf.seek(off);
             this.offset = off;
         }
@@ -108,7 +112,7 @@ public class FileFilterInputStreamCache implements FilterInputStreamCache {
 
     @Override
     public void copyTo(final int cacheOffset, final byte[] b, final int off, final int len) throws IOException {
-        if(cacheOffset != offset) {
+        if (cacheOffset != offset) {
             raf.seek(cacheOffset);
             this.offset = cacheOffset;
         }
@@ -117,11 +121,11 @@ public class FileFilterInputStreamCache implements FilterInputStreamCache {
 
     @Override
     public void invalidate() throws IOException {
-        
+
         raf.close();
-        
-        if(tempFile != null && (!externalFile)) {
-           TemporaryFileManager.getInstance().returnTemporaryFile(tempFile);
+
+        if (tempFile != null && (!externalFile)) {
+            TemporaryFileManager.getInstance().returnTemporaryFile(tempFile);
         }
     }
 }
--- src/org/exist/util/io/MemoryMappedFileFilterInputStreamCache.java
+++ src/org/exist/util/io/MemoryMappedFileFilterInputStreamCache.java
@@ -1,47 +1,49 @@
 /*
-Copyright (c) 2012, Adam Retter
-All rights reserved.
+ Copyright (c) 2012, Adam Retter
+ All rights reserved.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
-    * Redistributions of source code must retain the above copyright
-      notice, this list of conditions and the following disclaimer.
-    * Redistributions in binary form must reproduce the above copyright
-      notice, this list of conditions and the following disclaimer in the
-      documentation and/or other materials provided with the distribution.
-    * Neither the name of Adam Retter Consulting nor the
-      names of its contributors may be used to endorse or promote products
-      derived from this software without specific prior written permission.
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
+ * Redistributions of source code must retain the above copyright
+ notice, this list of conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright
+ notice, this list of conditions and the following disclaimer in the
+ documentation and/or other materials provided with the distribution.
+ * Neither the name of Adam Retter Consulting nor the
+ names of its contributors may be used to endorse or promote products
+ derived from this software without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
-ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
-WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL Adam Retter BE LIABLE FOR ANY
-DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
-(INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
-LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
-ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
-(INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL Adam Retter BE LIABLE FOR ANY
+ DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 package org.exist.util.io;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.RandomAccessFile;
 import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
 
 /**
- * Cache implementation for CachingFilterInputStream
- * Backed by a Memory Mapped File
+ * Cache implementation for CachingFilterInputStream Backed by a Memory Mapped
+ * File
  *
- * @version 1.0
+ * @version 1.1
  *
  * @author Adam Retter <adam.retter@googlemail.com>
+ * @author Tobi Krebs <tobi.krebs AT gmail.com>
  */
-public class MemoryMappedFileFilterInputStreamCache implements FilterInputStreamCache {
+public class MemoryMappedFileFilterInputStreamCache extends AbstractFilterInputStreamCache {
 
     private final static long DEFAULT_MEMORY_MAP_SIZE = 64 * 1024 * 1024; //64MB
 
@@ -50,32 +52,37 @@ public class MemoryMappedFileFilterInputStreamCache implements FilterInputStream
     private MappedByteBuffer buf;
     private File tempFile = null;
     private final long memoryMapSize = DEFAULT_MEMORY_MAP_SIZE;
-    
+
     private boolean externalFile = true;
 
-    public MemoryMappedFileFilterInputStreamCache() throws IOException {
-        this(null);
+    public MemoryMappedFileFilterInputStreamCache(InputStream src) throws IOException {
+        this(src, null);
     }
-    
-    public MemoryMappedFileFilterInputStreamCache(final File f) throws FileNotFoundException, IOException {
-        
-        if(f == null) {
+
+    public MemoryMappedFileFilterInputStreamCache(InputStream src, final File f) throws IOException {
+        super(src);
+
+        if (f == null) {
             tempFile = TemporaryFileManager.getInstance().getTemporaryFile();
             externalFile = false;
         } else {
             tempFile = f;
             externalFile = true;
         }
-        
+
         /**
          * Check the applicability of these bugs to this code:
-         *  http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4724038
-         *  http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6417205 (fixed in 1.6)
+         * http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4724038
+         * http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=6417205 (fixed in
+         * 1.6)
          */
-        
-        this.raf =  new RandomAccessFile(tempFile, "rw");
+        this.raf = new RandomAccessFile(tempFile, "rw");
         this.channel = raf.getChannel();
         this.buf = channel.map(FileChannel.MapMode.READ_WRITE, 0, getMemoryMapSize());
+    }
+
+    private void setTempFile(File f) {
+
     }
 
     private long getMemoryMapSize() {
@@ -85,12 +92,12 @@ public class MemoryMappedFileFilterInputStreamCache implements FilterInputStream
     private void increaseSize(final long bytes) throws IOException {
 
         long factor = (bytes / getMemoryMapSize());
-        if(factor == 0 || bytes % getMemoryMapSize() > 0) {
+        if (factor == 0 || bytes % getMemoryMapSize() > 0) {
             factor++;
         }
 
         buf.force();
-        
+
         //TODO revisit this based on the comment below, I now believe setting position in map does work, but you have to have the correct offset added in as well! Adam
         final int position = buf.position();
         buf = channel.map(FileChannel.MapMode.READ_WRITE, 0, buf.capacity() + (getMemoryMapSize() * factor));
@@ -101,7 +108,7 @@ public class MemoryMappedFileFilterInputStreamCache implements FilterInputStream
     @Override
     public void write(final byte[] b, final int off, final int len) throws IOException {
 
-        if(buf.remaining() < len) {
+        if (buf.remaining() < len) {
             //we need to remap the file
             increaseSize(len - buf.remaining());
         }
@@ -112,18 +119,18 @@ public class MemoryMappedFileFilterInputStreamCache implements FilterInputStream
     @Override
     public void write(final int i) throws IOException {
 
-        if(buf.remaining() < 1) {
+        if (buf.remaining() < 1) {
             //we need to remap the file
             increaseSize(1);
         }
 
-        buf.put((byte)i);
+        buf.put((byte) i);
     }
 
     @Override
     public byte get(final int off) throws IOException {
 
-       if(off > buf.capacity()) {
+        if (off > buf.capacity()) {
             //we need to remap the file
             increaseSize(off - buf.capacity());
         }
@@ -139,7 +146,7 @@ public class MemoryMappedFileFilterInputStreamCache implements FilterInputStream
     @Override
     public void copyTo(final int cacheOffset, final byte[] b, final int off, final int len) throws IOException {
 
-        if(off + len > buf.capacity()) {
+        if (off + len > buf.capacity()) {
             //we need to remap the file
             increaseSize(off + len - buf.capacity());
         }
@@ -168,9 +175,9 @@ public class MemoryMappedFileFilterInputStreamCache implements FilterInputStream
         channel.close();
         raf.close();
         //System.gc();
-        
-        if(tempFile != null && (!externalFile)) {
-           TemporaryFileManager.getInstance().returnTemporaryFile(tempFile);
+
+        if (tempFile != null && (!externalFile)) {
+            TemporaryFileManager.getInstance().returnTemporaryFile(tempFile);
         }
     }
 }
