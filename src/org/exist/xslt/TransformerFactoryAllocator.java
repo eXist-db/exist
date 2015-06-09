a067ba1d6c639cac443491f4e6216f33cb41073f
Dmitriy Shabanov <shabanovd@gmail.com>
2015-06-09 19:08:14 +0300

    Revert "[bugfix] Make the TransformerFactory a singleton"
    Former-commit-id: e3564c3702eef37c151a884e0828e7d109ea5e06

--- src/org/exist/xslt/TransformerFactoryAllocator.java
+++ src/org/exist/xslt/TransformerFactoryAllocator.java
@@ -1,6 +1,6 @@
 /*
  *  eXist Open Source Native XML Database
- *  Copyright (C) 2001-2015 The eXist Project
+ *  Copyright (C) 2001-07 The eXist Project
  *  http://exist-db.org
  *
  *  This program is free software; you can redistribute it and/or
@@ -16,6 +16,8 @@
  *  You should have received a copy of the GNU Lesser General Public
  *  License along with this library; if not, write to the Free Software
  *  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ *  $Id: TransformerFactoryAllocator.java 0000 2006-08-10 22:39:00 +0000 (Thu, 10 Aug 2006) deliriumsky $
  */
 package org.exist.xslt;
 
@@ -36,26 +38,24 @@ import org.exist.storage.BrokerPool;
  * Within eXist this class should be used instead of
  * directly calling SAXTransformerFactory.newInstance() directly
  *
- * @author Adam Retter <adam.retter@googlemail.com>
+ * @author Adam Retter <adam.retter@devon.gov.uk>
  * @author Andrzej Taramina <andrzej@chaeron.com>
  */
 
 public class TransformerFactoryAllocator {
-    private final static Logger LOG = LogManager.getLogger(TransformerFactoryAllocator.class);
+    private final static Logger LOG = LogManager.getLogger( TransformerFactoryAllocator.class );
 
-    public static final String CONFIGURATION_ELEMENT_NAME = "transformer";
-    public final static String TRANSFORMER_CLASS_ATTRIBUTE = "class";
-    public final static String PROPERTY_TRANSFORMER_CLASS = "transformer.class";
+    public static final String CONFIGURATION_ELEMENT_NAME  						= "transformer";
+    public final static String TRANSFORMER_CLASS_ATTRIBUTE 						= "class";
+    public final static String PROPERTY_TRANSFORMER_CLASS  						= "transformer.class";
 
     public final static String CONFIGURATION_TRANSFORMER_ATTRIBUTE_ELEMENT_NAME = "attribute";
-    public final static String PROPERTY_TRANSFORMER_ATTRIBUTES = "transformer.attributes";
+    public final static String PROPERTY_TRANSFORMER_ATTRIBUTES 					= "transformer.attributes"; 
 
-    public final static String TRANSFORMER_CACHING_ATTRIBUTE = "caching";
-    public final static String PROPERTY_CACHING_ATTRIBUTE = "transformer.caching";
+    public final static String TRANSFORMER_CACHING_ATTRIBUTE 					= "caching";
+    public final static String PROPERTY_CACHING_ATTRIBUTE 						= "transformer.caching";
 
-    public final static String PROPERTY_BROKER_POOL = "transformer.brokerPool";
-
-    private static SAXTransformerFactory saxTransformerFactory = null;
+    public final static String PROPERTY_BROKER_POOL 							= "transformer.brokerPool";
 
     //private constructor
     private TransformerFactoryAllocator() {
@@ -67,78 +67,81 @@ public class TransformerFactoryAllocator {
      * the required interface, the default factory is returned.
      *
      * @param pool A database broker pool, used for reading the conf.xml configuration
-     * @return A SAXTransformerFactory, for which newInstance() can then be called
+     *
+     * @return  A SAXTransformerFactory, for which newInstance() can then be called
+     *
      *
      * Typical usage:
      *
      * Instead of SAXTransformerFactory.newInstance() use
      * TransformerFactoryAllocator.getTransformerFactory(broker).newInstance()
      */
-    public static SAXTransformerFactory getTransformerFactory(final BrokerPool pool) {
-        if(saxTransformerFactory == null) {
-            synchronized (TransformerFactoryAllocator.class) {
-                // Check again, after having acquired the lock to make sure
-                // the instance was not created meanwhile by another thread
-                if (saxTransformerFactory == null) {
-                    // Lazy initialisation
-                    saxTransformerFactory = initTransformerFactory(pool);
-                }
-            }
-        }
-
-        return saxTransformerFactory;
-    }
-
-    private static SAXTransformerFactory initTransformerFactory(final BrokerPool pool) {
-        //Get the transformer class name from conf.xml
-        final String transformerFactoryClassName = (String) pool.getConfiguration().getProperty(PROPERTY_TRANSFORMER_CLASS);
-
-        //Was a TransformerFactory class specified?
+    public static SAXTransformerFactory getTransformerFactory(BrokerPool pool) {
         SAXTransformerFactory factory;
-        if (transformerFactoryClassName == null) {
+        //Get the transformer class name from conf.xml
+        final String transformerFactoryClassName = (String)pool.getConfiguration().getProperty(PROPERTY_TRANSFORMER_CLASS);
+        //Was a TransformerFactory class specified?
+        if( transformerFactoryClassName == null ) {
             //No, use the system default
-            factory = (SAXTransformerFactory) TransformerFactory.newInstance();
+            factory = (SAXTransformerFactory)TransformerFactory.newInstance();
         } else {
             //Try and load the specified TransformerFactory class
             try {
-                factory = (SAXTransformerFactory) Class.forName(transformerFactoryClassName).newInstance();
+                factory = (SAXTransformerFactory)Class.forName( transformerFactoryClassName ).newInstance();
                 if (LOG.isDebugEnabled()) {
-                    LOG.debug("Set transformer factory: " + transformerFactoryClassName);
+                    LOG.debug( "Set transformer factory: " + transformerFactoryClassName );
                 }
-                final Hashtable<String, Object> attributes = (Hashtable<String, Object>) pool.getConfiguration().getProperty(PROPERTY_TRANSFORMER_ATTRIBUTES);
+                final Hashtable<String,Object> attributes = (Hashtable<String, Object>)pool.getConfiguration().getProperty( PROPERTY_TRANSFORMER_ATTRIBUTES);
                 final Enumeration<String> attrNames = attributes.keys();
-                while (attrNames.hasMoreElements()) {
+                while( attrNames.hasMoreElements() ) {
                     final String name = attrNames.nextElement();
-                    final Object value = attributes.get(name);
+                    final Object value = attributes.get( name );
                     try {
-                        factory.setAttribute(name, value);
-                        if (LOG.isDebugEnabled()) {
-                            LOG.debug("Set transformer attribute: " + ", " +
-                                    "name: " + name + ", value: " + value);
+                        factory.setAttribute( name, value );
+                        if( LOG.isDebugEnabled() ) {
+                            LOG.debug( "Set transformer attribute: " + ", " +
+                                "name: " + name + ", value: " + value );
                         }
-                    } catch (final IllegalArgumentException iae) {
-                        LOG.warn("Unable to set attribute for TransformerFactory: '" +
-                                transformerFactoryClassName +
-                                "', name: " + name +
-                                ", value: " + value +
-                                ", exception: " + iae.getMessage());
+                    }
+                    catch(final IllegalArgumentException iae ) {
+                        LOG.warn( "Unable to set attribute for TransformerFactory: '" +
+                            transformerFactoryClassName +
+                            "', name: " + name +
+                            ", value: " + value +
+                            ", exception: " + iae.getMessage());
                     }
                 }
                 try {
                     factory.setAttribute(PROPERTY_BROKER_POOL, pool);
-                } catch (final Exception e) {
+                } catch(final Exception e) {
                     //some transformers do not support "setAttribute"
                 }
-            } catch (final ClassNotFoundException | InstantiationException | IllegalAccessException e) {
-                if (LOG.isDebugEnabled()) {
-                    LOG.debug("Error found loading the requested TrAX Transformer Factory '" + transformerFactoryClassName + "'. Using default TrAX Transformer Factory instead: " + e);
+            } 
+            catch( final ClassNotFoundException cnfe ) {
+                if( LOG.isDebugEnabled() ) {
+                    LOG.debug("Cannot find the requested TrAX factory '" +
+                        transformerFactoryClassName + "'. Using default TrAX Transformer Factory instead." );
                 }
                 //Fallback to system default
-                factory = (SAXTransformerFactory) TransformerFactory.newInstance();
+                factory = (SAXTransformerFactory)TransformerFactory.newInstance();
+            }
+            catch( final ClassCastException cce ) {
+                if( LOG.isDebugEnabled() ) {
+                    LOG.debug( "The indicated class '" + transformerFactoryClassName +
+                        "' is not a TrAX Transformer Factory. Using default TrAX Transformer Factory instead." );
+                }
+                //Fallback to system default
+                factory = (SAXTransformerFactory)TransformerFactory.newInstance();
+            }
+            catch( final Exception e ) {
+                if( LOG.isDebugEnabled() ) {
+                    LOG.debug( "Error found loading the requested TrAX Transformer Factory '" + transformerFactoryClassName + "'. Using default TrAX Transformer Factory instead: " + e );
+                }
+                //Fallback to system default
+                factory = (SAXTransformerFactory)TransformerFactory.newInstance();
             }
         }
-
-        return factory;
+        return( factory );
     }
 
 }
