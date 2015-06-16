e5d947c5a7f26738286be60044f54273e046fda3
Dmitriy Shabanov <shabanovd@gmail.com>
2015-06-16 23:04:01 +0300

    TokenStream must be closed to release resources associated with by it
    Former-commit-id: 46ddf1b13b18e81aca96acbd69b23917044a04a3

--- extensions/indexes/lucene/src/org/exist/indexing/lucene/LuceneMatchListener.java
+++ extensions/indexes/lucene/src/org/exist/indexing/lucene/LuceneMatchListener.java
@@ -212,9 +212,7 @@ public class LuceneMatchListener extends AbstractMatchListener {
         LOG.debug("Analyzer: " + analyzer + " for path: " + path);
         String str = extractor.getText().toString();
         //Token token;
-        try {
-
-            TokenStream tokenStream = analyzer.tokenStream(null, new StringReader(str));
+        try (TokenStream tokenStream = analyzer.tokenStream(null, new StringReader(str))) {
             tokenStream.reset();
             MarkableTokenFilter stream = new MarkableTokenFilter(tokenStream);
             while (stream.incrementToken()) {
