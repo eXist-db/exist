918f3d025fc566a58123c3ffa61c87562fef2b70
Dmitriy Shabanov <shabanovd@gmail.com>
2016-02-08 20:03:13 +0300

    fix file handle leak

--- src/org/exist/xmldb/LocalCollection.java
+++ src/org/exist/xmldb/LocalCollection.java
@@ -19,6 +19,7 @@
  */
 package org.exist.xmldb;
 
+import java.io.InputStream;
 import java.net.URISyntaxException;
 import java.util.ArrayList;
 import java.util.Date;
@@ -541,7 +542,9 @@ public class LocalCollection extends AbstractLocal implements CollectionImpl {
             try {
                 final long conLength = res.getStreamLength();
                 if (conLength != -1) {
-                    collection.addBinaryResource(transaction, broker, resURI, res.getStreamContent(), res.getMimeType(), conLength, res.datecreated, res.datemodified);
+                    try (InputStream is = res.getStreamContent()) {
+                        collection.addBinaryResource(transaction, broker, resURI, is, res.getMimeType(), conLength, res.datecreated, res.datemodified);
+                    }
                 } else {
                     collection.addBinaryResource(transaction, broker, resURI, (byte[]) res.getContent(), res.getMimeType(), res.datecreated, res.datemodified);
                 }
