1ed3f1ef26440859aabac0b4af8ef38dc60f1566
Dmitriy Shabanov <shabanovd@gmail.com>
2016-02-26 11:37:12 +0300

    Based on cherry-picked commit 2a496a6 (code view)

--- src/org/exist/xquery/Option.java
+++ src/org/exist/xquery/Option.java
@@ -43,6 +43,8 @@ public class Option {
     public final static QName OPTIMIZE_IMPLICIT_TIMEZONE = new QName("implicit-timezone", Namespaces.EXIST_NS);
     public final static QName CURRENT_DATETIME = new QName("current-dateTime", Namespaces.EXIST_NS);
 	
+    private final static String[] EMPTY = new String[0];
+
     private final static String paramPattern =
 		"\\s*([\\w\\.-]+)\\s*=\\s*('[^']*'|\"[^\"]*\"|[^\"\'\\s][^\\s]*)";
 	
@@ -70,9 +72,11 @@ public class Option {
 		return tokenize(contents);
 	}
 
-    public static String[] tokenize(String contents) {
-        if(contents == null)
-			{return new String[0];}
+
+    public static String[] tokenize(final String contents) {
+        if(contents == null) {
+            return EMPTY;
+        }
 		final StringTokenizer tok = new StringTokenizer(contents, " \r\t\n");
 		final String[] items = new String[tok.countTokens()];
 		for(int i = 0; tok.hasMoreTokens(); i++) {
@@ -81,7 +85,7 @@ public class Option {
 		return items;
     }
 
-    public static synchronized String[] parseKeyValuePair(String s) {
+    public static String[] parseKeyValuePair(final String s) {
         final Matcher matcher = pattern.matcher(s);
 		if(matcher.matches()) {
 			String value = matcher.group(2);
@@ -92,11 +96,11 @@ public class Option {
 		}
 		return null;
 	}
-	
-	public boolean equals(Object other) {
-		if (other instanceof Option) {
-			return qname.equals(((Option) other).qname);
-		}
-		return false;
+
+	@Override
+	public boolean equals(final Object other) {
+		return other != null && 
+            other instanceof Option &&
+            qname.equals(((Option) other).qname);
 	}
 }
