2993d7c2149704188d013ea4fe0bbcfdd361695b
Adam Retter <adam.retter@googlemail.com>
2016-10-12 22:01:47 -0400

    [feature] Update to Lucene 4.8.1

--- extensions/indexes/lucene/src/org/exist/indexing/lucene/LuceneIndex.java
+++ extensions/indexes/lucene/src/org/exist/indexing/lucene/LuceneIndex.java
@@ -49,7 +49,7 @@ import java.nio.file.Path;
 
 public class LuceneIndex extends AbstractIndex implements RawBackupSupport {
     
-    public final static Version LUCENE_VERSION_IN_USE = Version.LUCENE_47;
+    public final static Version LUCENE_VERSION_IN_USE = Version.LUCENE_48;
 
     private static final Logger LOG = LogManager.getLogger(LuceneIndexWorker.class);
 
