aa6b4558c97fd08f09564fd62d31c52e00411589
Adam Retter <adam.retter@googlemail.com>
2017-02-23 19:04:29 +0000

    [ignore] Code reformatting and cleanup

--- extensions/expath/src/org/expath/exist/ZipEntryFunctions.java
+++ extensions/expath/src/org/expath/exist/ZipEntryFunctions.java
@@ -27,12 +27,14 @@ import java.io.Reader;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 import javax.xml.transform.stream.StreamSource;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.exist.dom.persistent.BinaryDocument;
 import org.exist.dom.QName;
 import org.exist.dom.persistent.DocumentImpl;
 import org.exist.security.PermissionDeniedException;
+import org.exist.storage.DBBroker;
 import org.exist.storage.lock.Lock.LockMode;
 import org.exist.xmldb.XmldbURI;
 import org.exist.xquery.BasicFunction;
@@ -54,102 +56,104 @@ import org.exist.xquery.value.Type;
 import org.exist.xquery.value.StringValue;
 import org.xml.sax.SAXException;
 
+import static java.nio.charset.StandardCharsets.UTF_8;
+
 /**
  * @author Adam Retter <adam@existsolutions.com>
  * @version EXPath Zip Client Module Candidate 12 October 2010 http://expath.org/spec/zip/20101012
  */
 public class ZipEntryFunctions extends BasicFunction {
 
-    private static final Logger logger = LogManager.getLogger(ZipEntryFunctions.class);
+    protected static final Logger LOG = LogManager.getLogger(ZipEntryFunctions.class);
 
-    private final static FunctionParameterSequenceType HREF_PARAM = new FunctionParameterSequenceType("href", Type.ANY_URI, Cardinality.EXACTLY_ONE, "The URI for locating the Zip file");
-    private final static FunctionParameterSequenceType ENTRY_PARAM = new FunctionParameterSequenceType("entry", Type.STRING, Cardinality.EXACTLY_ONE, "The entry within the Zip file to address");
+    private static final FunctionParameterSequenceType HREF_PARAM = new FunctionParameterSequenceType("href", Type.ANY_URI, Cardinality.EXACTLY_ONE, "The URI for locating the Zip file");
+    private static final FunctionParameterSequenceType ENTRY_PARAM = new FunctionParameterSequenceType("entry", Type.STRING, Cardinality.EXACTLY_ONE, "The entry within the Zip file to address");
 
-    private final static String BINARY_ENTRY_NAME = "binary-entry";
-    private final static String HTML_ENTRY_NAME = "html-entry";
-    private final static String TEXT_ENTRY_NAME = "text-entry";
-    private final static String XML_ENTRY_NAME = "xml-entry";
+    private static final String BINARY_ENTRY_NAME = "binary-entry";
+    private static final String HTML_ENTRY_NAME = "html-entry";
+    private static final String TEXT_ENTRY_NAME = "text-entry";
+    private static final String XML_ENTRY_NAME = "xml-entry";
 
-    public final static FunctionSignature signatures[] = {
-        //zip:binary-entry($href as xs:anyURI, $entry as xs:string) as xs:base64Binary
-        new FunctionSignature(
-            new QName(BINARY_ENTRY_NAME, ZipModule.NAMESPACE_URI, ZipModule.PREFIX),
-            "Extracts the binary stream from the file positioned at $entry within the ZIP file identified by $href and returns it as a Base64 item.",
-            new SequenceType[]{
-                HREF_PARAM,
-                ENTRY_PARAM
-            },
-            new FunctionReturnSequenceType(Type.BASE64_BINARY, Cardinality.EXACTLY_ONE, "The binary representation of the entry from the Zip file.")
-        ),
-        //zip:html-entry($href as xs:anyURI, $entry as xs:string) as document-node()
-        new FunctionSignature(
-            new QName(HTML_ENTRY_NAME, ZipModule.NAMESPACE_URI, ZipModule.PREFIX),
-            "Extracts the html file positioned at $entry within the ZIP file identified by $href, and returns a document node. Because an HTML document is not necessarily a well-formed XML document, an implementation may use a specific parser in order to produce an XDM document node, like [TagSoup] or [HTML Tidy]; the details of this process are implementation-defined.",
-            new SequenceType[]{
-                HREF_PARAM,
-                ENTRY_PARAM
-            },
-            new FunctionReturnSequenceType(Type.DOCUMENT, Cardinality.EXACTLY_ONE, "The document-node of the entry from the Zip file.")
-        ),
-        //zip:text-entry($href as xs:anyURI, $entry as xs:string) as xs:string
-        new FunctionSignature(
-            new QName(TEXT_ENTRY_NAME, ZipModule.NAMESPACE_URI, ZipModule.PREFIX),
-            "Extracts the contents of the text file positioned at entry within the ZIP file identified by $href and returns it as a string.",
-            new SequenceType[]{
-                HREF_PARAM,
-                ENTRY_PARAM
-            },
-            new FunctionReturnSequenceType(Type.STRING, Cardinality.EXACTLY_ONE, "The string value of the entry from the Zip file.")
-        ),
-        //zip:xml-entry($href as xs:anyURI, $entry as xs:string) as document-node()
-        new FunctionSignature(
-            new QName(XML_ENTRY_NAME, ZipModule.NAMESPACE_URI, ZipModule.PREFIX),
-            "Extracts the content from the XML file positioned at $entry within the ZIP file identified by $href and returns it as a document-node.",
-            new SequenceType[]{
-                HREF_PARAM,
-                ENTRY_PARAM
-            },
-            new FunctionReturnSequenceType(Type.DOCUMENT, Cardinality.EXACTLY_ONE, "The document-node of the entry from the Zip file.")
-        )
+    public static final FunctionSignature signatures[] = {
+            //zip:binary-entry($href as xs:anyURI, $entry as xs:string) as xs:base64Binary
+            new FunctionSignature(
+                    new QName(BINARY_ENTRY_NAME, ZipModule.NAMESPACE_URI, ZipModule.PREFIX),
+                    "Extracts the binary stream from the file positioned at $entry within the ZIP file identified by $href and returns it as a Base64 item.",
+                    new SequenceType[]{
+                            HREF_PARAM,
+                            ENTRY_PARAM
+                    },
+                    new FunctionReturnSequenceType(Type.BASE64_BINARY, Cardinality.EXACTLY_ONE, "The binary representation of the entry from the Zip file.")
+            ),
+            //zip:html-entry($href as xs:anyURI, $entry as xs:string) as document-node()
+            new FunctionSignature(
+                    new QName(HTML_ENTRY_NAME, ZipModule.NAMESPACE_URI, ZipModule.PREFIX),
+                    "Extracts the html file positioned at $entry within the ZIP file identified by $href, and returns a document node. Because an HTML document is not necessarily a well-formed XML document, an implementation may use a specific parser in order to produce an XDM document node, like [TagSoup] or [HTML Tidy]; the details of this process are implementation-defined.",
+                    new SequenceType[]{
+                            HREF_PARAM,
+                            ENTRY_PARAM
+                    },
+                    new FunctionReturnSequenceType(Type.DOCUMENT, Cardinality.EXACTLY_ONE, "The document-node of the entry from the Zip file.")
+            ),
+            //zip:text-entry($href as xs:anyURI, $entry as xs:string) as xs:string
+            new FunctionSignature(
+                    new QName(TEXT_ENTRY_NAME, ZipModule.NAMESPACE_URI, ZipModule.PREFIX),
+                    "Extracts the contents of the text file positioned at entry within the ZIP file identified by $href and returns it as a string.",
+                    new SequenceType[]{
+                            HREF_PARAM,
+                            ENTRY_PARAM
+                    },
+                    new FunctionReturnSequenceType(Type.STRING, Cardinality.EXACTLY_ONE, "The string value of the entry from the Zip file.")
+            ),
+            //zip:xml-entry($href as xs:anyURI, $entry as xs:string) as document-node()
+            new FunctionSignature(
+                    new QName(XML_ENTRY_NAME, ZipModule.NAMESPACE_URI, ZipModule.PREFIX),
+                    "Extracts the content from the XML file positioned at $entry within the ZIP file identified by $href and returns it as a document-node.",
+                    new SequenceType[]{
+                            HREF_PARAM,
+                            ENTRY_PARAM
+                    },
+                    new FunctionReturnSequenceType(Type.DOCUMENT, Cardinality.EXACTLY_ONE, "The document-node of the entry from the Zip file.")
+            )
     };
 
     /**
      * SendRequestFunction Constructor
      *
-     * @param context	The Context of the calling XQuery
+     * @param context   The Context of the calling XQuery
      * @param signature The actual signature of the function
      */
-    public ZipEntryFunctions(XQueryContext context, FunctionSignature signature) {
+    public ZipEntryFunctions(final XQueryContext context, final FunctionSignature signature) {
         super(context, signature);
     }
 
     @Override
-    public Sequence eval(Sequence[] args, Sequence contextSequence) throws XPathException {
-        XmldbURI uri = ((AnyURIValue)args[0].itemAt(0)).toXmldbURI();
-        String entryName = args[1].itemAt(0).getStringValue();
+    public Sequence eval(final Sequence[] args, final Sequence contextSequence) throws XPathException {
+        final XmldbURI uri = ((AnyURIValue) args[0].itemAt(0)).toXmldbURI();
+        final String entryName = args[1].itemAt(0).getStringValue();
 
-        ZipFileSource zipFileSource =  new ZipFileFromDb(uri);
+        final ZipFileSource zipFileSource = new ZipFileFromDb(uri);
         ZipInputStream zis = null;
 
         boolean mustClose = true;
         Sequence result = Sequence.EMPTY_SEQUENCE;
 
         try {
-            zis = zipFileSource.getStream();
+            zis = zipFileSource.getStream(context.getBroker());
             ZipEntry zipEntry;
-            while((zipEntry = zis.getNextEntry()) != null) {
+            while ((zipEntry = zis.getNextEntry()) != null) {
                 try {
-                    if(zipEntry.getName().equals(entryName)) {
+                    if (zipEntry.getName().equals(entryName)) {
                         //process
-                        if(isCalledAs(BINARY_ENTRY_NAME)) {
-                            result = extractBinaryEntry(zis, zipEntry);
+                        if (isCalledAs(BINARY_ENTRY_NAME)) {
+                            result = extractBinaryEntry(zis);
                             mustClose = false;
-                        } else if(isCalledAs(HTML_ENTRY_NAME)) {
-                            result = extractHtmlEntry(zis, zipEntry);
-                        } else if(isCalledAs(TEXT_ENTRY_NAME)) {
-                            result = extractStringEntry(zis, zipEntry);
-                        } else if(isCalledAs(XML_ENTRY_NAME)) {
-                            result = extractXmlEntry(zis, zipEntry);
+                        } else if (isCalledAs(HTML_ENTRY_NAME)) {
+                            result = extractHtmlEntry(zis);
+                        } else if (isCalledAs(TEXT_ENTRY_NAME)) {
+                            result = extractStringEntry(zis);
+                        } else if (isCalledAs(XML_ENTRY_NAME)) {
+                            result = extractXmlEntry(zis);
                         }
                         break;
                     }
@@ -161,15 +165,16 @@ public class ZipEntryFunctions extends BasicFunction {
                     }*/
                 }
             }
-        } catch(IOException ioe) {
+        } catch (final IOException | PermissionDeniedException ioe) {
             LOG.error(ioe.getMessage(), ioe);
-            throw new XPathException(ioe.getMessage(), ioe);
-        } catch(PermissionDeniedException pde) {
-            LOG.error(pde.getMessage(), pde);
-            throw new XPathException(pde.getMessage(), pde);
+            throw new XPathException(this, ioe.getMessage(), ioe);
         } finally {
-            if(zis != null && mustClose) {
-                try { zis.close(); } catch (IOException ioe) { LOG.warn(ioe.getMessage(), ioe); }
+            if (zis != null && mustClose) {
+                try {
+                    zis.close();
+                } catch (final IOException ioe) {
+                    LOG.warn(ioe.getMessage(), ioe);
+                }
             }
             zipFileSource.close();
         }
@@ -177,86 +182,79 @@ public class ZipEntryFunctions extends BasicFunction {
         return result;
     }
 
-    private BinaryValue extractBinaryEntry(ZipInputStream zis, ZipEntry zipEntry) throws XPathException {
+    private BinaryValue extractBinaryEntry(final ZipInputStream zis) throws XPathException {
         return BinaryValueFromInputStream.getInstance(context, new Base64BinaryValueType(), zis);
     }
 
-    private StringValue extractStringEntry(ZipInputStream zis, ZipEntry zipEntry) throws XPathException, IOException{
-        Reader reader = new InputStreamReader(zis);
-        char buf[] = new char[1024];
-        StringBuilder builder = new StringBuilder();
+    private StringValue extractStringEntry(final ZipInputStream zis) throws XPathException, IOException {
+        final char buf[] = new char[4096];
+        final StringBuilder builder = new StringBuilder();
         int read = -1;
-        try {
-            while((read = reader.read(buf)) > -1) {
+        try (final Reader reader = new InputStreamReader(zis, UTF_8)) {
+            while ((read = reader.read(buf)) > -1) {
                 builder.append(buf, 0, read);
             }
-        } finally {
-            reader.close();
         }
         return new StringValue(builder.toString());
     }
 
-    private org.exist.dom.memtree.DocumentImpl extractHtmlEntry(ZipInputStream zis, ZipEntry zipEntry) throws XPathException {
+    private org.exist.dom.memtree.DocumentImpl extractHtmlEntry(final ZipInputStream zis) throws XPathException {
         try {
             return ModuleUtils.htmlToXHtml(context, new StreamSource(zis), null, null);
-        } catch(SAXException saxe) {
-            throw new XPathException(saxe.getMessage(), saxe);
-        } catch(IOException ioe) {
-            throw new XPathException(ioe.getMessage(), ioe);
+        } catch (final SAXException | IOException saxe) {
+            throw new XPathException(this, saxe.getMessage(), saxe);
         }
     }
 
-    private NodeValue extractXmlEntry(ZipInputStream zis, ZipEntry zipEntry) throws XPathException {
+    private NodeValue extractXmlEntry(final ZipInputStream zis) throws XPathException {
         try {
             return ModuleUtils.streamToXML(context, zis);
-        } catch(SAXException saxe) {
-            throw new XPathException(saxe.getMessage(), saxe);
-        } catch(IOException ioe) {
-            throw new XPathException(ioe.getMessage(), ioe);
+        } catch (final SAXException | IOException saxe) {
+            throw new XPathException(this, saxe.getMessage(), saxe);
         }
     }
 
-    public interface ZipFileSource {
-        public ZipInputStream getStream() throws IOException, PermissionDeniedException;
-        public void close();
+    public interface ZipFileSource extends AutoCloseable {
+        ZipInputStream getStream(final DBBroker broker) throws IOException, PermissionDeniedException;
+
+        @Override
+        void close();
     }
 
-    private class ZipFileFromDb implements ZipFileSource {
+    protected static class ZipFileFromDb implements ZipFileSource {
         private BinaryDocument binaryDoc = null;
         private final XmldbURI uri;
 
-        public ZipFileFromDb(XmldbURI uri) {
+        public ZipFileFromDb(final XmldbURI uri) {
             this.uri = uri;
         }
 
         @Override
-        public ZipInputStream getStream() throws IOException, PermissionDeniedException {
-
-            if(binaryDoc == null) {
-                binaryDoc = getDoc();
+        public ZipInputStream getStream(final DBBroker broker) throws IOException, PermissionDeniedException {
+            if (binaryDoc == null) {
+                binaryDoc = getDoc(broker);
             }
 
-            return new ZipInputStream(context.getBroker().getBinaryResource(binaryDoc));
+            return new ZipInputStream(broker.getBinaryResource(binaryDoc));
         }
 
         @Override
         public void close() {
-            if(binaryDoc != null) {
-               binaryDoc.getUpdateLock().release(LockMode.READ_LOCK);
+            if (binaryDoc != null) {
+                binaryDoc.getUpdateLock().release(LockMode.READ_LOCK);
             }
         }
 
-        private BinaryDocument getDoc() throws PermissionDeniedException {
-
-            DocumentImpl doc = context.getBroker().getXMLResource(uri, LockMode.READ_LOCK);
-            if(doc == null) {
+        private BinaryDocument getDoc(final DBBroker broker) throws PermissionDeniedException {
+            final DocumentImpl doc = broker.getXMLResource(uri, LockMode.READ_LOCK);
+            if (doc == null) {
                 return null;
             } else if (doc.getResourceType() != DocumentImpl.BINARY_FILE) {
                 doc.getUpdateLock().release(LockMode.READ_LOCK);
                 return null;
             }
 
-            return (BinaryDocument)doc;
+            return (BinaryDocument) doc;
         }
     }
 }
