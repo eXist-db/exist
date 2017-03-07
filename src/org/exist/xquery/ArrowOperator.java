3ae94ffd97536c9eca611847ed6e9285eafb5334
Dmitriy Shabanov <shabanovd@gmail.com>
2017-03-07 10:44:48 +0300

    raise error if xquery is < 3.1

--- src/org/exist/xquery/ArrowOperator.java
+++ src/org/exist/xquery/ArrowOperator.java
@@ -1,3 +1,22 @@
+/*
+ * eXist Open Source Native XML Database
+ * Copyright (C) 2001-2017 The eXist Project
+ * http://exist-db.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
 package org.exist.xquery;
 
 import org.exist.dom.QName;
@@ -42,6 +61,11 @@ public class ArrowOperator extends AbstractExpression {
 
     @Override
     public void analyze(final AnalyzeContextInfo contextInfo) throws XPathException {
+        if(getContext().getXQueryVersion() < 31) {
+            throw new XPathException(this,
+                ErrorCodes.EXXQDY0003,
+                "arrow operator is not available before XQuery 3.1");
+        }
         this.cachedContextInfo = contextInfo;
         leftExpr.analyze(contextInfo);
         if (fcall != null) {
