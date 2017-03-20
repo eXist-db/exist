bb000ec3159ca14e1ec25b5de10f40bfe7e3d7bf
Dmitriy Shabanov <shabanovd@gmail.com>
2017-03-20 22:29:41 +0300

    IDocument interface and ImplementationType enum

---
+++ src/org/exist/dom/ImplementationType.java
@@ -0,0 +1,33 @@
+/*
+ * eXist Open Source Native XML Database
+ * Copyright (C) 2001-2017 The eXist Project
+ * http://exist-db.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package org.exist.dom;
+
+/**
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ */
+public enum ImplementationType {
+
+  /** Node is a constructed in-memory node */
+  IN_MEMORY_NODE,
+
+  /** Node is a persistent, i.e. stored in the database */
+  PERSISTENT_NODE
+
+}
