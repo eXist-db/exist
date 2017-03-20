c944d014251f1c7499fb21bab0bff06119b3a8a2
Adam Retter <adam.retter@googlemail.com>
2017-03-19 20:13:36 -0400

    [ignore] Reformatted code

--- src/org/exist/xquery/functions/fn/FunDocAvailable.java
+++ src/org/exist/xquery/functions/fn/FunDocAvailable.java
@@ -1,6 +1,6 @@
 /*
  * eXist Open Source Native XML Database
- * Copyright (C) 2005-2009 The eXist Project
+ * Copyright (C) 2005-2017 The eXist Project
  * http://exist-db.org
  *
  * This program is free software; you can redistribute it and/or
@@ -16,8 +16,6 @@
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- *  
- *  $Id$
  */
 package org.exist.xquery.functions.fn;
 
@@ -38,7 +36,7 @@ import org.exist.xquery.value.Type;
 
 /**
  * Implements the XQuery's fn:doc-available() function.
- * 
+ *
  * @author Pierrick Brihaye <pierrick.brihaye@free.fr>
  * @author wolf
  */
@@ -46,47 +44,46 @@ public class FunDocAvailable extends Function {
 
     protected static final Logger logger = LogManager.getLogger(FunDocAvailable.class);
 
-    public final static FunctionSignature signature =
-        new FunctionSignature(
-            new QName("doc-available", Function.BUILTIN_FUNCTION_NS),
-            "Returns whether or not the document, $document-uri, " +
-            "specified in the input sequence is available. " +
-            XMLDBModule.ANY_URI,
-            new SequenceType[] { 
-                new FunctionParameterSequenceType("document-uri", Type.STRING,
-                    Cardinality.ZERO_OR_ONE, "The document URI")
-            },
-            new FunctionReturnSequenceType(Type.BOOLEAN, Cardinality.EXACTLY_ONE,
-                "true() if the document is available, false() otherwise"));	
+    public static final FunctionSignature signature =
+            new FunctionSignature(
+                    new QName("doc-available", Function.BUILTIN_FUNCTION_NS),
+                    "Returns whether or not the document, $document-uri, " +
+                            "specified in the input sequence is available. " +
+                            XMLDBModule.ANY_URI,
+                    new SequenceType[]{
+                            new FunctionParameterSequenceType("document-uri", Type.STRING,
+                                    Cardinality.ZERO_OR_ONE, "The document URI")
+                    },
+                    new FunctionReturnSequenceType(Type.BOOLEAN, Cardinality.EXACTLY_ONE,
+                            "true() if the document is available, false() otherwise"));
 
-    public FunDocAvailable(XQueryContext context) {
+    public FunDocAvailable(final XQueryContext context) {
         super(context, signature);
     }
 
-    /**
-     * @see org.exist.xquery.Function#getDependencies()
-     */
+    @Override
     public int getDependencies() {
         return Dependency.CONTEXT_SET;
     }
 
-    /**
-     * @see org.exist.xquery.Expression#eval(Sequence, Item)
-     */
-    public Sequence eval(Sequence contextSequence, Item contextItem)
+    @Override
+    public Sequence eval(final Sequence contextSequence, final Item contextItem)
             throws XPathException {
         if (context.getProfiler().isEnabled()) {
             context.getProfiler().start(this);
             context.getProfiler().message(this, Profiler.DEPENDENCIES,
-                "DEPENDENCIES", Dependency.getDependenciesName(this.getDependencies()));
-            if (contextSequence != null)
-                {context.getProfiler().message(this, Profiler.START_SEQUENCES,
-                    "CONTEXT SEQUENCE", contextSequence);}
-            if (contextItem != null)
-                {context.getProfiler().message(this, Profiler.START_SEQUENCES,
-                    "CONTEXT ITEM", contextItem.toSequence());}
+                    "DEPENDENCIES", Dependency.getDependenciesName(this.getDependencies()));
+            if (contextSequence != null) {
+                context.getProfiler().message(this, Profiler.START_SEQUENCES,
+                        "CONTEXT SEQUENCE", contextSequence);
+            }
+            if (contextItem != null) {
+                context.getProfiler().message(this, Profiler.START_SEQUENCES,
+                        "CONTEXT ITEM", contextItem.toSequence());
+            }
         }
-        Sequence result;
+
+        final Sequence result;
         final Sequence arg = getArgument(0).eval(contextSequence, contextItem);
         if (arg.isEmpty()) {
             result = BooleanValue.FALSE;
@@ -99,15 +96,14 @@ public class FunDocAvailable extends Function {
                 throw e;
             }
         }
-        if (context.getProfiler().isEnabled()) 
-            {context.getProfiler().end(this, "", result);}
+
+        if (context.getProfiler().isEnabled()) {
+            context.getProfiler().end(this, "", result);
+        }
         return result;
     }
 
-    /**
-     * @see org.exist.xquery.Expression#resetState(boolean)
-     * @param postOptimization
-     */
+    @Override
     public void resetState(boolean postOptimization) {
         super.resetState(postOptimization);
         getArgument(0).resetState(postOptimization);
