05b116e5ef8b144ce373186f4ee018991e0d5b2f
Adam Retter <adam.retter@googlemail.com>
2017-04-13 10:17:18 -0400

    [bugfix] systemctl --user is inhertently insecure (and as such unsupported in RHEL derived distributions); Instead install eXist as a system service Counterpart to 140c718

--- tools/yajsw/bin/uninstallDaemon.sh
+++ tools/yajsw/bin/uninstallDaemon.sh
@@ -4,6 +4,8 @@
 #
 # -----------------------------------------------------------------------------
 
+set -e
+
 # resolve links - $0 may be a softlink
 PRG="$0"
 
@@ -37,11 +39,14 @@ if [ `pgrep -P 1 systemd | head -n 1` ]; then
     case $systemd_response in
 	[Yy][Ee][Ss]|[YyJj])
 	    echo "Stopping service ...";
-	    systemctl --user stop eXist-db;
+	    systemctl stop eXist-db;
 	    echo "Disabling service ...";
-	    systemctl --user disable eXist-db;
-	    if [ -e "$HOME/.local/share/systemd/user/eXist-db.service" ]; then 
-		rm -f "$HOME/.local/share/systemd/user/eXist-db.service";
+	    systemctl disable eXist-db;
+
+            systemd_sys_dir="/etc/systemd/system"
+            systemd_service="${systemd_sys_dir}/eXist-db.service"
+	    if [ -e "$systemd_service" ]; then
+		rm -f "$systemd_service";
 	    fi
 	    ;;
 	[Nn][Oo]|[Nn])
