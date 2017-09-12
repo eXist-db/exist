924d4f5646d7a4bbda928fce2142e8f3645eff94
wshager <wshager@gmail.com>
2017-09-12 15:32:49 +0200

    handle multiple x-forwarded-for IP addresses

--- extensions/security/iprange/src/org/exist/security/realm/iprange/IPRangeServlet.java
+++ extensions/security/iprange/src/org/exist/security/realm/iprange/IPRangeServlet.java
@@ -2,17 +2,17 @@
  *  eXist Open Source Native XML Database
  *  Copyright (C) 2009-2017 The eXist Project
  *  http://exist-db.org
- *  
+ *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License
  *  as published by the Free Software Foundation; either version 2
  *  of the License, or (at your option) any later version.
- *  
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Lesser General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
@@ -63,11 +63,14 @@ public class IPRangeServlet extends HttpServlet {
 
 
         // Get reverse proxy header when available, otherwise use regular IP address
-        String ipAddress = request.getHeader("X-Forwarded-For");
-        if (ipAddress == null) {
+        String ipAddress = request.getHeader("X-Forwarded-For").replaceAll("\\s","");
+        // there may be a comma-separated chain of proxies
+        if (!ipAddress.isEmpty()) {
+            String[] xFFs = ipAddress.split(",");
+            if(xFFs.length > 1) ipAddress = xFFs[xFFs.length - 1];
+        } else {
             ipAddress = request.getRemoteAddr();
         }
-
 
         LOG.info("Detected IPaddress " + ipAddress);
 
