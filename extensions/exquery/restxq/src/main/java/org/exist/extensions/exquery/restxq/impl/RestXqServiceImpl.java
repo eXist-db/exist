1fa73b0b9a957b8b02a91c0a28ec71d28af9e211
Maan Al Balkhi <maan.al.balkhi@gmail.com>
2017-09-29 20:00:06 +0200

    remove unused imports

--- extensions/exquery/restxq/src/main/java/org/exist/extensions/exquery/restxq/impl/RestXqServiceImpl.java
+++ extensions/exquery/restxq/src/main/java/org/exist/extensions/exquery/restxq/impl/RestXqServiceImpl.java
@@ -26,17 +26,10 @@
  */
 package org.exist.extensions.exquery.restxq.impl;
 
-import java.io.BufferedReader;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
-import java.util.Base64;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.Collectors;
 
 import org.apache.commons.io.input.CloseShieldInputStream;
 import org.apache.commons.io.output.ByteArrayOutputStream;
