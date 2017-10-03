82eb7d04040db42dacd55a3875a9a3e5c1dad245
Adam Retter <adam.retter@googlemail.com>
2017-10-03 12:41:47 +0100

    [ignore] code cleanup

--- src/org/exist/xquery/util/SerializerUtils.java
+++ src/org/exist/xquery/util/SerializerUtils.java
@@ -21,24 +21,26 @@ public class SerializerUtils {
      * Parse output:serialization-parameters XML fragment into serialization
      * properties as defined by the fn:serialize function.
      *
-     * @param parent the parent expression calling this method
+     * @param parent     the parent expression calling this method
      * @param parameters root node of the XML fragment
      * @param properties parameters are added to the given properties
      */
-    public static void getSerializationOptions(Expression parent, NodeValue parameters, Properties properties) throws XPathException {
+    public static void getSerializationOptions(final Expression parent, final NodeValue parameters, final Properties properties) throws XPathException {
         try {
             final XMLStreamReader reader = parent.getContext().getXMLStreamReader(parameters);
             while (reader.hasNext() && (reader.next() != XMLStreamReader.START_ELEMENT)) {
             }
-            if (!reader.getNamespaceURI().equals(Namespaces.XSLT_XQUERY_SERIALIZATION_NS)) {
+            if (!Namespaces.XSLT_XQUERY_SERIALIZATION_NS.equals(reader.getNamespaceURI())) {
                 throw new XPathException(parent, FnModule.SENR0001, "serialization parameter elements should be in the output namespace");
             }
+
             while (reader.hasNext()) {
                 final int status = reader.next();
                 if (status == XMLStreamReader.START_ELEMENT) {
                     final String key = reader.getLocalName();
-                    if (properties.contains(key))
-                    {throw new XPathException(parent, FnModule.SEPM0019, "serialization parameter specified twice: " + key);}
+                    if (properties.contains(key)) {
+                        throw new XPathException(parent, FnModule.SEPM0019, "serialization parameter specified twice: " + key);
+                    }
                     String value = reader.getAttributeValue("", "value");
                     if (value == null) {
                         // backwards compatibility: use element text as value
