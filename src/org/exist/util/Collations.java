1f231ccab556c4f1e2073aec201568064122494c
Adam Retter <adam.retter@googlemail.com>
2017-11-20 22:09:27 +0000

    [bugfix] Make sure to use correct Rules for HTML ASCII Case Insensitive Collation

--- src/org/exist/util/Collations.java
+++ src/org/exist/util/Collations.java
@@ -682,9 +682,9 @@ public class Collations {
         Collator collator = htmlAsciiCaseInsensitiveCollator.get();
         if (collator == null) {
             htmlAsciiCaseInsensitiveCollator.compareAndSet(null,
-                    new RuleBasedCollator("&a=A, b=B, c=C, d=D, e=E, f=F, g=G, h=H, "
-                    + "i=I, j=J, k=K, l=L, m=M, n=N, o=O, p=P, q=Q, r=R, s=S, t=T, "
-                    + "u=U, v=V, u=U, v=V, w=W, x=X, y=Y, z=Z").freeze());
+                    new RuleBasedCollator("&a=A &b=B &c=C &d=D &e=E &f=F &g=G &h=H "
+                    + "&i=I &j=J &k=K &l=L &m=M &n=N &o=O &p=P &q=Q &r=R &s=S &t=T "
+                    + "&u=U &v=V &w=W &x=X &y=Y &z=Z").freeze());
             collator = htmlAsciiCaseInsensitiveCollator.get();
         }
 
