0372276ff6f6a626a7195d20d66691ee5a00a2b3
Adam Retter <adam.retter@googlemail.com>
2017-12-24 19:58:31 +0000

    [test] Tests for HTML/ASCII Collator

---
+++ test/src/org/exist/util/CollationsTest.java
@@ -0,0 +1,42 @@
+/*
+ * eXist Open Source Native XML Database
+ * Copyright (C) 2001-2017 The eXist Project
+ * http://exist-db.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
+package org.exist.util;
+
+import com.ibm.icu.text.Collator;
+import org.exist.xquery.XPathException;
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertFalse;
+import static org.exist.util.Collations.HTML_ASCII_CASE_INSENSITIVE_COLLATION_URI;
+import static org.junit.Assert.assertTrue;
+
+public class CollationsTest {
+
+    @Test
+    public void htmlAscii_contains() throws XPathException {
+        final Collator collator = Collations.getCollationFromURI(HTML_ASCII_CASE_INSENSITIVE_COLLATION_URI);
+
+        assertTrue(Collations.contains(collator, "iNPut", "pu"));
+        assertTrue(Collations.contains(collator, "iNPut", "PU"));
+        assertTrue(Collations.contains(collator,"h&#244;tel", "h&#244;t"));
+        assertFalse(Collations.contains(collator, "h&#244;tel", "H&#212;T"));
+    }
+}
