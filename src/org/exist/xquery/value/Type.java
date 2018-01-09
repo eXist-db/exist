002647c449ddfc199a6e5a563ef5bda46c8309ba
Dannes Wessels <dannes@exist-db.org>
2018-01-09 22:43:27 +0100

    Update map -> map(*) and empty() -> empty-sequence() according to spec. Pending: additional tests

--- src/org/exist/xquery/value/Type.java
+++ src/org/exist/xquery/value/Type.java
@@ -189,7 +189,7 @@ public class Type {
         //TODO use parentheses after the nodes name  ?
         defineBuiltInType(NODE, "node()");
         defineBuiltInType(ITEM, "item()");
-        defineBuiltInType(EMPTY, "empty()");
+        defineBuiltInType(EMPTY, "empty-sequence()","empty");
 
         defineBuiltInType(ELEMENT, "element()");
         defineBuiltInType(DOCUMENT, "document-node()");
@@ -202,7 +202,7 @@ public class Type {
 
         defineBuiltInType(JAVA_OBJECT, "object");
         defineBuiltInType(FUNCTION_REFERENCE, "function");
-        defineBuiltInType(MAP, "map");
+        defineBuiltInType(MAP, "map(*)", "map");
         defineBuiltInType(ARRAY, "array");
         defineBuiltInType(NUMBER, "numeric");
 
