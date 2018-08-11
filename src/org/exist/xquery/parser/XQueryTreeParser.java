80208977308d94242ae88d394cb2afef84ec6802
Adam Retter <adam.retter@googlemail.com>
2018-08-11 16:02:26 +0530

    [ignore] Recompiled the XQuery parser

--- src/org/exist/xquery/parser/XQueryTreeParser.java
+++ src/org/exist/xquery/parser/XQueryTreeParser.java
@@ -59,10 +59,10 @@ public class XQueryTreeParser extends antlr.TreeParser       implements XQueryTr
 	private XQueryContext staticContext;
 	private XQueryContext context;
 	private ExternalModule myModule = null;
-	protected ArrayList exceptions= new ArrayList(2);
-	protected boolean foundError= false;
-	protected Map declaredNamespaces = new HashMap();
-	protected Set declaredGlobalVars = new TreeSet();
+	protected List<Exception> exceptions = new ArrayList<>(2);
+	protected boolean foundError = false;
+	protected Map<String, String> declaredNamespaces = new HashMap<>();
+	protected Set<QName> declaredGlobalVars = new TreeSet<>();
 
 	public XQueryTreeParser(XQueryContext context) {
         this(context, null);
