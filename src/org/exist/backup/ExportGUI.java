eXist-db/exist

commit f886c0df93f9e072826f85d37a56ebe35d675286

[bugfix] Fixed thread naming and grouping per-database instance. Also likely fixed some subtle concurrency issues in the Admin Client and FileSyncThread Closes https://github.com/eXist-db/exist/issues/1699

src/org/exist/backup/ExportGUI.java
@@ -47,6 +47,7 @@ import javax.swing.filechooser.FileFilter;
 import org.exist.security.PermissionDeniedException;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.exist.util.ThreadUtils.newInstanceThread;
 
 
 /**
@@ -337,7 +338,7 @@ public class ExportGUI extends javax.swing.JFrame {
                 closeLog();
             }
         };
-        new Thread(checkRun).start();
+        newInstanceThread(pool, "export-gui.check-run", checkRun).start();
     } // GEN-LAST:event_startBtncheck
 
 
@@ -358,7 +359,7 @@ public class ExportGUI extends javax.swing.JFrame {
                 closeLog();
             }
         };
-        new Thread(th).start();
+        newInstanceThread(pool, "export-gui.export", th).start();
     } // GEN-LAST:event_exportBtnActionPerformed
 
 

extensions/debuggee/src/org/exist/debuggee/ScriptRunner.java
@@ -31,6 +31,8 @@ import org.exist.storage.DBBroker;
 import org.exist.xquery.CompiledXQuery;
 import org.exist.xquery.XQuery;
 
+import static org.exist.util.ThreadUtils.nameInstanceThread;
+
 /**
  * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
  *
@@ -48,7 +50,7 @@ public class ScriptRunner implements Runnable, Observer {
 		this.session = session;
 		expression = compiled;
 		
-		thread = new Thread(this);
+		thread = newInstanceThread(BrokerPool.getInstance(), "scriptRunner" this);
 		thread.setDaemon(true);
 		thread.setName("Debug session "+compiled.getContext().hashCode());
 	}

extensions/debuggee/src/org/exist/debugger/DebuggerImpl.java
@@ -50,6 +50,8 @@ import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.w3c.dom.Text;
 
+import static org.exist.util.ThreadUtils.newGlobalThread;
+
 /**
  * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
  * 
@@ -117,7 +119,7 @@ public class DebuggerImpl implements Debugger, org.exist.debuggee.Status {
 		sources = new HashMap<String, DebuggingSource>();
 		currentTransactionId = 1;
 		
-		Thread session = new Thread(new HttpSession(this, url));
+		Thread session = newGlobalThread("debuggerHttpSession" new HttpSession(this, url));
 		session.start();
 
 		// 30s timeout

extensions/fluent/src/org/exist/fluent/Database.java
@@ -30,6 +30,8 @@ import java.nio.file.Path;
 import java.text.MessageFormat;
 import java.util.*;
 
+import static org.exist.util.ThreadUtils.newInstanceThread;
+
 /**
  * <p>The global entry point to an embedded instance of the <a href='http://exist-db.org'>eXist </a>database.
  * The static methods on this class control the lifecycle of the database connection.  It follows that
@@ -561,7 +563,7 @@ public class Database {
 		
 		public void start() {
 			if (thread != null) return;
-			thread = new Thread(this, "Database defragmenter");
+			thread = newInstanceThread(pool, "fluent.database-defragmenter", this);
 			thread.setPriority(Thread.NORM_PRIORITY-3);
 			thread.setDaemon(true);
 			thread.start();

extensions/fluent/src/org/exist/fluent/WeakMultiValueHashMap.java
@@ -3,6 +3,8 @@ package org.exist.fluent;
 import java.lang.ref.WeakReference;
 import java.util.*;
 
+import static org.exist.util.ThreadUtils.newGlobalThread;
+
 class WeakMultiValueHashMap<K,V> {
 
 	/**
@@ -84,7 +86,7 @@ class WeakMultiValueHashMap<K,V> {
 	
 	private static final Sweeper SWEEPER = new Sweeper();
 	static {
-		Thread thread = new Thread(SWEEPER, "WeakMultiValueHashMap sweeper");
+		Thread thread = newGlobalThread("fluent.weakMultiValueHashMap.sweeper", SWEEPER);
 		thread.setPriority(Thread.NORM_PRIORITY-3);
 		thread.setDaemon(true);
 		thread.start();

extensions/modules/src/org/exist/xquery/modules/exi/EXIUtils.java
@@ -50,7 +50,7 @@ public class EXIUtils {
                 Serializer serializer = context.getBroker().newSerializer();
 
                 NodeValue node = (NodeValue) item;
-                return new NodeInputStream(serializer, node);
+                return new NodeInputStream(context.getBroker().getBrokerPool(), serializer, node);
             default:
                 LOG.error("Wrong item type " + Type.getTypeName(item.getType()));
                 throw new XPathException("wrong item type " + Type.getTypeName(item.getType()));

src/org/exist/Database.java
@@ -58,6 +58,8 @@ public interface Database {
 
     public String getId();
 
+    ThreadGroup getThreadGroup();
+
     /**
      * 
      * @return SecurityManager

src/org/exist/backup/Backup.java
@@ -23,6 +23,7 @@ package org.exist.backup;
 
 import org.exist.util.FileUtils;
 import com.evolvedbinary.j8fu.function.FunctionE;
+import org.exist.util.NamedThreadGroupFactory;
 import org.exist.util.SystemExitCodes;
 import org.xml.sax.SAXException;
 import org.xml.sax.helpers.AttributesImpl;
@@ -74,6 +75,10 @@ public class Backup
 	private static final String	EXIST_GENERATED_FILENAME_DOTDOT_FILENAME = "_eXist_generated_backup_filename_dotdot_file_";
 	
     private static final int 	currVersion             = 1;
+
+    private static final AtomicInteger backupThreadId = new AtomicInteger();
+    private static final NamedThreadGroupFactory backupThreadGroupFactory = new NamedThreadGroupFactory("java-backup-tool");
+    private final ThreadGroup backupThreadGroup = backupThreadGroupFactory.newThreadGroup(null);
 
     private Path           target;
     private XmldbURI         rootCollection;
@@ -193,13 +198,17 @@ public class Backup
             final BackupDialog dialog = new BackupDialog( parent, false );
             dialog.setSize( new Dimension( 350, 150 ) );
             dialog.setVisible( true );
-            final BackupThread thread = new BackupThread( current, dialog, this );
-            thread.start();
+            final BackupRunnable backupRunnable = new BackupRunnable(current, dialog, this);
+            final Thread backupThread = newBackupThread("backup-" + backupThreadId.getAndIncrement(), backupRunnable);
+            backupThread.start();
+
+
+            //super("exist-backupThread-" + backupThreadId.getAndIncrement());
 
             if( parent == null ) {
 
                 // if backup runs as a single dialog, wait for it (or app will terminate)
-                while( thread.isAlive() ) {
+                while( backupThread.isAlive() ) {
 
                     synchronized( this ) {
 
@@ -486,15 +495,24 @@ public class Backup
         serializer.endElement(Namespaces.EXIST_NS, "acl", "acl");
     }
 
-    private static class BackupThread extends Thread {
+    /**
+     * Create a new thread for this backup instance.
+     *
+     * @param threadName the name of the thread
+     * @param runnable the function to execute on the thread
+     *
+     * @return the thread
+     */
+    private Thread newBackupThread(final String threadName, final Runnable runnable) {
+        return new Thread(backupThreadGroup, runnable, backupThreadGroup.getName() + "." + threadName);
+    }
+
+    private static class BackupRunnable implements Runnable {
         private final Collection collection;
         private final BackupDialog dialog;
         private final Backup backup;
 
-        private static final AtomicInteger backupThreadId = new AtomicInteger();
-
-        public BackupThread(final Collection collection, final BackupDialog dialog, final Backup backup) {
-            super("exist-backupThread-" + backupThreadId.getAndIncrement());
+        public BackupRunnable(final Collection collection, final BackupDialog dialog, final Backup backup) {
             this.collection = collection;
             this.dialog = dialog;
             this.backup = backup;

commit 97e447497c8cd9b945f1c6b52051befcbdd94f71

[bugfix] Fix a regression with thread creation introduced in f886c0d

src/org/exist/backup/ExportGUI.java
@@ -47,6 +47,7 @@ import javax.swing.filechooser.FileFilter;
 import org.exist.security.PermissionDeniedException;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
+import static org.exist.util.ThreadUtils.newGlobalThread;
 import static org.exist.util.ThreadUtils.newInstanceThread;
 
 
@@ -338,7 +339,11 @@ public class ExportGUI extends javax.swing.JFrame {
                 closeLog();
             }
         };
-        newInstanceThread(pool, "export-gui.check-run", checkRun).start();
+        if (pool != null) {
+            newInstanceThread(pool, "export-gui.check-run", checkRun).start();
+        } else {
+            newGlobalThread("export-gui.check-run", checkRun).start();
+        }
     } // GEN-LAST:event_startBtncheck
 
 
@@ -359,7 +364,11 @@ public class ExportGUI extends javax.swing.JFrame {
                 closeLog();
             }
         };
-        newInstanceThread(pool, "export-gui.export", th).start();
+        if (pool != null) {
+            newInstanceThread(pool, "export-gui.export", th).start();
+        } else {
+            newGlobalThread("export-gui.export", th).start();
+        }
     } // GEN-LAST:event_exportBtnActionPerformed
 
 