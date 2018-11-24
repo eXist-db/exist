388e5b13a8243a677944d3b919e6c64dfd4b0a2f
Adam Retter <adam.retter@googlemail.com>
2018-11-24 12:08:39 +0800

    [ignore] code cleanup

--- src/org/exist/xquery/functions/util/BinaryDoc.java
+++ src/org/exist/xquery/functions/util/BinaryDoc.java
@@ -1,23 +1,21 @@
 /*
- *  eXist Open Source Native XML Database
- *  Copyright (C) 2001-09 The eXist Project
- *  http://exist-db.org
- *  
- *  This program is free software; you can redistribute it and/or
- *  modify it under the terms of the GNU Lesser General Public License
- *  as published by the Free Software Foundation; either version 2
- *  of the License, or (at your option) any later version.
- *  
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Lesser General Public License for more details.
- *  
- *  You should have received a copy of the GNU Lesser General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *  
- *  $Id$
+ * eXist Open Source Native XML Database
+ * Copyright (C) 2001-2018 The eXist Project
+ * http://exist-db.org
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program; if not, write to the Free Software Foundation
+ * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
  */
 package org.exist.xquery.functions.util;
 
@@ -29,106 +27,103 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.exist.dom.persistent.BinaryDocument;
 import org.exist.dom.persistent.DocumentImpl;
-import org.exist.dom.QName;
 import org.exist.dom.persistent.LockedDocument;
 import org.exist.security.PermissionDeniedException;
 import org.exist.storage.lock.Lock.LockMode;
+import org.exist.storage.txn.TransactionException;
+import org.exist.storage.txn.Txn;
 import org.exist.xmldb.XmldbURI;
 import org.exist.xquery.BasicFunction;
-import org.exist.xquery.Cardinality;
 import org.exist.xquery.FunctionSignature;
 import org.exist.xquery.XPathException;
 import org.exist.xquery.XQueryContext;
 import org.exist.xquery.value.Base64BinaryDocument;
 import org.exist.xquery.value.BooleanValue;
 import org.exist.xquery.value.FunctionParameterSequenceType;
-import org.exist.xquery.value.FunctionReturnSequenceType;
 import org.exist.xquery.value.Sequence;
-import org.exist.xquery.value.SequenceType;
 import org.exist.xquery.value.Type;
 
-public class BinaryDoc extends BasicFunction {
-	
-	protected static final Logger logger = LogManager.getLogger(BinaryDoc.class);
+import static org.exist.xquery.FunctionDSL.*;
+import static org.exist.xquery.functions.util.UtilModule.functionSignature;
 
-    public final static FunctionSignature signatures[] = {
-        new FunctionSignature(
-            new QName("binary-doc", UtilModule.NAMESPACE_URI, UtilModule.PREFIX),
+public class BinaryDoc extends BasicFunction {
+
+    protected static final Logger logger = LogManager.getLogger(BinaryDoc.class);
+
+    private static final FunctionParameterSequenceType FS_PARAM_BINARY_RESOURCE = optParam("binary-resource", Type.STRING, "The path to the binary resource");
+
+    private static final String FS_BINARY_DOC_NAME = "binary-doc";
+    static final FunctionSignature FS_BINARY_DOC = functionSignature(
+            FS_BINARY_DOC_NAME,
             "Retrieves the binary resource and returns its contents as a " +
-            "value of type xs:base64Binary. An empty sequence is returned if the resource " +
-            "could not be found or $binary-resource was empty.",
-            new SequenceType[] {
-                new FunctionParameterSequenceType("binary-resource", Type.STRING, Cardinality.ZERO_OR_ONE, "The path to the binary resource")
-            },
-            new FunctionReturnSequenceType(Type.BASE64_BINARY, Cardinality.ZERO_OR_ONE, "the binary document")
-        ),
-        new FunctionSignature(
-            new QName("binary-doc-available", UtilModule.NAMESPACE_URI, UtilModule.PREFIX),
+                    "value of type xs:base64Binary. An empty sequence is returned if the resource " +
+                    "could not be found or $binary-resource was empty.",
+            returnsOpt(Type.BASE64_BINARY, "the binary document"),
+            FS_PARAM_BINARY_RESOURCE
+    );
+
+    private static final String FS_BINARY_DOC_AVAILABLE_NAME = "binary-doc-available";
+    static final FunctionSignature FS_BINARY_DOC_AVAILABLE = functionSignature(
+            FS_BINARY_DOC_AVAILABLE_NAME,
             "Checks if the binary resource identified by $binary-resource is available.",
-            new SequenceType[] {
-                new FunctionParameterSequenceType("binary-resource", Type.STRING, Cardinality.ZERO_OR_ONE, "The path to the binary resource")
-            },
-            new FunctionReturnSequenceType(Type.BOOLEAN, Cardinality.EXACTLY_ONE, "true if the binary document is available")
-        ),
-        new FunctionSignature(
-            new QName("is-binary-doc", UtilModule.NAMESPACE_URI, UtilModule.PREFIX),
+            returns(Type.BOOLEAN, "true if the binary document is available"),
+            FS_PARAM_BINARY_RESOURCE
+    );
+
+    private static final String FS_IS_BINARY_DOC_NAME = "is-binary-doc";
+    static final FunctionSignature FS_IS_BINARY_DOC = functionSignature(
+            FS_IS_BINARY_DOC_NAME,
             "Checks if the resource identified by $binary-resource is a binary resource.",
-            new SequenceType[] {
-                new FunctionParameterSequenceType("binary-resource", Type.STRING, Cardinality.ZERO_OR_ONE, "The path to the binary resource")
-            },
-            new FunctionReturnSequenceType(Type.BOOLEAN, Cardinality.EXACTLY_ONE, "true if the resource is a binary document")
-        )
-    };
-    
-    public BinaryDoc(XQueryContext context, FunctionSignature signature) {
+            returns(Type.BOOLEAN, "true if the resource is a binary document"),
+            FS_PARAM_BINARY_RESOURCE
+    );
+
+    public BinaryDoc(final XQueryContext context, final FunctionSignature signature) {
         super(context, signature);
     }
 
     @Override
-    public Sequence eval(Sequence[] args, Sequence contextSequence)
+    public Sequence eval(final Sequence[] args, final Sequence contextSequence)
             throws XPathException {
-    	
-        final Sequence defaultReturn = (isCalledAs("binary-doc") ? Sequence.EMPTY_SEQUENCE : BooleanValue.FALSE);
 
+        final Sequence emptyParamReturnValue = isCalledAs(FS_BINARY_DOC_NAME) ? Sequence.EMPTY_SEQUENCE : BooleanValue.FALSE;
         if (args[0].isEmpty()) {
-            return defaultReturn;
+            return emptyParamReturnValue;
         }
 
         final String path = args[0].getStringValue();
         try(final LockedDocument lockedDoc = context.getBroker().getXMLResource(XmldbURI.xmldbUriFor(path), LockMode.READ_LOCK);) {
             if(lockedDoc == null) {
-                return defaultReturn;
+                return emptyParamReturnValue;
             }
 
             final DocumentImpl doc = lockedDoc.getDocument();
 
             if(doc.getResourceType() != DocumentImpl.BINARY_FILE) {
-                return defaultReturn;
-            } else if(isCalledAs("binary-doc")) {
-                final BinaryDocument bin = (BinaryDocument) doc;
-                final InputStream is = context.getBroker().getBinaryResource(bin);
+                return emptyParamReturnValue;
+            } else if(isCalledAs(FS_BINARY_DOC_NAME)) {
+                try (final Txn transaction = context.getBroker().continueOrBeginTransaction()) {
+                    final BinaryDocument bin = (BinaryDocument) doc;
+                    final InputStream is = context.getBroker().getBinaryResource(transaction, bin);
+                    final Base64BinaryDocument b64doc = Base64BinaryDocument.getInstance(context, is);
+                    b64doc.setUrl(path);
 
-                /*
-                long binaryLength = context.getBroker().getBinaryResourceSize(bin);
+                    transaction.commit();
 
-                byte[] data = new byte[(binaryLength > (long)Integer.MAX_VALUE)?Integer.MAX_VALUE:(int)binaryLength];
-                is.read(data);
-                is.close(); */
-                final Base64BinaryDocument b64doc = Base64BinaryDocument.getInstance(context, is);
-                b64doc.setUrl(path);
-                return b64doc;
+                    return b64doc;
+                }
             } else {
                 return BooleanValue.TRUE;
             }
         } catch (final URISyntaxException e) {
-        	logger.error("Invalid resource URI", e);
-            throw new XPathException(this, "Invalid resource uri",e);
+            logger.error("Invalid resource URI", e);
+            throw new XPathException(this, "Invalid resource uri", e);
         } catch (final PermissionDeniedException e) {
-        	logger.info(path + ": permission denied to read resource", e);
+            logger.error(path + ": permission denied to read resource", e);
             throw new XPathException(this, path + ": permission denied to read resource");
-        } catch (final IOException e) {
-        	logger.error(path + ": I/O error while reading resource", e);
-            throw new XPathException(this, path + ": I/O error while reading resource",e);
+        } catch (final IOException | TransactionException e) {
+            logger.error(path + ": I/O error while reading resource", e);
+            throw new XPathException(this, path + ": I/O error while reading resource", e);
         }
     }
 }
