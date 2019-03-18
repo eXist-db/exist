29ff59a7cc76bd763a604eddf3316591a99bcbcf
Wolfgang Meier <wolfgang@existsolutions.com>
2019-03-18 18:36:04 +0100

    [feature] Add ft:facets to retrieve facet information on a node returned by ft:query. Refactoring and cleanup.

--- extensions/indexes/lucene/src/main/java/org/exist/indexing/lucene/LuceneFacetConfig.java
+++ extensions/indexes/lucene/src/main/java/org/exist/indexing/lucene/LuceneFacetConfig.java
@@ -20,29 +20,56 @@
 
 package org.exist.indexing.lucene;
 
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.facet.FacetField;
 import org.apache.lucene.facet.FacetsConfig;
+import org.exist.dom.persistent.DocumentImpl;
+import org.exist.dom.persistent.NodeProxy;
+import org.exist.numbering.NodeId;
+import org.exist.security.PermissionDeniedException;
+import org.exist.storage.DBBroker;
 import org.exist.util.DatabaseConfigurationException;
+import org.exist.xquery.CompiledXQuery;
+import org.exist.xquery.XPathException;
+import org.exist.xquery.XQuery;
+import org.exist.xquery.XQueryContext;
+import org.exist.xquery.value.Sequence;
+import org.exist.xquery.value.SequenceIterator;
 import org.w3c.dom.Element;
 
 import java.util.Map;
 
 public class LuceneFacetConfig {
 
-    public final static String CATEGORY_ATTR = "category";
-    public final static String XPATH_ATTR = "xpath";
+    static final Logger LOG = LogManager.getLogger(LuceneFacetConfig.class);
 
-    protected String category;
+    public final static String DIMENSION = "dimension";
+    public final static String XPATH_ATTR = "expression";
+    public final static String HIERARCHICAL = "hierarchical";
+
+    protected String dimension;
+
     protected String expression;
 
-    protected FacetsBuilder builder;
+    protected boolean isHierarchical = false;
+
+    private boolean isValid = true;
+
+    private CompiledXQuery compiled = null;
 
     public LuceneFacetConfig(Element configElement, FacetsConfig facetsConfig, Map<String, String> namespaces) throws DatabaseConfigurationException {
-        this.category = configElement.getAttribute(CATEGORY_ATTR);
+        this.dimension = configElement.getAttribute(DIMENSION);
 
         final String xpath = configElement.getAttribute(XPATH_ATTR);
         if (xpath == null || xpath.isEmpty()) {
             throw new DatabaseConfigurationException("facet definition needs an attribute 'xpath': " + configElement.toString());
         }
+
+        final String hierarchicalOpt = configElement.getAttribute(HIERARCHICAL);
+        isHierarchical = hierarchicalOpt != null &&
+                (hierarchicalOpt.equalsIgnoreCase("true") || hierarchicalOpt.equalsIgnoreCase("yes"));
 
         final StringBuilder sb = new StringBuilder();
         namespaces.forEach((prefix, uri) -> {
@@ -55,16 +82,61 @@ public class LuceneFacetConfig {
 
         this.expression = sb.toString();
 
-        facetsConfig.setMultiValued(category, true);
-
-        builder = new FacetsBuilder(this);
+        facetsConfig.setHierarchical(dimension, isHierarchical);
+        facetsConfig.setMultiValued(dimension, !isHierarchical);
     }
 
-    public String getCategory() {
-        return category;
+    public String getDimension() {
+        return dimension;
     }
 
     public String getExpression() {
         return expression;
+    }
+
+    public void build(DBBroker broker, DocumentImpl document, NodeId nodeId, Document luceneDoc) {
+        compile(broker);
+
+        if (!isValid) {
+            return;
+        }
+
+        final XQuery xquery = broker.getBrokerPool().getXQueryService();
+        final NodeProxy currentNode = new NodeProxy(document, nodeId);
+        try {
+            Sequence result = xquery.execute(broker, compiled, currentNode);
+
+            if (!result.isEmpty()) {
+                if (isHierarchical) {
+                    String paths[] = new String[result.getItemCount()];
+                    int j = 0;
+                    for (SequenceIterator i = result.unorderedIterator(); i.hasNext(); j++) {
+                        paths[j] = i.nextItem().getStringValue();
+                    }
+                    luceneDoc.add(new FacetField(dimension, paths));
+                } else {
+                    for (SequenceIterator i = result.unorderedIterator(); i.hasNext(); ) {
+                        luceneDoc.add(new FacetField(dimension, i.nextItem().getStringValue()));
+                    }
+                }
+            }
+        } catch (PermissionDeniedException e) {
+            e.printStackTrace();
+        } catch (XPathException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void compile(DBBroker broker) {
+        if (compiled == null && isValid) {
+            final XQuery xquery = broker.getBrokerPool().getXQueryService();
+            final XQueryContext context = new XQueryContext(broker.getBrokerPool());
+            try {
+                this.compiled = xquery.compile(broker, context, expression);
+            } catch (XPathException | PermissionDeniedException e) {
+                LOG.error("Failed to compile facet expression: " + expression + ": " + e.getMessage(), e);
+                isValid = false;
+            }
+        }
     }
 }
