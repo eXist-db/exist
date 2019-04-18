e93b33a034435460a0caca8d8e9b7b350138f92f
Adam Retter <adam.retter@googlemail.com>
2019-04-18 22:52:23 +0800

    [feature] Maven build now supports codesigning and DMG package creation on Mac

---
+++ exist-distribution/src/main/scripts/create-dmg-unix.sh
@@ -0,0 +1,51 @@
+#!/usr/bin/env bash
+
+# $1 is .app dir
+# $2 is app name e.g. VolName
+# $3 is background image
+# $4 is Volume icons
+# $5 is DS_Store
+# $6 is the output DMG file path and name
+
+set -e
+set -x
+
+tmp_dmg=/tmp/$2-dmg-tmp
+tmp_dmg_mount=$tmp_dmg-mount
+
+# Calculate the size for an image and add 10MB to ensure there is enough space!
+base_size=$(du -sm $1 | sed 's/\([0-9]*\).*/\1/')
+img_size=$(($base_size + 10))
+
+# Create a temporary Disk Image
+dd if=/dev/zero of=$tmp_dmg.dmg bs=1M count=$img_size
+/sbin/mkfs.hfsplus -v $2 tmp_dmg.dmg
+
+# Attach the temporary image
+username=$(whoami)
+mkdir -p $tmp_dmg_mount
+sudo mount -o loop,uid=$username $tmp_dmg.dmg $tmp_dmg_mount
+
+# Copy the app into the image
+cp -r $1 $tmp_dmg_mount
+
+# Copy the background, the volume icon and DS_Store files
+mkdir -p $tmp_dmg_mount/$2/.DropDMGBackground
+cp $3 $tmp_dmg_mount/$2/.DropDMGBackground/
+cp $4 $tmp_dmg_mount/$2/.VolumeIcon.icns
+cp $5 $tmp_dmg_mount/$2/.DS_Store
+
+# Add a symbolic link to the Applications directory
+ln -s /Applications $tmp_dmg_mount/$2/Applications
+
+# Detach the temporary image
+sudo umount $tmp_dmg_mount
+
+# Copy it to a new image
+cp $tmp_dmg.dmg $6
+
+# Delete the temporary image
+rm $tmp_dmg.dmg
+
+# Delete the mount point
+rm -r $tmp_dmg_mount
