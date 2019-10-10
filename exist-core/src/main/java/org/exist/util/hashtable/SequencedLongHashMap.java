beb076561cf5541f5b5623139d09087a10ac14f1
Adam Retter <adam.retter@googlemail.com>
2019-10-10 20:02:24 +0100

    [refactor] Eliminate SequencedLongHashMap custom code in favour of fastutil

--- exist-core/src/main/java/org/exist/util/hashtable/SequencedLongHashMap.java
+++ exist-core/src/main/java/org/exist/util/hashtable/SequencedLongHashMap.java
@@ -27,141 +27,45 @@ package org.exist.util.hashtable;
  *
  * If a duplicate entry is added, the old entry is removed from the list and appended to the end. The
  * map thus implements a "Last Recently Used" (LRU) behaviour.
+ *
+ * @author <a href="mailto:adam@evolvedbinary.com">Adam Retter</a>
  */
-
+import it.unimi.dsi.fastutil.longs.Long2ObjectLinkedOpenHashMap;
+import it.unimi.dsi.fastutil.longs.Long2ObjectMap;
+import it.unimi.dsi.fastutil.longs.LongIterator;
+import it.unimi.dsi.fastutil.objects.ObjectBidirectionalIterator;
 import net.jcip.annotations.NotThreadSafe;
 
+import javax.annotation.Nullable;
 import java.util.Iterator;
+import java.util.NoSuchElementException;
 
 @NotThreadSafe
-public class SequencedLongHashMap<V> extends AbstractHashtable<Long, V> {
+public class SequencedLongHashMap<V> {
 
-    /**
-     * Represents an entry in the map. Each entry
-     * has a link to the next and previous entries in
-     * the order in which they were inserted.
-     *
-     * @author wolf
-     */
-    public final static class Entry<V> {
-        final long key;
-        V value;
+    private static final int DEFAULT_SIZE = 1031; // prime number
 
-        /** points to the next entry in insertion order. */
-        Entry<V> next = null;
+    private final Long2ObjectLinkedOpenHashMap<V> map;
 
-        /** points to the previous entry in insertion order. */
-        Entry<V> prev = null;
-
-        /** points to the prev entry if more than one key maps
-         * to the same bucket in the table.
-         */
-        Entry<V> prevDup = null;
-
-        /** points to the next entry if more than one key maps
-         * to the same bucket in the table.
-         */
-        Entry<V> nextDup = null;
-
-        public Entry(final long key, final V value) {
-            this.key = key;
-            this.value = value;
-        }
-
-        public Entry<V> getNext() {
-            return next;
-        }
-
-        public long getKey() {
-            return key;
-        }
-
-        public V getValue() {
-            return value;
-        }
-
-        public String toString() {
-            return Long.toString(key);
-        }
-    }
-
-    private long[] keys;
-    private Entry<V>[] values;
-
-    /** points to the first entry inserted. */
-    private Entry<V> first = null;
-
-    /** points to the last inserted entry. */
-    private Entry<V> last = null;
-
-    @SuppressWarnings("unchecked")
     SequencedLongHashMap() {
-        super();
-        keys = new long[tabSize];
-        values = (Entry<V>[]) new Entry[tabSize];
+        this.map = new Long2ObjectLinkedOpenHashMap<>(DEFAULT_SIZE);
     }
 
-    @SuppressWarnings("unchecked")
     public SequencedLongHashMap(final int iSize) {
-        super(iSize);
-        keys = new long[tabSize];
-        values = (Entry<V>[]) new Entry[tabSize];
+        this.map = new Long2ObjectLinkedOpenHashMap<>(iSize);
     }
 
     /**
-     * Add a new entry for the key.
+     * Adds a pair to the map; if the key is already present, it is moved to the
+     * last position of the iteration order.
      *
      * @param key The key
      * @param value The value
+     *
+     * @return the old value, or null if no value was present for the given key.
      */
-    @SuppressWarnings("unchecked")
-    public void put(final long key, final V value) {
-        final Entry<V> entry = insert(key, value);
-
-        if (first == null) {
-            first = entry;
-            last = first;
-        } else {
-            last.next = entry;
-            entry.prev = last;
-            last = entry;
-        }
-    }
-
-    protected Entry insert(long key, V value) {
-        if (value == null) {
-            throw new IllegalArgumentException("Illegal value: null");
-        }
-        int idx = hash(key) % tabSize;
-        if (idx < 0) {
-            idx *= -1;
-        }
-        // look for an empty bucket
-        if (values[idx] == null) {
-            keys[idx] = key;
-            values[idx] = new Entry<>(key, value);
-            ++items;
-            return values[idx];
-        }
-
-        Entry<V> next = values[idx];
-        while (next != null) {
-            if (next.key == key) {
-                // duplicate value
-                next.value = value;
-                removeEntry(next);
-                return next;
-            }
-            next = next.nextDup;
-        }
-
-        // add a new entry to the chain
-        next = new Entry<>(key, value);
-        next.nextDup = values[idx];
-        values[idx].prevDup = next;
-        values[idx] = next;
-        ++items;
-        return next;
+    public @Nullable V put(final long key, final V value) {
+        return map.putAndMoveToLast(key, value);
     }
 
     /**
@@ -172,23 +76,8 @@ public class SequencedLongHashMap<V> extends AbstractHashtable<Long, V> {
      *
      * @return the value associated with the key, or null if the key is absent
      */
-    public V get(final long key) {
-        int idx = hash(key) % tabSize;
-        if (idx < 0) {
-            idx *= -1;
-        }
-        if (values[idx] == null) {
-            return null;
-        } // key does not exist
-
-        Entry<V> next = values[idx];
-        while (next != null) {
-            if (next.key == key) {
-                return next.value;
-            }
-            next = next.nextDup;
-        }
-        return null;
+    public @Nullable V get(final long key) {
+        return map.get(key);
     }
 
     /**
@@ -196,162 +85,92 @@ public class SequencedLongHashMap<V> extends AbstractHashtable<Long, V> {
      *
      * @return the first entry
      */
-    public Entry<V> getFirstEntry() {
-        return first;
-    }
+//    public Entry<V> getFirstEntry() {
+//        return first;
+//    }
 
     /**
      * Remove the entry specified by key from the map.
      *
      * @param key The key
      *
-     * @return the previous value
+     * @return the previous value, or null if there was no previous value
      */
-    public V remove(final long key) {
-        final Entry<V> entry = removeFromHashtable(key);
-        if (entry != null) {
-            removeEntry(entry);
-            return entry.value;
-        } else {
-            return null;
-        }
-    }
-
-    private Entry<V> removeFromHashtable(final long key) {
-        int idx = hash(key) % tabSize;
-        if (idx < 0) {
-            idx *= -1;
-        }
-        if (values[idx] == null) {
-            return null; // key does not exist
-        }
-
-        Entry<V> next = values[idx];
-        while (next != null) {
-            if (next.key == key) {
-                if (next.prevDup == null) {
-                    values[idx] = next.nextDup;
-                    if (values[idx] != null) {
-                        values[idx].prevDup = null;
-                    }
-                } else {
-                    next.prevDup.nextDup = next.nextDup;
-                    if (next.nextDup != null) {
-                        next.nextDup.prevDup = next.prevDup;
-                    }
-                }
-                --items;
-                return next;
-            }
-            next = next.nextDup;
-        }
-        return null;
+    public @Nullable V remove(final long key) {
+        return map.remove(key);
     }
 
     /**
-     * Remove the first entry added to the map.
+     * Removes the mapping associated with the first key in iteration order.
      *
-     * @return the first entry from the map, or null if the map is empty
+     * @return the value previously associated with the first key in iteration
+     *         order, or null if the map is empty
      */
-    public Entry<V> removeFirst() {
-        if (first == null) {
+    public @Nullable V removeFirst() {
+        if (map.isEmpty()) {
             return null;
         }
-        final Entry<V> head = first;
-        removeFromHashtable(first.key);
-        removeEntry(first);
-        return head;
-    }
-
-    /**
-     * Remove an entry.
-     *
-     * @param entry The entry to remove
-     */
-    private void removeEntry(final Entry<V> entry) {
-        if (entry.prev == null) {
-            if (entry.next == null) {
-                first = null;
-                last = null;
-            } else {
-                entry.next.prev = null;
-                first = entry.next;
-            }
-        } else {
-            entry.prev.next = entry.next;
-            if (entry.next == null) {
-                last = entry.prev;
-            } else {
-                entry.next.prev = entry.prev;
-            }
-        }
-        entry.prev = null;
-        entry.next = null;
+        return map.removeFirst();
     }
 
     /**
      * Clear the map.
      */
     public void clear() {
-        for (int i = 0; i < tabSize; i++) {
-            values[i] = null;
-        }
-        items = 0;
-        first = null;
-        last = null;
-    }
-
-    protected static int hash(final long l) {
-        return (int) (l ^ (l >>> 32));
+        map.clear();
+        map.trim();  // also resize the map to reduce memory use!
     }
 
     /**
      * Returns an iterator over all keys in the
      * order in which they were inserted.
+     *
+     * @return an iterator over the keys in the map
      */
-    @Override
-    public Iterator<Long> iterator() {
-        return new SequencedLongIterator<>(IteratorType.KEYS);
+    public LongIterator iterator() {
+       return map.keySet().iterator();
     }
 
     /**
      * Returns an iterator over all values in the order
      * in which they were inserted.
+     *
+     * @return an iterator over the values in the map
      */
-    @Override
     public Iterator<V> valueIterator() {
-        return new SequencedLongIterator<>(IteratorType.VALUES);
+        return map.values().iterator();
     }
 
-    public class SequencedLongIterator<T> extends AbstractHashSetIterator<T> {
-        private Entry<V> current;
+    /**
+     * Returns an iterator over all entries in the order
+     * in which they were inserted.
+     *
+     * NOTE: The iterator might return always the same
+     * entry instance, suitably mutated.
+     *
+     * @return an iterator over the entries in the map
+     */
+    public Iterator<Long2ObjectMap.Entry<V>> fastEntrySetIterator() {
+        return map.long2ObjectEntrySet().fastIterator();
+    }
 
-        public SequencedLongIterator(final IteratorType type) {
-            super(type);
-            current = first;
-        }
+    /**
+     * Returns the number of key/value mappings in this map. If the map contains
+     * more than {@link Integer#MAX_VALUE} elements, returns
+     * {@link Integer#MAX_VALUE}.
+     *
+     * @return the number of key-value mappings in this map.
+     */
+    public int size() {
+        return map.size();
+    }
 
-        @Override
-        public boolean hasNext() {
-            return current != null;
-        }
-
-        @SuppressWarnings("unchecked")
-        @Override
-        public T next() {
-            if (current == null) {
-                return null;
-            }
-            final Entry next = current;
-            current = current.next;
-            switch (returnType) {
-                case KEYS:
-                    return (T) Long.valueOf(next.key);
-                case VALUES:
-                    return (T) next.value;
-            }
-
-            throw new IllegalStateException("This never happens");
-        }
+    /**
+     * Add all the entries from the other map to this map.
+     *
+     * @param other the other map.
+     */
+    public void putAll(final SequencedLongHashMap<V> other) {
+        map.putAll(other.map);
     }
 }
