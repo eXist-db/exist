75204f22481b945fa64021bf4d5cad65eefd63b9
Otmar Humbel <ohumbel@gmail.com>
2019-12-17 10:30:51 +0100

    Indent according to coding standards
    This hopefully triggers a successful appveyor build.

--- exist-core/src/test/java/org/exist/installer/JobsXmlConfigurationTest.java
+++ exist-core/src/test/java/org/exist/installer/JobsXmlConfigurationTest.java
@@ -34,57 +34,57 @@ import java.nio.file.Paths;
 import org.junit.Test;
 
 /**
- * Make sure that the configuration inside <code>installer/jobs.xml</code> references existing file names only
+ * Make sure that the configuration inside <code>installer/jobs.xml</code>
+ * references existing file names only
  * 
  * @author <a href="mailto:ohumbel@gmail.com">Otmar Humbel</a>
  */
 public class JobsXmlConfigurationTest {
 
-  private static final String INSTALL_PATH_VAR = "$INSTALL_PATH";
+    private static final String INSTALL_PATH_VAR = "$INSTALL_PATH";
 
-  @Test
-  public void testIntegrityOfJobsXml() throws URISyntaxException, IOException {
-    String repositoryRoot = getRepositoryRoot();
-    Path jobsXmlPath = getJobsXmlPath(repositoryRoot);
-    Files.readAllLines(jobsXmlPath).stream().forEach(line -> assertInstallationPath(line, repositoryRoot));
-  }
-
-  private String getRepositoryRoot() throws URISyntaxException {
-    Class<? extends JobsXmlConfigurationTest> thisClass = getClass();
-    String resourceName = thisClass.getSimpleName().concat(".class");
-    URL resourceURL = thisClass.getResource(resourceName);
-    assertNotNull(resourceName + " not found", resourceURL);
-    File file = new File(resourceURL.toURI());
-    String resourcePath = file.getAbsolutePath().replace('\\', '/');
-    int testRootIndex = resourcePath.indexOf("/exist-core/target/test-classes/");
-    if (testRootIndex < 0) {
-      testRootIndex = resourcePath.indexOf("/test/classes/"); // inside IDE
+    @Test
+    public void testIntegrityOfJobsXml() throws URISyntaxException, IOException {
+	String repositoryRoot = getRepositoryRoot();
+	Path jobsXmlPath = getJobsXmlPath(repositoryRoot);
+	Files.readAllLines(jobsXmlPath).stream().forEach(line -> assertInstallationPath(line, repositoryRoot));
     }
-    assertTrue("test root not found inside " + resourcePath, testRootIndex > 0);
-    return resourcePath.substring(0, testRootIndex);
 
-  }
-
-  private Path getJobsXmlPath(String repositoryRoot) {
-    Path jobsXmlPath = Paths.get(repositoryRoot, "installer", "jobs.xml");
-    assertTrue(jobsXmlPath + " not found", Files.exists(jobsXmlPath));
-    assertTrue("cannot read " + jobsXmlPath, Files.isReadable(jobsXmlPath));
-    return jobsXmlPath;
-  }
-
-  private void assertInstallationPath(String line, String repoRoot) {
-    if (line.contains(INSTALL_PATH_VAR)) {
-      int startIndex = line.indexOf(INSTALL_PATH_VAR) + INSTALL_PATH_VAR.length();
-      int closingIndex = line.indexOf("</", startIndex);
-      if (closingIndex > startIndex) {
-        String fileName = repoRoot.concat(line.substring(startIndex, closingIndex)).replace('\\', '/');
-        Path filePath = Paths.get(fileName);
-        // intentionally exclude conf.xslt which is not present in the repository
-        if (!fileName.endsWith("conf.xslt")) {
-          assertTrue(filePath + " not found", Files.exists(filePath));
-        }
-      }
+    private String getRepositoryRoot() throws URISyntaxException {
+	Class<? extends JobsXmlConfigurationTest> thisClass = getClass();
+	String resourceName = thisClass.getSimpleName().concat(".class");
+	URL resourceURL = thisClass.getResource(resourceName);
+	assertNotNull(resourceName + " not found", resourceURL);
+	File file = new File(resourceURL.toURI());
+	String resourcePath = file.getAbsolutePath().replace('\\', '/');
+	int testRootIndex = resourcePath.indexOf("/exist-core/target/test-classes/");
+	if (testRootIndex < 0) {
+	    testRootIndex = resourcePath.indexOf("/test/classes/"); // inside IDE
+	}
+	assertTrue("test root not found inside " + resourcePath, testRootIndex > 0);
+	return resourcePath.substring(0, testRootIndex);
     }
-  }
+
+    private Path getJobsXmlPath(String repositoryRoot) {
+	Path jobsXmlPath = Paths.get(repositoryRoot, "installer", "jobs.xml");
+	assertTrue(jobsXmlPath + " not found", Files.exists(jobsXmlPath));
+	assertTrue("cannot read " + jobsXmlPath, Files.isReadable(jobsXmlPath));
+	return jobsXmlPath;
+    }
+
+    private void assertInstallationPath(String line, String repoRoot) {
+	if (line.contains(INSTALL_PATH_VAR)) {
+	    int startIndex = line.indexOf(INSTALL_PATH_VAR) + INSTALL_PATH_VAR.length();
+	    int closingIndex = line.indexOf("</", startIndex);
+	    if (closingIndex > startIndex) {
+		String fileName = repoRoot.concat(line.substring(startIndex, closingIndex)).replace('\\', '/');
+		Path filePath = Paths.get(fileName);
+		// intentionally exclude conf.xslt which is not present in the repository
+		if (!fileName.endsWith("conf.xslt")) {
+		    assertTrue(filePath + " not found", Files.exists(filePath));
+		}
+	    }
+	}
+    }
 
 }
