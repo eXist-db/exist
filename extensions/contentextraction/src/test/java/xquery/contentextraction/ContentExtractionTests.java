2f6ba5283662225debfa87adc837ab41effeac4e
Adam Retter <adam.retter@googlemail.com>
2020-01-15 15:10:08 +0000

    [test] Tests for content extraction module

---
+++ extensions/contentextraction/src/test/java/xquery/contentextraction/ContentExtractionTests.java
@@ -0,0 +1,11 @@
+package xquery.contentextraction;
+
+import org.exist.test.runner.XSuite;
+import org.junit.runner.RunWith;
+
+@RunWith(XSuite.class)
+@XSuite.XSuiteFiles({
+    "src/test/xquery/contentextraction"
+})
+public class ContentExtractionTests {
+}
