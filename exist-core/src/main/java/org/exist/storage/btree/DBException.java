ea59c919d46ee10d0ffdb2b574a69177697b9d01
Adam Retter <adam.retter@googlemail.com>
2020-03-18 19:16:31 +0100

    [license] The dbXML License, Version 1.0, is a copy of The Apache Software License, Version 1.1, but with the Organisation details changed. Make the text formatting consistent with https://www.apache.org/licenses/LICENSE-1.1

--- exist-core/src/main/java/org/exist/storage/btree/DBException.java
+++ exist-core/src/main/java/org/exist/storage/btree/DBException.java
@@ -1,8 +1,5 @@
-package org.exist.storage.btree;
-
 /*
  * dbXML License, Version 1.0
- *
  *
  * Copyright (c) 1999-2001 The dbXML Group, L.L.C.
  * All rights reserved.
@@ -23,14 +20,13 @@ package org.exist.storage.btree;
  *    if any, must include the following acknowledgment:
  *       "This product includes software developed by
  *        The dbXML Group (http://www.dbxml.com/)."
- *    Alternately, this acknowledgment may appear in the software
- *    itself, if and wherever such third-party acknowledgments normally
- *    appear.
+ *    Alternately, this acknowledgment may appear in the software itself,
+ *    if and wherever such third-party acknowledgments normally appear.
  *
- * 4. The names "dbXML" and "The dbXML Group" must not be used to
- *    endorse or promote products derived from this software without
- *    prior written permission. For written permission, please contact
- *    info@dbxml.com.
+ * 4. The names "dbXML" and "The dbXML Group" must
+ *    not be used to endorse or promote products derived from this
+ *    software without prior written permission. For written
+ *    permission, please contact info@dbxml.com.
  *
  * 5. Products derived from this software may not be called "dbXML",
  *    nor may "dbXML" appear in their name, without prior written
@@ -39,16 +35,17 @@ package org.exist.storage.btree;
  * THIS SOFTWARE IS PROVIDED ``AS IS'' AND ANY EXPRESSED OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
  * OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED.  IN NO EVENT SHALL THE DBXML GROUP OR ITS CONTRIBUTORS
- * BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY,
- * OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT
- * OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR
- * BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
- * LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
- * NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
- * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
+ * DISCLAIMED.  IN NO EVENT SHALL THE DBXML GROUP OR
+ * ITS CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF
+ * USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND
+ * ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
+ * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
+ * SUCH DAMAGE.
  */
+package org.exist.storage.btree;
 
 /**
  * A DBException is thrown by the database if an exception occurs in the
