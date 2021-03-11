eXist-db/exist

commit f69628dd9baea23a0637fc06a0a29d981a3faaff

[hotfix] xs:anyType is the top of a Type hierarchy, don't look further Closes https://github.com/eXist-db/exist/issues/3727

exist-core/src/main/java/org/exist/xquery/value/Type.java
@@ -536,11 +536,13 @@ public class Type {
     public static int getSuperType(final int subtype) {
         if (subtype == ITEM || subtype == NODE) {
             return ITEM;
+        } else if (subtype == ANY_TYPE) {
+            return subtype;
         }
 
         final int supertype = superTypes[subtype];
         if (supertype == 0) {
-            LOG.warn("eXist does not define a super-type for the sub-type {}", getTypeName(subtype), new Throwable());
+            LOG.warn("eXist-db does not define a super-type for the sub-type {}", getTypeName(subtype), new Throwable());
             return ITEM;
         }
 

commit 0390a780a3a42f18c16bc2245cb3a159b2d6b011

[bugfix] Fix a regression in finding the roots of the type hierarchy caused by f69628d

exist-core/src/main/java/org/exist/xquery/value/Type.java
@@ -579,7 +579,7 @@ public class Type {
         //Don't introduce a shortcut (starting at getSuperType(type1) here
         //type2 might be a super-type of type1
         int t;
-        for (t = type1; t != ITEM; t = getSuperType(t)) {
+        for (t = type1; t != ITEM && t != ANY_TYPE; t = getSuperType(t)) {
             //Shortcut
             if (t == type2) {
                 return t;
@@ -587,12 +587,12 @@ public class Type {
             t1.add(t);
         }
         //Starting from type2's super type : the shortcut should have done its job
-        for (t = getSuperType(type2); t != ITEM; t = getSuperType(t)) {
+        for (t = getSuperType(type2); t != ITEM && t != ANY_TYPE; t = getSuperType(t)) {
             if (t1.contains(t)) {
                 return t;
             }
         }
-        return ITEM;
+        return t;
     }
 
     /**