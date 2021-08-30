94d4b1a78a2bd4f8b897af34354535959c3c73d0
Adam Retter <adam.retter@googlemail.com>
2021-08-30 22:16:37 +0200

    [refactor] Remove now unneeded code

--- exist-core/src/main/java/org/exist/util/XMLReaderPool.java
+++ exist-core/src/main/java/org/exist/util/XMLReaderPool.java
@@ -23,27 +23,16 @@ package org.exist.util;
 
 import org.apache.commons.pool.PoolableObjectFactory;
 import org.apache.commons.pool.impl.StackObjectPool;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
-import org.exist.Namespaces;
 import org.exist.storage.BrokerPool;
 import org.exist.storage.BrokerPoolService;
-import org.exist.validation.GrammarPool;
-import org.xml.sax.SAXNotRecognizedException;
-import org.xml.sax.SAXNotSupportedException;
 import org.xml.sax.XMLReader;
-import org.xml.sax.ext.DefaultHandler2;
 
 /**
  * Maintains a pool of XMLReader objects.
- * <p>
+ *
  * The pool is available through {@link BrokerPool#getParserPool()}.
  */
 public class XMLReaderPool extends StackObjectPool<XMLReader> implements BrokerPoolService {
-
-    private static final Logger LOG = LogManager.getLogger(XMLReaderPool.class);
-
-    private static final DefaultHandler2 DUMMY_HANDLER = new DefaultHandler2();
 
     /**
      * Constructs an XML Reader Pool.
@@ -75,23 +64,7 @@ public class XMLReaderPool extends StackObjectPool<XMLReader> implements BrokerP
     }
 
     public void returnXMLReader(final XMLReader reader) {
-        if (reader == null) {
-            return;
-        }
-
         try {
-            reader.setContentHandler(DUMMY_HANDLER);
-            reader.setErrorHandler(DUMMY_HANDLER);
-            reader.setProperty(Namespaces.SAX_LEXICAL_HANDLER, DUMMY_HANDLER);
-
-            // DIZZZ; workaround Xerces bug. Cached DTDs cause for problems during validation parsing.
-            final GrammarPool grammarPool =
-                    (GrammarPool) getReaderProperty(reader,
-                            XMLReaderObjectFactory.APACHE_PROPERTIES_INTERNAL_GRAMMARPOOL);
-            if (grammarPool != null) {
-                grammarPool.clearDTDs();
-            }
-
             super.returnObject(reader);
 
         } catch (final Exception e) {
@@ -103,21 +76,6 @@ public class XMLReaderPool extends StackObjectPool<XMLReader> implements BrokerP
     public void returnObject(final XMLReader obj) throws Exception {
         returnXMLReader(obj);
     }
-
-    private Object getReaderProperty(final XMLReader xmlReader, final String propertyName) {
-        Object object = null;
-        try {
-            object = xmlReader.getProperty(propertyName);
-
-        } catch (final SAXNotRecognizedException ex) {
-            LOG.error("SAXNotRecognizedException: {}", ex.getMessage());
-
-        } catch (final SAXNotSupportedException ex) {
-            LOG.error("SAXNotSupportedException: {}", ex.getMessage());
-        }
-        return object;
-    }
-
 
     // just used for config properties
     public interface XmlParser {
