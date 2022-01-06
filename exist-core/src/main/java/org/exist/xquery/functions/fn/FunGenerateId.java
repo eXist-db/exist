6c7213b0a6e302e608649eda4d33a22645635ebb
Adam Retter <adam.retter@googlemail.com>
2022-01-06 16:33:43 +0100

    [ignore] Code cleanup

--- exist-core/src/main/java/org/exist/xquery/functions/fn/FunGenerateId.java
+++ exist-core/src/main/java/org/exist/xquery/functions/fn/FunGenerateId.java
@@ -21,47 +21,48 @@
  */
 package org.exist.xquery.functions.fn;
 
-import org.exist.dom.QName;
 import org.exist.xquery.*;
 import org.exist.xquery.value.*;
 
+import static org.exist.xquery.FunctionDSL.*;
+import static org.exist.xquery.functions.fn.FnModule.functionSignatures;
+
 public class FunGenerateId extends BasicFunction {
 
-    public final static FunctionSignature[] signatures = {
-        new FunctionSignature(
-            new QName("generate-id", Function.BUILTIN_FUNCTION_NS),
-            "This function returns a string that uniquely identifies a given node. Without an argument, the node to identify is " +
-            "taken from the current context item.",
-            null,
-            new FunctionReturnSequenceType(Type.STRING, Cardinality.EXACTLY_ONE, "Unique identifier for the node")),
-        new FunctionSignature(
-            new QName("generate-id", Function.BUILTIN_FUNCTION_NS),
-            "This function returns a string that uniquely identifies a given node.",
-            new SequenceType[] {
-                new FunctionParameterSequenceType("node", Type.NODE, Cardinality.ZERO_OR_ONE,
-                    "The node for which an identifier will be generated. If it is the empty sequence, the result will " +
-                    "be the empty string")
-            },
-            new FunctionReturnSequenceType(Type.STRING, Cardinality.EXACTLY_ONE, "Unique identifier for the node"))
-    };
+    private static final String FN_GENERATE_ID = "generate-id";
+    public static final FunctionSignature[] signatures = functionSignatures(
+            FN_GENERATE_ID,
+            "This function returns a string that uniquely identifies a given node. Without an argument, the node to identify is taken from the current context item.",
+            returns(Type.STRING, "Unique identifier for the node"),
+            arities(
+                    arity(),
+                    arity(
+                            optParam("node", Type.NODE, "The node for which an identifier will be generated. If it is the empty sequence, the result will be the empty string")
+                    )
+            )
+    );
 
-    public FunGenerateId(XQueryContext context, FunctionSignature signature) {
+    public FunGenerateId(final XQueryContext context, final FunctionSignature signature) {
         super(context, signature);
     }
 
     @Override
-    public Sequence eval(Sequence[] args, Sequence contextSequence) throws XPathException {
-        NodeValue node = null;
+    public Sequence eval(final Sequence[] args, final Sequence contextSequence) throws XPathException {
+        final NodeValue node;
         if (getArgumentCount() == 0) {
-            if (contextSequence.isEmpty())
-                {throw new XPathException(this, ErrorCodes.XPDY0002, "No context item available in call to generate-id");}
+            if (contextSequence.isEmpty()) {
+                throw new XPathException(this, ErrorCodes.XPDY0002, "No context item available in call to generate-id");
+            }
             final Item contextItem = contextSequence.itemAt(0);
-            if (Type.subTypeOf(contextItem.getType(), Type.NODE))
-                {throw new XPathException(this, ErrorCodes.XPTY0004, "Context item is not a node in call to generate-id");}
+            if (Type.subTypeOf(contextItem.getType(), Type.NODE)) {
+                throw new XPathException(this, ErrorCodes.XPTY0004, "Context item is not a node in call to generate-id");
+            }
             node = (NodeValue) contextItem;
+
         } else {
-            if (args[0].isEmpty())
-                {return StringValue.EMPTY_STRING;}
+            if (args[0].isEmpty()) {
+                return StringValue.EMPTY_STRING;
+            }
             node = (NodeValue) args[0].itemAt(0);
         }
 
