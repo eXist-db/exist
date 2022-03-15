8313dac648c4a32b91bd2cad9eb1b6ab52c1dffb
Adam Retter <adam.retter@googlemail.com>
2022-03-15 19:57:50 +0100

    [bugfix] sm:id#0 did not previously return the effective account details if only setUid was in play

--- exist-core/src/test/java/org/exist/xquery/functions/securitymanager/IdFunctionTest.java
+++ exist-core/src/test/java/org/exist/xquery/functions/securitymanager/IdFunctionTest.java
@@ -135,6 +135,8 @@ public class IdFunctionTest {
         expect(mckContext.getEffectiveUser()).andReturn(mckUser);
         expect(mckUser.getId()).andReturn(1);
 
+        expect(mckUser.getGroupIds()).andReturn(new int[] {101, 102}).times(2);
+
         replay(mckUser, mckContext);
 
         final IdFunction idFunctions = new IdFunction(mckContext, IdFunction.FNS_ID);
@@ -156,5 +158,65 @@ public class IdFunctionTest {
         assertEquals("", actualEffectiveUsername);
 
         verify(mckUser, mckContext);
+    }
+
+    /**
+     * Test of eval method, of class IdFunction.
+     * when real and effective users are have the same username/id
+     * but have different group memberships - as can happen with setGid
+     * without setUid.
+     */
+    @Test
+    public void differingByGroupRealAndEffectiveUsers() throws XPathException, XpathException {
+        final XQueryContext mckContext = createMockBuilder(XQueryContext.class)
+                .addMockedMethod("pushDocumentContext")
+                .addMockedMethod("getDocumentBuilder", new Class[0])
+                .addMockedMethod("popDocumentContext")
+                .addMockedMethod("getRealUser")
+                .addMockedMethod("getEffectiveUser")
+                .createMock();
+
+        final Subject mckRealUser = EasyMock.createMock(Subject.class);
+        final String realUsername = "user1";
+        mckContext.pushDocumentContext();
+        expectLastCall().once();
+        expect(mckContext.getDocumentBuilder()).andReturn(new MemTreeBuilder());
+        mckContext.popDocumentContext();
+        expectLastCall().once();
+        expect(mckContext.getRealUser()).andReturn(mckRealUser).times(2);
+        expect(mckRealUser.getName()).andReturn(realUsername);
+        expect(mckRealUser.getGroups()).andReturn(new String[]{"realGroup1"});
+        expect(mckRealUser.getId()).andReturn(101);
+        expect(mckRealUser.getGroupIds()).andReturn(new int[] {101});
+
+        final Subject mckEffectiveUser = EasyMock.createMock(Subject.class);
+        final String effectiveUsername = "user1";
+        expect(mckContext.getEffectiveUser()).andReturn(mckEffectiveUser).times(2);
+        expect(mckEffectiveUser.getId()).andReturn(101);
+        expect(mckEffectiveUser.getName()).andReturn(effectiveUsername);
+        expect(mckEffectiveUser.getGroups()).andReturn(new String[]{"realGroup1", "effectiveGroup1"});
+        expect(mckEffectiveUser.getGroupIds()).andReturn(new int[] {101, 102});
+
+        replay(mckEffectiveUser, mckRealUser, mckContext);
+
+        final IdFunction idFunctions = new IdFunction(mckContext, IdFunction.FNS_ID);
+        final Sequence result = idFunctions.eval(new Sequence[]{Sequence.EMPTY_SEQUENCE}, null);
+
+        assertEquals(1, result.getItemCount());
+
+        final XpathEngine xpathEngine = XMLUnit.newXpathEngine();
+        final Map<String, String> namespaces = new HashMap<>();
+        namespaces.put("sm", "http://exist-db.org/xquery/securitymanager");
+        xpathEngine.setNamespaceContext(new SimpleNamespaceContext(namespaces));
+
+        final DocumentImpl resultDoc = (DocumentImpl)result.itemAt(0);
+
+        final String actualRealUsername = xpathEngine.evaluate("/sm:id/sm:real/sm:username", resultDoc);
+        assertEquals(realUsername, actualRealUsername);
+
+        final String actualEffectiveUsername = xpathEngine.evaluate("/sm:id/sm:effective/sm:username", resultDoc);
+        assertEquals(effectiveUsername, actualEffectiveUsername);
+
+        verify(mckEffectiveUser, mckRealUser, mckContext);
     }
 }
