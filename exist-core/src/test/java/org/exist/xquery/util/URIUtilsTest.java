7053a4526fc9b90b69852f101f6acc836be4866d
Adam Retter <adam.retter@googlemail.com>
2022-03-30 11:44:47 +0200

    [refactor] Switch test to JUnit 5

--- exist-core/src/test/java/org/exist/xquery/util/URIUtilsTest.java
+++ exist-core/src/test/java/org/exist/xquery/util/URIUtilsTest.java
@@ -32,9 +32,9 @@
  */
 package org.exist.xquery.util;
 
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 /**
  * @author <a href="mailto:adam@evolvedbinary.com">Adam Retter</a>
