563a1a8df834cdef147aa7aaecea53643b88cfd8
Alan Paxton <alan.paxton@gmail.com>
2022-04-20 16:47:28 +0100

    [test] Tests for exploring missing spellouts

--- exist-core/src/test/java/org/exist/xquery/functions/integer/IntegerPictureTest.java
+++ exist-core/src/test/java/org/exist/xquery/functions/integer/IntegerPictureTest.java
@@ -22,10 +22,12 @@
 
 package org.exist.xquery.functions.integer;
 
+import com.ibm.icu.text.RuleBasedNumberFormat;
 import org.exist.xquery.XPathException;
 import org.junit.Test;
 
 import java.math.BigInteger;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -386,5 +388,48 @@ public class IntegerPictureTest {
         modifierFail("ctt");
         modifierFail("c()t");
         modifierFail("c()");
+    }
+
+    @Test public void localesAndSpellouts() {
+        List<String> iso639Alpha2Codes = Arrays.asList(new String[]{
+                "aa", "ab", "ae", "af", "ak", "am", "an", "ar", "as", "av", "ay", "az",
+                "ba", "be", "bg", "bi", "bm", "bn", "bo", "br", "bs",
+                "ca", "ce", "ch", "co", "cr", "cs", "cu", "cv", "cy",
+                "da", "de", "dv", "dz",
+                "ee", "el", "en", "eo", "es", "et", "eu",
+                "fa", "ff", "fi", "fj", "fo", "fr", "fy",
+                "ga", "gd", "gl", "gn", "gu", "gv",
+                "ha", "he", "hi", "ho", "hr", "ht", "hu", "hy", "hz",
+                "ia", "id", "ie", "ig", "ii", "ik", "io", "is", "it", "iu",
+                "ja", "jv",
+                "ka", "kg", "ki", "kj", "kk", "kl", "km", "kn", "ko", "kr", "ks", "ku", "kv", "kw", "ky",
+                "la", "lb", "lg", "li", "ln", "lo", "lt", "lu", "lv",
+                "mg", "mh", "mi", "mk", "ml", "mn", "mr", "ms", "mt", "my",
+                "na", "nb", "nd", "ne", "ng", "nl", "nn", "no", "nr", "nv", "ny",
+                "oc", "oj", "om", "or", "os",
+                "pa", "pi", "pl", "ps", "pt",
+                "rm", "rn", "ro", "ru", "rw",
+                "sa", "sc", "sd", "se", "sg", "si", "sk", "sl", "sm", "sn", "so", "sq", "sr", "ss", "st", "su", "sv", "sw",
+                "ta", "te", "tg", "th", "ti", "tk", "tl", "tn", "to", "tr", "ts", "tt", "tw", "ty",
+                "ug", "uk", "ur", "uz",
+                "ve", "vi", "vo",
+                "wa", "wo",
+                "xh",
+                "yi", "yo",
+                "za", "zh", "zu"});
+        for (String isoCode : iso639Alpha2Codes) {
+            Locale locale = (new Locale.Builder()).setLanguage(isoCode).build();
+            RuleBasedNumberFormat ruleBasedNumberFormat = new RuleBasedNumberFormat( locale, RuleBasedNumberFormat.SPELLOUT );
+            int i = 0;
+            Set<String> names = new HashSet<>();
+            System.err.println(isoCode + " --->>>");
+            for (String ruleSetName : ruleBasedNumberFormat.getRuleSetNames()) {
+                System.err.println(isoCode + " " + (++i) + " " + ruleSetName);
+                names.add(ruleSetName);
+            }
+            System.err.println("<<<---" + isoCode);
+            assertEquals(true, names.contains("%spellout-ordinal") || names.contains("%spellout-ordinal-masculine"));
+            assertEquals(true, names.contains("%spellout-cardinal") || names.contains("%spellout-cardinal-masculine"));
+        }
     }
 }
