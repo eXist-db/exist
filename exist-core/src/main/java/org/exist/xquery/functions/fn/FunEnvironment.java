5f3b623e1e08a74cba9e3509a7d503569c86f62f
Adam Retter <adam.retter@googlemail.com>
2022-06-20 13:48:09 +0200

    [ignore] Reformat code

--- exist-core/src/main/java/org/exist/xquery/functions/fn/FunEnvironment.java
+++ exist-core/src/main/java/org/exist/xquery/functions/fn/FunEnvironment.java
@@ -7,21 +7,22 @@
  * modify it under the terms of the GNU Lesser General Public License
  * as published by the Free Software Foundation; either version 2
  * of the License, or (at your option) any later version.
- *  
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public License
  * along with this program; if not, write to the Free Software Foundation
  * Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
- *  
+ *
  *  $Id$
  */
 package org.exist.xquery.functions.fn;
 
 import java.util.Map;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.exist.dom.QName;
@@ -41,38 +42,38 @@ import org.exist.xquery.value.ValueSequence;
 
 public class FunEnvironment extends BasicFunction {
 
-    protected static final Logger logger = LogManager.getLogger(FunEnvironment.class);
+    private static final Logger LOGGER = LogManager.getLogger(FunEnvironment.class);
 
     public final static FunctionSignature signature[] = {
-        new FunctionSignature(
-            new QName("available-environment-variables", Function.BUILTIN_FUNCTION_NS),
-            "Returns a list of environment variable names.",
-            null,
-            new FunctionReturnSequenceType(Type.STRING, Cardinality.ZERO_OR_MORE, 
-            "Returns a sequence of strings, being the names of the environment variables. User must be DBA.")
-        ),
-        new FunctionSignature(
-            new QName("environment-variable", Function.BUILTIN_FUNCTION_NS),
-            "Returns the value of a system environment variable, if it exists.",
-            new SequenceType[] {
-                new FunctionParameterSequenceType("name", Type.STRING,
-                    Cardinality.EXACTLY_ONE, "Name of environment variable.")
-            },
-            new FunctionReturnSequenceType(Type.STRING, Cardinality.ZERO, "Corrensponding value of the environment variable, "
-            + "if there is no environment variable with a matching name, the function returns the empty sequence. User must be DBA.")
-        )
+            new FunctionSignature(
+                    new QName("available-environment-variables", Function.BUILTIN_FUNCTION_NS),
+                    "Returns a list of environment variable names.",
+                    null,
+                    new FunctionReturnSequenceType(Type.STRING, Cardinality.ZERO_OR_MORE,
+                            "Returns a sequence of strings, being the names of the environment variables. User must be DBA.")
+            ),
+            new FunctionSignature(
+                    new QName("environment-variable", Function.BUILTIN_FUNCTION_NS),
+                    "Returns the value of a system environment variable, if it exists.",
+                    new SequenceType[]{
+                            new FunctionParameterSequenceType("name", Type.STRING,
+                                    Cardinality.EXACTLY_ONE, "Name of environment variable.")
+                    },
+                    new FunctionReturnSequenceType(Type.STRING, Cardinality.ZERO, "Corrensponding value of the environment variable, "
+                            + "if there is no environment variable with a matching name, the function returns the empty sequence. User must be DBA.")
+            )
     };
 
-    public FunEnvironment(XQueryContext context, FunctionSignature signature) {
+    public FunEnvironment(final XQueryContext context, final FunctionSignature signature) {
         super(context, signature);
     }
 
     @Override
-    public Sequence eval(Sequence[] args, Sequence contextSequence) throws XPathException {
+    public Sequence eval(final Sequence[] args, final Sequence contextSequence) throws XPathException {
 
         if (!context.getSubject().hasDbaRole()) {
             final String txt = "Permission denied, calling user '" + context.getSubject().getName() + "' must be a DBA to call this function.";
-            logger.error(txt);
+            LOGGER.error(txt);
             return Sequence.EMPTY_SEQUENCE;
         }
 
--- exist-core/src/main/java/org/exist/xquery/functions/util/SystemProperty.java
+++ exist-core/src/main/java/org/exist/xquery/functions/util/SystemProperty.java
@@ -2,21 +2,21 @@
  *  eXist Open Source Native XML Database
  *  Copyright (C) 2004-09 The eXist Project
  *  http://exist-db.org
- *  
+ *
  *  This program is free software; you can redistribute it and/or
  *  modify it under the terms of the GNU Lesser General Public License
  *  as published by the Free Software Foundation; either version 2
  *  of the License, or (at your option) any later version.
- *  
+ *
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *  GNU Lesser General Public License for more details.
- *  
+ *
  *  You should have received a copy of the GNU Lesser General Public License
  *  along with this program; if not, write to the Free Software
  *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
- *  
+ *
  *  $Id$
  */
 package org.exist.xquery.functions.util;
@@ -35,42 +35,37 @@ import org.exist.xquery.value.SequenceType;
 import org.exist.xquery.value.StringValue;
 import org.exist.xquery.value.Type;
 
+import static org.exist.xquery.FunctionDSL.param;
+import static org.exist.xquery.FunctionDSL.returnsOpt;
+import static org.exist.xquery.functions.util.UtilModule.functionSignature;
+
 /**
- * Libary function to retrieve the value of a system property.
+ * Library function to retrieve the value of a system property.
+ *
  * @author Wolfgang Meier
  * @author Loren Cahlander
  */
 public class SystemProperty extends BasicFunction {
 
-    public final static FunctionSignature signature = new FunctionSignature(
-        new QName("system-property", UtilModule.NAMESPACE_URI, UtilModule.PREFIX),
-        "Returns the value of a system property. Similar to the corresponding XSLT function. " +
-        "Predefined properties are: vendor, vendor-url, product-name, product-version, product-build, and all Java " +
-        "system properties.",
-        new SequenceType[] { 
-            new FunctionParameterSequenceType("property-name", Type.STRING, Cardinality.EXACTLY_ONE, "The name of the system property to retrieve the value of.")
-        },
-        new FunctionReturnSequenceType(Type.STRING, Cardinality.ZERO_OR_ONE, "the value of the named system property")
+    public final static FunctionSignature signature = functionSignature(
+            "system-property",
+            "Returns the value of a system property. Similar to the corresponding XSLT function. " +
+                    "Predefined properties are: vendor, vendor-url, product-name, product-version, product-build, and all Java " +
+                    "System Properties.",
+            returnsOpt(Type.STRING, "the value of the named system property"),
+            param("property-name", Type.STRING, "The name of the system property to retrieve the value of.")
     );
-	
-    /**
-     * @param context
-     * @param signature
-     */
-    public SystemProperty(XQueryContext context, FunctionSignature signature) {
+
+    public SystemProperty(final XQueryContext context, final FunctionSignature signature) {
         super(context, signature);
     }
 
-    /* (non-Javadoc)
-     * @see org.exist.xquery.BasicFunction#eval(org.exist.xquery.value.Sequence[], org.exist.xquery.value.Sequence)
-     */
     @Override
-    public Sequence eval(Sequence[] args, Sequence contextSequence) throws XPathException {
-
+    public Sequence eval(final Sequence[] args, final Sequence contextSequence) throws XPathException {
         final String key = args[0].getStringValue();
         String value = SystemProperties.getInstance().getSystemProperty(key, null);
-        if(value == null) {
-                value = System.getProperty(key);
+        if (value == null) {
+            value = System.getProperty(key);
         }
         return value == null ? Sequence.EMPTY_SEQUENCE : new StringValue(value);
     }
