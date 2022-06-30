eXist-db/exist

commit 1e9131801ff488505d05595e5ea8ae874ba5bcb4

[bugfix] Don't destroy a BinaryValue if it is within a Map or Array in the returned Context Sequence (i.e. still in-scope)

exist-core/src/main/java/org/exist/xquery/value/BinaryValueFromFile.java
@@ -146,8 +146,7 @@ public class BinaryValueFromFile extends BinaryValue {
     @Override
     public void destroy(final XQueryContext context, final Sequence contextSequence) {
         // do not close if this object is part of the contextSequence
-        if (contextSequence == this ||
-                (contextSequence instanceof ValueSequence && ((ValueSequence) contextSequence).containsValue(this))) {
+        if (contextSequence == this || contextSequence.containsReference(this)) {
             return;
         }
         try {

exist-core/src/main/java/org/exist/xquery/value/BinaryValueFromInputStream.java
@@ -121,8 +121,7 @@ public class BinaryValueFromInputStream extends BinaryValue {
     @Override
     public void destroy(XQueryContext context, final Sequence contextSequence) {
         // do not close if this object is part of the contextSequence
-        if (contextSequence == this
-                || (contextSequence instanceof ValueSequence && ((ValueSequence) contextSequence).containsValue(this))) {
+        if (contextSequence == this || contextSequence.containsReference(this)) {
             return;
         }
         LOG.debug("Closing input stream");

commit 12e1924f617b613d88334d3357364f8b8edc0cc1

[bugfix] Avoid an NPE when disposing Binary Values under some circumstances. Regression introduced in 1e91318

exist-core/src/main/java/org/exist/dom/memtree/NodeImpl.java
@@ -42,6 +42,7 @@ import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.ext.LexicalHandler;
 
+import javax.annotation.Nullable;
 import javax.xml.XMLConstants;
 import java.util.Iterator;
 import java.util.Properties;
@@ -762,7 +763,7 @@ public abstract class NodeImpl<T extends NodeImpl> implements INode<DocumentImpl
     }
 
     @Override
-    public void destroy(final XQueryContext context, final Sequence contextSequence) {
+    public void destroy(final XQueryContext context, @Nullable final Sequence contextSequence) {
     }
 
 

exist-core/src/main/java/org/exist/dom/persistent/NodeProxy.java
@@ -45,6 +45,7 @@ import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 import org.xml.sax.ext.LexicalHandler;
 
+import javax.annotation.Nullable;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.XMLStreamReader;
 import java.io.IOException;
@@ -987,7 +988,7 @@ public class NodeProxy implements NodeSet, NodeValue, NodeHandle, DocumentSet, C
     }
 
     @Override
-    public void destroy(final XQueryContext context, final Sequence contextSequence) {
+    public void destroy(final XQueryContext context, @Nullable final Sequence contextSequence) {
         // Nothing to do
     }
 

exist-core/src/main/java/org/exist/xquery/DeferredFunctionCall.java
@@ -35,6 +35,7 @@ import org.exist.xquery.value.Sequence;
 import org.exist.xquery.value.SequenceIterator;
 import org.exist.xquery.value.Type;
 
+import javax.annotation.Nullable;
 import java.util.Iterator;
 
 public abstract class DeferredFunctionCall implements Sequence {
@@ -309,7 +310,7 @@ public abstract class DeferredFunctionCall implements Sequence {
     }
 
     @Override
-    public void destroy(XQueryContext context, Sequence contextSequence) {
+    public void destroy(final XQueryContext context, @Nullable final Sequence contextSequence) {
         // nothing to do
     }
 }

exist-core/src/main/java/org/exist/xquery/ModuleContext.java
@@ -311,7 +311,7 @@ public class ModuleContext extends XQueryContext {
     }
 
     @Override
-    public void popLocalVariables(final LocalVariable var, final Sequence resultSequence) {
+    public void popLocalVariables(final LocalVariable var, @Nullable final Sequence resultSequence) {
         parentContext.popLocalVariables(var, resultSequence);
     }
 

exist-core/src/main/java/org/exist/xquery/VariableImpl.java
@@ -30,6 +30,8 @@ import org.exist.xquery.value.*;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
+import javax.annotation.Nullable;
+
 /**
  * An XQuery/XPath variable, consisting of a QName and a value.
  * 
@@ -164,9 +166,10 @@ public class VariableImpl implements Variable {
         this.initialized = initialized;
     }
 
-    public void destroy(XQueryContext context, Sequence contextSequence) {
-        if (value != null)
-            {value.destroy(context, contextSequence);}
+    public void destroy(final XQueryContext context, @Nullable final Sequence contextSequence) {
+        if (value != null) {
+			value.destroy(context, contextSequence);
+		}
     }
 
     @Override

exist-core/src/main/java/org/exist/xquery/XQueryContext.java
@@ -2354,7 +2354,7 @@ public class XQueryContext implements BinaryValueManager, Context {
      * @param var       only clear variables after this variable, or null
      * @param resultSeq the result sequence
      */
-    public void popLocalVariables(@Nullable final LocalVariable var, final Sequence resultSeq) {
+    public void popLocalVariables(@Nullable final LocalVariable var, @Nullable final Sequence resultSeq) {
         if (var != null) {
             // clear all variables registered after var. they should be out of scope.
             LocalVariable outOfScope = var.after;

exist-core/src/main/java/org/exist/xquery/value/AbstractSequence.java
@@ -32,6 +32,7 @@ import org.exist.xquery.ErrorCodes;
 import org.exist.xquery.XPathException;
 import org.exist.xquery.XQueryContext;
 
+import javax.annotation.Nullable;
 import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Iterator;
@@ -287,7 +288,7 @@ public abstract class AbstractSequence implements Sequence {
     }
 
     @Override
-    public void destroy(final XQueryContext context, final Sequence contextSequence) {
+    public void destroy(final XQueryContext context, @Nullable final Sequence contextSequence) {
         // do nothing by default
     }
 }

exist-core/src/main/java/org/exist/xquery/value/ArrayListValueSequence.java
@@ -255,7 +255,7 @@ public class ArrayListValueSequence extends AbstractSequence implements MemoryNo
     }
 
     @Override
-    public void destroy(final XQueryContext context, final Sequence contextSequence) {
+    public void destroy(final XQueryContext context, @Nullable final Sequence contextSequence) {
         for (final Item value : values) {
             value.destroy(context, contextSequence);
         }

exist-core/src/main/java/org/exist/xquery/value/AtomicValue.java
@@ -42,6 +42,7 @@ import org.exist.xquery.util.ExpressionDumper;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 
+import javax.annotation.Nullable;
 import java.util.Iterator;
 import java.util.Properties;
 
@@ -362,7 +363,7 @@ public abstract class AtomicValue implements Item, Sequence, Indexable {
     }
 
     @Override
-    public void destroy(final XQueryContext context, final Sequence contextSequence) {
+    public void destroy(final XQueryContext context, @Nullable final Sequence contextSequence) {
         // nothing to be done by default
     }
 

exist-core/src/main/java/org/exist/xquery/value/BinaryValueFromFile.java
@@ -26,6 +26,7 @@ import org.exist.util.io.ByteBufferInputStream;
 import org.exist.xquery.XPathException;
 import org.exist.xquery.XQueryContext;
 
+import javax.annotation.Nullable;
 import java.io.InputStream;
 import java.io.IOException;
 import java.io.OutputStream;
@@ -144,9 +145,9 @@ public class BinaryValueFromFile extends BinaryValue {
     }
 
     @Override
-    public void destroy(final XQueryContext context, final Sequence contextSequence) {
+    public void destroy(final XQueryContext context, @Nullable final Sequence contextSequence) {
         // do not close if this object is part of the contextSequence
-        if (contextSequence == this || contextSequence.containsReference(this)) {
+        if (contextSequence != null && (contextSequence == this || contextSequence.containsReference(this))) {
             return;
         }
         try {

exist-core/src/main/java/org/exist/xquery/value/BinaryValueFromInputStream.java
@@ -29,6 +29,7 @@ import org.exist.util.io.FilterInputStreamCacheFactory;
 import org.exist.xquery.XPathException;
 import org.exist.xquery.XQueryContext;
 
+import javax.annotation.Nullable;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -119,9 +120,9 @@ public class BinaryValueFromInputStream extends BinaryValue {
     }
 
     @Override
-    public void destroy(XQueryContext context, final Sequence contextSequence) {
+    public void destroy(final XQueryContext context, @Nullable final Sequence contextSequence) {
         // do not close if this object is part of the contextSequence
-        if (contextSequence == this || contextSequence.containsReference(this)) {
+        if (contextSequence != null && (contextSequence == this || contextSequence.containsReference(this))) {
             return;
         }
         LOG.debug("Closing input stream");

exist-core/src/main/java/org/exist/xquery/value/Item.java
@@ -30,6 +30,7 @@ import org.exist.xquery.XQueryContext;
 import org.xml.sax.ContentHandler;
 import org.xml.sax.SAXException;
 
+import javax.annotation.Nullable;
 import java.util.Properties;
 
 /**
@@ -70,7 +71,7 @@ public interface Item {
      * @param context current context
      * @param contextSequence the sequence to clean up
      */
-    void destroy(XQueryContext context, Sequence contextSequence);
+    void destroy(XQueryContext context, @Nullable Sequence contextSequence);
 
     /**
      * Convert this item into an atomic value, whose type corresponds to

exist-core/src/main/java/org/exist/xquery/value/Sequence.java
@@ -356,7 +356,7 @@ public interface Sequence {
      * @param context the XQuery context
      * @param contextSequence the context sequence
      */
-    void destroy(XQueryContext context, Sequence contextSequence);
+    void destroy(final XQueryContext context, @Nullable final Sequence contextSequence);
 
     static Sequence of(@Nullable final XmldbURI... uris) throws XPathException {
         if (uris == null || uris.length == 0 || (uris.length == 1 && uris[0] == null)) {

exist-core/src/main/java/org/exist/xquery/value/SubSequence.java
@@ -472,7 +472,7 @@ public class SubSequence extends AbstractSequence {
     }
 
     @Override
-    public void destroy(final XQueryContext context, final Sequence contextSequence) {
+    public void destroy(final XQueryContext context, @Nullable final Sequence contextSequence) {
         sequence.destroy(context, contextSequence);
     }
 

exist-core/src/main/java/org/exist/xquery/value/ValueSequence.java
@@ -35,6 +35,7 @@ import org.exist.xquery.NodeTest; // Needed by Eclipse to disambiguate
 import org.w3c.dom.Document;
 import org.w3c.dom.Node;
 
+import javax.annotation.Nullable;
 import java.util.*;
 
 /**
@@ -385,7 +386,7 @@ public class ValueSequence extends AbstractSequence implements MemoryNodeSet {
     }
 
     @Override
-    public void destroy(final XQueryContext context, final Sequence contextSequence) {
+    public void destroy(final XQueryContext context, @Nullable final Sequence contextSequence) {
         holderVar = null;
         for (int i = 0; i <= size; i++) {
             values[i].destroy(context, contextSequence);