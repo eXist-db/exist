c99ffea7e6bb51b800b392bc32229c647a051cd7
Gabriele Tomassetti <gabriele@strumenta.com>
2022-07-04 14:05:01 +0200

    [test] Improve testing for Tumbling Window

--- exist-core/src/test/java/org/exist/xquery/TumblingWindowClauseTest.java
+++ exist-core/src/test/java/org/exist/xquery/TumblingWindowClauseTest.java
@@ -24,14 +24,16 @@ package org.exist.xquery;
 import antlr.RecognitionException;
 import antlr.TokenStreamException;
 import org.exist.EXistException;
-import org.exist.security.PermissionDeniedException;
-import org.exist.util.LockException;
+import org.exist.storage.BrokerPool;
+import org.exist.storage.DBBroker;
+import org.exist.test.ExistEmbeddedServer;
+import org.exist.xquery.parser.XQueryAST;
 import org.exist.xquery.parser.XQueryLexer;
 import org.exist.xquery.parser.XQueryParser;
+import org.exist.xquery.parser.XQueryTreeParser;
+import org.junit.ClassRule;
 import org.junit.Test;
-import org.xml.sax.SAXException;
 
-import java.io.IOException;
 import java.io.StringReader;
 
 import static org.junit.Assert.*;
@@ -41,20 +43,107 @@ import static org.junit.Assert.*;
  */
 public class TumblingWindowClauseTest {
 
-    @Test
-    public void query() throws EXistException, PermissionDeniedException, IOException, SAXException, LockException, RecognitionException, XPathException, TokenStreamException {
-        final String query =  "xquery version \"3.0\";\n" +
-                        "for tumbling window $w in (2, 4, 6, 8, 10, 12, 14)\n" +
-                        "    start at $s when fn:true()\n" +
-                        "    only end at $e when $e - $s eq 2\n" +
-                        "return <window>{ $w }</window>";
+    @ClassRule
+    public static final ExistEmbeddedServer existEmbeddedServer = new ExistEmbeddedServer(true, true);
 
-        // parse the query into the internal syntax tree
-        final XQueryLexer lexer = new XQueryLexer(new StringReader(query));
-        final XQueryParser xparser = new XQueryParser(lexer);
-        xparser.xpath();
-        if (xparser.foundErrors()) {
-            fail(xparser.getErrorMessage());
+    @Test
+    public void simpleWindowConditions() throws EXistException, RecognitionException, XPathException, TokenStreamException {
+        final String query = "xquery version \"3.1\";\n" +
+                "for tumbling window $w in (2, 4, 6, 8, 10, 12, 14)\n" +
+                "    start at $s when fn:true()\n" +
+                "    only end at $e when $e - $s eq 2\n" +
+                "return <window>{ $w }</window>";
+
+        final BrokerPool pool = existEmbeddedServer.getBrokerPool();
+        try (final DBBroker broker = pool.getBroker()) {
+            // parse the query into the internal syntax tree
+            final XQueryContext context = new XQueryContext(broker.getBrokerPool());
+            final XQueryLexer lexer = new XQueryLexer(context, new StringReader(query));
+            final XQueryParser xparser = new XQueryParser(lexer);
+            xparser.xpath();
+            if (xparser.foundErrors()) {
+                fail(xparser.getErrorMessage());
+                return;
+            }
+
+            final XQueryAST ast = (XQueryAST) xparser.getAST();
+
+            final XQueryTreeParser treeParser = new XQueryTreeParser(context);
+            final PathExpr expr = new PathExpr(context);
+            treeParser.xpath(ast, expr);
+            if (treeParser.foundErrors()) {
+                fail(treeParser.getErrorMessage());
+                return;
+            }
+
+            assertTrue("Expression should be of type WindowExpr", expr.getFirst() instanceof WindowExpr);
+        }
+    }
+
+    @Test
+    public void complexWindowCondition() throws EXistException, RecognitionException, XPathException, TokenStreamException {
+        final String query = "xquery version \"3.1\";\n" +
+                "for tumbling window $w in (2, 4, 6, 8, 10, 12, 14)\n" +
+                "   start $first next $second when $first/price < $second/price\n" +
+                "   end $last next $beyond when $last/price > $beyond/price\n" +
+                "return <window>{ $w }</window>";
+
+        final BrokerPool pool = existEmbeddedServer.getBrokerPool();
+        try (final DBBroker broker = pool.getBroker()) {
+            // parse the query into the internal syntax tree
+            final XQueryContext context = new XQueryContext(broker.getBrokerPool());
+            final XQueryLexer lexer = new XQueryLexer(context, new StringReader(query));
+            final XQueryParser xparser = new XQueryParser(lexer);
+            xparser.xpath();
+            if (xparser.foundErrors()) {
+                fail(xparser.getErrorMessage());
+                return;
+            }
+
+            final XQueryAST ast = (XQueryAST) xparser.getAST();
+
+            final XQueryTreeParser treeParser = new XQueryTreeParser(context);
+            final PathExpr expr = new PathExpr(context);
+            treeParser.xpath(ast, expr);
+            if (treeParser.foundErrors()) {
+                fail(treeParser.getErrorMessage());
+                return;
+            }
+
+            assertTrue("Expression should be of type WindowExpr", expr.getFirst() instanceof WindowExpr);
+        }
+    }
+
+    @Test
+    public void noEndWindowCondition() throws EXistException, RecognitionException, XPathException, TokenStreamException {
+        final String query = "xquery version \"3.1\";\n" +
+                "for tumbling window $w in (2, 4, 6, 8, 10, 12, 14)\n" +
+                "   start $first next $second when $first/price < $second/price\n" +
+                "return <window>{ $w }</window>";
+
+        final BrokerPool pool = existEmbeddedServer.getBrokerPool();
+        try (final DBBroker broker = pool.getBroker()) {
+            // parse the query into the internal syntax tree
+            final XQueryContext context = new XQueryContext(broker.getBrokerPool());
+            final XQueryLexer lexer = new XQueryLexer(context, new StringReader(query));
+            final XQueryParser xparser = new XQueryParser(lexer);
+            xparser.xpath();
+            if (xparser.foundErrors()) {
+                fail(xparser.getErrorMessage());
+                return;
+            }
+
+            final XQueryAST ast = (XQueryAST) xparser.getAST();
+
+            final XQueryTreeParser treeParser = new XQueryTreeParser(context);
+            final PathExpr expr = new PathExpr(context);
+            treeParser.xpath(ast, expr);
+            if (treeParser.foundErrors()) {
+                fail(treeParser.getErrorMessage());
+                return;
+            }
+
+            assertTrue("Expression should be of type WindowExpr", expr.getFirst() instanceof WindowExpr);
         }
     }
 }
