a0ec78aecc25589ec457c1e4f8e6286745f54feb
Gabriele Tomassetti <gabriele@strumenta.com>
2022-07-20 17:27:39 +0200

    [test] Add new tests found in reference

--- exist-core/src/test/java/org/exist/xquery/update/XQueryUpdate3Test.java
+++ exist-core/src/test/java/org/exist/xquery/update/XQueryUpdate3Test.java
@@ -245,7 +245,7 @@ public class XQueryUpdate3Test {
     public void copyModifyExprTest() throws EXistException, RecognitionException, XPathException, TokenStreamException, PermissionDeniedException
     {
         String query = "copy $je := $e\n" +
-                "   modify $je\n" +
+                "   modify delete node $je/salary\n" +
                 "   return $je";
 
         BrokerPool pool = BrokerPool.getInstance();
@@ -264,11 +264,50 @@ public class XQueryUpdate3Test {
 
             XQueryTreeParser treeParser = new XQueryTreeParser(context);
             PathExpr expr = new PathExpr(context);
-            treeParser.expr(ast, expr);
+            Expression ret = treeParser.expr(ast, expr);
             if (treeParser.foundErrors()) {
                 fail(treeParser.getErrorMessage());
                 return;
             }
+
+            assertTrue(ret instanceof CopyModifyExpression);
+            assertEquals("$je",((CopyModifyExpression) ret).getReturnExpr().toString());
+        }
+    }
+
+    @Test
+    public void copyModifyExprTestComplexModify() throws EXistException, RecognitionException, XPathException, TokenStreamException, PermissionDeniedException
+    {
+        String query = "copy $newx := $oldx\n" +
+                "   modify (rename node $newx as \"newx\", \n" +
+                "           replace value of node $newx with $newx * 2)\n" +
+                "   return ($oldx, $newx)";
+
+        BrokerPool pool = BrokerPool.getInstance();
+        try(final DBBroker broker = pool.getBroker()) {
+            // parse the query into the internal syntax tree
+            XQueryContext context = new XQueryContext(broker.getBrokerPool());
+            XQueryLexer lexer = new XQueryLexer(context, new StringReader(query));
+            XQueryParser xparser = new XQueryParser(lexer);
+            xparser.expr();
+            if (xparser.foundErrors()) {
+                fail(xparser.getErrorMessage());
+                return;
+            }
+
+            XQueryAST ast = (XQueryAST) xparser.getAST();
+
+            XQueryTreeParser treeParser = new XQueryTreeParser(context);
+            PathExpr expr = new PathExpr(context);
+            Expression ret = treeParser.expr(ast, expr);
+            if (treeParser.foundErrors()) {
+                fail(treeParser.getErrorMessage());
+                return;
+            }
+
+            assertTrue(ret instanceof CopyModifyExpression);
+            assertEquals("( replace $newx \"newx\", replace $newx VALUE $newx * 2 )",((CopyModifyExpression) ret).getModifyExpr().toString());
+            assertEquals("( $oldx, $newx )",((CopyModifyExpression) ret).getReturnExpr().toString());
         }
     }
 
@@ -339,6 +378,48 @@ public class XQueryUpdate3Test {
 
             assertTrue(expr.getFirst() instanceof InsertExpr);
             assertEquals(Expression.Category.UPDATING, expr.getFirst().getCategory());
+            assertEquals(InsertExpr.Choice.AFTER, ((InsertExpr) expr.getFirst()).getChoice());
+        }
+    }
+
+    @Test
+    public void insertExprAsLast() throws EXistException, RecognitionException, XPathException, TokenStreamException, PermissionDeniedException
+    {
+        String query =
+                "insert node $new-police-report\n" +
+                        "   as last into fn:doc(\"insurance.xml\")/policies\n" +
+                        "      /policy[id = $pid]\n" +
+                        "      /driver[license = $license]\n" +
+                        "      /accident[date = $accdate]\n" +
+                        "      /police-reports";
+
+        BrokerPool pool = BrokerPool.getInstance();
+        try(final DBBroker broker = pool.getBroker()) {
+            // parse the query into the internal syntax tree
+            XQueryContext context = new XQueryContext(broker.getBrokerPool());
+            XQueryLexer lexer = new XQueryLexer(context, new StringReader(query));
+            XQueryParser xparser = new XQueryParser(lexer);
+            xparser.expr();
+            if (xparser.foundErrors()) {
+                fail(xparser.getErrorMessage());
+                return;
+            }
+
+            XQueryAST ast = (XQueryAST) xparser.getAST();
+
+            XQueryTreeParser treeParser = new XQueryTreeParser(context);
+            PathExpr expr = new PathExpr(context);
+            treeParser.expr(ast, expr);
+
+            if (treeParser.foundErrors()) {
+                fail(treeParser.getErrorMessage());
+                return;
+            }
+
+            assertTrue(expr.getFirst() instanceof InsertExpr);
+            assertEquals(Expression.Category.UPDATING, expr.getFirst().getCategory());
+            assertEquals("$new-police-report", ((InsertExpr) expr.getFirst()).getSourceExpr().toString());
+            assertEquals(InsertExpr.Choice.LAST, ((InsertExpr) expr.getFirst()).getChoice());
         }
     }
 
@@ -347,6 +428,42 @@ public class XQueryUpdate3Test {
     {
         String query =
                 "delete node fn:doc(\"bib.xml\")/books/book[1]/author[last()]";
+
+        BrokerPool pool = BrokerPool.getInstance();
+        try(final DBBroker broker = pool.getBroker()) {
+            // parse the query into the internal syntax tree
+            XQueryContext context = new XQueryContext(broker.getBrokerPool());
+            XQueryLexer lexer = new XQueryLexer(context, new StringReader(query));
+            XQueryParser xparser = new XQueryParser(lexer);
+            xparser.expr();
+            if (xparser.foundErrors()) {
+                fail(xparser.getErrorMessage());
+                return;
+            }
+
+            XQueryAST ast = (XQueryAST) xparser.getAST();
+
+            XQueryTreeParser treeParser = new XQueryTreeParser(context);
+            PathExpr expr = new PathExpr(context);
+            treeParser.expr(ast, expr);
+
+            if (treeParser.foundErrors()) {
+                fail(treeParser.getErrorMessage());
+                return;
+            }
+
+            assertTrue(expr.getFirst() instanceof DeleteExpr);
+            assertEquals(Expression.Category.UPDATING, expr.getFirst().getCategory());
+            assertEquals("doc(\"bib.xml\")/child::{}books/child::{}book[1]/child::{}author[last()]", ((DeleteExpr) expr.getFirst()).getTargetExpr().toString());
+        }
+    }
+
+    @Test
+    public void deleteExprComplex() throws EXistException, RecognitionException, XPathException, TokenStreamException, PermissionDeniedException
+    {
+        String query =
+                "delete nodes /email/message\n" +
+                        "     [date > xs:dayTimeDuration(\"P365D\")]";
 
         BrokerPool pool = BrokerPool.getInstance();
         try(final DBBroker broker = pool.getBroker()) {
@@ -483,6 +600,42 @@ public class XQueryUpdate3Test {
             assertTrue(expr.getFirst() instanceof RenameExpr);
             assertEquals("doc(\"bib.xml\")/child::{}books/child::{}book[1]/child::{}author[1]",((RenameExpr) expr.getFirst()).getTargetExpr().toString());
             assertEquals("\"principal-author\"",((RenameExpr) expr.getFirst()).getNewNameExpr().toString());
+        }
+    }
+
+    @Test
+    public void renameExprWithExpr() throws EXistException, RecognitionException, XPathException, TokenStreamException, PermissionDeniedException
+    {
+        String query =
+                "rename node fn:doc(\"bib.xml\")/books/book[1]/author[1]\n" +
+                        "as $newname";
+
+        BrokerPool pool = BrokerPool.getInstance();
+        try(final DBBroker broker = pool.getBroker()) {
+            // parse the query into the internal syntax tree
+            XQueryContext context = new XQueryContext(broker.getBrokerPool());
+            XQueryLexer lexer = new XQueryLexer(context, new StringReader(query));
+            XQueryParser xparser = new XQueryParser(lexer);
+            xparser.expr();
+            if (xparser.foundErrors()) {
+                fail(xparser.getErrorMessage());
+                return;
+            }
+
+            XQueryAST ast = (XQueryAST) xparser.getAST();
+
+            XQueryTreeParser treeParser = new XQueryTreeParser(context);
+            PathExpr expr = new PathExpr(context);
+            treeParser.expr(ast, expr);
+
+            if (treeParser.foundErrors()) {
+                fail(treeParser.getErrorMessage());
+                return;
+            }
+
+            assertTrue(expr.getFirst() instanceof RenameExpr);
+            assertEquals("doc(\"bib.xml\")/child::{}books/child::{}book[1]/child::{}author[1]",((RenameExpr) expr.getFirst()).getTargetExpr().toString());
+            assertEquals("$newname",((RenameExpr) expr.getFirst()).getNewNameExpr().toString());
         }
     }
 }
