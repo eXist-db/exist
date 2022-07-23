506e94a759fac31a7344c0e77a58dd52cc9f5f2e
Adam Retter <adam.retter@googlemail.com>
2022-07-23 17:41:18 +0200

    [bugfix] Add missing error codes

--- exist-core/src/main/java/org/exist/xquery/value/SequenceType.java
+++ exist-core/src/main/java/org/exist/xquery/value/SequenceType.java
@@ -222,12 +222,12 @@ public class SequenceType {
      */
     public void checkCardinality(Sequence seq) throws XPathException {
         if (!seq.isEmpty() && cardinality == Cardinality.EMPTY_SEQUENCE) {
-            throw new XPathException((Expression) null, "Empty sequence expected; got " + seq.getItemCount());
+            throw new XPathException((Expression) null, ErrorCodes.XPTY0004, "Empty sequence expected; got " + seq.getItemCount());
         }
         if (seq.isEmpty() && cardinality.atLeastOne()) {
-            throw new XPathException((Expression) null, "Empty sequence is not allowed here");
+            throw new XPathException((Expression) null, ErrorCodes.XPTY0004, "Empty sequence is not allowed here");
         } else if (seq.hasMany() && cardinality.atMostOne()) {
-            throw new XPathException((Expression) null, "Sequence with more than one item is not allowed here");
+            throw new XPathException((Expression) null, ErrorCodes.XPTY0004, "Sequence with more than one item is not allowed here");
         }
     }
 
