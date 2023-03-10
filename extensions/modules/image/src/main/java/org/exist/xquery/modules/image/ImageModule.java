a82a6913af54214839548ff550fcfc21ebb5e8eb
Adam Retter <adam.retter@googlemail.com>
2023-03-10 22:58:37 +0100

    [ignore] Automated reformat of Java code

--- extensions/modules/image/src/main/java/org/exist/xquery/modules/image/ImageModule.java
+++ extensions/modules/image/src/main/java/org/exist/xquery/modules/image/ImageModule.java
@@ -34,33 +34,32 @@ import org.exist.xquery.FunctionDef;
 
 /**
  * eXist Image Module Extension
- * 
+ * <p>
  * An extension module for the eXist Native XML Database that allows operations
  * on images stored in the eXist database.
- * 
+ *
  * @author <a href="mailto:adam@exist-db.org">Adam Retter</a>
  * @author ljo
- * @serial 2006-03-10
  * @version 1.0
- *
- * @see org.exist.xquery.AbstractInternalModule#AbstractInternalModule(org.exist.xquery.FunctionDef[], java.util.Map) 
+ * @serial 2006-03-10
+ * @see org.exist.xquery.AbstractInternalModule#AbstractInternalModule(org.exist.xquery.FunctionDef[], java.util.Map)
  */
 public class ImageModule extends AbstractInternalModule {
 
     public final static String NAMESPACE_URI = "http://exist-db.org/xquery/image";
-	
+
     public final static String PREFIX = "image";
     public final static String INCLUSION_DATE = "2006-03-13";
     public final static String RELEASED_IN_VERSION = "eXist-1.2";
 
     private final static FunctionDef[] functions = {
-        new FunctionDef(GetWidthFunction.signature, GetWidthFunction.class),
-        new FunctionDef(GetHeightFunction.signature, GetHeightFunction.class),
-        new FunctionDef(ScaleFunction.signature, ScaleFunction.class),
-        new FunctionDef(GetThumbnailsFunction.signature, GetThumbnailsFunction.class),
-        new FunctionDef(CropFunction.signature, CropFunction.class)
+            new FunctionDef(GetWidthFunction.signature, GetWidthFunction.class),
+            new FunctionDef(GetHeightFunction.signature, GetHeightFunction.class),
+            new FunctionDef(ScaleFunction.signature, ScaleFunction.class),
+            new FunctionDef(GetThumbnailsFunction.signature, GetThumbnailsFunction.class),
+            new FunctionDef(CropFunction.signature, CropFunction.class)
     };
-	
+
     public ImageModule(Map<String, List<?>> parameters) {
         super(functions, parameters);
     }
@@ -84,17 +83,15 @@ public class ImageModule extends AbstractInternalModule {
     public String getReleaseVersion() {
         return RELEASED_IN_VERSION;
     }
-	
+
     /**
      * Create a thumbnail.
      *
-     * @author <a href="mailto:rtroilo@gmail.com">Rafael Troilo</a>
-     *
-     * @param image the image
+     * @param image  the image
      * @param height the height of the thumbnail
-     * @param width the width of the thumbnail
-     *
+     * @param width  the width of the thumbnail
      * @return the thumbnail
+     * @author <a href="mailto:rtroilo@gmail.com">Rafael Troilo</a>
      */
     protected static BufferedImage createThumb(final Image image, final int height, final int width) {
         int thumbWidth = 0;
@@ -104,26 +101,26 @@ public class ImageModule extends AbstractInternalModule {
         int imageHeight = image.getHeight(null);
         int imageWidth = image.getWidth(null);
 
-        if(imageHeight >= imageWidth) {
-            scaleFactor = (double)height / (double)imageHeight;
-            thumbWidth = (int)(imageWidth * scaleFactor);
+        if (imageHeight >= imageWidth) {
+            scaleFactor = (double) height / (double) imageHeight;
+            thumbWidth = (int) (imageWidth * scaleFactor);
             thumbHeight = height;
-            if(thumbWidth > width) { // thumbwidth is greater than
+            if (thumbWidth > width) { // thumbwidth is greater than
                 // maxThumbWidth, so we have to scale
                 // again
-                scaleFactor = (double)width / (double)thumbWidth;
-                thumbHeight = (int)(thumbHeight * scaleFactor);
+                scaleFactor = (double) width / (double) thumbWidth;
+                thumbHeight = (int) (thumbHeight * scaleFactor);
                 thumbWidth = width;
             }
         } else {
-            scaleFactor = (double)width / (double)imageWidth;
-            thumbHeight = (int)(imageHeight * scaleFactor);
+            scaleFactor = (double) width / (double) imageWidth;
+            thumbHeight = (int) (imageHeight * scaleFactor);
             thumbWidth = width;
-            if(thumbHeight > height) { // thumbHeight is greater than
+            if (thumbHeight > height) { // thumbHeight is greater than
                 // maxThumbHeight, so we have to scale
                 // again
-                scaleFactor = (double)height / (double)thumbHeight;
-                thumbWidth = (int)(thumbWidth * scaleFactor);
+                scaleFactor = (double) height / (double) thumbHeight;
+                thumbWidth = (int) (thumbWidth * scaleFactor);
                 thumbHeight = height;
             }
         }
@@ -139,7 +136,7 @@ public class ImageModule extends AbstractInternalModule {
                 graphics2D.dispose();
             }
         }
-        
+
         return thumbImage;
     }
 }
--- extensions/modules/image/src/main/java/org/exist/xquery/modules/image/ScaleFunction.java
+++ extensions/modules/image/src/main/java/org/exist/xquery/modules/image/ScaleFunction.java
@@ -47,106 +47,97 @@ import org.exist.xquery.value.SequenceType;
 import org.exist.xquery.value.Type;
 
 /**
- * eXist Image Module Extension ScaleFunction 
- * 
+ * eXist Image Module Extension ScaleFunction
+ * <p>
  * Scale's an Image
- * 
+ *
  * @author <a href="mailto:adam@exist-db.org">Adam Retter</a>
  * @author Loren Cahlander
- * @serial 2007-01-16
  * @version 1.0
- *
+ * @serial 2007-01-16
  * @see org.exist.xquery.BasicFunction#BasicFunction(org.exist.xquery.XQueryContext, org.exist.xquery.FunctionSignature)
  */
-public class ScaleFunction extends BasicFunction
-{
-	private static final Logger logger = LogManager.getLogger(ScaleFunction.class);
-	
-	private final static int MAXHEIGHT = 100;
-	private final static int MAXWIDTH = 100;
+public class ScaleFunction extends BasicFunction {
+    private static final Logger logger = LogManager.getLogger(ScaleFunction.class);
 
-	
-	public final static FunctionSignature signature = new FunctionSignature(
-			new QName("scale", ImageModule.NAMESPACE_URI, ImageModule.PREFIX),
-			"Scale the image image to a specified dimension.  If no dimensions are specified, then the default values are 'maxheight = 100' and 'maxwidth = 100'.",
-			new SequenceType[] {
-				new FunctionParameterSequenceType("image", Type.BASE64_BINARY, Cardinality.EXACTLY_ONE, "The image data"),
-				new FunctionParameterSequenceType("dimension", Type.INTEGER, Cardinality.ZERO_OR_MORE, "The maximum dimension of the scaled image. expressed in pixels (maxheight, maxwidth).  If empty, then the default values are 'maxheight = 100' and 'maxwidth = 100'."),
-				new FunctionParameterSequenceType("mimeType", Type.STRING, Cardinality.EXACTLY_ONE, "The mime-type of the image")
-			},
-			new FunctionReturnSequenceType(Type.BASE64_BINARY, Cardinality.ZERO_OR_ONE, "the scaled image or an empty sequence if $image is invalid"));
+    private final static int MAXHEIGHT = 100;
+    private final static int MAXWIDTH = 100;
 
-	
-	/**
-	 * ScaleFunction Constructor
-	 * 
-	 * @param context	The Context of the calling XQuery
-	 */
-	public ScaleFunction(XQueryContext context)
-	{
-		super(context, signature);
-	}
 
-	/**
-	 * evaluate the call to the xquery scale() function,
-	 * it is really the main entry point of this class
-	 * 
-	 * @param args		arguments from the scale() function call
-	 * @param contextSequence	the Context Sequence to operate on (not used here internally!)
-	 * @return		A sequence representing the result of the scale() function call
-	 * 
-	 * @see org.exist.xquery.BasicFunction#eval(org.exist.xquery.value.Sequence[], org.exist.xquery.value.Sequence)
-	 */
-	public Sequence eval(Sequence[] args, Sequence contextSequence) throws XPathException
-	{
-		//was an image and a mime-type speficifed
-		if(args[0].isEmpty() || args[2].isEmpty()) {
-			return Sequence.EMPTY_SEQUENCE;
-		}
-		
-		//get the maximum dimensions to scale to
-		int maxHeight = MAXHEIGHT;
-		int maxWidth = MAXWIDTH;
+    public final static FunctionSignature signature = new FunctionSignature(
+            new QName("scale", ImageModule.NAMESPACE_URI, ImageModule.PREFIX),
+            "Scale the image image to a specified dimension.  If no dimensions are specified, then the default values are 'maxheight = 100' and 'maxwidth = 100'.",
+            new SequenceType[]{
+                    new FunctionParameterSequenceType("image", Type.BASE64_BINARY, Cardinality.EXACTLY_ONE, "The image data"),
+                    new FunctionParameterSequenceType("dimension", Type.INTEGER, Cardinality.ZERO_OR_MORE, "The maximum dimension of the scaled image. expressed in pixels (maxheight, maxwidth).  If empty, then the default values are 'maxheight = 100' and 'maxwidth = 100'."),
+                    new FunctionParameterSequenceType("mimeType", Type.STRING, Cardinality.EXACTLY_ONE, "The mime-type of the image")
+            },
+            new FunctionReturnSequenceType(Type.BASE64_BINARY, Cardinality.ZERO_OR_ONE, "the scaled image or an empty sequence if $image is invalid"));
 
-		if(!args[1].isEmpty())
-		{
-			maxHeight = ((IntegerValue) args[1].itemAt(0)).getInt();
-			if(args[1].hasMany())
-				maxWidth = ((IntegerValue) args[1].itemAt(1)).getInt();
-		}
 
-		//get the mime-type
-		String mimeType = args[2].itemAt(0).getStringValue();
-		String formatName = mimeType.substring(mimeType.indexOf("/")+1);
-		
-		//TODO currently ONLY tested for JPEG!!!
-		Image image = null;
-		BufferedImage bImage = null;
-		try (//get the image data
-			 InputStream inputStream = ((BinaryValue) args[0].itemAt(0)).getInputStream() )
-		{
+    /**
+     * ScaleFunction Constructor
+     *
+     * @param context The Context of the calling XQuery
+     */
+    public ScaleFunction(XQueryContext context) {
+        super(context, signature);
+    }
 
-			image = ImageIO.read(inputStream);
-		
-			if(image == null) {
-                            logger.error("Unable to read image data!");
-                            return Sequence.EMPTY_SEQUENCE;
-			}
-			
-			//scale the image
-			bImage = ImageModule.createThumb(image, maxHeight, maxWidth);
-		
-			//get the new scaled image
-			try (final UnsynchronizedByteArrayOutputStream os = new UnsynchronizedByteArrayOutputStream()) {
-				ImageIO.write(bImage, formatName, os);
+    /**
+     * evaluate the call to the xquery scale() function,
+     * it is really the main entry point of this class
+     *
+     * @param args            arguments from the scale() function call
+     * @param contextSequence the Context Sequence to operate on (not used here internally!)
+     * @return A sequence representing the result of the scale() function call
+     * @see org.exist.xquery.BasicFunction#eval(org.exist.xquery.value.Sequence[], org.exist.xquery.value.Sequence)
+     */
+    public Sequence eval(Sequence[] args, Sequence contextSequence) throws XPathException {
+        //was an image and a mime-type speficifed
+        if (args[0].isEmpty() || args[2].isEmpty()) {
+            return Sequence.EMPTY_SEQUENCE;
+        }
 
-				//return the new scaled image data
-				return BinaryValueFromInputStream.getInstance(context, new Base64BinaryValueType(), os.toInputStream(), this);
-			}
-		}
-		catch(Exception e)
-		{
-			throw new XPathException(this, e.getMessage());
-		}
-	}
+        //get the maximum dimensions to scale to
+        int maxHeight = MAXHEIGHT;
+        int maxWidth = MAXWIDTH;
+
+        if (!args[1].isEmpty()) {
+            maxHeight = ((IntegerValue) args[1].itemAt(0)).getInt();
+            if (args[1].hasMany())
+                maxWidth = ((IntegerValue) args[1].itemAt(1)).getInt();
+        }
+
+        //get the mime-type
+        String mimeType = args[2].itemAt(0).getStringValue();
+        String formatName = mimeType.substring(mimeType.indexOf("/") + 1);
+
+        //TODO currently ONLY tested for JPEG!!!
+        Image image = null;
+        BufferedImage bImage = null;
+        try (//get the image data
+             InputStream inputStream = ((BinaryValue) args[0].itemAt(0)).getInputStream()) {
+
+            image = ImageIO.read(inputStream);
+
+            if (image == null) {
+                logger.error("Unable to read image data!");
+                return Sequence.EMPTY_SEQUENCE;
+            }
+
+            //scale the image
+            bImage = ImageModule.createThumb(image, maxHeight, maxWidth);
+
+            //get the new scaled image
+            try (final UnsynchronizedByteArrayOutputStream os = new UnsynchronizedByteArrayOutputStream()) {
+                ImageIO.write(bImage, formatName, os);
+
+                //return the new scaled image data
+                return BinaryValueFromInputStream.getInstance(context, new Base64BinaryValueType(), os.toInputStream(), this);
+            }
+        } catch (Exception e) {
+            throw new XPathException(this, e.getMessage());
+        }
+    }
 }
