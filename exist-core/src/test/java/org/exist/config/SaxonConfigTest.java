77b0c1afe26fafa1da186c40db23fe68c4e6cdca
Alan Paxton <alan.paxton@gmail.com>
2023-04-06 10:37:00 +0100

    [refactor] Rename, add licence header, remove license testing
    Saxon config tests that we find the saxon config file; the part about looking for a license file is not implemented.

--- exist-core/src/test/java/org/exist/config/SaxonConfigTest.java
+++ exist-core/src/test/java/org/exist/config/SaxonConfigTest.java
@@ -1,3 +1,25 @@
+/*
+ * eXist-db Open Source Native XML Database
+ * Copyright (C) 2001 The eXist-db Authors
+ *
+ * info@exist-db.org
+ * http://www.exist-db.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+
 package org.exist.config;
 
 import org.exist.test.ExistEmbeddedServer;
@@ -7,7 +29,7 @@ import org.junit.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-public class SaxonLicenceTest {
+public class SaxonConfigTest {
 
   @ClassRule
   public static final ExistEmbeddedServer existEmbeddedServer = new ExistEmbeddedServer(true, true);
@@ -17,8 +39,7 @@ public class SaxonLicenceTest {
     final var brokerPool = existEmbeddedServer.getBrokerPool();
 
     final var existConfiguration = brokerPool.getConfiguration();
-    assertThat(existConfiguration.getProperty("saxon.configuration")).isEqualTo("saxon-config-for-exist.xml");
-    assertThat(existConfiguration.getProperty("saxon.license")).isEqualTo("saxon-license-for-exist.lic");
+    assertThat(existConfiguration.getProperty("saxon.configuration")).isEqualTo("saxon-config.xml");
 
     final var saxonConfigurationHolder = SaxonConfigurationHolder.GetHolderForBroker(brokerPool);
     final var saxonConfiguration = saxonConfigurationHolder.getConfiguration();
