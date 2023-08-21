4a6313ccc5bd0b96d1f41aa8d0f15a59a4f11359
Adam Retter <adam.retter@googlemail.com>
2023-08-21 15:47:03 -0400

    [ignore] Remove unused import

--- extensions/indexes/lucene/src/main/java/org/exist/xquery/modules/lucene/Query.java
+++ extensions/indexes/lucene/src/main/java/org/exist/xquery/modules/lucene/Query.java
@@ -37,7 +37,6 @@ import javax.annotation.Nullable;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
 import static org.exist.xquery.FunctionDSL.*;
