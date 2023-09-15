55dbb6cadef7a798d3b5a2d5742251ddddf8a54c
Nico Verwer <nico.verwer.jbp@gmail.com>
2023-09-15 16:36:17 +0200

    fix for #5028
    The Java code of DirectoryList throws an IllegalStateException when a non-existent directory is supplied as its first argument, which is an unchecked exception and therefore not caught in Java.

--- extensions/modules/file/src/main/java/org/exist/xquery/modules/file/DirectoryList.java
+++ extensions/modules/file/src/main/java/org/exist/xquery/modules/file/DirectoryList.java
@@ -172,6 +172,8 @@ public class DirectoryList extends BasicFunction {
             builder.endElement();
 
             return (NodeValue) builder.getDocument().getDocumentElement();
+        } catch (final IllegalStateException e) {
+          throw new XPathException(this, e.getMessage());
         } catch (final IOException e) {
             throw new XPathException(this, e.getMessage());
         } finally {
