e4c030f9e3c9118a109dbb9d57a89b6d6d92fa48
Juri Leino <github@line-o.de>
2024-04-09 15:13:01 +0200

    [bugfix] NPE in NativeBroker.defragXMLResource
    A regression introduced in c553667 causes a NullPointerException to be thrown whenever an XML-resource
    needs to be defragmented.
    This will be triggered on frequent writes to any XML-resource and will effectively remove the file
    from the database and from any indexes.

--- exist-core/src/main/java/org/exist/storage/NativeBroker.java
+++ exist-core/src/main/java/org/exist/storage/NativeBroker.java
@@ -3172,7 +3172,7 @@ public class NativeBroker implements DBBroker {
                 }
             }.run();
             // create a copy of the old doc to copy the nodes into it
-            final DocumentImpl tempDoc = new DocumentImpl(null, null, doc.getCollection(), doc.getDocId(), doc.getFileURI());
+            final DocumentImpl tempDoc = new DocumentImpl(null, pool, doc.getCollection(), doc.getDocId(), doc.getFileURI());
             tempDoc.copyOf(this, doc, doc);
             final StreamListener listener = getIndexController().getStreamListener(doc, ReindexMode.STORE);
             // copy the nodes
