5a3e16b3c470fcb8f8b2bc7db338b8262f201115
Dannes Wessels <dizzzz@users.noreply.github.com>
2025-04-13 21:29:41 +0200

    [refactor] Isolate classloader in separate package as it is externally used too

--- exist-start/src/main/java/org/exist/start/classloader/Classpath.java
+++ exist-start/src/main/java/org/exist/start/classloader/Classpath.java
@@ -36,7 +36,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.exist.start;
+package org.exist.start.classloader;
 
 import java.io.File;
 import java.net.MalformedURLException;
