1a1fefebbe5bd81438b9e0c99ec63c5181af970d
duncdrum <d.paterson@me.com>
2025-09-27 16:47:57 +0200

    [fix] BinaryTokenStream

---
+++ extensions/indexes/lucene/src/main/java/org/exist/indexing/lucene/ByteTermAttributeImpl.java
@@ -0,0 +1,8 @@
+package org.exist.indexing.lucene;
+
+import org.apache.lucene.util.AttributeImpl;
+
+public abstract class ByteTermAttributeImpl extends AttributeImpl {
+    public void fillBytesRef() {
+    }
+}
