f5d1b089e283dc29d4cf854d075a81317a7f5e81
duncdrum <d.paterson@me.com>
2026-02-27 11:12:52 +0100

    [fix] Handle missing index configuration in MutableCollection more gracefully
    - Updated the logic in MutableCollection to fall back to the broker's index configuration when the collection's configuration lacks an <index> element, preventing potential NullPointerExceptions.
    - Added a regression test to verify that ft:query does not throw an NPE when the collection.xconf is missing the index element or has an empty index.
    
    closes #2948

--- exist-core/src/main/java/org/exist/collections/MutableCollection.java
+++ exist-core/src/main/java/org/exist/collections/MutableCollection.java
@@ -1962,7 +1962,9 @@ public class MutableCollection implements Collection {
             return broker.getIndexConfiguration();
         }
         //... otherwise return the general config (the broker's one)
-        return conf.getIndexConfiguration();
+        // Fall back to broker config when collection.xconf has no <index> element (fixes #2948)
+        final IndexSpec spec = conf.getIndexConfiguration();
+        return (spec != null) ? spec : broker.getIndexConfiguration();
     }
 
     @Override
