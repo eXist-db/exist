96ca354005cb4535d64a367e143f16085af6ffa5
Juri Leino <github@line-o.de>
2026-03-10 12:00:10 +0100

    [test] improve InspectModuleTest
    Testcases are much more concise and assert more properties.

--- exist-core/src/test/java/org/exist/xquery/functions/inspect/InspectModuleTest.java
+++ exist-core/src/test/java/org/exist/xquery/functions/inspect/InspectModuleTest.java
@@ -30,6 +30,7 @@ import org.exist.security.PermissionDeniedException;
 import org.exist.storage.BrokerPool;
 import org.exist.storage.DBBroker;
 import org.exist.storage.lock.Lock;
+import org.exist.storage.txn.TransactionException;
 import org.exist.storage.txn.Txn;
 import org.exist.test.ExistEmbeddedServer;
 import org.exist.util.LockException;
@@ -49,9 +50,7 @@ import java.io.IOException;
 import java.util.Optional;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class InspectModuleTest {
 
@@ -60,172 +59,120 @@ public class InspectModuleTest {
 
     private static final XmldbURI TEST_COLLECTION = XmldbURI.ROOT_COLLECTION_URI.append("test-inspectModule");
     private static final XmldbURI TEST_MODULE = XmldbURI.create("test.xqm");
-    private static final String MODULE =
-            "xquery version \"1.0\";\n" +
-            "module namespace x = \"http://xyz.com\";\n" +
-            "\n" +
-            "(:~\n" +
-            " : Some description.\n" +
-            " : @return taxonomy[@type = \"reign\"]\n" +
-            " :)\n" +
-            "declare function x:fun1() as xs:string {\n" +
-            "  \"hello from fun1\"\n" +
-            "};\n" +
-            "\n" +
-            "(:~\n" +
-            " : Some other description.\n" +
-            " : \n" +
-            " : @param one first parameter\n" +
-            " : @param two second parameter\n" +
-            " : \n" +
-            " : @return our result\n" +
-            " :)\n" +
-            "declare function x:fun2($one as xs:int, $two as xs:float) as xs:string {\n" +
-            "  \"hello from fun2\"\n" +
-            "};\n" +
-            "\n" +
-            "(:~\n" +
-            " : This is a multiline description and therefore\n" +
-            " : spans multiple\n" +
-            " : lines.\n" +
-            " : \n" +
-            " : @return another result\n" +
-            " :)\n" +
-            "declare function x:fun3() {\n" +
-            "  \"hello from fun3\"\n" +
-            "};\n" +
-            "\n" +
-            "(:~\n" +
-            " : An annotated function.\n" +
-            " : \n" +
-            " : @return another result\n" +
-            " :)\n" +
-            "declare %public %x:path(\"/x/y/z\") function x:fun4() {\n" +
-            "  \"hello from fun4\"\n" +
-            "};\n";
+    private static final XmldbURI MODULE_LOAD_PATH = TEST_COLLECTION.append(TEST_MODULE).toCollectionPathURI();
+    private static final String INSPECT_MODULE = """
+            xquery version "3.1";
+            module namespace x = "http://xyz.com";
+
+            (:~
+             : Some description.
+             : @return taxonomy[@type = "reign"]
+             :)
+            declare function x:fun1() as xs:string {
+              "hello from fun1"
+            };
+            
+            (:~
+             : Some other description.
+             :
+             : @param one first parameter
+             : @param two second parameter
+             :
+             : @return our result
+             :)
+            declare function x:fun2($one as xs:int, $two as xs:float) as xs:string {
+              "hello from fun2"
+            };
+            
+            (:~
+             : This is a multiline description and therefore
+             : spans multiple
+             : lines.
+             :
+             : @return another result
+             :)
+            declare function x:fun3() {
+              "hello from fun3"
+            };
+            
+            (:~
+             : An annotated function.
+             :
+             : @return another result
+             :)
+            declare %public %x:path("/x/y/z") function x:fun4() {
+              "hello from fun4"
+            };
+            """;
+    private static final String MAIN_MODULE = """
+            import module namespace inspect = "http://exist-db.org/xquery/inspection";
+            
+            inspect:inspect-module(xs:anyURI("xmldb:exist://%s"))/function[@name eq "%s"]
+            """;
 
     @Ignore("https://github.com/eXist-db/exist/issues/1386")
     @Test
     public void xqDoc_withAtSignInline() throws PermissionDeniedException, XPathException, EXistException {
-        final BrokerPool pool = existEmbeddedServer.getBrokerPool();
-        final XQuery xqueryService = pool.getXQueryService();
-        try(final DBBroker broker = pool.get(Optional.of(pool.getSecurityManager().getSystemSubject()));
-                final Txn transaction = pool.getTransactionManager().beginTransaction()) {
+        final String functionName = "x:fun1";
+        final String expectedDescription = "Some description.";
+        final String[] expectedAnnotations = {};
+        final String[] expectedAnnotationValues = {};
+        final String[] expectedParameters = {};
+        final String expectedReturn = "taxonomy[@type = \"reign\"]";
 
-            final String query =
-                    "import module namespace inspect = \"http://exist-db.org/xquery/inspection\";\n" +
-                    "inspect:inspect-module(xs:anyURI(\"xmldb:exist://" + TEST_COLLECTION.append(TEST_MODULE).toCollectionPathURI() + "\"))\n" +
-                    "/function[@name eq \"x:fun1\"]";
-
-            final Sequence result = xqueryService.execute(broker, query, null);
-
-            assertNotNull(result);
-            assertEquals(1, result.getItemCount());
-            final Item item1 = result.itemAt(0);
-            assertTrue(item1 instanceof ElementImpl);
-
-            final Element function = (Element)item1;
-
-            final NodeList descriptions = function.getElementsByTagName("description");
-            assertEquals(1, descriptions.getLength());
-            assertEquals("Some description.", descriptions.item(0).getFirstChild().getTextContent());
-
-            final NodeList arguments = function.getElementsByTagName("argument");
-            assertEquals(0, arguments.getLength());
-
-            final NodeList returns = function.getElementsByTagName("returns");
-            assertEquals(1, returns.getLength());
-            assertEquals("taxonomy[@type = \"reign\"]", returns.item(0).getFirstChild().getTextContent());
-
-            transaction.commit();
-        }
+        assertInspection(functionName, expectedDescription, expectedParameters, expectedAnnotations, expectedAnnotationValues, expectedReturn);
     }
 
     @Test
     public void xqDoc_withParamsAndReturn() throws PermissionDeniedException, XPathException, EXistException {
-        final BrokerPool pool = existEmbeddedServer.getBrokerPool();
-        final XQuery xqueryService = pool.getXQueryService();
-        try(final DBBroker broker = pool.get(Optional.of(pool.getSecurityManager().getSystemSubject()));
-            final Txn transaction = pool.getTransactionManager().beginTransaction()) {
+        final String functionName = "x:fun2";
+        final String expectedDescription = "Some other description.";
+        final String[] expectedAnnotations = {};
+        final String[] expectedAnnotationValues = {};
+        final String[] expectedParameters = { "first parameter", "second parameter" };
+        final String expectedReturn = "our result";
 
-            final String query =
-                    "import module namespace inspect = \"http://exist-db.org/xquery/inspection\";\n" +
-                            "inspect:inspect-module(xs:anyURI(\"xmldb:exist://" + TEST_COLLECTION.append(TEST_MODULE).toCollectionPathURI() + "\"))\n" +
-                            "/function[@name eq \"x:fun2\"]";
-
-            final Sequence result = xqueryService.execute(broker, query, null);
-
-            assertNotNull(result);
-            assertEquals(1, result.getItemCount());
-            final Item item1 = result.itemAt(0);
-            assertTrue(item1 instanceof ElementImpl);
-
-            final Element function = (Element)item1;
-
-            final NodeList descriptions = function.getElementsByTagName("description");
-            assertEquals(1, descriptions.getLength());
-            assertEquals("Some other description.", descriptions.item(0).getFirstChild().getNodeValue());
-
-            final NodeList arguments = function.getElementsByTagName("argument");
-            assertEquals(2, arguments.getLength());
-            assertEquals("first parameter", arguments.item(0).getFirstChild().getNodeValue());
-            assertEquals("second parameter", arguments.item(1).getFirstChild().getNodeValue());
-
-            final NodeList returns = function.getElementsByTagName("returns");
-            assertEquals(1, returns.getLength());
-            assertEquals("our result", returns.item(0).getFirstChild().getNodeValue());
-
-            transaction.commit();
-        }
+        assertInspection(functionName, expectedDescription, expectedParameters, expectedAnnotations, expectedAnnotationValues, expectedReturn);
     }
 
     @Test
-    public void xqDoc_multilineDesciption() throws PermissionDeniedException, XPathException, EXistException {
-        final BrokerPool pool = existEmbeddedServer.getBrokerPool();
-        final XQuery xqueryService = pool.getXQueryService();
-        try(final DBBroker broker = pool.get(Optional.of(pool.getSecurityManager().getSystemSubject()));
-            final Txn transaction = pool.getTransactionManager().beginTransaction()) {
+    public void xqDoc_multilineDescription() throws PermissionDeniedException, XPathException, EXistException {
+        final String functionName = "x:fun3";
+        final String expectedDescription = "This is a multiline description and therefore\n spans multiple\n lines.";
+        final String[] expectedAnnotations = {};
+        final String[] expectedAnnotationValues = {};
+        final String[] expectedParameters = {};
+        final String expectedReturn = "another result";
 
-            final String query =
-                    "import module namespace inspect = \"http://exist-db.org/xquery/inspection\";\n" +
-                            "inspect:inspect-module(xs:anyURI(\"xmldb:exist://" + TEST_COLLECTION.append(TEST_MODULE).toCollectionPathURI() + "\"))\n" +
-                            "/function[@name eq \"x:fun3\"]";
-
-            final Sequence result = xqueryService.execute(broker, query, null);
-
-            assertNotNull(result);
-            assertEquals(1, result.getItemCount());
-            final Item item1 = result.itemAt(0);
-            assertTrue(item1 instanceof ElementImpl);
-
-            final Element function = (Element)item1;
-
-            final NodeList descriptions = function.getElementsByTagName("description");
-            assertEquals(1, descriptions.getLength());
-            assertEquals("This is a multiline description and therefore\n spans multiple\n lines.", descriptions.item(0).getFirstChild().getNodeValue());
-
-            final NodeList arguments = function.getElementsByTagName("argument");
-            assertEquals(0, arguments.getLength());
-
-            final NodeList returns = function.getElementsByTagName("returns");
-            assertEquals(1, returns.getLength());
-            assertEquals("another result", returns.item(0).getFirstChild().getNodeValue());
-
-            transaction.commit();
-        }
+        assertInspection(functionName, expectedDescription, expectedParameters, expectedAnnotations, expectedAnnotationValues, expectedReturn);
     }
 
     @Test
     public void xqDoc_onAnnotatedFunction() throws PermissionDeniedException, XPathException, EXistException {
+        final String functionName = "x:fun4";
+        final String expectedDescription = "An annotated function.";
+        final String[] expectedAnnotations = { "public", "x:path" };
+        final String[] expectedAnnotationValues = { null, "/x/y/z" };
+        final String[] expectedParameters = {};
+        final String expectedReturn = "another result";
+
+        assertInspection(functionName, expectedDescription, expectedParameters, expectedAnnotations, expectedAnnotationValues, expectedReturn);
+    }
+
+    private static void assertInspection(
+            String functionName,
+            String expectedDescription,
+            String[] expectedParameters,
+            String[] expectedAnnotations,
+            String[] expectedAnnotationValues,
+            String expectedReturn
+    ) throws XPathException, PermissionDeniedException, EXistException {
         final BrokerPool pool = existEmbeddedServer.getBrokerPool();
         final XQuery xqueryService = pool.getXQueryService();
         try(final DBBroker broker = pool.get(Optional.of(pool.getSecurityManager().getSystemSubject()));
             final Txn transaction = pool.getTransactionManager().beginTransaction()) {
 
-            final String query =
-                    "import module namespace inspect = \"http://exist-db.org/xquery/inspection\";\n" +
-                            "inspect:inspect-module(xs:anyURI(\"xmldb:exist://" + TEST_COLLECTION.append(TEST_MODULE).toCollectionPathURI() + "\"))\n" +
-                            "/function[@name eq \"x:fun4\"]";
+            final String query = MAIN_MODULE.formatted(MODULE_LOAD_PATH, functionName);
 
             final Sequence result = xqueryService.execute(broker, query, null);
 
@@ -238,20 +185,29 @@ public class InspectModuleTest {
 
             final NodeList descriptions = function.getElementsByTagName("description");
             assertEquals(1, descriptions.getLength());
-            assertEquals("An annotated function.", descriptions.item(0).getFirstChild().getNodeValue());
-
-            final NodeList annotations = function.getElementsByTagName("annotation");
-            assertEquals(2, annotations.getLength());
-            assertEquals("public", ((Element)annotations.item(0)).getAttribute("name"));
-            assertEquals("x:path", ((Element)annotations.item(1)).getAttribute("name"));
-            assertEquals("/x/y/z", annotations.item(1).getFirstChild().getFirstChild().getNodeValue());
+            assertEquals(expectedDescription, descriptions.item(0).getFirstChild().getNodeValue());
 
             final NodeList arguments = function.getElementsByTagName("argument");
-            assertEquals(0, arguments.getLength());
+            assertEquals(expectedParameters.length, arguments.getLength());
+            for (int p = 0; p < expectedParameters.length; p++) {
+                assertEquals(expectedParameters[p], arguments.item(p).getFirstChild().getNodeValue());
+            }
+
+            final NodeList annotations = function.getElementsByTagName("annotation");
+            assertEquals(expectedAnnotations.length, annotations.getLength());
+            for (int a = 0; a < expectedAnnotations.length; a++) {
+                final Element annotation = (Element) annotations.item(a);
+                assertEquals(expectedAnnotations[a], annotation.getAttribute("name"));
+                if (expectedAnnotationValues[a] == null) {
+                    assertNull(annotation.getFirstChild());
+                } else {
+                    assertEquals(expectedAnnotationValues[a], annotation.getFirstChild().getFirstChild().getNodeValue());
+                }
+            }
 
             final NodeList returns = function.getElementsByTagName("returns");
             assertEquals(1, returns.getLength());
-            assertEquals("another result", returns.item(0).getFirstChild().getNodeValue());
+            assertEquals(expectedReturn, returns.item(0).getFirstChild().getNodeValue());
 
             transaction.commit();
         }
@@ -265,8 +221,7 @@ public class InspectModuleTest {
 
             final Collection testCollection = broker.getOrCreateCollection(transaction, TEST_COLLECTION);
 
-            broker.storeDocument(transaction, TEST_MODULE, new StringInputSource(MODULE.getBytes(UTF_8)), MimeType.XQUERY_TYPE, testCollection);
-
+            broker.storeDocument(transaction, TEST_MODULE, new StringInputSource(INSPECT_MODULE.getBytes(UTF_8)), MimeType.XQUERY_TYPE, testCollection);
             broker.saveCollection(transaction, testCollection);
 
             transaction.commit();
