04e2e5621b7d438d40d59507c164fcd87ca5298d
Dannes Wessels <dizzzz@gmail.com>
2026-05-08 21:22:54 +0200

    [refactor] make code a bit more readable

--- extensions/modules/sql/src/main/java/org/exist/xquery/modules/sql/ExecuteFunction.java
+++ extensions/modules/sql/src/main/java/org/exist/xquery/modules/sql/ExecuteFunction.java
@@ -185,8 +185,8 @@ public class ExecuteFunction extends BasicFunction {
 
         // get the Connection
         final long connectionUID = ((IntegerValue) args[0].itemAt(0)).getLong();
-        final Connection con = SQLModule.retrieveConnection(context, connectionUID);
-        if (con == null) {
+        final Connection connection = SQLModule.retrieveConnection(context, connectionUID);
+        if (connection == null) {
             throw new XPathException(this, "No such SQL Connection");
         }
 
@@ -194,7 +194,7 @@ public class ExecuteFunction extends BasicFunction {
 
         //setup the SQL statement
         String sql = null;
-        Statement stmt = null;
+        Statement statement = null;
 
         try {
             final boolean makeNodeFromColumnName;
@@ -207,7 +207,7 @@ public class ExecuteFunction extends BasicFunction {
 
                 // get the static SQL statement
                 sql = args[1].getStringValue();
-                stmt = con.createStatement();
+                statement = connection.createStatement();
                 makeNodeFromColumnName = ((BooleanValue) args[2].itemAt(0)).effectiveBooleanValue();
                 if (args.length == 5) {
                     namespaceUri = args[3].itemAt(0).getStringValue();
@@ -219,7 +219,7 @@ public class ExecuteFunction extends BasicFunction {
                 }
 
                 //execute the static SQL statement
-                executeResult = stmt.execute(sql);
+                executeResult = statement.execute(sql);
 
             } else if (args.length == 4 || args.length == 6) {
                     //get the prepared statement
@@ -229,9 +229,9 @@ public class ExecuteFunction extends BasicFunction {
                         throw new XPathException(this, "No such SQL PreparedStatement");
                     }
                     sql = stmtWithSQL.getSql();
-                    stmt = stmtWithSQL.getStmt();
+                    statement = stmtWithSQL.getStmt();
 
-                    if (stmt.getConnection() != con) {
+                    if (statement.getConnection() != connection) {
                         throw new XPathException(this, "SQL Connection does not match that used for creating the PreparedStatement");
                     }
 
@@ -247,18 +247,18 @@ public class ExecuteFunction extends BasicFunction {
 
                     if (!args[2].isEmpty()) {
                         parametersElement = (Element) args[2].itemAt(0);
-                        setParametersOnPreparedStatement(stmt, parametersElement);
+                        setParametersOnPreparedStatement(statement, parametersElement);
                     }
 
                     //execute the PreparedStatement
-                    executeResult = ((PreparedStatement) stmt).execute();
+                    executeResult = ((PreparedStatement) statement).execute();
 
             } else {
                 throw new XPathException(this, "Unknown function call: " + getSignature());
             }
 
             // return the XML result set
-            return resultAsElement(makeNodeFromColumnName, namespacePrefix, namespaceUri, executeResult, stmt, this);
+            return resultAsElement(makeNodeFromColumnName, namespacePrefix, namespaceUri, executeResult, statement, this);
 
         } catch (final SQLException sqle) {
             LOG.error("sql:execute() Caught SQLException \"{}\" for SQL: \"{}\"", sqle.getMessage(), sql, sqle);
@@ -266,9 +266,9 @@ public class ExecuteFunction extends BasicFunction {
 
         } finally {
             // if it's not a prepared statement then close it
-            if (stmt != null && !(stmt instanceof PreparedStatement)) {
+            if (statement != null && !(statement instanceof PreparedStatement)) {
                 try {
-                    stmt.close();
+                    statement.close();
                 } catch (final SQLException se) {
                     LOG.warn("Unable to close JDBC PreparedStatement: {}", se.getMessage(), se);
                 }
