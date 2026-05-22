27e901fb742bbf623b0b83f35bcab7c7b378601b
duncdrum <d.paterson@me.com>
2026-05-22 10:23:46 +0200

    [bugfix] work around Jetty 12.1 PathResource Windows resolve regression
    Jetty 12.1 resolves sub-paths with path.resolve(uri.getPath()), which throws
    InvalidPathException for Windows drive URIs (/D:/...). Exploded test webapps
    fail in WebAppContext.getWebInf() with available=false and 503 on every path.
    
    WindowsPathResource wraps PathResource on Windows and restores Jetty 12.0
    resolve behaviour via Paths.get(resolvedUri). Enable throwUnavailableOnStartupException
    so startup failures surface immediately in CI.

--- exist-core/src/main/java/org/exist/jetty/WebAppContext.java
+++ exist-core/src/main/java/org/exist/jetty/WebAppContext.java
@@ -21,24 +21,37 @@
  */
 package org.exist.jetty;
 
+import org.eclipse.jetty.util.resource.Resource;
+import org.eclipse.jetty.util.resource.ResourceFactory;
 import org.exist.storage.BrokerPool;
 
 /**
- * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
+ * eXist {@link org.eclipse.jetty.ee10.webapp.WebAppContext} with Windows path handling for
+ * Jetty 12.1 ({@link WindowsPathResource}).
  *
+ * @author <a href="mailto:shabanovd@gmail.com">Dmitriy Shabanov</a>
  */
 public class WebAppContext extends org.eclipse.jetty.ee10.webapp.WebAppContext {
 
     @Override
-	public String toString() {
-		return "eXist-db Open Source Native XML Database";
-	}
+    public String toString() {
+        return "eXist-db Open Source Native XML Database";
+    }
 
     @Override
-	protected void doStop() throws Exception {
-		super.doStop();
+    public void setBaseResource(final Resource baseResource) {
+        super.setBaseResource(WindowsPathResource.wrapIfNeeded(baseResource, ResourceFactory.of(this)));
+    }
 
-		BrokerPool.stopAll(true);
-	}
+    @Override
+    public Resource newResource(final String urlOrPath) {
+        return WindowsPathResource.wrapIfNeeded(super.newResource(urlOrPath), ResourceFactory.of(this));
+    }
 
+    @Override
+    protected void doStop() throws Exception {
+        super.doStop();
+
+        BrokerPool.stopAll(true);
+    }
 }
