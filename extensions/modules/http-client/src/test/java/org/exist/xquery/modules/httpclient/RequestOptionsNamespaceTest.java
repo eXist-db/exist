eafb9c215b9cfab643b951654b9989c7012b07d2
Dannes Wessels <dizzzz@gmail.com>
2026-06-20 16:04:28 +0200

    [refactor] Enhance attribute parsing in HttpClient: add namespace support for exist specific attributes.

---
+++ extensions/modules/http-client/src/test/java/org/exist/xquery/modules/httpclient/RequestOptionsNamespaceTest.java
@@ -0,0 +1,95 @@
+/*
+ * eXist-db Open Source Native XML Database
+ * Copyright (C) 2001 The eXist-db Authors
+ *
+ * info@exist-db.org
+ * http://www.exist-db.org
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ */
+package org.exist.xquery.modules.httpclient;
+
+import org.exist.Namespaces;
+import org.exist.xquery.XPathException;
+import org.exist.xquery.modules.httpclient.config.RequestOptions;
+import org.junit.Test;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class RequestOptionsNamespaceTest {
+
+    @Test
+    public void namespacedAttribute() throws XPathException, ParserConfigurationException {
+        final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setNamespaceAware(true);
+        final DocumentBuilder db = dbf.newDocumentBuilder();
+        final Document doc = db.newDocument();
+        final Element reqElem = doc.createElementNS(HttpClientModule.NAMESPACE_URI, "http:request");
+        reqElem.setAttributeNS(HttpClientModule.NAMESPACE_URI, "http:follow-redirect", "false");
+        doc.appendChild(reqElem);
+
+        final RequestOptions options = RequestOptionsParser.parse(reqElem);
+        assertFalse("follow-redirect should be false when set via namespaced attribute", options.requestOptions().followRedirect());
+    }
+
+    @Test
+    public void existNamespacedAttribute() throws XPathException, ParserConfigurationException {
+        final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setNamespaceAware(true);
+        final DocumentBuilder db = dbf.newDocumentBuilder();
+        final Document doc = db.newDocument();
+        final Element reqElem = doc.createElementNS(HttpClientModule.NAMESPACE_URI, "http:request");
+        reqElem.setAttributeNS(Namespaces.EXIST_NS, "exist:follow-redirect", "false");
+        doc.appendChild(reqElem);
+
+        final RequestOptions options = RequestOptionsParser.parse(reqElem);
+        assertFalse("follow-redirect should be false when set via exist-namespaced attribute", options.requestOptions().followRedirect());
+    }
+
+    @Test
+    public void autoAcceptEncodingExistNamespaced() throws XPathException, ParserConfigurationException {
+        final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setNamespaceAware(true);
+        final DocumentBuilder db = dbf.newDocumentBuilder();
+        final Document doc = db.newDocument();
+        final Element reqElem = doc.createElementNS(HttpClientModule.NAMESPACE_URI, "http:request");
+        reqElem.setAttributeNS(Namespaces.EXIST_NS, "exist:auto-accept-encoding", "false");
+        doc.appendChild(reqElem);
+
+        final RequestOptions options = RequestOptionsParser.parse(reqElem);
+        assertFalse("auto-accept-encoding should be false when set via exist-namespaced attribute", options.requestOptions().autoAcceptEncoding());
+    }
+
+    @Test
+    public void autoAcceptEncodingNoNamespaceIgnored() throws XPathException, ParserConfigurationException {
+        final DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        dbf.setNamespaceAware(true);
+        final DocumentBuilder db = dbf.newDocumentBuilder();
+        final Document doc = db.newDocument();
+        final Element reqElem = doc.createElementNS(HttpClientModule.NAMESPACE_URI, "http:request");
+        reqElem.setAttribute("auto-accept-encoding", "false");
+        doc.appendChild(reqElem);
+
+        final RequestOptions options = RequestOptionsParser.parse(reqElem);
+        assertTrue("auto-accept-encoding should be true (default) when set via no-namespace attribute", options.requestOptions().autoAcceptEncoding());
+    }
+}
